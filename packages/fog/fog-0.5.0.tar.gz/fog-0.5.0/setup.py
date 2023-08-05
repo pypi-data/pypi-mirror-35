eb07ad24a28e25996e3fdb95e7a68de3637930bf
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-05 20:13:44 +0000

    Add package fog/0.5.0/fog-0.5.0.tar.gz

---
+++ packages/fog/fog-0.5.0.tar.gz/fog-0.5.0/setup.py
@@ -0,0 +1,29 @@
+from setuptools import setup, find_packages
+from Cython.Build import cythonize
+
+with open('./README.md', 'r') as f:
+    long_description = f.read()
+
+setup(name='fog',
+    version='0.5.0',
+    description='A fuzzy matching & clustering library for python.',
+    long_description=long_description,
+    long_description_content_type='text/markdown',
+    url='http://github.com/Yomguithereal/fog',
+    license='MIT',
+    author='Guillaume Plique',
+    author_email='kropotkinepiotr@gmail.com',
+    keywords='fuzzy',
+    python_requires='>=3',
+    packages=find_packages(exclude=['experiments', 'test']),
+    ext_modules=cythonize('fog/metrics/*.pyx'),
+    package_data={'docs': ['README.md']},
+    install_requires=[
+        'dill==0.2.7.1',
+        'phylactery==0.1.1',
+        'Unidecode==1.0.22'
+    ],
+    entry_points={
+        'console_scripts': ['fog=fog.cli:main']
+    },
+    zip_safe=True)
