170594e7fd9138de728b1a6d6f1ffeee47dfb4ca
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-05 21:11:21 +0000

    Add package mstatistics/0.1.0/mstatistics-0.1.0.tar.gz

---
+++ packages/mstatistics/mstatistics-0.1.0.tar.gz/mstatistics-0.1.0/setup.py
@@ -0,0 +1,11 @@
+from setuptools import setup
+
+setup(name='mstatistics',
+      version='0.1.0',
+      description='Provide a robust statistical library for Python to be used in conjunction with NumPy, SciPy, Pandas, and other common data science packages.',
+      url='http://github.com/MaxMifkovic/mstatistics',
+      author='Max Mifkovic',
+      author_email='maxmifkovic@gmail.com',
+      license='MIT',
+      packages=['mstatistics'],
+      zip_safe=False)
