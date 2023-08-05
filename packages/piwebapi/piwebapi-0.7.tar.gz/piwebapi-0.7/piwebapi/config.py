0f342d0ca731e436ba8e427a8a2c2d3869479177
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-05 22:11:17 +0000

    Add package piwebapi/0.7/piwebapi-0.7.tar.gz

---
+++ packages/piwebapi/piwebapi-0.7.tar.gz/piwebapi-0.7/piwebapi/config.py
@@ -0,0 +1,7 @@
+# module level persistence
+global_persistence = 1000000
+# disable cache
+enable_cache = True
+# webapi server address
+webapi_server = "http://10.10.143.28/piwebapi/"
+auth_config = ('','')
