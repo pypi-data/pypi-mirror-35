8f33177c6e9756dea3eb9566ae1c486190e9c6f8
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-05 21:09:24 +0000

    Add package sspals/0.0.7/sspals-0.0.7-py2.7.egg

---
+++ packages/sspals/sspals-0.0.7-py2.7.egg/sspals/__init__.py
@@ -0,0 +1,7 @@
+#! python
+from __future__ import print_function, division  # Require Python 2.6 or later
+
+from .sim import sim
+from .chmx import chmx
+from .cfd import cfd
+from .sspals import sspals, chmx_sspals, signal
