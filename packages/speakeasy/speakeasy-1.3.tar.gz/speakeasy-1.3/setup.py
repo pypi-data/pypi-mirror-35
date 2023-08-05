f3665baa3832467c2c50375eb5a1502c7545eba7
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-05 18:10:24 +0000

    Add package speakeasy/1.3/speakeasy-1.3.tar.gz

---
+++ packages/speakeasy/speakeasy-1.3.tar.gz/speakeasy-1.3/setup.py
@@ -0,0 +1,27 @@
+from setuptools import setup
+import speakeasy
+
+setup(
+    name='speakeasy',
+    version=speakeasy.__version__,
+    description='Metrics aggregation server',
+    author='Eric Wong',
+    install_requires=[
+        'argparse',
+        'pyzmq',
+        'ujson',
+    ],
+    packages=['speakeasy', 'speakeasy.emitter'],
+    scripts=['bin/speakeasy'],
+    license='Apache License 2.0',
+    url='https://github.com/etdub/speakeasy',
+    classifiers=[
+        'Intended Audience :: Developers',
+        'Natural Language :: English',
+        'Programming Language :: Python',
+        'Programming Language :: Python :: 2.7',
+        'Programming Language :: Python :: 3',
+        'Programming Language :: Python :: 3.3',
+        'License :: OSI Approved :: Apache Software License',
+    ],
+)
