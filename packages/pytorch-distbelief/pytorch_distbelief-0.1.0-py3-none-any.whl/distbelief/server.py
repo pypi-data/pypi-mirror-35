a346a36fd9d549f4b7e9df60cbef50c98e3d9ec0
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-05 21:10:48 +0000

    Add package pytorch-distbelief/0.1.0/pytorch_distbelief-0.1.0-py3-none-any.whl

---
+++ packages/pytorch-distbelief/pytorch_distbelief-0.1.0-py3-none-any.whl/distbelief/server.py
@@ -0,0 +1,33 @@
+# 
+"""
+Parameter server for distbelief
+"""
+import logging
+import torch
+import torch.optim
+from distbelief.utils.messaging import MessageCode, MessageListener, send_message
+from distbelief.utils.serialization import ravel_model_params, unravel_model_params
+
+_LOGGER = logging.getLogger(__name__)
+
+class ParameterServer(MessageListener):
+    """ParameterServer"""
+    def __init__(self, model):
+        _LOGGER.info("Creating ParameterServer")
+        self.parameter_shard = torch.rand(ravel_model_params(model).numel())
+        self.model = model
+        #init superclass
+        super().__init__(model)
+
+    def receive(self, sender, message_code, parameter):
+        print("Processing message: {} from sender {}".format(message_code.name, sender))
+
+        if message_code == MessageCode.ParameterUpdate:
+            #be sure to clone here
+            self.parameter_shard = parameter.clone()
+
+        elif message_code == MessageCode.ParameterRequest:
+            send_message(MessageCode.ParameterUpdate, self.parameter_shard, dst=sender)    
+
+        elif message_code == MessageCode.GradientUpdate:
+            self.parameter_shard.add_(parameter)
