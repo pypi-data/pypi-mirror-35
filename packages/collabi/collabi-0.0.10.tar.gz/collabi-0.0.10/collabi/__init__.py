757605ee10c74227060426429293675852519054
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-05 18:09:59 +0000

    Add package collabi/0.0.10/collabi-0.0.10.tar.gz

---
+++ packages/collabi/collabi-0.0.10.tar.gz/collabi-0.0.10/collabi/__init__.py
@@ -0,0 +1,8 @@
+"""UnityCloud Collaboration Command Line Interface
+"""
+
+__author__ = 'Unity'
+__version__ = '0.0.10'
+__license__ = 'BSD'
+
+__all__ = ['CloudConfig', 'Rest', 'Core', 'Auth', 'Collab']
