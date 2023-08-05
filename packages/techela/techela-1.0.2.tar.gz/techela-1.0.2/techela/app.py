93fd1ea19bd7a78970537d23959f9a63127cb342
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-05 22:13:00 +0000

    Add package techela/1.0.2/techela-1.0.2.tar.gz

---
+++ packages/techela/techela-1.0.2.tar.gz/techela-1.0.2/techela/app.py
@@ -0,0 +1,9 @@
+import techela
+import threading
+import webbrowser
+
+port = 5543
+url = "http://127.0.0.1:{0}".format(port)
+
+threading.Timer(1.25, lambda: webbrowser.open(url)).start()
+techela.app.run(port=port, debug=True, use_reloader=False)
