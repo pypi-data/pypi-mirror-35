ee856e62f9a7cb394df1a6b825a8d3c1ec21ad1a
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-05 22:13:09 +0000

    Add package micropython-wiegand/1.1.0/micropython-wiegand-1.1.0.zip

---
+++ packages/micropython-wiegand/micropython-wiegand-1.1.0.zip/micropython-wiegand-1.1.0/setup.py
@@ -0,0 +1,40 @@
+
+from setuptools import setup
+
+setup(
+    name='micropython-wiegand',
+
+    description='MicroPython class to read from a Wiegand card reader',
+    long_description='MicroPython class to read from a Wiegand card reader',
+
+    # The project's main homepage.
+    url='https://github.com/pjz/micropython-wiegand',
+
+    # Author details
+    author='Paul Jimenez',
+    author_email='pj@place.org',
+
+    # Choose your license
+    license='LGPLv3+',
+
+    # See https://pypi.python.org/pypi?%3Aaction=list_classifiers
+    classifiers=[
+        'Development Status :: 4 - Beta',
+        'Intended Audience :: Developers',
+        'Topic :: Software Development :: Libraries',
+        'Topic :: System :: Hardware',
+        'License :: OSI Approved :: GNU Lesser General Public License v3 or later (LGPLv3+)',
+        'Programming Language :: Python :: 3',
+        'Programming Language :: Python :: 3.4',
+        'Programming Language :: Python :: 3.5',
+    ],
+
+    # What does your project relate to?
+    keywords='micropython wiegand keycard',
+
+    # SemVer
+    version='1.1.0',
+
+    # a single-file module
+    py_modules=['wiegand'],
+)
