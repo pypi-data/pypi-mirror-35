5f374808ce12eaa413253c8d8703ac30504aeaf7
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-05 15:04:22 +0000

    Add package tftf/0.0.2/tftf-0.0.2.tar.gz

---
+++ packages/tftf/tftf-0.0.2.tar.gz/tftf-0.0.2/tftf/layers/Activation.py
@@ -0,0 +1,23 @@
+from .Layer import Layer
+
+
+class Activation(Layer):
+    def __init__(self, activation='linear'):
+        super().__init__()
+        self.input_dim = None
+        self.output_dim = None
+        self.activation = self.activation_initializer(activation)
+
+    def __repr__(self):
+        return '<{}: {}({}, {})>'.format(self.__class__.__name__,
+                                         self.activation.__name__,
+                                         self.input_dim,
+                                         self.output_dim)
+
+    def forward(self, x):
+        return self.activation(x)
+
+    def initialize_output_dim(self):
+        super().initialize_output_dim()
+        self.output_dim = self.input_dim
+        return self.output_dim
