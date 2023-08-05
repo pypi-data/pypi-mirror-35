f0076681424ae82fd16363d5e7f8faf32f5e92a9
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-05 22:10:43 +0000

    Add package vk-client/0.1.5/vk-client-0.1.5.tar.gz

---
+++ packages/vk-client/vk-client-0.1.5.tar.gz/vk-client-0.1.5/setup.py
@@ -0,0 +1,43 @@
+import os
+from codecs import open
+from setuptools import setup, find_packages
+
+
+here_dir = os.path.abspath(os.path.dirname(__file__))
+about = {}
+with open(os.path.join(here_dir, "src", "vk_client", "__about__.py")) as f:
+    exec(f.read(), about)
+
+tests_deps = [
+    "tox",
+    "pytest"
+]
+
+captcha_handlers_deps = [
+    "requests",
+    "Pillow"
+]
+
+setup(
+    name=about["__title__"],
+    version=about["__version__"],
+    description=about["__description__"],
+    packages=find_packages("src"),
+    package_dir={"": "src"},
+    url=about["__url__"],
+    author=about["__author__"],
+    author_email=about["__author_email__"],
+    install_requires=[
+        "arrow",
+        "cached-property",
+        "six",
+        "enum34",
+        "more-itertools",
+        "vk"
+    ],
+    tests_require=tests_deps,
+    extras_require={
+        "tests": tests_deps,
+        "captcha-handlers": captcha_handlers_deps
+    }
+)
