9f14a6c611cc736142ec786539c3f1d7926d7595
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-05 15:26:10 +0000

    Add package nester1.1.6/1.0.0/nester1.1.6-1.0.0.tar.gz

---
+++ packages/nester1.1.6/nester1.1.6-1.0.0.tar.gz/nester1.1.6-1.0.0/nester.py
@@ -0,0 +1,30 @@
+import sys
+"""
+遞歸循環
+"""
+def print_lol(the_list,level):
+    '''
+    這個含數有一個位置參數,名為the_list
+    這可以是任何PYTHON列表(包含或不包含堑套列表)
+    所提供列表中的各個數據項會(遞歸地)打印到屏幕上,而且各佔一行
+    :param the_list:
+    :param lvl:
+    :return:
+    '''
+    for each_item in the_list:
+        if isinstance(each_item,list):
+            print_lol(each_item,level+1)
+        else:
+            for tab_stop in range(level):
+                print('\t',end='')
+            print(each_item)
+
+
+
+
+
+
+
+
+
+
