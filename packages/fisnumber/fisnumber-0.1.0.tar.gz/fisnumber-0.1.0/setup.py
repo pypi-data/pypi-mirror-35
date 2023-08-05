5bf57dd531ee66168e495ed36a47258e2673474f
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-05 15:23:54 +0000

    Add package fisnumber/0.1.0/fisnumber-0.1.0.tar.gz

---
+++ packages/fisnumber/fisnumber-0.1.0.tar.gz/fisnumber-0.1.0/setup.py
@@ -0,0 +1,11 @@
+from distutils.core import setup
+
+setup(
+    name = 'fisnumber',
+    version = '0.1.0',
+    description = 'A Python package example',
+    author = 'Frederico Alves',
+    author_email = 'fredslz@gmail.com',
+    url = 'https://github.com/zaiste/zaiste-py',
+    py_modules=['fisnumber']
+)
