8dd6c182921bc26a12a40fb3a8c9e5d9fd58506f
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-05 17:06:20 +0000

    Add package Albot.Online/0.22b0/Albot.Online-0.22b0.tar.gz

---
+++ packages/Albot.Online/Albot.Online-0.22b0.tar.gz/Albot.Online-0.22b0/setup.py
@@ -0,0 +1,12 @@
+from setuptools import setup
+
+setup(
+    name='Albot.Online',
+    version='0.22b0',
+    packages=['AlbotOnline', 'AlbotOnline.Snake', 'AlbotOnline.BrickPuzzle'],
+    url='https://www.albot.online/',
+    license='MIT',
+    author='Albot.Online',
+    author_email='Albot.Online.info@gmail.com',
+    description='A simple library for connecting you and supplying game functions for  the Albot.Onlie Client.'
+)
