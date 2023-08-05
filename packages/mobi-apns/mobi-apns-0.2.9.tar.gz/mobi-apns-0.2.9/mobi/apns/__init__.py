684977c8fd265d3c37151effa35ca57fbfd15508
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-05 16:57:18 +0000

    Add package mobi-apns/0.2.9/mobi-apns-0.2.9.tar.gz

---
+++ packages/mobi-apns/mobi-apns-0.2.9.tar.gz/mobi-apns-0.2.9/mobi/apns/__init__.py
@@ -0,0 +1,9 @@
+"""A library for interacting with APNs using HTTP/2 and token based authentication.
+
+"""
+
+__author__ = "Gene Sluder"
+__email__ = "gene@mobi.com"
+__version__ = "0.1.0"
+
+from .client import APNsClient, APNsMessage
