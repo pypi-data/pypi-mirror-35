77aede37fa606faa3db6a43bb18b5390af472a9b
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-05 16:55:34 +0000

    Add package edc-consent/0.2.13/edc_consent-0.2.13-py3-none-any.whl

---
+++ packages/edc-consent/edc_consent-0.2.13-py3-none-any.whl/edc_consent/choices.py
@@ -0,0 +1,9 @@
+from edc_constants.constants import YES, NO
+
+YES_DECLINED = 'Yes_declined'
+
+YES_NO_DECLINED_COPY = (
+    (YES, 'Yes and participant accepted the copy'),
+    (NO, 'No'),
+    (YES_DECLINED, 'Yes but participant declined the copy'),
+)
