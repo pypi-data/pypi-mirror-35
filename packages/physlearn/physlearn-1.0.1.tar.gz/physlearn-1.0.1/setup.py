edc18e0bd776ad0d6c9455bd11d8578bd41abd74
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-05 22:12:53 +0000

    Add package physlearn/1.0.1/physlearn-1.0.1.tar.gz

---
+++ packages/physlearn/physlearn-1.0.1.tar.gz/physlearn-1.0.1/setup.py
@@ -0,0 +1,12 @@
+from setuptools import setup
+
+setup(
+    name='physlearn',
+    version='1.0.1',
+    packages=['physlearn', 'physlearn.examples', 'physlearn.NeuralNet', 'physlearn.Optimizer'],
+    url='https://github.com/aeDeaf/physlearn',
+    license='GNU',
+    author='andrey',
+    author_email='abdrey21and@gmail.com',
+    description='A simple machine learning library'
+)
