89d0303f49bc7d6e10a6172dc4fdbeff6fb6fbdf
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-05 15:06:24 +0000

    Add package flv2faceimg0.0.1/0.0.1/flv2faceimg0.0.1-0.0.1.tar.gz

---
+++ packages/flv2faceimg0.0.1/flv2faceimg0.0.1-0.0.1.tar.gz/flv2faceimg0.0.1-0.0.1/setup.py
@@ -0,0 +1,29 @@
+# -*- coding: utf-8 -*-
+
+from distutils.core import setup
+
+from setuptools import setup, find_packages
+
+setup(
+
+    name = 'flv2faceimg0.0.1',
+
+    version = '0.0.1',
+
+    keywords = ('simple', 'test'),
+
+    description = 'just a simple test of vipkid',
+
+    license = 'MIT',
+
+    author = 'yin',
+
+    author_email = 'yinmengmeng@vipkid.com.cn',
+
+    packages = find_packages(),
+
+    platforms = 'any',
+
+    py_modules=['flv2faceimg.flv2faceimg']
+
+)
