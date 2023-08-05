01feee757a71372008634d219ccbe8cbe0eddcf7
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-05 16:57:23 +0000

    Add package MAnorm/1.1.4/MAnorm-1.1.4.tar.gz

---
+++ packages/MAnorm/MAnorm-1.1.4.tar.gz/MAnorm-1.1.4/CHANGELOG.rst
@@ -0,0 +1,46 @@
+ChangeLog
+=========
+
+v1.1.4 (2018-08-17)
+-------------------
+
+* Fix an issue in setting matplotlib backend
+
+v1.1.3 (2018-01-19)
+-------------------
+
+* Fix a bug in the file name of filtered biased peaks
+
+* Fix a typo
+
+v1.1.2 (2018-01-18)
+-------------------
+
+* Keep five digits for floats in the output files
+
+* Fix a typo
+
+
+v1.1.1 (2018-01-11)
+-------------------
+
+* Add test module
+
+Bugs fixed:
+
+* Rename the file name of filtered biased peaks
+
+
+
+v1.1 (2017-11-07)
+-----------------
+
+Improvements:
+
+* Refactor the package for better performance and compatibility
+
+Bugs fixed:
+
+* Fix the coordinates of peaks to be consistent with the corresponding coordinate system
+* Fix the approximate equation in p-value calculation
+* Fix the summit calculation of merged common peaks
---
+++ packages/MAnorm/MAnorm-1.1.4.tar.gz/MAnorm-1.1.4/setup.py
@@ -0,0 +1,41 @@
+#!/usr/bin/env python
+
+from setuptools import setup, find_packages
+from manorm import __version__
+
+DESCRIPTION = "A robust model for quantitative comparison of ChIP-Seq data sets."
+with open('README.rst') as fin:
+    LONG_DESCRIPTION = fin.read()
+
+INSTALL_REQUIRES = ["numpy",
+                    "matplotlib",
+                    "statsmodels",
+                    "scipy"]
+
+CLASSIFIERS = ["Development Status :: 5 - Production/Stable",
+               "Environment :: Console",
+               "Intended Audience :: Science/Research",
+               "License :: OSI Approved :: BSD License",
+               "Operating System :: OS Independent",
+               "Programming Language :: Python",
+               "Programming Language :: Python :: 2",
+               "Programming Language :: Python :: 2.7",
+               "Topic :: Scientific/Engineering :: Bio-Informatics"]
+
+setup(
+    name="MAnorm",
+    version=__version__,
+    packages=find_packages(exclude=["test"]),
+    description=DESCRIPTION,
+    long_description=LONG_DESCRIPTION,
+    url="https://github.com/shao-lab/MAnorm",
+    author="Semal",
+    author_email="",
+    maintainer="Hayden Sun",
+    maintainer_email="sunhongduo@picb.ac.cn",
+    license="BSD",
+    entry_points={"console_scripts": ["manorm=manorm.cmdline:main"]},
+    install_requires=INSTALL_REQUIRES,
+    classifiers=CLASSIFIERS,
+    zip_safe=False,
+)
