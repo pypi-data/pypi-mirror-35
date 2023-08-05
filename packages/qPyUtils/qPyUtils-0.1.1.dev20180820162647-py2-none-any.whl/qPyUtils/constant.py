4c7c3cfa5a3d997dc724a93084e310cb3e346eed
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-05 18:10:15 +0000

    Add package qPyUtils/0.1.1.dev20180820162647/qPyUtils-0.1.1.dev20180820162647-py2-none-any.whl

---
+++ packages/qPyUtils/qPyUtils-0.1.1.dev20180820162647-py2-none-any.whl/qPyUtils/constant.py
@@ -0,0 +1,37 @@
+# coding=utf-8
+"""
+constant values
+
+Authors: qianweishuo<qzy922@gmail.com>
+Date:    2018/8/13 上午11:36
+"""
+
+import sys
+
+from typing import TypeVar
+
+INF = sys.maxsize
+
+
+def dummy_fn(*args, **kwargs):
+    # type: (Any, Any) -> None
+    """
+    blackhole, just return None for anything
+    :param args:
+    :param kwargs:
+    :return:
+    """
+    pass
+
+
+T = TypeVar('T')
+
+
+def identity_fn(arg):
+    # type: (T) -> T
+    """
+    identity function; just return anything back
+    :param arg:
+    :return:
+    """
+    return arg
