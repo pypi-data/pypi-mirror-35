cf42bbfcb199a7f97d57e28d9853e6a60040f702
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-05 16:57:18 +0000

    Add package pyliburo/0.11/pyliburo-0.11.tar.gz

---
+++ packages/pyliburo/pyliburo-0.11.tar.gz/pyliburo-0.11/readme.txt
@@ -0,0 +1,49 @@
+to run the environment:
+
+1.) install anaconda for python2.7
+(instructions on how to use anaconda: http://conda.pydata.org/docs/using/envs.html)
+
+2.) libraries to download:
+
+    a.) conda install -c https://conda.anaconda.org/dlr-sc pythonocc-core=0.17
+
+    b.) conda install scipy (BSD accepted license)
+
+3.) pip install pyliburo
+
+4.) install Daysim from http://daysim.ning.com/ for the daylighting analysis
+
+    a.) if c:/daysim/radiance/bin on env path, delete it
+	
+5.) conda install spyder (for editing the scripts)
+
+6.) download example files from 
+(https://github.com/chenkianwee/pyliburo_example_files)
+
+#========================================================
+
+#list of libraries used in pyliburo
+
+#========================================================
+
+1.) lxml ((BSD) libxml2 and libxslt2 (MIT))
+
+2.) pyshp (mit license)
+
+3.) pythonocc (GNU LGPL3)
+
+4.) numpy (BSD 3-clause "New" or "Revised" License)
+
+5.) scipy (BSD 3-clause "New" or "Revised" License)
+
+6.) pycollada (BSD 3-clause "New" or "Revised" License)
+
+7.) networkx (BSD 3-clause "New" or "Revised" License)
+
+8.) scikit-learn (BSD 3-clause "New" or "Revised" License)
+
+9.) pymf
+
+10.) cvxopt (GNU General Public License v3.0)
+
+11.) matplotlib 
---
+++ packages/pyliburo/pyliburo-0.11.tar.gz/pyliburo-0.11/setup.py
@@ -0,0 +1,33 @@
+from setuptools import setup, find_packages
+
+__author__ = "Chen Kian Wee"
+__copyright__ = "Copyright 2016, Chen Kian Wee"
+__credits__ = ["Chen Kian Wee"]
+__license__ = "GPL3"
+__version__ = "0.11"
+__maintainer__ = "Chen Kian Wee"
+__email__ = "chenkianwee@gmail.com"
+__status__ = "Development"
+
+LONG_DESCRIPTION = "refer to https://github.com/chenkianwee/pyliburo for full installation instructions"
+    
+INSTALL_REQUIRES = ['lxml', 'pyshp', 'numpy', 'pycollada', 'networkx', 'scikit-learn', 'pymf', 'matplotlib']
+
+setup(name='pyliburo',
+      packages = find_packages(),
+      package_data={
+          'pyliburo': ['databases/ettv/*.csv'],
+          },
+      version=__version__,
+      description='Python Library for Urban Optimization',
+      long_description=LONG_DESCRIPTION,
+      author=__author__,
+      author_email=__email__,
+      url='https://github.com/chenkianwee/pyliburo',
+      download_url = 'https://github.com/architecture-building-systems/pyliburo/archive/0.11.tar.gz',
+      keywords = ["urban design", "architecture design", "design optimisation"],
+      install_requires=INSTALL_REQUIRES,
+      classifiers = ['Development Status :: 3 - Alpha',
+                     'License :: OSI Approved :: GNU General Public License v3 (GPLv3)',
+                     'Programming Language :: Python :: 2.7',],
+      )
