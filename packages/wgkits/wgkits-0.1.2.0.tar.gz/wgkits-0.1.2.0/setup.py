bbf704831d42af8c1edaeb1a7b1aac21ea2272ec
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-05 15:26:05 +0000

    Add package wgkits/0.1.2.0/wgkits-0.1.2.0.tar.gz

---
+++ packages/wgkits/wgkits-0.1.2.0.tar.gz/wgkits-0.1.2.0/setup.py
@@ -0,0 +1,29 @@
+import setuptools
+
+with open("README.md", "r") as fh:
+    long_description = fh.read()
+
+setuptools.setup(
+    name="wgkits",
+    version="0.1.2.0",
+    author="rontgen",
+    author_email="rontgen@pku.edu.cn",
+    description="Android build apk tools",
+    long_description=long_description,
+    long_description_content_type="text/markdown",
+    url="https://github.com/rontgen/wgkits",
+    packages=setuptools.find_packages(),
+    classifiers=(
+        "Programming Language :: Python :: 2",
+        "License :: OSI Approved :: MIT License",
+        "Operating System :: OS Independent",
+    ),
+    install_requires=[
+        'jinja2',
+        'backports.shutil_which',
+        'opencc-python-reimplemented',
+        'pandas',
+        'openpyxl',
+        'matplotlib'
+    ],
+)
