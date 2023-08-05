6777f6757b8cd12507fbb82431f2d907153f0dfb
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-05 21:08:54 +0000

    Add package requestsdefaulter/0.1.0/requestsdefaulter-0.1.0-py3.6.egg

---
+++ packages/requestsdefaulter/requestsdefaulter-0.1.0-py3.6.egg/requestsdefaulter/__init__.py
@@ -0,0 +1,16 @@
+import requests
+
+name = "requestsdefaulter"
+
+def default_headers(default_functions):
+    original_prepare_headers = requests.models.PreparedRequest.prepare_headers
+
+    def new_prepare_headers(self, headers):
+        defaults = default_functions()
+
+        if headers is not None:
+            defaults.update(headers)
+
+        original_prepare_headers(self, defaults)
+
+    requests.models.PreparedRequest.prepare_headers = new_prepare_headers
