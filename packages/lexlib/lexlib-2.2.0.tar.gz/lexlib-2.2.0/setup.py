1a0d1328f0195c9cfa070b100f3102441b1e101d
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-05 15:30:30 +0000

    Add package lexlib/2.2.0/lexlib-2.2.0.tar.gz

---
+++ packages/lexlib/lexlib-2.2.0.tar.gz/lexlib-2.2.0/setup.py
@@ -0,0 +1,21 @@
+from setuptools import setup
+
+ver = "2.2.0"
+repo = "https://github.com/cranndarach/lexlib"
+setup(name="lexlib",
+      version=ver,
+      description="Utilities for research involving words.",
+      keywords="psycholinguistics research words language linguistics \
+                psychology cognitive science cogsci",
+      url=repo,
+      download_url="{}/tarball/{}".format(repo, ver),
+      author="R. J. Steiner",
+      author_email="r.steiner@uconn.edu",
+      license="MIT",
+      packages=["lexlib"],
+      classifiers=[
+          "Development Status :: 4 - Beta",
+          "Intended Audience :: Science/Research",
+          "License :: OSI Approved :: MIT License"
+          ]
+      )
