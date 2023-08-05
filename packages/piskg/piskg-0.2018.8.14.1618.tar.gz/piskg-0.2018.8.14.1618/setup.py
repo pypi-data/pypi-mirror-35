daa14792c883f513f4d8f07b6dd93ad2f108146a
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-05 15:25:38 +0000

    Add package piskg/0.2018.8.14.1618/piskg-0.2018.8.14.1618.tar.gz

---
+++ packages/piskg/piskg-0.2018.8.14.1618.tar.gz/piskg-0.2018.8.14.1618/setup.py
@@ -0,0 +1,47 @@
+#!/usr/bin/env python
+
+from setuptools import setup
+from piskg import __version__
+
+with open('README.md') as f:
+    long_description = f.read()
+desc = """Python IPFS Swarm Key Generator"""
+
+setup(
+    name='piskg',
+    version=__version__,
+    url='https://github.com/machawk1/piskg',
+    download_url="https://github.com/machawk1/piskg",
+    author='Mat Kelly',
+    author_email='mkelly@cs.odu.edu',
+    description=desc,
+    packages=['piskg'],
+    license='MIT',
+    long_description=long_description,
+    long_description_content_type="text/markdown",
+    provides=[
+        'piskg'
+    ],
+    entry_points="""
+        [console_scripts]
+        piskg = piskg.__main__:main
+    """,
+    package_data={},
+    keywords='ipfs keygen',
+    classifiers=[
+        'License :: OSI Approved :: MIT License',
+        
+        "Programming Language :: Python :: 2",
+        "Programming Language :: Python :: 2.7",
+        "Programming Language :: Python :: 3",
+        "Programming Language :: Python :: 3.7",
+        
+        "Topic :: Security :: Cryptography",
+        
+        "Topic :: System :: Distributed Computing",
+        
+        'Topic :: Utilities',
+    ]
+)
+
+# Publish to pypi: rm -rf dist; python setup.py sdist bdist_wheel; twine upload dist/*
