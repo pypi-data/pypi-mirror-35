7cd7c83678ff0545ee7349f35ab68c7081ec7432
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-05 21:08:55 +0000

    Add package kurdish/1.0.1/Kurdish-1.0.1.tar.gz

---
+++ packages/kurdish/Kurdish-1.0.1.tar.gz/Kurdish-1.0.1/setup.py
@@ -0,0 +1,29 @@
+import setuptools
+
+setuptools.setup(
+	name='Kurdish',
+	version='1.0.1',
+	author='Dolan Hêriş',
+	author_email='dolanskurd@mail.com',
+	url='https://github.com/dolanskurd',
+	description=('Kurdish Language Library for converting characters and numbers in Persian, English and also Arabic to Kurdish and vice versa.'),
+	keywords='Kurdish Language Module Library',
+	py_modules=['Kurdish'],
+	packages=setuptools.find_packages(),
+	classifiers=[
+        'Development Status :: 4 - Beta',
+        'Topic :: Documentation',
+        'Topic :: Printing',
+        'License :: OSI Approved :: MIT License',
+        'Programming Language :: Python',
+        'Programming Language :: Python :: 3',
+        'Programming Language :: Python :: 3.7',
+        'Topic :: Software Development :: Libraries :: Python Modules',
+        'Topic :: Text Editors :: Text Processing',
+        'Topic :: Text Processing :: Fonts',
+        'Topic :: Text Editors :: Word Processors',
+        'Topic :: Utilities',
+        'Intended Audience :: Developers',
+	],
+)
+
