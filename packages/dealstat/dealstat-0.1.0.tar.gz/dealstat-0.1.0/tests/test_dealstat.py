86cd4e10bc8fa3044497fd09cccc56b94aa48a66
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-05 21:09:40 +0000

    Add package dealstat/0.1.0/dealstat-0.1.0.tar.gz

---
+++ packages/dealstat/dealstat-0.1.0.tar.gz/dealstat-0.1.0/tests/test_dealstat.py
@@ -0,0 +1,25 @@
+#!/usr/bin/env python
+# -*- coding: utf-8 -*-
+
+"""Tests for `dealstat` package."""
+
+import pytest
+
+
+from dealstat import dealstat
+
+
+@pytest.fixture
+def response():
+    """Sample pytest fixture.
+
+    See more at: http://doc.pytest.org/en/latest/fixture.html
+    """
+    # import requests
+    # return requests.get('https://github.com/audreyr/cookiecutter-pypackage')
+
+
+def test_content(response):
+    """Sample pytest test function with the pytest fixture as an argument."""
+    # from bs4 import BeautifulSoup
+    # assert 'GitHub' in BeautifulSoup(response.content).title.string
