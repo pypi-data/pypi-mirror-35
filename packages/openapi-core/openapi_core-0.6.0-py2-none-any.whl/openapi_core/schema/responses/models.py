1f8b6394dfd456f32c2cb8293041c4c5d7d193d3
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-05 21:11:46 +0000

    Add package openapi-core/0.6.0/openapi_core-0.6.0-py2-none-any.whl

---
+++ packages/openapi-core/openapi_core-0.6.0-py2-none-any.whl/openapi_core/schema/responses/models.py
@@ -0,0 +1,33 @@
+"""OpenAPI core responses models module"""
+from openapi_core.schema.content.exceptions import MimeTypeNotFound
+from openapi_core.schema.content.models import Content
+from openapi_core.schema.media_types.exceptions import InvalidContentType
+from openapi_core.schema.responses.exceptions import MissingResponseContent
+
+
+class Response(object):
+
+    def __init__(
+            self, http_status, description, headers=None, content=None,
+            links=None):
+        self.http_status = http_status
+        self.description = description
+        self.headers = headers and dict(headers) or {}
+        self.content = content and Content(content) or Content()
+        self.links = links and dict(links) or {}
+
+    def __getitem__(self, mimetype):
+        return self.get_content_type(mimetype)
+
+    def get_content_type(self, mimetype):
+        try:
+            return self.content[mimetype]
+        except MimeTypeNotFound:
+            raise InvalidContentType(
+                "Invalid mime type `{0}`".format(mimetype))
+
+    def get_value(self, response):
+        if not response.data:
+            raise MissingResponseContent("Missing response content")
+
+        return response.data
