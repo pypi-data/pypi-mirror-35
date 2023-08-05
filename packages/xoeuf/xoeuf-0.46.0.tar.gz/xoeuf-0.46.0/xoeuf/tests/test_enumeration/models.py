d2f3edfce89d1bc5daa47154c4fe773a2de61c4d
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-05 22:11:36 +0000

    Add package xoeuf/0.46.0/xoeuf-0.46.0.tar.gz

---
+++ packages/xoeuf/xoeuf-0.46.0.tar.gz/xoeuf-0.46.0/xoeuf/tests/test_enumeration/models.py
@@ -0,0 +1,50 @@
+#!/usr/bin/env python
+# -*- coding: utf-8 -*-
+# ---------------------------------------------------------------------
+# Copyright (c) Merchise Autrement [~º/~] and Contributors
+# All rights reserved.
+#
+# This is free software; you can do what the LICENCE file allows you to.
+#
+
+from __future__ import (division as _py3_division,
+                        print_function as _py3_print,
+                        absolute_import as _py3_abs_import)
+
+from enum import IntEnum, Enum
+from xoeuf import models, fields
+
+
+class COLORS(IntEnum):
+    Blue = 0
+    Red = 1
+    Green = 2
+
+
+class CARS(Enum):
+    FORD = object()
+    CHEV = object()
+
+
+class Pax(object):
+    single = 1
+    double = 2
+    __members__ = {'single': single, 'double': double}
+
+
+class WorkType(int):
+    pass
+
+
+class WORK_TYPE(object):
+    easy = WorkType(0)
+    hard = WorkType(1)
+    __members__ = {'easy': easy, 'hard': hard}
+
+
+class Model(models.Model):
+    _name = 'test.enum.model'
+    color = fields.Enumeration(COLORS)
+    car = fields.Enumeration(CARS)
+    pax = fields.Enumeration(Pax)
+    wtype = fields.Enumeration(WORK_TYPE, default=WORK_TYPE.hard)
