0b7266715ed6488c40ac312664513989f241e3d8
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-05 22:12:52 +0000

    Add package duckietown-shell/0.2.18/duckietown-shell-0.2.18.tar.gz

---
+++ packages/duckietown-shell/duckietown-shell-0.2.18.tar.gz/duckietown-shell-0.2.18/lib/dt_shell/constants.py
@@ -0,0 +1,7 @@
+# -*- coding: utf-8 -*-
+
+
+class DTShellConstants(object):
+    commands_remote_url = 'https://github.com/duckietown/duckietown-shell-commands'
+    ROOT = '~/.dt-shell/'
+    ENV_COMMANDS = 'DTSHELL_COMMANDS'
