99964f496175afd8118d464901f84ecbf2f982e1
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-05 17:06:00 +0000

    Add package lpschedule-generator/0.8.3/lpschedule_generator-0.8.3-py2-none-any.whl

---
+++ packages/lpschedule-generator/lpschedule_generator-0.8.3-py2-none-any.whl/lpschedule_generator/_version.py
@@ -0,0 +1,10 @@
+# -*- coding: utf-8 -*-
+#
+#    Copyright (C) 2018 lpschedule-generator contributors. See CONTRIBUTORS.
+#
+#    This file is part of lpschedule-generator.
+#
+#    SPDX-License-Identifier: GPL-3.0-or-later
+#
+
+__version__ = u'0.8.3'
