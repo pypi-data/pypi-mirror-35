b24bc544c461021db67d04aba4e1035d7baf0ddc
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-05 22:12:25 +0000

    Add package testupload/0.0.1/testupload-0.0.1.tar.gz

---
+++ packages/testupload/testupload-0.0.1.tar.gz/testupload-0.0.1/setup.py
@@ -0,0 +1,27 @@
+"""A setuptools based setup module.
+
+See:
+https://packaging.python.org/en/latest/distributing.html
+https://github.com/pypa/sampleproject
+"""
+
+# Always prefer setuptools over distutils
+from setuptools import setup, find_packages
+from os import path
+# io.open is needed for projects that support Python 2.7
+# It ensures open() defaults to text mode with universal newlines,
+# and accepts an argument to specify the text encoding
+# Python 3 only projects can skip this import
+from io import open
+
+here = path.abspath(path.dirname(__file__))
+
+# Arguments marked as "Required" below must be included for upload to PyPI.
+# Fields marked as "Optional" may be commented out.
+
+setup(
+    name='testupload',  # Required
+    version='0.0.1',  # Required
+    description='just a test',  # Required
+    packages=find_packages(),  # Required
+)
