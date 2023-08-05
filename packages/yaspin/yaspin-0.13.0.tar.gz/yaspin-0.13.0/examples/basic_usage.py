bd8f51f70c633026372e9bda8d144b7dd3c42634
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-05 15:26:35 +0000

    Add package yaspin/0.13.0/yaspin-0.13.0.tar.gz

---
+++ packages/yaspin/yaspin-0.13.0.tar.gz/yaspin-0.13.0/examples/basic_usage.py
@@ -0,0 +1,38 @@
+# -*- coding: utf-8 -*-
+
+"""
+examples.basic_usage
+~~~~~~~~~~~~~~~~~~~~
+
+Common usage patterns for the yaspin spinner.
+"""
+
+import time
+
+from yaspin import Spinner, yaspin
+
+
+def context_manager_default():
+    with yaspin(text="Braille"):
+        time.sleep(3)
+
+
+def context_manager_line():
+    line_spinner = Spinner("-\\|/", 150)
+    with yaspin(line_spinner, "line"):
+        time.sleep(3)
+
+
+@yaspin(Spinner("⢄⢂⢁⡁⡈⡐⡠", 80), text="Dots")
+def decorated_function():
+    time.sleep(3)
+
+
+def main():
+    context_manager_default()
+    context_manager_line()
+    decorated_function()
+
+
+if __name__ == "__main__":
+    main()
---
+++ packages/yaspin/yaspin-0.13.0.tar.gz/yaspin-0.13.0/yaspin/__init__.py
@@ -0,0 +1,10 @@
+# -*- coding: utf-8 -*-
+
+from __future__ import absolute_import
+
+from .__version__ import __version__  # noqa
+from .base_spinner import Spinner
+from .yaspin import yaspin
+
+
+__all__ = ("yaspin", "Spinner")
