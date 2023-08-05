0fd49a45bef55c7a2fd48ffd95db4d204b70d146
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-05 15:32:06 +0000

    Add package text2ml/3.0.0/text2ml-3.0.0.tar.gz

---
+++ packages/text2ml/text2ml-3.0.0.tar.gz/text2ml-3.0.0/README.md
@@ -0,0 +1,20 @@
+<p align="center">
+  <img src="https://i.imgur.com/WYQmxDw.png" alt="Text2ml"/>
+  <br>
+  <a href="https://pypi.org/project/text2ml"><img src="https://img.shields.io/pypi/v/text2ml.svg" /></a>
+</p>
+
+## What is Text2ml?
+
+Text2ml is a simple, dependency-free module for Telegram bot API that formats text + entities into formatted text.
+
+## Installation
+
+This module can be installed from [PyPi](https://pypi.org/project/text2ml) by running:
+
+```
+pip install text2ml
+```
+## Usage
+
+Soon
---
+++ packages/text2ml/text2ml-3.0.0.tar.gz/text2ml-3.0.0/setup.py
@@ -0,0 +1,16 @@
+import setuptools
+
+with open("README.md", "r") as fh:
+    long_description = fh.read()
+
+setuptools.setup(
+    name='text2ml',
+    version='3.0.0',
+    packages=setuptools.find_packages(),
+    url='https://github.com/marminino/text2ml',
+    author='Alisson Lauffer',
+    author_email='alissonvitortc@gmail.com',
+    description='A module for Telegram Bot API that can format text + entities into formatted text',
+    long_description=long_description,
+    long_description_content_type="text/markdown"
+)
