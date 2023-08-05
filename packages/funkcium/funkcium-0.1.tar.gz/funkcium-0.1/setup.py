3cc103db62e309f6e250a8d07123551792442302
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-05 15:04:44 +0000

    Add package funkcium/0.1/funkcium-0.1.tar.gz

---
+++ packages/funkcium/funkcium-0.1.tar.gz/funkcium-0.1/setup.py
@@ -0,0 +1,21 @@
+from setuptools import setup
+
+setup(name='funkcium',
+      version='0.1',
+      description='Yet Another Python functional library, with scala inspiration',
+      url='https://github.com/nykh/funkcium',
+      author='nykh',
+      author_email='nykh.pypi.199421@tryninja.io',
+      license='MIT',
+      packages=['funkcium'],
+      classifiers=[
+        "Programming Language :: Python",
+        "Programming Language :: Python :: 3",
+        "Development Status :: 3 - Alpha",
+        "Intended Audience :: Developers",
+        "License :: OSI Approved :: BSD License",
+        "Operating System :: OS Independent",
+        "Topic :: Software Development",
+        "Topic :: Software Development :: Libraries :: Python Modules"
+    ]
+)
