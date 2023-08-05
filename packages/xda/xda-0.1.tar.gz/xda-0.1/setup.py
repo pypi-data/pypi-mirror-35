8108829a3f064961e575993b25ec698ad065ee0a
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-05 18:10:01 +0000

    Add package xda/0.1/xda-0.1.tar.gz

---
+++ packages/xda/xda-0.1.tar.gz/xda-0.1/setup.py
@@ -0,0 +1,14 @@
+# -*- coding: utf-8 -*-
+
+from distutils.core import setup
+
+setup(
+    name='xda',
+    version='0.1',
+    description='xda',
+    author='Ville Korhonen',
+    author_email='ville@xd.fi',
+    url='https://github.com/ypcs/xda',
+    packages=['xda',],
+    package_dir={'': 'src',},
+)
