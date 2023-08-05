0a92dbfaedf9c080db4e0833cd2e89da4134219f
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-05 15:05:36 +0000

    Add package django-workon/1.2.19/django-workon-1.2.19.tar.gz

---
+++ packages/django-workon/django-workon-1.2.19.tar.gz/django-workon-1.2.19/workon/utils/cache.py
@@ -0,0 +1,64 @@
+from functools import partial
+from django.utils.functional import cached_property
+from django.core.cache import cache
+
+__all__ = ["cached_property", "memoize", "cache_get_or_set"]
+
+class memoize(object):
+    """Decorator that caches a function's return value each time it is called.
+    If called later with the same arguments, the cached value is returned, and
+    not re-evaluated.
+    """
+    def __init__(self, func):
+        self.func = func
+        self.cache = {}
+    def __call__(self, *args):
+        try:
+            return self.cache[args]
+        except KeyError:
+            value = self.func(*args)
+            self.cache[args] = value
+            return value
+        except TypeError:
+             # uncachable -- for instance, passing a list as an argument.
+             # Better to not cache than to blow up entirely.
+            return self.func(*args)
+    def __repr__(self):
+        """Return the function's docstring."""
+        return self.func.__doc__
+    def __get__(self, obj, objtype):
+        """Support instance methods."""
+        fn = partial(self.__call__, obj)
+        fn.reset = self._reset
+        return fn
+    def _reset(self):
+        self.cache = {}
+
+
+def cache_get_or_set(key, value, ttl):
+    return cache.get_or_set(key, value, ttl)
+
+
+class cached_property__old:
+    """
+    Decorator that converts a method with a single self argument into a
+    property cached on the instance.
+
+    Optional ``name`` argument allows you to make cached properties of other
+    methods. (e.g.  url = cached_property(get_absolute_url, name='url') )
+    """
+    def __init__(self, func, name=None):
+        self.func = func
+        self.__doc__ = getattr(func, '__doc__')
+        self.name = name or func.__name__
+
+    def __get__(self, instance, cls=None):
+        """
+        Call the function and put the return value in instance.__dict__ so that
+        subsequent attribute access on the instance returns the cached value
+        instead of calling cached_property.__get__().
+        """
+        if instance is None:
+            return self
+        res = instance.__dict__[self.name] = self.func(instance)
+        return res
