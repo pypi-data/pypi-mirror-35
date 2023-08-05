b049adc7ed73cf81f7f47c5b6fa3bac347b36ff4
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-05 15:05:28 +0000

    Add package autoslot/2018.8.1/autoslot-2018.8.1.tar.gz

---
+++ packages/autoslot/autoslot-2018.8.1.tar.gz/autoslot-2018.8.1/README.rst
@@ -0,0 +1,91 @@
+.. image:: https://img.shields.io/badge/stdlib--only-yes-green.svg
+    :target: https://img.shields.io/badge/stdlib--only-yes-green.svg
+
+.. image:: https://travis-ci.org/cjrh/autoslot.svg?branch=master
+    :target: https://travis-ci.org/cjrh/autoslot
+
+.. image:: https://coveralls.io/repos/github/cjrh/autoslot/badge.svg?branch=master
+    :target: https://coveralls.io/github/cjrh/autoslot?branch=master
+
+.. image:: https://img.shields.io/pypi/pyversions/autoslot.svg
+    :target: https://pypi.python.org/pypi/autoslot
+
+.. image:: https://img.shields.io/github/tag/cjrh/autoslot.svg
+    :target: https://img.shields.io/github/tag/cjrh/autoslot.svg
+
+.. image:: https://img.shields.io/badge/install-pip%20install%20autoslot-ff69b4.svg
+    :target: https://img.shields.io/badge/install-pip%20install%20autoslot-ff69b4.svg
+
+.. image:: https://img.shields.io/pypi/v/autoslot.svg
+    :target: https://img.shields.io/pypi/v/autoslot.svg
+
+.. image:: https://img.shields.io/badge/calver-YYYY.MM.MINOR-22bfda.svg
+    :target: http://calver.org/
+
+autoslot
+========
+
+Automatic "__slots__".
+
+Demo
+----
+
+.. code-block:: python
+
+   from autoslot import Slots
+
+   class Compact(Slots):
+       def __init__(self, a, b):
+           self.x = a
+           self.y = b
+
+This produces *exactly* the same class as if you had done:
+
+.. code-block:: python
+
+   class Compact:
+       __slots__ = {'x', 'y'}
+       def __init__(self, a, b):
+           self.x = a
+           self.y = b
+
+Simply: the code inside ``__init__()`` is scanned to find all assignments
+to attributes on ``self``, and these are added as ``__slots__``.
+
+The benefit of using ``autoslot.Slots`` over a manual slots declaration is
+that you can modify the
+code inside the ``__init__()`` method to add more attributes, and those
+changes will *automatically* be reflected in the ``__slots__`` definition.
+
+You can also have the best of both worlds: slots for fields you expect,
+**as well as** a ``__dict__`` for those you don't:
+
+.. code-block:: python
+
+   from autoslot import SlotsPlusDict
+
+   class SemiCompact(SlotsPlusDict):
+       def __init__(self, a, b):
+           self.x = a
+           self.y = b
+
+   inst = SemiCompact(1, 2)
+   inst.z = 123  # <-- This won't fail!
+
+Attributes ``x`` and ``y`` will be stored in slots, while all other
+dynamically-assigned attributes will go into the usual ``__dict__`` instance
+inside the class.  If most of your class's attributes appear in the ``__init__()``
+method (these will become slots), then the space bloat caused by dictionary
+hash-table expansion will be contained to only the dynamically-assigned
+attributes.
+
+How does it work?
+-----------------
+
+See for yourself! The code is tiny.
+
+In words: the metaclass finds the ``__init__()`` method, if present, and
+accesses its bytecode. It looks for all assignments to attributes of
+``self``, and considers those to be desired ``__slots__`` entries. Then the
+metaclass injects ``__slots__`` into the namespace of the class definition
+and thereafter allows class creation to proceed as normal.
---
+++ packages/autoslot/autoslot-2018.8.1.tar.gz/autoslot-2018.8.1/tests/test_slots.py
@@ -0,0 +1,333 @@
+import pytest
+from autoslot import Slots, SlotsPlusDict
+
+
+def test_normal():
+    """This is just normal behaviour: nothing different."""
+    class A():
+        def __init__(self, a, b):
+            self.x = a
+            self.y = b
+
+    a = A(1, 2)
+    assert hasattr(a, 'x')
+    assert hasattr(a, 'y')
+    assert hasattr(a, '__dict__')
+    assert not hasattr(a, '__slots__')
+    a.z = 3
+    assert hasattr(a, 'z')
+
+
+def test_slots():
+    """Basic usage of the Slots metaclass."""
+    class A(Slots):
+        def __init__(self, a, b):
+            self.x = a
+            self.y = b
+            # Testing to see that the
+            # bytecode processor identifies things
+            # correctly.
+            self.x = 'bleh'
+
+    assert '__module__' in A.__dict__
+    assert '__init__' in A.__dict__
+    assert '__slots__' in A.__dict__
+    assert A.__dict__['__slots__'] == {'x', 'y'}
+
+    a = A(1, 2)
+    assert hasattr(a, 'x')
+    assert hasattr(a, 'y')
+    # Just checking that we didn't pick up the wrong names
+    assert not hasattr(a, 'a')
+    assert not hasattr(a, 'b')
+    assert hasattr(a, '__slots__')
+    assert not hasattr(a, '__dict__')
+    # Can't assign new attributes
+    with pytest.raises(AttributeError):
+        a.z = 3
+
+
+def test_no_init():
+    class A(Slots):
+        pass
+
+    a = A()
+    assert hasattr(a, '__slots__')
+    assert not hasattr(a, '__dict__')
+
+
+def test_conditional():
+    """What happens if you conditionally create attributes inside
+    __init__()?"""
+    class A(Slots):
+        def __init__(self, a):
+            if a == 0:
+                self.x = 1
+            elif a == 1:
+                self.y = 1
+
+    # "if" is hit
+    a = A(0)
+    assert hasattr(a, '__slots__')
+    assert not hasattr(a, '__dict__')
+    # Both attributes will get slots.
+    assert {'x', 'y'} < set(dir(a))
+
+    # "elif" is hit
+    a = A(1)
+    assert hasattr(a, '__slots__')
+    assert not hasattr(a, '__dict__')
+    # Both attributes will get slots.
+    assert {'x', 'y'} < set(dir(a))
+
+    # Neither branch hit
+    a = A(2)
+    assert hasattr(a, '__slots__')
+    assert not hasattr(a, '__dict__')
+    # Both attributes will get slots.
+    assert {'x', 'y'} < set(dir(a))
+
+
+def test_inherit_new():
+    """Normal inheritance will propagate the metaclass. Note that
+    you MUST call super().__init__() if you want slots for the parents
+    to be created."""
+    class A(Slots):
+        def __init__(self, a, b):
+            self.x = a
+            self.y = b
+
+    class B(A):
+        def __init__(self, c, d):
+            super().__init__(1, 2)
+            self.w = c
+            self.z = d
+
+    class C(B):
+        """Missing call to superclass initializer."""
+        def __init__(self, e, f):
+            self.ww = e
+            self.zz = f
+
+    a = A(1, 2)
+    assert hasattr(a, '__slots__')
+    assert not hasattr(a, '__dict__')
+    assert hasattr(a, 'x')
+    assert hasattr(a, 'y')
+
+    # Instances of B have slots from A in addition to their own.
+    b = B(3, 4)
+    assert hasattr(b, 'x')
+    assert hasattr(b, 'y')
+    assert hasattr(b, 'w')
+    assert hasattr(b, 'z')
+    assert hasattr(b, '__slots__')
+    assert not hasattr(b, '__dict__')
+
+    c = C(5, 6)
+    assert hasattr(c, '__slots__')
+    assert not hasattr(c, '__dict__')
+    assert not hasattr(c, 'x')
+    assert not hasattr(c, 'y')
+    assert not hasattr(c, 'w')
+    assert not hasattr(c, 'z')
+    assert hasattr(c, 'ww')
+    assert hasattr(c, 'zz')
+
+    with pytest.raises(AttributeError):
+        c.www = 123
+
+
+def test_wrong_instance_var():
+    """Also works if you don't use 'self' as the instance var name"""
+    class A(Slots):
+        def __init__(blah, a, b):
+            blah.x = a
+            blah.y = b
+
+    a = A(1, 2)
+    assert hasattr(a, 'x')
+    assert hasattr(a, 'y')
+    # Just checking that we didn't pick up the wrong things in
+    # slots_metaclass.assignments_to_self.
+    assert not hasattr(a, 'a')
+    assert not hasattr(a, 'b')
+    assert hasattr(a, '__slots__')
+    assert not hasattr(a, '__dict__')
+    # Can't assign new attributes
+    with pytest.raises(AttributeError):
+        a.z = 3
+
+
+def test_slots_plus_dict():
+    """You can also have both: slots for some vars, and dynamic assignment
+    for other vars."""
+    class A(SlotsPlusDict):
+        def __init__(self, a, b):
+            self.x = a
+            self.y = b
+
+    a = A(1, 2)
+    assert hasattr(a, 'x')
+    assert hasattr(a, 'y')
+    # Just checking that we didn't pick up the wrong things in
+    # slots_metaclass.assignments_to_self.
+    assert not hasattr(a, 'a')
+    assert not hasattr(a, 'b')
+    assert hasattr(a, '__slots__')
+    assert hasattr(a, '__dict__')
+    # This now succeeds because there is a __dict__
+    a.z = 3
+
+    assert {'x', 'y'} < set(dir(a))
+    # Note that x and y ARE NOT in the dict. This is where the space
+    # savings come from.
+    assert dict(z=3) == a.__dict__
+
+
+def test_slots_plus_dict_empty():
+    """You will always have to pay the cost of having an empty dict
+    laying around though."""
+    class A(SlotsPlusDict):
+        def __init__(self, a, b):
+            self.x = a
+            self.y = b
+
+    a = A(1, 2)
+    assert hasattr(a, 'x')
+    assert hasattr(a, 'y')
+    # Just checking that we didn't pick up the wrong things in
+    # slots_metaclass.assignments_to_self.
+    assert not hasattr(a, 'a')
+    assert not hasattr(a, 'b')
+    assert hasattr(a, '__slots__')
+    assert hasattr(a, '__dict__')
+
+    assert {'x', 'y'} < set(dir(a))
+    # Doesn't lazy-initialize, unfortunately.
+    assert {} == a.__dict__
+
+
+def test_slots_existing():
+    """You can also provide your own slots if you like"""
+    class A(Slots):
+        __slots__ = ('z',)
+        def __init__(self, a, b):
+            self.x = a
+            self.y = b
+
+    a = A(1, 2)
+    assert hasattr(a, '__slots__')
+    assert not hasattr(a, '__dict__')
+    assert {'x', 'y', 'z'} == a.__slots__
+    assert hasattr(a, 'x')
+    assert hasattr(a, 'y')
+
+    # The instance does not have an attribute until you assign it.
+    assert not hasattr(a, 'z')
+    with pytest.raises(AttributeError):
+        a.z
+
+    # Assign to it, then hasattr will fail
+    a.z = 123
+    assert hasattr(a, 'z')
+
+    with pytest.raises(AttributeError):
+        a.totallynew = 456
+
+
+def test_slots_existing_with_dict():
+    """You can also provide your own slots if you like"""
+    class A(SlotsPlusDict):
+        __slots__ = {'z'}
+        def __init__(self, a, b):
+            self.x = a
+            self.y = b
+
+    a = A(1, 2)
+    assert hasattr(a, '__slots__')
+    assert hasattr(a, '__dict__')
+    # NOTE! even though __dict__ was injected internally into the
+    # slots array of the class, in the INSTANCE, __dict__ no longer
+    # appears.
+    assert {'x', 'y', 'z'} == a.__slots__
+    assert hasattr(a, 'x')
+    assert hasattr(a, 'y')
+
+    assert not hasattr(a, 'z')
+    with pytest.raises(AttributeError):
+        a.z
+
+    a.z = 123
+    assert hasattr(a, 'z')
+
+    a.totallynew = 456
+    assert a.totallynew == 456
+
+def test_much_inherit():
+    """Very long inheritance chain."""
+    class A(Slots):
+        def __init__(self):
+            self.x = 1
+
+    class B(A):
+        def __init__(self):
+            super().__init__()
+            self.y = 2
+
+    class C(B):
+        def __init__(self):
+            super().__init__()
+            self.z = 3
+
+    class D(C):
+        def __init__(self):
+            super().__init__()
+            self.u = 4
+
+    class E(D):
+        def __init__(self):
+            super().__init__()
+            self.v = 5
+
+    e = E()
+    assert hasattr(e, '__slots__')
+    assert not hasattr(e, '__dict__')
+    assert all(hasattr(e, attr) for attr in 'x y z u v'.split())
+
+    with pytest.raises(AttributeError):
+        e.w = 123
+
+
+def test_much_inherit_dict():
+    """Very long inheritance chain."""
+    class A(SlotsPlusDict):
+        def __init__(self):
+            self.x = 1
+
+    class B(A):
+        def __init__(self):
+            super().__init__()
+            self.y = 2
+
+    class C(B):
+        def __init__(self):
+            super().__init__()
+            self.z = 3
+
+    class D(C):
+        def __init__(self):
+            super().__init__()
+            self.u = 4
+
+    class E(D):
+        def __init__(self):
+            super().__init__()
+            self.v = 5
+
+    e = E()
+    assert hasattr(e, '__slots__')
+    assert hasattr(e, '__dict__')
+    assert all(hasattr(e, attr) for attr in 'x y z u v'.split())
+
+    e.w = 123
