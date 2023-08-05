97b665c288e7eec518425a52e47010d47a4e349b
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-05 15:24:32 +0000

    Add package alv/1.0.3/alv-1.0.3.tar.gz

---
+++ packages/alv/alv-1.0.3.tar.gz/alv-1.0.3/setup.py
@@ -0,0 +1,34 @@
+import setuptools
+
+with open("README.md", "r") as fh:
+    long_description = fh.read()
+
+with open('alv/version.py') as fh:
+    exec(fh.read())
+    
+setuptools.setup(
+    name="alv",
+    version=__version__,
+    author="Lars Arvestad",
+    author_email="arve@math.su.se",
+    description="A console-based sequence alignment viewer",
+    scripts = ['bin/alv'],
+    long_description=long_description,
+    long_description_content_type="text/markdown",
+    url="https://github.com/arvestad/alv",
+    test_suite = "tests",
+    packages=setuptools.find_packages(),
+    python_requires='>=3',
+    install_requires=[
+        'biopython>=1.70',
+        'colorama>=0.3.8',
+    ],
+    classifiers=(
+        "Programming Language :: Python :: 3",
+        "License :: OSI Approved :: GNU General Public License v3 (GPLv3)",
+        "Operating System :: OS Independent",
+        "Topic :: Scientific/Engineering :: Bio-Informatics",
+    ),
+)
+
+
