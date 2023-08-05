9e205dcc075ed8780af4dbaca8c6d0e2d4af7373
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-05 21:12:08 +0000

    Add package vector-2d/0.0.3/vector_2d-0.0.3.tar.gz

---
+++ packages/vector-2d/vector_2d-0.0.3.tar.gz/vector_2d-0.0.3/setup.py
@@ -0,0 +1,16 @@
+# -*- coding: utf-8 -*-
+
+from setuptools import setup
+
+setup(
+    name='vector_2d',
+    version='0.0.3',
+    description='A module witch implements a two-dimensional vector, both in cartesian and polar coordinates.',
+    long_description='A module witch implements a two-dimensional vector, both in cartesian and polar coordinates.',
+    license='MIT',
+    packages=['vector_2D'],
+    author='Álvaro Torres Ochaita',
+    author_email='torrestal@gmail.com',
+    keywords=['vector'],
+    url='https://github.com/betados/vector_2D'
+)
