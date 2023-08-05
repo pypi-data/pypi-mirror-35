edaca2ff259e1270821aaabea0429309396288dd
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-05 15:25:30 +0000

    Add package pypcapkit/0.9.10/pypcapkit-0.9.10-py2.py3-none-any.whl

---
+++ packages/pypcapkit/pypcapkit-0.9.10-py2.py3-none-any.whl/pcapkit/dumpkit/__init__.py
@@ -0,0 +1,44 @@
+# -*- coding: utf-8 -*-
+"""dump utilities
+
+`pcapkit.dumpkit` is the collection of dumpers for
+`pcapkit` implementation, which is alike those described
+in [`dictdumper`](https://github.com/JarryShaw/dictdumper).
+
+"""
+from pcapkit.ipsuite.pcap.frame import Frame
+from pcapkit.ipsuite.pcap.header import Header
+
+
+__all__ = ['PCAP', 'NotImplementedIO']
+
+
+class NotImplementedIO:
+    """Unspecified output format."""
+    @property
+    def kind(self):
+        return NotImplemented
+
+    def __init__(self, *args, **kwargs):
+        pass
+
+    def __call__(self, *args, **kwargs):
+        pass
+
+
+class PCAP:
+    """PCAP file dumper."""
+    @property
+    def kind(self):
+        return 'pcap'
+
+    def __init__(self, filename, *, protocol):
+        self._file = filename
+        packet = Header(protocol=protocol).data
+        with open(self._file, 'wb') as file:
+            file.write(packet)
+
+    def __call__(self, frame, **kwargs):
+        packet = Frame(frame).data
+        with open(self._file, 'ab') as file:
+            file.write(packet)
---
+++ packages/pypcapkit/pypcapkit-0.9.10-py2.py3-none-any.whl/pcapkit/protocols/internet/ipv6_opts.py
@@ -0,0 +1,144 @@
+# -*- coding: utf-8 -*-
+"""destination options for IPv6
+
+`pcapkit.protocols.internet.ipv6_opts` contains
+`IPv6_Opts` only, which implements extractor for
+Destination Options for IPv6 (IPv6-Opts), whose structure
+is described as below.
+
++-+-+-+-+-+-+-+-+-+-+-+-+-+-+-+-+-+-+-+-+-+-+-+-+-+-+-+-+-+-+-+-+
+|  Next Header  |  Hdr Ext Len  |                               |
++-+-+-+-+-+-+-+-+-+-+-+-+-+-+-+-+                               +
+|                                                               |
+.                                                               .
+.                            Options                            .
+.                                                               .
+|                                                               |
++-+-+-+-+-+-+-+-+-+-+-+-+-+-+-+-+-+-+-+-+-+-+-+-+-+-+-+-+-+-+-+-+
+
+"""
+# TODO: Implements extractor of all destination options.
+from pcapkit.corekit.infoclass import Info
+from pcapkit.protocols.protocol import Protocol
+
+
+__all__ = ['IPv6_Opts']
+
+
+class IPv6_Opts(Protocol):
+    """This class implements Destination Options for IPv6.
+
+    Properties:
+        * name -- str, name of corresponding procotol
+        * info -- Info, info dict of current instance
+        * alias -- str, acronym of corresponding procotol
+        * layer -- str, `Internet`
+        * length -- int, header length of corresponding protocol
+        * protocol -- str, name of next layer protocol
+        * protochain -- ProtoChain, protocol chain of current instance
+
+    Methods:
+        * read_ipv6_opts -- read Destination Options for IPv6 (IPv6-Opts)
+
+    Attributes:
+        * _file -- BytesIO, bytes to be extracted
+        * _info -- Info, info dict of current instance
+        * _protos -- ProtoChain, protocol chain of current instance
+
+    Utilities:
+        * _read_protos -- read next layer protocol type
+        * _read_fileng -- read file buffer
+        * _read_unpack -- read bytes and unpack to integers
+        * _read_binary -- read bytes and convert into binaries
+        * _read_packet -- read raw packet data
+        * _decode_next_layer -- decode next layer protocol type
+        * _import_next_layer -- import next layer protocol extractor
+
+    """
+    ##########################################################################
+    # Properties.
+    ##########################################################################
+
+    @property
+    def name(self):
+        """Name of current protocol."""
+        return 'IPv6 Hop-by-Hop Options'
+
+    @property
+    def alias(self):
+        """Acronym of corresponding procotol."""
+        return 'IPv6-Opts'
+
+    @property
+    def length(self):
+        """Header length of current protocol."""
+        return self._info.length
+
+    @property
+    def payload(self):
+        """Payload of current instance."""
+        if self.extension:
+            raise UnsupportedCall(f"'{self.__class__.__name__}' object has no attribute 'payload'")
+        return self._next
+
+    @property
+    def protocol(self):
+        """Name of next layer protocol."""
+        return self._info.next
+
+    ##########################################################################
+    # Methods.
+    ##########################################################################
+
+    def read_ipv6_opts(self, length, extension):
+        """Read Destination Options for IPv6.
+
+        Structure of IPv6-Opts header [RFC 8200]:
+            +-+-+-+-+-+-+-+-+-+-+-+-+-+-+-+-+-+-+-+-+-+-+-+-+-+-+-+-+-+-+-+-+
+            |  Next Header  |  Hdr Ext Len  |                               |
+            +-+-+-+-+-+-+-+-+-+-+-+-+-+-+-+-+                               +
+            |                                                               |
+            .                                                               .
+            .                            Options                            .
+            .                                                               .
+            |                                                               |
+            +-+-+-+-+-+-+-+-+-+-+-+-+-+-+-+-+-+-+-+-+-+-+-+-+-+-+-+-+-+-+-+-+
+
+            Octets      Bits        Name                    Discription
+              0           0     opt.next                Next Header
+              1           8     opt.length              Header Extensive Length
+              2          16     opt.options             Options
+
+        """
+        if length is None:
+            length = len(self)
+
+        _next = self._read_protos(1)
+        _hlen = self._read_unpack(1)
+        _opts = self._read_fileng(_hlen - 1)
+
+        ipv6_opts = dict(
+            next = _next,
+            length = _hlen + 1,
+            options = _opts,
+        )
+
+        if length is not None:
+            length -= ipv6_opts['length']
+        ipv6_opts['packet'] = self._read_packet(header=ipv6_opts['length'], payload=length)
+
+        if extension:
+            self._protos = None
+            return ipv6_opts
+        return self._decode_next_layer(ipv6_opts, _next, length)
+
+    ##########################################################################
+    # Data models.
+    ##########################################################################
+
+    def __init__(self, _file, length=None, *, extension=False, **kwargs):
+        self._file = _file
+        self._info = Info(self.read_ipv6_opts(length, extension))
+
+    def __length_hint__(self):
+        return 2
