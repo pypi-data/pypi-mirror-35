daa49acf74061599f38dad0df17fdedc30a495c7
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-05 20:13:01 +0000

    Add package hyperspaces/0.2.1/hyperspaces-0.2.1.tar.gz

---
+++ packages/hyperspaces/hyperspaces-0.2.1.tar.gz/hyperspaces-0.2.1/hyperspace/__init__.py
@@ -0,0 +1,36 @@
+from .hyperdrive import hyperband
+from .hyperdrive import hyperbelt
+from .hyperdrive import hyperdrive
+#from .hyperdrive import robodrive 
+
+from .rover.latin_hypercube_sampler import sample_latin_hypercube
+from .rover.latin_hypercube_sampler import lhs_start
+
+from .space.skopt.space import HyperInteger
+from .space.skopt.space import HyperReal
+from .space.skopt.space import HyperCategorical
+from .space.skopt.mapping_space import check_dimension
+from .space.skopt.mapping_space import create_hyperspace
+
+from .space.robo.space import RoboInteger
+from .space.robo.space import RoboReal
+from .space.robo.mapping_space import check_robo_dimension
+from .space.robo.mapping_space import create_robospace
+
+__version__ = "0.2"
+
+__all__ = (
+    "check_dimension",
+    "check_robo_dimension",
+    "create_hyperspace",
+    "dualdrive",
+    "HyperCategorical",
+    "hyperdrive",
+    "HyperInteger",
+    "HyperReal",
+#    "robodrive",
+    "RoboInteger",
+    "RoboReal",
+    "lhs_start",
+    "sample_latin_hypercube"
+)
