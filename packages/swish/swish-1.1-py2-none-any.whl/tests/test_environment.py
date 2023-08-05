2251d1a9857b9d4e26f2a89f024c5a7e97118dd2
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-05 15:30:30 +0000

    Add package swish/1.1/swish-1.1-py2-none-any.whl

---
+++ packages/swish/swish-1.1-py2-none-any.whl/tests/test_environment.py
@@ -0,0 +1,23 @@
+import unittest
+
+from swish.environment import Environment
+from swish.exceptions import ConfigurationError
+
+
+class EnvironmentTestCase(unittest.TestCase):
+
+    def test_parse_environment_string(self):
+        environment = Environment.parse_environment("test")
+        self.assertEqual(environment, Environment.Test)
+
+    def test_parse_environment_string_error(self):
+        with self.assertRaises(ConfigurationError):
+            Environment.parse_environment("invalid")
+
+    def test_parse_environment_object(self):
+        environment = Environment.parse_environment(Environment.Test)
+        self.assertEqual(environment, Environment.Test)
+
+    def test_environment_str(self):
+        environment = Environment.Test
+        self.assertEqual(str(environment), environment.name)
