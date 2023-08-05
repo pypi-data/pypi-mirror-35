8fc76dc12650e404274d02cfea630a7b868939d8
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-05 22:13:13 +0000

    Add package alerta/5.2.1/alerta-5.2.1-py2.py3-none-any.whl

---
+++ packages/alerta/alerta-5.2.1-py2.py3-none-any.whl/alertaclient/commands/cmd_uptime.py
@@ -0,0 +1,20 @@
+
+from datetime import datetime, timedelta
+
+import click
+
+
+@click.command('uptime', short_help='Display server uptime')
+@click.pass_obj
+def cli(obj):
+    """Display API server uptime in days, hours."""
+    client = obj['client']
+    status = client.mgmt_status()
+
+    now = datetime.fromtimestamp(int(status['time']) / 1000.0)
+    uptime = datetime(1, 1, 1) + timedelta(seconds=int(status['uptime']) / 1000.0)
+
+    click.echo('{0} up {1} days {2:02d}:{3:02d}'.format(
+        now.strftime('%H:%M'),
+        uptime.day - 1, uptime.hour, uptime.minute
+    ))
