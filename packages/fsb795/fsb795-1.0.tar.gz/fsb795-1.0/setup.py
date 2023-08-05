823da9705fa8ef0b0288214d9484e3fd71cb26b8
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-05 17:06:14 +0000

    Add package fsb795/1.0/fsb795-1.0.tar.gz

---
+++ packages/fsb795/fsb795-1.0.tar.gz/fsb795-1.0/setup.py
@@ -0,0 +1,24 @@
+import fsb795
+from setuptools import setup, find_packages
+setup(name='fsb795',
+    version='1.0',
+    py_modules=['fsb795'],
+    description='Obtaining the attributes of a qualified certificate, defined by the Order of the FSB 795', 
+    long_description='Obtaining the attributes of a qualified certificate, \n\tdefined by the Order of the Federal Security Service \n\tof the Russian Federation of December 27, 2011 No. 795', 
+    packages=find_packages(),
+    install_requires=[
+	'pyasn1', 'six'
+    ],
+    platforms=['any'],
+    classifiers=[
+	'Development Status :: 4 - Beta',
+	'Intended Audience :: Developers',
+	'License :: OSI Approved :: MIT License',
+	'Programming Language :: Python',
+	'Programming Language :: Python :: 2.7',
+	'Programming Language :: Python :: 3.5',
+	],
+    test_suite='test795',
+    author='Vladimir Orlov', 
+    author_email='vorlov@lissi.ru', 
+    license='GNU General Public License')
