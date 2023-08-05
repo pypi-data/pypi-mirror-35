044655d01c561d9e193788fcf455b41ab9e2b87e
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-05 16:55:08 +0000

    Add package arxiv-auth/0.0.8/arxiv-auth-0.0.8.tar.gz

---
+++ packages/arxiv-auth/arxiv-auth-0.0.8.tar.gz/arxiv-auth-0.0.8/setup.py
@@ -0,0 +1,20 @@
+"""Install arXiv auth package."""
+
+from setuptools import setup, find_packages
+
+setup(
+    name='arxiv-auth',
+    version='0.0.8',
+    packages=[f'arxiv.{package}' for package
+              in find_packages('./arxiv', exclude=['*test*'])],
+    install_requires=[
+        "pycountry",
+        "sqlalchemy",
+        "mysqlclient",
+        "python-dateutil",
+        "arxiv-base",
+        "pyjwt",
+        "redis"
+    ],
+    zip_safe=False
+)
