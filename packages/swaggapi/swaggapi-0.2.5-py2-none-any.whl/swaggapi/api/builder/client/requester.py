c89ba2b906fbc16d608680663b6757d382d33346
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-05 16:56:54 +0000

    Add package swaggapi/0.2.5/swaggapi-0.2.5-py2-none-any.whl

---
+++ packages/swaggapi/swaggapi-0.2.5-py2-none-any.whl/swaggapi/api/builder/client/requester.py
@@ -0,0 +1,34 @@
+import os
+
+from swaggapi.api.builder.common.model import AbstractAPIModel
+
+
+class Requester(object):
+    def __init__(self, host, port, base_url, logger=None):
+        self.base_url = os.path.join("http://{}:{}/".format(host, port),
+                                     base_url)
+        self.logger = logger
+
+    def request(self, request_type, method, data):
+        if not isinstance(data, AbstractAPIModel):
+            raise ValueError("data must be an instance of AbstractAPIModel!")
+
+        response = request_type.execute(self.base_url, method,
+                                        data.obj, logger=self.logger)
+
+        response_code = response.status_code
+        responses_models = request_type.RESPONSES_MODELS[method]
+        if responses_models is None or \
+                not response_code in responses_models:
+            responses_models = request_type.DEFAULT_RESPONSES
+        try:
+            content = response.json() if response.content else {}
+        except:
+            with open('error.html', "w") as f:
+                f.write(response.content)
+
+            raise RuntimeError("Invalid response received!")
+
+        response = responses_models[response_code](content)
+        response.code = response_code
+        return response
