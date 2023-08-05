2dc6be7312d6130b80ef94d12b9c42b145c2d58f
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-05 21:10:48 +0000

    Add package sayminimal/3.0.0/sayminimal-3.0.0.tar.gz

---
+++ packages/sayminimal/sayminimal-3.0.0.tar.gz/sayminimal-3.0.0/setup.py
@@ -0,0 +1,52 @@
+"""setuptools based setup module"""
+
+from setuptools import setup
+
+# Convert the markdown readme to ReST using Pandoc
+try:
+    import pypandoc
+    long_description = pypandoc.convert('README.md', 'rst')
+except ImportError:
+    long_description = open('README.md').read()
+
+
+setup(
+    name='sayminimal',
+    version="3.0.0",
+    description='A minimalist write-only Twitter/Mastodon client.',
+    long_description=long_description,
+    url='https://github.com/mduo13/sayminimal',
+    author='mDuo13',
+    author_email='mduo13@gmail.com',
+    license='GPLv3',
+    classifiers=[
+        'Development Status :: 3 - Alpha',
+        'Environment :: X11 Applications :: GTK',
+        'Intended Audience :: End Users/Desktop',
+        'License :: OSI Approved :: GNU General Public License v3 (GPLv3)',
+        'Operating System :: POSIX',
+        'Programming Language :: Python :: 3',
+        'Programming Language :: Python :: 3.4',
+        'Programming Language :: Python :: 3.5',
+        'Programming Language :: Python :: 3.6',
+        'Programming Language :: Python :: 3.7',
+        'Topic :: Communications',
+    ],
+    keywords='twitter mastodon social microblogging',
+    packages=[
+        'sayminimal',
+    ],
+    entry_points={
+        'console_scripts': [
+            'sayminimal = sayminimal.tweet:main',
+        ],
+    },
+    install_requires=[
+        'PyYAML',
+        'tweepy',
+        'Mastodon.py',
+    ],
+    package_data={
+        '': ["fusion.glade"],
+    }
+)
