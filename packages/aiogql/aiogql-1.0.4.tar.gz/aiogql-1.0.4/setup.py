bca95538618b647fc9b3d9ef0a28096a916ac156
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-05 17:05:34 +0000

    Add package aiogql/1.0.4/aiogql-1.0.4.tar.gz

---
+++ packages/aiogql/aiogql-1.0.4.tar.gz/aiogql-1.0.4/setup.py
@@ -0,0 +1,27 @@
+from setuptools import setup, find_packages
+
+setup(
+    name='aiogql',
+    version='1.0.4',
+    description='A fork of gql which uses asyncio & aiohttp for execution',
+    long_description=open('README.rst').read(),
+    url='https://github.com/cipriantarta/aiogql',
+    author='Ciprian Tarta',
+    license='MIT',
+    classifiers=[
+        'Intended Audience :: Developers',
+        'Topic :: Software Development :: Libraries',
+        'Programming Language :: Python :: 3.5',
+        'Programming Language :: Python :: 3.6',
+        'Programming Language :: Python :: Implementation :: PyPy',
+    ],
+    keywords='api graphql protocol rest relay gql client',
+    packages=find_packages(include=["gql*"]),
+    install_requires=[
+        'six>=1.10.0',
+        'graphql-core>=2.0',
+        'promise>=0.4.0',
+        'aiohttp>=3.1.3',
+    ],
+    tests_require=['pytest>=3.5', 'mock'],
+)
