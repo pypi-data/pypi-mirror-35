15f6969338df04e9e8888b30e731f4b1a2eba5d4
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-05 15:30:19 +0000

    Add package memoization/0.0.7/memoization-0.0.7.tar.gz

---
+++ packages/memoization/memoization-0.0.7.tar.gz/memoization-0.0.7/setup.py
@@ -0,0 +1,13 @@
+from setuptools import setup, find_packages
+
+setup(name='memoization',
+      version='0.0.7',
+      description='A minimalist functional caching lib for Python, with TTL and auto memory management support. '
+                  '(https://github.com/lonelyenvoy/python-memoization)',
+      keywords='memoization memorization remember decorator cache caching function callable'
+               'functional ttl limited capacity fast high-performance optimization',
+      url='https://github.com/lonelyenvoy/python-memoization',
+      author='lonelyenvoy',
+      author_email='petrinchor@gmail.com',
+      license='MIT',
+      packages=find_packages())
