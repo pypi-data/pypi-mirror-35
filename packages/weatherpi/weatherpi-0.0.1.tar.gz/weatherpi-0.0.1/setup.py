93f59e3c714aef749bdb36a80480812eeda328a4
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-05 15:29:27 +0000

    Add package weatherpi/0.0.1/weatherpi-0.0.1.tar.gz

---
+++ packages/weatherpi/weatherpi-0.0.1.tar.gz/weatherpi-0.0.1/setup.py
@@ -0,0 +1,10 @@
+from setuptools import setup, find_packages
+
+setup(
+    name="weatherpi",
+    description="A weatherstation running on Raspberry Pi",
+    author=["Noah Biederbeck", "Maximilian Sackel", "Jan Spinne"],
+    license="MIT",
+    version="0.0.1",
+    packages=find_packages(),
+)
