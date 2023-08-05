8b1acbdf8673a015be0b5d47151d857188955e6f
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-05 15:26:30 +0000

    Add package test-pseudo-j/0.5/test_pseudo_j-0.5.tar.gz

---
+++ packages/test-pseudo-j/test_pseudo_j-0.5.tar.gz/test_pseudo_j-0.5/pseudo_java/__init__.py
@@ -0,0 +1,13 @@
+import java_ast_translator
+import yaml
+
+def translate(name,source,options):
+	j = java_ast_translator.JavaASTTranslator()
+	return j.Test(name,source,options)
+
+def translate_to_yaml(name,source):
+	translation = translate(name,source,None)
+	print(translation)
+	noaliases = yaml.dumper.SafeDumper
+	noaliases.ignore_aliases = lambda self, data: True
+	return yaml.dump(translation,Dumper = noaliases)
