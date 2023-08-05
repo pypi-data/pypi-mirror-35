a1f6957170aaccb7b7113496674f45a2c8f4372e
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-05 18:11:07 +0000

    Add package aiortc/0.9.5/aiortc-0.9.5.tar.gz

---
+++ packages/aiortc/aiortc-0.9.5.tar.gz/aiortc-0.9.5/aiortc/rtcrtpparameters.py
@@ -0,0 +1,85 @@
+from collections import OrderedDict
+
+import attr
+
+
+@attr.s
+class RTCRtpCodecParameters:
+    """
+    The :class:`RTCRtpCodecParameters` dictionary provides information on
+    codec settings.
+    """
+    name = attr.ib(type=str)
+    "The codec MIME subtype, for instance `'PCMU'`."
+    clockRate = attr.ib(type=int)
+    "The codec clock rate expressed in Hertz."
+    channels = attr.ib(default=None)
+    "The number of channels supported (e.g. two for stereo)."
+    payloadType = attr.ib(default=None)
+    "The value that goes in the RTP Payload Type Field."
+    rtcpFeedback = attr.ib(default=attr.Factory(list))
+    "Transport layer and codec-specific feedback messages for this codec."
+    parameters = attr.ib(default=attr.Factory(OrderedDict))
+    "Codec-specific parameters available for signaling."
+
+    def __str__(self):
+        s = '%s/%d' % (self.name, self.clockRate)
+        if self.channels == 2:
+            s += '/2'
+        return s
+
+
+@attr.s
+class RTCRtpHeaderExtensionParameters:
+    """
+    The :class:`RTCRtpHeaderExtensionParameters` dictionary enables a header
+    extension to be configured for use within an :class:`RTCRtpSender` or
+    :class:`RTCRtpReceiver`.
+    """
+    id = attr.ib(type=int)
+    "The value that goes in the packet."
+    uri = attr.ib(type=str)
+    "The URI of the RTP header extension."
+
+
+@attr.s
+class RTCRtpCapabilities:
+    codecs = attr.ib(default=attr.Factory(list))
+
+
+@attr.s
+class RTCRtcpFeedback:
+    """
+    The :class:`RTCRtcpFeedback` dictionary provides information on RTCP feedback messages.
+    """
+    type = attr.ib()
+    parameter = attr.ib(default=None)
+
+
+@attr.s
+class RTCRtcpParameters:
+    """
+    The :class:`RTCRtcpParameters` dictionary provides information on RTCP settings.
+    """
+    cname = attr.ib(default=None)
+    "The Canonical Name (CNAME) used by RTCP."
+    mux = attr.ib(default=False)
+    "Whether RTP and RTCP are multiplexed."
+    ssrc = attr.ib(default=None)
+    "The Synchronization Source identifier."
+
+
+@attr.s
+class RTCRtpParameters:
+    """
+    The :class:`RTCRtpParameters` dictionary describes the configuration of
+    an :class:`RTCRtpReceiver` or an :class:`RTCRtpSender`.
+    """
+    codecs = attr.ib(default=attr.Factory(list))
+    "A list of :class:`RTCRtpCodecParameters` to send or receive."
+    headerExtensions = attr.ib(default=attr.Factory(list))
+    "A list of :class:`RTCRtpHeaderExtensionParameters`."
+    muxId = attr.ib(default='')
+    "The muxId assigned to the RTP stream, if any, empty string if unset."
+    rtcp = attr.ib(default=attr.Factory(RTCRtcpParameters))
+    "Parameters to configure RTCP."
---
+++ packages/aiortc/aiortc-0.9.5.tar.gz/aiortc-0.9.5/tests/test_rtcrtpsender.py
@@ -0,0 +1,121 @@
+import asyncio
+from unittest import TestCase
+
+from aiortc.codecs import PCMU_CODEC
+from aiortc.exceptions import InvalidStateError
+from aiortc.mediastreams import AudioStreamTrack, VideoStreamTrack
+from aiortc.rtcrtpparameters import RTCRtpCodecParameters, RTCRtpParameters
+from aiortc.rtcrtpsender import RTCRtpSender
+from aiortc.rtp import RtpPacket, is_rtcp, seq_plus_one
+
+from .utils import dummy_dtls_transport_pair, run
+
+
+class ClosedDtlsTransport:
+    state = 'closed'
+
+
+class FakeDtlsTransport:
+    queue = asyncio.Queue(maxsize=1)
+    state = 'connected'
+
+    async def _send_rtp(self, data):
+        if not is_rtcp(data):
+            packet = RtpPacket.parse(data)
+            await self.queue.put(packet)
+
+
+class RTCRtpSenderTest(TestCase):
+    def test_construct(self):
+        transport, _ = dummy_dtls_transport_pair()
+
+        sender = RTCRtpSender('audio', transport)
+        self.assertEqual(sender.kind, 'audio')
+        self.assertEqual(sender.transport, transport)
+
+    def test_construct_invalid_dtls_transport_state(self):
+        transport = ClosedDtlsTransport()
+
+        with self.assertRaises(InvalidStateError):
+            RTCRtpSender('audio', transport)
+
+    def test_connection_error(self):
+        """
+        Close the underlying transport before the sender.
+        """
+        transport, _ = dummy_dtls_transport_pair()
+
+        sender = RTCRtpSender(AudioStreamTrack(), transport)
+        self.assertEqual(sender.kind, 'audio')
+        self.assertEqual(sender.transport, transport)
+
+        run(sender.send(RTCRtpParameters(codecs=[PCMU_CODEC])))
+
+        run(transport.close())
+
+    def test_send_keyframe(self):
+        """
+        Ask for a keyframe.
+        """
+        transport = FakeDtlsTransport()
+
+        sender = RTCRtpSender(VideoStreamTrack(), transport)
+        self.assertEqual(sender.kind, 'video')
+        self.assertEqual(sender.transport, transport)
+
+        run(sender.send(RTCRtpParameters(codecs=[
+            RTCRtpCodecParameters(name='VP8', clockRate=90000, payloadType=100),
+        ])))
+
+        # wait for one packet to be transmitted, and ask for keyframe
+        packet = run(transport.queue.get())
+        sender._send_keyframe()
+
+        # wait for packet to be transmitted
+        rtx_packet = run(transport.queue.get())
+        self.assertEqual(rtx_packet.sequence_number, seq_plus_one(packet.sequence_number))
+
+        # clean shutdown
+        run(sender.stop())
+
+    def test_retransmit(self):
+        """
+        Ask for an RTP packet retransmission.
+        """
+        transport = FakeDtlsTransport()
+
+        sender = RTCRtpSender(VideoStreamTrack(), transport)
+        self.assertEqual(sender.kind, 'video')
+        self.assertEqual(sender.transport, transport)
+
+        run(sender.send(RTCRtpParameters(codecs=[
+            RTCRtpCodecParameters(name='VP8', clockRate=90000, payloadType=100),
+        ])))
+
+        # wait for one packet to be transmitted, and ask to retransmit
+        packet = run(transport.queue.get())
+        run(sender._retransmit(packet.sequence_number))
+
+        # wait for packet to be transmitted
+        rtx_packet = run(transport.queue.get())
+        self.assertEqual(rtx_packet.sequence_number, packet.sequence_number)
+
+        # clean shutdown
+        run(sender.stop())
+
+    def test_stop(self):
+        transport, _ = dummy_dtls_transport_pair()
+
+        sender = RTCRtpSender(AudioStreamTrack(), transport)
+        self.assertEqual(sender.kind, 'audio')
+        self.assertEqual(sender.transport, transport)
+
+        run(sender.send(RTCRtpParameters(codecs=[PCMU_CODEC])))
+
+        # clean shutdown
+        run(sender.stop())
+
+    def test_stop_before_send(self):
+        transport, _ = dummy_dtls_transport_pair()
+        sender = RTCRtpSender(AudioStreamTrack(), transport)
+        run(sender.stop())
