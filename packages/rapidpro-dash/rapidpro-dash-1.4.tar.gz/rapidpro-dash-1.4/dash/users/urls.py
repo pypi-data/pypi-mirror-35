530d4283fcb1fa7bff173ec44426da17d389aa8e
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-05 15:25:16 +0000

    Add package rapidpro-dash/1.4/rapidpro-dash-1.4.tar.gz

---
+++ packages/rapidpro-dash/rapidpro-dash-1.4.tar.gz/rapidpro-dash-1.4/dash/users/urls.py
@@ -0,0 +1,16 @@
+from smartmin.users.views import login
+
+from django.conf import settings
+from django.conf.urls import url
+from django.contrib.auth.views import logout
+
+from . import views
+
+logout_url = getattr(settings, "LOGOUT_REDIRECT_URL", None)
+
+urlpatterns = [
+    url(r"^login/$", login, dict(template_name="smartmin/users/login.html"), name="users.user_login"),
+    url(r"^logout/$", logout, dict(redirect_field_name="go", next_page=logout_url), name="users.user_logout"),
+]
+
+urlpatterns += views.UserCRUDL().as_urlpatterns()
