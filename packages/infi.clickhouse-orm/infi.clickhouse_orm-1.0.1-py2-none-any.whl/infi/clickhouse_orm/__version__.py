79cfee79f524cb832e62c45758777a5b089e2535
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-05 15:04:14 +0000

    Add package infi.clickhouse-orm/1.0.1/infi.clickhouse_orm-1.0.1-py2-none-any.whl

---
+++ packages/infi.clickhouse-orm/infi.clickhouse_orm-1.0.1-py2-none-any.whl/infi/clickhouse_orm/__version__.py
@@ -0,0 +1,11 @@
+__version__ = "1.0.1"
+__git_commiter_name__ = "Itai Shirav"
+__git_commiter_email__ = "itais@infinidat.com"
+__git_branch__ = u'(HEAD detached at v1.0.1)'
+__git_remote_tracking_branch__ = '(No remote tracking)'
+__git_remote_url__ = '(Not remote tracking)'
+__git_head_hash__ = '238fd11d6b04ad2c27f913b13ecfa790a727df7c'
+__git_head_subject__ = u'Finished Release v1.0.1'
+__git_head_message__ = u''
+__git_dirty_diff__ = u''
+__git_commit_date__ = '2018-08-13 08:46:10'
