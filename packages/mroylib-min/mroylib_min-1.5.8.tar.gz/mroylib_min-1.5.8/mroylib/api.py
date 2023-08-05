e1c94c178150002986d6e7e405d0e55ed13e6a24
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-05 15:06:08 +0000

    Add package mroylib-min/1.5.8/mroylib_min-1.5.8.tar.gz

---
+++ packages/mroylib-min/mroylib_min-1.5.8.tar.gz/mroylib_min-1.5.8/mroylib/api.py
@@ -0,0 +1,284 @@
+from qlib.data import dbobj, Cache
+from qlib.file import ensure_path
+
+from concurrent.futures.thread import  ThreadPoolExecutor
+from termcolor import colored
+from functools import partial
+
+import importlib
+import base64
+import pickle
+import json
+import os
+import sys
+import logging
+
+DEFAULT_BASE_DIR = os.path.expanduser("~/DefaultApiDir/plugins")
+MODULES_PATH = os.path.join(BASE_DIR, 'Plugins')
+ensure_path(DEFAULT_BASE_DIR)
+ensure_path(MODULES_PATH)
+
+
+class OO:pass
+class Repo(dbobj):pass
+
+def load(name):
+    try:
+        return importlib.import_module("Plugins.%s" % name)
+    except ModuleNotFoundError as e:
+        files = os.listdir(MODULES_PATH)
+        if (name + ".bash") in files:
+            def _run(*args, **kargs):
+                res = os.popen('bash %s {}'.format(" ".join(['"%s"' % i for i in args])) % os.path.join(MODULES_PATH, name + ".bash")).read()
+                return res
+            OO.run = _run
+            return OO
+        return str(e)
+    
+
+
+class BaseApi:
+    BASE_REPO = None
+    BASE_DIR = DEFAULT_BASE_DIR
+    MODULES_PATH = os.path.join(BASE_DIR, 'Plugins')
+    REPO_DB = os.path.join(BASE_DIR, 'repo.db')
+    REPO_NOW_USE = os.path.join(BASE_DIR, 'repo.now')
+    exes = ThreadPoolExecutor(max_workers=40)
+
+    def __init__(self, name, loop=None, callback=None):
+        assert self.__class__.BASE_REPO is not None
+        self.name = name
+        self.loop = loop
+        self.__callback = callback
+        self.Permission = None
+        self._Obj = self.load(self.name)
+        if hasattr(self._Obj, 'Permission'):
+            self.Permission = self._Obj.Permission
+
+
+    def set_callback(self, callback):
+        self.__callback = callback
+
+    def set_repo(self, name,url, path):
+        c = Cache(REPO_DB)
+        if path.endswith("/"):
+            path = path[:-1]
+        r = Repo(name=name, url=url, path=path)
+        os.popen("cd %s && git init || git remote add %s  %s" % (r.path.strip(), r.name.strip(), r.url.strip()))
+        r.save(c)
+
+    def update(self, repo_name):
+        c = Cache(REPO_DB)
+        r = c.query_one(Repo, name=repo_name)
+        if not r:
+            r = c.query_one(Repo)
+        
+        if r:
+            return os.popen("cd %s && pwd &&  git fetch --all && git reset --hard %s/master" % (r.path.strip(), r.name)).read()
+        else:
+            base_repo = self.__class__.BASE_REPO
+            assert base_repo is not None
+            self.set_repo('origin', base_repo, MODULES_PATH)
+            res = self.update(repo_name)
+            return "rebuild... " + res
+
+    def switch_repo(self, repo_name):
+        c = Cache(REPO_DB)
+        r = c.query_one(Repo, name=repo_name)
+        with open(REPO_NOW_USE, 'w') as fp:
+            fp.write(r.path.strip())
+    
+    def use_repo(self):
+        if os.path.exists(REPO_NOW_USE):
+            with open(REPO_NOW_USE) as fp:
+                N = fp.read()
+                dname = os.path.dirname(N)
+                mname = os.path.basename(N)
+                if dname not in sys.path:
+                    sys.path += [dname]
+                return mname
+        else:
+            dname = os.path.dirname(MODULES_PATH)
+            mname = os.path.basename(MODULES_PATH)
+            if dname not in sys.path:
+                sys.path += [dname]
+            return mname
+
+
+    def cat(self, file):
+        c = Cache(REPO_DB)
+        r = c.query_one(Repo, name=self.use_repo())
+        if r:
+            files = os.listdir(r.path)
+            for f in fils:
+                if file in f:
+                    with open(os.path.join(r.path, f)) as fp:
+                        return fp.read()
+            return "no file: %s" % file
+        return "no current repo"
+
+
+    def load(self, name):
+        mname = self.use_repo()
+        try:
+            return importlib.import_module("%s.%s" % (mname, name))
+        except ModuleNotFoundError as e:
+            files = os.listdir(MODULES_PATH)
+            if (name + ".bash") in files:
+                def _run(*args, **kargs):
+                    res = os.popen('bash %s {}'.format(" ".join(['"%s"' % i for i in args])) % os.path.join(MODULES_PATH, name + ".bash")).read()
+                    return res
+                OO.run = _run
+                return OO
+            return str(e)
+        
+
+    def run(self, *args, **kargs):
+        if self.name == 'self':
+            if len(args) == 1 and args[0]:
+
+                wargs = args[0].split(",")
+                if len(wargs) ==3:
+                    name = ''
+                    path = ''
+                    url = ''
+                    for i in wargs:
+
+                        if 'https://github' in  i:
+                            url = i.strip()
+                            continue
+                        elif '/' in i:
+                            path = i
+                            if not os.path.exists(path):
+                                try:
+                                    os.mkdir(path)
+                                except Exception as e:
+                                    return path + " Not found"
+                            continue
+
+                        name = i
+
+                    self.set_repo(name, url, path)
+                    return "repo set : name=%s url=%s path=%s " % (name, url, path)
+                elif len(wargs) == 2 and wargs[0] == 'use':
+                    self.switch_repo(wargs[1].strip())
+                    return 'switch to : %s' % wargs[1]
+                elif wargs[0] == 'ls':
+                    c = Cache(REPO_DB)
+                    rs = c.query(Repo)
+                    return json.dumps([r.get_dict() for r in rs])
+                elif len(wargs) ==2 and wargs[0] == 'cat':
+                    
+                    return self.cat(wargs[1])
+                else:
+                    c = Cache(REPO_DB)
+                    r = c.query_one(Repo, name=wargs[0])
+                    if r:
+                        res = self.update(r.name)
+                        return res
+                    return """suport:
+                    Was sagst du? %s 
+                        curl http://xxxx -d module=self -d args=ls # ls all repo
+                        curl http://xxxx -d module=self -d args=help
+                        curl http://xxxx -d module=self -d args=name,url,path # set repo
+                        curl http://xxxx -d module=self -d args=use,origin  # switch repo
+                        curl http://xxxx -d module=self  # this will update use now repo
+
+                    """ % wargs
+                
+            else:
+                res = self.update("")
+                return res
+        else:
+            
+            if isinstance(self._Obj, str):
+                return self._Obj
+            if not self.loop:
+                logging.warn("loop is None.")
+            
+            fff = partial(self._Obj.run, *args, **kargs)
+            if 'loop' in self._Obj.run.__code__.co_varnames:
+                logging.info("patch with loop")
+                
+                fff = partial(fff, loop=self.loop)
+
+                
+            
+
+            futu = self.__class__.exes.submit(fff)
+            if hasattr(self._Obj, 'callback'):
+                self.__callback = self._Obj.callback
+            futu.add_done_callback(self.callback)
+
+    def _callback(self, r):
+        print(colored("[+]",'green'), r)
+
+    def callback(self, result):
+        raise NotImplementedError("Not implement!!")
+
+
+
+class BaseArgs:
+
+    def __init__(self, handle, tp=None):
+        self.handle = handle
+        self.args = []
+        self.kargs = dict()
+        self._tp = tp
+        self.parse()
+
+
+    def get_parameter(self):
+        raise NotImplementedError("")
+
+    def get_parameter_keys(self):
+        raise NotImplementedError("")
+
+    def finish(self,D):
+        raise NotImplementedError("")
+
+    def parse(self):    
+        tp = self.get_parameter("type")
+        args = self.get_parameter('args')
+        self.module = self.get_parameter('module')
+        self.type = tp
+        self.kwargs = {}
+
+        keys = self.get_parameter_keys()
+        for k in keys:
+            if k in ['type', 'args', 'module']:
+                continue
+            self.kwargs[k] = self.get_parameter(k)
+
+        if tp == 'base64':
+            if isinstance(args, str):
+                args = args.encode('utf8', 'ignore')
+            args = json.loads(base64.b64decode(args))
+            if isinstance(args, (list, tuple,)):
+                self.args = args
+            else:
+                self.kargs = args
+        elif tp =='json':
+            args = json.loads(args)
+            if isinstance(args, (list, tuple,)):
+                self.args = args
+            else:
+                self.kargs = args
+        else:
+            self.args = [args]
+
+    def after_dealwith(self, data):
+        b_data = {'res':None, 'type':'json'}
+        
+        if isinstance(data, str) or isinstance(data, (list, dict, tuple, )):
+            b_data['res'] = data
+        elif isinstance(data, (int,float,bool,)):
+            b_data['res'] = data
+        else:
+            b_data['res'] = base64.b64encode(pickle.dumps(data))
+            b_data['type'] = 'pickle'
+
+        D = json.dumps(b_data)
+        self.finish(D)
+            
+
