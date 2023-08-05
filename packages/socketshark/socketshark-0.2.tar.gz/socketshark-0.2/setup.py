101d0ac4b1871dcd7d0d31e45635213a94eaf344
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-05 15:29:49 +0000

    Add package socketshark/0.2/socketshark-0.2.tar.gz

---
+++ packages/socketshark/socketshark-0.2.tar.gz/socketshark-0.2/setup.py
@@ -0,0 +1,48 @@
+from setuptools import setup
+
+install_requires = [
+    'aiohttp',
+    'aioredis',
+    'click',
+    'structlog[dev]',
+    'websockets',
+]
+
+tests_require = install_requires + [
+    'aioresponses',
+    'pytest',
+    'pytest-asyncio',
+]
+
+setup(
+    name='socketshark',
+    version='0.2',
+    url='http://github.com/closeio/socketshark',
+    license='MIT',
+    description='WebSocket message router',
+    test_suite='tests',
+    tests_require=tests_require,
+    platforms='any',
+    install_requires=install_requires,
+    classifiers=[
+        'Intended Audience :: Developers',
+        'License :: OSI Approved :: MIT License',
+        'Operating System :: OS Independent',
+        'Topic :: Software Development :: Libraries :: Python Modules',
+        'Programming Language :: Python',
+        'Programming Language :: Python :: 3',
+        'Programming Language :: Python :: 3.5',
+        'Programming Language :: Python :: 3.6',
+        'Programming Language :: Python :: 3.7',
+    ],
+    packages=[
+        'socketshark',
+        'socketshark.backend',
+        'socketshark.metrics',
+    ],
+    entry_points={
+        'console_scripts': [
+            'socketshark = socketshark.__main__:run',
+        ],
+    },
+)
