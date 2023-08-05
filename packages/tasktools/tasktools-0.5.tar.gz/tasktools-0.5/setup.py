5da78674f1ee6354a69b373096d6c8f09647b129
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-05 15:30:28 +0000

    Add package tasktools/0.5/tasktools-0.5.tar.gz

---
+++ packages/tasktools/tasktools-0.5.tar.gz/tasktools-0.5/setup.py
@@ -0,0 +1,11 @@
+from setuptools import setup
+
+setup(name='tasktools',
+      version='0.5',
+      description='Some useful tools for asycnio Tasks',
+      url='http://gitlab.csn.uchile.cl/dpineda/tasktools',
+      author='David Pineda Osorio',
+      author_email='dpineda@csn.uchile.cl',
+      license='MIT',
+      packages=['tasktools'],
+      zip_safe=False)
