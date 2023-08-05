80bc347d86c776642f63b82e89ebe6a20d3e9c9a
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-05 21:09:25 +0000

    Add package fsoopify/0.1.19/fsoopify-0.1.19.tar.gz

---
+++ packages/fsoopify/fsoopify-0.1.19.tar.gz/fsoopify-0.1.19/fsoopify/__init__.py
@@ -0,0 +1,15 @@
+#!/usr/bin/env python
+# -*- coding: utf-8 -*-
+#
+# Copyright (c) 2017~2999 - cologler <skyoflw@gmail.com>
+# ----------
+#
+# ----------
+
+from .paths import Path
+from .nodes import (
+    NodeInfo, DirectoryInfo, FileInfo,
+)
+from .serialize import (
+    FormatNotFoundError, SerializeError
+)
