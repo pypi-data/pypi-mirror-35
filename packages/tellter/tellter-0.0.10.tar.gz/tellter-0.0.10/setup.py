9cf1737d459fbfc98908c4c715a66371fc68b68e
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-05 20:12:06 +0000

    Add package tellter/0.0.10/tellter-0.0.10.tar.gz

---
+++ packages/tellter/tellter-0.0.10.tar.gz/tellter-0.0.10/setup.py
@@ -0,0 +1,21 @@
+import setuptools
+
+with open("README.md", "r") as fh:
+    long_description = fh.read()
+
+setuptools.setup(
+    name="tellter",
+    version="0.0.10",
+    author="Tellter",
+    author_email="contact@tellter.net",
+    description="Tellter Official Public API - Currently supports TNS and Safechat.",
+    long_description=long_description,
+    long_description_content_type="text/markdown",
+    url="https://tellter.com/",
+    packages=setuptools.find_packages(),
+    classifiers=[
+        "Programming Language :: Python :: 3",
+        "License :: OSI Approved :: MIT License",
+        "Operating System :: OS Independent",
+    ],
+)
