3d7d9d03e2f082a36b29f306fa5876cc976a30cc
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-05 16:54:36 +0000

    Add package somosmas-common/1.0.4/somosmas_common-1.0.4.tar.gz

---
+++ packages/somosmas-common/somosmas_common-1.0.4.tar.gz/somosmas_common-1.0.4/setup.py
@@ -0,0 +1,22 @@
+#!/usr/bin/env python
+
+from distutils.core import setup
+
+from setuptools import find_packages
+
+setup(
+    name='somosmas_common',
+    version='1.0.4',
+    description=(
+        'Package for the software components that are common for the S+ projects',  # noqa
+    ),
+    author='Omar Diaz',
+    author_email='zcool2005@gmail.com',
+    packages=find_packages(),
+    include_package_data=True,
+    install_requires=[
+      'requests',
+      'django-oauth-toolkit'
+    ],
+    zip_safe=False,
+)
