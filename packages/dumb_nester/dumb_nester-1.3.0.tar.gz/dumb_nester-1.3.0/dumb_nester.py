b4f3b66122c1a1e46e287ea8f90586998924ce8b
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-05 17:05:12 +0000

    Add package dumb_nester/1.3.0/dumb_nester-1.3.0.tar.gz

---
+++ packages/dumb_nester/dumb_nester-1.3.0.tar.gz/dumb_nester-1.3.0/dumb_nester.py
@@ -0,0 +1,10 @@
+def print_lol(the_list,indent=False,val=0):
+    for each_item in the_list:
+        if isinstance(each_item,list):
+            print_lol(each_item,indent,val+1)
+        else:
+            if indent: 
+                for tab_stop in range(val):
+                    print("\t",end='')
+                print(each_item)
+
