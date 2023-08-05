9bc77b10928ea3bad7fdce90d559bd7d7fb61f4f
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-05 15:06:27 +0000

    Add package yahoofinancials/0.9/yahoofinancials-0.9.tar.gz

---
+++ packages/yahoofinancials/yahoofinancials-0.9.tar.gz/yahoofinancials-0.9/setup.py
@@ -0,0 +1,24 @@
+try:
+    from setuptools import setup
+except ImportError:
+    from distutils.core import setup
+
+setup(
+    name='yahoofinancials',
+    version='0.9',
+    description='A powerful financial data module used for pulling both fundamental and technical data from Yahoo Finance',
+    url='https://github.com/JECSand/yahoofinancials',
+    download_url='https://github.com/JECSand/yahoofinancials/archive/0.9.tar.gz',
+    author='Connor Sanders',
+    author_email='connor@exceleri.com',
+    license='MIT',
+    keywords = ['finance data', 'stocks', 'commodities', 'cryptocurrencies', 'currencies', 'forex', 'yahoo finance'],
+    packages=['yahoofinancials'],
+    install_requires=[
+        "requests",
+        "beautifulsoup4",
+        "pytz"
+    ],
+    classifiers=[],
+    zip_safe=False
+)
