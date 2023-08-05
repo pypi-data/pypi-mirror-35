719093b1727efe0ce6e78e284a1d249f93852e66
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-05 15:30:49 +0000

    Add package django-honeypot/0.7.0/django-honeypot-0.7.0.tar.gz

---
+++ packages/django-honeypot/django-honeypot-0.7.0.tar.gz/django-honeypot-0.7.0/setup.py
@@ -0,0 +1,36 @@
+from setuptools import setup, find_packages
+
+long_description = open('README.rst').read()
+
+setup(
+    name='django-honeypot',
+    version="0.7.0",
+    package_dir={'honeypot': 'honeypot'},
+    packages=['honeypot', 'honeypot.templatetags'],
+    description='Django honeypot field utilities',
+    author='James Turk',
+    author_email='james.p.turk@gmail.com',
+    license='BSD License',
+    url='http://github.com/jamesturk/django-honeypot/',
+    long_description=long_description,
+    platforms=["any"],
+    classifiers=[
+        'Development Status :: 4 - Beta',
+        'Intended Audience :: Developers',
+        'License :: OSI Approved :: BSD License',
+        'Natural Language :: English',
+        'Operating System :: OS Independent',
+        'Programming Language :: Python',
+        'Programming Language :: Python :: 2',
+        'Programming Language :: Python :: 2.7',
+        'Programming Language :: Python :: 3',
+        'Programming Language :: Python :: 3.4',
+        'Programming Language :: Python :: 3.5',
+        'Programming Language :: Python :: 3.6',
+        'Programming Language :: Python :: 3.7',
+        'Environment :: Web Environment',
+    ],
+    include_package_data=True,
+    zip_safe=False,
+    install_requires=['setuptools'],
+)
