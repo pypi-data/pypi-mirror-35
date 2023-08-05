99d2aa71e3e2a4a756c8a0dc9205eb6b747d0eb1
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-05 15:05:36 +0000

    Add package hokusai/0.4.6/hokusai-0.4.6-py2-none-any.whl

---
+++ packages/hokusai/hokusai-0.4.6-py2-none-any.whl/hokusai/commands/check.py
@@ -0,0 +1,109 @@
+import os
+
+from hokusai.lib.command import command
+from hokusai.lib.config import config
+from hokusai.services.ecr import ECR
+from hokusai.services.kubectl import Kubectl
+from hokusai.lib.common import print_red, print_green, shout
+from hokusai.lib.exceptions import CalledProcessError, HokusaiError
+
+@command
+def check():
+  return_code = 0
+
+  def check_ok(check_item):
+    print_green(u'\u2714 ' + check_item + ' found')
+
+  def check_err(check_item):
+    print_red(u'\u2718 ' + check_item + ' not found')
+
+  config.check()
+
+  try:
+    config.project_name
+    check_ok('Config project-name')
+  except HokusaiError:
+    check_err('Config project-name')
+
+  try:
+    shout('docker --version')
+    check_ok('docker')
+  except CalledProcessError:
+    check_err('docker')
+    return_code += 1
+
+  try:
+    shout('docker-compose --version')
+    check_ok('docker-compose')
+  except CalledProcessError:
+    check_err('docker-compose')
+    return_code += 1
+
+  try:
+    shout('kubectl version')
+    check_ok('kubectl')
+  except CalledProcessError:
+    check_err('kubectl')
+    return_code += 1
+
+  try:
+    shout('git version')
+    check_ok('git')
+  except CalledProcessError:
+    check_err('git')
+    return_code += 1
+
+  if os.environ.get('AWS_ACCESS_KEY_ID') is not None:
+    check_ok('$AWS_ACCESS_KEY_ID')
+  else:
+    check_err('$AWS_ACCESS_KEY_ID')
+    return_code += 1
+
+  if os.environ.get('AWS_SECRET_ACCESS_KEY') is not None:
+    check_ok('$AWS_SECRET_ACCESS_KEY')
+  else:
+    check_err('$AWS_SECRET_ACCESS_KEY')
+    return_code += 1
+
+  ecr = ECR()
+  if ecr.project_repo_exists():
+    check_ok("ECR repository '%s'" % config.project_name)
+  else:
+    check_err("ECR repository '%s'" % config.project_name)
+    return_code += 1
+
+  if not os.path.isfile(os.path.join(os.getcwd(), 'hokusai/build.yml')):
+    if os.path.isfile(os.path.join(os.getcwd(), 'hokusai/common.yml')):
+      check_ok('./hokusai/common.yml')
+    else:
+      check_err('./hokusai/build.yml')
+  else:
+    check_ok('./hokusai/build.yml')
+    return_code += 1
+
+  if os.path.isfile(os.path.join(os.getcwd(), 'hokusai/development.yml')):
+    check_ok('./hokusai/development.yml')
+  else:
+    check_err('./hokusai/development.yml')
+    return_code += 1
+
+  if os.path.isfile(os.path.join(os.getcwd(), 'hokusai/test.yml')):
+    check_ok('./hokusai/test.yml')
+  else:
+    check_err('./hokusai/test.yml')
+    return_code += 1
+
+  for context in ['staging', 'production']:
+    if context in Kubectl('staging').contexts():
+      check_ok("kubectl context '%s'" % context)
+    else:
+      check_err("kubectl context '%s'" % context)
+      return_code += 1
+
+    if os.path.isfile(os.path.join(os.getcwd(), "hokusai/%s.yml" % context)):
+      check_ok("./hokusai/%s.yml" % context)
+    else:
+      check_err("./hokusai/%s.yml" % context)
+      return_code += 1
+
+  return return_code
---
+++ packages/hokusai/hokusai-0.4.6-py2-none-any.whl/hokusai/lib/config.py
@@ -0,0 +1,56 @@
+import os
+import sys
+
+from collections import OrderedDict
+
+import yaml
+
+from hokusai.lib.common import print_red, YAML_HEADER
+from hokusai.lib.exceptions import HokusaiError
+
+HOKUSAI_CONFIG_FILE = os.path.join(os.getcwd(), 'hokusai', 'config.yml')
+
+class HokusaiConfig(object):
+  def create(self, project_name):
+    config = OrderedDict([
+      ('project-name', project_name)
+    ])
+
+    with open(HOKUSAI_CONFIG_FILE, 'w') as f:
+      payload = YAML_HEADER + yaml.safe_dump(config, default_flow_style=False)
+      f.write(payload)
+
+    return self
+
+  def check(self):
+    if not os.path.isfile(HOKUSAI_CONFIG_FILE):
+      raise HokusaiError("Hokusai is not set up for this project - run 'hokusai setup'")
+    return self
+
+  def get(self, key):
+    self.check()
+    config_file = open(HOKUSAI_CONFIG_FILE, 'r')
+    config_data = config_file.read()
+    config_file.close()
+    config = yaml.safe_load(config_data)
+    try:
+      return config[key]
+    except KeyError:
+      return None
+
+  @property
+  def project_name(self):
+    project = self.get('project-name')
+    if project is None:
+      raise HokusaiError("Unconfigured 'project-name'! Plz check ./hokusai/config.yml")
+    return project
+
+  @property
+  def pre_deploy(self):
+    return self.get('pre-deploy')
+
+  @property
+  def post_deploy(self):
+    return self.get('post-deploy')
+
+config = HokusaiConfig()
