b8eb339cb2b0180111f88b7960a26bace063816f
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-05 17:06:54 +0000

    Add package pyvter/0.0.1/pyvter-0.0.1.tar.gz

---
+++ packages/pyvter/pyvter-0.0.1.tar.gz/pyvter-0.0.1/setup.py
@@ -0,0 +1,18 @@
+import setuptools
+
+setuptools.setup(
+    name="pyvter",
+    version="0.0.1",
+    author="jhd",
+    author_email="author@example.com",
+    description="placeholder package",
+    long_description="placeholder",
+    long_description_content_type="text/markdown",
+    url="https://github.com/jhd/pyvter",
+    packages=setuptools.find_packages(),
+    classifiers=[
+        "Programming Language :: Python :: 3",
+        "License :: OSI Approved :: GNU Affero General Public License v3",
+        "Operating System :: OS Independent",
+    ],
+)
