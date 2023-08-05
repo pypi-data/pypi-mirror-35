ce61614161ac062f6b08ec157f0fab51e6e05b33
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-05 16:56:45 +0000

    Add package workfront-sdk/0.15/workfront-sdk-0.15.tar.gz

---
+++ packages/workfront-sdk/workfront-sdk-0.15.tar.gz/workfront-sdk-0.15/workfront/objects/status.py
@@ -0,0 +1,22 @@
+
+class WFTaskStatus:
+    '''
+    @summary: Possible status of a task.
+    '''
+    new = "NEW"
+    in_progress = "INP"
+    complete = "CPL"
+    failure = "FLR"
+    ready_to_start = "RTS"
+
+
+class WFProjectStatus:
+    '''
+    @summary: Possible status of a project.
+    '''
+    planning = "PLN"
+    on_hold = "ONH"
+    in_progress = "CUR"
+    cancelled = "DED"
+    rejected = "REJ"
+    complete = "CPL"
