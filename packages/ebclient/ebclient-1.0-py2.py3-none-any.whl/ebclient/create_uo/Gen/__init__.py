01e261b8ccf95cf452c5cd6f752355eb2e4be298
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-05 21:08:55 +0000

    Add package ebclient/1.0/ebclient-1.0-py2.py3-none-any.whl

---
+++ packages/ebclient/ebclient-1.0-py2.py3-none-any.whl/ebclient/create_uo/Gen/__init__.py
@@ -0,0 +1,11 @@
+#!/usr/bin/env python
+# -*- coding: utf-8 -*-
+
+"""Key generation constants"""
+LEGACY_RANDOM = 0
+CLIENT = 1
+COMP1 = 2
+COMP2 = 3
+COMP3 = 4
+SERVER_RANDOM = 5
+SERVER_DERIVED = 6
