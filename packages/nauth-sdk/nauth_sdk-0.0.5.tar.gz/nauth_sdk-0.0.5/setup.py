754e4cb44830928f3ac9e9e74de5f43a52fa0704
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-05 22:12:52 +0000

    Add package nauth-sdk/0.0.5/nauth_sdk-0.0.5.tar.gz

---
+++ packages/nauth-sdk/nauth_sdk-0.0.5.tar.gz/nauth_sdk-0.0.5/setup.py
@@ -0,0 +1,22 @@
+import setuptools
+
+with open("README.md", "r") as fh:
+    long_description = fh.read()
+
+setuptools.setup(
+    name="nauth_sdk",
+    version="0.0.5",
+    author="ntkomata",
+    author_email="ntkomata@outlook.com",
+    description="A simple sdk for nauth",
+    long_description=long_description,
+    long_description_content_type="text/markdown",
+    url="",
+    install_requires=['requests'],
+    packages=setuptools.find_packages(),
+    classifiers=[
+        "Programming Language :: Python :: 3",
+        "License :: OSI Approved :: MIT License",
+        "Operating System :: OS Independent",
+    ],
+)
