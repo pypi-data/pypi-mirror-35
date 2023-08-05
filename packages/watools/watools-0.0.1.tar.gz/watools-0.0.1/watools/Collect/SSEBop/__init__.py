ebeaf94ef915c89e767719d2caac0d20ef6a94ca
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-05 18:09:18 +0000

    Add package watools/0.0.1/watools-0.0.1.tar.gz

---
+++ packages/watools/watools-0.0.1.tar.gz/watools-0.0.1/watools/Collect/SSEBop/__init__.py
@@ -0,0 +1,31 @@
+# -*- coding: utf-8 -*-
+"""
+Authors: Tim Hessels and Gonzalo Espinoza
+         UNESCO-IHE 2017
+Contact: t.hessels@unesco-ihe.org
+         g.espinoza@unesco-ihe.org
+Repository: https://github.com/wateraccounting/watools
+Module: Collect/SSEBop
+
+Restrictions:
+The data and this python file may not be distributed to others without
+permission of the WA+ team due data restriction of the SSEBop developers.
+
+Description:
+This module downloads SSEBop data from
+ftp.wateraccounting.unesco-ihe.org. Use the SSEBop.monthly function to
+download and create monthly SSEBop images in Gtiff format.
+Data is available between 2003-01-01 till 2014-10-31. If the FTP version is used
+The data goes till present if the V4 version is used (Default)
+
+Examples:
+from watools.Collect import SSEBop
+SSEBop.monthly(Dir='C:/Temp/', Startdate='2008-12-01', Enddate='2011-01-20',
+           latlim=[-10, 30], lonlim=[-20, -10])
+"""
+
+from .monthly import main as monthly
+
+__all__ = ['monthly']
+
+__version__ = '0.1'
