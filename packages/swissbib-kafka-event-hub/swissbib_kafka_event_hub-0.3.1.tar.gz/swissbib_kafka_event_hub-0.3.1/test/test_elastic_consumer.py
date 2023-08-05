d91ab28ac13f445227bc4f3ef31fa85602f32c91
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-05 15:24:37 +0000

    Add package swissbib-kafka-event-hub/0.3.1/swissbib_kafka_event_hub-0.3.1.tar.gz

---
+++ packages/swissbib-kafka-event-hub/swissbib_kafka_event_hub-0.3.1.tar.gz/swissbib_kafka_event_hub-0.3.1/test/test_elastic_consumer.py
@@ -0,0 +1,25 @@
+import logging
+import os
+import sys
+
+
+sys.path.append(os.path.dirname(os.path.dirname(os.path.abspath(__file__))))
+from kafka_event_hub.consumers import ElasticConsumer
+
+
+class TestElasticConsumer(object):
+
+    def setup_class(self):
+        self.consumer = ElasticConsumer('configs/elastic/elastic.yml')
+
+    def test_consume(self):
+        self.consumer.consume()
+
+
+if __name__ == '__main__':
+    logging.basicConfig(stream=sys.stdout, level=logging.DEBUG)
+    consumer = TestElasticConsumer()
+    logging.debug('Setup class')
+    consumer.setup_class()
+    logging.debug('Consume message')
+    consumer.test_consume()
