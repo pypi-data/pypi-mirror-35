2b45e0d3087ea328df8aaf9ad6f55801e89c7953
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-05 20:11:34 +0000

    Add package wurst/0.1.2/wurst-0.1.2-py3-none-any.whl

---
+++ packages/wurst/wurst-0.1.2-py3-none-any.whl/wurst/brightway/__init__.py
@@ -0,0 +1,6 @@
+try:
+    from .extract_database import extract_brightway2_databases
+    from .write_database import write_brightway2_database
+except ImportError:
+    print("Brightway2 not present")
+    extract_brightway2_databases = write_brightway2_database = None
