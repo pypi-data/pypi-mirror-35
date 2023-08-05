6bb8c0250c0183d40c584276fb8301a0dec45b95
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-05 18:09:42 +0000

    Add package tellter/0.0.1/tellter-0.0.1.tar.gz

---
+++ packages/tellter/tellter-0.0.1.tar.gz/tellter-0.0.1/setup.py
@@ -0,0 +1,21 @@
+import setuptools
+
+with open("README.md", "r") as fh:
+    long_description = fh.read()
+
+setuptools.setup(
+    name="tellter",
+    version="0.0.1",
+    author="Tellter",
+    author_email="contact@tellter.net",
+    description="Tellter Public API",
+    long_description=long_description,
+    long_description_content_type="text/markdown",
+    url="https://tellter.com/",
+    packages=setuptools.find_packages(),
+    classifiers=[
+        "Programming Language :: Python :: 3",
+        "License :: OSI Approved :: MIT License",
+        "Operating System :: OS Independent",
+    ],
+)
