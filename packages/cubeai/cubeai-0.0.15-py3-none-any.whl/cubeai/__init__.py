cfd29ce313b400d3914fafbf38f24a1e2f416e31
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-05 20:12:03 +0000

    Add package cubeai/0.0.15/cubeai-0.0.15-py3-none-any.whl

---
+++ packages/cubeai/cubeai-0.0.15-py3-none-any.whl/cubeai/__init__.py
@@ -0,0 +1,7 @@
+from cubeai.solver.search import dfs, bfs, ucs, a_star, null_heuristic
+from cubeai.cube.cube import Cube, Face, Action, NUM_FACES
+from cubeai.solver.solver import solve, random_actions, \
+    generate_random_sequence
+from cubeai.solver.learning import scrambled_cube_generator, batch_generator
+
+name = "cubeai"
