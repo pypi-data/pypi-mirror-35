d105bdc45fa1709c19f5543c63d74dd43b3f8583
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-05 15:30:10 +0000

    Add package lfdeploy/1.2.5/lfdeploy-1.2.5.tar.gz

---
+++ packages/lfdeploy/lfdeploy-1.2.5.tar.gz/lfdeploy-1.2.5/setup.py
@@ -0,0 +1,19 @@
+import setuptools
+
+with open("README.md", "r") as fh:
+    long_description = fh.read()
+
+setuptools.setup(
+    name="lfdeploy",
+    version="1.2.5",
+    author="Przemyslaw Winszczyk",
+    author_email="n3onix@gmail.com",
+    description="Improved tfdeploy module",
+    url="https://github.com/LendFlo/Lendflo-tfdeploy",
+    packages = ['lfdeploy'],
+    classifiers=(
+        "Programming Language :: Python :: 3",
+        "License :: OSI Approved :: MIT License",
+        "Operating System :: OS Independent",
+    ),
+)
