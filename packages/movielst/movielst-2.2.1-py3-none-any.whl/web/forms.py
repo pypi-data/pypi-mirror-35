4844ebe73983c69807b11477945c75907d055899
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-05 17:06:34 +0000

    Add package movielst/2.2.1/movielst-2.2.1-py3-none-any.whl

---
+++ packages/movielst/movielst-2.2.1-py3-none-any.whl/web/forms.py
@@ -0,0 +1,12 @@
+from flask_wtf import FlaskForm
+from wtforms import StringField, SubmitField
+
+
+class SettingsForm(FlaskForm):
+    log_level_field = StringField('Log level')
+    log_location_field = StringField('Log location')
+    location_field = StringField('Index location')
+    use_external_api_field = StringField('External API')
+    omdb_api_key_field = StringField('OMDb API key')
+    tmdb_api_key_field = StringField('TMDb API key')
+    submit = SubmitField('Save')
