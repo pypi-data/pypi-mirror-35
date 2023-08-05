eba00a230c26782415a853f4bd6e000bb8adb4fa
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-05 17:06:54 +0000

    Add package pyvter/0.0.2/pyvter-0.0.2.tar.gz

---
+++ packages/pyvter/pyvter-0.0.2.tar.gz/pyvter-0.0.2/setup.py
@@ -0,0 +1,18 @@
+import setuptools
+
+setuptools.setup(
+    name="pyvter",
+    version="0.0.2",
+    author="jhd",
+    author_email="author@example.com",
+    description="Interactive Statistics Workflows",
+    long_description="Interactive Statistics Workflows",
+    long_description_content_type="text/markdown",
+    url="https://github.com/jhd/pyvter",
+    packages=setuptools.find_packages(),
+    classifiers=[
+        "Programming Language :: Python :: 3",
+        "License :: OSI Approved :: GNU Affero General Public License v3",
+        "Operating System :: OS Independent",
+    ],
+)
