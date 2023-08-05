1f2a59c56bab07858adbafb896a470612fd97a16
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-05 22:10:18 +0000

    Add package golflang-encodings/1.0.4/golflang-encodings-1.0.4.tar.gz

---
+++ packages/golflang-encodings/golflang-encodings-1.0.4.tar.gz/golflang-encodings-1.0.4/setup.py
@@ -0,0 +1,32 @@
+#!/usr/bin/env python3
+"""golflang-encodings: a bunch of codecs for various esoteric programming languages"""
+
+from setuptools import setup, find_packages
+
+setup(
+    name='golflang-encodings',
+
+    version='1.0.4',
+
+    description='a bunch of codecs for various esoteric programming languages',
+
+    url='https://github.com/PPCG-v2/golflang-encodings',
+    
+    packages=find_packages(),
+
+    author='Mego',
+
+    license='MIT',
+
+    classifiers=[
+        'Development Status :: 5 - Production/Stable',
+        'License :: OSI Approved :: MIT License',
+        'Programming Language :: Python :: 3',
+        'Programming Language :: Python :: 3.3',
+        'Programming Language :: Python :: 3.4',
+        'Programming Language :: Python :: 3.5',
+        'Programming Language :: Python :: 3.6',
+    ],
+
+    keywords='text encoding unicode',
+)
