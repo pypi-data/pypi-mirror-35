9d0b259e08f8816403cdd1c51231bbfbac79f52c
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-05 18:10:04 +0000

    Add package mriqc/0.14.1/mriqc-0.14.1.tar.gz

---
+++ packages/mriqc/mriqc-0.14.1.tar.gz/mriqc-0.14.1/mriqc/workflows/__init__.py
@@ -0,0 +1,22 @@
+#!/usr/bin/env python
+# -*- coding: utf-8 -*-
+# emacs: -*- mode: python; py-indent-offset: 4; indent-tabs-mode: nil -*-
+# vi: set ft=python sts=4 ts=4 sw=4 et:
+"""
+
+.. automodule:: mriqc.workflows.anatomical
+    :members:
+    :undoc-members:
+    :show-inheritance:
+
+
+.. automodule:: mriqc.workflows.functional
+    :members:
+    :undoc-members:
+    :show-inheritance:
+
+
+"""
+from __future__ import print_function, division, absolute_import, unicode_literals
+from .anatomical import anat_qc_workflow
+from .functional import fmri_qc_workflow
