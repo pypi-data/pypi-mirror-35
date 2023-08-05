e7248a6184573737451266a37b630634bd71dd81
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-05 15:25:00 +0000

    Add package mailigen/1.0/mailigen-1.0.tar.gz

---
+++ packages/mailigen/mailigen-1.0.tar.gz/mailigen-1.0/README.md
@@ -0,0 +1,2 @@
+# Mailigen API
+This package is the oficial library of the Mailigen API build of the dev team of Mailigen.
---
+++ packages/mailigen/mailigen-1.0.tar.gz/mailigen-1.0/mailigen/examples/mgapi_listUnsubscribe.py
@@ -0,0 +1,20 @@
+from mailigen.lib.config import * #contains apikey
+from mailigen.lib.MGAPI import MGAPI
+
+# This Example shows how to ping using the MGAPI.php class and do some basic error checking.
+
+api = MGAPI(apikey)
+
+id = listId
+email_address = my_email
+delete_member = False
+send_goodbye = True
+send_notify = False
+
+retval = api.listUnsubscribe(id, email_address, delete_member, send_goodbye, send_notify)
+if api.errorCode:
+	print "Unable to load listUnsubscribe()!"
+	print "\tCode=", api.errorCode
+	print "\tMsg=", api.errorMessage
+else:
+	print "Returned: ", retval
