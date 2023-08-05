8566921404196fb831d3107244af69f7693010d5
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-05 17:05:14 +0000

    Add package ttyrecgenerator/0.6.0/ttyrecgenerator-0.6.0.tar.gz

---
+++ packages/ttyrecgenerator/ttyrecgenerator-0.6.0.tar.gz/ttyrecgenerator-0.6.0/ttyrecgenerator/cmd_ttyrecgenerator.py
@@ -0,0 +1,45 @@
+## @namespace ttyrecgenerator.cmd_ttyrecgenerator
+## @brief Program that generate gifs and video from console output
+import argparse
+import time
+import colorama
+import datetime
+import gettext
+import os
+import pkg_resources
+import platform
+import subprocess
+
+from .__init__ import __version__, __versiondate__
+from .libttyrecgenerator import platform_incompatibility
+
+try:
+    t = gettext.translation('ttyrecgenerator', pkg_resources.resource_filename('ttyrecgenerator', "locale"))
+    _ = t.gettext
+except:
+    _ = str
+
+def main():
+    parser=argparse.ArgumentParser(prog='ttyrecgenerator', 
+                                   description=_('Create an animated gif/video from the output of the program passed as parameter'), 
+                                   epilog=_("Developed by Mariano Muñoz 2018-{} under GNU General Public License 3.0".format(__versiondate__.year)), 
+                                   formatter_class=argparse.RawTextHelpFormatter)
+    parser.add_argument('--version', action='version', version=__version__)
+    parser.add_argument('program',  help=_("Path to program"))
+    parser.add_argument('--output', help=_("Ttyrec output path"), action="store", default="ttyrecord.rec")
+    parser.add_argument('--video', help=_("Makes a simulation and doesn't remove files"), action="store_true", default=False)
+    parser.add_argument('--lc_all', help=_("Operating system environment variable LC_ALL to force translation"), action="store", default="C")
+    args=parser.parse_args()
+
+    if platform.system()!="Linux":
+        platform_incompatibility()
+    else:
+        new_env = dict( os.environ )
+        new_env['LC_ALL'] = args.lc_all
+        subprocess.run(["xterm", "-hold", "-bg", "black", "-geometry", "140x400", "-fa", "monaco", "-fs", "18", "-fg", "white", "-e", "ttyrec -e '{0}' {1}; ttygif {1}".format(args.program, args.output)],env=new_env, stderr=subprocess.STDOUT)
+        os.system("mv tty.gif {}.gif".format(args.output))
+        if args.video==True:
+            subprocess.run(["ffmpeg", "-i", "{}.gif".format(args.output), "-c:v", "libx264", "-pix_fmt", "yuv420p", "-movflags", "+faststart", "{}.mp4".format(args.output)])
+
+if __name__ == "__main__":
+    main()
---
+++ packages/ttyrecgenerator/ttyrecgenerator-0.6.0.tar.gz/ttyrecgenerator-0.6.0/ttyrecgenerator/libttyrecgenerator.py
@@ -0,0 +1,76 @@
+## @namespace ttyrecgenerator.libttyrecgenerator
+## @brief Library to generate gifs and video from console output
+import argparse
+import time
+import colorama
+import datetime
+import gettext
+import locale
+import os
+import pkg_resources
+import platform
+import subprocess
+
+try:
+    t = gettext.translation('ttyrecgenerator', pkg_resources.resource_filename('ttyrecgenerator', "locale"))
+    _ = t.gettext
+except:
+    _ = str
+
+## @brief Class with predefinied pauses and colors. Allows to launch commands and pretend it's launch
+## The use of this class is not mandatory
+## This class provide methods with predefined pauses
+class RecSession:
+    def __init__(self):
+        self.__hostname="MyLinux"
+        self.__cwd="/ttyrec"
+
+    ## This function emulates the hostnmae and the current directory, with the colors of a unix tty
+    def path(self):
+        return "{} {}".format(colorama.Style.BRIGHT + colorama.Fore.RED + self.__hostname + colorama.Style.RESET_ALL, colorama.Style.BRIGHT + colorama.Fore.BLUE + self.__cwd + " # " + colorama.Style.RESET_ALL)
+
+    ## This function prints comments is yellow
+    ## # must be added to s
+    def comment(self, s, sleep=3):
+        print(self.path()+ colorama.Fore.YELLOW + s + colorama.Style.RESET_ALL)
+        time.sleep(sleep)
+
+
+    ## This function launches a command and prints the command in green
+    def command(self, s, sleep=5):
+        print(self.path() + colorama.Fore.GREEN + s + colorama.Style.RESET_ALL)
+        p=subprocess.run(s,shell=True, stderr=subprocess.STDOUT)
+        time.sleep(sleep)
+
+    ## This method changes current directory and shows a cd command in green
+    ## @param dir Path to change to
+    ## @param new_cwd string with the new path to show in console
+    ## @param sleep Integer with the number of seconds to sleep after function
+    def chdir(self, dir, new_cwd, sleep=5):
+        self.__cwd=new_cwd
+        print(self.path() + colorama.Fore.GREEN + "cd " + dir + colorama.Style.RESET_ALL)
+        os.chdir(dir)
+        print()
+        time.sleep(sleep)
+
+    ## This method launches commands with a pipe and prints it in green
+    def command_pipe(self, c1,c2, sleep=5):
+        cmd = "{}|{}".format(c1,c2)
+        print(self.path() + colorama.Fore.GREEN + cmd + colorama.Style.RESET_ALL)
+        ps = subprocess.Popen(cmd,shell=True,stdout=subprocess.PIPE,stderr=subprocess.STDOUT)
+        output = ps.communicate()[0]
+        print (output.decode('utf-8'))
+        time.sleep(6)
+
+    ## Sometimes when you are documenting a command. If you invoke it from the script, you enter in a bucle
+    ##
+    ## This function prints in console in the command color, but doesn't call the command
+    ## @param s String with the command
+    ## @param sleep Number of seconds to sleep after function
+    def command_fake(self, s, sleep=5):
+        print(self.path() + colorama.Fore.GREEN + s + colorama.Style.RESET_ALL)
+        time.sleep(sleep)
+
+
+def platform_incompatibility():
+    print( colorama.Style.BRIGHT + colorama.Fore.RED +  _("This funcionality doen't work on {}. Please move to Linux.").format(platform.system()) + colorama.Style.RESET_ALL)
