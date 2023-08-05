deffe4eed9720ec65f67552ac3ee8b827fcedb84
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-05 15:32:12 +0000

    Add package cloud-volume/0.28.1/cloud-volume-0.28.1.tar.gz

---
+++ packages/cloud-volume/cloud-volume-0.28.1.tar.gz/cloud-volume-0.28.1/setup.py
@@ -0,0 +1,57 @@
+import os
+import setuptools
+
+join = os.path.join
+third_party_dir = './ext/third_party'
+compressedsegdir = join(third_party_dir, 'compressed_segmentation')
+
+# NOTE: Run if _compressed_segmentation.cpp does not exist:
+# cython -3 --fast-fail -v --cplus \
+#    -I./ext/third_party/compressed_segmentation/include \
+#    ./ext/third_party/compressed_segmentation/src/_compressed_segmentation.pyx
+
+try:
+  import numpy as np
+except ImportError:
+  np = None
+
+setup_requires = ['pbr']
+
+extensions = []
+if np:
+  setup_requires.append('numpy')
+  extensions = [
+    setuptools.Extension(
+        '_compressed_segmentation',
+        optional=True,
+        sources=[ join(compressedsegdir, 'src', x) for x in ( 
+            'compress_segmentation.cc', 'decompress_segmentation.cc',
+            '_compressed_segmentation.cpp'
+        )],
+        language='c++',
+        include_dirs=[ join(compressedsegdir, 'include'), np.get_include() ],
+        extra_compile_args=[
+          '-O3', '-std=c++11'
+        ],
+    ),
+  ]
+
+setuptools.setup(
+  setup_requires=setup_requires,
+  extras_require={
+    ':python_version == "2.7"': ['futures'],
+    ':python_version == "2.6"': ['futures'],
+  },
+  ext_modules=extensions,
+  pbr=True)
+
+if not np:
+  yellow = "\033[1;93m"
+  reset = "\033[m"
+  print(yellow \
+    + "WARNING: the fpzip extension has not been compiled. " \
+    + "Please reinstall after running \"pip install numpy\"." \
+    + reset)
+
+
+
