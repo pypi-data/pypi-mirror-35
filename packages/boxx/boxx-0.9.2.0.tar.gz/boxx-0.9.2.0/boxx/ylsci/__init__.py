95de4a597396e77d808032e4457adac2d81fb682
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-05 22:09:10 +0000

    Add package boxx/0.9.2.0/boxx-0.9.2.0.tar.gz

---
+++ packages/boxx/boxx-0.9.2.0.tar.gz/boxx-0.9.2.0/boxx/ylsci/__init__.py
@@ -0,0 +1,16 @@
+# -*- coding: utf-8 -*-
+"""
+Math and Scientific Computing module
+
+@author: yl
+"""
+
+from __future__ import unicode_literals
+
+from .ylnp import  e, pi, nan, inf
+from .ylnp import npFloatTypes, npIntTypes, npBoolTypes, npStrTypes
+from .ylnp import savenp, loadnp, plot3dSurface, isNumpyType, testNumpyMultiprocessing
+
+from .ylvector import sin, cos, arccos, arctan, deg2rad, rad2deg
+from .ylvector import Vector
+
