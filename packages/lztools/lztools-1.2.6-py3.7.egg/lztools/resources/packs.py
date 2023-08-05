a5651826c371ce4786c1d61b2edefefee46b181c
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-05 15:29:59 +0000

    Add package lztools/1.2.6/lztools-1.2.6-py3.7.egg

---
+++ packages/lztools/lztools-1.2.6-py3.7.egg/lztools/resources/packs.py
@@ -0,0 +1,17 @@
+#!/usr/bin/env python3
+
+pip_requires = [
+    'flickrapi',
+    'zope.proxy',
+    'click',
+    'pip',
+    'GitPython',
+    'ansiwrap',
+    'lztools.text'
+]
+
+apt_requires = [
+    'asciiart',
+    'toilet',
+    'lolcat'
+]
