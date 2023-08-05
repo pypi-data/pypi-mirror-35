602554709fab85268aa9665cd0bff388517b429a
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-05 15:05:16 +0000

    Add package tileIndexPythonGenerator/0.1.0.2/tileIndexPythonGenerator-0.1.0.2.tar.gz

---
+++ packages/tileIndexPythonGenerator/tileIndexPythonGenerator-0.1.0.2.tar.gz/tileIndexPythonGenerator-0.1.0.2/tileIndexPythonGenerator/__init__.py
@@ -0,0 +1,3 @@
+from .tileIndexPythonGenerator import createFromListStruct
+from .tileIndexPythonGenerator import tileIndex as create
+from .tileIndexPythonGenerator import tileIndexes as createMulti
