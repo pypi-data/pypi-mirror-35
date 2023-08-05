5e1e3c14a720b014eaf52e3c3d8208e2de099d60
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-05 17:07:25 +0000

    Add package EPAB/2018.8.20.1/EPAB-2018.8.20.1-py36-none-any.whl

---
+++ packages/EPAB/EPAB-2018.8.20.1-py36-none-any.whl/epab/utils/_exe_version.py
@@ -0,0 +1,67 @@
+# coding=utf-8
+"""
+Get version info from executable
+"""
+import traceback
+import typing
+from pathlib import Path
+
+import pefile
+
+
+class VersionInfo:
+    """
+    Simple version info
+    """
+
+    def __init__(self, file_version, full_version):
+        self._file_version = file_version
+        self._full_version = full_version
+
+    @property
+    def file_version(self) -> str:
+        """
+        Returns: simple version
+        """
+        return self._file_version
+
+    @property
+    def full_version(self) -> str:
+        """
+        Returns: full version
+        """
+        return self._full_version
+
+    def __repr__(self):
+        return f'{self.__class__.__name__}({self.file_version}, {self.full_version})'
+
+    def __str__(self):
+        return self.file_version
+
+
+# pylint: disable=inconsistent-return-statements
+def get_product_version(path: typing.Union[str, Path]) -> VersionInfo:
+    """
+    Get version info from executable
+
+    Args:
+        path: path to the executable
+
+    Returns: VersionInfo
+    """
+    path = Path(path).absolute()
+    pe_info = pefile.PE(str(path))
+
+    try:
+        for file_info in pe_info.FileInfo:  # pragma: no branch
+            if file_info.Key == b'StringFileInfo':  # pragma: no branch
+                for string in file_info.StringTable:  # pragma: no branch
+                    if b'FileVersion' in string.entries.keys():  # pragma: no branch
+                        file_version = string.entries[b'SpecialBuild'].decode('utf8')
+                        full_version = string.entries[b'PrivateBuild'].decode('utf8')
+                        return VersionInfo(file_version, full_version)
+
+        raise RuntimeError(f'unable to obtain version from {path}')
+    except (KeyError, AttributeError) as exc:
+        traceback.print_exc()
+        raise RuntimeError(f'unable to obtain version from {path}') from exc
---
+++ packages/EPAB/EPAB-2018.8.20.1-py36-none-any.whl/test/test_cmd/test_linters.py
@@ -0,0 +1,160 @@
+# coding=utf-8
+
+import itertools
+from pathlib import Path
+
+import pytest
+from mockito import mock, verify, verifyNoMoreInteractions, verifyStubbedInvocationsAreUsed, when
+
+import epab.utils
+from epab.core import CONFIG, CTX
+from epab.linters import _flake8, _lint, _mypy, _pep8, _pylint, _safety, _sort
+
+
+@pytest.fixture(autouse=True, name='repo')
+def _all():
+    repo = mock(spec=epab.utils.Repo)
+    CTX.repo = repo
+    yield repo
+
+
+@pytest.mark.parametrize(
+    'amend_stage',
+    itertools.product([False, True], repeat=2),
+)
+def test_lint(amend_stage):
+    amend, stage = amend_stage
+    context = mock()
+    _lint._lint(context, amend, stage)
+    verify(context).invoke(_safety.safety)
+    verify(context).invoke(_pylint.pylint)
+    verify(context).invoke(_flake8.flake8)
+    verify(context).invoke(_mypy.mypy)
+    verify(context).invoke(_sort.sort, amend=amend, stage=stage)
+    verify(context).invoke(_pep8.pep8, amend=amend, stage=stage)
+    verifyNoMoreInteractions(context)
+
+
+@pytest.mark.parametrize(
+    'amend_stage',
+    itertools.product([False, True], repeat=2),
+)
+def test_lint_appveyor(amend_stage):
+    amend, stage = amend_stage
+    CTX.appveyor = True
+    context = mock()
+    _lint._lint(context, amend, stage)
+    verify(context).invoke(_safety.safety)
+    verify(context).invoke(_pylint.pylint)
+    verify(context).invoke(_flake8.flake8)
+    verify(context).invoke(_mypy.mypy)
+    verify(context).invoke(_pep8.pep8, amend=amend, stage=stage)
+    verifyNoMoreInteractions(context)
+
+
+def test_pep8():
+    when(epab.utils).run(
+        f'autopep8 -r --in-place --max-line-length {CONFIG.lint__line_length} {CONFIG.package}', mute=True)
+    when(epab.utils).run(f'autopep8 -r --in-place --max-line-length {CONFIG.lint__line_length} test', mute=True)
+    _pep8._pep8()
+    verifyStubbedInvocationsAreUsed()
+
+
+@pytest.mark.long
+def test_pep8_amend():
+    with when(CTX.repo).amend_commit(append_to_msg='pep8 [auto]'):
+        CTX.run_once = {}
+        _pep8._pep8(amend=True)
+        verify(CTX.repo).amend_commit(...)
+
+
+def test_pep8_stage():
+    when(epab.utils).run(
+        f'autopep8 -r --in-place --max-line-length {CONFIG.lint__line_length} {CONFIG.package}', mute=True)
+    when(epab.utils).run(f'autopep8 -r --in-place --max-line-length {CONFIG.lint__line_length} test', mute=True)
+    with when(CTX.repo).stage_all():
+        CTX.run_once = {}
+        _pep8._pep8(stage=True)
+    verifyStubbedInvocationsAreUsed()
+
+
+def test_isort_package_dir():
+    Path(f'./{CONFIG.package}').mkdir()
+    test_file = Path(f'./{CONFIG.package}/test.py')
+    test_file.touch()
+    when(_sort.isort).SortImports(
+        file_path=test_file.absolute(),
+        known_first_party=CONFIG.package,
+        **_sort.SETTINGS
+    )
+    _sort._sort()
+    verify(_sort.isort).SortImports(...)
+
+
+def test_isort_test_dir():
+    Path('./test').mkdir()
+    test_file = Path('./test/test.py')
+    test_file.touch()
+    when(_sort.isort).SortImports(
+        file_path=test_file.absolute(),
+        known_first_party=CONFIG.package,
+        **_sort.SETTINGS
+    )
+    _sort._sort()
+    verify(_sort.isort).SortImports(...)
+
+
+def test_isort_ignore():
+    test_file = Path('./test.py')
+    test_file.touch()
+    when(_sort.isort).SortImports(
+        file_path=test_file.absolute(),
+        known_first_party=CONFIG.package,
+        **_sort.SETTINGS
+    )
+    # when(epab.utils).run(contains('setup.py isort'))
+    _sort._sort()
+    verify(_sort.isort, times=0).SortImports(...)
+
+
+def test_isort_amend():
+    # when(epab.utils).run(contains('setup.py isort'))
+    when(CTX.repo).amend_commit(append_to_msg='sorting imports [auto]')
+    _sort._sort(amend=True)
+    verifyStubbedInvocationsAreUsed()
+
+
+def test_isort_stage():
+    # when(epab.utils).run(contains('setup.py isort'))
+    when(CTX.repo).stage_all()
+    _sort._sort(stage=True)
+    verifyStubbedInvocationsAreUsed()
+
+
+def test_flake8():
+    base_cmd = ' '.join((_flake8.IGNORE, _flake8.MAX_LINE_LENGTH, _flake8.EXCLUDE, _flake8.MAX_COMPLEXITY))
+    when(epab.utils).run(f'flake8 {base_cmd}', mute=True)
+    _flake8._flake8()
+    verifyStubbedInvocationsAreUsed()
+
+
+def test_safety():
+    when(epab.utils).run('safety check --bare', mute=True)
+    _safety._safety()
+    verifyStubbedInvocationsAreUsed()
+
+
+@pytest.mark.parametrize(
+    'params,cmd',
+    [
+        ((None, True), 'pylint ./test --reports=y'),
+        ((None, False), 'pylint ./test --reports=n'),
+        (('src', False), 'pylint src --reports=n'),
+    ]
+)
+def test_pylint(params, cmd):
+    CONFIG.load()
+    CONFIG.package = 'test'
+    with when(epab.utils).run(f'{cmd} {_pylint.BASE_CMD}', mute=True):
+        _pylint._pylint(*params)
+        verify(epab.utils).run(...)
