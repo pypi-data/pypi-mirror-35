b07349c47a7adc941c76dc3b6668c7ded478680e
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-05 22:10:28 +0000

    Add package nauth-sdk/0.0.1/nauth_sdk-0.0.1-py3-none-any.whl

---
+++ packages/nauth-sdk/nauth_sdk-0.0.1-py3-none-any.whl/nauth_sdk/settings.py
@@ -0,0 +1,5 @@
+nid = 'c6daaff4-6580-448d-8eb3-4eb38e1731ce'
+nkey = 'eyJ0eXAiOiJKV1QiLCJhbGciOiJIUzI1NiJ9.eyJuaWQiOiJjNmRhYWZmNC02NTgwLTQ0OGQtOGViMy00ZWIzOGUxNzMxY2UifQ.4zgfbBIbReNq8kCyI2_z3xw5krInnVDjf3snC6HdrLE'
+
+nauth_host_location = '120.79.184.169:88'
+sitename = 'hhunavi'
