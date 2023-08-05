a745bc23db8a6095edd8d0b0d32ae3eaa9b1e510
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-05 16:54:53 +0000

    Add package stats-comp/0.0.1/stats_comp-0.0.1.tar.gz

---
+++ packages/stats-comp/stats_comp-0.0.1.tar.gz/stats_comp-0.0.1/setup.py
@@ -0,0 +1,16 @@
+import setuptools
+
+setuptools.setup(
+    name="stats_comp",
+    version="0.0.1",
+    author="Shrey",
+    author_email="shrey.anand@ymail.com",
+    description="A package to explore and visualize huge data sets",
+    url='https://github.com/Shreyanand/insights-ds-analysis-worker/statscomp',
+    packages=setuptools.find_packages(),
+    classifiers=[
+        "Programming Language :: Python :: 3",
+        "License :: OSI Approved :: MIT License",
+        "Operating System :: OS Independent",
+    ]
+    )
