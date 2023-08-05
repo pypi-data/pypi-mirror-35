b01390172319bf1c35c6b714fd2b046db4d7719f
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-05 18:08:27 +0000

    Add package abimap/0.3.1/abimap-0.3.1.tar.gz

---
+++ packages/abimap/abimap-0.3.1.tar.gz/abimap-0.3.1/tests/test_get_version_from_string.py
@@ -0,0 +1,29 @@
+# -*- coding: utf-8 -*-
+
+import pytest
+
+from abimap import symver
+
+
+@pytest.mark.skipif(pytest.__version__ < '3.4', reason="caplog not supported")
+def test_get_version_from_string(testcases, caplog):
+    if testcases:
+        for tc in testcases:
+            if tc["exceptions"]:
+                with pytest.raises(Exception) as e:
+                    assert (symver.get_version_from_string(tc["input"]) ==
+                            tc["output"])
+                    for expected in tc["exceptions"]:
+                        assert expected in str(e.value)
+                    if tc["warnings"]:
+                        for expected in tc["warnings"]:
+                            assert "WARNING  " + expected in caplog.text
+            else:
+                assert (symver.get_version_from_string(tc["input"]) ==
+                        tc["output"])
+                if tc["warnings"]:
+                    for expected in tc["warnings"]:
+                        assert "WARNING  " + expected in caplog.text
+    else:
+        # If no test cases were found, fail
+        assert 0
