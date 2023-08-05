df4d70cce831a3c2c4ac0ba420f588aa8184c41f
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-05 15:26:30 +0000

    Add package test-pseudo-j/0.6.5/test_pseudo_j-0.6.5.tar.gz

---
+++ packages/test-pseudo-j/test_pseudo_j-0.6.5.tar.gz/test_pseudo_j-0.6.5/setup.py
@@ -0,0 +1,32 @@
+import setuptools
+
+with open("README.md", "r") as fh:
+    long_description = fh.read()
+
+setuptools.setup(
+    name="test_pseudo_j",
+    version="0.6.5",
+    author="David Fagneray",
+    author_email="david.fagneray@gmail.com",
+    description="A Java8 compiler to Pseudo",
+    long_description=long_description,
+    url="https://github.com/dfagneray/pseudo_java",
+    keywords=['compiler', 'generation', 'c++', 'ruby', 'c#', 'javascript', 'go', 'python', 'pseudo'],
+    packages=['pseudo_java'],
+    install_requires=[
+        'PyYAML',
+        'colorama',
+        'termcolor',
+        'pseudo>=0.2.3'
+    ],
+    classifiers=(
+        "Programming Language :: Python :: 3",
+        "License :: OSI Approved :: MIT License",
+        "Operating System :: OS Independent",
+    ),
+    entry_points={
+        'console_scripts': [
+            'pseudo-java=pseudo_java.main:main',
+        ],
+    },
+)
