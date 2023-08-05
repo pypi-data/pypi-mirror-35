aa03eecc75fb51d5c16a10e63146b714cd2ba775
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-05 15:04:34 +0000

    Add package hydrostats/0.67/hydrostats-0.67.tar.gz

---
+++ packages/hydrostats/hydrostats-0.67.tar.gz/hydrostats-0.67/hydrostats/__init__.py
@@ -0,0 +1,12 @@
+# python 3.6
+# -*- coding: utf-8 -*-
+"""
+Created on June 11, 17:16:19, 2018
+@author: Wade Roberts
+
+For full documentation, see https://waderoberts123.github.io/Hydrostats/
+"""
+from hydrostats.metrics import *
+from hydrostats.ens_metrics import *
+from hydrostats.analyze import *
+
