11c995122d1cc11eef877593241a4a7ba541b1f3
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-05 17:04:53 +0000

    Add package zinnia-theme-bootstrap-4/0.6/zinnia_theme_bootstrap_4-0.6-py2-none-any.whl

---
+++ packages/zinnia-theme-bootstrap-4/zinnia_theme_bootstrap_4-0.6-py2-none-any.whl/zinnia_bootstrap4/__init__.py
@@ -0,0 +1,8 @@
+"""zinnia_bootstrap"""
+__version__ = '0.6'
+__license__ = 'BSD License'
+
+__author__ = 'Sharmila Gopirajan'
+__email__ = 'siva.sharmi@gmail.com'
+
+__url__ = 'https://github.com/sharmi/zinnia-theme-bootstrap-4'
