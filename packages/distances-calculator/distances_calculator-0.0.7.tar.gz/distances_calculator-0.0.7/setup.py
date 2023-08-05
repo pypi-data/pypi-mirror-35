7a4fc7cfded7e6f5fbe00b8be842c4436ad1384b
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-05 18:10:05 +0000

    Add package distances-calculator/0.0.7/distances_calculator-0.0.7.tar.gz

---
+++ packages/distances-calculator/distances_calculator-0.0.7.tar.gz/distances_calculator-0.0.7/setup.py
@@ -0,0 +1,32 @@
+import setuptools
+
+with open("README.md", "r") as fh:
+    long_description = fh.read()
+
+setuptools.setup(
+    name="distances_calculator",
+    keywords='distance calculator, open street map, csv, big data',
+    version="0.0.7",
+    author="tim-hub",
+
+    description="Calculate distances between 2 addresses, Distances Calculator is based on open street map api.",
+    long_description=long_description,
+    long_description_content_type="text/markdown",
+    url="https://github.com/tim-hub/calculator",
+    packages=setuptools.find_packages(),
+    install_requires=[
+        'requests',
+        'pandas',
+        'argparse',
+    ],
+    classifiers=[
+        "Programming Language :: Python :: 3",
+        "License :: OSI Approved :: MIT License",
+        "Operating System :: OS Independent",
+    ],
+    entry_points = {
+        'console_scripts': ['distances-calc=calculator.command_line:main'],
+    },
+    test_suite='nose.collector',
+    tests_require=['nose'],
+)
