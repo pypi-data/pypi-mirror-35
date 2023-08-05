3020d6420cc245c54f4e9aaf59642f63fb7c6e9b
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-05 15:05:04 +0000

    Add package pygin/0.1.2.1/pygin-0.1.2.1.tar.gz

---
+++ packages/pygin/pygin-0.1.2.1.tar.gz/pygin-0.1.2.1/pygin/example_games/Balance/game_objects/mesh_objects/screen_fader.py
@@ -0,0 +1,51 @@
+from pygin.basic_objects.basic_rectangle import BasicRectangle
+from pygin.components.animation import Animation
+from pygin.components.animator import Animator
+from pygin.key_frame import KeyFrame
+from pygame.math import Vector2
+from pygin.engine import Engine
+from pygin.material import Material
+from pygin.game_object import GameObject
+from pygin.color import Color
+from pygin.time import Time
+
+
+class ScreenFader(BasicRectangle):
+
+    def __init__(self, fade="in", fade_duration=0.7):
+        """
+        Constructor, will decide whether to fade in or fade out
+        :param fade: string telling fade in or out
+        """
+        self.fade = fade
+        self.fade_duration = fade_duration
+        if fade == "in":
+            alp = 255
+        else:
+            alp = 0
+        super().__init__(Vector2(0, 0), Vector2(Engine.screen_width, Engine.screen_height),
+                         Material(Color.black, alpha=alp), 1000)
+
+    def start(self):
+        """
+        Create a animation that fades the entire screen
+        Pass this animation to animator and play it
+        """
+        key_frames = list()
+        if self.fade == "in":
+            key_frames.append(KeyFrame(0.0, alpha=255))
+            key_frames.append(KeyFrame(self.fade_duration, alpha=0))
+        else:
+            key_frames.append(KeyFrame(0.0, alpha=0))
+            key_frames.append(KeyFrame(self.fade_duration, alpha=255))
+        self.animation = Animation(self, key_frames, should_loop=False, unscaled="True")
+        self.animator = Animator(self, animation_list=[self.animation])
+        self.animator.play()
+        self.creation_time = Time.now()
+
+    def update(self):
+        """
+        Will destroy the animation after finished it
+        """
+        if Time.now() - self.creation_time > self.fade_duration*2:
+            GameObject.destroy(self)
---
+++ packages/pygin/pygin-0.1.2.1.tar.gz/pygin-0.1.2.1/pygin/example_games/Balance/scripts/constants.py
@@ -0,0 +1,7 @@
+class Constants:
+    screen_width = 360
+    screen_height = 640
+    circCenter_x = 180
+    circCenter_y = 520
+    circRadius = 95
+    current_score = 0
