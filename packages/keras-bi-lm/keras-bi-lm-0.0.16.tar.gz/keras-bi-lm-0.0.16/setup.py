2dde1e6c572631b14d9c93a68403848742e0c25c
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-05 15:26:20 +0000

    Add package keras-bi-lm/0.0.16/keras-bi-lm-0.0.16.tar.gz

---
+++ packages/keras-bi-lm/keras-bi-lm-0.0.16.tar.gz/keras-bi-lm-0.0.16/setup.py
@@ -0,0 +1,23 @@
+from setuptools import setup
+
+setup(
+    name='keras-bi-lm',
+    version='0.0.16',
+    packages=['keras_bi_lm'],
+    url='https://github.com/CyberZHG/keras-bi-lm',
+    license='MIT',
+    author='CyberZHG',
+    author_email='CyberZHG@gmail.com',
+    description='Train the Bi-LM model and use it as a feature extraction method',
+    long_description=open('README.rst', 'r').read(),
+    install_requires=[
+        'numpy',
+        'keras',
+    ],
+    classifiers=(
+        "Programming Language :: Python :: 2",
+        "Programming Language :: Python :: 3",
+        "License :: OSI Approved :: MIT License",
+        "Operating System :: OS Independent",
+    ),
+)
