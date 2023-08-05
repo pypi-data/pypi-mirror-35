e37fc5bb8a2f5814da651eed8dcfef0376fd22ae
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-05 20:12:12 +0000

    Add package EPAB/2018.8.22.3/EPAB-2018.8.22.3-py36-none-any.whl

---
+++ packages/EPAB/EPAB-2018.8.22.3-py36-none-any.whl/epab/cmd/__init__.py
@@ -0,0 +1,15 @@
+# coding=utf-8
+"""
+Manages commands
+"""
+
+from ._chglog import chglog
+from ._release import release
+from ._reqs import reqs
+from ._pytest import pytest
+from ._install_hooks import install_hooks
+from ._push import push
+from ._freeze import freeze, flat_freeze
+from ._next_version import next_version
+from ._print_version import print_version
+from ._compile_qt import compile_qt_resources
---
+++ packages/EPAB/EPAB-2018.8.22.3-py36-none-any.whl/epab/cmd/_install_hooks.py
@@ -0,0 +1,64 @@
+# coding=utf-8
+"""
+Installs Git hooks
+"""
+from pathlib import Path
+
+import click
+
+import epab.utils
+from epab.core import VERSION
+
+PRE_PUSH = """#!/bin/sh
+#
+# This Git hook was installed by EPAB {version}
+#
+PATH="{venv}:$PATH"
+echo `epab --version`
+
+epab reqs -a chglog -a
+
+epab -d pytest -l -x --ff
+if [ "$?" -ne "0" ]
+then
+    echo "Tests failed"
+    exit 1
+fi
+exit 0"""
+
+
+PRE_COMMIT = """#!/bin/sh
+#
+# This Git hook was installed by EPAB {version}
+#
+PATH="{venv}:$PATH"
+echo `epab --version`
+
+epab -d -ns lint
+if [ "$?" -ne "0" ]
+then
+    echo "Linting error"
+    exit 1
+fi
+
+exit 0"""
+
+
+def _make_venv_path() -> str:
+    venv, _ = epab.utils.run('pipenv --venv', filters='Courtesy Notice:')
+    venv_path = Path(venv.rstrip(), 'Scripts').absolute()
+    return Path('/', venv_path.parts[0].replace(':', '').lower(), *venv_path.parts[1:]).as_posix()
+
+
+def _install_hooks():
+    venv_path = _make_venv_path()
+    Path('./.git/hooks/pre-push').write_text(PRE_PUSH.format(venv=venv_path, version=VERSION))
+    Path('./.git/hooks/pre-commit').write_text(PRE_COMMIT.format(venv=venv_path, version=VERSION))
+
+
+@click.command()
+def install_hooks():
+    """
+    Install Git hooks
+    """
+    _install_hooks()
