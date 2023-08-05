e42298afce6a0841776327bf07d3b1ae25d9d955
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-05 15:24:14 +0000

    Add package confi/0.0.2/confi-0.0.2.tar.gz

---
+++ packages/confi/confi-0.0.2.tar.gz/confi-0.0.2/setup.py
@@ -0,0 +1,17 @@
+from setuptools import setup, find_packages
+
+with open("README.md", "r") as fh:
+    long_description = fh.read()
+
+setup(
+    name='confi',
+    version='0.0.2',
+    tests_require='pytest',
+    packages=find_packages(),
+    long_description=long_description,
+    include_package_data=True,
+    description='Comfortable python app configuration',
+    author='Boris Tseitlin',
+    author_email='btseytlin@start.film',
+    keywords=['configuration', 'env', 'docker']
+)
