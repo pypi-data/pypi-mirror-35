9e6dd5fcad25e5925790f6cf07ae6d44be9a9596
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-05 21:11:38 +0000

    Add package fullList/1.0.0/fullList-1.0.0.tar.gz

---
+++ packages/fullList/fullList-1.0.0.tar.gz/fullList-1.0.0/setup.py
@@ -0,0 +1,12 @@
+from distutils.core import setup
+
+setup(
+        name = 'fullList',
+        version = '1.0.0',
+        py_modules = ['fullList'],
+        author = 'bereska',
+        author_email = 'bereska@hotmail.com',
+        url = 'https://parustrans.ru',
+        discription = 'A simple printer of nested lists',
+        )
+
