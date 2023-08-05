fb2893bd1d7243753aa77bef181cdaff65a476ed
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-05 21:11:53 +0000

    Add package django-any-urlfield/2.5.1/django-any-urlfield-2.5.1.tar.gz

---
+++ packages/django-any-urlfield/django-any-urlfield-2.5.1.tar.gz/django-any-urlfield-2.5.1/any_urlfield/tests/test_validation.py
@@ -0,0 +1,23 @@
+from __future__ import unicode_literals
+
+from unittest import skipIf
+
+import django
+from django.core.exceptions import ValidationError
+from django.test import TestCase
+
+from any_urlfield.validators import ExtendedURLValidator
+
+
+class ValidationTests(TestCase):
+    maxDiff = None
+
+    @skipIf(django.VERSION < (1, 8), "extended validation not supported in Django 1.7 and below")
+    def test_url_validation(self):
+        v = ExtendedURLValidator()
+        v('https://google.com')
+        v('tel://+44(0)123-45.67#8*9')
+        v('mailto://test@example.com?subject=Greetings')
+
+        self.assertRaises(ValidationError, v, 'tel://not a phone number')
+        self.assertRaises(ValidationError, v, 'mailto://not an email address')
