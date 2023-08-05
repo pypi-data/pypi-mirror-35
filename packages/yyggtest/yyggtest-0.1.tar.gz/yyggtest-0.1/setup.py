19c63ce9b64628bd1b3c729d0d46183501372438
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-05 22:11:51 +0000

    Add package yyggtest/0.1/yyggtest-0.1.tar.gz

---
+++ packages/yyggtest/yyggtest-0.1.tar.gz/yyggtest-0.1/setup.py
@@ -0,0 +1,11 @@
+from setuptools import setup
+
+setup(name='yyggtest',
+      version='0.1',
+      description='trying to upload yyggtest',
+      url='https://github.com/YueGan/random_walker',
+      author='GG',
+      author_email='gg@gg.com',
+      license='MIT',
+      packages=['yyggtest'],
+      zip_safe=False)
