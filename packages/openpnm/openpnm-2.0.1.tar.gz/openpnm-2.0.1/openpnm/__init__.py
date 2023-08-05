1a9716fe5468cf2d0f1844f6a2828ab9c20e4f43
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-05 21:10:14 +0000

    Add package openpnm/2.0.1/openpnm-2.0.1.tar.gz

---
+++ packages/openpnm/openpnm-2.0.1.tar.gz/openpnm-2.0.1/openpnm/__init__.py
@@ -0,0 +1,33 @@
+r"""
+
+::
+
+     o-o                o--o  o   o o   o
+    o   o               |   | |\  | |\ /|
+    |   | o-o  o-o o-o  o--o  | \ | | o |
+    o   o |  | |-' |  | |     |  \| |   |
+     o-o  o-o  o-o o  o o     o   o o   o
+          |
+          o
+
+**OpenPNM**
+
+OpenPNM is a package for performing pore network simulations of transport in
+porous materials
+
+"""
+
+__version__ = '2.0.1'
+
+from . import utils
+from .utils import Workspace, Project
+from . import core
+from . import models
+from . import network
+from . import topotools
+from . import geometry
+from . import phases
+from . import physics
+from . import algorithms
+from . import io
+from . import materials
