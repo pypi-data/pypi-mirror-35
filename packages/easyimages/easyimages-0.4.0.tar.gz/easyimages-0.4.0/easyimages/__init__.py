c70e14e15080f9d8c3f4511ed691fd15c7dd5195
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-05 22:12:45 +0000

    Add package easyimages/0.4.0/easyimages-0.4.0.tar.gz

---
+++ packages/easyimages/easyimages-0.4.0.tar.gz/easyimages-0.4.0/easyimages/__init__.py
@@ -0,0 +1,9 @@
+# -*- coding: utf-8 -*-
+
+"""Top-level package for easyimages."""
+
+__author__ = """Jakub Cieslik"""
+__email__ = 'kubacieslik@gmail.com'
+__version__ = '0.2.0'
+
+from .easyimages import EasyImage, EasyImageList, bbox
