9bfcb0266e9b6c050e1a28254ba03e5a322351e9
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-05 16:56:18 +0000

    Add package django-short-url/1.1.3/django-short-url-1.1.3.tar.gz

---
+++ packages/django-short-url/django-short-url-1.1.3.tar.gz/django-short-url-1.1.3/django_short_url/urls.py
@@ -0,0 +1,9 @@
+# -*- coding: utf-8 -*-
+
+from django.conf.urls import include, url
+from django_short_url import views as surl_views
+
+
+urlpatterns = [
+    url(r'^(?P<surl>\w+)', surl_views.short_url_redirect, name='short_url_redirect'),
+]
