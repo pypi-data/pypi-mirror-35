8a1d85a55a89abd981014486dc18cf1e798999a9
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-05 21:10:22 +0000

    Add package mongo-datatables/0.2.6/mongo_datatables-0.2.6.tar.gz

---
+++ packages/mongo-datatables/mongo_datatables-0.2.6.tar.gz/mongo_datatables-0.2.6/setup.py
@@ -0,0 +1,29 @@
+from setuptools import setup
+
+
+def readme():
+    with open('README.rst') as f:
+        return f.read()
+
+setup(name='mongo_datatables',
+      version='0.2.6',
+      description='Classes for connecting DataTables and Editor to MongoDB',
+      long_description=readme(),
+      url='http://github.com/wholeshoot/mongo_datatables',
+      author='Paul Olsen',
+      author_email='python@wholeshoot.com',
+      license='MIT',
+      packages=['mongo_datatables'],
+      install_requires=['pymongo'],
+      classifiers=[
+          'Development Status :: 3 - Alpha',
+          'License :: OSI Approved :: MIT License',
+          'Programming Language :: Python :: 3.4',
+          'Programming Language :: Python :: 3.5',
+          'Programming Language :: Python :: 3.6',
+          'Topic :: Database :: Database Engines/Servers',
+      ],
+      keywords='flask django pymongo mongodb',
+      include_package_data=True,
+      zip_safe=False
+      )
