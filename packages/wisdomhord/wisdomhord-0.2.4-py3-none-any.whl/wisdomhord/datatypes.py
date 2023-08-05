517c61c32a4d98120539a52cfabe9cd2d18d4c3d
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-05 16:55:47 +0000

    Add package wisdomhord/0.2.4/wisdomhord-0.2.4-py3-none-any.whl

---
+++ packages/wisdomhord/wisdomhord-0.2.4-py3-none-any.whl/wisdomhord/datatypes.py
@@ -0,0 +1,68 @@
+import datetime
+import datarum
+
+class BaseType(object):
+
+    @staticmethod
+    def cast_to_hord(value):
+        return str(value)
+
+class String(BaseType):
+
+    @staticmethod
+    def cast_from_hord(value):
+        return String.escape_characters(value, reverse=True)
+
+    @staticmethod
+    def cast_to_hord(value):
+        return String.escape_characters(value)
+
+    @staticmethod
+    def escape_characters(value, reverse=False):
+        escapes = [(('|', '\|'))]
+        for escape in escapes:
+            escape = escape[::-1] if reverse else escape
+            value = value.replace(*escape)
+        return value
+
+class Boolean(BaseType):
+
+    @staticmethod
+    def cast_from_hord(value):
+        return value == 'True'
+
+    @staticmethod
+    def cast_to_hord(value):
+        return 'True' if value else 'False'
+
+class Integer(BaseType):
+
+    @staticmethod
+    def cast_from_hord(value):
+        return int(value)
+
+class Float(BaseType):
+
+    @staticmethod
+    def cast_from_hord(value):
+        return float(value)
+
+class DateTime(BaseType):
+
+    @staticmethod
+    def cast_from_hord(value):
+        return datetime.datetime.strptime(value, '%d.%m.%Y // %H.%M.%S')
+
+    @staticmethod
+    def cast_to_hord(value):
+        return datetime.datetime.strftime(value, '%d.%m.%Y // %H.%M.%S')
+
+class Wending(BaseType):
+
+    @staticmethod
+    def cast_from_hord(value):
+        return datarum.wending.strptime(value, '{daeg} {month} {gere} // {tid_zero}.{minute_zero}.{second_zero}')
+
+    @staticmethod
+    def cast_to_hord(value):
+        return value.strftime('{daeg} {month} {gere} // {tid_zero}.{minute_zero}.{second_zero}')
