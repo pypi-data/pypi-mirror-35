d86506c93429a703001c0ce3c1d53636761bf19a
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-05 15:23:55 +0000

    Add package haveibeenpwned/0.0.2/haveibeenpwned-0.0.2.tar.gz

---
+++ packages/haveibeenpwned/haveibeenpwned-0.0.2.tar.gz/haveibeenpwned-0.0.2/haveibeenpwned/status.py
@@ -0,0 +1,36 @@
+# -*- coding: utf-8 -*-
+import urllib3
+
+
+# Supress warning about insecure request.
+urllib3.disable_warnings(urllib3.exceptions.InsecureRequestWarning)
+_pwned_url = 'https://haveibeenpwned.com/api/v2/breachedaccount/'
+_headers = {'user-agent': 'Pwnage-Checker-PyPi-Package'}
+
+
+def _get_response(email):
+    """
+    :param email: {String}
+    :return: {String}
+    """
+    return urllib3.PoolManager().urlopen('GET', '{}{}'.format(_pwned_url, email), headers=_headers)
+
+
+def pwned(email):
+    """
+    If we get anything in response its pwned.
+    :param email: {String}
+    :return: {Boolean}
+    """
+    response = _get_response(email)
+    return True if response.data else False
+
+
+def pwned_full(email):
+    """
+    Get back full response.
+    :param email: {String}
+    :return: {String}
+    """
+    response = _get_response(email)
+    return response.data if response.data else None
