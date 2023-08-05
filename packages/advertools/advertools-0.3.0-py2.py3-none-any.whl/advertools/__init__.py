31792dd6473a596ceee6150c1fd3543c86847ff4
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-05 15:06:01 +0000

    Add package advertools/0.3.0/advertools-0.3.0-py2.py3-none-any.whl

---
+++ packages/advertools/advertools-0.3.0-py2.py3-none-any.whl/advertools/__init__.py
@@ -0,0 +1,15 @@
+# -*- coding: utf-8 -*-
+
+"""Top-level package for advertools."""
+
+__author__ = """Elias Dabbas"""
+__email__ = 'eliasdabbas@gmail.com'
+__version__ = '0.3.0'
+
+from advertools.ad_create import ad_create
+from advertools.ad_from_string import ad_from_string
+from advertools.extract import extract_mentions, extract_hashtags, extract_emoji
+from advertools.kw_generate import *
+from advertools.stopwords import stopwords
+from advertools.url_builders import url_utm_ga
+from advertools.word_frequency import word_frequency
