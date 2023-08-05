0d2912df879477ee365f0d5b397e265c7881c64d
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-05 18:08:31 +0000

    Add package example-pkg-yanama/0.0.3/example_pkg_yanama-0.0.3-py3-none-any.whl

---
+++ packages/example-pkg-yanama/example_pkg_yanama-0.0.3-py3-none-any.whl/iaas/apps.py
@@ -0,0 +1,5 @@
+from django.apps import AppConfig
+
+
+class IaasConfig(AppConfig):
+    name = 'iaas'
---
+++ packages/example-pkg-yanama/example_pkg_yanama-0.0.3-py3-none-any.whl/saltmanager/apps.py
@@ -0,0 +1,5 @@
+from django.apps import AppConfig
+
+
+class SaltmanagerConfig(AppConfig):
+    name = 'saltmanager'
