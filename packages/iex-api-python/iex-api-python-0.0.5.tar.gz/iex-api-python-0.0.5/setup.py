b99927b847bbb8d3706fe47538be371228d976ba
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-05 18:10:09 +0000

    Add package iex-api-python/0.0.5/iex-api-python-0.0.5.tar.gz

---
+++ packages/iex-api-python/iex-api-python-0.0.5.tar.gz/iex-api-python-0.0.5/setup.py
@@ -0,0 +1,23 @@
+import setuptools
+import glob
+import os
+
+required = [
+    "requests",
+    "pandas",
+    "arrow",
+    "socketIO-client-nexus"
+]
+
+setuptools.setup(name='iex-api-python',
+                 version="0.0.5",
+                 description='Fetch data from the IEX API',
+                 long_description=open('README.md').read().strip(),
+                 author='Daniel E. Cook',
+                 author_email='danielecook@gmail.com',
+                 url='http://www.github.com/danielecook/iex-api-python',
+                 packages=['iex'],
+                 install_requires=required,
+                 keywords=['finance', 'stock', 'market', 'market-data', 'IEX', 'API'],
+                 license='MIT License',
+                 zip_safe=False)
