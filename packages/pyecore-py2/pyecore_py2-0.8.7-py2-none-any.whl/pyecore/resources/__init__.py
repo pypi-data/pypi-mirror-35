a3f7b634e4d63bcb8612f349c1a5c1b9d3cf5408
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-05 21:11:17 +0000

    Add package pyecore-py2/0.8.7/pyecore_py2-0.8.7-py2-none-any.whl

---
+++ packages/pyecore-py2/pyecore_py2-0.8.7-py2-none-any.whl/pyecore/resources/__init__.py
@@ -0,0 +1,12 @@
+from .resource import ResourceSet, Resource, URI, global_registry
+from . import xmi
+from .. import ecore as Ecore
+
+# Register basic resource factory
+ResourceSet.resource_factory = {'xmi': xmi.XMIResource,
+                                'ecore': xmi.XMIResource,
+                                '*': xmi.XMIResource}
+
+global_registry[Ecore.nsURI] = Ecore
+
+__all__ = ['ResourceSet', 'Resource', 'URI', 'global_registry']
