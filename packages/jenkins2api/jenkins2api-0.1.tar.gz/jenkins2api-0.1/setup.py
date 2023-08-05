66411b43a26a86e0c932e1e9a6638fa8d1456cbf
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-05 20:12:12 +0000

    Add package jenkins2api/0.1/jenkins2api-0.1.tar.gz

---
+++ packages/jenkins2api/jenkins2api-0.1.tar.gz/jenkins2api-0.1/setup.py
@@ -0,0 +1,11 @@
+from setuptools import setup
+
+setup(name='jenkins2api',
+      version='0.1',
+      description='A jenkins api',
+      url='https://github.com/dineshdh/jenkins2api',
+      author='Dinesh Dharmawardena',
+      author_email='dinesh_dh@outlook.com',
+      license='MIT',
+      packages=['jenkins2api'],
+      zip_safe=False)
