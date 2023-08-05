82cb93b62692831c79d72fa63a7df08e1bcbe93e
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-05 17:08:04 +0000

    Add package mbstrdecoder/0.4.2/mbstrdecoder-0.4.2-py2.py3-none-any.whl

---
+++ packages/mbstrdecoder/mbstrdecoder-0.4.2-py2.py3-none-any.whl/mbstrdecoder/_func.py
@@ -0,0 +1,38 @@
+# encoding: utf-8
+
+"""
+.. codeauthor:: Tsuyoshi Hombashi <>
+"""
+
+from __future__ import absolute_import, unicode_literals
+
+from chardet.universaldetector import UniversalDetector
+
+
+def to_codec_name(name):
+    if not name:
+        return None
+
+    return name.lower().replace("-", "_")
+
+
+def detect_file_encoding(file_path):
+    detector = UniversalDetector()
+    READ_SIZE = 4 * 1024
+
+    try:
+        with open(file_path, mode="rb") as f:
+            while True:
+                binary = f.read(READ_SIZE)
+                if not binary:
+                    break
+
+                detector.feed(binary)
+                if detector.done:
+                    break
+    except IOError:
+        return None
+    finally:
+        detector.close()
+
+    return to_codec_name(detector.result.get("encoding"))
