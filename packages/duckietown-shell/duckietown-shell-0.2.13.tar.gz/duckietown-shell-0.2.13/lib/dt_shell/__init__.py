6b9a713cd2123cd5d871bc3ae4c532d2d053f3e7
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-05 22:12:45 +0000

    Add package duckietown-shell/0.2.13/duckietown-shell-0.2.13.tar.gz

---
+++ packages/duckietown-shell/duckietown-shell-0.2.13.tar.gz/duckietown-shell-0.2.13/lib/dt_shell/__init__.py
@@ -0,0 +1,28 @@
+# -*- coding: utf-8 -*-
+import traceback
+
+__version__ = '0.2.13'
+
+
+from .cli import DTShell
+
+
+from .dt_command_abs import DTCommandAbs
+from .dt_command_placeholder import DTCommandPlaceholder
+
+
+def cli_main():
+    # TODO: register handler for Ctrl-C
+    import sys
+
+    arguments = sys.argv[1:]
+    shell = DTShell()
+    try:
+        if arguments:
+            cmdline = " ".join(arguments)
+            shell.onecmd(cmdline)
+        else:
+            shell.cmdloop()
+    except Exception as e:
+        print(traceback.format_exc(e))
+        sys.exit(2)
---
+++ packages/duckietown-shell/duckietown-shell-0.2.13.tar.gz/duckietown-shell-0.2.13/lib/dt_shell/constants.py
@@ -0,0 +1,6 @@
+# -*- coding: utf-8 -*-
+
+class DTShellConstants(object):
+    commands_remote_url = 'https://github.com/duckietown/duckietown-shell-commands'
+    ROOT = '~/.dt-shell/'
+    ENV_COMMANDS = 'DTSHELL_COMMANDS'
