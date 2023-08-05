d0d12a8ce94df01c5a9c2c3c4c5affa7d8f2a970
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-05 17:07:27 +0000

    Add package html2dict/0.2/html2dict-0.2.tar.gz

---
+++ packages/html2dict/html2dict-0.2.tar.gz/html2dict-0.2/setup.py
@@ -0,0 +1,44 @@
+from setuptools import setup
+
+NAME = 'html2dict'
+DESCRIPTION = 'Simple html tables extractor.'
+URL = 'https://github.com/B-Souty/html2dict'
+EMAIL = 'benjamin.souty@gmail.com'
+AUTHOR = 'B-Souty'
+REQUIRES_PYTHON = '>=3.6.0'
+VERSION = '0.2'
+
+REQUIRED = [
+    'lxml',
+    'requests'
+]
+
+try:
+    with open('README.md') as f:
+        long_description = '\n' + f.read()
+except FileNotFoundError:
+    long_description = DESCRIPTION
+
+setup(
+    name=NAME,
+    version=VERSION,
+    description=DESCRIPTION,
+    long_description=long_description,
+    # The below field needs to be fixed by pypa
+    # long_description_content_type='text/markdown',
+    author=AUTHOR,
+    author_email=EMAIL,
+    python_requires=REQUIRES_PYTHON,
+    url=URL,
+    packages=['html2dict'],
+    install_requires=REQUIRED,
+    license='MIT',
+    classifiers=[
+        # Trove classifiers
+        # Full list: https://pypi.python.org/pypi?%3Aaction=list_classifiers
+        'License :: OSI Approved :: MIT License',
+        'Programming Language :: Python',
+        'Programming Language :: Python :: 3',
+        'Programming Language :: Python :: 3.6',
+    ],
+)
