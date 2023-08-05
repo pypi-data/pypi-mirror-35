069239e0e9d804a35fb2f7c1f1858a58efa76175
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-05 17:06:00 +0000

    Add package sonm/0.0.6/sonm-0.0.6.tar.gz

---
+++ packages/sonm/sonm-0.0.6.tar.gz/sonm-0.0.6/setup.py
@@ -0,0 +1,18 @@
+# python setup.py sdist bdist_egg upload
+from setuptools import setup, find_packages
+
+setup(
+    name='sonm',
+    version='0.0.6',
+    description='Wrapper for SONM API https://github.com/sonm-io',
+    author='Telminov Sergey',
+    author_email='sergey@telminov.ru',
+    url='https://github.com/telminov/sonm',
+    include_package_data=True,
+    packages=find_packages(),
+    data_files=[('', ['sonm/templates/bid.jinja2', 'sonm/templates/task.jinja2'])],
+    license='The MIT License',
+    install_requires=[
+        'jinja2',
+    ]
+)
