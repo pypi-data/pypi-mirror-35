9082388e0b6b7e22cc7f5d9241e2d40fb75e1433
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-05 18:10:35 +0000

    Add package PyFeeds/0.0.0/pyfeeds-0.0.0.tar.gz

---
+++ packages/PyFeeds/pyfeeds-0.0.0.tar.gz/pyfeeds-0.0.0/setup.py
@@ -0,0 +1,22 @@
+from setuptools import setup
+
+setup(
+    name="pyfeeds",
+    version="0.0.0",
+    # Author details
+    author="Florian Preinstorfer, Lukas Anzinger",
+    author_email="florian@nblock.org, lukas@lukasanzinger.at",
+    url="https://github.com/nblock/feeds",
+    classifiers=[
+        "Development Status :: 4 - Beta",
+        "Environment :: Console",
+        "Framework :: Scrapy",
+        "Intended Audience :: Developers",
+        "License :: OSI Approved :: GNU Affero General Public License v3",
+        "Programming Language :: Python :: 3",
+        "Programming Language :: Python :: 3.4",
+        "Programming Language :: Python :: 3.5",
+        "Programming Language :: Python :: 3.6",
+        "Topic :: Internet :: WWW/HTTP",
+    ],
+)
