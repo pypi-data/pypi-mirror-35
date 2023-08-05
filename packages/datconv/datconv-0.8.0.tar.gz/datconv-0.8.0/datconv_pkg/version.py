4b7630db149a6ba794bbc6cdbeec65c094a70752
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-05 21:12:08 +0000

    Add package datconv/0.8.0/datconv-0.8.0.tar.gz

---
+++ packages/datconv/datconv-0.8.0.tar.gz/datconv-0.8.0/datconv_pkg/version.py
@@ -0,0 +1,15 @@
+"""This module contains package meta information."""
+
+# Set version number here
+datconv_author  = "Grzegorz Wierzchowski <gwierzchowski@wp.pl>"
+datconv_status  = '4 - Beta'
+datconv_version = '0.8.0'
+datconv_date    = '2018 August'
+
+#Development Status :: 1 - Planning
+#Development Status :: 2 - Pre-Alpha
+#Development Status :: 3 - Alpha
+#Development Status :: 4 - Beta
+#Development Status :: 5 - Production/Stable
+#Development Status :: 6 - Mature
+#Development Status :: 7 - Inactive
