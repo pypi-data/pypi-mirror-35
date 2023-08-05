35e32eb4775c43aaa41fddee07ee153e673ba5e4
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-05 22:09:11 +0000

    Add package ews-cli/0.1.0.8/ews-cli-0.1.0.8.tar.gz

---
+++ packages/ews-cli/ews-cli-0.1.0.8.tar.gz/ews-cli-0.1.0.8/ews_cli/msg.py
@@ -0,0 +1,54 @@
+from . import util
+
+class header:
+
+    def __init__(self, msg):
+
+        self.sep=', '
+        self.subject = ''
+        self.author = ''
+        self.sender = ''
+        self.to_list = []
+        self.cc_list = []
+        self.to_email = ''
+        self.cc_email = ''
+        self.reply_to = ''
+
+        self.to_or_cc_list = []
+
+        self.get_email_headers(msg)
+
+    def get_email_headers(self, msg):
+
+        self.sender = msg.sender.email_address
+        self.author = msg.author.email_address
+        self.subject = msg.subject
+
+        if msg.to_recipients != None:
+
+            for mb in msg.to_recipients:
+                self.to_list.append(mb.email_address)
+
+            self.to_email = self.sep.join(self.to_list)
+
+        if msg.cc_recipients != None:
+
+            for mb in msg.cc_recipients:
+                self.cc_list.append(mb.email_address)
+
+        self.cc_email = self.sep.join(self.cc_list)
+
+        self.to_or_cc_list = self.to_list + self.cc_list
+
+        if msg.reply_to != None:
+            self.reply_to = msg.reply_to.email_address
+
+    def show_headers(self):
+        """ Show headers for debugging. """
+        print('Message Headers', flush=True)
+        print(util.get_entry('Subject : {0}', self.subject), flush=True)
+        print(util.get_entry('Sender  : {0}', self.sender), flush=True)
+        print(util.get_entry('From    : {0}', self.author), flush=True)
+        print(util.get_entry('To      : {0}', self.to_email), flush=True)
+        print(util.get_entry('CC      : {0}', self.cc_email), flush=True)
+        print(util.get_entry('Reply-To: {0}', self.reply_to), flush=True)
---
+++ packages/ews-cli/ews-cli-0.1.0.8.tar.gz/ews-cli-0.1.0.8/ews_cli/rules.py
@@ -0,0 +1,161 @@
+import os.path
+import yaml
+from collections import defaultdict
+from . import util, msg
+
+class RuleFilter:
+
+    def __init__(self, name, folder, sender, author, to, reply_to, subject, partial):
+        self.name = name
+        self.folder = folder
+        self.sender = sender
+        self.author = author
+        self.to = to
+        self.reply_to = reply_to
+        self.subject = subject
+        self.partial = partial
+
+class FilterCollection:
+
+    def __init__(self):
+        self.load_rules()
+
+    def __getitem__(self,index):
+        return self.filters[index]
+
+    def __setitem__(self,index,value):
+        self.filters[index] = value
+
+    def __delitem__(self,index):
+        del self.filters[index]
+
+    def __len__(self):
+        return len(self.filters)
+
+    def add_filter( self, name, folder, sender='', author='', to='', reply_to='', subject='', partial=False):
+        f = RuleFilter(name,folder,sender,author,to,reply_to,subject,partial)
+        self.filters.append(f)
+
+    def load_rules(self):
+        self.filters = []
+
+        cfg = util.get_config("filters.yaml")
+
+        if os.path.isfile(cfg):
+
+            stream = open(cfg, 'r')
+
+            flt = yaml.load(stream)
+
+            for data in flt:
+
+                name = data['filter_name']
+                folder = data['target_folder']
+
+                header = data['header']
+
+                sender = header['sender']
+                author = header['from']
+                to = header['to']
+                reply_to = header['reply_to']
+                subject = header['subject']
+
+                if data['match'] == 'full':
+                    partial = False
+                else:
+                    partial = True
+
+                self.add_filter(name,folder,sender,author,to,reply_to,subject,partial)
+
+            try:
+                from . import rules_custom as rc
+                rc.load_custom(self)
+            except ImportError:
+                print('No Custom Rules Found... skipping...')
+
+    def save_rules(self):
+        flt = []
+
+        for rule in self.filters:
+            data = {}
+            header = {}
+
+            data['filter_name'] = rule.name
+            data['target_folder'] = rule.folder
+
+            header['sender'] = rule.sender
+            header['from'] = rule.author
+            header['to'] = rule.to
+            header['reply_to'] = rule.reply_to
+            header['subject'] = rule.subject
+
+            data['header'] = header
+
+            if rule.partial:
+                data['match'] = 'partial'
+            else:
+                data['match'] = 'full'
+
+            flt.append(data)
+
+        if len(flt) > 0:
+            cfg = util.get_config("filters.yaml")
+            stream = open(cfg, 'w')
+            yaml.dump(flt, stream)
+
+    def process_msg(self, item):
+
+        m = msg.header(item)
+
+        for rule in self.filters:
+            # Sender:
+            if m.sender != '':
+                if rule.sender != '':
+                    if rule.partial == False:
+                        if rule.sender == m.sender:
+                            return rule.folder
+                    else:
+                        if rule.sender in m.sender:
+                            return rule.folder
+            # From:
+            if m.author != '':
+                if rule.author != '':
+                    if rule.partial == False:
+                        if rule.author == m.author:
+                            return rule.folder
+                    else:
+                        if rule.author in m.author:
+                            return rule.folder
+            # To: or CC:
+            if m.to_email != '' or m.cc_email != '':
+                if rule.to != '':
+                    for to_email in m.to_or_cc_list:
+                        to_list = rule.to.split(';')
+                        for entry in to_list:
+                            if rule.partial == False:
+                                if entry == to_email:
+                                    return rule.folder
+                            else:
+                                if entry in to_email:
+                                    return rule.folder
+            # Reply-To:
+            if m.reply_to != '':
+                if rule.reply_to != '':
+                    if rule.partial == False:
+                        if rule.reply_to == m.reply_to:
+                            return rule.folder
+                    else:
+                        if rule.reply_to in m.reply_to:
+                            return rule.folder
+            # Subject:
+            if m.subject != '':
+                if rule.subject != '':
+                    if rule.partial == False:
+                        if rule.subject == m.subject:
+                            return rule.folder
+                    else:
+                        if rule.subject in m.subject:
+                            return rule.folder
+
+        # No Match
+        return ''
