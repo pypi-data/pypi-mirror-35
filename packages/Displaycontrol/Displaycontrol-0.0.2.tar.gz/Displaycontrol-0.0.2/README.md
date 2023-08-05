3592fd0e44cccdd42ce388d7f0a10177470d8e38
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-05 20:11:28 +0000

    Add package Displaycontrol/0.0.2/Displaycontrol-0.0.2.tar.gz

---
+++ packages/Displaycontrol/Displaycontrol-0.0.2.tar.gz/Displaycontrol-0.0.2/README.md
@@ -0,0 +1,3 @@
+# Displaycontrol
+
+python package to control projectors and displays via serial or ethernet connection.
---
+++ packages/Displaycontrol/Displaycontrol-0.0.2.tar.gz/Displaycontrol-0.0.2/setup.py
@@ -0,0 +1,28 @@
+from setuptools import setup
+from os import path
+from io import open
+
+here = path.abspath(path.dirname(__file__))
+
+# Get the long description from the README file
+with open(path.join(here, 'README.md'), encoding='utf-8') as f:
+    long_description = f.read()
+
+setup(
+    name='Displaycontrol',
+    version='0.0.2',
+    packages=['displaycontrol'],
+    license='Creative Commons Attribution-Noncommercial-Share Alike license',
+    description='Package to control displays and projectors via serial and ethernet connection.',
+    long_description=long_description,
+    url='https://github.com/TopRedMedia/displaycontrol',
+    author='Mirko Haaser',
+    author_email='mirko@topred.media',
+    classifiers=[
+        'Development Status :: 3 - Alpha',
+        'Intended Audience :: Developers',
+        'Topic :: System :: Hardware',
+        'License :: OSI Approved :: MIT License',
+        'Programming Language :: Python :: 2.7',
+    ]
+)
