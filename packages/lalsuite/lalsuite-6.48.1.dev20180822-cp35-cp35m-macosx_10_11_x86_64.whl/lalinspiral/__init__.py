4f2bcb58ab01e807ce2ac9e252315d642a3a5042
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-05 20:13:35 +0000

    Add package lalsuite/6.48.1.dev20180822/lalsuite-6.48.1.dev20180822-cp35-cp35m-macosx_10_11_x86_64.whl

---
+++ packages/lalsuite/lalsuite-6.48.1.dev20180822-cp35-cp35m-macosx_10_11_x86_64.whl/lalinspiral/__init__.py
@@ -0,0 +1,7 @@
+# Import SWIG wrappings, if available
+from .lalinspiral import *
+
+__version__ = "1.7.7.1"
+
+## \addtogroup lalinspiral_python
+"""This package provides Python wrappings and extensions to LALInspiral"""
