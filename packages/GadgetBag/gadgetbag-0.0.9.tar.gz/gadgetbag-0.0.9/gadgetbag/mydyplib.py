8b5b876e5434c9996e64e90a442f6c85f47c4425
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-05 15:25:04 +0000

    Add package GadgetBag/0.0.9/gadgetbag-0.0.9.tar.gz

---
+++ packages/GadgetBag/gadgetbag-0.0.9.tar.gz/gadgetbag-0.0.9/gadgetbag/mydyplib.py
@@ -0,0 +1,23 @@
+#!/usr/bin/python
+# -*- coding: UTF-8 -*-
+
+import os
+def add(x,y):
+    return x+y
+
+def division(x,y):
+    return x/y
+
+def mutiply(x,y):
+    return x*y
+
+def subtract(x,y):
+    return x-y
+
+def main():
+    pass
+
+def dypprint():
+    print("dyp yz home")
+if __name__ == '__main__':
+    print("os.path.dirname(os.path.realpath(__file__))=%s" % os.path.dirname(os.path.realpath(__file__)))
