b43453ab01e543296f4268f2ac75c45d4b0a236c
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-05 22:13:41 +0000

    Add package gym-futures-exchange/0.0.2/gym_futures_exchange-0.0.2.tar.gz

---
+++ packages/gym-futures-exchange/gym_futures_exchange-0.0.2.tar.gz/gym_futures_exchange-0.0.2/setup.py
@@ -0,0 +1,11 @@
+from setuptools import setup
+
+setup(
+	name='gym_futures_exchange',
+    version='0.0.2',
+    url='https://github.com/nethask/gym-futures-exchange',
+  	author='Artyom Knyazev',
+ 	author_email='nethask@gmail.com',
+  	license='MIT',
+    install_requires=['gym']
+)  
