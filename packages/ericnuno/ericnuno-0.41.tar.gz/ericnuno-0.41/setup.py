9b7579c1bce95ce3774d7c03c8c498dd9e005691
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-05 15:31:33 +0000

    Add package ericnuno/0.41/ericnuno-0.41.tar.gz

---
+++ packages/ericnuno/ericnuno-0.41.tar.gz/ericnuno-0.41/setup.py
@@ -0,0 +1,15 @@
+import setuptools
+from setuptools import setup
+
+setup(name='ericnuno',
+    version='0.41',
+    description='Erics common functions',
+    url="https://github.com/kingmold/ericnuno",
+    author='Eric Nuno',
+    author_email='ericnuno@gmail.com',
+    license='MIT',
+    packages=setuptools.find_packages(),
+    classifiers=(
+        "Programming Language :: Python :: 3",
+        "License :: OSI Approved :: MIT License",
+        "Operating System :: OS Independent"))
