c2240eca90dec095936f8dc8997c31101b7f7f90
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-05 21:09:41 +0000

    Add package workflowwebtools/0.1.0/workflowwebtools-0.1.0.tar.gz

---
+++ packages/workflowwebtools/workflowwebtools-0.1.0.tar.gz/workflowwebtools-0.1.0/workflowwebtools/__init__.py
@@ -0,0 +1,8 @@
+"""Module used by the WorkflowWebTools webpages to parse and pass information.
+
+:author: Daniel Abercrombie <dabercro@mit.edu>
+"""
+
+__version__ = '0.1.0'
+
+__all__ = ['globalerrors', 'manageactions', 'manageusers', 'showlog', 'reasonsmanip']
