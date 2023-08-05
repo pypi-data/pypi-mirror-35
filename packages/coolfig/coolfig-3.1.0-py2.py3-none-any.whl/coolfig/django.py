0639bfdd44e796a4852e7951c6a54a4e110f025c
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-05 21:09:25 +0000

    Add package coolfig/3.1.0/coolfig-3.1.0-py2.py3-none-any.whl

---
+++ packages/coolfig/coolfig-3.1.0-py2.py3-none-any.whl/coolfig/django.py
@@ -0,0 +1,99 @@
+from __future__ import absolute_import
+
+import os
+import sys
+
+import six
+
+from . import types
+from .providers import FallbackProvider
+from .schema import DictValue, Settings, StaticValue, Value
+
+
+class BaseDjangoSettings(Settings):
+    SECRET_KEY = Value(str)
+    DEBUG = Value(types.boolean, default=False)
+    ALLOWED_HOSTS = Value(types.list(str), default=tuple())
+
+    DATABASES = DictValue(types.django_db_url, str.lower)
+    CACHES = DictValue(types.django_cache_url, str.lower)
+
+    # Internationalization
+    # https://docs.djangoproject.com/en/1.8/topics/i18n/
+    LANGUAGE_CODE = Value(str, default="en-us")
+    TIME_ZONE = Value(str, default="UTC")
+    USE_I18N = Value(types.boolean, default=True)
+    USE_L10N = Value(types.boolean, default=True)
+    USE_TZ = Value(types.boolean, default=True)
+
+    # Static files (CSS, JavaScript, Images)
+    # https://docs.djangoproject.com/en/1.8/howto/static-files/
+    STATIC_URL = Value(str, default="/static/")
+
+    def install(self, name=None):
+        if not name:
+            name = os.environ["DJANGO_SETTINGS_MODULE"]
+        try:
+            self.__file__ = sys.modules[name].__file__
+        except (KeyError, AttributeError):
+            pass
+        sys.modules[name] = self
+
+    def load_apps(self, apps=None):
+        if apps is None:
+            apps = getattr(self, "INSTALLED_APPS", [])
+
+        for app_path in apps:
+            settings_path = get_app_settings_path(app_path)
+
+            try:
+                app_settings = types.dottedpath(settings_path)
+            except (ImportError, AttributeError):
+                pass
+            else:
+                self.merge(app_settings)
+
+
+def get_app_settings_path(app_path):
+    try:
+        from django.apps import AppConfig
+    except ImportError:
+        # AppConfigs not supported on this django version
+        pass
+    else:
+        if "." in app_path:
+            # Check if this is a path to a Django AppConfig class
+            try:
+                app_config = types.dottedpath(app_path)
+            except (ImportError, AttributeError):
+                pass
+            else:
+                if isinstance(app_config, type) and issubclass(
+                    app_config, AppConfig
+                ):
+                    try:
+                        return app_config.settings
+                    except AttributeError:
+                        app_path = app_config.name
+
+    return app_path + ".settings.AppSettings"
+
+
+def make_django_settings(static_config, base=BaseDjangoSettings):
+    static_config = {
+        k: StaticValue(v)
+        for k, v in six.iteritems(static_config)
+        if k.upper() == k
+    }
+    return type("DjangoSettings", (base,), static_config)
+
+
+def load_django_settings(
+    provider, static_config, base=BaseDjangoSettings, apps=None, name=None
+):
+    if isinstance(provider, (list, tuple)):
+        provider = FallbackProvider(provider)
+    settings_class = make_django_settings(static_config, base)
+    settings = settings_class(provider)
+    settings.install(name)
+    settings.load_apps(apps)
---
+++ packages/coolfig/coolfig-3.1.0-py2.py3-none-any.whl/coolfig/types.py
@@ -0,0 +1,59 @@
+"""
+Common types for settings classes.
+"""
+import importlib
+
+
+class LazyCallable(object):
+    def __init__(self, module_path, callable_path):
+        self._module_path = module_path
+        self._callable_path = callable_path
+        self._func = None
+
+    @property
+    def func(self):
+        if self._func is None:
+            self._load()
+        return self._func
+
+    def _load(self):
+        try:
+            func = importlib.import_module(self._module_path)
+            for part in self._callable_path.split("."):
+                func = getattr(func, part)
+            self._func = func
+        except (ImportError, AttributeError):
+            self._func = self._not_implemented
+
+    def _not_implemented(self, *args, **kwargs):
+        raise NotImplementedError(
+            "'{}.{}' could not be imported".format(
+                self._module_path, self._callable_path
+            )
+        )
+
+    def __call__(self, *args, **kwargs):
+        return self.func(*args, **kwargs)
+
+
+def boolean(string):
+    return string.lower() in set(["1", "true", "yes", "on", "y"])
+
+
+sqlalchemy_url = LazyCallable("sqlalchemy.engine.url", "make_url")
+
+django_db_url = LazyCallable("environ", "Env.db_url_config")
+django_cache_url = LazyCallable("environ", "Env.cache_url_config")
+
+
+def list(inner_type, sep=","):
+    def convert(string):
+        return [inner_type(s.strip()) for s in string.split(sep)]
+
+    return convert
+
+
+def dottedpath(string):
+    module_path, name = string.rsplit(".", 1)
+    module = importlib.import_module(module_path)
+    return getattr(module, name)
