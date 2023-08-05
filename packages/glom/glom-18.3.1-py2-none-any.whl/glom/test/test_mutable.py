bce4c2ed94690cf0904e3358b0f454bcb871f5bf
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-05 20:12:32 +0000

    Add package glom/18.3.1/glom-18.3.1-py2-none-any.whl

---
+++ packages/glom/glom-18.3.1-py2-none-any.whl/glom/test/test_mutable.py
@@ -0,0 +1,90 @@
+import pytest
+
+from glom import glom, Path, T, Spec, Glommer, PathAssignError
+from glom.core import UnregisteredTarget
+from glom.mutable import Assign, assign
+
+
+def test_assign():
+    class Foo(object):
+        pass
+
+    assert glom({}, Assign(T['a'], 1)) == {'a': 1}
+    assert glom({'a': {}}, Assign(T['a']['a'], 1)) == {'a': {'a': 1}}
+    assert glom({'a': {}}, Assign('a.a', 1)) == {'a': {'a': 1}}
+    assert glom(Foo(), Assign(T.a, 1)).a == 1
+    assert glom({}, Assign('a', 1)) == {'a': 1}
+    assert glom(Foo(), Assign('a', 1)).a == 1
+    assert glom({'a': Foo()}, Assign('a.a', 1))['a'].a == 1
+    def r():
+        r = {}
+        r['r'] = r
+        return r
+    assert glom(r(), Assign('r.r.r.r.r.r.r.r.r', 1)) == {'r': 1}
+    assert glom(r(), Assign(T['r']['r']['r']['r'], 1)) == {'r': 1}
+    assert glom(r(), Assign(Path('r', 'r', T['r']), 1)) == {'r': 1}
+    assert assign(r(), Path('r', 'r', T['r']), 1) == {'r': 1}
+    with pytest.raises(TypeError, match='path argument must be'):
+        Assign(1, 'a')
+    with pytest.raises(ValueError, match='path must have at least one element'):
+        Assign(T, 1)
+
+
+def test_assign_spec_val():
+    output = glom({'b': 'c'}, Assign('a', Spec('b')))
+    assert output['a'] == output['b'] == 'c'
+
+
+def test_unregistered_assign():
+    # test with bare target registry
+    glommer = Glommer(register_default_types=False)
+
+    with pytest.raises(UnregisteredTarget, match='assign'):
+        glommer.glom({}, Assign('a', 'b'))
+
+    # test for unassignable tuple
+    with pytest.raises(UnregisteredTarget, match='assign'):
+        glom({'a': ()}, Assign('a.0', 'b'))
+
+
+def test_bad_assign_target():
+    class BadTarget(object):
+        def __setattr__(self, name, val):
+            raise Exception("and you trusted me?")
+
+    # sanity check
+    spec = Assign('a', 'b')
+    ok_target = lambda: None
+    glom(ok_target, spec)
+    assert ok_target.a == 'b'
+
+    with pytest.raises(PathAssignError, match='could not assign'):
+        glom(BadTarget(), spec)
+    return
+
+
+def test_sequence_assign():
+    target = {'alist': [0, 1, 2]}
+    assign(target, 'alist.2', 3)
+    assert target['alist'][2] == 3
+
+    with pytest.raises(PathAssignError, match='could not assign') as exc_info:
+        assign(target, 'alist.3', 4)
+
+    # the following test is because pypy's IndexError is different than CPython's:
+    # E         - PathAssignError(IndexError('list index out of range',), Path('alist'), '3')
+    # E         + PathAssignError(IndexError('list assignment index out of range',), Path('alist'), '3')
+    # E         ?                                  +++++++++++
+
+    exc_repr = repr(exc_info.value)
+    assert exc_repr.startswith('PathAssignError(')
+    assert exc_repr.endswith("'3')")
+    return
+
+
+def test_invalid_assign_op_target():
+    target = {'afunc': lambda x: 'hi %s' % x}
+    spec = T['afunc'](x=1)
+
+    with pytest.raises(ValueError):
+        assign(target, spec, None)
