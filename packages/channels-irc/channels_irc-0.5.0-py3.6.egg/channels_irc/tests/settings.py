70ed36ef8eda04450977c754c281b335b3675430
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-05 22:13:32 +0000

    Add package channels-irc/0.5.0/channels_irc-0.5.0-py3.6.egg

---
+++ packages/channels-irc/channels_irc-0.5.0-py3.6.egg/channels_irc/tests/settings.py
@@ -0,0 +1,16 @@
+INSTALLED_APPS: [
+    'channels',
+    'channels_irc',
+]
+
+DATABASES: {
+    "default": {
+        "ENGINE": "django.db.backends.sqlite3",
+    }
+}
+
+CHANNEL_LAYERS: {
+    "default": {
+        "BACKEND": "channels.layers.InMemoryChannelLayer",
+    },
+}
