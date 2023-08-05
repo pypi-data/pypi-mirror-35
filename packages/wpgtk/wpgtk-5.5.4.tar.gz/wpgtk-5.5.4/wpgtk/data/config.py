dbbc2e966fe0cd09f46aee567b0732076c913802
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-05 22:12:25 +0000

    Add package wpgtk/5.5.4/wpgtk-5.5.4.tar.gz

---
+++ packages/wpgtk/wpgtk-5.5.4.tar.gz/wpgtk-5.5.4/wpgtk/data/config.py
@@ -0,0 +1,69 @@
+import configparser
+import shutil
+import os
+
+import logging
+
+__version__ = '5.5.4'
+
+settings = None
+wpgtk = None
+keywords = None
+
+HOME = os.path.expanduser("~")
+WPG_DIR = os.path.join(HOME, ".config/wpg")
+CONF_FILE = os.path.join(WPG_DIR, "wpg.conf")
+MODULE_DIR = os.path.abspath(os.path.join(__file__, "../../"))
+CONF_BACKUP = os.path.join(MODULE_DIR, "misc/wpg.conf")
+WALL_DIR = os.path.join(WPG_DIR, "wallpapers")
+SAMPLE_DIR = os.path.join(WALL_DIR, "sample")
+XRES_DIR = os.path.join(WALL_DIR, "xres")
+CSS_DIR = os.path.join(WALL_DIR, "css")
+SHELL_DIR = os.path.join(WALL_DIR, "shell")
+SCHEME_DIR = os.path.join(WALL_DIR, "schemes")
+OPT_DIR = os.path.join(WPG_DIR, "templates")
+
+RCC = []  # random color cache
+
+
+FILE_DIC = {'templates':  os.path.join(HOME, ".config/wpg/templates"),
+            'icon-step1': os.path.join(HOME, ".icons/flattrcolor/scripts"
+                                             "/replace_folder_file.sh"),
+            'icon-step2': os.path.join(HOME, ".icons/flattrcolor/scripts"
+                                             "/replace_script.sh")}
+
+
+def write_conf(config_path=CONF_FILE):
+    global settings
+    with open(config_path, 'w') as config_file:
+        settings.write(config_file)
+
+
+def load_sections():
+    global settings
+    global wpgtk
+    global keywords
+    settings = configparser.ConfigParser()
+    settings.optionxform = str
+    settings.read(CONF_FILE)
+    wpgtk = settings['wpgtk']
+    keywords = settings['keywords']
+
+
+def init():
+    try:
+        if not os.path.isdir(SCHEME_DIR):
+            logging.info('creating dirs...')
+            os.makedirs(XRES_DIR, exist_ok=True)
+            os.makedirs(SAMPLE_DIR, exist_ok=True)
+            os.makedirs(SCHEME_DIR, exist_ok=True)
+            os.makedirs(OPT_DIR, exist_ok=True)
+        load_sections()
+        return 0
+    except:
+        logging.error("not a valid config file")
+        logging.info("copying default config file")
+        pass
+
+    shutil.copy(CONF_BACKUP, CONF_FILE)
+    load_sections()
---
+++ packages/wpgtk/wpgtk-5.5.4.tar.gz/wpgtk-5.5.4/wpgtk/data/themer.py
@@ -0,0 +1,112 @@
+import pywal
+import shutil
+import logging
+from os.path import realpath
+from os import remove, path, symlink
+from subprocess import Popen
+from . import color
+from . import config
+from . import files
+from . import util
+from . import sample
+
+
+def create_theme(filepath):
+    """create a colors-scheme from a filepath"""
+    filepath = realpath(filepath)
+    filename = filepath.split("/").pop().replace(" ", "_")
+    tmplink = path.join(config.WALL_DIR, ".tmp.link")
+
+    symlink(filepath, tmplink)
+
+    shutil.move(tmplink, path.join(config.WALL_DIR, filename))
+    return color.get_color_list(filename)
+
+
+def set_theme(wallpaper, colorscheme, restore=False):
+    """apply a given wallpaper and a given colorscheme"""
+    set_wall = config.wpgtk.getboolean("set_wallpaper", True)
+    colors = color.get_pywal_dict(path.join(config.WALL_DIR, colorscheme))
+    pywal.sequences.send(colors, config.WPG_DIR)
+
+    if not restore:
+        color.apply_colorscheme(colors)
+        pywal.reload.i3()
+        pywal.reload.polybar()
+
+    if set_wall:
+        pywal.wallpaper.change(path.join(config.WALL_DIR, wallpaper))
+
+    pywal.export.color(colors, "css",
+                       path.join(config.WPG_DIR, "current.css"))
+    pywal.export.color(colors, "shell",
+                       path.join(config.WPG_DIR, "current.sh"))
+    pywal.export.color(colors, "xresources",
+                       path.join(config.WPG_DIR, "current.Xres"))
+
+    flags = "-rs" if set_wall else "-nrs"
+    with open(path.join(config.WPG_DIR, "wp_init.sh"), "w") as script:
+        script.writelines(["#!/bin/bash\n",
+                           "wpg %s %s %s" % (flags, wallpaper, colorscheme)])
+
+    Popen(['chmod', '+x', path.join(config.WPG_DIR, "wp_init.sh")])
+    util.xrdb_merge(path.join(config.XRES_DIR, colorscheme + ".Xres"))
+    util.xrdb_merge(path.join(config.HOME, ".Xresources"))
+
+    files.change_current(wallpaper)
+
+    if config.wpgtk.getboolean('execute_cmd'):
+        Popen(config.wpgtk['command'].split(' '))
+
+
+def delete_theme(filename):
+    try:
+        remove(path.join(config.WALL_DIR, filename))
+        remove(path.join(config.XRES_DIR, (filename + '.Xres')))
+        files.delete_colorschemes(filename)
+    except IOError as e:
+        logging.error("file not available")
+
+
+def get_current():
+    image = realpath(path.join(config.WPG_DIR, '.current')).split('/').pop()
+    return image
+
+
+def import_theme(wallpaper, json_file, theme=False):
+    json_file = realpath(json_file)
+    filename = json_file.split("/").pop()
+    if theme:
+        theme = pywal.theme.file(filename)
+        color_list = list(theme["colors"].values())
+    else:
+        try:
+            color_list = color.get_color_list(json_file, True)
+        except IOError:
+            logging.error("file does not exist")
+            return
+
+    color.write_colors(wallpaper, color_list)
+    sample.create_sample(color_list, files.get_sample_path(wallpaper))
+    logging.info("applied %s to %s" % (filename, wallpaper))
+
+
+def set_pywal_theme(theme_name):
+    current = get_current()
+    theme = pywal.theme.file(theme_name)
+
+    color_list = list(theme["colors"].values())
+    color.write_colors(current, color_list)
+    sample.create_sample(color_list, files.get_sample_path(current))
+
+    set_theme(current, current)
+
+
+def export_theme(wallpaper, json_path="."):
+    try:
+        if(path.isdir(json_path)):
+            json_path = path.join(json_path, wallpaper + ".json")
+        shutil.copy2(path.join(files.get_cache_path(wallpaper)), json_path)
+        logging.info("theme for %s successfully exported", wallpaper)
+    except IOError as e:
+        logging.error("file not available")
