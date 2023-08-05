087a6050021a22d3ce8d26b773efa1b74f760c0f
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-05 21:09:22 +0000

    Add package torch-cluster/1.1.4/torch_cluster-1.1.4.tar.gz

---
+++ packages/torch-cluster/torch_cluster-1.1.4.tar.gz/torch_cluster-1.1.4/cpu/utils.h
@@ -0,0 +1,63 @@
+#pragma once
+
+#include <torch/torch.h>
+
+std::tuple<at::Tensor, at::Tensor> remove_self_loops(at::Tensor row,
+                                                     at::Tensor col) {
+  auto mask = row != col;
+  return std::make_tuple(row.masked_select(mask), col.masked_select(mask));
+}
+
+std::tuple<at::Tensor, at::Tensor, at::Tensor>
+remove_self_loops(at::Tensor row, at::Tensor col, at::Tensor weight) {
+  auto mask = row != col;
+  return std::make_tuple(row.masked_select(mask), col.masked_select(mask),
+                         weight.masked_select(mask));
+}
+
+at::Tensor randperm(int64_t n) {
+  auto out = at::empty(n, torch::CPU(at::kLong));
+  at::randperm_out(out, n);
+  return out;
+}
+
+std::tuple<at::Tensor, at::Tensor> rand(at::Tensor row, at::Tensor col) {
+  auto perm = randperm(row.size(0));
+  return std::make_tuple(row.index_select(0, perm), col.index_select(0, perm));
+}
+
+std::tuple<at::Tensor, at::Tensor> sort_by_row(at::Tensor row, at::Tensor col) {
+  at::Tensor perm;
+  std::tie(row, perm) = row.sort();
+  return std::make_tuple(row, col.index_select(0, perm));
+}
+
+std::tuple<at::Tensor, at::Tensor, at::Tensor>
+sort_by_row(at::Tensor row, at::Tensor col, at::Tensor weight) {
+  at::Tensor perm;
+  std::tie(row, perm) = row.sort();
+  return std::make_tuple(row, col.index_select(0, perm),
+                         weight.index_select(0, perm));
+}
+
+at::Tensor degree(at::Tensor row, int64_t num_nodes) {
+  auto zero = zeros(num_nodes, row.options());
+  auto one = ones(row.size(0), row.options());
+  return zero.scatter_add_(0, row, one);
+}
+
+std::tuple<at::Tensor, at::Tensor> to_csr(at::Tensor row, at::Tensor col,
+                                          int64_t num_nodes) {
+  std::tie(row, col) = sort_by_row(row, col);
+  row = degree(row, num_nodes).cumsum(0);
+  row = at::cat({zeros(1, row.options()), row}, 0); // Prepend zero.
+  return std::make_tuple(row, col);
+}
+
+std::tuple<at::Tensor, at::Tensor, at::Tensor>
+to_csr(at::Tensor row, at::Tensor col, at::Tensor weight, int64_t num_nodes) {
+  std::tie(row, col, weight) = sort_by_row(row, col, weight);
+  row = degree(row, num_nodes).cumsum(0);
+  row = at::cat({zeros(1, row.options()), row}, 0); // Prepend zero.
+  return std::make_tuple(row, col, weight);
+}
