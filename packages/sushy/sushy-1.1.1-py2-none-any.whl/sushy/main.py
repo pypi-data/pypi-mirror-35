495777551e6f9c78eddde889fafc1fb6b4fcfe18
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-05 20:11:41 +0000

    Add package sushy/1.1.1/sushy-1.1.1-py2-none-any.whl

---
+++ packages/sushy/sushy-1.1.1-py2-none-any.whl/sushy/main.py
@@ -0,0 +1,112 @@
+# Copyright 2017 Red Hat, Inc.
+# All Rights Reserved.
+#
+#    Licensed under the Apache License, Version 2.0 (the "License"); you may
+#    not use this file except in compliance with the License. You may obtain
+#    a copy of the License at
+#
+#         http://www.apache.org/licenses/LICENSE-2.0
+#
+#    Unless required by applicable law or agreed to in writing, software
+#    distributed under the License is distributed on an "AS IS" BASIS, WITHOUT
+#    WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied. See the
+#    License for the specific language governing permissions and limitations
+#    under the License.
+
+from sushy import connector
+from sushy import exceptions
+from sushy.resources import base
+from sushy.resources.manager import manager
+from sushy.resources.system import system
+
+
+class Sushy(base.ResourceBase):
+
+    identity = base.Field('Id', required=True)
+    """The Redfish root service identity"""
+
+    name = base.Field('Name')
+    """The Redfish root service name"""
+
+    uuid = base.Field('UUID')
+    """The Redfish root service UUID"""
+
+    _systems_path = base.Field(['Systems', '@odata.id'])
+    """SystemCollection path"""
+
+    _managers_path = base.Field(['Managers', '@odata.id'])
+    """ManagerCollection path"""
+
+    def __init__(self, base_url, username=None, password=None,
+                 root_prefix='/redfish/v1/', verify=True):
+        """A class representing a RootService
+
+        :param base_url: The base URL to the Redfish controller. It
+            should include scheme and authority portion of the URL. For
+            example: https://mgmt.vendor.com
+        :param username: User account with admin/server-profile access
+            privilege
+        :param password: User account password
+        :param root_prefix: The default URL prefix. This part includes
+            the root service and version. Defaults to /redfish/v1
+        :param verify: Either a boolean value, a path to a CA_BUNDLE
+            file or directory with certificates of trusted CAs. If set to
+            True the driver will verify the host certificates; if False
+            the driver will ignore verifying the SSL certificate; if it's
+            a path the driver will use the specified certificate or one of
+            the certificates in the directory. Defaults to True.
+        """
+        self._root_prefix = root_prefix
+        super(Sushy, self).__init__(
+            connector.Connector(base_url, username, password, verify),
+            path=self._root_prefix)
+
+    def _parse_attributes(self):
+        super(Sushy, self)._parse_attributes()
+        self.redfish_version = self.json.get('RedfishVersion')
+
+    def get_system_collection(self):
+        """Get the SystemCollection object
+
+        :raises: MissingAttributeError, if the collection attribute is
+            not found
+        :returns: a SystemCollection object
+        """
+        if not self._systems_path:
+            raise exceptions.MissingAttributeError(
+                attribute='Systems/@odata.id', resource=self._path)
+
+        return system.SystemCollection(self._conn, self._systems_path,
+                                       redfish_version=self.redfish_version)
+
+    def get_system(self, identity):
+        """Given the identity return a System object
+
+        :param identity: The identity of the System resource
+        :returns: The System object
+        """
+        return system.System(self._conn, identity,
+                             redfish_version=self.redfish_version)
+
+    def get_manager_collection(self):
+        """Get the ManagerCollection object
+
+        :raises: MissingAttributeError, if the collection attribute is
+            not found
+        :returns: a ManagerCollection object
+        """
+        if not self._managers_path:
+            raise exceptions.MissingAttributeError(
+                attribute='Managers/@odata.id', resource=self._path)
+
+        return manager.ManagerCollection(self._conn, self._managers_path,
+                                         redfish_version=self.redfish_version)
+
+    def get_manager(self, identity):
+        """Given the identity return a Manager object
+
+        :param identity: The identity of the Manager resource
+        :returns: The Manager object
+        """
+        return manager.Manager(self._conn, identity,
+                               redfish_version=self.redfish_version)
---
+++ packages/sushy/sushy-1.1.1-py2-none-any.whl/sushy/tests/unit/test_main.py
@@ -0,0 +1,102 @@
+# Copyright 2017 Red Hat, Inc.
+# All Rights Reserved.
+#
+#    Licensed under the Apache License, Version 2.0 (the "License"); you may
+#    not use this file except in compliance with the License. You may obtain
+#    a copy of the License at
+#
+#         http://www.apache.org/licenses/LICENSE-2.0
+#
+#    Unless required by applicable law or agreed to in writing, software
+#    distributed under the License is distributed on an "AS IS" BASIS, WITHOUT
+#    WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied. See the
+#    License for the specific language governing permissions and limitations
+#    under the License.
+
+import json
+
+import mock
+
+from sushy import connector
+from sushy import exceptions
+from sushy import main
+from sushy.resources.manager import manager
+from sushy.resources.system import system
+from sushy.tests.unit import base
+
+
+class MainTestCase(base.TestCase):
+
+    @mock.patch.object(connector, 'Connector', autospec=True)
+    def setUp(self, mock_connector):
+        super(MainTestCase, self).setUp()
+        self.conn = mock.Mock()
+        mock_connector.return_value = self.conn
+        with open('sushy/tests/unit/json_samples/root.json', 'r') as f:
+            self.conn.get.return_value.json.return_value = json.loads(f.read())
+        self.root = main.Sushy(
+            'http://foo.bar:1234', username='foo', password='bar',
+            verify=True)
+        mock_connector.assert_called_once_with(
+            'http://foo.bar:1234', 'foo', 'bar', True)
+
+    def test__parse_attributes(self):
+        self.root._parse_attributes()
+        self.assertEqual('RootService', self.root.identity)
+        self.assertEqual('Root Service', self.root.name)
+        self.assertEqual('1.0.2', self.root.redfish_version)
+        self.assertEqual('92384634-2938-2342-8820-489239905423',
+                         self.root.uuid)
+        self.assertEqual('/redfish/v1/Systems', self.root._systems_path)
+        self.assertEqual('/redfish/v1/Managers', self.root._managers_path)
+
+    @mock.patch.object(system, 'SystemCollection', autospec=True)
+    def test_get_system_collection(self, mock_system_collection):
+        self.root.get_system_collection()
+        mock_system_collection.assert_called_once_with(
+            self.root._conn, '/redfish/v1/Systems',
+            redfish_version=self.root.redfish_version)
+
+    @mock.patch.object(system, 'System', autospec=True)
+    def test_get_system(self, mock_system):
+        self.root.get_system('fake-system-id')
+        mock_system.assert_called_once_with(
+            self.root._conn, 'fake-system-id',
+            redfish_version=self.root.redfish_version)
+
+    @mock.patch.object(manager, 'ManagerCollection', autospec=True)
+    def test_get_manager_collection(self, ManagerCollection_mock):
+        self.root.get_manager_collection()
+        ManagerCollection_mock.assert_called_once_with(
+            self.root._conn, '/redfish/v1/Managers',
+            redfish_version=self.root.redfish_version)
+
+    @mock.patch.object(manager, 'Manager', autospec=True)
+    def test_get_manager(self, Manager_mock):
+        self.root.get_manager('fake-manager-id')
+        Manager_mock.assert_called_once_with(
+            self.root._conn, 'fake-manager-id',
+            redfish_version=self.root.redfish_version)
+
+
+class BareMinimumMainTestCase(base.TestCase):
+
+    @mock.patch.object(connector, 'Connector', autospec=True)
+    def setUp(self, mock_connector):
+        super(BareMinimumMainTestCase, self).setUp()
+        self.conn = mock.Mock()
+        mock_connector.return_value = self.conn
+        with open('sushy/tests/unit/json_samples/'
+                  'bare_minimum_root.json', 'r') as f:
+            self.conn.get.return_value.json.return_value = json.loads(f.read())
+        self.root = main.Sushy('http://foo.bar:1234', verify=True)
+
+    def test_get_system_collection_when_systems_attr_absent(self):
+        self.assertRaisesRegex(
+            exceptions.MissingAttributeError,
+            'Systems/@odata.id', self.root.get_system_collection)
+
+    def test_get_manager_collection_when_managers_attr_absent(self):
+        self.assertRaisesRegex(
+            exceptions.MissingAttributeError,
+            'Managers/@odata.id', self.root.get_manager_collection)
