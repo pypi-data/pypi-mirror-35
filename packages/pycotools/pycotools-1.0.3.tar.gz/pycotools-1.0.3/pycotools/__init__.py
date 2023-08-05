4b4547fcafa873aa396cdece380c921b718fc57e
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-05 15:29:25 +0000

    Add package pycotools/1.0.3/pycotools-1.0.3.tar.gz

---
+++ packages/pycotools/pycotools-1.0.3.tar.gz/pycotools-1.0.3/pycotools/__init__.py
@@ -0,0 +1,29 @@
+# import viz
+# import tasks
+# import errors
+# import misc
+# import model
+import os
+
+import logging.config
+
+global LOG_FILENAME
+LOG_CONFIG_FILE = os.path.join(os.path.dirname(os.path.abspath(__file__)),
+                               'logging_config.conf')
+
+if os.path.isfile(LOG_CONFIG_FILE) is not True:
+    raise Exception
+logging.config.fileConfig(LOG_CONFIG_FILE,
+                          disable_existing_loggers=False)
+
+LOG = logging.getLogger('root')
+
+
+
+global __version__
+#version
+MAJOR = 1
+MINOR = 0
+MICRO = 3
+
+__version__ = '%d.%d.%d' % (MAJOR, MINOR, MICRO)
