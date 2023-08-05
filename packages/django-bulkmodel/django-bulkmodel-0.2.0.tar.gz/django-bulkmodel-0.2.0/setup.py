699691fd39acc9c98a0f67343c261137d5e8be91
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-05 15:05:03 +0000

    Add package django-bulkmodel/0.2.0/django-bulkmodel-0.2.0.tar.gz

---
+++ packages/django-bulkmodel/django-bulkmodel-0.2.0.tar.gz/django-bulkmodel-0.2.0/setup.py
@@ -0,0 +1,42 @@
+import os
+from setuptools import find_packages, setup
+
+with open(os.path.join(os.path.dirname(__file__), 'README.md')) as readme:
+    README = readme.read()
+
+# allow setup.py to be run from any path
+os.chdir(os.path.normpath(os.path.join(os.path.abspath(__file__), os.pardir)))
+
+setup(
+    name='django-bulkmodel',
+    version='0.2.0',
+    packages=find_packages(),
+    include_package_data=True,
+    license='Apache Software License',
+    description='Adds missing features to Django ORM for working with data in bulk operations',
+    long_description=README,
+    long_description_content_type="text/markdown",
+    url='https://github.com/ailling/django-bulkmodel',
+    author='Alan Illing',
+    author_email='alanilling@protonmail.com',
+    keywords = ['django', 'orm', 'database', 'bulk', 'data'],
+
+    install_requires= [
+        'django>=1.9',
+    ],
+
+    classifiers=[
+        'Environment :: Web Environment',
+        'Framework :: Django',
+        'Framework :: Django :: 1.11',
+        'Intended Audience :: Developers',
+        'License :: OSI Approved :: Apache Software License',
+        'Operating System :: OS Independent',
+        'Programming Language :: Python',
+        'Programming Language :: Python :: 3.4',
+        'Programming Language :: Python :: 3.5',
+        'Programming Language :: Python :: 3.6',
+        'Topic :: Database',
+        'Topic :: Database :: Front-Ends'
+    ],
+)
