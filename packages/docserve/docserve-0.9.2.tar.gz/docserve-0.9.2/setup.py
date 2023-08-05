1d9fa4a6d86583244cbf193296bb710eed4f47a3
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-05 15:05:47 +0000

    Add package docserve/0.9.2/docserve-0.9.2.tar.gz

---
+++ packages/docserve/docserve-0.9.2.tar.gz/docserve-0.9.2/setup.py
@@ -0,0 +1,42 @@
+from os import environ
+from setuptools import setup, find_packages
+
+
+with open('README.md', 'r') as f:
+    long_description = f.read()
+
+setup(
+    name='docserve',
+    version='0.9.2',
+    author='Hardy & Ellis Inventions LTD',
+    author_email='support@heinventions.com',
+
+    description="A tool for quick and easy Markdown project documentation.",
+    long_description=long_description,
+    long_description_content_type="text/markdown",
+
+    url="https://github.com/HEInventions/DocServe",
+
+    python_requires='>=3.6.0',
+
+    packages=find_packages(),
+    include_package_data=True,
+    py_modules=['docserve'],
+    install_requires=[
+        'Flask==1.0.2',
+        'Markdown==2.6.11',
+        'markdown-checklist==0.4.1',
+        'timeago==1.0.8',
+        'Frozen-Flask==0.15'
+    ],
+    classifiers=(
+        "Programming Language :: Python :: 3",
+        "License :: OSI Approved :: MIT License",
+    ),
+
+    entry_points = {
+        'console_scripts': [
+            'docserve = docserve.docserve:main',
+        ],
+    },
+)
