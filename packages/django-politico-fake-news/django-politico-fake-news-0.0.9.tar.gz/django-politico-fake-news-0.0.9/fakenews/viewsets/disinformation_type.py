c43926e73eb4d42adc40d27693ca1cb12e679d97
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-05 15:05:29 +0000

    Add package django-politico-fake-news/0.0.9/django-politico-fake-news-0.0.9.tar.gz

---
+++ packages/django-politico-fake-news/django-politico-fake-news-0.0.9.tar.gz/django-politico-fake-news-0.0.9/fakenews/viewsets/disinformation_type.py
@@ -0,0 +1,19 @@
+from .base import TokenAuthedViewSet
+
+from fakenews.models import DisinformationType
+from fakenews.serializers import (
+    DisinformationTypeSerializer,
+    DisinformationTypeListSerializer
+)
+
+
+class DisinformationTypeViewset(TokenAuthedViewSet):
+    queryset = DisinformationType.objects.all()
+    serializer_class = DisinformationTypeSerializer
+    lookup_field = 'pk'
+    throttle_classes = []
+
+    def get_serializer_class(self):
+        if hasattr(self, 'action') and self.action == 'list':
+            return DisinformationTypeListSerializer
+        return DisinformationTypeSerializer
