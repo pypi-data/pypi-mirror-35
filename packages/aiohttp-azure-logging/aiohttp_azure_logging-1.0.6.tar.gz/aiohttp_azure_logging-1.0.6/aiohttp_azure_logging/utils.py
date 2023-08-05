f436130d3e8f0d668cdb99f7ad950797c4c2b884
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-05 20:12:01 +0000

    Add package aiohttp-azure-logging/1.0.6/aiohttp_azure_logging-1.0.6.tar.gz

---
+++ packages/aiohttp-azure-logging/aiohttp_azure_logging-1.0.6.tar.gz/aiohttp_azure_logging-1.0.6/aiohttp_azure_logging/utils.py
@@ -0,0 +1,18 @@
+import inspect
+from aiohttp.web_request import Request, BaseRequest
+
+def find_request():
+    """
+    Finds the aiohttp post processed request object from the stack
+    """
+    print("finding request")
+    request = None
+    frames = inspect.stack()
+    for frame in [frames[i].frame for i in range(len(frames)-1, 0, -1)]:
+        if ('request' in frame.f_locals.keys()
+                and isinstance(frame.f_locals.get('request')) == Request
+                and isinstance(frame.f_locals.get('request')) != BaseRequest
+           ):
+            request = frame.f_locals.get('request')
+            break
+    return request
