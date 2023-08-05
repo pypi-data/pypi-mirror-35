e283d5c57c4b69e61513cdca3567482cfcfa35df
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-05 15:05:48 +0000

    Add package jparse/0.1.2/jparse-0.1.2-py2.py3-none-any.whl

---
+++ packages/jparse/jparse-0.1.2-py2.py3-none-any.whl/jparse/__init__.py
@@ -0,0 +1,7 @@
+# -*- coding: utf-8 -*-
+# @Author: Eli Song
+# @Date:   2018-07-12 23:25:08
+# @Last Modified by:   Eli Song
+# @Last Modified time: 2018-08-12 23:34:19
+
+from .jparse import JParser
