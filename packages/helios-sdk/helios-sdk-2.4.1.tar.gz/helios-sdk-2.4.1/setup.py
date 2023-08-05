3beca7aeb5c99ac82d8880b0c07f2e1d89071c77
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-05 21:10:23 +0000

    Add package helios-sdk/2.4.1/helios-sdk-2.4.1.tar.gz

---
+++ packages/helios-sdk/helios-sdk-2.4.1.tar.gz/helios-sdk-2.4.1/setup.py
@@ -0,0 +1,34 @@
+from setuptools import find_packages
+from setuptools import setup
+
+version = '2.4.1'
+
+setup(name='helios-sdk',
+      version=version,
+      description='Use the Helios APIs in Python',
+      author='Michael Bayer',
+      author_email='mbayer@harris.com',
+      url='https://github.com/harris-helios/helios-sdk-python',
+      download_url='https://github.com/harris-helios/'
+                   'helios-sdk-python/archive/{}.tar.gz'.format(version),
+      license='MIT',
+      install_requires=['requests>=2.0.0',
+                        'numpy>=1.13.0',
+                        'pandas>=0.21.0',
+                        'Pillow>=5.0.0',
+                        'python-dateutil>=2.7.0'],
+      extras_require={
+          'tests': ['pytest>=3.5.0'],
+      },
+      classifiers=[
+          'Development Status :: 5 - Production/Stable',
+          'Intended Audience :: Developers',
+          'License :: OSI Approved :: MIT License',
+          'Programming Language :: Python :: 2',
+          'Programming Language :: Python :: 2.7',
+          'Programming Language :: Python :: 3',
+          'Programming Language :: Python :: 3.6',
+          'Topic :: Software Development :: Libraries',
+          'Topic :: Software Development :: Libraries :: Python Modules'
+      ],
+      packages=find_packages())
