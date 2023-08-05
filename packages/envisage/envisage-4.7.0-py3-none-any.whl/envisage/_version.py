17c5f47cc0e6eff46e534b2506d168bb77d2b72b
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-05 15:31:04 +0000

    Add package envisage/4.7.0/envisage-4.7.0-py3-none-any.whl

---
+++ packages/envisage/envisage-4.7.0-py3-none-any.whl/envisage/_version.py
@@ -0,0 +1,8 @@
+# This file was automatically generated from the `setup.py` script.
+version = '4.7.0'
+full_version = '4.7.0'
+git_revision = 'Unknown'
+is_released = True
+
+if not is_released:
+    version = full_version
