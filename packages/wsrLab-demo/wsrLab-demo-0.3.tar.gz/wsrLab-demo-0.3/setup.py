d921cea5c72abe9c86fd841aa3171dab6363d269
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-05 16:55:44 +0000

    Add package wsrLab-demo/0.3/wsrLab-demo-0.3.tar.gz

---
+++ packages/wsrLab-demo/wsrLab-demo-0.3.tar.gz/wsrLab-demo-0.3/setup.py
@@ -0,0 +1,13 @@
+# _*_ coding:utf-8 _*_
+from setuptools import setup
+
+setup(
+	name='wsrLab-demo',
+	version='0.3',
+	author='wsr',
+	author_email='702183233@qq.com',
+	url='http://www.x.com',
+	description="这是一个测试文件，用户PyPi测试！",
+	packages=['wsrLab-demo'],
+	zip_safe=False
+)
