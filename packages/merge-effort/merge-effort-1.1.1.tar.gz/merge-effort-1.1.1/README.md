38e0ffe8bc9bec6dfd6b26e0ab676dd81a191734
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-05 21:10:19 +0000

    Add package merge-effort/1.1.1/merge-effort-1.1.1.tar.gz

---
+++ packages/merge-effort/merge-effort-1.1.1.tar.gz/merge-effort-1.1.1/README.md
@@ -0,0 +1,102 @@
+# Merge Effort
+
+This project aims at analyzing the merge effort of Git projects. In this regard, it calculates some metrics: 
+
+* The number of actions (i.e., lines added or removed) in each branch
+* The number of actions in the merge commit
+* The rework: similar actions in both branches
+* The wasted actions: actions in the branches that were not merged
+* The extra actions: actions not in the branches that were added during the merge
+
+## Getting Started
+
+### Prerequisites
+
+This project requires python, pygit2 and libgit2, and it was tested on the following versions:
+
+```
+python==3.6
+pygit2==0.27.0
+libgit2==0.27.0
+```
+
+First of all, if you are using Mac OS or Linux you need to install libgit2. If you have Anaconda installed in your computer, you can simple do:
+
+```
+$ conda install -c conda-forge libgit2
+```
+
+Otherwise, on Mac OS you can install libgit2 using homebrew:
+
+```
+$ brew install libgit2
+```
+
+On Linux you can install the latest version of libgit2 by doing:
+
+```
+$ wget https://github.com/libgit2/libgit2/archive/v0.27.0.tar.gz
+$ tar xzf v0.27.0.tar.gz
+$ cd libgit2-0.27.0/
+$ cmake .
+$ make
+$ sudo make install
+$ sudo ldconfig
+```
+
+For more information http://www.pygit2.org/install.html
+
+### Installing
+
+To install merge-effort you should do:
+
+```
+$ pip install merge-effort
+```
+
+## Basic Usage
+
+To run the script with a local repository:
+
+```
+merge-effort --local [path]
+
+```
+
+or you can run it passing a git url:
+
+```
+merge-effort --url [git_url]
+
+```
+
+By defaul the script will analyze all merge commits in the repository, but you can pass one or more commits using their hash
+
+```
+merge-effort --url [git_url] --commit [commit1 commit2]
+
+```
+
+By default the script will retun the merge effort, but if you want to see the metrics normalized you can set --normalized
+
+```
+merge-effort --url [git_url] --normalized
+
+```
+
+## Team
+
+
+* Tayane Silva Fernandes de Moura (UFF, Brazil)
+* Leonardo Gresta Paulino Murta (UFF, Brazil)
+
+
+## License
+
+Copyright (c) 2018 Universidade Federal Fluminense (UFF)
+
+Permission is hereby granted, free of charge, to any person obtaining a copy of this software and associated documentation files (the "Software"), to deal in the Software without restriction, including without limitation the rights to use, copy, modify, merge, publish, distribute, sublicense, and/or sell copies of the Software, and to permit persons to whom the Software is furnished to do so, subject to the following conditions:
+
+The above copyright notice and this permission notice shall be included in all copies or substantial portions of the Software.
+
+THE SOFTWARE IS PROVIDED "AS IS", WITHOUT WARRANTY OF ANY KIND, EXPRESS OR IMPLIED, INCLUDING BUT NOT LIMITED TO THE WARRANTIES OF MERCHANTABILITY, FITNESS FOR A PARTICULAR PURPOSE AND NONINFRINGEMENT. IN NO EVENT SHALL THE AUTHORS OR COPYRIGHT HOLDERS BE LIABLE FOR ANY CLAIM, DAMAGES OR OTHER LIABILITY, WHETHER IN AN ACTION OF CONTRACT, TORT OR OTHERWISE, ARISING FROM, OUT OF OR IN CONNECTION WITH THE SOFTWARE OR THE USE OR OTHER DEALINGS IN THE SOFTWARE.
---
+++ packages/merge-effort/merge-effort-1.1.1.tar.gz/merge-effort-1.1.1/mergeeffort/merge_analysis.py
@@ -0,0 +1,131 @@
+from datetime import datetime
+startTime = datetime.now()
+from collections import Counter
+
+from pygit2 import *
+import os
+import shutil
+import argparse
+import time
+import traceback
+
+
+def get_actions(diff_a_b):
+	actions = Counter()
+	for d in diff_a_b:
+		file_name = d.delta.new_file.path
+		for h in d.hunks:
+			for l in h.lines:
+				actions.update([file_name+l.origin+l.content])
+	return actions
+
+def clone(url):
+	repo_url = url
+	current_working_directory = os.getcwd()
+	repo_path = current_working_directory + "/build/" + str(time.time())
+	repo = clone_repository(repo_url, repo_path) 
+
+	return repo
+
+
+def calculate_rework(parent1_actions, parent2_actions):
+	rework_actions = parent1_actions & parent2_actions
+	return (sum(rework_actions.values()))
+
+def calculate_wasted_effort(parents_actions, merge_actions):
+	wasted_actions = parents_actions - merge_actions
+
+	return (sum(wasted_actions.values()))
+
+def calculate_additional_effort(parents_actions, merge_actions):
+	additional_actions = merge_actions - parents_actions
+	return (sum(additional_actions.values()))
+
+
+def analyse(commits, repo, normalized=False):
+	commits_metrics = {}
+	try:
+		for commit in commits:
+			if (len(commit.parents)==2):
+				parent1 = commit.parents[0]
+				parent2 = commit.parents[1]
+				base = repo.merge_base(parent1.hex, parent2.hex)
+				if(base): 
+					base_version = repo.get(base)
+					
+					diff_base_final = repo.diff(base_version, commit, context_lines=0)
+					diff_base_parent1 = repo.diff(base_version, parent1, context_lines=0)
+					diff_base_parent2 = repo.diff(base_version, parent2, context_lines=0)
+
+					merge_actions = get_actions(diff_base_final)
+					parent1_actions = get_actions(diff_base_parent1)
+					parent2_actions = get_actions(diff_base_parent2)
+
+					commits_metrics[commit.hex] = calculate_metrics(merge_actions, parent1_actions, parent2_actions, normalized)
+				else:
+					print(commit.hex + " - this merge doesn't have a base version")
+	except:
+		print ("Unexpected error")
+		print (traceback.format_exc())
+
+	return commits_metrics	
+
+def delete_repo_folder(folder):
+		shutil.rmtree(folder)
+
+def calculate_metrics(merge_actions, parent1_actions, parent2_actions, normalized):	
+	metrics = {}
+	
+	parents_actions = parent1_actions + parent2_actions 
+
+	if(normalized):
+		metrics['rework'] = calculate_rework(parent1_actions, parent2_actions)/sum(parents_actions.values())
+		metrics['wasted']  = calculate_wasted_effort(parents_actions, merge_actions)/sum(parents_actions.values())
+		metrics['extra'] =calculate_additional_effort(parents_actions, merge_actions)/sum(merge_actions.values())
+
+	else:
+		metrics['branch1'] = len(parent1_actions)
+		metrics['branch2'] = len(parent2_actions)
+		metrics['merge'] = len(merge_actions)
+		metrics['rework'] = calculate_rework(parent1_actions, parent2_actions)
+		metrics['wasted']  = calculate_wasted_effort(parents_actions, merge_actions)
+		metrics['extra'] = calculate_additional_effort(parents_actions, merge_actions)
+		
+	return metrics
+
+			
+def main():
+	parser = argparse.ArgumentParser(description='Merge effort analysis')
+	group = parser.add_mutually_exclusive_group(required=True)
+	group.add_argument("--url", help="set an url for a git repository")
+	group.add_argument("--local", help="set the path of a local git repository")
+	parser.add_argument("--commit", nargs='+', help="set the commit (or a list of commits separated by comma) to analyse. Default: all merge commits")
+	parser.add_argument("--normalized",action='store_true', help="show metrics normalized")
+	args = parser.parse_args()
+
+	if args.url:
+		repo = clone(args.url) 
+
+	elif args.local:
+		repo = Repository(args.local)
+
+	commits = []
+	if args.commit:
+		for commit in args.commit:
+			commits.append(repo.get(commit))
+
+	else:
+		commits = repo.walk(repo.head.target, GIT_SORT_TIME | GIT_SORT_REVERSE)
+
+	commits_metrics = analyse(commits, repo, args.normalized)
+	print(commits_metrics)
+	print("Total of merge commits: " + str(len(commits_metrics)))
+	if args.url:
+		delete_repo_folder(repo.workdir)
+
+	print(datetime.now() - startTime)
+
+	
+if __name__ == '__main__':
+	main()	
+
