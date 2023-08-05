f5aadd1d3be02022b73876e2172e28941bf723f3
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-05 15:04:24 +0000

    Add package vat-build-utils/0.2.2/vat_build_utils-0.2.2-py2.py3-none-any.whl

---
+++ packages/vat-build-utils/vat_build_utils-0.2.2-py2.py3-none-any.whl/build_utils/rules/write_output_to_hcl.py
@@ -0,0 +1,11 @@
+import logging
+import os
+
+from build_utils import utils
+
+logger = logging.getLogger(__name__)
+
+def write_output_to_hcl(execution_context, path):
+    hcl_output = utils.dump_config_to_hcl(execution_context.output)
+    output_file_path = os.path.join(execution_context.dir_path, path)
+    utils.write_text_file(output_file_path, hcl_output)
