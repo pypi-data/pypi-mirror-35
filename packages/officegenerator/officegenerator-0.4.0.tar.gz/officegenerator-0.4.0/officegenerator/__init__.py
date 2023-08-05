208bc4ef46f44cb68fac74bce713c1696f4cb9bb
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-05 16:56:46 +0000

    Add package officegenerator/0.4.0/officegenerator-0.4.0.tar.gz

---
+++ packages/officegenerator/officegenerator-0.4.0.tar.gz/officegenerator-0.4.0/officegenerator/__init__.py
@@ -0,0 +1,9 @@
+## @package officegenerator
+## @brief Generate office files
+
+import datetime
+from .libodfgenerator import *
+from .libxlsxgenerator import *
+
+__version__ = '0.4.0'
+__versiondate__=datetime.date(2018,8,17)
