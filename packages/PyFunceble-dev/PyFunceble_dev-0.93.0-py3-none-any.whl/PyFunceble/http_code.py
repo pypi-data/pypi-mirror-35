3efcaa4a39f0bccb8124512fa62505b46d102c22
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-05 15:24:26 +0000

    Add package PyFunceble-dev/0.93.0/PyFunceble_dev-0.93.0-py3-none-any.whl

---
+++ packages/PyFunceble-dev/PyFunceble_dev-0.93.0-py3-none-any.whl/PyFunceble/http_code.py
@@ -0,0 +1,148 @@
+#!/usr/bin/env python3
+
+# pylint:disable=line-too-long
+"""
+The tool to check domains or IP availability.
+
+::
+
+
+    :::::::::  :::   ::: :::::::::: :::    ::: ::::    :::  ::::::::  :::::::::: :::::::::  :::        ::::::::::
+    :+:    :+: :+:   :+: :+:        :+:    :+: :+:+:   :+: :+:    :+: :+:        :+:    :+: :+:        :+:
+    +:+    +:+  +:+ +:+  +:+        +:+    +:+ :+:+:+  +:+ +:+        +:+        +:+    +:+ +:+        +:+
+    +#++:++#+    +#++:   :#::+::#   +#+    +:+ +#+ +:+ +#+ +#+        +#++:++#   +#++:++#+  +#+        +#++:++#
+    +#+           +#+    +#+        +#+    +#+ +#+  +#+#+# +#+        +#+        +#+    +#+ +#+        +#+
+    #+#           #+#    #+#        #+#    #+# #+#   #+#+# #+#    #+# #+#        #+#    #+# #+#        #+#
+    ###           ###    ###         ########  ###    ####  ########  ########## #########  ########## ##########
+
+This submodule will provide HTTP Code extraction logic and interface.
+
+Author:
+    Nissar Chababy, @funilrys, contactTATAfunilrysTODTODcom
+
+Special thanks:
+    https://pyfunceble.readthedocs.io/en/dev/special-thanks.html
+
+Contributors:
+    http://pyfunceble.readthedocs.io/en/dev/special-thanks.html
+
+Project link:
+    https://github.com/funilrys/PyFunceble
+
+Project documentation:
+    https://pyfunceble.readthedocs.io
+
+Project homepage:
+    https://funilrys.github.io/PyFunceble/
+
+License:
+::
+
+
+    MIT License
+
+    Copyright (c) 2017-2018 Nissar Chababy
+
+    Permission is hereby granted, free of charge, to any person obtaining a copy
+    of this software and associated documentation files (the "Software"), to deal
+    in the Software without restriction, including without limitation the rights
+    to use, copy, modify, merge, publish, distribute, sublicense, and/or sell
+    copies of the Software, and to permit persons to whom the Software is
+    furnished to do so, subject to the following conditions:
+
+    The above copyright notice and this permission notice shall be included in all
+    copies or substantial portions of the Software.
+
+    THE SOFTWARE IS PROVIDED "AS IS", WITHOUT WARRANTY OF ANY KIND, EXPRESS OR
+    IMPLIED, INCLUDING BUT NOT LIMITED TO THE WARRANTIES OF MERCHANTABILITY,
+    FITNESS FOR A PARTICULAR PURPOSE AND NONINFRINGEMENT. IN NO EVENT SHALL THE
+    AUTHORS OR COPYRIGHT HOLDERS BE LIABLE FOR ANY CLAIM, DAMAGES OR OTHER
+    LIABILITY, WHETHER IN AN ACTION OF CONTRACT, TORT OR OTHERWISE, ARISING FROM,
+    OUT OF OR IN CONNECTION WITH THE SOFTWARE OR THE USE OR OTHER DEALINGS IN THE
+    SOFTWARE.
+"""
+# pylint: enable=line-too-long
+import urllib3.exceptions as urllib3_exceptions
+
+# pylint: disable=bad-continuation
+import PyFunceble
+from PyFunceble import requests, socket
+
+
+class HTTPCode:  # pylint: disable=too-few-public-methods
+    """
+    Get and return the HTTP code status of a given domain.
+
+    Argument:
+        - full_url: bool
+            - False: We check only in a domain mode environnment.
+            - True: We check in a www environnment.
+
+    """
+
+    def __init__(self, full_url=False):  # pragma: no cover
+        self.full_url = full_url
+
+        if full_url:
+            self.to_get = PyFunceble.CONFIGURATION["URL"]
+        else:
+            self.to_get = "http://%s:80" % PyFunceble.CONFIGURATION["domain"]
+
+        if PyFunceble.CONFIGURATION["user_agent"]:
+            self.headers = {"User-Agent": PyFunceble.CONFIGURATION["user_agent"]}
+        else:
+            self.headers = {}
+
+    def _access(self):  # pragma: no cover
+        """
+        Get the HTTP code status.
+
+        Returns: int or None
+            int: The catched HTTP status_code.
+            None: Nothing catched.
+        """
+
+        try:
+            req = requests.head(
+                self.to_get,
+                timeout=PyFunceble.CONFIGURATION["seconds_before_http_timeout"],
+                headers=self.headers,
+            )
+
+            return req.status_code
+
+        except (
+            requests.exceptions.InvalidURL,
+            socket.timeout,
+            requests.exceptions.Timeout,
+            requests.ConnectionError,
+            urllib3_exceptions.InvalidHeader,
+            UnicodeDecodeError,  # The probability that this happend in production is minimal.
+        ):
+            return None
+
+    def get(self):
+        """
+        Return the HTTP code status.
+
+        Returns: str or int
+            str: if no status_code is catched.
+            int: the status_code.
+        """
+        if PyFunceble.HTTP_CODE["active"]:
+            http_code = self._access()
+            list_of_valid_http_code = []
+
+            for codes in [
+                PyFunceble.HTTP_CODE["list"]["up"],
+                PyFunceble.HTTP_CODE["list"]["potentially_down"],
+                PyFunceble.HTTP_CODE["list"]["potentially_up"],
+            ]:
+                list_of_valid_http_code.extend(codes)
+
+            if http_code not in list_of_valid_http_code or http_code is None:
+                return "*" * 3
+
+            return http_code
+
+        return None
---
+++ packages/PyFunceble-dev/PyFunceble_dev-0.93.0-py3-none-any.whl/PyFunceble/iana.py
@@ -0,0 +1,296 @@
+#!/usr/bin/env python3
+
+# pylint:disable=line-too-long
+"""
+The tool to check domains or IP availability.
+
+::
+
+
+    :::::::::  :::   ::: :::::::::: :::    ::: ::::    :::  ::::::::  :::::::::: :::::::::  :::        ::::::::::
+    :+:    :+: :+:   :+: :+:        :+:    :+: :+:+:   :+: :+:    :+: :+:        :+:    :+: :+:        :+:
+    +:+    +:+  +:+ +:+  +:+        +:+    +:+ :+:+:+  +:+ +:+        +:+        +:+    +:+ +:+        +:+
+    +#++:++#+    +#++:   :#::+::#   +#+    +:+ +#+ +:+ +#+ +#+        +#++:++#   +#++:++#+  +#+        +#++:++#
+    +#+           +#+    +#+        +#+    +#+ +#+  +#+#+# +#+        +#+        +#+    +#+ +#+        +#+
+    #+#           #+#    #+#        #+#    #+# #+#   #+#+# #+#    #+# #+#        #+#    #+# #+#        #+#
+    ###           ###    ###         ########  ###    ####  ########  ########## #########  ########## ##########
+
+This submodule will provide the IANA logic and interface.
+
+Author:
+    Nissar Chababy, @funilrys, contactTATAfunilrysTODTODcom
+
+Special thanks:
+    https://pyfunceble.readthedocs.io/en/dev/special-thanks.html
+
+Contributors:
+    http://pyfunceble.readthedocs.io/en/dev/special-thanks.html
+
+Project link:
+    https://github.com/funilrys/PyFunceble
+
+Project documentation:
+    https://pyfunceble.readthedocs.io
+
+Project homepage:
+    https://funilrys.github.io/PyFunceble/
+
+License:
+::
+
+
+    MIT License
+
+    Copyright (c) 2017-2018 Nissar Chababy
+
+    Permission is hereby granted, free of charge, to any person obtaining a copy
+    of this software and associated documentation files (the "Software"), to deal
+    in the Software without restriction, including without limitation the rights
+    to use, copy, modify, merge, publish, distribute, sublicense, and/or sell
+    copies of the Software, and to permit persons to whom the Software is
+    furnished to do so, subject to the following conditions:
+
+    The above copyright notice and this permission notice shall be included in all
+    copies or substantial portions of the Software.
+
+    THE SOFTWARE IS PROVIDED "AS IS", WITHOUT WARRANTY OF ANY KIND, EXPRESS OR
+    IMPLIED, INCLUDING BUT NOT LIMITED TO THE WARRANTIES OF MERCHANTABILITY,
+    FITNESS FOR A PARTICULAR PURPOSE AND NONINFRINGEMENT. IN NO EVENT SHALL THE
+    AUTHORS OR COPYRIGHT HOLDERS BE LIABLE FOR ANY CLAIM, DAMAGES OR OTHER
+    LIABILITY, WHETHER IN AN ACTION OF CONTRACT, TORT OR OTHERWISE, ARISING FROM,
+    OUT OF OR IN CONNECTION WITH THE SOFTWARE OR THE USE OR OTHER DEALINGS IN THE
+    SOFTWARE.
+"""
+# pylint: enable=line-too-long
+import PyFunceble
+from PyFunceble.helpers import Dict, Download, Regex
+from PyFunceble.lookup import Lookup
+
+
+class IANA:  # pragma: no cover pylint: disable=too-few-public-methods
+    """
+    Logic behind the update of `iana-domains-db.json`
+    """
+
+    def __init__(self):
+        if not PyFunceble.CONFIGURATION["quiet"]:
+            print("Update of iana-domains-db", end=" ")
+
+        self.destination = PyFunceble.OUTPUTS["default_files"]["iana"]
+        self.iana_db = {}
+
+        self.update()
+
+    @classmethod
+    def _data(cls):
+        """
+        Get the database from IANA website.
+        """
+        iana_url = "https://www.iana.org/domains/root/db"
+
+        return Download(iana_url, return_data=True).text()
+
+    @classmethod
+    def _referer(cls, extension):
+        """
+        Return the referer for the given extension.
+
+        :pram extension: A string, a valid domain extension.
+        """
+
+        manual_server = {
+            "aaa": "whois.nic.aaa",
+            "abb": "whois.nic.abb",
+            "able": "whois.nic.able",
+            "accenture": "whois.nic.accenture",
+            "aetna": "whois.nic.aetna",
+            "aig": "whois.nic.aig",
+            "americanexpress": "whois.nic.americanexpress",
+            "amex": "whois.nic.amex",
+            "amica": "whois.nic.amica",
+            "amsterdam": "whois.nic.amsterdam",
+            "analytics": "whois.nic.analytics",
+            "aramco": "whois.nic.aramco",
+            "arte": "whois.nic.arte",
+            "as": "whois.nic.as",
+            "athleta": "whois.nic.athleta",
+            "audible": "whois.nic.audible",
+            "author": "whois.nic.author",
+            "aws": "whois.nic.aws",
+            "axa": "whois.nic.axa",
+            "azure": "whois.nic.azure",
+            "baby": "whois.nic.baby",
+            "banamex": "whois.nic.banamex",
+            "bananarepublic": "whois.nic.bananarepublic",
+            "baseball": "whois.nic.baseball",
+            "bharti": "whois.nic.bharti",
+            "bing": "whois.nic.bing",
+            "bloomberg": "whois.nic.bloomberg",
+            "bm": "whois.afilias-srs.net",
+            "book": "whois.nic.book",
+            "booking": "whois.nic.booking",
+            "bot": "whois.nic.bot",
+            "bz": "whois.afilias-grs.net",
+            "buzz": "whois.nic.buzz",
+            "call": "whois.nic.call",
+            "calvinklein": "whois.nic.calvinklein",
+            "caravan": "whois.nic.caravan",
+            "cartier": "whois.nic.cartier",
+            "caseih": "whois.nic.caseih",
+            "cbn": "whois.nic.cbn",
+            "cbre": "whois.nic.cbre",
+            "cd": "chois.nic.cd",
+            "chase": "whois.nic.chase",
+            "circle": "whois.nic.circle",
+            "cisco": "whois.nic.cisco",
+            "citadel": "whois.nic.citadel",
+            "citi": "whois.nic.citi",
+            "citic": "whois.nic.citic",
+            "cm": "whois.netcom.cm",
+            "coupon": "whois.nic.coupon",
+            "crown": "whois.nic.crown",
+            "crs": "whois.nic.crs",
+            "deal": "whois.nic.deal",
+            "dealer": "whois.nic.dealer",
+            "dell": "whois.nic.dell",
+            "dhl": "whois.nic.dhl",
+            "discover": "whois.nic.discover",
+            "dnp": "whois.nic.dnp",
+            "duns": "whois.nic.duns",
+            "dupont": "whois.nic.dupont",
+            "earth": "whois.nic.earth",
+            "energy": "whois.nic.energy",
+            "epost": "whois.nic.epost",
+            "everbank": "whois.nic.everbank",
+            "farmers": "whois.nic.farmers",
+            "fast": "whois.nic.fast",
+            "ferrero": "whois.nic.ferrero",
+            "fire": "whois.nic.fire",
+            "fj": "whois.usp.ac.fj",
+            "flickr": "whois.nic.flickr",
+            "flir": "whois.nic.flir",
+            "food": "whois.nic.food",
+            "ford": "whois.nic.ford",
+            "fox": "whois.nic.fox",
+            "free": "whois.nic.free",
+            "frontier": "whois.nic.frontier",
+            "ftr": "whois.nic.ftr",
+            "ga": "whois.my.ga",
+            "gap": "whois.nic.gap",
+            "gh": "whois.nic.gh",
+            "gmo": "whois.nic.gmo",
+            "got": "whois.nic.got",
+            "grainger": "whois.nic.grainger",
+            "grocery": "whois.nic.grocery",
+            "guardian": "whois.nic.guardian",
+            "gucci": "whois.nic.gucci",
+            "hair": "whois.nic.hair",
+            "hbo": "whois.nic.hbo",
+            "health": "whois.nic.health",
+            "homegoods": "whois.nic.homegoods",
+            "homesense": "whois.nic.homesense",
+            "honeywell": "whois.nic.honeywell",
+            "hoteles": "whois.nic.hoteles",
+            "hotels": "whois.nic.hotels",
+            "hotmail": "whois.nic.hotmail",
+            "hyatt": "whois.nic.hyatt",
+            "hsbc": "whois.nic.hsbc",
+            "hot": "whois.nic.hot",
+            "ieee": "whois.nic.ieee",
+            "imdb": "whois.nic.imdb",
+            "int": "whois.iana.org",
+            "intel": "whois.nic.intel",
+            "intuit": "whois.nic.intuit",
+            "ipirange": "whois.nic.ipiranga",
+            "itau": "whois.nic.itau",
+            "iwc": "whois.nic.iwc",
+            "jetzt": "whois.nic.jetzt",
+            "jlc": "whois.nic.jlc",
+            "jmp": "whois.nic.jmp",
+            "jnj": "whois.nic.jnj",
+            "jot": "whois.nic.jot",
+            "joy": "whois.nic.joy",
+            "jpmorgan": "whois.nic.jpmorgan",
+            "jprs": "whois.nic.jprs",
+            "kinder": "whois.nic.kinder",
+            "kindle": "whois.nic.kindle",
+            "kpmg": "whois.nic.kpmg",
+            "kpn": "whois.nic.kpn",
+            "kred": "whois.nic.kred",
+            "kw": "whois.nic.kw",
+            "lanxess": "whois.nic.lanxess",
+            "lc": "whois2.afilias-grs.net",
+            "lifeinsurance": "whois.nic.lifeinsurance",
+            "like": "whois.nic.like",
+            "lilly": "whois.nic.lilly",
+            "lincoln": "whois.nic.lincoln",
+            "living": "whois.nic.living",
+            "lk": "whois.nic.lk",
+            "loft": "whois.nic.loft",
+            "microsoft": "whois.nic.microsoft",
+            "nagoya": "whois.nic.nagoya",
+            "nyc": "whois.nic.nyc",
+            "ps": "whois.pnina.ps",
+            "ren": "whois.nic.ren",
+            "rw": "whois.ricta.org.rw",
+            "shaw": "whois.afilias-srs.net",
+            "shop": "whois.nic.shop",
+            "sl": "whois.nic.sl",
+            "stream": "whois.nic.stream",
+            "tokyo": "whois.nic.tokyo",
+            "tvs": "whois.nic.tvs",
+            "uno": "whois.nic.uno",
+            "za": "whois.registry.net.za",
+            "xn--e1a4c": "whois.eu",
+            "xn--ses554g": "whois.registry.knet.cn",
+        }
+
+        whois_record = Lookup().whois(
+            PyFunceble.CONFIGURATION["iana_whois_server"], "hello." + extension
+        )
+
+        if whois_record:
+            regex_referer = r"(refer:)\s+(.*)"
+
+            matched = Regex(
+                whois_record, regex_referer, return_data=True, rematch=True
+            ).match()
+
+            if matched:
+                return matched[1]
+
+        if extension in manual_server:
+            return manual_server[extension]
+
+        return None
+
+    def _extensions(self, line):
+        """
+        Extract the extention from the given line.
+        Plus get its referer.
+
+        Argument:
+            - line: str
+                The line from self.iana_url.
+        """
+
+        regex_valid_extension = r"(/domains/root/db/)(.*)(\.html)"
+
+        if "/domains/root/db/" in line:
+            matched = Regex(
+                line, regex_valid_extension, return_data=True, rematch=True
+            ).match()[1]
+
+            if matched:
+                self.iana_db.update({matched: self._referer(matched)})
+
+    def update(self):
+        """
+        Update the content of the `iana-domains-db` file.
+        """
+
+        list(map(self._extensions, self._data().split("\n")))
+        Dict(self.iana_db).to_json(self.destination)
+
+        if not PyFunceble.CONFIGURATION["quiet"]:
+            print(PyFunceble.CONFIGURATION["done"])
