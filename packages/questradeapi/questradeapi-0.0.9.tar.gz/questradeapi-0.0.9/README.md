571f25723716504dc6a864ba79126245450671cb
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-05 16:55:26 +0000

    Add package questradeapi/0.0.9/questradeapi-0.0.9.tar.gz

---
+++ packages/questradeapi/questradeapi-0.0.9.tar.gz/questradeapi-0.0.9/README.md
@@ -0,0 +1,17 @@
+# Questrade API Wrapper
+This package is a Python wrapper for the [Questrade API][api].
+
+## Installation
+To install QuestradeAPI, you can use [pipenv][pipenv] (or pip, of course):
+```
+pip install questradeapi
+```
+
+## Documentation
+All the documentation is available at https://questradeapi.readthedocs.io/en/latest/index.html. :book::smile:
+
+## Issue Reporting
+I you find a bug, have a feature request, or have design suggestions, please do not hesitate to report it in the issues section of this repository. For any security related issues, please do not report them publicly on the public GitHub issue tracker but contact me direcly by email.
+
+## Author
+[Antoine Viscardi](https://antoineviscardi.github.io)
---
+++ packages/questradeapi/questradeapi-0.0.9.tar.gz/questradeapi-0.0.9/setup.py
@@ -0,0 +1,26 @@
+import setuptools
+
+with open('README.md', 'r') as fh:
+    long_description = fh.read()
+
+setuptools.setup(
+    name='questradeapi',
+    version='0.0.9',
+    author='Antoine Viscardi',
+    author_email='antoine.viscardi@gmail.com',
+    description='Python wrapper for the Questrade API',
+    long_description=long_description,
+    long_description_content_type='text/markdown',
+    url='https://github.com/antoineviscardi/questradeapi',
+    license='MIT',
+    packages=setuptools.find_packages(),
+    classifiers=(
+        'Programming Language :: Python :: 3',
+        'License :: OSI Approved :: MIT License',
+        'Operating System :: OS Independent',    
+    ),
+    install_requires=(
+        'requests',
+        'tzlocal',
+    ),
+)
