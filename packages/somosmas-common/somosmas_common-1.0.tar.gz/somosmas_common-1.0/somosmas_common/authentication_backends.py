efe1fc89d186e11cac66caecae4f2c4cea4d2582
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-05 16:57:27 +0000

    Add package somosmas-common/1.0/somosmas_common-1.0.tar.gz

---
+++ packages/somosmas-common/somosmas_common-1.0.tar.gz/somosmas_common-1.0/somosmas_common/authentication_backends.py
@@ -0,0 +1,45 @@
+import requests
+from django.config import settings
+from django.contrib.auth.models import User
+
+
+class OAuthBackend:
+    def authenticate(self, request, token=None):
+        response = requests.post(
+            settings.OAUTH_INTROSPECT_URL,
+            headers={
+                'Authorization': 'Bearer {}'.format(
+                    settings.OAUTH_INTROSPECT_TOKEN
+                )
+            },
+            data={
+                'token': token
+            }
+        ).json()
+
+        if response['active']:
+            user_data = requests.post(
+                settings.OAUTH_BASE_URL + '/graphql/',
+                data="""
+                    query {
+                        me {
+                          id
+                          username
+                          firstName
+                          lastName
+                          email                        
+                        }
+                    }
+                """,
+                headers={
+                    'Authorization: Bearer {}'.format(token)
+                }
+            ).json()['data']
+            return User(
+                username=user_data['username'],
+                first_name=user_data['firstName'],
+                last_name=user_data['lastName'],
+                email=user_data['email'],
+            )
+        else:
+            return None
