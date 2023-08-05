9dd16b72b04a8d0f43a05b17312ff50ee02fd437
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-05 15:30:41 +0000

    Add package instatools/0.1.0/instatools-0.1.0.tar.gz

---
+++ packages/instatools/instatools-0.1.0.tar.gz/instatools-0.1.0/instatools/cli.py
@@ -0,0 +1,16 @@
+# -*- coding: utf-8 -*-
+
+"""Console script for instatools."""
+import sys
+import click
+
+
+@click.command()
+def main():
+    """Console script for instatools."""
+    click.echo("")
+    return 0
+
+
+if __name__ == "__main__":
+    sys.exit(main())  # pragma: no cover
