123d8b3a46b2380942ff9e515d14aba0a2454b94
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-05 15:04:08 +0000

    Add package pypcapkit/0.9.9.post1/pypcapkit-0.9.9.post1.tar.gz

---
+++ packages/pypcapkit/pypcapkit-0.9.9.post1.tar.gz/pypcapkit-0.9.9.post1/pcapkit/protocols/internet/hopopt.py
@@ -0,0 +1,138 @@
+# -*- coding: utf-8 -*-
+"""IPv6 hop-by-hop options
+
+`pcapkit.protocols.internet.hopopt` contains `HOPOPT`
+only, which implements extractor for IPv6 Hop-by-Hop
+Options header (HOPOPT), whose structure is described
+as below.
+
++-+-+-+-+-+-+-+-+-+-+-+-+-+-+-+-+-+-+-+-+-+-+-+-+-+-+-+-+-+-+-+-+
+|  Next Header  |  Hdr Ext Len  |                               |
++-+-+-+-+-+-+-+-+-+-+-+-+-+-+-+-+                               +
+|                                                               |
+.                                                               .
+.                            Options                            .
+.                                                               .
+|                                                               |
++-+-+-+-+-+-+-+-+-+-+-+-+-+-+-+-+-+-+-+-+-+-+-+-+-+-+-+-+-+-+-+-+
+
+"""
+# TODO: Implements extractor of all hop-by-hop options.
+from pcapkit.corekit.infoclass import Info
+from pcapkit.protocols.protocol import Protocol
+
+
+__all__ = ['HOPOPT']
+
+
+class HOPOPT(Protocol):
+    """This class implements IPv6 Hop-by-Hop Options.
+
+    Properties:
+        * name -- str, name of corresponding procotol
+        * info -- Info, info dict of current instance
+        * alias -- str, acronym of corresponding procotol
+        * layer -- str, `Internet`
+        * length -- int, header length of corresponding protocol
+        * protocol -- str, name of next layer protocol
+        * protochain -- ProtoChain, protocol chain of current instance
+
+    Methods:
+        * read_hopopt -- read IPv6 Hop-by-Hop Options (HOPOPT)
+
+    Attributes:
+        * _file -- BytesIO, bytes to be extracted
+        * _info -- Info, info dict of current instance
+        * _protos -- ProtoChain, protocol chain of current instance
+
+    Utilities:
+        * _read_protos -- read next layer protocol type
+        * _read_fileng -- read file buffer
+        * _read_unpack -- read bytes and unpack to integers
+        * _read_binary -- read bytes and convert into binaries
+        * _read_packet -- read raw packet data
+        * _decode_next_layer -- decode next layer protocol type
+        * _import_next_layer -- import next layer protocol extractor
+
+    """
+    ##########################################################################
+    # Properties.
+    ##########################################################################
+
+    @property
+    def name(self):
+        """Name of current protocol."""
+        return 'IPv6 Hop-by-Hop Options'
+
+    @property
+    def length(self):
+        """Header length of current protocol."""
+        return self._info.length
+
+    @property
+    def payload(self):
+        """Payload of current instance."""
+        if self.extension:
+            raise UnsupportedCall(f"'{self.__class__.__name__}' object has no attribute 'payload'")
+        return self._next
+
+    @property
+    def protocol(self):
+        """Name of next layer protocol."""
+        return self._info.next
+
+    ##########################################################################
+    # Methods.
+    ##########################################################################
+
+    def read_hopopt(self, length, extension):
+        """Read IPv6 Hop-by-Hop Options.
+
+        Structure of HOPOPT header [RFC 8200]:
+            +-+-+-+-+-+-+-+-+-+-+-+-+-+-+-+-+-+-+-+-+-+-+-+-+-+-+-+-+-+-+-+-+
+            |  Next Header  |  Hdr Ext Len  |                               |
+            +-+-+-+-+-+-+-+-+-+-+-+-+-+-+-+-+                               +
+            |                                                               |
+            .                                                               .
+            .                            Options                            .
+            .                                                               .
+            |                                                               |
+            +-+-+-+-+-+-+-+-+-+-+-+-+-+-+-+-+-+-+-+-+-+-+-+-+-+-+-+-+-+-+-+-+
+
+            Octets      Bits        Name                    Discription
+              0           0     hopopt.next             Next Header
+              1           8     hopopt.length           Header Extensive Length
+              2          16     hopopt.options          Options
+
+        """
+        if length is None:
+            length = len(self)
+
+        _next = self._read_protos(1)
+        _hlen = self._read_unpack(1)
+        _opts = self._read_fileng(_hlen - 1)
+
+        hopopt = dict(
+            next = _next,
+            length = _hlen + 1,
+            options = _opts,
+        )
+
+        length -= hopopt['length']
+        hopopt['packet'] = self._read_packet(header=hopopt['length'], payload=length)
+
+        if extension:
+            self._protos = None
+            return hopopt
+        return self._decode_next_layer(hopopt, _next, length)
+
+    ##########################################################################
+    # Data models.
+    ##########################################################################
+
+    def __init__(self, _file, length=None, *, extension=False, **kwargs):
+        self._file = _file
+        self._info = Info(self.read_hopopt(length, extension))
+
+    def __length_hint__(self):
+        return 2
---
+++ packages/pypcapkit/pypcapkit-0.9.9.post1.tar.gz/pypcapkit-0.9.9.post1/pcapkit/protocols/internet/internet.py
@@ -0,0 +1,185 @@
+# -*- coding: utf-8 -*-
+"""root internet layer protocol
+
+`pcapkit.protocols.internet.internet` contains both
+`ETHERTYPE` and `Internet`. The former is a dictionary
+of ethertype IEEE 802 numbers, registered in IANA. And the
+latter is a base class for internet layer protocols, eg.
+AH, IP, IPsec, IPv4, IPv6, IPX, and etc.
+
+"""
+from pcapkit.corekit.protochain import ProtoChain
+from pcapkit.protocols.protocol import Protocol
+from pcapkit.protocols.transport.transport import TP_PROTO
+from pcapkit.utilities.decorators import beholder
+
+
+__all__ = ['Internet', 'ETHERTYPE']
+
+
+# Ethertype IEEE 802 Numbers
+ETHERTYPE = {
+    # Link Layer
+    0x0806 : 'ARP',     # Address Resolution Protocol
+    0x8035 : 'RARP',    # Reverse Address Resolution Protocol
+    0x8100 : 'VLAN',    # 802.1Q Customer VLAN Tag Type
+
+    # Internet Layer
+    0x0800 : 'IPv4',    # Internet Protocol version 4
+    0x8137 : 'IPX',     # Internetwork Packet Exchange
+    0x86dd : 'IPv6',    # Internet Protocol version 6
+}
+
+
+class Internet(Protocol):
+    """Abstract base class for internet layer protocol family.
+
+    Properties:
+        * name -- str, name of corresponding procotol
+        * info -- Info, info dict of current instance
+        * alias -- str, acronym of corresponding procotol
+        * layer -- str, `Internet`
+        * length -- int, header length of corresponding protocol
+        * protocol -- str, name of next layer protocol
+        * protochain -- ProtoChain, protocol chain of current instance
+
+    Attributes:
+        * _file -- BytesIO, bytes to be extracted
+        * _info -- Info, info dict of current instance
+        * _protos -- ProtoChain, protocol chain of current instance
+
+    Utilities:
+        * _read_protos -- read next layer protocol type
+        * _read_fileng -- read file buffer
+        * _read_unpack -- read bytes and unpack to integers
+        * _read_binary -- read bytes and convert into binaries
+        * _read_packet -- read raw packet data
+        * _decode_next_layer -- decode next layer protocol type
+        * _import_next_layer -- import next layer protocol extractor
+
+    """
+    __layer__ = 'Internet'
+
+    ##########################################################################
+    # Properties.
+    ##########################################################################
+
+    # protocol layer
+    @property
+    def layer(self):
+        """Protocol layer."""
+        return self.__layer__
+
+    ##########################################################################
+    # Utilities.
+    ##########################################################################
+
+    def _read_protos(self, size):
+        """Read next layer protocol type.
+
+        Positional arguments:
+            * size  -- int, buffer size
+
+        Returns:
+            * str -- next layer's protocol name
+
+        """
+        _byte = self._read_unpack(size)
+        _prot = TP_PROTO.get(_byte)
+        return _prot
+
+    def _decode_next_layer(self, dict_, proto=None, length=None, *, version=4):
+        """Decode next layer extractor.
+
+        Positional arguments:
+            * dict_ -- dict, info buffer
+            * proto -- str, next layer protocol name
+            * length -- int, valid (not padding) length
+
+        Keyword Arguments:
+            * version -- int, IP version (4 in default)
+                            <keyword> 4 / 6
+
+        Returns:
+            * dict -- current protocol with next layer extracted
+
+        """
+        if self._onerror:
+            flag, next_ = beholder(self._import_next_layer)(self, proto, length, version=version)
+        else:
+            flag, next_ = self._import_next_layer(proto, length, version=version)
+        info, chain, alias = next_.info, next_.protochain, next_.alias
+
+        # make next layer protocol name
+        if flag:
+            if proto is None and chain:
+                layer = chain.alias[0].lower()
+                proto, chain = chain.tuple[0], None
+            else:
+                layer = str(alias or proto or 'Raw').lower()
+        else:
+            layer, proto = 'raw', 'Raw'
+
+        # write info and protocol chain into dict
+        self._next = next_
+        self._protos = ProtoChain(proto, chain, alias)
+        dict_[layer] = info
+        return dict_
+
+    def _import_next_layer(self, proto, length=None, *, version=4, extension=False):
+        """Import next layer extractor.
+
+        Positional arguments:
+            * proto -- str, next layer protocol name
+            * length -- int, valid (not padding) length
+
+        Keyword Arguments:
+            * version -- int, IP version (4 in default)
+                            <keyword> 4 / 6
+            * extension -- bool, if is extension header (False in default)
+                            <keyword> True / False
+
+        Returns:
+            * bool -- flag if extraction of next layer succeeded
+            * Info -- info of next layer
+            * ProtoChain -- protocol chain of next layer
+            * str -- alias of next layer
+
+        Protocols:
+            * IPv4 -- internet layer
+            * IPv6 -- internet layer
+            * AH -- internet layer
+            * TCP -- transport layer
+            * UDP -- transport layer
+
+        """
+        if self._sigterm:
+            from pcapkit.protocols.raw import Raw as Protocol
+        elif proto == 'AH':
+            from pcapkit.protocols.internet.ah import AH as Protocol
+        elif proto == 'HIP':
+            from pcapkit.protocols.internet.hip import HIP as Protocol
+        elif proto == 'HOPOPT':
+            from pcapkit.protocols.internet.hopopt import HOPOPT as Protocol
+        elif proto == 'IPv6-Frag':
+            from pcapkit.protocols.internet.ipv6_frag import IPv6_Frag as Protocol
+        elif proto == 'IPv6-Opts':
+            from pcapkit.protocols.internet.ipv6_opts import IPv6_Opts as Protocol
+        elif proto == 'IPv6-Route':
+            from pcapkit.protocols.internet.ipv6_route import IPv6_Route as Protocol
+        elif proto == 'MH':
+            from pcapkit.protocols.internet.mh import MH as Protocol
+        elif proto == 'IPv4':
+            from pcapkit.protocols.internet.ipv4 import IPv4 as Protocol
+        elif proto == 'IPv6':
+            from pcapkit.protocols.internet.ipv6 import IPv6 as Protocol
+        elif proto == 'TCP':
+            from pcapkit.protocols.transport.tcp import TCP as Protocol
+        elif proto == 'UDP':
+            from pcapkit.protocols.transport.udp import UDP as Protocol
+        else:
+            from pcapkit.protocols.raw import Raw as Protocol
+        next_ = Protocol(self._file, length,
+                            version=version, extension=extension,
+                            error=self._onerror, layer=self._exlayer, protocol=self._exproto)
+        return True, next_
