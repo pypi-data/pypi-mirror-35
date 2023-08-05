9de5ec529d7be0ce4c72ecb51870536b00991f3a
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-05 15:25:06 +0000

    Add package sphinx-paragraph-extractor/1.0.1/sphinx-paragraph-extractor-1.0.1.tar.gz

---
+++ packages/sphinx-paragraph-extractor/sphinx-paragraph-extractor-1.0.1.tar.gz/sphinx-paragraph-extractor-1.0.1/README.md
@@ -0,0 +1,10 @@
+# paragraph-extractor
+
+A builder for Sphinx to extract paragraphs from docs.
+
+
+## Usage
+
+```
+sphinx-build -b paragraph-extractor ${SOURCE_DIR} ${OUTPUT_DIR}
+```
---
+++ packages/sphinx-paragraph-extractor/sphinx-paragraph-extractor-1.0.1.tar.gz/sphinx-paragraph-extractor-1.0.1/setup.py
@@ -0,0 +1,32 @@
+#!/usr/bin/env python3
+# coding: utf-8
+
+from setuptools import find_packages
+from setuptools import setup
+
+setup(
+    name='sphinx-paragraph-extractor',
+    version='1.0.1',
+    description='',
+    url='https://github.com/mitsuse/sphinx-paragraph-extractor',
+    author='Tomoya Kose',
+    author_email='tomoya@mitsuse.jp',
+    install_requires=[
+        'sphinx>=1.7.4<2.0.0',
+    ],
+    license='MIT',
+    classifiers=[
+        'License :: OSI Approved :: MIT License',
+        'Programming Language :: Python :: 3.6',
+        'Programming Language :: Python :: 3.7',
+    ],
+    keywords=[],
+    packages=find_packages(exclude=[
+        'tests',
+    ]),
+    entry_points={
+        'sphinx.builders': [
+            'paragraph-extractor = sphinx_paragraph_extractor',
+        ],
+    },
+)
