16fd848cf806d41973bbac1d0f96fc1388373e9d
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-05 22:09:09 +0000

    Add package soothsayer/2018.8/soothsayer-2018.8.tar.gz

---
+++ packages/soothsayer/soothsayer-2018.8.tar.gz/soothsayer-2018.8/setup.py
@@ -0,0 +1,35 @@
+import re
+from setuptools import setup
+from ast import literal_eval
+
+# Version
+version = None
+with open("./soothsayer/__init__.py", "r") as f:
+    for line in f.readlines():
+        line = line.strip()
+        if line.startswith("__version__"):
+            version = literal_eval(line.split("=")[-1].strip())
+
+setup(name='soothsayer',
+      version=version,
+      description='High-level API for (bio-)informatics',
+      url='https://github.com/jolespin/soothsayer',
+      author='Josh L. Espinoza',
+      author_email='jespinoz@jcvi.org',
+      license='MIT',
+      packages=["soothsayer"],
+      install_requires=[
+    # "python >= 3.6",
+	"matplotlib >= 2.2.2",
+	"scipy >= 1.0",
+	"scikit-learn >= 0.19.1",
+    "numpy >= 1.9.2",# , < 1.14.0",
+    'pandas >= 0.19.2, < 0.23.0',
+	'networkx >= 2.0',
+	'ete3 >= 3.0',
+	'scikit-bio >= 0.5',
+    "biopython >= 1.5",
+    "xarray >= 0.10.3",
+    "tqdm >=4.19",
+      ],
+      zip_safe=True)
---
+++ packages/soothsayer/soothsayer-2018.8.tar.gz/soothsayer-2018.8/soothsayer/__init__.py
@@ -0,0 +1,21 @@
+# ==============
+# ∆ Soothsayer ∆
+# ==============
+# High-level API for (bio-)informatics
+# ------------------------------------
+# GitHub: https://github.com/jolespin/soothsayer
+# PyPI
+#
+#
+# =======
+# Contact
+# =======
+# Producer: Josh L. Espinoza
+# Contac: jespinoz@jcvi.org, jol.espinoz@gmail.com
+# Google Scholar: https://scholar.google.com/citations?user=r9y1tTQAAAAJ&hl=en
+#
+#
+# =======
+# Version
+# =======
+__version__= "2018.08"
