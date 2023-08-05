f0b53ccd051772f8c309322182523fadba044b8f
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-05 15:30:25 +0000

    Add package django-staticinline/1.3.1/django_staticinline-1.3.1-py2.py3-none-any.whl

---
+++ packages/django-staticinline/django_staticinline-1.3.1-py2.py3-none-any.whl/staticinline/apps.py
@@ -0,0 +1,98 @@
+import mimetypes
+import base64
+import hashlib
+
+from django.apps import AppConfig
+from django.utils.safestring import mark_safe
+
+
+class StaticInlineAppConfig(AppConfig):
+    name = 'staticinline'
+    verbose_name = 'Static Inline Files'
+    encoder_response_format = 'utf-8'
+
+    # Default cache timeout if not specified individually in the template tag
+    # with the ``cache_timeout`` argument.
+    cache_timeout = 60 * 60
+
+    def build_cache_key(self, path, encode=None):
+        s = '{0}{1}'.format(path, encode or '')
+        return 'staticinline-{0}'.format(hashlib.sha1(s.encode()).hexdigest())
+
+    def data_response(self, data):
+        """
+        This method transforms the data right before its written in the
+        template. This is applied to all files, while 'encoder' are set
+        individually per file.
+
+        :param str data: The [optionally encoded] file content.
+        :return: The [optionally encoded] file content.
+        :rtype: str:
+        """
+        return mark_safe(data)
+
+    def get_encoder(self):
+        """
+        List of all registered encoders which can be used in the staticinline
+        template tag. Example::
+
+            {% staticinline "myfile.gif" encode="base64" %}
+
+        This can be manually extended by providing a custom AppConfig.
+        """
+        return {
+            'base64': self.encode_base64,
+            'data': self.encode_data_uri,
+            'sri': self.encode_sri,
+        }
+
+    def encode_base64(self, data, path):
+        """
+        Encodes with standard Base64.
+
+        :param bytes data: Input data to encode.
+        :return: Base64 encoded input string.
+        :rtype: str
+        :raises Exception: if the file is not suitable to be Base64 encoded.
+        """
+        return base64.b64encode(data).decode(self.encoder_response_format)
+
+    def encode_data_uri(self, data, path):
+        """
+        Convert to the data URI scheme.
+
+        :param bytes data: Input data to encode.
+        :return: data URI string.
+        :rtype: str
+        """
+        mimetype = mimetypes.guess_type(path)[0]
+        if mimetype is None:
+            prefix = 'data:;base64,'
+        else:
+            prefix = 'data:{0};base64,'.format(mimetype)
+        return prefix + self.encode_base64(data, data)
+
+    def encode_sri(self, data, path):
+        """
+        Adds Subresource Integrity encoder to staticinline. Read more:
+
+        https://developer.mozilla.org/en-US/docs/Web/Security/Subresource_Integrity
+
+        Example Usage::
+
+          {% load staticfiles %}
+          {% load staticinline %}
+
+          <link
+            rel="stylesheet"
+            href="{% static "base.css" %}"
+            integrity="{% staticinline "base.css" encode="sri" %}"
+            crossorigin="anonymous"/>
+
+        :param bytes data: File content to build the SRI hash of.
+        :return: sha256 hash
+        :rtype: str
+        """
+        hash = hashlib.sha256(data).digest()
+        hash_base64 = base64.b64encode(hash).decode()
+        return 'sha256-{}'.format(hash_base64)
---
+++ packages/django-staticinline/django_staticinline-1.3.1-py2.py3-none-any.whl/staticinline/tests/test_templatetag.py
@@ -0,0 +1,207 @@
+from django.core.exceptions import ImproperlyConfigured
+from django.template import Template
+from django.template.context import Context
+from django.test import override_settings
+from django.test.testcases import TestCase
+from django.core.cache import cache
+
+from staticinline.templatetags import staticinline
+
+try:
+    from unittest import mock
+except ImportError:
+    import mock
+
+
+def render(source, **context):
+    return Template(source).render(Context(context))
+
+
+class StaticInlineTests(TestCase):
+    template = (
+        '{% load staticinline %}' '<script>{% staticinline "somefile" %}</script>'
+    )
+
+    def test_found(self):
+        """
+        Static file is are correctly included inline the template.
+        """
+        with mock.patch.object(staticinline, 'read_static_file') as reader:
+            reader.return_value = 'alert("hi")'
+            rendered = render(self.template)
+            self.assertEqual(rendered, '<script>alert("hi")</script>')
+
+    def test_file_missing(self):
+        """
+        An empty string is returned if the inlined file is missing and
+        DEBUG is off.
+        """
+        with mock.patch.object(staticinline, 'read_static_file') as reader:
+            reader.side_effect = ValueError('Not found')
+            rendered = render(self.template)
+            self.assertEqual(rendered, '<script></script>')
+
+    @override_settings(DEBUG=True)
+    def test_file_missing_debug(self):
+        """
+        An error raised from read_static_file if the inlined file is
+        missing and DEBUG is on.
+        """
+        with mock.patch.object(staticinline, 'read_static_file') as reader:
+            reader.side_effect = ValueError('Not found')
+            self.assertRaises(ValueError, render, self.template)
+
+
+class CachedStaticInlineTests(TestCase):
+    template_cached = (
+        '{% load staticinline %}'
+        '<script>{% staticinline "somefile" cache=True cache_timeout=60 %}</script>'
+    )
+    template_cached_encoder = (
+        '{% load staticinline %}'
+        '{% staticinline "some-other-file" encode="base64" cache=True cache_timeout=60 %}'
+    )
+
+    def tearDown(self):
+        cache.clear()
+
+    def test_cache(self):
+        """
+        Static file is are correctly included inline the template.
+        """
+        # Call once to set in cache
+        with mock.patch.object(staticinline, 'read_static_file') as reader:
+            reader.return_value = 'alert("hi")'
+            rendered = render(self.template_cached)
+            self.assertEqual(rendered, '<script>alert("hi")</script>')
+
+        # Call again to test cache retrieval
+        with mock.patch.object(staticinline, 'read_static_file') as reader:
+            reader.return_value = 'alert("hi")'
+            rendered = render(self.template_cached)
+            self.assertEqual(rendered, '<script>alert("hi")</script>')
+
+    def test_cache_encoder(self):
+        """
+        Static file is are correctly included inline the template.
+        """
+        # Call once to set in cache
+        with mock.patch.object(staticinline, 'read_static_file') as reader:
+            reader.return_value = b'it is bytestring data'
+            rendered = render(self.template_cached_encoder)
+            self.assertEqual(rendered, 'aXQgaXMgYnl0ZXN0cmluZyBkYXRh')
+
+        # Call again to test cache retrieval
+        with mock.patch.object(staticinline, 'read_static_file') as reader:
+            reader.return_value = b'it is bytestring data'
+            rendered = render(self.template_cached_encoder)
+            self.assertEqual(rendered, 'aXQgaXMgYnl0ZXN0cmluZyBkYXRh')
+
+
+class EncoderTests(TestCase):
+    def test_unregistered(self):
+        """
+        Using an unregistered encoder will raise ImproperlyConfigured.
+        """
+        template = (
+            '{% load staticinline %}'
+            '{% staticinline "somefile" encode="doesnotexist" %}'
+        )
+        with mock.patch.object(staticinline, 'read_static_file') as reader:
+            reader.return_value = 'alert("hi")'
+            self.assertRaises(ImproperlyConfigured, render, template)
+
+    def test_error(self):
+        """
+        If the encoder raises any exception, return an empty string when
+        DEBUG is off.
+        """
+        with mock.patch.object(staticinline, 'read_static_file') as reader:
+            reader.return_value = b'it is bytestring data'
+            rendered = render(
+                '{% load staticinline %}'
+                'My Key: {% staticinline "somefile" encode="broken" %}'
+            )
+            self.assertEqual(rendered, 'My Key: ')
+
+    @override_settings(DEBUG=True)
+    def test_error_debug(self):
+        """
+        If the encoder raises any exception, the exception is raised
+        when DEBUG is on.
+        """
+        with mock.patch.object(staticinline, 'read_static_file') as reader:
+            reader.return_value = b'it is bytestring data'
+            template = (
+                '{% load staticinline %}'
+                'My Key: {% staticinline "somefile" encode="broken" %}'
+            )
+            self.assertRaises(ZeroDivisionError, render, template)
+
+    def test_custom(self):
+        """
+        Custom encoders can be used in a separate appconfig.
+        In testapp/apps.py, the 'uppercase' encoder is added.
+        """
+        with mock.patch.object(staticinline, 'read_static_file') as reader:
+            reader.return_value = b'shouting'
+            rendered = render(
+                '{% load staticinline %}'
+                '{% staticinline "somefile" encode="uppercase" %}'
+            )
+            self.assertEqual(rendered, 'SHOUTING')
+
+    def test_base64(self):
+        """
+        The 'base64' encoder is shipped with this application.
+        """
+        with mock.patch.object(staticinline, 'read_static_file') as reader:
+            reader.return_value = b'it is bytestring data'
+            rendered = render(
+                '{% load staticinline %}'
+                'My Key: {% staticinline "somefile" encode="base64" %}'
+            )
+            self.assertEqual(rendered, 'My Key: aXQgaXMgYnl0ZXN0cmluZyBkYXRh')
+
+    @override_settings(DEBUG=True)
+    def test_data(self):
+        """
+        The 'data' URI encoder is shipped with this application.
+        """
+        with mock.patch.object(staticinline, 'read_static_file') as reader:
+            reader.return_value = b'png content'
+            rendered = render(
+                '{% load staticinline %}'
+                '<img src="{% staticinline "a.png" encode="data" %}">'
+            )
+            self.assertEqual(
+                rendered, '<img src="data:image/png;base64,cG5nIGNvbnRlbnQ=">'
+            )
+
+    @override_settings(DEBUG=True)
+    def test_data_unknown_mimetype(self):
+        """
+        The data URI still works if no mimetype can be guessed.
+        """
+        with mock.patch.object(staticinline, 'read_static_file') as reader:
+            reader.return_value = b'png content'
+            rendered = render(
+                '{% load staticinline %}'
+                '<img src="{% staticinline "somefile" encode="data" %}">'
+            )
+            self.assertEqual(rendered, '<img src="data:;base64,cG5nIGNvbnRlbnQ=">')
+
+    def test_sri(self):
+        """
+        The 'base64' encoder is shipped with this application.
+        """
+        with mock.patch.object(staticinline, 'read_static_file') as reader:
+            reader.return_value = b'alert("hi")'
+            rendered = render(
+                '{% load staticinline %}'
+                'integrity="{% staticinline "somefile" encode="sri" %}"'
+            )
+            self.assertEqual(
+                rendered,
+                'integrity="sha256-fdu2bQSeIdU5fvNNkRCjiwUEOOb+NLZDyGNVShZ1vCM="',
+            )
