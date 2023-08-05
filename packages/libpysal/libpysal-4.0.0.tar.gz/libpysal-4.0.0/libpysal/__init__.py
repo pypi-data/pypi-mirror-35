0bc2f4973a3d068e36cb5f873bd85ba3847c771d
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-05 21:11:35 +0000

    Add package libpysal/4.0.0/libpysal-4.0.0.tar.gz

---
+++ packages/libpysal/libpysal-4.0.0.tar.gz/libpysal-4.0.0/libpysal/__init__.py
@@ -0,0 +1,35 @@
+__version__ = "4.0.0"
+
+# __version__ has to be define in the first line
+
+"""
+Python Spatial Analysis Library
+===============================
+
+
+Documentation
+-------------
+PySAL documentation is available in two forms: python docstrings and an html \
+        webpage at http://pysal.org/
+
+Available sub-packages
+----------------------
+
+cg
+    Basic data structures and tools for Computational Geometry
+examples
+    Example data sets for testing and documentation
+io
+    Basic functions used by several sub-packages
+weights
+    Tools for creating and manipulating weights
+"""
+from . import cg
+from . import io
+from . import weights
+from . import examples
+
+# Assign pysal.open to dispatcher
+
+#from .version import version as __version__
+
