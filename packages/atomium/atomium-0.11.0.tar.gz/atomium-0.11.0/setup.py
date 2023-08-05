d3b4a360478e5ace4fec342bf2ff520a0c680a26
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-05 21:09:17 +0000

    Add package atomium/0.11.0/atomium-0.11.0.tar.gz

---
+++ packages/atomium/atomium-0.11.0.tar.gz/atomium-0.11.0/setup.py
@@ -0,0 +1,24 @@
+from setuptools import setup
+
+setup(
+ name="atomium",
+ version="0.11.0",
+ description="A molecular modeller and file parser.",
+ url="https://atomium.samireland.com",
+ author="Sam Ireland",
+ author_email="mail@samireland.com",
+ license="MIT",
+ classifiers=[
+  "Development Status :: 4 - Beta",
+  "Intended Audience :: Science/Research",
+  "License :: OSI Approved :: MIT License",
+  "Topic :: Scientific/Engineering :: Chemistry",
+  "Programming Language :: Python :: 3",
+  "Programming Language :: Python :: 3.5",
+  "Programming Language :: Python :: 3.6",
+  "Programming Language :: Python :: 3.7",
+ ],
+ keywords="chemistry bioinformatics proteins biochemistry molecules PDB XYZ",
+ packages=["atomium", "atomium.files", "atomium.models"],
+ install_requires=["numpy", "requests", "rmsd", "paramiko"]
+)
