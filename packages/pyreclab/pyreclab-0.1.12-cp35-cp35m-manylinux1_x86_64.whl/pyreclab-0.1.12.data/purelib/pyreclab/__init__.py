948ec5c02ec2ebb556f03aa0691609e7cdd2cf31
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-05 22:10:27 +0000

    Add package pyreclab/0.1.12/pyreclab-0.1.12-cp35-cp35m-manylinux1_x86_64.whl

---
+++ packages/pyreclab/pyreclab-0.1.12-cp35-cp35m-manylinux1_x86_64.whl/pyreclab-0.1.12.data/purelib/pyreclab/__init__.py
@@ -0,0 +1,12 @@
+from pyreclab.libpyreclab import MostPopular, UserAvg, ItemAvg, SlopeOne, UserKnn, ItemKnn, SVD, IFAls, IFAlsConjugateGradient
+
+__all__ = [ 'MostPopular',
+            'UserAvg',
+            'ItemAvg',
+            'SlopeOne',
+            'UserKnn',
+            'ItemKnn',
+            'SVD',
+            'IFAls',
+            'IFAlsConjugateGradient' ]
+
