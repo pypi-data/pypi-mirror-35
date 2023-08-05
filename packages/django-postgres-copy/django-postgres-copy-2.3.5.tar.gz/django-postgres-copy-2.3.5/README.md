5b7bfaaca79f03280a992067281cdcb273e36239
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-05 15:24:38 +0000

    Add package django-postgres-copy/2.3.5/django-postgres-copy-2.3.5.tar.gz

---
+++ packages/django-postgres-copy/django-postgres-copy-2.3.5.tar.gz/django-postgres-copy-2.3.5/README.md
@@ -0,0 +1,14 @@
+# django-postgres-copy
+
+Quickly import and export delimited data with Django support for PostgreSQL's COPY command
+
+[![Build Status](https://travis-ci.org/california-civic-data-coalition/django-postgres-copy.svg?branch=master)](https://travis-ci.org/california-civic-data-coalition/django-postgres-copy)
+[![PyPI version](https://badge.fury.io/py/django-postgres-copy.svg)](http://badge.fury.io/py/django-postgres-copy)
+[![Coverage Status](https://coveralls.io/repos/california-civic-data-coalition/django-postgres-copy/badge.svg?branch=master)](https://coveralls.io/r/california-civic-data-coalition/django-postgres-copy?branch=master)
+[![Documentation Status](https://readthedocs.org/projects/django-postgres-copy/badge/?version=latest)](https://django-postgres-copy.californiacivicdata.org/)
+
+* Documentation: [django-postgres-copy.californiacivicdata.org](http://django-postgres-copy.californiacivicdata.org)
+* Issues: [github.com/california-civic-data-coalition/django-postgres-copy/issues](https://github.com/california-civic-data-coalition/django-postgres-copy/issues)
+* Packaging: [pypi.python.org/pypi/django-postgres-copy](https://pypi.python.org/pypi/django-postgres-copy)
+* Testing: [travis-ci.org/california-civic-data-coalition/django-postgres-copy](https://travis-ci.org/california-civic-data-coalition/django-postgres-copy)
+* Coverage: [coveralls.io/r/california-civic-data-coalition/django-postgres-copy](https://coveralls.io/r/california-civic-data-coalition/django-postgres-copy)
---
+++ packages/django-postgres-copy/django-postgres-copy-2.3.5.tar.gz/django-postgres-copy-2.3.5/setup.py
@@ -0,0 +1,96 @@
+import os
+from setuptools import setup
+from distutils.core import Command
+
+
+class TestCommand(Command):
+    user_options = []
+
+    def initialize_options(self):
+        pass
+
+    def finalize_options(self):
+        pass
+
+    def run(self):
+        import django
+        from django.core.management import call_command
+        from django.conf import settings
+        settings.configure(
+            DATABASES={
+                'default': {
+                    'NAME': 'test',
+                    'USER': 'postgres',
+                    'ENGINE': 'django.db.backends.postgresql_psycopg2'
+                },
+                'alternative': {
+                    'NAME': 'test_alternative',
+                    'USER': 'postgres',
+                    'ENGINE': 'django.db.backends.postgresql_psycopg2'
+                },
+                'sqlite': {
+                    'NAME': 'sqlite',
+                    'ENGINE': 'django.db.backends.sqlite3'
+                },
+                'secondary': {
+                    'NAME': 'test_secondary',
+                    'USER': 'postgres',
+                    'ENGINE': 'django.db.backends.postgresql_psycopg2'
+                }
+            },
+            INSTALLED_APPS=("tests",),
+            DATABASE_ROUTERS=['tests.router.CustomRouter'],
+            LOGGING = {
+                'version': 1,
+                'disable_existing_loggers': False,
+                'handlers': {
+                    'file': {
+                        'level': 'DEBUG',
+                        'class': 'logging.FileHandler',
+                        'filename': os.path.join(os.path.dirname(__file__), 'tests.log'),
+                    },
+                },
+                'formatters': {
+                    'verbose': {
+                        'format': '%(levelname)s|%(asctime)s|%(module)s|%(message)s',
+                        'datefmt': "%d/%b/%Y %H:%M:%S"
+                    }
+                },
+                'loggers': {
+                    'postgres_copy': {
+                        'handlers': ['file'],
+                        'level': 'DEBUG',
+                        'propagate': True,
+                    },
+                }
+            }
+        )
+        django.setup()
+        call_command('test', 'tests')
+        # call_command('test', 'tests.tests.PostgresCopyToTest.test_related_fields')
+
+
+setup(
+    name='django-postgres-copy',
+    version='2.3.5',
+    description="Quickly import and export delimited data with Django support for PostgreSQL's COPY command",
+    author='Ben Welsh',
+    author_email='ben.welsh@gmail.com',
+    url='http://django-postgres-copy.californiacivicdata.org/',
+    license="MIT",
+    packages=("postgres_copy",),
+    install_requires=("psycopg2>=2.7.3",),
+    cmdclass={'test': TestCommand},
+    classifiers=[
+        'Development Status :: 5 - Production/Stable',
+        'Programming Language :: Python',
+        'Programming Language :: Python :: 2.7',
+        'Programming Language :: Python :: 3.5',
+        'Programming Language :: Python :: 3.6',
+        'Programming Language :: Python :: 3.7',
+        'Framework :: Django',
+        'Framework :: Django :: 1.11',
+        'Framework :: Django :: 2.0',
+        'License :: OSI Approved :: MIT License',
+    ],
+)
