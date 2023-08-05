237e96f76bc14470caf28842eb27760f429657f4
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-05 15:29:39 +0000

    Add package easywsy/0.3.65/easywsy-0.3.65.tar.gz

---
+++ packages/easywsy/easywsy-0.3.65.tar.gz/easywsy-0.3.65/easywsy/__init__.py
@@ -0,0 +1,8 @@
+from . import api  # noqa
+from . import check  # noqa
+from . import error  # noqa
+from . import ws  # noqa
+
+from .ws.web_service import WebService  # noqa
+
+wsapi = api.decorators
