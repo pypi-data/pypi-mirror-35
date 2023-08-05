c1260502b887fe9ae73eca79d9973373ebc36094
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-05 16:57:17 +0000

    Add package dirstate/1.0/dirstate-1.0-py2-none-any.whl

---
+++ packages/dirstate/dirstate-1.0-py2-none-any.whl/dirstate/state.py
@@ -0,0 +1,49 @@
+import os
+import glob
+import ntpath
+import shutil
+from dirstate.utils import compress, new_state_id, parse_state_id, decompress
+from dirstate.constants import DIRECTORY
+
+
+def get_states(directory):
+    return filter(
+        lambda x: x['directory'] == directory,
+        [
+            dict(
+                parse_state_id(ntpath.basename(fname.split('.tar.gz')[0])),
+                **dict(file=fname)
+            )
+            for fname in glob.glob(os.path.join(DIRECTORY, '*.tar.gz'))
+        ]
+    )
+
+
+def save_state(directory):
+    state_id = new_state_id(directory)
+
+    compress(
+        os.path.join(DIRECTORY, state_id),
+        directory
+    )
+
+    return state_id
+
+
+def set_state(directory, timestamp):
+    state = None
+
+    states = filter(
+        lambda x: x['timestamp'] == timestamp,
+        get_states(directory)
+    )
+
+    if not states:
+        return False
+    else:
+        state = states[0]
+
+    shutil.rmtree(directory)
+    decompress(state['file'], directory)
+
+    return timestamp
