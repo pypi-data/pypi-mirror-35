61cb15164f63f55bfda1689199a58b444e8dff7d
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-05 15:31:11 +0000

    Add package luigi-monitor/1.1.4/luigi-monitor-1.1.4.tar.gz

---
+++ packages/luigi-monitor/luigi-monitor-1.1.4.tar.gz/luigi-monitor-1.1.4/setup.py
@@ -0,0 +1,29 @@
+try:
+    from setuptools import setup
+except ImportError:
+    from distutils.core import setup
+
+setup(
+    name="luigi-monitor",
+    version="1.1.4",
+    description="Send summary messages of your Luigi jobs to Slack.",
+    long_description=open("README.md").read(),
+    url="https://github.com/hudl/luigi-monitor",
+    author="Hudl",
+    author_email="alex.debrie@hudl.com",
+    license="MIT",
+    packages=['luigi_monitor'],
+    classifiers=[
+        "Development Status :: 3 - Alpha",
+        "Intended Audience :: Developers",
+        "License :: OSI Approved :: MIT License",
+        "Programming Language :: Python :: 2.7",
+        "Programming Language :: Python :: 3.6",
+        "Programming Language :: Python :: 3.7"
+    ],
+    keywords="luigi",
+    install_requires=["requests", "luigi"],
+    entry_points={
+        "console_scripts": ["luigi-monitor=luigi_monitor.luigi_monitor:run"]
+    }
+)
