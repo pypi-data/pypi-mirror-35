7e8310e55efd43d7fbdad693e1cdc9bb52833e6b
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-05 16:55:50 +0000

    Add package ta/0.1.11/ta-0.1.11.tar.gz

---
+++ packages/ta/ta-0.1.11.tar.gz/ta-0.1.11/ta/utils.py
@@ -0,0 +1,12 @@
+# -*- coding: utf-8 -*-
+import math
+import pandas as pd
+
+
+def dropna(df):
+    """Drop rows with "Nans" values
+    """
+    df = df[df < math.exp(709)] # big number
+    df = df[df != 0.0]
+    df = df.dropna()
+    return df
