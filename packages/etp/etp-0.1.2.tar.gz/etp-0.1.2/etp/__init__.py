6fd4dec96da41f0924ae0e7f91c6a30c034f3dcc
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-05 15:31:38 +0000

    Add package etp/0.1.2/etp-0.1.2.tar.gz

---
+++ packages/etp/etp-0.1.2.tar.gz/etp-0.1.2/etp/__init__.py
@@ -0,0 +1,1 @@
+from enhanced_threadpool_setup import EnhancedThreadpool
---
+++ packages/etp/etp-0.1.2.tar.gz/etp-0.1.2/setup.py
@@ -0,0 +1,22 @@
+import setuptools
+
+with open("README.markdown", "r") as fh:
+    long_description = fh.read()
+
+
+setuptools.setup(
+      name='etp',
+      version='0.1.2',
+      description='A ThreadPool extension',
+      long_description=long_description,
+      long_description_content_type="text/markdown",
+      url='https://github.com/dobbleg1000/enhanced_threadpool',
+      author='DobbleG1000',
+      packages=setuptools.find_packages(),
+      classifiers=[
+        "Programming Language :: Python :: 3",
+        "License :: OSI Approved :: GNU General Public License v3 (GPLv3)",
+        "Operating System :: OS Independent",
+        "Natural Language :: English",
+      ],
+      zip_safe=False)
