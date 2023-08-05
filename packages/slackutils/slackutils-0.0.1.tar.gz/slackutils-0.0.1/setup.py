c7b7cc5d7afd01c5ed5ba03d7c129d18326ae605
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-05 21:09:31 +0000

    Add package slackutils/0.0.1/slackutils-0.0.1.tar.gz

---
+++ packages/slackutils/slackutils-0.0.1.tar.gz/slackutils-0.0.1/setup.py
@@ -0,0 +1,22 @@
+
+import setuptools
+
+with open("README.md", "r") as fh:
+    long_description = fh.read()
+
+setuptools.setup(
+    name="slackutils",
+    version="0.0.1",
+    author="Mads Wilthil",
+    author_email="mads.wilthil@gmail.com",
+    description="A small library to simplify slack web API calls",
+    long_description=long_description,
+    long_description_content_type="text/markdown",
+    url="https://github.com/madsWilthil/slackutils",
+    packages=setuptools.find_packages(),
+    classifiers=[
+        "Programming Language :: Python :: 3",
+        "License :: OSI Approved :: MIT License",
+        "Operating System :: OS Independent"
+    ],
+)
