9e74cc30aff1f241b629876a3dba88a88bacacbd
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-05 22:11:25 +0000

    Add package psautohint/1.7.1b1/psautohint-1.7.1b1.zip

---
+++ packages/psautohint/psautohint-1.7.1b1.zip/psautohint-1.7.1b1/libpsautohint/src/stemreport.c
@@ -0,0 +1,48 @@
+/*
+ * Copyright 2014 Adobe Systems Incorporated (http://www.adobe.com/).
+ * All Rights Reserved.
+ *
+ * This software is licensed as OpenSource, under the Apache License, Version
+ * 2.0.
+ * This license is available at: http://opensource.org/licenses/Apache-2.0.
+ */
+
+#include "ac.h"
+
+void
+AddVStem(Fixed top, Fixed bottom, bool curved)
+{
+    if (curved && !gAllStems)
+        return;
+
+    if (gAddVStemCB != NULL) {
+        gAddVStemCB(top, bottom, gGlyphName);
+    }
+}
+
+void
+AddHStem(Fixed right, Fixed left, bool curved)
+{
+    if (curved && !gAllStems)
+        return;
+
+    if (gAddHStemCB != NULL) {
+        gAddHStemCB(right, left, gGlyphName);
+    }
+}
+
+void
+AddGlyphExtremes(Fixed bot, Fixed top)
+{
+    if (gAddGlyphExtremesCB != NULL) {
+        gAddGlyphExtremesCB(top, bot, gGlyphName);
+    }
+}
+
+void
+AddStemExtremes(Fixed bot, Fixed top)
+{
+    if (gAddStemExtremesCB != NULL) {
+        gAddStemExtremesCB(top, bot, gGlyphName);
+    }
+}
