3aa73f8e69e52ec97e63ef2bf5b891492e6641fe
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-05 15:29:24 +0000

    Add package ml8/0.0.4/ml8-0.0.4.tar.gz

---
+++ packages/ml8/ml8-0.0.4.tar.gz/ml8-0.0.4/ml8/__init__.py
@@ -0,0 +1,9 @@
+__version__ = '0.0.4'
+#从本文件夹里import models.py和exceptions.py
+#from . import models, exceptions
+
+from . import sklearn
+from . import datasets
+from . import classifier
+from .ml8 import *
+
