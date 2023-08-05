e189dc80ea88cfbc341052f1ec0a700ca1f9d282
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-05 16:54:47 +0000

    Add package SecureBytes/0.2.5/SecureBytes-0.2.5.tar.gz

---
+++ packages/SecureBytes/SecureBytes-0.2.5.tar.gz/SecureBytes-0.2.5/setup.py
@@ -0,0 +1,17 @@
+# -*- encoding: utf-8 -*-
+
+try:
+    from setuptools import setup, Extension
+except ImportError:
+    from distutils.core import setup, Extension
+
+setup(
+    name='SecureBytes',
+    version='0.2.5',
+    description='Clears the contents of bytes containing cryptographic material',
+    author=u'Erik Aronesty',
+    author_email='erik@getvida.io',
+    url='https://github.com/vidaid/pysecbytes',
+    license='MIT',
+    ext_modules=[Extension('SecureBytes', ['pysecbytes.cpp'])],
+)
