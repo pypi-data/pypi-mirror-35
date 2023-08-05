0020de2bc13b6dfde73445a01a34a3821b07ead7
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-05 22:12:40 +0000

    Add package panoptes/0.3.0/panoptes-0.3.0-py3-none-any.whl

---
+++ packages/panoptes/panoptes-0.3.0-py3-none-any.whl/panoptes/aws/__init__.py
@@ -0,0 +1,12 @@
+""" Panoptes - AWS
+Responsible to import analysis functions from AWS.
+"""
+
+from panoptes.aws import analysis
+from panoptes.aws import authentication
+from panoptes.aws import output
+from panoptes.aws import whitelist
+
+
+if __name__ == "__main__":
+    pass
