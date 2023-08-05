7d359985b2ef1ecba1d0f84a239ee9607ba76f2c
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-05 15:31:20 +0000

    Add package triopg/0.2.0/triopg-0.2.0-py3-none-any.whl

---
+++ packages/triopg/triopg-0.2.0-py3-none-any.whl/triopg/_tests/conftest.py
@@ -0,0 +1,53 @@
+import pytest
+import trio_asyncio
+import asyncpg
+from asyncpg.cluster import Cluster
+import tempfile
+from functools import partial
+
+import triopg
+
+
+@pytest.fixture()
+async def asyncio_loop():
+    async with trio_asyncio.open_loop() as loop:
+        yield loop
+
+
+@pytest.fixture(scope='session')
+def cluster():
+    cluster_dir = tempfile.mkdtemp()
+    cluster = Cluster(cluster_dir)
+
+    cluster.init()
+    try:
+        cluster.start(port='dynamic')
+        yield cluster
+        cluster.stop()
+    finally:
+        cluster.destroy()
+
+
+@pytest.fixture
+def postgresql_connection_specs(cluster):
+    return {'database': 'postgres', **cluster.get_connection_spec()}
+
+
+@pytest.fixture()
+async def asyncpg_conn(asyncio_loop, postgresql_connection_specs):
+    conn = await trio_asyncio.run_asyncio(
+        partial(asyncpg.connect, **postgresql_connection_specs)
+    )
+    try:
+        yield conn
+    finally:
+        await trio_asyncio.run_asyncio(conn.close)
+
+
+@pytest.fixture()
+async def triopg_conn(asyncio_loop, postgresql_connection_specs):
+    conn = await triopg.connect(**postgresql_connection_specs)
+    try:
+        yield conn
+    finally:
+        await conn.close()
