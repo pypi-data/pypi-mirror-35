12fcc97d018099ac3985ac26018ed5b2f91a8c0a
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-05 15:05:08 +0000

    Add package merged-excel-tools/1.3/merged_excel_tools-1.3.tar.gz

---
+++ packages/merged-excel-tools/merged_excel_tools-1.3.tar.gz/merged_excel_tools-1.3/README.md
@@ -0,0 +1,27 @@
+# merged_excel_tools
+读取合并单元格的工具类
+
+这是用来读取特定格式(合并单元格)的Excel表格的工具类，格式如下
+![table.png](https://s1.ax1x.com/2018/08/13/Pg8gH0.png)
+
+## install
+使用pip
+```python
+pip install merged_excel_tools
+```
+## 使用方法
+```python
+from merged_excel_tools import merged_excel
+me = merged_excel("example.xlsx")
+ranges=me.getMergedList()
+for range in ranges:
+    # 这是合并单元格的首
+    start = me.getRowFromRange(range, 0)
+    # 这是合并单元格的尾
+    end = me.getRowFromRange(range, 1)
+    #这是A区域的合并单元格的值
+    As = me.get("A",start)
+```
+
+### 注意
+使用openpyxl来读取excel表格
---
+++ packages/merged-excel-tools/merged_excel_tools-1.3.tar.gz/merged_excel_tools-1.3/setup.py
@@ -0,0 +1,22 @@
+import setuptools
+
+with open ( "README.md" , "r",encoding="utf-8" ) as fh :
+    long_description = fh . read ()
+
+setuptools . setup (
+    name = "merged_excel_tools" ,
+    version = "1.3" ,
+    author = "zhangtao103239" ,
+    author_email = "zhangtao103239@163.com" ,
+    description = "merged cells excel tools" ,
+    long_description = long_description ,
+    long_description_content_type = "text/markdown" ,
+    url = "https://github.com/zhangtao103239/read_merged_tools" ,
+    packages = setuptools . find_packages (),
+    classifiers = (
+        "Programming Language :: Python :: 3" ,
+        "License :: OSI Approved :: MIT License" ,
+        "Operating System :: OS Independent" ,
+    ),
+    install_requires=['openpyxl']
+)
