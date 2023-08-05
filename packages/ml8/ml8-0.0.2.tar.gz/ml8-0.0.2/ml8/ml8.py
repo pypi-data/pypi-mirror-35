9dbe42898823edee87c76c1c36090e3777e531b8
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-05 15:29:17 +0000

    Add package ml8/0.0.2/ml8-0.0.2.tar.gz

---
+++ packages/ml8/ml8-0.0.2.tar.gz/ml8-0.0.2/ml8/ml8.py
@@ -0,0 +1,22 @@
+#Wikicivi Crawler Client SDK
+import os,time
+import threading
+import datetime
+import urllib
+import imageio
+
+
+
+
+class ml8:
+    def __init__(self):
+        self.apimap = {}
+    
+
+
+
+
+    #LOG,INFO,WARN,ERROR,
+    def intro():
+        print("这是TechYoung课程的机器学习辅助工具包2")
+        return True
