584327e9535a6852b431698cb75a01b8a65bddcc
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-05 15:05:30 +0000

    Add package asyncwebsockets/0.3.0/asyncwebsockets-0.3.0-py3-none-any.whl

---
+++ packages/asyncwebsockets/asyncwebsockets-0.3.0-py3-none-any.whl/asyncwebsockets/client.py
@@ -0,0 +1,40 @@
+"""
+Client connection utilities.
+"""
+import yarl
+from wsproto.events import ConnectionEstablished
+
+from asyncwebsockets._specific import Websocket
+
+try:
+    from contextlib import acontextmanager
+except ImportError:
+    from async_generator import asynccontextmanager as acontextmanager
+
+
+@acontextmanager
+async def open_websocket(url: str):
+    """
+    Opens a websocket.
+    """
+    url = yarl.URL(url)
+    # automatically use ssl if it's websocket secure
+    ssl = url.scheme == "wss"
+    addr = (url.host, int(url.port))
+    ws = Websocket()
+    await ws.__ainit__(addr=addr, ssl=ssl, path=url.path)
+
+    try:
+        event = await ws.next_event()
+        if not isinstance(event, ConnectionEstablished):
+            raise ConnectionError("Failed to establish a connection")
+        yield ws
+    finally:
+        await ws.close()
+
+
+try:
+    import curio.meta
+    curio.meta.safe_generator(open_websocket.__wrapped__)
+except ImportError:
+    pass
