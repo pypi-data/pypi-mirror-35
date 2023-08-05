f6980c94c766915b68859fbe683efefe73d0993d
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-05 15:04:30 +0000

    Add package damu-blog/1.0/damu_blog-1.0.tar.gz

---
+++ packages/damu-blog/damu_blog-1.0.tar.gz/damu_blog-1.0/data.py
@@ -0,0 +1,9 @@
+'''
+数据定义模块
+    定义各种项目中临时需要的数据
+'''
+authors = dict()
+
+articles = dict()
+
+current_login = None
---
+++ packages/damu-blog/damu_blog-1.0.tar.gz/damu_blog-1.0/manager.py
@@ -0,0 +1,10 @@
+'''
+管理类型定义模块
+    主要针对Models类型的对象进行增删改查的处理
+'''
+class AuthorManager:
+    pass
+
+
+class ArticleManager:
+    pass
