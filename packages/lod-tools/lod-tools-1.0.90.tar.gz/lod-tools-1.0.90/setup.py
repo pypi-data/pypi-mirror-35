68ecb22be3e3a884ec6cdf8aa1e4c22eb8892133
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-05 15:24:57 +0000

    Add package lod-tools/1.0.90/lod-tools-1.0.90.tar.gz

---
+++ packages/lod-tools/lod-tools-1.0.90.tar.gz/lod-tools-1.0.90/setup.py
@@ -0,0 +1,24 @@
+from distutils.core import setup
+setup(
+  name = 'lod-tools',
+  packages = ['lod',],
+  version = '1.0.90',
+  description = 'A program for interacting with the API of elody.com',
+  long_description = 'A program for interacting with the API of elody.com',
+  author = 'Florian Dietz',
+  author_email = 'floriandietz44@gmail.com',
+  url='https://elody.com',
+  license = 'MIT',
+  package_data={
+      '': ['*.txt', # this covers both the LICENSE.txt file in this folder, and the TRUTH.txt file in the /lod/ folder
+      ]
+   },
+   entry_points = {
+        'console_scripts': [
+            'lod-tools=lod.lod_tools:main',
+        ],
+    },
+    install_requires=[
+        'docker',
+    ],
+)
