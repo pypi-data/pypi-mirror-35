6561bbe76134caa1277b2207a441b35ff1f5a9d7
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-05 15:06:00 +0000

    Add package ueditor/1.3/ueditor-1.3.tar.gz

---
+++ packages/ueditor/ueditor-1.3.tar.gz/ueditor-1.3/setup.py
@@ -0,0 +1,20 @@
+from setuptools import setup, find_packages
+
+long_description = "\n".join([
+    open('README.rst', 'r').read(),
+])
+
+setup(
+    name='ueditor',
+    version='1.3',
+    packages=find_packages(),
+    zip_safe=False,
+    include_package_data=True,
+    url='https://github.com/newpanjing/ueditor.git',
+    license='Apache Listen 2.0',
+    author='panjing',
+    author_email='newpanjing@icloud.com',
+    description='django ueditor plugin',
+    long_description=long_description,
+    install_requires=['django', 'shortid8'],
+)
