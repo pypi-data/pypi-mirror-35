8d184334163892c802499b74141674618e09f812
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-05 17:05:49 +0000

    Add package ttyrecgenerator/0.3.0/ttyrecgenerator-0.3.0.tar.gz

---
+++ packages/ttyrecgenerator/ttyrecgenerator-0.3.0.tar.gz/ttyrecgenerator-0.3.0/ttyrecgenerator/__init__.py
@@ -0,0 +1,8 @@
+import datetime
+from .libttyrecgenerator import RecSession 
+
+#language = gettext.translation ('ttyrecgenerator', 'locale' )
+#language.install()
+
+__version__ = '0.3.0'
+__versiondate__=datetime.date(2018,8,18)
