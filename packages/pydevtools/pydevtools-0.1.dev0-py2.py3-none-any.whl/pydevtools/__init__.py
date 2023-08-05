a4818be1f395141c018e683eb305a64ca63c9d46
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-05 15:25:41 +0000

    Add package pydevtools/0.1.dev0/pydevtools-0.1.dev0-py2.py3-none-any.whl

---
+++ packages/pydevtools/pydevtools-0.1.dev0-py2.py3-none-any.whl/pydevtools/__init__.py
@@ -0,0 +1,20 @@
+'''
+A toolkit of useful tools for python3 development
+'''
+import debug
+import cache
+
+
+
+#information about the code
+__author__ = "Anders Ballegaard"
+__copyright__ = "Copyright 2018, Anders Ballegaard"
+__credits__ = ["Anders Ballegaard"]
+__license__ = "MIT"
+__maintainer__ = "Anders Ballegaard"
+__email__ = "anderstb[at]hotmail.dk"
+__status__ = "Production"
+
+
+#limit the amount of functions and vars exposed from the module
+__all__ = ['optimize.timer']
