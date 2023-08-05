ba370e5df8e024aade7659c841818d21186352ac
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-05 15:29:38 +0000

    Add package easywsy/0.1.1/easywsy-0.1.1.tar.gz

---
+++ packages/easywsy/easywsy-0.1.1.tar.gz/easywsy-0.1.1/setup.py
@@ -0,0 +1,28 @@
+# -*- coding: utf-8 -*-
+from setuptools import setup
+
+
+with open("README.md", "r") as fh:
+    long_description = fh.read()
+
+
+setup(
+    name='easywsy',
+    version='0.1.01',
+    description='Simple Web Service development API based on suds',
+    long_description=long_description,
+    long_description_content_type="text/markdown",
+    url='https://gitlab.e-mips.com.ar/infra/easywsy',
+    author='Martín Nicolás Cuesta',
+    author_email='cuesta.martin.n@hotmail.com',
+    packages=['easywsy', 'easywsy.api', 'easywsy.ws',
+              'easywsy.error', 'easywsy.check'],
+    license='AGPL3+',
+    python_requires='>=3.5',
+    install_requires=[
+        'suds-py3',
+    ],
+    zip_safe=False,
+    setup_requires=["pytest-runner"],
+    tests_require=["pytest"],
+)
