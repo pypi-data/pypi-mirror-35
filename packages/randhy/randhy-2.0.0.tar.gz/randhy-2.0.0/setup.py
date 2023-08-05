c33a23252e5cea75304bc4d2e75c853c462dc0fd
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-05 17:07:26 +0000

    Add package randhy/2.0.0/randhy-2.0.0.tar.gz

---
+++ packages/randhy/randhy-2.0.0.tar.gz/randhy-2.0.0/setup.py
@@ -0,0 +1,21 @@
+from setuptools import setup
+
+
+__author__ = 'Sasho Savkov'
+__credits__ = ["William Morgan"]
+__license__ = "MIT"
+__version__ = "2.0.0"
+__email__ = "me@sasho.io"
+__status__ = "Production"
+
+
+setup(
+      name='randhy',
+      version='2.0.0',
+      description='Approximate randomisation library',
+      author='Sasho Savkov',
+      author_email='me@sasho.io',
+      url='https://www.github.com/asavkov/randhy/',
+      package_dir={'': 'src'},
+      packages=['randhy']
+)
