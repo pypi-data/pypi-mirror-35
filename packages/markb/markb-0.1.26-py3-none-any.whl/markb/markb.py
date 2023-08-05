e068a1fb2883517bc32df2a28b5989fb38d60348
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-05 16:58:29 +0000

    Add package markb/0.1.26/markb-0.1.26-py3-none-any.whl

---
+++ packages/markb/markb-0.1.26-py3-none-any.whl/markb/markb.py
@@ -0,0 +1,32 @@
+#!/usr/bin/env python3
+# mdbro
+# Get a markdown file via argument and output to a tempfile.
+# Open automatically with browser. Fun!
+
+from argparse import ArgumentParser
+from markdown import markdown
+from tempfile import NamedTemporaryFile
+import webbrowser
+
+
+def main():
+    description = """Render markdown files to
+    a temporary file and open it in a browser (YOUR browser!)"""
+    parser = ArgumentParser(description=description)
+    parser.add_argument("filename", help="A markdown file")
+    args = parser.parse_args()
+
+    try:
+        with open(args.filename) as md:
+            html = markdown(md.read())
+
+        tempfile = NamedTemporaryFile(mode="w+", delete=False, suffix=".html")
+        tempfile.write(html)
+        webbrowser.open("file://{}".format(tempfile.name))
+
+    except FileNotFoundError:
+        print('File not found "{filename}"'.format(filename=args.filename))
+
+
+if __name__ == "__main__":
+    main()
