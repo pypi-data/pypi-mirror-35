a67bba9f9f7e9bad929e27050861e806221ba6d8
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-05 18:11:05 +0000

    Add package rotatedicom/1.0.1/rotatedicom-1.0.1.tar.gz

---
+++ packages/rotatedicom/rotatedicom-1.0.1.tar.gz/rotatedicom-1.0.1/setup.py
@@ -0,0 +1,22 @@
+import setuptools
+setuptools.setup(
+    name = "rotatedicom",
+    version = "1.0.1",
+    packages = ["rotatedicom"],
+    
+    author="Dibya Raj Ghosh", 
+    author_email="dibyaraj11@gmail.com", 
+    maintainer="Dibya", 
+    maintainer_email="dibyaraj11@gmail.com", 
+    description="It will rotate image by a specific angle provided by user",
+    long_description="""
+    Input-Dicom image
+    Output-Dicom image
+    It will rotate image by a specific angle provided by user
+    This function will accept 3 mandatory parameter- source folder, destination folder, 
+    rotation-angle(positive  value means -anticlowise and negative value means clockwise rotation)
+
+    """, 
+
+    license="MIT"
+    )
