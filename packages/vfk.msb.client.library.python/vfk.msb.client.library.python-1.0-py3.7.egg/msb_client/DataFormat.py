9207bc81f0a2d23bd8a011ddcdf9357a96aab65d
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-05 15:30:10 +0000

    Add package vfk.msb.client.library.python/1.0/vfk.msb.client.library.python-1.0-py3.7.egg

---
+++ packages/vfk.msb.client.library.python/vfk.msb.client.library.python-1.0-py3.7.egg/msb_client/DataFormat.py
@@ -0,0 +1,29 @@
+# -*- coding: utf-8 -*-
+"""
+Copyright (c) 2017
+Fraunhofer Institute for Manufacturing Engineering
+and Automation (IPA)
+Author: Daniel Stock
+mailto: daniel DOT stock AT ipa DOT fraunhofer DOT com
+See the file "LICENSE" for the full license governing this code.
+"""
+
+from msb_client.DataType import *
+
+
+class DataFormat:
+    def __init__(self, dataType=None, isArray=None):
+        self.isArray = isArray
+        dataFormat = {}
+        dataObject = {}
+        if isArray != None and isArray:
+            dataObject["type"] = "array"
+            dataObject["items"] = getDataType(dataType)
+            dataFormat["dataObject"] = dataObject
+        else:
+            dataObject = getDataType(dataType)
+            dataFormat["dataObject"] = dataObject
+        self.dataFormat = dataFormat
+
+    def getDataFormat(self):
+        return self.dataFormat
---
+++ packages/vfk.msb.client.library.python/vfk.msb.client.library.python-1.0-py3.7.egg/msb_client/__init__.py
@@ -0,0 +1,15 @@
+"""
+Copyright (c) 2017
+Fraunhofer Institute for Manufacturing Engineering
+and Automation (IPA)
+Author: Daniel Stock
+mailto: daniel DOT stock AT ipa DOT fraunhofer DOT com
+See the file "LICENSE" for the full license governing this code.
+"""
+
+from msb_client.MsbClient import *
+from msb_client.Event import *
+from msb_client.Function import *
+from msb_client.DataType import *
+from msb_client.DataFormat import *
+from msb_client.ComplexDataFormat import *
