609bdff4ddd9f90462f229972c4c6e6d72df7f38
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-05 15:29:18 +0000

    Add package calx/0.0.0/calx-0.0.0.tar.gz

---
+++ packages/calx/calx-0.0.0.tar.gz/calx-0.0.0/setup.py
@@ -0,0 +1,28 @@
+
+#!/usr/bin/env python
+
+import setuptools
+
+VERSION = (0, 0, 0)
+VERSION_STR = ".".join([str(x) for x in VERSION])
+
+with open("README.md", "r") as fh:
+    long_description = fh.read()
+
+setuptools.setup(
+    name='calx',
+    version=VERSION_STR,
+    description="",
+    long_description=long_description,
+    long_description_content_type="text/markdown",
+    author='HLemke',
+    author_email='htlemke@gmail.com',
+    url='https://github.com/htlemke/calx',
+    packages=setuptools.find_packages(),
+    requires=['numpy','scipy','matplotlib'],
+    classifiers=(
+        "Programming Language :: Python :: 3",
+        "License :: OSI Approved :: MIT License",
+        "Operating System :: OS Independent",
+    ),
+)
