5385a4a09786206fc064f6995b5a5701f1994d51
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-05 22:11:32 +0000

    Add package interpolation/0.2.1/interpolation-0.2.1.tar.gz

---
+++ packages/interpolation/interpolation-0.2.1.tar.gz/interpolation-0.2.1/setup.py
@@ -0,0 +1,16 @@
+from setuptools import setup, find_packages  # Always prefer setuptools over distutils
+
+setup(
+    name='interpolation',
+    version='0.2.1',
+    description='Interpolation in Python',
+    url='https://github.com/econforge/interpolation.py',
+    author='Chase Coleman, Spencer Lyon and Pablo Winant',
+    author_email='pablo.winant@gmail.com',
+    license='BSD-2',
+    classifiers=[
+        'Development Status :: 4 - Beta',
+    ],
+    packages=find_packages(exclude=['contrib', 'docs', 'tests*']),
+    install_requires=["numba", "numpy", "scipy"],
+)
