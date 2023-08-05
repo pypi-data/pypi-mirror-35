b921eabb30606f678690095270da4953136e5e3b
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-05 21:12:04 +0000

    Add package jy-word/0.1/jy_word-0.1.tar.gz

---
+++ packages/jy-word/jy_word-0.1.tar.gz/jy_word-0.1/setup.py
@@ -0,0 +1,19 @@
+# -*- coding: utf-8 -*-
+# !/usr/bin/python
+# Create Date 2018/6/4 0004
+__author__ = 'huohuo'
+from setuptools import setup
+setup(name='jy_word',
+      version='0.1',
+      description='jy_word',
+      url='https://github.com/hp910219/jy_word',
+      author='hp910219',
+      author_email='hp910219@126.com',
+      license='MIT',
+      packages=['jy_word'],
+      zip_safe=False)
+
+if __name__ == "__main__":
+      pass
+    
+
