365c8c100b92db8969760197438c77ae2882a4f2
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-05 17:07:42 +0000

    Add package docb/0.12.0/docb-0.12.0-py2.py3-none-any.whl

---
+++ packages/docb/docb-0.12.0-py2.py3-none-any.whl/docb/testcase.py
@@ -0,0 +1,20 @@
+import unittest
+from .loading import DocbHandler
+from envs import env
+
+docb_handler = DocbHandler({
+    'dynamodb': {
+        'backend': 'docb.db.DynamoDB',
+        'connection': {
+            'table': env('DYNAMO_TABLE_TEST'),
+            'endpoint_url': env('DYNAMO_ENDPOINT_URL_TEST')
+        }
+    },
+})
+
+
+class DocbTestCase(unittest.TestCase):
+
+    def tearDown(self):
+        for db_label in list(docb_handler._databases.keys()):
+            docb_handler.get_db(db_label).flush_db()
