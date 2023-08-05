5fe0f1a6582bb960f3de073637006663ac8d05e8
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-05 15:24:38 +0000

    Add package django-staticinline/1.2/django_staticinline-1.2-py2.py3-none-any.whl

---
+++ packages/django-staticinline/django_staticinline-1.2-py2.py3-none-any.whl/staticinline/templatetags/staticinline.py
@@ -0,0 +1,74 @@
+from __future__ import absolute_import
+from logging import getLogger
+
+from django.apps import apps
+from django.conf import settings
+from django.core.exceptions import ImproperlyConfigured
+from django.template.defaulttags import register
+from django.utils.safestring import mark_safe
+
+from staticinline.main import read_static_file
+
+logger = getLogger(__file__)
+config = apps.get_app_config('staticinline')
+
+
+@register.simple_tag()
+def staticinline(path, encode=None):
+    """
+    Similar to Django's native `static` templatetag, but this includes
+    the file directly in the template, rather than a link to it.
+
+    Example::
+
+    {% load staticinline %}
+
+    <style type="text/css">{% staticinline "myfile.css" %}</style>
+    <script>{% staticinline "myfile.js" %}</script>
+
+    Becomes::
+
+        <style type="text/css">body{ color: red; }</style>
+        <script>alert("Hello World");</script>
+
+    Raises a ValueError if the the file does not exist, and
+    DEBUG is enabled.
+
+    :param str path: Filename of the file to include.
+    :return: Returns the the file content *or* ``''`` (empty string) if the
+        file was not found, and ``DEBUG`` is ``False``.
+    :rtype: str
+    :raises ValueError: if the file is not found and ``DEBUG`` is ``True``
+    """
+    try:
+        data = read_static_file(path, mode='rb' if encode else 'r')
+    except ValueError:
+        if settings.DEBUG:
+            raise
+        return ''
+
+    # If we don't encode the file further, we can return it right away.
+    if not encode:
+        return mark_safe(data)
+
+    encoder_registry = config.get_encoder()
+
+    if encode not in encoder_registry:
+        raise ImproperlyConfigured(
+            '"{0}" is not a registered encoder. Valid values are: {1}'.format(
+                encode, ', '.join(encoder_registry.keys())
+            )
+        )
+    try:
+        return mark_safe(encoder_registry[encode](data, path))
+    # Anything could go wrong since we don't control the encoding
+    # list itself. In case of an error raise that exception, unless
+    # DEBUG mode is off. Then, same as above, return an empty string.
+    except Exception as e:
+        logger.error(
+            'Error encoding to data format %s in static file "%s".',
+            encode, path)
+        logger.exception(e)
+        if settings.DEBUG:
+            raise e
+    return ''
---
+++ packages/django-staticinline/django_staticinline-1.2-py2.py3-none-any.whl/staticinline/tests/testapp/apps.py
@@ -0,0 +1,31 @@
+"""
+Custom app config for staticinline to demonstrate and test
+the 'custom encoder' functionality.
+"""
+
+from staticinline.apps import StaticInlineAppConfig
+
+
+class CustomizedStaticInlineAppConfig(StaticInlineAppConfig):
+    """
+    Add a custom encoder to the list to test that behavior
+    """
+    def get_encoder(self):
+        encoder = super(CustomizedStaticInlineAppConfig, self).get_encoder()
+        encoder.update({
+            'uppercase': self.uppercase,
+            'broken': self.broken,
+        })
+        return encoder
+
+    def uppercase(self, data, path):
+        """
+        Sample encoder that turns the incoming text data uppercase.
+        """
+        return data.decode(self.encoder_response_format).upper()
+
+    def broken(self, data, path):
+        """
+        This intentionally raises an Exception to test error reporting.
+        """
+        return 1/0
