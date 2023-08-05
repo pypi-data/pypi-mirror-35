9f7e77db4ef0890242a7417384513be0b2fcd5e4
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-05 15:05:49 +0000

    Add package anndata/0.6.8/anndata-0.6.8.tar.gz

---
+++ packages/anndata/anndata-0.6.8.tar.gz/anndata-0.6.8/setup.py
@@ -0,0 +1,45 @@
+from setuptools import setup, find_packages
+from pathlib import Path
+import versioneer
+
+package_name = 'anndata'
+
+req_path = Path('requires.txt')
+if not req_path.is_file():
+    req_path = Path(package_name + '.egg-info') / req_path
+with req_path.open() as requirements:
+    requires = [l.strip() for l in requirements]
+
+with open('README.rst') as readme_f:
+    readme = readme_f.read()
+
+setup(
+    name=package_name,
+    version=versioneer.get_version(),
+    cmdclass=versioneer.get_cmdclass(),
+    description='An annotated data matrix.',
+    long_description=readme,
+    url='http://github.com/theislab/anndata',
+    author='Alex Wolf, Philipp Angerer, Sergei Rybakov',
+    author_email='alex.wolf@helmholtz-muenchen.de',
+    license='BSD-3-Clause',
+    install_requires=requires,
+    python_requires='>=3.5',
+    packages=find_packages(),
+    zip_safe=False,
+    classifiers=[
+        'Environment :: Console',
+        'Framework :: Jupyter',
+        'Intended Audience :: Developers',
+        'Intended Audience :: Science/Research',
+        'Natural Language :: English',
+        'Operating System :: MacOS :: MacOS X',
+        'Operating System :: Microsoft :: Windows',
+        'Operating System :: POSIX :: Linux',
+        'Programming Language :: Python :: 3',
+        'Programming Language :: Python :: 3.5',
+        'Programming Language :: Python :: 3.6',
+        'Topic :: Scientific/Engineering :: Bio-Informatics',
+        'Topic :: Scientific/Engineering :: Visualization',
+    ],
+)
