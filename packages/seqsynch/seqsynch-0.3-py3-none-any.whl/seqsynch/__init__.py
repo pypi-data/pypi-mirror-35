150ae267037aa73381732f3faf624fa88935d029
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-05 15:26:11 +0000

    Add package seqsynch/0.3/seqsynch-0.3-py3-none-any.whl

---
+++ packages/seqsynch/seqsynch-0.3-py3-none-any.whl/seqsynch/__init__.py
@@ -0,0 +1,10 @@
+# -*- coding: utf-8 -*-
+"""
+@author: mezeda01
+this is an init file"""
+
+from . import config as conf
+from . import agent as agent
+
+def main():
+    agent.main()
---
+++ packages/seqsynch/seqsynch-0.3-py3-none-any.whl/seqsynch/command_line.py
@@ -0,0 +1,9 @@
+# -*- coding: utf-8 -*-
+"""
+@author: mezeda01
+this is an command_line file"""
+
+import seqsynch
+
+def main():
+    seqsynch.main()
