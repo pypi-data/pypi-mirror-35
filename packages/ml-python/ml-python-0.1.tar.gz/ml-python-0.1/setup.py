d8c0f245cfb6adb859a93776e889b196aa9c7afa
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-05 16:56:57 +0000

    Add package ml-python/0.1/ml-python-0.1.tar.gz

---
+++ packages/ml-python/ml-python-0.1.tar.gz/ml-python-0.1/setup.py
@@ -0,0 +1,17 @@
+from setuptools import setup
+
+setup(
+    name='ml-python',
+    version='0.1',
+    packages=['ml',],
+    author="Vivek Verma",
+    author_email="vivekverma3141@gmail.com",
+    url="https://github.com/vivek3141/ml",
+    license='MIT',
+    long_description=open('README.txt').read(),
+    classifiers=[
+        "Programming Language :: Python :: 3",
+        "License :: OSI Approved :: MIT License",
+        "Operating System :: OS Independent",
+    ]
+)
