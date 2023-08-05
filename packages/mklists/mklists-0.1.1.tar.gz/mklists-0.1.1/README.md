3d01db7b76f97138e78f6fb356b3a00ab349383e
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-05 16:57:24 +0000

    Add package mklists/0.1.1/mklists-0.1.1.tar.gz

---
+++ packages/mklists/mklists-0.1.1.tar.gz/mklists-0.1.1/README.md
@@ -0,0 +1,3 @@
+mklists
+
+Rebuild plain-text todo lists using rules
---
+++ packages/mklists/mklists-0.1.1.tar.gz/mklists-0.1.1/setup.py
@@ -0,0 +1,29 @@
+from setuptools import setup, find_packages
+
+with open("README.md", "r") as fh:
+    long_description = fh.read()
+
+setup(
+    name = 'mklists',
+    version = '0.1.1',
+    license = 'MIT',
+    description = 'Rebuild plain-text todo lists using rules',
+    long_description=long_description,
+    author = 'Tom Baker',
+    author_email='tom@tombaker.org',
+    classifiers=[
+        'Development Status :: 1 - Planning',
+        'Programming Language :: Python :: 3.7',
+        'License :: OSI Approved :: MIT License',
+        'Topic :: Text Processing',
+    ],
+    url = 'https://github.com/tombaker/mklists',
+    packages = find_packages(exclude=['tests']),
+    install_requires =[
+        'pyyaml', 'Click', 'dataclasses', 'string'
+    ],
+    entry_points = """
+        [console_scripts]
+        mklists=mklists.cli:cli
+    """
+)
