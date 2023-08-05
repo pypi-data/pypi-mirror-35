4b3bd32f861d96b107dd5e537ccaeb02a7178ea1
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-05 21:08:58 +0000

    Add package jy-word/0.7/jy-word-0.7.tar.gz

---
+++ packages/jy-word/jy-word-0.7.tar.gz/jy-word-0.7/setup.py
@@ -0,0 +1,22 @@
+# -*- coding: utf-8 -*-
+# !/usr/bin/python
+# Create Date 2018/6/4 0004
+__author__ = 'huohuo'
+from setuptools import setup, find_packages
+
+setup(
+    name='jy-word',
+    version='0.7',
+    keywords=('word', 'test'),
+    description='generate word',
+    license='MIT License',
+    author='hp910219',
+    author_email='hp910219@126.com',
+    url='https://github.com/hp910219/jy-word.git',
+    packages=find_packages(exclude=['test']),
+)
+
+if __name__ == "__main__":
+      pass
+    
+
