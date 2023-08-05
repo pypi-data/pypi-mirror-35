bf4eb7dd6dc4df169df504a740dd47b4b530ea53
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-05 15:05:38 +0000

    Add package plane-war/1.0/plane_war-1.0.tar.gz

---
+++ packages/plane-war/plane_war-1.0.tar.gz/plane_war-1.0/setup.py
@@ -0,0 +1,10 @@
+from distutils.core import setup
+
+setup(
+    name='plane_war',
+    version='1.0',
+    description='飞机大战',
+    author='折戟',
+    author_email='969465519@qq.com',
+    py_modules = ["__init__","game_data","game_engine","game_event","game_sprites","main"]
+)
