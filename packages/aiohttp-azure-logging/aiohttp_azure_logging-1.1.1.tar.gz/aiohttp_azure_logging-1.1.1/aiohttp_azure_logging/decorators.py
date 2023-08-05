fa854bd8a3d832ad2e1a83112cb32d94b342d897
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-05 21:09:11 +0000

    Add package aiohttp-azure-logging/1.1.1/aiohttp_azure_logging-1.1.1.tar.gz

---
+++ packages/aiohttp-azure-logging/aiohttp_azure_logging-1.1.1.tar.gz/aiohttp_azure_logging-1.1.1/aiohttp_azure_logging/decorators.py
@@ -0,0 +1,41 @@
+from .utils import find_request
+import time, datetime
+import asyncio
+import inspect
+
+def log_execution(func, oms, event_name="EXEC"):
+    def wrap(*args, **kwargs):
+        event_time = datetime.datetime.utcnow()
+        exc_start = time.time()
+        result = func(*args, **kwargs)
+        exec_time = time.time() - exc_start
+        req = find_request()
+        asyncio.ensure_future(oms.create_event(
+            log_type="serverLog",
+            name=event_name,
+            request=req,
+            event_time=event_time,
+            event_data={
+                'execution': exec_time,
+                'api': getattr(func, '__name__', '_ModuleError'),
+                'controller': getattr(func, '__module__', '_ModuleError')
+            }))
+        return result
+    async def async_wrap(*args, **kwargs):
+        event_time = datetime.datetime.utcnow()
+        exc_start = time.time()
+        result = await func(*args, **kwargs)
+        exec_time = time.time() - exc_start
+        req = find_request()
+        asyncio.ensure_future(oms.create_event(
+            log_type="serverLog",
+            name=event_name,
+            request=req,
+            event_time=event_time,
+            event_data={
+                'execution': exec_time,
+                'api': getattr(func, '__name__', '_ModuleError'),
+                'controller': getattr(func, '__module__', '_ModuleError')
+            }))
+        return result
+    return async_wrap if inspect.iscoroutinefunction(func) else wrap
