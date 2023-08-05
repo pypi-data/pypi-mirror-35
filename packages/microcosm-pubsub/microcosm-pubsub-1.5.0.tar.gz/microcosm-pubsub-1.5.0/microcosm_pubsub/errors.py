ecf7a049ff0643d3b92c6fe3205b8d06bf5a2db6
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-05 18:10:30 +0000

    Add package microcosm-pubsub/1.5.0/microcosm-pubsub-1.5.0.tar.gz

---
+++ packages/microcosm-pubsub/microcosm-pubsub-1.5.0.tar.gz/microcosm-pubsub-1.5.0/microcosm_pubsub/errors.py
@@ -0,0 +1,30 @@
+"""
+PubSub errors.
+
+"""
+
+
+class TopicNotDefinedError(Exception):
+    pass
+
+
+class Nack(Exception):
+    """
+    An error that causes the current message to be nacked.
+
+    """
+    def __init__(self, visibility_timeout_seconds):
+        self.visibility_timeout_seconds = visibility_timeout_seconds
+
+    def __repr__(self):
+        return "Nack({})".format(self.visibility_timeout_seconds)
+
+
+class SkipMessage(Exception):
+    """
+    Control-flow exception to skip resource processing.
+
+    """
+    def __init__(self, reason, extra=None):
+        super().__init__(reason)
+        self.extra = extra or dict()
