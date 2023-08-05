7070f84f30273696285fcfa878a59653ac549a5b
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-05 17:07:28 +0000

    Add package pretty-format-json/0.2.1/pretty-format-json-0.2.1.tar.gz

---
+++ packages/pretty-format-json/pretty-format-json-0.2.1.tar.gz/pretty-format-json-0.2.1/setup.py
@@ -0,0 +1,56 @@
+# Always prefer setuptools over distutils
+from setuptools import setup, find_packages
+from os import path
+from io import open
+
+here = path.abspath(path.dirname(__file__))
+
+# Get the long description from the README file
+with open(path.join(here, 'README.md'), encoding='utf-8') as f:
+    long_description = f.read()
+
+# Arguments marked as "Required" below must be included for upload to PyPI.
+# Fields marked as "Optional" may be commented out.
+
+gh_repo = 'https://github.com/weaming/pretty-format-json'
+
+setup(
+    name='pretty-format-json',  # Required
+
+    version='0.2.1',  # Required
+
+    # This is a one-line description or tagline of what your project does.
+    description='Pretty print json contains python style coments, string literal.',  # Required
+
+    url=gh_repo,  # Optional
+    author='weaming',  # Optional
+    author_email='garden.yuen@gmail.com',  # Optional
+
+    packages=find_packages(exclude=['contrib', 'docs', 'tests']),  # Required
+
+    # This field adds keywords for your project which will appear on the
+    # project page. What does your project relate to?
+    #
+    # Note that this is a string of words separated by whitespace, not a list.
+    keywords='json format',  # Optional
+
+    # To provide executable scripts, use entry points in preference to the
+    # "scripts" keyword. Entry points provide cross-platform support and allow
+    # `pip` to create the appropriate form of executable for the target
+    # platform.
+    #
+    # For example, the following would provide a command called `sample` which
+    # executes the function `main` from this package when invoked:
+    entry_points={  # Optional
+        'console_scripts': [
+            'pretty_format_json=pretty_format_json.format:main',
+            'yaml_json=pretty_format_json.yaml_json:main',
+            'csv_json=pretty_format_json.csv_json:main',
+        ],
+    },
+
+    project_urls={  # Optional
+        'Bug Reports': gh_repo,
+        'Source': gh_repo,
+    },
+    )
