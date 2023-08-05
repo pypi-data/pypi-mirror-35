1401fb54739f957c24f03b6f6ec86042cea24457
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-05 21:11:02 +0000

    Add package pankus/2.2.1/pankus-2.2.1.tar.gz

---
+++ packages/pankus/pankus-2.2.1.tar.gz/pankus-2.2.1/setup.py
@@ -0,0 +1,39 @@
+#!/usr/bin/env python3
+# -*- coding: utf-8 -*-
+#from distutils.core import setup
+from setuptools import setup
+
+setup(
+    name='pankus',
+    version='2.2.1',
+    packages=[
+        'taurus',
+    ],
+    package_data={
+        'taurus': ['SQL/*.sql',
+                   'SQL/intopp/*.sql',
+                   'SQL/initial/*.sql',
+                   'SQL/route/*.sql',
+                   'SQL/mst/*.sql',
+                   'SQL/analysis/*.sql'
+                   ],     # All files from folder
+    },
+    requires=[
+        'progressbar2',
+        'numpy',
+    ],
+    url='http://github.com/mk45/pankus',
+    license='(c) Politechnika Wrocławska',
+    author='Maciej Kamiński',
+    author_email='maciej.kaminski@pwr.edu.pl',
+    description='Spatial planning software',
+    classifiers=[
+        'Development Status :: 3 - Alpha',
+        'Environment :: Console',
+        'Intended Audience :: Science/Research',
+        'License :: Free For Educational Use',
+        'Programming Language :: Python :: 3',
+        'Programming Language :: SQL',
+        'Topic :: Scientific/Engineering :: GIS',
+    ]
+)
