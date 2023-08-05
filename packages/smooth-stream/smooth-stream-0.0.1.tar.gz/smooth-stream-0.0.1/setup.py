1db5881e32671e5267c22dcbed4258b3fb446072
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-05 20:11:58 +0000

    Add package smooth-stream/0.0.1/smooth-stream-0.0.1.tar.gz

---
+++ packages/smooth-stream/smooth-stream-0.0.1.tar.gz/smooth-stream-0.0.1/setup.py
@@ -0,0 +1,12 @@
+from setuptools import setup
+
+setup(
+    name='smooth-stream',
+    version='0.0.1',
+    packages=['camera'],
+    url='https://github.com/CT83/SmoothStream',
+    license='GPLv3',
+    author='rohan',
+    author_email='rohansawantct83@gmail.com',
+    description='Webcam, PiCamera Streaming over the Network in Python made easy.'
+)
