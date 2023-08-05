f00fe4a421966eed8f8bbc20bd3c5a01f7539c6a
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-05 18:09:11 +0000

    Add package nester-liz/1.1.0/nester_liz-1.1.0.tar.gz

---
+++ packages/nester-liz/nester_liz-1.1.0.tar.gz/nester_liz-1.1.0/setup.py
@@ -0,0 +1,11 @@
+from distutils.core import setup
+ 
+setup(
+		name		='nester_liz',
+		version		='1.1.0',
+		py_modules	=['nester_liz001'],
+		author		='liz',
+		author_email='18137803582@163.com',
+		description	='A simple printer of nested lists',
+		)
+		
