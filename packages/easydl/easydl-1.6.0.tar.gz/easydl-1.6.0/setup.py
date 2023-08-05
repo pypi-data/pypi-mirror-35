06fa5836059b0383d0f919bd26039f76141cc111
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-05 15:29:32 +0000

    Add package easydl/1.6.0/easydl-1.6.0.tar.gz

---
+++ packages/easydl/easydl-1.6.0.tar.gz/easydl-1.6.0/setup.py
@@ -0,0 +1,13 @@
+from setuptools import setup, find_packages
+setup(name='easydl',
+      version='1.6.0',
+      description='"some accumulative update"',
+      url='https://github.com/thuml/easydl',
+      license='MIT',
+      packages=['easydl','easydl.tf', 'easydl.common', 'easydl.pytorch'],
+      install_requires=['tensorflow','tensorlayer','tensorpack','matplotlib','pathlib2', 'docopt', 'six'],
+      entry_points={
+        'console_scripts': [
+                'runTask= easydl:runTask'],
+    },
+      zip_safe=False)
