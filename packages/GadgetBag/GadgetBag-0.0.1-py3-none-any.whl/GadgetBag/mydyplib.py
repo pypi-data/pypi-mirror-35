f1d7e571a34a1c4548c6b470c7a46d15dc67ee72
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-05 15:23:45 +0000

    Add package GadgetBag/0.0.1/GadgetBag-0.0.1-py3-none-any.whl

---
+++ packages/GadgetBag/GadgetBag-0.0.1-py3-none-any.whl/GadgetBag/mydyplib.py
@@ -0,0 +1,23 @@
+#!/usr/bin/python
+# -*- coding: UTF-8 -*-
+
+
+def add(x,y):
+    return x+y
+
+def division(x,y):
+    return x/y
+
+def mutiply(x,y):
+    return x*y
+
+def subtract(x,y):
+    return x-y
+
+def main():
+    pass
+
+def dypprint():
+    print("dyp yz home")
+if __name__ == '__main__':
+    main()
