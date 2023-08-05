f2c37eb7dbc963aa9d8d55fe6ad5a0956e4bfbe2
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-05 15:05:24 +0000

    Add package encoding-tools/0.0.3/encoding_tools-0.0.3.tar.gz

---
+++ packages/encoding-tools/encoding_tools-0.0.3.tar.gz/encoding_tools-0.0.3/setup.py
@@ -0,0 +1,31 @@
+from setuptools import setup
+
+with open("README.md", "r") as f:
+    long_description = f.read()
+
+setup(
+    name='encoding_tools',
+    version="0.0.3",
+    description="A package to deal with encoding.",
+    long_description=long_description,
+    long_description_content_type='text/markdown',
+    author='Thomas Berdy',
+    author_email='thomas.berdy@outlook.com',
+    url='https://github.com/Tberdy/encoding_tools',
+    packages=[
+        'encoding_tools'
+    ],
+    classifiers=(
+        'Programming Language :: Python :: 3',
+        "License :: OSI Approved :: MIT License",
+        'Operating System :: OS Independent',
+        'Topic :: Text Processing :: Linguistic',
+        'Topic :: Software Development :: Libraries :: Python Modules',
+    ),
+    install_requires=(
+        'chardet',
+        'Unidecode',
+    ),
+    include_package_data=True,
+    zip_safe=False,
+)
