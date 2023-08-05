956fcd3ed2681532f1bafb655c8862172352421d
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-05 15:25:04 +0000

    Add package oceandb-driver-interface/0.0.6/oceandb_driver_interface-0.0.6-py2.py3-none-any.whl

---
+++ packages/oceandb-driver-interface/oceandb_driver_interface-0.0.6-py2.py3-none-any.whl/oceandb_driver_interface/__init__.py
@@ -0,0 +1,11 @@
+"""Top-level package for oceandb-plugin-system."""
+
+__author__ = """OceanProtocol"""
+__email__ = 'enrique@oceanprotocol.com'
+__version__ = '0.0.1'
+
+from oceandb_driver_interface.oceandb import (
+    OceanDb
+)
+
+from oceandb_driver_interface.exceptions import *
