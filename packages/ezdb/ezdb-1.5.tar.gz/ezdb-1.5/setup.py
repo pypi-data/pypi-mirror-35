7c07da342f78a128283dd0ae87e0a0a324bf9030
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-05 17:08:19 +0000

    Add package ezdb/1.5/ezdb-1.5.tar.gz

---
+++ packages/ezdb/ezdb-1.5.tar.gz/ezdb-1.5/setup.py
@@ -0,0 +1,16 @@
+import setuptools
+setuptools.setup(
+  name='ezdb',
+  version='1.5',
+  description='Access a remote database like you would a native python structure',
+  long_description='Access a remote database like you would a native python structure',
+  author='Tyler Young',
+  packages=setuptools.find_packages(),
+  author_email='tyler.young@villanova.edu',
+  url='https://github.com/tyoung03/ezdb', 
+  keywords = ['database', 'ezdb', 'EZDB', 'easy', 'db', 'python'],
+  classifiers = [
+        "Programming Language :: Python :: 2",
+        "Operating System :: OS Independent",],
+)
+
