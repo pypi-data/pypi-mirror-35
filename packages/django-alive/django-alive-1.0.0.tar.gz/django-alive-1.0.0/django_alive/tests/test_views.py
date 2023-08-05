c991b3b57db470ed1aeb1afa13d9426661f3adba
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-05 20:12:18 +0000

    Add package django-alive/1.0.0/django-alive-1.0.0.tar.gz

---
+++ packages/django-alive/django-alive-1.0.0.tar.gz/django-alive-1.0.0/django_alive/tests/test_views.py
@@ -0,0 +1,39 @@
+import json
+
+from django.test import TestCase
+from django.urls import reverse
+
+from .. import checks
+
+try:
+    from unittest.mock import patch
+except ImportError:
+    from mock import patch
+
+
+class ViewTestCase(TestCase):
+    def test_liveness(self):
+        response = self.client.get(reverse("alive_alive"))
+        self.assertEqual(response.status_code, 200)
+        self.assertEqual(response.content.decode("utf8"), "ok")
+
+    def test_healthcheck(self):
+        response = self.client.get(reverse("alive_health"))
+        self.assertEqual(response.status_code, 200)
+        self.assertEqual(json.loads(response.content.decode("utf8")), {"healthy": True})
+
+    def test_healtcheck_failed(self):
+        err_msg = "database failed"
+
+        def bad_database_check(*args, **kwargs):
+            raise checks.HealthcheckFailure(err_msg)
+
+        with patch(
+            "django_alive.checks.check_database", side_effect=bad_database_check
+        ):
+            response = self.client.get(reverse("alive_health"))
+        self.assertEqual(response.status_code, 503)
+        self.assertEqual(
+            json.loads(response.content.decode("utf8")),
+            {"healthy": False, "errors": [err_msg]},
+        )
