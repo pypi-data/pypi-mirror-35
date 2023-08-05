45fbdcadc8b456b259f690bdb2c1eba392daac76
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-05 20:13:06 +0000

    Add package imagerotation/1.0.6/imagerotation-1.0.6.tar.gz

---
+++ packages/imagerotation/imagerotation-1.0.6.tar.gz/imagerotation-1.0.6/imagerotation/dicomutils.py
@@ -0,0 +1,41 @@
+"""
+Rotate dicom image
+"""
+
+import SimpleITK as sitk
+import imutils
+import os
+import sys
+import numpy as np
+import cv2
+
+__author__ = "Dibya Raj Ghosh"
+
+def rotateimage(inputDirectory,outputDiretory,degOfRotation):
+    """
+    Input-Dicom image
+    Output-Dicom image
+    It will rotate image by a specific angle provided by user
+    This function will accept 3 mandatory parameter- source folder, destination folder, 
+    rotation-angle(positive  value means -anticlowise and negative value means clockwise rotation)
+    """
+    dcmList=os.listdir(inputDirectory)
+    for imgName in dcmList:
+        path=inputDirectory+'\\'+imgName
+        dcm = sitk.ReadImage(path)
+        array = sitk.GetArrayFromImage(dcm)
+        imgarray = array[0] #sitk reads image as volume adding z-dimension
+        rotate = imutils.rotate(imgarray,int(degOfRotation))
+        filtered_image = sitk.GetImageFromArray(rotate)
+        for key in dcm.GetMetaDataKeys():
+            filtered_image.SetMetaData(key, dcm.GetMetaData(key))
+
+        filtered_image.SetSpacing(dcm.GetSpacing())
+        writer = sitk.ImageFileWriter()
+        writer.KeepOriginalImageUIDOn()
+        #writer
+        writer.SetFileName(outputDiretory+'//'+imgName)
+        writer.Execute(filtered_image)
+        print(imgName+' saved successfully')
+
+
