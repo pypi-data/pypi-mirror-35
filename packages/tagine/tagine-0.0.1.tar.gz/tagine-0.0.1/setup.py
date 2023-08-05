8e6303d91a286278b4b9f5e86eca7383fec63067
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-05 15:04:11 +0000

    Add package tagine/0.0.1/tagine-0.0.1.tar.gz

---
+++ packages/tagine/tagine-0.0.1.tar.gz/tagine-0.0.1/setup.py
@@ -0,0 +1,26 @@
+#! /usr/bin/env python
+# -*- coding: utf-8 -*_
+# Author: Liu Yang <mkliuyang@gmail.com>
+
+from distutils.core import setup
+
+setup(
+    name='tagine',
+    version='0.0.1',
+    description='Python Distribution Utilities',
+    author='Liu Yang',
+    author_email='mkliuyang@gmail.com',
+    url='https://gitlab.yunfutech.com/nlp_projects/Tagine',
+    classifiers=[
+        'Development Status :: 4 - Beta',
+        'Operating System :: OS Independent',
+        'Intended Audience :: Developers',
+        'License :: OSI Approved :: BSD License',
+        'Programming Language :: Python',
+        'Programming Language :: Python :: 3',
+        'Programming Language :: Python :: 3.4',
+        'Programming Language :: Python :: 3.5',
+        'Programming Language :: Python :: 3.6',
+        'Topic :: Software Development :: Libraries'
+    ],
+)
