3455fe6a7089ef3ee8c64652bd34b73034f4e9a9
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-05 15:26:44 +0000

    Add package eqi-analysis/1.0.0/eqi-analysis-1.0.0.tar.gz

---
+++ packages/eqi-analysis/eqi-analysis-1.0.0.tar.gz/eqi-analysis-1.0.0/eqi_analysis/core/__init__.py
@@ -0,0 +1,6 @@
+"""EQI Factor Analysis Core Module."""
+
+__all__ = ['analysis', 'extension', 'refdata']
+
+
+from .extension import EQIDF
---
+++ packages/eqi-analysis/eqi-analysis-1.0.0.tar.gz/eqi-analysis-1.0.0/setup.py
@@ -0,0 +1,31 @@
+#!/usr/bin/env python
+
+from setuptools import setup, find_packages
+import versioneer
+
+install_requires = [
+    'eqi-utils>=1.0.1',
+    'cx_Oracle>=6.0b2',
+    'sqlalchemy>=1.2.8',
+    'alphalens-eqi>=1.0.0',
+    ''
+]
+
+setup(
+    name='eqi-analysis',
+    version=versioneer.get_version(),
+    cmdclass=versioneer.get_cmdclass(),
+    description='Factor analysis package for EQI project.',
+    long_description=open('README.rst').read(),
+    author='Jinpeng Zhang',
+    author_email='jinzha098718@gmail.com',
+    url='https://github.com/jinzha098718/eqi-analysis',
+    license='MIT',
+    classifiers=[
+        "Programming Language :: Python :: 3.6",
+        "License :: OSI Approved :: MIT License",
+        "Operating System :: OS Independent"
+    ],
+    packages=find_packages(include='eqi_analysis.*'),
+    install_requires=install_requires
+)
