1e9ecbe3fbc51c4933ec87b51417646aef81c297
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-05 15:04:32 +0000

    Add package bddrest/1.15.0b0/bddrest-1.15.0b0.tar.gz

---
+++ packages/bddrest/bddrest-1.15.0b0.tar.gz/bddrest-1.15.0b0/bddrest/__init__.py
@@ -0,0 +1,11 @@
+
+from .headerset import HeaderSet
+from .response import HTTPStatus, Response
+from .specification import Call, FirstCall, AlteredCall
+from .authoring import Given, when, story, response, Story, Append, Update, \
+    Remove, status, given_form
+from .exceptions import InvalidUrlParametersError, CallVerifyError
+
+
+__version__ = '1.15.0b'
+
