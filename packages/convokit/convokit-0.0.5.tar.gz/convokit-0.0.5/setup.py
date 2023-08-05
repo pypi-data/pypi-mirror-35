bed511deaa3b3b25fb07a1e5382f3f94e6ca3184
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-05 15:24:28 +0000

    Add package convokit/0.0.5/convokit-0.0.5.tar.gz

---
+++ packages/convokit/convokit-0.0.5.tar.gz/convokit-0.0.5/setup.py
@@ -0,0 +1,23 @@
+from setuptools import setup
+
+setup(
+    name = "convokit",
+    author = "Cristian Danescu-Niculescu-Mizil, Andrew Wang",
+    author_email = "azw7@cornell.edu",
+    url = "https://github.com/CornellNLP/Cornell-Conversational-Analysis-Toolkit",
+    description = "Cornell Conversational Analysis Toolkit",
+    version = "0.0.5",
+    packages = ["convokit"],
+    package_data = {"convokit": ["data/*.txt"]},
+    entry_points = {
+        "console_scripts": ["convokit = convokit.command_line:command_line_main"]
+    },
+    install_requires = [
+        "matplotlib>=1.5.0",
+        "pandas>=0.20.3",
+        "spacy>=2.0.11",
+        "scipy>=0.16.0",
+        "scikit-learn>=0.16.1",
+        "nltk>=3.3"
+    ]
+)
