7a98fd56e2d10e8c42fd1ab9feb4769c7b244033
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-05 16:58:01 +0000

    Add package kamonohashi-sdk/0.3.3/kamonohashi-sdk-0.3.3.tar.gz

---
+++ packages/kamonohashi-sdk/kamonohashi-sdk-0.3.3.tar.gz/kamonohashi-sdk-0.3.3/kamonohashi/api/__init__.py
@@ -0,0 +1,13 @@
+from __future__ import absolute_import
+
+# flake8: noqa
+
+# import apis into api package
+from kamonohashi.api.account_api import AccountApi
+from kamonohashi.api.cluster_api import ClusterApi
+from kamonohashi.api.data_api import DataApi
+from kamonohashi.api.data_set_api import DataSetApi
+from kamonohashi.api.job_api import JobApi
+from kamonohashi.api.preprocessing_api import PreprocessingApi
+from kamonohashi.api.registry_api import RegistryApi
+from kamonohashi.api.storage_api import StorageApi
