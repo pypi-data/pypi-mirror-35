8d1adbda64d3b0691bfd6275501fe493bf3412f2
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-05 15:05:30 +0000

    Add package asyncwebsockets/0.3.0/asyncwebsockets-0.3.0.tar.gz

---
+++ packages/asyncwebsockets/asyncwebsockets-0.3.0.tar.gz/asyncwebsockets-0.3.0/README.rst
@@ -0,0 +1,38 @@
+asyncwebsockets
+=======
+
+asyncwebsockets is a `curio`_ + `trio`_ compatible library for connecting and serving websockets.
+
+
+Installation
+------------
+
+To install the latest stable version::
+
+    $ pip install asyncwebsockets
+
+To install the latest development version::
+
+    $ pip install git+https://github.com/Fuyukai/asyncwebsockets.git#egg=asyncwebsockets
+
+
+Basic Usage
+-----------
+
+.. code-block:: python3
+
+    import multio
+    import asyncwebsockets
+
+    async def test():
+        async with asyncwebsockets.open_websocket("wss://echo.websocket.org") as ws:
+            await ws.send("test")
+            evt = await ws.next_event()
+            print(type(evt), getattr(evt, 'data', None))
+
+
+    multio.init("curio")
+    multio.run(test)
+
+.. _curio: https://curio.readthedocs.io/en/latest/
+.. _trio: https://trio.readthedocs.io/en/latest/
---
+++ packages/asyncwebsockets/asyncwebsockets-0.3.0.tar.gz/asyncwebsockets-0.3.0/setup.py
@@ -0,0 +1,37 @@
+import sys
+from pathlib import Path
+
+from setuptools import setup
+
+if sys.version_info[0:2] < (3, 6):
+    raise RuntimeError("This package requires Python 3.6+.")
+
+setup(
+    name="asyncwebsockets",
+    use_scm_version={
+        "version_scheme": "guess-next-dev",
+        "local_scheme": "dirty-tag"
+    },
+    packages=[
+        "asyncwebsockets",
+        "asyncwebsockets._specific"
+    ],
+    url="https://github.com/Fuyukai/asyncwebsockets",
+    license="MIT",
+    author="Laura Dickinson",
+    author_email="l@veriny.tf",
+    description="A websocket library for curio + trio",
+    long_description=Path(__file__).with_name("README.rst").read_text(encoding="utf-8"),
+    setup_requires=[
+        "setuptools_scm",
+        "pytest-runner"
+    ],
+    install_requires=[
+        "multio",
+        "wsproto>=0.11.0",
+        "async_generator",
+        "yarl"
+    ],
+    extras_require={},
+    python_requires=">=3.6",
+)
