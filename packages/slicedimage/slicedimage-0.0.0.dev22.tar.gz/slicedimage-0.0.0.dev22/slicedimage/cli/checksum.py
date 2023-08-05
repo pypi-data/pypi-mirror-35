b046975d399b7a9e729d6a2988a04642a3f2df2e
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-05 15:31:27 +0000

    Add package slicedimage/0.0.0.dev22/slicedimage-0.0.0.dev22.tar.gz

---
+++ packages/slicedimage/slicedimage-0.0.0.dev22.tar.gz/slicedimage-0.0.0.dev22/slicedimage/cli/checksum.py
@@ -0,0 +1,55 @@
+from __future__ import absolute_import, division, print_function, unicode_literals
+
+from slicedimage import Reader, Writer
+from slicedimage.io import resolve_path_or_url
+from ._base import CliCommand
+
+
+class ChecksumCommand(CliCommand):
+    @classmethod
+    def register_parser(cls, subparser_root):
+        checksum_command = subparser_root.add_parser(
+            "checksum",
+            help="Read a partition file and add missing checksums.")
+        checksum_command.add_argument(
+            "in_url",
+            help="URL for the source partition file")
+        checksum_command.add_argument(
+            "out_path",
+            help="Path to write partition file with checksums")
+        checksum_command.add_argument(
+            "--pretty",
+            action="store_true",
+            help="Pretty-print the output file")
+
+        return checksum_command
+
+    @classmethod
+    def run_command(cls, args):
+        _, name, baseurl = resolve_path_or_url(args.in_url)
+        slicedimage = Reader.parse_doc(name, baseurl)
+
+        Writer.write_to_path(
+            slicedimage,
+            args.out_path,
+            pretty=args.pretty,
+            tile_opener=fake_file_opener,
+            tile_writer=null_writer)
+
+
+def fake_file_opener(partition_path, tile, ext):
+    class fake_handle(object):
+        def __init__(self, name):
+            self.name = name
+
+        def __enter__(self):
+            return self
+
+        def __exit__(self, exc_type, exc_val, exc_tb):
+            pass
+
+    return fake_handle(tile._file_or_url)
+
+
+def null_writer(tile, fh):
+    pass
---
+++ packages/slicedimage/slicedimage-0.0.0.dev22.tar.gz/slicedimage-0.0.0.dev22/tests/io/v0_0_0/test_write.py
@@ -0,0 +1,124 @@
+import codecs
+import json
+import numpy
+import os
+import sys
+import tempfile
+import unittest
+
+
+pkg_root = os.path.abspath(os.path.join(os.path.dirname(__file__), "..", "..", ".."))  # noqa
+sys.path.insert(0, pkg_root)  # noqa
+
+
+import slicedimage
+from tests.utils import TemporaryDirectory
+
+
+baseurl = "file://{}".format(os.path.abspath(os.path.dirname(__file__)))
+
+
+class TestWrite(unittest.TestCase):
+    def test_write_tileset(self):
+        image = slicedimage.TileSet(
+            ["x", "y", "ch", "hyb"],
+            {'ch': 2, 'hyb': 2},
+            (100, 100),
+        )
+
+        for hyb in range(2):
+            for ch in range(2):
+                tile = slicedimage.Tile(
+                    {
+                        'x': (0.0, 0.01),
+                        'y': (0.0, 0.01),
+                    },
+                    {
+                        'hyb': hyb,
+                        'ch': ch,
+                    },
+                )
+                tile.numpy_array = numpy.zeros((100, 100))
+                tile.numpy_array[hyb, ch] = 1
+                image.add_tile(tile)
+
+        with TemporaryDirectory() as tempdir, \
+                tempfile.NamedTemporaryFile(suffix=".json", dir=tempdir) as partition_file:
+            partition_doc = slicedimage.v0_0_0.Writer().generate_partition_document(
+                image, partition_file.name)
+            writer = codecs.getwriter("utf-8")
+            json.dump(partition_doc, writer(partition_file))
+            partition_file.flush()
+
+            basename = os.path.basename(partition_file.name)
+            baseurl = "file://{}".format(os.path.dirname(partition_file.name))
+
+            loaded = slicedimage.Reader.parse_doc(basename, baseurl)
+
+            for hyb in range(2):
+                for ch in range(2):
+                    tiles = [_tile
+                             for _tile in loaded.tiles(
+                                 lambda tile: (tile.indices['hyb'] == hyb and
+                                               tile.indices['ch'] == ch))]
+
+                    self.assertEqual(len(tiles), 1)
+
+                    expected = numpy.zeros((100, 100))
+                    expected[hyb, ch] = 1
+
+                    self.assertEqual(tiles[0].numpy_array.all(), expected.all())
+                    self.assertIsNotNone(tiles[0].sha256)
+
+    def test_write_collection(self):
+        image = slicedimage.TileSet(
+            ["x", "y", "ch", "hyb"],
+            {'ch': 2, 'hyb': 2},
+            (100, 100),
+        )
+
+        for hyb in range(2):
+            for ch in range(2):
+                tile = slicedimage.Tile(
+                    {
+                        'x': (0.0, 0.01),
+                        'y': (0.0, 0.01),
+                    },
+                    {
+                        'hyb': hyb,
+                        'ch': ch,
+                    },
+                )
+                tile.numpy_array = numpy.zeros((100, 100))
+                tile.numpy_array[hyb, ch] = 1
+                image.add_tile(tile)
+        collection = slicedimage.Collection()
+        collection.add_partition("fov002", image)
+
+        with TemporaryDirectory() as tempdir, \
+                tempfile.NamedTemporaryFile(suffix=".json", dir=tempdir) as partition_file:
+            partition_doc = slicedimage.v0_0_0.Writer().generate_partition_document(
+                collection, partition_file.name)
+            writer = codecs.getwriter("utf-8")
+            json.dump(partition_doc, writer(partition_file))
+            partition_file.flush()
+
+            basename = os.path.basename(partition_file.name)
+            baseurl = "file://{}".format(os.path.dirname(partition_file.name))
+
+            loaded = slicedimage.Reader.parse_doc(basename, baseurl)
+
+            for hyb in range(2):
+                for ch in range(2):
+                    tiles = [_tile
+                             for _tile in loaded.tiles(
+                                 lambda tile: (tile.indices['hyb'] == hyb and
+                                               tile.indices['ch'] == ch))]
+
+                    self.assertEqual(len(tiles), 1)
+
+                    expected = numpy.zeros((100, 100))
+                    expected[hyb, ch] = 1
+
+                    self.assertEqual(tiles[0].numpy_array.all(), expected.all())
+                    self.assertIsNotNone(tiles[0].sha256)
