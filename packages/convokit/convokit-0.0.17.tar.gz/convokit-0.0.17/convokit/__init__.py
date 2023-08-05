c6d934010dc03654fe2db7255b5ff53d8974c753
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-05 15:24:52 +0000

    Add package convokit/0.0.17/convokit-0.0.17.tar.gz

---
+++ packages/convokit/convokit-0.0.17.tar.gz/convokit-0.0.17/convokit/__init__.py
@@ -0,0 +1,6 @@
+from .model import *
+from .coordination import *
+from .util import *
+from .questionTypology import *
+from .politenessStrategies import *
+#__path__ = __import__('pkgutil').extend_path(__path__, __name__)
