0cf235092c6344840a6e16957fff264b6553e627
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-05 15:29:35 +0000

    Add package marmee/0.2.1/marmee-0.2.1.tar.gz

---
+++ packages/marmee/marmee-0.2.1.tar.gz/marmee-0.2.1/setup.py
@@ -0,0 +1,68 @@
+#!/usr/bin/env python
+# -*- coding: utf-8 -*-
+
+"""The setup script."""
+
+from setuptools import setup, find_packages
+
+with open('README.rst') as readme_file:
+    readme = readme_file.read()
+
+with open('HISTORY.rst') as history_file:
+    history = history_file.read()
+
+requirements = [
+    # TODO: Put package requirements here
+    'pendulum',
+    'earthengine-api',
+    'marshmallow==3.0.0b11',
+    'marshmallow-oneofschema>=master',
+    'stacpy'
+]
+
+deps_links = [
+    'git+https://github.com/francbartoli/marshmallow-oneofschema.git@master#egg=marshmallow-oneofschema'
+]
+
+setup_requirements = [
+    'pytest-runner',
+    # TODO(francbartoli): Put setup requirements (distutils extensions, etc.) here
+]
+
+test_requirements = [
+    'pytest',
+    # TODO: Put package test requirements here
+]
+
+setup(
+    name='marmee',
+    version='0.2.1',
+    description="Execute safely Google Earth Engine algorithm through\
+a tunnel like a marmot",
+    long_description=readme + '\n\n' + history,
+    author="Francesco Bartoli",
+    author_email='francesco.bartoli@geobeyond.it',
+    url='https://github.com/francbartoli/marmee',
+    packages=find_packages(include=['marmee']),
+    include_package_data=True,
+    install_requires=requirements,
+    dependency_links=deps_links,
+    license="MIT license",
+    zip_safe=False,
+    keywords='marmee',
+    classifiers=[
+        'Development Status :: 2 - Pre-Alpha',
+        'Intended Audience :: Developers',
+        'License :: OSI Approved :: MIT License',
+        'Natural Language :: English',
+        "Programming Language :: Python :: 2",
+        'Programming Language :: Python :: 2.7',
+        'Programming Language :: Python :: 3',
+        'Programming Language :: Python :: 3.4',
+        'Programming Language :: Python :: 3.5',
+        'Programming Language :: Python :: 3.6',
+    ],
+    test_suite='tests',
+    tests_require=test_requirements,
+    setup_requires=setup_requirements,
+)
