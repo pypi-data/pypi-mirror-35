4274ffb6090bc88b41671f81a7232611e72fe12d
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-05 16:56:00 +0000

    Add package anyioc/0.1.0/anyioc-0.1.0-py3.7.egg

---
+++ packages/anyioc/anyioc-0.1.0-py3.7.egg/anyioc/utils.py
@@ -0,0 +1,26 @@
+# -*- coding: utf-8 -*-
+#
+# Copyright (c) 2018~2999 - Cologler <skyoflw@gmail.com>
+# ----------
+#
+# ----------
+
+from inspect import signature, Parameter
+
+def auto_inject(func):
+    '''
+    wrap the func and auto inject by parameter name.
+
+    var keyword parameter and var positional parameter will not be inject.
+    '''
+    sign = signature(func)
+    names = [p.name for p in sign.parameters.values() if p.kind in (
+        Parameter.POSITIONAL_OR_KEYWORD,
+        Parameter.KEYWORD_ONLY
+    )]
+    def new_func(ioc):
+        kwargs = {}
+        for name in names:
+            kwargs[name] = ioc.get(name)
+        return func(**kwargs)
+    return new_func
