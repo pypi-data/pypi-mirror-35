695457480fe4f7033f1ad95f0d096298dc297e28
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-05 15:24:57 +0000

    Add package ypconnector/1.1.2/ypconnector-1.1.2.tar.gz

---
+++ packages/ypconnector/ypconnector-1.1.2.tar.gz/ypconnector-1.1.2/setup.py
@@ -0,0 +1,22 @@
+import setuptools
+
+with open("README.md", "r") as fh:
+    long_description = fh.read()
+
+setuptools.setup(
+    name="ypconnector",
+    version="1.1.2",
+    author="YappesLib",
+    author_email="info@yappes.com",
+    description="Python SDK for integrating APIs published via Yappes platform",
+    long_description=long_description,
+    long_description_content_type="text/markdown",
+    url="https://github.com/yappes-technologies/ypconnector-python",
+    packages=setuptools.find_packages(),
+    install_requires=["requests"],
+    classifiers=[
+        "Programming Language :: Python :: 3",
+        "License :: OSI Approved :: MIT License",
+        "Operating System :: OS Independent"
+    ]
+)
