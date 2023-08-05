087f44e24b49c6e5769dbdda2e4138fe120eaa28
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-05 17:05:59 +0000

    Add package negima/0.1.3/negima-0.1.3-py3-none-any.whl

---
+++ packages/negima/negima-0.1.3-py3-none-any.whl/negima/__init__.py
@@ -0,0 +1,16 @@
+'''
+=================================
+Pythonのメソッドと引数
+=================================
+
+:Author: cocodrips
+:Created: 2018/07/16
+:Tags: python 
+:Ripositly: https://github.com/cocodrips/negima
+
+:abstract:
+'''
+name = "negima"
+
+from .parser import Morpheme, Parser
+from .merger import NormType, MorphemeMerger
