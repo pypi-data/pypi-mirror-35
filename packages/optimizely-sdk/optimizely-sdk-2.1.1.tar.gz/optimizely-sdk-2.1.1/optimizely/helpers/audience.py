32f43612a317f2fe17f8dab0c2adfe32eb437088
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-05 20:11:10 +0000

    Add package optimizely-sdk/2.1.1/optimizely-sdk-2.1.1.tar.gz

---
+++ packages/optimizely-sdk/optimizely-sdk-2.1.1.tar.gz/optimizely-sdk-2.1.1/optimizely/helpers/audience.py
@@ -0,0 +1,58 @@
+# Copyright 2016, Optimizely
+# Licensed under the Apache License, Version 2.0 (the "License");
+# you may not use this file except in compliance with the License.
+# You may obtain a copy of the License at
+#
+# http://www.apache.org/licenses/LICENSE-2.0
+#
+# Unless required by applicable law or agreed to in writing, software
+# distributed under the License is distributed on an "AS IS" BASIS,
+# WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
+# See the License for the specific language governing permissions and
+# limitations under the License.
+
+from . import condition as condition_helper
+
+
+def is_match(audience, attributes):
+  """ Given audience information and user attributes determine if user meets the conditions.
+
+  Args:
+    audience: Dict representing the audience.
+    attributes: Dict representing user attributes which will be used in determining if the audience conditions are met.
+
+  Return:
+    Boolean representing if user satisfies audience conditions or not.
+  """
+  condition_evaluator = condition_helper.ConditionEvaluator(audience.conditionList, attributes)
+  return condition_evaluator.evaluate(audience.conditionStructure)
+
+
+def is_user_in_experiment(config, experiment, attributes):
+  """ Determine for given experiment if user satisfies the audiences for the experiment.
+
+  Args:
+    config: project_config.ProjectConfig object representing the project.
+    experiment: Object representing the experiment.
+    attributes: Dict representing user attributes which will be used in determining if the audience conditions are met.
+
+  Returns:
+    Boolean representing if user satisfies audience conditions for any of the audiences or not.
+  """
+
+  # Return True in case there are no audiences
+  if not experiment.audienceIds:
+    return True
+
+  # Return False if there are audiences, but no attributes
+  if not attributes:
+    return False
+
+  # Return True if conditions for any one audience are met
+  for audience_id in experiment.audienceIds:
+    audience = config.get_audience(audience_id)
+
+    if is_match(audience, attributes):
+      return True
+
+  return False
---
+++ packages/optimizely-sdk/optimizely-sdk-2.1.1.tar.gz/optimizely-sdk-2.1.1/optimizely/helpers/enums.py
@@ -0,0 +1,67 @@
+# Copyright 2016-2018, Optimizely
+# Licensed under the Apache License, Version 2.0 (the "License");
+# you may not use this file except in compliance with the License.
+# You may obtain a copy of the License at
+#
+# http://www.apache.org/licenses/LICENSE-2.0
+#
+# Unless required by applicable law or agreed to in writing, software
+# distributed under the License is distributed on an "AS IS" BASIS,
+# WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
+# See the License for the specific language governing permissions and
+# limitations under the License.
+
+import logging
+
+
+class HTTPVerbs(object):
+  GET = 'GET'
+  POST = 'POST'
+
+
+class LogLevels(object):
+  NOTSET = logging.NOTSET
+  DEBUG = logging.DEBUG
+  INFO = logging.INFO
+  WARNING = logging.WARNING
+  ERROR = logging.ERROR
+  CRITICAL = logging.CRITICAL
+
+
+class Errors(object):
+  INVALID_ATTRIBUTE_ERROR = 'Provided attribute is not in datafile.'
+  INVALID_ATTRIBUTE_FORMAT = 'Attributes provided are in an invalid format.'
+  INVALID_AUDIENCE_ERROR = 'Provided audience is not in datafile.'
+  INVALID_DATAFILE = 'Datafile has invalid format. Failing "{}".'
+  INVALID_EVENT_TAG_FORMAT = 'Event tags provided are in an invalid format.'
+  INVALID_EXPERIMENT_KEY_ERROR = 'Provided experiment is not in datafile.'
+  INVALID_EVENT_KEY_ERROR = 'Provided event is not in datafile.'
+  INVALID_FEATURE_KEY_ERROR = 'Provided feature key is not in the datafile.'
+  INVALID_GROUP_ID_ERROR = 'Provided group is not in datafile.'
+  INVALID_INPUT_ERROR = 'Provided "{}" is in an invalid format.'
+  INVALID_VARIATION_ERROR = 'Provided variation is not in datafile.'
+  INVALID_VARIABLE_KEY_ERROR = 'Provided variable key is not in the feature flag.'
+  NONE_FEATURE_KEY_PARAMETER = '"None" is an invalid value for feature key.'
+  NONE_USER_ID_PARAMETER = '"None" is an invalid value for user ID.'
+  NONE_VARIABLE_KEY_PARAMETER = '"None" is an invalid value for variable key.'
+  UNSUPPORTED_DATAFILE_VERSION = 'Provided datafile has unsupported version. ' \
+                                 'Please use SDK version 1.1.0 or earlier for datafile version 1.'
+
+
+class NotificationTypes(object):
+  """ NotificationTypes for the notification_center.NotificationCenter
+      format is NOTIFICATION TYPE: list of parameters to callback.
+
+      ACTIVATE notification listener has the following parameters:
+      Experiment experiment, str user_id, dict attributes (can be None), Variation variation, Event event
+      TRACK notification listener has the following parameters:
+      str event_key, str user_id, dict attributes (can be None), event_tags (can be None), Event event
+  """
+  ACTIVATE = "ACTIVATE:experiment, user_id, attributes, variation, event"
+  TRACK = "TRACK:event_key, user_id, attributes, event_tags, event"
+
+
+class ControlAttributes(object):
+  BOT_FILTERING = '$opt_bot_filtering'
+  BUCKETING_ID = '$opt_bucketing_id'
+  USER_AGENT = '$opt_user_agent'
