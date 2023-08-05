86a09d0762bb48432b15219a9ce522bac003725c
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-05 16:58:12 +0000

    Add package somosmas-common/1.0.2/somosmas_common-1.0.2.tar.gz

---
+++ packages/somosmas-common/somosmas_common-1.0.2.tar.gz/somosmas_common-1.0.2/setup.py
@@ -0,0 +1,21 @@
+#!/usr/bin/env python
+
+from distutils.core import setup
+
+from setuptools import find_packages
+
+setup(
+    name='somosmas_common',
+    version='1.0.2',
+    description=(
+        'Package for the software components that are common for the S+ projects',  # noqa
+    ),
+    author='Omar Diaz',
+    author_email='zcool2005@gmail.com',
+    packages=find_packages(),
+    include_package_data=True,
+    install_requires=[
+      'requests',
+    ],
+    zip_safe=False,
+)
