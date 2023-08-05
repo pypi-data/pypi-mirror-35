0d8f0c2ea0df80ec85a3538d562c1e7d519f617f
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-05 16:55:19 +0000

    Add package botworks/0.0.2/botworks-0.0.2.tar.gz

---
+++ packages/botworks/botworks-0.0.2.tar.gz/botworks-0.0.2/setup.py
@@ -0,0 +1,22 @@
+import setuptools
+
+with open("README.md", "r") as fh:
+    long_description = fh.read()
+
+setuptools.setup(
+    name="botworks",
+    version="0.0.2",
+    author="William Hanson",
+    author_email="42045551+doubleyuhtee@users.noreply.github.com",
+    description="Slack bot framework",
+    long_description=long_description,
+    long_description_content_type="text/markdown",
+    url="https://github.com/doubleyuhtee/botworks",
+    packages=setuptools.find_packages(),
+    classifiers=(
+        "Programming Language :: Python :: 3",
+        "License :: OSI Approved :: MIT License",
+        "Operating System :: OS Independent",
+        "Development Status :: 4 - Beta"
+    ),
+)
