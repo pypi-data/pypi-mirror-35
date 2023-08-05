e22eb591e0118d6dae2d27200293b92e8ecb1cbb
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-05 17:06:14 +0000

    Add package angstrom/0.0.4/angstrom-0.0.4.tar.gz

---
+++ packages/angstrom/angstrom-0.0.4.tar.gz/angstrom-0.0.4/angstrom/__init__.py
@@ -0,0 +1,6 @@
+__version__ = "0.0.4"
+
+from angstrom.angstrom import mobj
+from angstrom.angstrom import base_connector
+from angstrom.angstrom import file_system_sql_loader
+from angstrom.angstrom import Db
---
+++ packages/angstrom/angstrom-0.0.4.tar.gz/angstrom-0.0.4/setup.py
@@ -0,0 +1,10 @@
+import setuptools
+from distutils.core import setup
+
+setup(name="angstrom",
+      version="0.0.4",
+      description="Tiny ORM for SQLite3",
+      author="sighphyr3",
+      author_email="liquidwicked64@gmail.com",
+      packages=setuptools.find_packages()
+     )
