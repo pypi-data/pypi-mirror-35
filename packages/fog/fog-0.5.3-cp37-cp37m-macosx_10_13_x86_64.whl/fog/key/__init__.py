017f3e505cce046eee674e5467f9da544cb8cea6
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-05 21:09:01 +0000

    Add package fog/0.5.3/fog-0.5.3-cp37-cp37m-macosx_10_13_x86_64.whl

---
+++ packages/fog/fog-0.5.3-cp37-cp37m-macosx_10_13_x86_64.whl/fog/key/__init__.py
@@ -0,0 +1,8 @@
+from fog.key.fingerprint import (
+    create_fingerprint,
+    create_ngrams_fingerprint,
+    fingerprint,
+    ngrams_fingerprint
+)
+from fog.key.omission import omission_key
+from fog.key.skeleton import skeleton_key
