4210feed902649d9bbc83d31f28603f63f1d9a57
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-05 17:05:03 +0000

    Add package dumb_nester/1.1.0/dumb_nester-1.1.0.tar.gz

---
+++ packages/dumb_nester/dumb_nester-1.1.0.tar.gz/dumb_nester-1.1.0/setup.py
@@ -0,0 +1,9 @@
+from distutils.core import setup
+
+setup(
+    name = 'dumb_nester',
+    version = '1.1.0',
+    py_modules = ['dumb_nester'],
+    author = 'aditi_ahuja',
+    description = 'Asimple printer of nested lists',
+)
