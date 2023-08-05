05415794f6132b19cc04c9ca2c5c67c63b789c1a
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-05 15:25:40 +0000

    Add package cn.xyz.versionChangeV3/0.2/cn.xyz.versionChangeV3-0.2-py2.py3-none-any.whl

---
+++ packages/cn.xyz.versionChangeV3/cn.xyz.versionChangeV3-0.2-py2.py3-none-any.whl/cn/xyz/versionChange/__init__.py
@@ -0,0 +1,15 @@
+#! /home/python35/bin/python
+"""
+-------------------------------------------------
+   File Name：     __init__
+   Description :
+   Author :       lvchenggang
+   date：          2018/8/15
+-------------------------------------------------
+   Change Activity:
+                   2018/8/15
+-------------------------------------------------
+"""
+__author__ = 'lvchenggang'
+
+__all__=['versionChange']
