de2dacecb65676ad687c9b2d67b3ac93ffa26706
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-05 15:05:55 +0000

    Add package scrapy-fieldstats/0.1.8/scrapy-fieldstats-0.1.8.tar.gz

---
+++ packages/scrapy-fieldstats/scrapy-fieldstats-0.1.8.tar.gz/scrapy-fieldstats-0.1.8/setup.py
@@ -0,0 +1,39 @@
+#!/usr/bin/env python
+# -*- coding: utf-8 -*-
+from setuptools import setup, find_packages
+
+requirements = []
+
+setup_requirements = [
+    'pytest-runner',
+]
+
+test_requirements = [
+    'pytest',
+]
+
+setup(
+    name='scrapy-fieldstats',
+    version='0.1.8',
+    description="A Scrapy extension to generate a summary of fields coverage from your scraped data.",
+    author="Valdir Stumm Junior",
+    author_email='stummjr@gmail.com',
+    url='https://github.com/stummjr/scrapy-fieldstats',
+    packages=find_packages(include=['scrapy_fieldstats']),
+    include_package_data=True,
+    install_requires=requirements,
+    license="MIT license",
+    zip_safe=False,
+    keywords='scrapy fields stats',
+    classifiers=[
+        'Development Status :: 2 - Pre-Alpha',
+        'Intended Audience :: Developers',
+        'License :: OSI Approved :: MIT License',
+        'Natural Language :: English',
+        'Programming Language :: Python :: 2.7',
+        'Programming Language :: Python :: 3.5',
+    ],
+    test_suite='tests',
+    tests_require=test_requirements,
+    setup_requires=setup_requirements,
+)
---
+++ packages/scrapy-fieldstats/scrapy-fieldstats-0.1.8.tar.gz/scrapy-fieldstats-0.1.8/tests/test_scrapy_fieldstats.py
@@ -0,0 +1,199 @@
+# -*- coding: utf-8 -*-
+from scrapy_fieldstats.fieldstats import FieldStatsExtension
+
+
+def extract_fake_items_and_compute_stats(fake_items, show_counts=False, skip_none=True):
+    ext = FieldStatsExtension(skip_none=skip_none)
+    for item in fake_items:
+        ext.compute_item(item)
+
+    if show_counts:
+        return ext.field_counts
+    return ext.build_fields_summary()
+
+
+def test_single_item():
+    fake_items = [{"field1": "value1"}]
+
+    field_stats = extract_fake_items_and_compute_stats(fake_items)
+    assert len(field_stats) == 1
+    assert field_stats['field1'] == '100.0%'
+
+    field_stats = extract_fake_items_and_compute_stats(fake_items, show_counts=True)
+    assert len(field_stats) == 1
+    assert field_stats['field1'] == 1
+
+
+def test_single_item_many_fields():
+    fake_items = [
+        {
+            "field1": "value1",
+            "field2": "value2",
+        }
+    ]
+
+    field_stats = extract_fake_items_and_compute_stats(fake_items)
+    assert len(field_stats) == 2
+    assert field_stats['field1'] == '100.0%'
+    assert field_stats['field2'] == '100.0%'
+
+    field_stats = extract_fake_items_and_compute_stats(fake_items, show_counts=True)
+    assert len(field_stats) == 2
+    assert field_stats['field1'] == 1
+    assert field_stats['field2'] == 1
+
+
+def test_many_items():
+    fake_items = [{"field1": "value1"}, {"field1": "value1"}]
+
+    field_stats = extract_fake_items_and_compute_stats(fake_items)
+    assert len(field_stats) == 1
+    assert field_stats['field1'] == '100.0%'
+
+    field_stats = extract_fake_items_and_compute_stats(fake_items, show_counts=True)
+    assert len(field_stats) == 1
+    assert field_stats['field1'] == 2
+
+
+def test_many_items_many_fields():
+    fake_items = [
+        {
+            "field1": "value1",
+            "field2": "value2",
+        },
+        {
+            "field1": "value1",
+            "field2": "value2",
+        }
+    ]
+
+    field_stats = extract_fake_items_and_compute_stats(fake_items)
+    assert len(field_stats) == 2
+    assert field_stats['field1'] == '100.0%'
+    assert field_stats['field2'] == '100.0%'
+
+    field_stats = extract_fake_items_and_compute_stats(fake_items, show_counts=True)
+    assert len(field_stats) == 2
+    assert field_stats['field1'] == 2
+    assert field_stats['field2'] == 2
+
+
+def test_many_items_many_fields_missing_field():
+    fake_items = [
+        {
+            "field1": "value1",
+        },
+        {
+            "field1": "value1",
+            "field2": "value2",
+        }
+    ]
+
+    field_stats = extract_fake_items_and_compute_stats(fake_items)
+    assert len(field_stats) == 2
+    assert field_stats['field1'] == '100.0%'
+    assert field_stats['field2'] == '50.0%'
+
+    field_stats = extract_fake_items_and_compute_stats(fake_items, show_counts=True)
+    assert len(field_stats) == 2
+    assert field_stats['field1'] == 2
+    assert field_stats['field2'] == 1
+
+
+def test_many_items_many_fields_empty_field():
+    fake_items = [
+        {
+            "field1": "value1",
+            "field2": "",
+        },
+        {
+            "field1": "value1",
+            "field2": "value2",
+        }
+    ]
+
+    field_stats = extract_fake_items_and_compute_stats(fake_items)
+    assert len(field_stats) == 2
+    assert field_stats['field1'] == '100.0%'
+    assert field_stats['field2'] == '100.0%'
+
+    field_stats = extract_fake_items_and_compute_stats(fake_items, show_counts=True)
+    assert len(field_stats) == 2
+    assert field_stats['field1'] == 2
+    assert field_stats['field2'] == 2
+
+
+def test_none_values():
+    fake_items = [
+        {"field1": None, "field2": ""},
+        {"field1": "value1", "field2": "value2"},
+    ]
+    field_stats = extract_fake_items_and_compute_stats(fake_items, show_counts=True)
+    assert field_stats['field1'] == 1
+    assert field_stats['field2'] == 2
+
+    field_stats = extract_fake_items_and_compute_stats(fake_items, show_counts=True, skip_none=False)
+    assert field_stats['field1'] == 2
+    assert field_stats['field2'] == 2
+
+
+def test_nested_items():
+    fake_items = [
+        {
+            "field1": "value1",
+            "field2": {
+                "field2.1": "value2.1",
+                "field2.2": "value2.2",
+                "field2.3": {
+                    "field2.3.1": "value2.3.1",
+                    "field2.3.2": "value2.3.2",
+                },
+            }
+        },
+        {
+            "field1": "value1",
+            "field2": {
+                "field2.1": "value2.1",
+                "field2.3": {
+                    "field2.3.1": "value2.3.1",
+                    "field2.3.2": "",
+                },
+                "field2.4": "value2.2",
+            }
+        }
+    ]
+
+    field_stats = extract_fake_items_and_compute_stats(fake_items)
+    assert field_stats['field1'] == '100.0%'
+    assert field_stats['field2']['field2.1'] == '100.0%'
+    assert field_stats['field2']['field2.2'] == '50.0%'
+    assert field_stats['field2']['field2.2'] == '50.0%'
+    assert field_stats['field2']['field2.3']['field2.3.2'] == '100.0%'
+
+    field_stats = extract_fake_items_and_compute_stats(fake_items, show_counts=True)
+    assert field_stats['field1'] == 2
+    assert field_stats['field2']['field2.1'] == 2
+    assert field_stats['field2']['field2.2'] == 1
+    assert field_stats['field2']['field2.2'] == 1
+    assert field_stats['field2']['field2.3']['field2.3.2'] == 2
+
+
+def test_items_false_values():
+    fake_items = [
+        {
+            "field1": False,
+            "field2": 0,
+        },
+        {
+            "field1": True,
+            "field2": 1,
+        }
+    ]
+
+    field_stats = extract_fake_items_and_compute_stats(fake_items)
+    assert field_stats['field1'] == '100.0%'
+    assert field_stats['field2'] == '100.0%'
+
+    field_stats = extract_fake_items_and_compute_stats(fake_items, show_counts=True)
+    assert field_stats['field1'] == 2
+    assert field_stats['field2'] == 2
