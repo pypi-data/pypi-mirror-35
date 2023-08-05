77834d3f2d98b758258191d7badc6a99a6022259
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-05 17:05:00 +0000

    Add package tma/0.1.3/tma-0.1.3.tar.gz

---
+++ packages/tma/tma-0.1.3.tar.gz/tma-0.1.3/tma/analyst/classify.py
@@ -0,0 +1,16 @@
+# -*- coding: UTF-8 -*-
+
+"""
+analyst.classify - 对交易数据进行分类分析
+====================================================================
+"""
+
+
+class ShareTrendClassifier(object):
+    """个股走势分类器"""
+    pass
+
+
+class IndexTrendClassifier(object):
+    """指数走势分类器"""
+    pass
