9af13b724a02c1cce82c6486900a39e3b4cbfb32
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-05 15:32:02 +0000

    Add package aladdin-connect/0.3/aladdin_connect-0.3.tar.gz

---
+++ packages/aladdin-connect/aladdin_connect-0.3.tar.gz/aladdin_connect-0.3/setup.py
@@ -0,0 +1,26 @@
+from setuptools import setup
+
+with open("README.md", "r") as fh:
+    long_description = fh.read()
+
+setup(
+    name='aladdin_connect',
+    version='0.3',
+    author='Josh Shoemaker',
+    author_email='shoejosh@gmail.com',
+    url='http://github.com/shoejosh/aladdin-connect',
+    packages=['aladdin_connect'],
+    scripts=[],
+    description='Python API for controlling Genie garage doors connected to Aladdin Connect devices',
+    long_description=long_description,
+    long_description_content_type="text/markdown",
+    classifiers=[
+        'Development Status :: 4 - Beta',
+        'Intended Audience :: Developers',
+        'License :: OSI Approved :: MIT License',
+        'Operating System :: OS Independent',
+        'Programming Language :: Python',
+    ],
+    install_requires=['requests'],
+    include_package_data=True
+)
