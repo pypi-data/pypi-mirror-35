1552a3852b34b5dabaa450be722bca11076be006
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-05 16:55:03 +0000

    Add package toolspy/0.2.30/toolspy-0.2.30.tar.gz

---
+++ packages/toolspy/toolspy-0.2.30.tar.gz/toolspy-0.2.30/setup.py
@@ -0,0 +1,12 @@
+from setuptools import setup
+
+setup(
+    name='toolspy',
+    version='0.2.30',
+    description='A library of generic utility functions',
+    long_description='A python toolset',
+    packages=['toolspy'],
+    include_package_data=True,
+    license='MIT',
+    author='SuryaSankar',
+    author_email='suryashankar.m@gmail.com')
