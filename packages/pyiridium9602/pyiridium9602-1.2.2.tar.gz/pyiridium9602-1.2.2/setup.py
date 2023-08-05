6db4380ccafd246675b310305d587b57c90e2c9e
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-05 15:30:31 +0000

    Add package pyiridium9602/1.2.2/pyiridium9602-1.2.2.tar.gz

---
+++ packages/pyiridium9602/pyiridium9602-1.2.2.tar.gz/pyiridium9602-1.2.2/setup.py
@@ -0,0 +1,83 @@
+"""
+    setup.py - Setup file to distribute the library
+See Also:
+    https://github.com/pypa/sampleproject
+    https://packaging.python.org/en/latest/distributing.html
+    https://pythonhosted.org/an_example_pypi_project/setuptools.html
+"""
+import os
+
+from setuptools import setup
+
+
+def parse_requirements(filename):
+    """ load requirements from a pip requirements file """
+    lineiter = (line.strip() for line in open(filename))
+    return [line for line in lineiter if line and not line.startswith("#")]
+
+
+def read(fname):
+    """Read in a file"""
+    with open(os.path.join(os.path.dirname(__file__), fname), "r") as file:
+        return file.read()
+
+
+if __name__ == "__main__":
+    # ===== Requirements =====
+    try:
+        requirements = parse_requirements("requirements.txt")
+    except FileNotFoundError:
+        requirements = []
+    # ===== END Requirements =====
+
+    setup(
+        name="pyiridium9602",
+        version="1.2.2",
+        description="Python 3 iridium communication library for the iridium 9602 modem.",
+        url="https://github.com/justengel/pyiridium9602",
+        download_url="https://github.com/justengel/pyiridium9602/archive/v1.2.2.tar.gz",
+
+        keywords=["iridium", "9602", "iridium9602"],
+
+        author="Justin Engel",
+        author_email="jengel@sealandaire.com",
+
+        license="MIT",
+
+        platforms="any",
+        classifiers=["Programming Language :: Python",
+                     "Programming Language :: Python :: 3",
+                     "Operating System :: OS Independent"],
+
+        scripts=[],
+
+        long_description=read("README.md"),
+        packages=["pyiridium9602"],
+        install_requires=requirements,
+
+        include_package_data=False,
+
+        # package_data={
+        #     'package': ['file.dat']
+        # }
+
+        # options to install extra requirements
+        extras_require={
+            'acoustic_types': ['numpy', 'scipy'],
+        }
+
+        # Data files outside of packages
+        # data_files=[('my_data', ["data/my_data.dat"])],
+
+        # keywords='sample setuptools development'
+
+        # entry_points={
+        #     'console_scripts': [
+        #         'foo = my_package.some_module:main_func',
+        #         'bar = other_module:some_func',
+        #     ],
+        #     'gui_scripts': [
+        #         'baz = my_package_gui:start_func',
+        #     ]
+        # }
+    )
