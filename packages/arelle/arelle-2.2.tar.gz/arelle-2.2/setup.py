4ae0b1ea264365aa75c6c6809b7cd65379605575
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-05 15:30:01 +0000

    Add package arelle/2.2/arelle-2.2.tar.gz

---
+++ packages/arelle/arelle-2.2.tar.gz/arelle-2.2/setup.py
@@ -0,0 +1,14 @@
+from distutils.core import setup
+setup(
+  name = 'arelle',
+  packages = ['arelle'], # this must be the same as the name above
+  package_data={'arelle': ['config/*', 'plugin/*', 'pyparsing/*', '__pycache__/*']},
+  version = '2.2',
+  description = 'arelle xblr modified by Lendflo',
+  author = 'Remi Tuyaerts / Przemyslaw Winszczyk',
+  author_email = 'remi.tuyaerts@lendflo.com',
+  url = 'https://github.com/LendFlo/pip-arelle', # use the URL to the github repo
+  download_url = 'https://github.com/LendFlo/pip-arelle/archive/0.2.2.tar.gz', # I'll explain this in a second
+  keywords = ['arelle', 'lendflo', 'fork'], # arbitrary keywords
+  classifiers = [],
+)
