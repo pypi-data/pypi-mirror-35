ad3314d6067fe752ca53d8ad5197beb3ee41854f
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-05 21:09:34 +0000

    Add package lusid-sdk/0.6.268/lusid-sdk-0.6.268.tar.gz

---
+++ packages/lusid-sdk/lusid-sdk-0.6.268.tar.gz/lusid-sdk-0.6.268/lusid/models/__init__.py
@@ -0,0 +1,236 @@
+# coding=utf-8
+# --------------------------------------------------------------------------
+# Copyright © 2018 FINBOURNE TECHNOLOGY LTD
+#
+# Permission is hereby granted, free of charge, to any person obtaining a copy
+# of this software and associated documentation files (the "Software"), to deal
+# in the Software without restriction, including without limitation the rights
+# to use, copy, modify, merge, publish, distribute, sublicense, and/or sell
+# copies of the Software, and to permit persons to whom the Software is
+# furnished to do so, subject to the following conditions:
+#
+# The above copyright notice and this permission notice shall be included in
+# all copies or substantial portions of the Software.
+#
+# THE SOFTWARE IS PROVIDED "AS IS", WITHOUT WARRANTY OF ANY KIND, EXPRESS OR
+# IMPLIED, INCLUDING BUT NOT LIMITED TO THE WARRANTIES OF MERCHANTABILITY,
+# FITNESS FOR A PARTICULAR PURPOSE AND NONINFRINGEMENT. IN NO EVENT SHALL THE
+# AUTHORS OR COPYRIGHT HOLDERS BE LIABLE FOR ANY CLAIM, DAMAGES OR OTHER
+# LIABILITY, WHETHER IN AN ACTION OF CONTRACT, TORT OR OTHERWISE, ARISING FROM,
+# OUT OF OR IN CONNECTION WITH THE SOFTWARE OR THE USE OR OTHER DEALINGS IN THE
+# SOFTWARE.
+# --------------------------------------------------------------------------
+
+from .clear_entity_caches_dto import ClearEntityCachesDto
+from .error_detail_base import ErrorDetailBase
+from .error_response import ErrorResponse, ErrorResponseException
+from .aggregate_spec import AggregateSpec
+from .property_filter import PropertyFilter
+from .aggregation_request import AggregationRequest
+from .field_schema import FieldSchema
+from .key_value_pair_property_key_field_schema import KeyValuePairPropertyKeyFieldSchema
+from .result_data_schema import ResultDataSchema
+from .list_aggregation_response import ListAggregationResponse
+from .aggregation_response_node_dictionary_string_object import AggregationResponseNodeDictionaryStringObject
+from .nested_aggregation_response import NestedAggregationResponse
+from .create_analytic_store_request import CreateAnalyticStoreRequest
+from .analytic_store_key_dto import AnalyticStoreKeyDto
+from .analytic_store_dto import AnalyticStoreDto
+from .link import Link
+from .deleted_entity_response import DeletedEntityResponse
+from .security_analytic_data_dto import SecurityAnalyticDataDto
+from .analytics_item_dto import AnalyticsItemDto
+from .analytics_storage_request import AnalyticsStorageRequest
+from .create_property_request import CreatePropertyRequest
+from .security_classification_dto import SecurityClassificationDto
+from .classifications_dto import ClassificationsDto
+from .txn_type_alias_dto import TxnTypeAliasDto
+from .property_dto import PropertyDto
+from .txn_property_mapping_dto import TxnPropertyMappingDto
+from .txn_movement_meta_data_dto import TxnMovementMetaDataDto
+from .txn_meta_data_dto import TxnMetaDataDto
+from .corporate_action_transition_component_dto import CorporateActionTransitionComponentDto
+from .corporate_action_transition_dto import CorporateActionTransitionDto
+from .upsert_corporate_action_request import UpsertCorporateActionRequest
+from .resource_id import ResourceId
+from .corporate_action_event_dto import CorporateActionEventDto
+from .error_detail import ErrorDetail
+from .try_upsert_corporate_actions_dto import TryUpsertCorporateActionsDto
+from .create_group_request import CreateGroupRequest
+from .version_dto import VersionDto
+from .group_dto import GroupDto
+from .processed_command_dto import ProcessedCommandDto
+from .complete_portfolio_dto import CompletePortfolioDto
+from .expanded_group_dto import ExpandedGroupDto
+from .update_group_request import UpdateGroupRequest
+from .login_response import LoginResponse
+from .web_log_message import WebLogMessage
+from .personalisation_dto import PersonalisationDto
+from .upsert_personalisations_response import UpsertPersonalisationsResponse
+from .create_portfolio_request import CreatePortfolioRequest
+from .portfolio_dto import PortfolioDto
+from .update_portfolio_request import UpdatePortfolioRequest
+from .portfolio_details_dto import PortfolioDetailsDto
+from .portfolio_details_request import PortfolioDetailsRequest
+from .perpetual_property_dto import PerpetualPropertyDto
+from .trade_dto import TradeDto
+from .holding_dto import HoldingDto
+from .create_perpetual_property_request import CreatePerpetualPropertyRequest
+from .target_tax_lot_dto import TargetTaxLotDto
+from .adjust_holding_request import AdjustHoldingRequest
+from .adjust_holdings_dto import AdjustHoldingsDto
+from .portfolio_properties_dto import PortfolioPropertiesDto
+from .upsert_portfolio_trade_request import UpsertPortfolioTradeRequest
+from .upsert_portfolio_trades_dto import UpsertPortfolioTradesDto
+from .add_trade_property_dto import AddTradePropertyDto
+from .create_derived_portfolio_request import CreateDerivedPortfolioRequest
+from .portfolio_search_result import PortfolioSearchResult
+from .property_definition_dto import PropertyDefinitionDto
+from .create_property_definition_request import CreatePropertyDefinitionRequest
+from .update_property_definition_request import UpdatePropertyDefinitionRequest
+from .create_unit_definition import CreateUnitDefinition
+from .create_property_data_format_request import CreatePropertyDataFormatRequest
+from .iunit_definition_dto import IUnitDefinitionDto
+from .property_data_format_dto import PropertyDataFormatDto
+from .update_property_data_format_request import UpdatePropertyDataFormatRequest
+from .reconciliation_request import ReconciliationRequest
+from .reconciliation_break_dto import ReconciliationBreakDto
+from .reference_portfolio_constituent_dto import ReferencePortfolioConstituentDto
+from .upsert_reference_portfolio_constituents_dto import UpsertReferencePortfolioConstituentsDto
+from .create_results_request import CreateResultsRequest
+from .results_dto import ResultsDto
+from .key_value_pair_string_field_schema import KeyValuePairStringFieldSchema
+from .schema_dto import SchemaDto
+from .property_schema_dto import PropertySchemaDto
+from .instrument_definition_dto import InstrumentDefinitionDto
+from .create_client_security_request import CreateClientSecurityRequest
+from .security_dto import SecurityDto
+from .try_add_client_securities_dto import TryAddClientSecuritiesDto
+from .try_delete_client_securities_dto import TryDeleteClientSecuritiesDto
+from .resource_list_security_dto import ResourceListSecurityDto
+from .try_lookup_securities_from_codes_dto import TryLookupSecuritiesFromCodesDto
+from .resource_list_analytic_store_key_dto import ResourceListAnalyticStoreKeyDto
+from .resource_list_txn_meta_data_dto import ResourceListTxnMetaDataDto
+from .resource_list_group_dto import ResourceListGroupDto
+from .resource_list_processed_command_dto import ResourceListProcessedCommandDto
+from .resource_list_personalisation_dto import ResourceListPersonalisationDto
+from .resource_list_scope import ResourceListScope
+from .resource_list_portfolio_dto import ResourceListPortfolioDto
+from .versioned_resource_list_holding_dto import VersionedResourceListHoldingDto
+from .versioned_resource_list_trade_dto import VersionedResourceListTradeDto
+from .resource_list_portfolio_search_result import ResourceListPortfolioSearchResult
+from .resource_list_property_definition_dto import ResourceListPropertyDefinitionDto
+from .resource_list_property_domain import ResourceListPropertyDomain
+from .resource_list_property_key import ResourceListPropertyKey
+from .resource_list_property_data_format_dto import ResourceListPropertyDataFormatDto
+from .resource_list_reconciliation_break_dto import ResourceListReconciliationBreakDto
+from .resource_list_reference_portfolio_constituent_dto import ResourceListReferencePortfolioConstituentDto
+from .resource_list_ui_data_type import ResourceListUiDataType
+
+__all__ = [
+    'ClearEntityCachesDto',
+    'ErrorDetailBase',
+    'ErrorResponse', 'ErrorResponseException',
+    'AggregateSpec',
+    'PropertyFilter',
+    'AggregationRequest',
+    'FieldSchema',
+    'KeyValuePairPropertyKeyFieldSchema',
+    'ResultDataSchema',
+    'ListAggregationResponse',
+    'AggregationResponseNodeDictionaryStringObject',
+    'NestedAggregationResponse',
+    'CreateAnalyticStoreRequest',
+    'AnalyticStoreKeyDto',
+    'AnalyticStoreDto',
+    'Link',
+    'DeletedEntityResponse',
+    'SecurityAnalyticDataDto',
+    'AnalyticsItemDto',
+    'AnalyticsStorageRequest',
+    'CreatePropertyRequest',
+    'SecurityClassificationDto',
+    'ClassificationsDto',
+    'TxnTypeAliasDto',
+    'PropertyDto',
+    'TxnPropertyMappingDto',
+    'TxnMovementMetaDataDto',
+    'TxnMetaDataDto',
+    'CorporateActionTransitionComponentDto',
+    'CorporateActionTransitionDto',
+    'UpsertCorporateActionRequest',
+    'ResourceId',
+    'CorporateActionEventDto',
+    'ErrorDetail',
+    'TryUpsertCorporateActionsDto',
+    'CreateGroupRequest',
+    'VersionDto',
+    'GroupDto',
+    'ProcessedCommandDto',
+    'CompletePortfolioDto',
+    'ExpandedGroupDto',
+    'UpdateGroupRequest',
+    'LoginResponse',
+    'WebLogMessage',
+    'PersonalisationDto',
+    'UpsertPersonalisationsResponse',
+    'CreatePortfolioRequest',
+    'PortfolioDto',
+    'UpdatePortfolioRequest',
+    'PortfolioDetailsDto',
+    'PortfolioDetailsRequest',
+    'PerpetualPropertyDto',
+    'TradeDto',
+    'HoldingDto',
+    'CreatePerpetualPropertyRequest',
+    'TargetTaxLotDto',
+    'AdjustHoldingRequest',
+    'AdjustHoldingsDto',
+    'PortfolioPropertiesDto',
+    'UpsertPortfolioTradeRequest',
+    'UpsertPortfolioTradesDto',
+    'AddTradePropertyDto',
+    'CreateDerivedPortfolioRequest',
+    'PortfolioSearchResult',
+    'PropertyDefinitionDto',
+    'CreatePropertyDefinitionRequest',
+    'UpdatePropertyDefinitionRequest',
+    'CreateUnitDefinition',
+    'CreatePropertyDataFormatRequest',
+    'IUnitDefinitionDto',
+    'PropertyDataFormatDto',
+    'UpdatePropertyDataFormatRequest',
+    'ReconciliationRequest',
+    'ReconciliationBreakDto',
+    'ReferencePortfolioConstituentDto',
+    'UpsertReferencePortfolioConstituentsDto',
+    'CreateResultsRequest',
+    'ResultsDto',
+    'KeyValuePairStringFieldSchema',
+    'SchemaDto',
+    'PropertySchemaDto',
+    'InstrumentDefinitionDto',
+    'CreateClientSecurityRequest',
+    'SecurityDto',
+    'TryAddClientSecuritiesDto',
+    'TryDeleteClientSecuritiesDto',
+    'ResourceListSecurityDto',
+    'TryLookupSecuritiesFromCodesDto',
+    'ResourceListAnalyticStoreKeyDto',
+    'ResourceListTxnMetaDataDto',
+    'ResourceListGroupDto',
+    'ResourceListProcessedCommandDto',
+    'ResourceListPersonalisationDto',
+    'ResourceListScope',
+    'ResourceListPortfolioDto',
+    'VersionedResourceListHoldingDto',
+    'VersionedResourceListTradeDto',
+    'ResourceListPortfolioSearchResult',
+    'ResourceListPropertyDefinitionDto',
+    'ResourceListPropertyDomain',
+    'ResourceListPropertyKey',
+    'ResourceListPropertyDataFormatDto',
+    'ResourceListReconciliationBreakDto',
+    'ResourceListReferencePortfolioConstituentDto',
+    'ResourceListUiDataType',
+]
