4e4ab331d5014ef469d0ce601fca5d0f335c75bf
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-05 18:08:36 +0000

    Add package python-hebrew-numbers/0.2.0/python-hebrew-numbers-0.2.0.tar.gz

---
+++ packages/python-hebrew-numbers/python-hebrew-numbers-0.2.0.tar.gz/python-hebrew-numbers-0.2.0/setup.py
@@ -0,0 +1,28 @@
+#!/usr/bin/env python
+
+import os, time
+
+try:
+    from setuptools import setup
+except ImportError:
+    from distutils.core import setup
+
+if os.path.exists("VERSION.txt"):
+    # this file can be written by CI tools (e.g. Travis)
+    with open("VERSION.txt") as version_file:
+        version = version_file.read().strip().strip("v")
+else:
+    version = str(time.time())
+    
+setup(
+    name='python-hebrew-numbers',
+    version=version,
+    author='Ori Hoch',
+    author_email='ori@uumpa.com',
+    description='conversion of hebrew numbers (Gimatria)',
+    packages=('hebrew_numbers',),
+    install_requires=['PyYaml'],
+    package_data={
+        'hebrew_numbers': ['../hebrew-special-numbers/styles/default.yml']
+    }
+)
