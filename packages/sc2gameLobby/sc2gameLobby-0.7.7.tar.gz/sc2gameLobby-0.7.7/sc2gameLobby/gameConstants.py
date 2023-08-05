da2f508a594b6d5536332bc75c6aa96fda474545
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-05 18:08:09 +0000

    Add package sc2gameLobby/0.7.7/sc2gameLobby-0.7.7.tar.gz

---
+++ packages/sc2gameLobby/sc2gameLobby-0.7.7.tar.gz/sc2gameLobby-0.7.7/sc2gameLobby/gameConstants.py
@@ -0,0 +1,35 @@
+
+from __future__ import absolute_import
+from __future__ import division       # python 2/3 compatibility
+from __future__ import print_function # python 2/3 compatibility
+
+import os
+
+from sc2common.constants import *
+from sc2common import types
+
+################################################################################
+class TimeoutExceeded(Exception): pass
+class UnknownPlayer(Exception): pass
+
+################################################################################
+# file/folder information
+SC2_FILE_REPLAY         = "SC2Replay"
+SC2_FILE_MAP            = "SC2Map"
+FOLDER_PLAYED_VIDEO     = "playedReplays"
+FOLDER_JSON             = "jsonData"
+FOLDER_ACTIVE_CONFIGS   = os.sep.join(os.path.abspath(__file__).split(os.sep)[:-1] + ["activeGames"])
+JSON_HEADERS            = ["label", "base-version", "version", "data-hash", "fixed-hash", "replay-hash"]
+FILE_GAME_VERSIONS      = "versions.json"
+MIN_VERSION_AI_API      = 55958 # 3.16.1 is the first version that released the API
+FOLDER_IGNORED_MAPS     = ["Melee", "mini_games", "Test"]
+
+################################################################################
+# misc
+MIN_REQUIRED_PLAYERS    = 2   # players
+INITIAL_TIMEOUT         = 120 # seconds
+DEFAULT_TIMEOUT         = 15  # seconds
+DEFAULT_HOST_DELAY      = 4   # seconds
+REPLAY_SAVE_FREQUENCY   = 10  # seconds
+URL_BASE                = "http://%s:%s/%s/"
+
