03fd807536e6ea9036c8d30ed384733d92fdebc0
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-05 20:12:55 +0000

    Add package storyblok/0.1.2/storyblok-0.1.2.tar.gz

---
+++ packages/storyblok/storyblok-0.1.2.tar.gz/storyblok-0.1.2/setup.py
@@ -0,0 +1,38 @@
+import os
+import sys
+
+try:
+    from setuptools import setup
+except ImportError:
+    from distutils.core import setup
+
+setup(
+    name='storyblok',
+    version='0.1.2',
+    description='Storyblok API library client for python',
+    author='Dominik Angerer',
+    author_email='da@storyblok.com',
+    url='https://github.com/storyblok/storyblok-python-sdk',
+    download_url = 'https://github.com/storyblok/storyblok-python-sdk/archive/0.1.2.tar.gz',
+    license='MIT',
+    install_requires=[
+        'requests >= 2.1.0'
+    ],
+    packages=[
+        'storyblok',
+        'storyblok.api',
+        'storyblok.error',
+        'storyblok.http_client',
+    ],
+    classifiers=[
+        'Development Status :: 5 - Production/Stable',
+        'Intended Audience :: Developers',
+        'License :: OSI Approved :: MIT License',
+        'Operating System :: OS Independent',
+        'Programming Language :: Python :: 2.6',
+        'Programming Language :: Python :: 2.7',
+        'Programming Language :: Python :: 3.2',
+        'Programming Language :: Python :: 3.3',
+        'Topic :: Software Development :: Libraries :: Python Modules',
+    ]
+)
