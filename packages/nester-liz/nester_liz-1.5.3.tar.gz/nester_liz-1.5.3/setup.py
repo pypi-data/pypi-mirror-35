25e33073509804988e4c8a571d7f96710821338b
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-05 22:10:47 +0000

    Add package nester-liz/1.5.3/nester_liz-1.5.3.tar.gz

---
+++ packages/nester-liz/nester_liz-1.5.3.tar.gz/nester_liz-1.5.3/setup.py
@@ -0,0 +1,11 @@
+from distutils.core import setup
+ 
+setup(
+		name		='nester_liz',
+		version		='1.5.3',
+		py_modules	=['nester_liz001'],
+		author		='liz',
+		author_email='18137803582@163.com',
+		description	='增加了space参数',
+		)
+		
