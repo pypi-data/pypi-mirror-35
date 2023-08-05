76987236bd507e501ab5535074cb357cebcf3e37
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-05 15:30:33 +0000

    Add package gitcoin/0.1.0/gitcoin-0.1.0-py3-none-any.whl

---
+++ packages/gitcoin/gitcoin-0.1.0-py3-none-any.whl/gitcoin/__init__.py
@@ -0,0 +1,13 @@
+"""Define the Gitcoin API client."""
+
+from gitcoin.client import Config
+from gitcoin.client import BountyConfig
+from gitcoin.client import Endpoint
+from gitcoin.client import Gitcoin
+
+__all__ = [
+    'Config',
+    'BountyConfig',
+    'Endpoint',
+    'Gitcoin',
+]
