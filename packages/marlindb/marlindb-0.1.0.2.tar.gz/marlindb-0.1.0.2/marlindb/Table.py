9b91ebdb111ea328da6b0e7b8415e61f8a77b011
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-05 22:09:38 +0000

    Add package marlindb/0.1.0.2/marlindb-0.1.0.2.tar.gz

---
+++ packages/marlindb/marlindb-0.1.0.2.tar.gz/marlindb-0.1.0.2/marlindb/Table.py
@@ -0,0 +1,41 @@
+# Copyright (C) DataInz Technologies Pte. Ltd. - All Rights Reserved
+# Unauthorized copying of this file, via any medium is strictly prohibited.
+# Proprietary and confidential.
+
+class Table:
+
+    # Constructor
+    def __init__(self, tableName, id, session, is_view=False):
+        self.tableName = tableName
+        self.id = id
+        self.is_view = is_view
+        self.session = session
+
+    # Function to get records from the table
+    def getRecords(self, columns, offset, limit, encoding='json'):
+        return True
+
+    # Function to execute aggregate operation on the table.
+    # The function returns a new table with the aggregate results.
+    def aggregate(self, columns, options, encoding='json'):
+        new_table = Table(tableName='', id=-1, session=self.session, is_view=True)
+        self.session.temp_views.append(new_table)
+        return new_table
+
+    # Function to execute filter operation on the table.
+    # The function returns a new table with filtered results
+    def filter(self, columns, conditions, encoding='json'):
+        new_table = Table(tableName='', id=-1, session=self.session, is_view=True)
+        self.session.temp_views.append(new_table)
+        return new_table
+
+    # Function to generate a sub view from the given table using the specified offset and limit.
+    # The function returns a new table with the filtered results
+    def subView(self, columns, offset, limit, encoding='json'):
+        new_table = Table(tableName='', id=-1, session=self.session, is_view=True)
+        self.session.temp_views.append(new_table)
+        return new_table
+
+    # Function to delete a table and its associated components from MarlinDB
+    def delete(self):
+        return True
