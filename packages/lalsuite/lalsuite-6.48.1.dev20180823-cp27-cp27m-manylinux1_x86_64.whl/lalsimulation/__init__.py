4af371f69d09fdcf19e871b671c71093421797db
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-05 21:09:49 +0000

    Add package lalsuite/6.48.1.dev20180823/lalsuite-6.48.1.dev20180823-cp27-cp27m-manylinux1_x86_64.whl

---
+++ packages/lalsuite/lalsuite-6.48.1.dev20180823-cp27-cp27m-manylinux1_x86_64.whl/lalsimulation/__init__.py
@@ -0,0 +1,7 @@
+# Import SWIG wrappings, if available
+from .lalsimulation import *
+
+__version__ = "1.7.3.1"
+
+## \addtogroup lalsimulation_python
+"""This package provides Python wrappings and extensions to LALSimulation"""
