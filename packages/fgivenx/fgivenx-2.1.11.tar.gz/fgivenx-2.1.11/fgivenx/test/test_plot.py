b38b30d09382200db9e678c99a77eddfaed4ccb8
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-05 15:26:14 +0000

    Add package fgivenx/2.1.11/fgivenx-2.1.11.tar.gz

---
+++ packages/fgivenx/fgivenx-2.1.11.tar.gz/fgivenx-2.1.11/fgivenx/test/test_plot.py
@@ -0,0 +1,39 @@
+import numpy
+import pytest
+from fgivenx.plot import plot, plot_lines
+import matplotlib.pyplot as plt
+import matplotlib
+
+
+def test_plot():
+    nx = 100
+    ny = 101
+    x = numpy.linspace(0, 1, nx)
+    y = numpy.linspace(0, 1, ny)
+    z = numpy.random.rand(ny, nx)
+
+    fig, ax = plt.subplots()
+    cbar = plot(x, y, z, ax)
+    assert type(cbar) is matplotlib.contour.QuadContourSet
+
+    with pytest.raises(TypeError):
+        plot(x, y, z, wrong_argument=None)
+
+    cbar = plot(x, y, z)
+    cbar = plot(x, y, z, smooth=1)
+    cbar = plot(x, y, z, rasterize_contours=True)
+    cbar = plot(x, y, z, lines=False)
+
+
+def test_plot_lines():
+    nx = 100
+    nsamps = 150
+    x = numpy.linspace(0, 1, nx)
+    m = numpy.random.normal(nsamps)
+    c = numpy.random.normal(nsamps)
+    fsamps = numpy.outer(x, m) + c
+    fig, ax = plt.subplots()
+    plot_lines(x, fsamps, ax)
+
+    plot_lines(x, fsamps)
+    plot_lines(x, fsamps, downsample=200)
