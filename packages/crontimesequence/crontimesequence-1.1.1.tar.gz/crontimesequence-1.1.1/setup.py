e3226e5c6c1d8131c1ad258d9b5e3ac7ede2f2ad
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-05 21:09:14 +0000

    Add package crontimesequence/1.1.1/crontimesequence-1.1.1.tar.gz

---
+++ packages/crontimesequence/crontimesequence-1.1.1.tar.gz/crontimesequence-1.1.1/setup.py
@@ -0,0 +1,37 @@
+#!/usr/bin/env python
+# -*- coding: utf-8 -*-
+
+import sys
+from distutils.core import setup
+
+
+
+# {{{ select package code folder
+if sys.version_info[0] > 2:
+	PACKAGE_FOLDER = 'lib3'
+else:
+	PACKAGE_FOLDER = 'lib2'
+# }}} select package code folder
+
+
+setup(name='crontimesequence',
+		version='1.1.1',
+		description='Generating datetime-sequence with crontab syntax in given datetime range',
+		py_modules=['crontimesequence', ],
+		package_dir={'': PACKAGE_FOLDER},
+		classifiers=['Development Status :: 5 - Production/Stable',
+			'Intended Audience :: Developers',
+			'License :: OSI Approved :: MIT License',
+			'Programming Language :: Python :: 2.6',
+			'Programming Language :: Python :: 2.7',
+			'Programming Language :: Python :: 3.0',
+			'Programming Language :: Python :: 3.1',
+			'Programming Language :: Python :: 3.2',
+			'Programming Language :: Python :: 3.3',
+			'Programming Language :: Python :: 3.4', ],
+		license='MIT License',
+	)
+
+
+
+# vim: ts=4 sw=4 ai nowrap
