57585941210e1a3e6bd53546fe5d9d52c77dd477
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-05 16:58:10 +0000

    Add package licant/0.18.6/licant-0.18.6-py2.py3-none-any.whl

---
+++ packages/licant/licant-0.18.6-py2.py3-none-any.whl/licant/__init__.py
@@ -0,0 +1,21 @@
+from licant.core import Core, Target
+from licant.core import core as default_core
+
+from licant.cli import cliexecute as ex
+from licant.cli import routine_decorator as routine
+
+from licant.core import do as do
+#from licant.core import add_target as add_target 
+
+from licant.make import add_makefile_target as add_makefile_target 
+
+import licant.scripter
+
+def execute(path):
+	licant.scripter.scriptq.execute(path)
+
+def execute_recursive(*argv, **kwargs):
+	licant.scripter.scriptq.execute(*argv, **kwargs)
+
+def about():
+	return "I'm Licant"
