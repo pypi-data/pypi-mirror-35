2c91a8d0b20008592f86d5031b18e2d1362d3667
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-05 15:26:29 +0000

    Add package memorious/0.7.9/memorious-0.7.9-py2.py3-none-any.whl

---
+++ packages/memorious/memorious-0.7.9-py2.py3-none-any.whl/memorious/model/common.py
@@ -0,0 +1,53 @@
+import json
+from datetime import datetime, date
+
+from memorious.core import connect_redis
+
+
+class Base(object):
+    conn = connect_redis()
+
+
+def unpack_int(value):
+    try:
+        return int(value)
+    except Exception:
+        return 0
+
+
+def pack_datetime(value):
+        if value is not None:
+            return str(value)
+
+
+def pack_now():
+    return pack_datetime(datetime.utcnow())
+
+
+def unpack_datetime(value):
+    if value is not None:
+        return datetime.strptime(value, "%Y-%m-%d %H:%M:%S.%f")
+
+
+class JSONEncoder(json.JSONEncoder):
+
+    def default(self, obj):
+        if isinstance(obj, (datetime, date)):
+            return obj.isoformat()
+        if isinstance(obj, bytes):
+            return obj.decode('utf-8')
+        if isinstance(obj, set):
+            return [o for o in obj]
+        return json.JSONEncoder.default(self, obj)
+
+
+def dump_json(data):
+    if data is None:
+        return
+    return JSONEncoder().encode(data)
+
+
+def load_json(encoded):
+    if encoded is None:
+        return
+    return json.loads(encoded)
