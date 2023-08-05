c495cb242b6ff2b34fb999b5a82804b55f4769aa
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-05 17:06:23 +0000

    Add package taoassets-taopy/0.6.4/taoassets-taopy-0.6.4.tar.gz

---
+++ packages/taoassets-taopy/taoassets-taopy-0.6.4.tar.gz/taoassets-taopy-0.6.4/setup.py
@@ -0,0 +1,34 @@
+# Copyright (C) 2017 chainside srl
+# Copyright (C) 2018 Bryce Weiner
+#
+# This file is part of the taopy package.
+#
+# It is subject to the license terms in the LICENSE.md file found in the top-level
+# directory of this distribution.
+#
+# No part of taopy, including this file, may be copied, modified,
+# propagated, or distributed except according to the terms contained in the
+# LICENSE.md file.
+
+
+import sys
+from distutils.core import setup
+from setuptools import find_packages
+
+TAOPYVERSION = '0.6.4'
+
+requirements = ['ecdsa==0.13']
+
+if sys.version_info.minor < 4:
+    requirements.append('enum34')
+
+setup(name='taoassets-taopy',
+      version=TAOPYVERSION,
+      packages=find_packages(),
+      install_requires=requirements,
+      extras_require={'develop': ['python-bitcoinlib==0.7.0']},
+      description='A Python3 SegWit-compliant library which provides tools to handle Tao data structures in a simple fashion.',
+      author='chainside srl, PeerAssets & TaoAssets development team',
+      url='https://github.com/taoblockchain/taopy',
+      python_requires='>=3',
+      keywords=['tao', 'blockchain', 'bitcoin', 'peercoin', 'taoassets'])
---
+++ packages/taoassets-taopy/taoassets-taopy-0.6.4.tar.gz/taoassets-taopy-0.6.4/taopy/__init__.py
@@ -0,0 +1,14 @@
+# Copyright (C) 2017 chainside srl
+# Copyright (C) 2018 Bryce Weiner
+#
+# This file is part of the taopy package.
+#
+# It is subject to the license terms in the LICENSE.md file found in the top-level
+# directory of this distribution.
+#
+# No part of taopy, including this file, may be copied, modified,
+# propagated, or distributed except according to the terms contained in the
+# LICENSE.md file.
+
+__all__ = ['utils',
+           'ds']
