c788d213dac5b2972bf09c40f0fad4b17a14033d
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-05 15:26:28 +0000

    Add package movielst/2.1.0/movielst-2.1.0.tar.gz

---
+++ packages/movielst/movielst-2.1.0.tar.gz/movielst-2.1.0/setup.py
@@ -0,0 +1,34 @@
+from setuptools import setup
+
+setup(name='movielst',
+      version='2.1.0',
+      description='Everything about your movies within the command line.',
+      url='https://github.com/Mozzo1000/movielst',
+      author='Andreas Backström',
+      author_email='andreas@simplymozzo.se',
+      license='MIT',
+      packages=['movielst'],
+      entry_points={
+               'console_scripts': ['movielst=movielst:main'],
+           },
+      install_requires=[
+          'guessit',
+          'terminaltables',
+          'tqdm',
+          'colorama',
+          'xlsxwriter'
+      ],
+      keywords=['movies', 'CLI', 'movies-within-CLI', 'python'],
+      classifiers=[
+          'Environment :: Console',
+          'License :: OSI Approved :: MIT License',
+          'Operating System :: MacOS :: MacOS X',
+          'Operating System :: Unix',
+          'Operating System :: POSIX',
+          'Programming Language :: Python',
+          'Programming Language :: Python :: 3.4',
+          'Topic :: Utilities',
+          'Topic :: Software Development :: Libraries :: Python Modules',
+          'Topic :: Software Development :: User Interfaces',
+          'Topic :: Software Development :: Version Control',
+      ],)
