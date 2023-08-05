e1ba988351f5170f4bbe2734b70a9a9e38952b95
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-05 17:08:34 +0000

    Add package retpy/0.1/retpy-0.1.tar.gz

---
+++ packages/retpy/retpy-0.1.tar.gz/retpy-0.1/setup.py
@@ -0,0 +1,10 @@
+from setuptools import setup
+
+setup(name='retpy',
+      version='0.1',
+      description='Coming Soon',
+      author='Erik Paulson',
+      author_email='epaulson@unit1127.com',
+      url='http://retpy.org',
+      packages=['retpy'],
+      zip_safe=False)
