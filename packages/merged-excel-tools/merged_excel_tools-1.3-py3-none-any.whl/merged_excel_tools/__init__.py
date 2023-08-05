e168bdeec0d48a074df582ee784e50df119483bb
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-05 15:05:08 +0000

    Add package merged-excel-tools/1.3/merged_excel_tools-1.3-py3-none-any.whl

---
+++ packages/merged-excel-tools/merged_excel_tools-1.3-py3-none-any.whl/merged_excel_tools/__init__.py
@@ -0,0 +1,20 @@
+class merged_excel():
+    ws = None
+
+    def __init__(self, xls_name):
+        from openpyxl import load_workbook
+        wb = load_workbook(filename=xls_name)
+        self.ws = wb.active
+
+    def getMergedList(self):
+        import re
+        ranges = self.ws.merged_cell_ranges
+        myRanges = [x for x in ranges if re.match("A[0-9]+:A[0-9]+", x)]
+        myRanges.sort(key=lambda x: self.getRowFromRange(x, 0))
+        return myRanges
+
+    def getRowFromRange(self, range, index):
+        return int(range.split(':')[index][1:])
+
+    def getValue(self,area,index):
+        return self.ws[("%s%d" % (area,index))].value
