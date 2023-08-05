f6cca7b12ad000b10dfddabefa1990bdcd44cfb1
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-05 17:04:32 +0000

    Add package gkeepapi/0.11.0/gkeepapi-0.11.0-py2.py3-none-any.whl

---
+++ packages/gkeepapi/gkeepapi-0.11.0-py2.py3-none-any.whl/gkeepapi/exception.py
@@ -0,0 +1,34 @@
+# -*- coding: utf-8 -*-
+"""
+.. moduleauthor:: Kai <z@kwi.li>
+"""
+
+class APIException(Exception):
+    """The API server returned an error."""
+    def __init__(self, code, msg):
+        super(APIException, self).__init__(msg)
+        self.code = code
+
+class KeepException(Exception):
+    """Generic Keep error."""
+    pass
+
+class LoginException(KeepException):
+    """Login exception."""
+    pass
+
+class LabelException(KeepException):
+    """Keep label error."""
+    pass
+
+class SyncException(KeepException):
+    """Keep consistency error."""
+    pass
+
+class MergeException(KeepException):
+    """Node consistency error."""
+    pass
+
+class InvalidException(KeepException):
+    """Constraint error."""
+    pass
