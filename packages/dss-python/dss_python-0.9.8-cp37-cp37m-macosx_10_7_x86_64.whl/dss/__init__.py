d97c895735d35ea19cd01243817df0625c538deb
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-05 15:04:00 +0000

    Add package dss-python/0.9.8/dss_python-0.9.8-cp37-cp37m-macosx_10_7_x86_64.whl

---
+++ packages/dss-python/dss_python-0.9.8-cp37-cp37m-macosx_10_7_x86_64.whl/dss/__init__.py
@@ -0,0 +1,6 @@
+'''A compatibility layer for DSS_CAPI that mimics the official OpenDSS COM interface.
+
+Copyright (c) 2016-2018 Paulo Meira
+'''
+from __future__ import absolute_import
+from .v7 import *
