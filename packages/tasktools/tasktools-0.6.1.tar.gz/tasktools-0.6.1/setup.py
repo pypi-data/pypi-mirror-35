98328165fa4c48d25a256bcdc23393c72dc7fd7c
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-05 16:55:05 +0000

    Add package tasktools/0.6.1/tasktools-0.6.1.tar.gz

---
+++ packages/tasktools/tasktools-0.6.1.tar.gz/tasktools-0.6.1/setup.py
@@ -0,0 +1,11 @@
+from setuptools import setup
+
+setup(name='tasktools',
+      version='0.6.1',
+      description='Some useful tools for asycnio Tasks',
+      url='https://tasktools.readthedocs.io/en/latest/',
+      author='David Pineda Osorio',
+      author_email='dpineda@uchile.cl',
+      license='MIT',
+      packages=['tasktools'],
+      zip_safe=False)
