0bca796ab2f78765e91a6d417cd6d895ebdb16b8
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-05 17:06:32 +0000

    Add package pydevtools/0.3.dev0/pydevtools-0.3.dev0.tar.gz

---
+++ packages/pydevtools/pydevtools-0.3.dev0.tar.gz/pydevtools-0.3.dev0/setup.py
@@ -0,0 +1,12 @@
+from distutils.core import setup
+from setuptools import find_packages
+
+setup(
+    name='pydevtools',
+    version='0.3dev',
+        author="AndersBallegaard",
+    author_email="anderstb@hotmail.dk",
+    packages=find_packages(exclude="tests/"),
+    license='MIT',
+    long_description=open('README.txt').read(),
+)
