d783ed6543ca1905182f19bb09f6eb6357a274c5
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-05 15:04:07 +0000

    Add package deprecation-factory/0.2.2/deprecation_factory-0.2.2.tar.gz

---
+++ packages/deprecation-factory/deprecation_factory-0.2.2.tar.gz/deprecation_factory-0.2.2/deprecation_factory/__init__.py
@@ -0,0 +1,8 @@
+__author__ = """Mark Harfouche"""
+__email__ = 'mark.harfouche@gmail.com'
+from .default_parameter_change import default_parameter_change
+from .kwonly_change import kwonly_change
+
+from ._version import get_versions
+__version__ = get_versions()['version']
+del get_versions
