d9691206d681ce57e51aaf24f14feda01fb8d559
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-05 18:10:29 +0000

    Add package nester-liz/1.4.0/nester_liz-1.4.0.tar.gz

---
+++ packages/nester-liz/nester_liz-1.4.0.tar.gz/nester_liz-1.4.0/nester_liz001.py
@@ -0,0 +1,18 @@
+'''这是一个"nesterliz001.py"文件提供了一个名为print——lol()的函数，这个函数的作用是打印列表，其中有可能
+包含（也可能不包含）嵌套列表。'''
+
+def print_lol(the_list, level=0):
+	'''这个函数取一个位置参数，名为'the_list'，这可以是任何pytho列表（也可以是包含嵌套列表的列表）。所指定的列表中的每个数据项会（递归地）输出到屏幕上，各数据项各占一行，且在遇到新的列表是自动缩进'''
+	
+
+	for each_item in the_list:
+			if isinstance(each_item, list):
+				print_lol(each_item, level+1)
+			else:
+				for tab_stop in range(level):
+					print("\t", end='')
+				print(each_item)
+
+
+
+
