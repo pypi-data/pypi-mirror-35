ddfb98cb0d08cab561e93fc0bbf57a2702cb1774
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-05 15:30:13 +0000

    Add package prototyping/0.1.0/prototyping-0.1.0.tar.gz

---
+++ packages/prototyping/prototyping-0.1.0.tar.gz/prototyping-0.1.0/setup.py
@@ -0,0 +1,20 @@
+from __future__ import absolute_import
+from __future__ import division
+from __future__ import print_function
+
+import setuptools
+
+
+setuptools.setup(
+    name='prototyping',
+    version='0.1.0',
+    description='Language tools for fast development and debugging.',
+    license='Apache 2.0',
+    url='http://github.com/danijar/prototyping',
+    packages=setuptools.find_packages(),
+    classifiers=[
+        'Programming Language :: Python :: 3',
+        'License :: OSI Approved :: Apache Software License',
+        'Intended Audience :: Science/Research',
+    ],
+)
