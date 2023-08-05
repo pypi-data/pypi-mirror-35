eeafdeb99635c4bd8ff689b431cbee65dab4cd3b
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-05 15:24:01 +0000

    Add package flv2img0.1.2/0.1.2/flv2img0.1.2-0.1.2.tar.gz

---
+++ packages/flv2img0.1.2/flv2img0.1.2-0.1.2.tar.gz/flv2img0.1.2-0.1.2/setup.py
@@ -0,0 +1,29 @@
+# -*- coding: utf-8 -*-
+
+from distutils.core import setup
+
+from setuptools import setup, find_packages
+
+setup(
+
+    name = 'flv2img0.1.2',
+
+    version = '0.1.2',
+
+    keywords = ('simple', 'test'),
+
+    description = 'just a simple test of vipkid',
+
+    license = 'MIT',
+
+    author = 'yin',
+
+    author_email = 'yinmengmeng@vipkid.com.cn',
+
+    packages = find_packages(),
+
+    platforms = 'any',
+
+    py_modules=['flv2img.flv2img']
+
+)
