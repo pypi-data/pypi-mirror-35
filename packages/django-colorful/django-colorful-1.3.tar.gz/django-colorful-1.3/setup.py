aeea519ef28e1f38fb367b0d9b3649e64d971dc9
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-05 17:06:04 +0000

    Add package django-colorful/1.3/django-colorful-1.3.tar.gz

---
+++ packages/django-colorful/django-colorful-1.3.tar.gz/django-colorful-1.3/setup.py
@@ -0,0 +1,46 @@
+#!/usr/bin/env python
+from setuptools import find_packages, setup
+
+import colorful
+
+setup(
+    name='django-colorful',
+    version=colorful.__version__,
+    description='An extension to the Django web framework that provides database and form color fields',
+    long_description=open('README.rst').read(),
+    url='https://github.com/charettes/django-colorful',
+    author='Simon Charette',
+    author_email='charette.s@gmail.com',
+    install_requires=[
+        'Django>=1.11',
+    ],
+    packages=find_packages(exclude=['tests', 'tests.*']),
+    include_package_data=True,
+    zip_safe=False,
+    license='MIT License',
+    keywords=['django color field'],
+    classifiers=[
+        'Development Status :: 5 - Production/Stable',
+        'Environment :: Web Environment',
+        'Framework :: Django',
+        'Framework :: Django :: 1.11',
+        'Framework :: Django :: 2.0',
+        'Framework :: Django :: 2.1',
+        'Intended Audience :: Developers',
+        'License :: OSI Approved :: MIT License',
+        'Operating System :: OS Independent',
+        'Programming Language :: Python',
+        'Programming Language :: Python :: 2',
+        'Programming Language :: Python :: 2.7',
+        'Programming Language :: Python :: 3',
+        'Programming Language :: Python :: 3.4',
+        'Programming Language :: Python :: 3.5',
+        'Programming Language :: Python :: 3.6',
+        'Programming Language :: Python :: 3.7',
+        'Topic :: Internet :: WWW/HTTP',
+        'Topic :: Internet :: WWW/HTTP :: Dynamic Content',
+        'Topic :: Internet :: WWW/HTTP :: WSGI',
+        'Topic :: Software Development :: Libraries :: Application Frameworks',
+        'Topic :: Software Development :: Libraries :: Python Modules',
+    ],
+)
