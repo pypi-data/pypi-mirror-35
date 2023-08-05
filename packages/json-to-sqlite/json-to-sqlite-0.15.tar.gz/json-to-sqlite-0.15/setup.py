57b1748ae6d0c3e37842ea6c4a536f1755f6684e
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-05 15:31:38 +0000

    Add package json-to-sqlite/0.15/json-to-sqlite-0.15.tar.gz

---
+++ packages/json-to-sqlite/json-to-sqlite-0.15.tar.gz/json-to-sqlite-0.15/setup.py
@@ -0,0 +1,27 @@
+#!/usr/bin/env python
+
+from setuptools import setup, find_packages
+
+setup(name = 'json-to-sqlite',
+      author = 'Jeremiah H. Savage',
+      author_email = 'jeremiahsavage@gmail.com',
+      version = 0.15,
+      description = 'convert to json to column based sqlite',
+      url = 'https://github.com/jeremiahsavage/json-to-sqlite',
+      license = 'Apache 2.0',
+      packages = find_packages(),
+      install_requires = [
+          'pandas',
+          'sqlalchemy'
+      ],
+      classifiers = [
+          'Development Status :: 3 - Alpha',
+          'Intended Audience :: Developers',
+          'License :: OSI Approved :: Apache Software License',
+          'Programming Language :: Python',
+          'Programming Language :: Python :: 3',
+      ],
+      entry_points={
+          'console_scripts': ['json_to_sqlite=json_to_sqlite.__main__:main']
+          },
+)
