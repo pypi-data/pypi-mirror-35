ba0fb20ae1159a7ec7b8f9d03f227bd2dfc29aca
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-05 18:08:35 +0000

    Add package seswatcher/0.1/seswatcher-0.1.tar.gz

---
+++ packages/seswatcher/seswatcher-0.1.tar.gz/seswatcher-0.1/setup.py
@@ -0,0 +1,24 @@
+# coding: utf-8
+from setuptools import setup, find_packages
+from pip.req import parse_requirements
+
+install_requires = [str(ir.req) for ir in parse_requirements("requirements.txt", session=False)]
+
+
+setup(
+    name='seswatcher',
+    version='0.1',
+    author='KhanhIceTea',
+    author_email="khanhicetea@gmail.com",
+    url="https://github.com/khanhicetea/seswatcher",
+    py_modules=['seswatcher'],
+    packages=find_packages(),
+    description='A small tool to prevent AWS SES from blocked by exceeding Bounces Rate or Complaints Rate',
+    dependency_links=[],
+    keywords=['ses', 'aws'],
+    install_requires=install_requires,
+    entry_points='''
+        [console_scripts]
+        seswatcher=seswatcher.main:watch
+    ''',
+)
