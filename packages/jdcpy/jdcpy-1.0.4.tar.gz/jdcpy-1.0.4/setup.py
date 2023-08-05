5c1b6314d371899a97094575746040a66056b52a
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-05 21:22:41 +0000

    Add package jdcpy/1.0.4/jdcpy-1.0.4.tar.gz

---
+++ packages/jdcpy/jdcpy-1.0.4.tar.gz/jdcpy-1.0.4/setup.py
@@ -0,0 +1,12 @@
+from distutils.core import setup
+
+setup(
+    name="jdcpy",
+    author="JaydenFish@ThizGroup",
+    author_email="xmyjd@163.com",
+    version="1.0.4",
+    py_modules=['jdcpy'],
+    install_requires=['pandas'],
+    description="jdcpy模块"
+    # long_description=open("source/jdcpy.rst").read()
+)
