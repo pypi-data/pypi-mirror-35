31976d8d166b475620d5ddcf1d2d006fda04dbe1
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-05 21:11:59 +0000

    Add package LyaRT-Grid/0.1.5/LyaRT_Grid-0.1.5.tar.gz

---
+++ packages/LyaRT-Grid/LyaRT_Grid-0.1.5.tar.gz/LyaRT_Grid-0.1.5/setup.py
@@ -0,0 +1,53 @@
+import setuptools
+
+from setuptools.command.develop import develop
+from setuptools.command.install import install
+
+import LyaRT_Grid as LG
+#====================================================================#
+
+class PostDevelopCommand(develop):
+    """Post-installation for development mode."""
+    def run(self):
+        LG.Download_data( During_Installation=True )
+
+        develop.run(self)
+
+
+class PostInstallCommand(install):
+    """Post-installation for installation mode."""
+    def run(self):
+        LG.Download_data( During_Installation=True )
+
+        install.run(self)
+
+#====================================================================#
+
+
+with open("README.md", "r") as fh:
+    long_description = fh.read()
+
+setuptools.setup(
+    name="LyaRT_Grid",
+    version="0.1.5",
+    author="Siddhartha Gurung Lopez",
+    author_email="sidgurung@cefca.es",
+    description="Fast Lyman alpha Radiative Transfer for everyone!",
+    long_description=long_description,
+    long_description_content_type="text/markdown",
+    url="https://github.com/sidgurun/LyaRT-Grid",
+    packages=setuptools.find_packages(),
+    #install_requires=['scikit-learn'],
+    install_requires=setuptools.find_packages(),
+    include_package_data = True,
+    classifiers=(
+        "Programming Language :: Python :: 3",
+        "License :: OSI Approved :: MIT License",
+        "Operating System :: OS Independent",
+    ),
+    cmdclass={ 'develop': PostDevelopCommand,
+               'install': PostInstallCommand, },
+    #test_suite='nose.collector',
+    #tests_require=['nose'],
+)
+
