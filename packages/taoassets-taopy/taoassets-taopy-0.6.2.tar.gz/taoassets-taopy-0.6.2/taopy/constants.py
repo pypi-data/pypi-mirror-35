a179d357f0ba03a752ff894b96bf58c14ba315f2
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-05 17:06:03 +0000

    Add package taoassets-taopy/0.6.2/taoassets-taopy-0.6.2.tar.gz

---
+++ packages/taoassets-taopy/taoassets-taopy-0.6.2.tar.gz/taoassets-taopy-0.6.2/taopy/constants.py
@@ -0,0 +1,60 @@
+from collections import namedtuple
+from decimal import Decimal
+
+
+Constants = namedtuple('Constants', [
+    'name',
+    'base58_prefixes',
+    'base58_raw_prefixes',
+    'bech32_hrp',
+    'bech32_net',
+    'xkeys_prefix',
+    'xpub_version',
+    'xprv_version',
+    'wif_prefix',
+    'from_unit',
+    'to_unit'
+])
+
+
+BitcoinMainnet = Constants(
+    name='BitcoinMainnet',
+    base58_prefixes={
+        '1': 'p2pkh',
+        '3': 'p2sh',
+    },
+    base58_raw_prefixes={
+        'p2pkh': bytearray(b'\x00'),
+        'p2sh': bytearray(b'\x05'),
+    },
+    bech32_hrp='bc',
+    bech32_net='mainnet',
+    xkeys_prefix='x',
+    xpub_version=b'\x04\x88\xb2\x1e',
+    xprv_version=b'\x04\x88\xad\xe4',
+    wif_prefix=0x80,
+    from_unit=Decimal('1e-8'),
+    to_unit=Decimal('1e8')
+)
+
+
+BitcoinTestnet = Constants(
+    name='BitcoinTestnet',
+    base58_prefixes={
+        'm': 'p2pkh',
+        'n': 'p2pkh',
+        '2': 'p2sh',
+    },
+    base58_raw_prefixes={
+        'p2pkh': bytearray(b'\x6f'),
+        'p2sh': bytearray(b'\xc4'),
+    },
+    bech32_hrp='tb',
+    bech32_net='testnet',
+    xkeys_prefix='t',
+    xpub_version=b'\x04\x35\x87\xcf',
+    xprv_version=b'\x04\x35\x83\x94',
+    wif_prefix=0xef,
+    from_unit=Decimal('1e-8'),
+    to_unit=Decimal('1e8')
+)
---
+++ packages/taoassets-taopy/taoassets-taopy-0.6.2.tar.gz/taoassets-taopy-0.6.2/taopy/structs/__init__.py
@@ -0,0 +1,11 @@
+# Copyright (C) 2017 chainside srl
+#
+# This file is part of the taopy package.
+#
+# It is subject to the license terms in the LICENSE.md file found in the top-level
+# directory of this distribution.
+#
+# No part of taopy, including this file, may be copied, modified,
+# propagated, or distributed except according to the terms contained in the
+# LICENSE.md file.
+
