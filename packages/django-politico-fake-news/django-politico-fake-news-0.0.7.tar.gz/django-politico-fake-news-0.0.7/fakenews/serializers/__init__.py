c3f1edc59d2276ebeec7bd171afff2e095e684e3
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-05 15:05:22 +0000

    Add package django-politico-fake-news/0.0.7/django-politico-fake-news-0.0.7.tar.gz

---
+++ packages/django-politico-fake-news/django-politico-fake-news-0.0.7.tar.gz/django-politico-fake-news-0.0.7/fakenews/serializers/__init__.py
@@ -0,0 +1,8 @@
+# flake8: noqa
+from .claim import ClaimSerializer
+from .disinformation_type import DisinformationTypeSerializer, DisinformationTypeListSerializer, DisinformationTypeContextSerializer
+from .fact_check import FactCheckSerializer, FactCheckFeedSerializer, FactCheckSlugsSerializer
+from .share import ShareSerializer
+from .source import SourceSerializer, SourceListSerializer
+from .tag import TagSerializer
+from .user import UserSerializer, UserListSerializer
