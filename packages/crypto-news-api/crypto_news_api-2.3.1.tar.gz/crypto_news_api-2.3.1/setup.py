86132250793035e8c45b7240491dfa91bdc28d6d
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-05 21:10:40 +0000

    Add package crypto-news-api/2.3.1/crypto_news_api-2.3.1.tar.gz

---
+++ packages/crypto-news-api/crypto_news_api-2.3.1.tar.gz/crypto_news_api-2.3.1/setup.py
@@ -0,0 +1,21 @@
+import setuptools
+
+with open('README.md', 'r') as fh:
+    long_description = fh.read()
+
+
+setuptools.setup(
+    name = 'crypto_news_api',
+    packages=['crypto_news_api'],
+    long_description=long_description,
+    long_description_content_type='text/markdown',
+    version = '2.3.1',
+    description = 'Client to get a crypto newsfeed in your app from the CryptoControl API',
+    author = 'Steven Enamakel',
+    author_email = 'enamakel@cryptocontrol.io',
+    url = 'https://github.com/cryptocontrol/python-api',
+    download_url = 'https://github.com/cryptocontrol/python-api/archive/2.2.0.tar.gz',
+    keywords = ['crypto', 'news', 'cryptocurrency', 'blockchain', 'api'],
+    classifiers = [],
+    install_reqs = ['requests']
+)
