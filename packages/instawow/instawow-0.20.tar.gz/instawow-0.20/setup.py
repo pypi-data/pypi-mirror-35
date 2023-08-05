30ff24a80c066389c1f5976a399af2c7a81bcc92
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-05 15:05:12 +0000

    Add package instawow/0.20/instawow-0.20.tar.gz

---
+++ packages/instawow/instawow-0.20.tar.gz/instawow-0.20/setup.py
@@ -0,0 +1,32 @@
+
+from pathlib import Path
+from setuptools import setup
+
+from instawow import __version__
+
+
+setup(name='instawow',
+      version=__version__,
+      description='A CLI for managing World of Warcraft add-ons.',
+      url='http://github.com/layday/instawow',
+      author='layday',
+      author_email='layday@protonmail.com',
+      license='GPLv3',
+      long_description=Path('README.rst').read_text(),
+      python_requires='~=3.6',
+      packages=['instawow'],
+      install_requires=['aiohttp==3.3.2',
+                        'click==6.7',
+                        'contextvars==2.2; python_version < "3.7"',
+                        'outdated==0.1.2',
+                        'parsel==1.5.0',
+                        'pydantic==0.12.1',
+                        'Send2Trash==1.5.0',
+                        'SQLAlchemy==1.2.10',
+                        'texttable==1.4.0',
+                        'tqdm==4.24.0',
+                        'yarl==1.2.6',
+                        'uvloop==0.11.2; platform_system != "Windows"',],
+      package_data={'instawow': ['assets/*']},
+      entry_points={'console_scripts': ['instawow = instawow.cli:cli',
+                                        'instawow-init = instawow.cli:init']})
