29005a66089d0b8699796e871cfea834bfbf0305
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-05 16:54:47 +0000

    Add package PyCausality/0.0.8/PyCausality-0.0.8.tar.gz

---
+++ packages/PyCausality/PyCausality-0.0.8.tar.gz/PyCausality-0.0.8/setup.py
@@ -0,0 +1,17 @@
+from setuptools import setup, find_packages
+
+setup(name="PyCausality",
+      version="0.0.8",
+      python_requires=">3.5.2",
+      description="Python package for detection and quantification \
+                    of statistical causality between time series. See \
+                    https://github.com/ZacKeskin/PyCausality for details",
+      author="Zac Keskin",
+      author_email="Zac.Keskin.17@ucl.ac.uk",
+      maintainer="Zac Keskin",
+      maintainer_email="zackeskin@outlook.com",
+      url="https://github.com/ZacKeskin/PyCausality",
+      license="GNU GPLv3",
+      packages=find_packages(),
+      install_requires = ['pandas','statsmodels','numpy', 'python-dateutil==2.6.1','nose', 'matplotlib'] 
+     )
