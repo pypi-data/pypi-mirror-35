24a0d13c13319010b61964119a55a06a98e07164
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-05 20:12:07 +0000

    Add package pkgconfig/1.4.0/pkgconfig-1.4.0.tar.gz

---
+++ packages/pkgconfig/pkgconfig-1.4.0.tar.gz/pkgconfig-1.4.0/setup.py
@@ -0,0 +1,18 @@
+from setuptools import setup
+
+VERSION = '1.4.0'
+
+setup(
+    name='pkgconfig',
+    version=VERSION,
+    author='Matthias Vogelgesang',
+    author_email='matthias.vogelgesang@gmail.com',
+    url='http://github.com/matze/pkgconfig',
+    license='MIT',
+    packages=['pkgconfig'],
+    description="Interface Python with pkg-config",
+    long_description=open('README.rst').read(),
+    tests_require=['nose>=1.0'],
+    python_requires='>=2.6, !=3.0.*, !=3.1.*, !=3.2.*',
+    test_suite='test',
+)
