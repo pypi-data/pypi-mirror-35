1479a12f5dafbb9dbbd1291a94ed73a3fc586876
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-05 15:25:00 +0000

    Add package tftf/0.0.9/tftf-0.0.9-py3-none-any.whl

---
+++ packages/tftf/tftf-0.0.9-py3-none-any.whl/tftf/layers/Dense.py
@@ -0,0 +1,26 @@
+import tensorflow as tf
+from .Layer import Layer
+from .initializers import zeros
+
+
+class Dense(Layer):
+    def __init__(self, output_dim,
+                 input_dim=None,
+                 initializer='glorot_normal',
+                 rng=None):
+        super().__init__()
+        self.output_dim = output_dim
+        self.input_dim = input_dim
+        self.initializer = initializer
+
+    def compile(self):
+        self.W = \
+            self.kernel_initializer(self.initializer,
+                                    shape=(self.input_dim, self.output_dim),
+                                    name='W')
+        self.b = zeros((self.output_dim), name='b')
+
+        self.params = [self.W, self.b]
+
+    def forward(self, x, **kwargs):
+        return tf.matmul(x, self.W) + self.b
