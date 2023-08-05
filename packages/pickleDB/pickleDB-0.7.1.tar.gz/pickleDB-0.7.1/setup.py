17580b57e919f38b5d65613eb014a7a2d53e1f5a
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-05 22:09:29 +0000

    Add package pickleDB/0.7.1/pickleDB-0.7.1.tar.gz

---
+++ packages/pickleDB/pickleDB-0.7.1.tar.gz/pickleDB-0.7.1/setup.py
@@ -0,0 +1,58 @@
+"""
+pickleDB
+--------
+
+pickleDB is lightweight, fast, and simple database based on Python's own
+json module. And it's BSD licensed!
+
+pickleDB is Fun
+```````````````
+
+::
+
+    >>> import pickledb
+
+    >>> db = pickledb.load('test.db', False)
+
+    >>> db.set('key', 'value')
+
+    >>> db.get('key')
+    'value'
+
+    >>> db.dump()
+    True
+
+
+And Easy to Install
+```````````````````
+
+::
+
+    $ pip install pickledb
+
+Links
+`````
+
+* `website <http://packages.python.org/pickleDB/>`_
+* `documentation <http://packages.python.org/pickleDB/commands.html>`_
+* `github repo <https://github.com/patx/pickledb>`_
+
+"""
+
+from distutils.core import setup
+
+setup(name = "pickleDB",
+    version="0.7.1",
+    description="A lightweight and simple database using simplejson.",
+    author="Harrison Erd",
+    author_email="erdh@mail.broward.edu",
+    license="three-clause BSD",
+    url="http://github.com/patx/pickledb",
+    long_description=__doc__,
+    classifiers = [
+        "Programming Language :: Python",
+        "License :: OSI Approved :: BSD License",
+        "Intended Audience :: Developers",
+        "Topic :: Database" ],
+    py_modules=['pickledb'],
+    install_requires=['simplejson'])
