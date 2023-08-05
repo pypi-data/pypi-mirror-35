d6ec59fb1eaa338d94a6c1904062075365bfc3f8
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-05 17:07:21 +0000

    Add package pywikihow/0.2/pywikihow-0.2.tar.gz

---
+++ packages/pywikihow/pywikihow-0.2.tar.gz/pywikihow-0.2/setup.py
@@ -0,0 +1,15 @@
+from setuptools import setup
+
+setup(
+    name='pywikihow',
+    version='0.2',
+    packages=['pywikihow'],
+    install_requires=[
+       'bs4'
+    ],
+    url='https://github.com/JarbasAI/PyWikiHow',
+    license='MIT',
+    author='jarbasAI',
+    author_email='jarbasai@mailfence.com',
+    description='unnoficial Wikihow api'
+)
