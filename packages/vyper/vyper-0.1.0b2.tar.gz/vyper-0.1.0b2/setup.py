1cc0efab3cf75211d57c86975d501236eb2a1916
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-05 21:08:55 +0000

    Add package vyper/0.1.0b2/vyper-0.1.0b2.tar.gz

---
+++ packages/vyper/vyper-0.1.0b2.tar.gz/vyper-0.1.0b2/setup.py
@@ -0,0 +1,39 @@
+# -*- coding: utf-8 -*-
+
+from setuptools import setup, find_packages
+
+
+with open('README.md') as f:
+    readme = f.read()
+
+
+setup(
+    name='vyper',
+    # *IMPORTANT*: Don't manually change the version here. Use the 'bumpversion' utility.
+    version='0.1.0-beta.2',
+    description='Vyper Programming Language for Ethereum',
+    long_description=readme,
+    author='Vitalik Buterin',
+    author_email='',
+    url='https://github.com/ethereum/vyper',
+    license="MIT",
+    packages=find_packages(exclude=('tests', 'docs')),
+    python_requires='>=3.6',
+    py_modules=['vyper'],
+    install_requires=[
+        'pycryptodome>=3.5.1,<4',
+    ],
+    setup_requires=[
+        'pytest-runner'
+    ],
+    tests_require=[
+       'pytest',
+       'pytest-cov',
+       'eth-tester==0.1.0b26',
+       'py-evm==0.2.0a18',
+    ],
+    scripts=[
+        'bin/vyper',
+        'bin/vyper-serve',
+    ]
+)
