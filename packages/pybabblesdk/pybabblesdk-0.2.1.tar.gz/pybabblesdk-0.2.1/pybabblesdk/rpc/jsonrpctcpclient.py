0a0a01690a06a2e55148bc23a72530f4a6916ad7
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-05 15:25:25 +0000

    Add package pybabblesdk/0.2.1/pybabblesdk-0.2.1.tar.gz

---
+++ packages/pybabblesdk/pybabblesdk-0.2.1.tar.gz/pybabblesdk-0.2.1/pybabblesdk/rpc/jsonrpctcpclient.py
@@ -0,0 +1,103 @@
+import json
+import socket
+import sys
+
+import requests
+import six
+
+__all__ = ['JSONRPCTCPClient']
+
+
+class JSONRPCTCPClient(object):
+    __id_counter = 0  # type: int
+
+    def __init__(self, endpoint):
+        """ Provides the connection to the Babble node.
+
+        :param endpoint: the babble node's ip and port
+        :type endpoint: tuple
+        """
+        self.__endpoint = endpoint
+
+    def call(self, method, args, expect_reply=False):
+        """ Call a function on the Babble node.
+
+        :param method: method to call on the babble node
+        :type method: str
+        :param args: list of arguments to be passed into the remote procedure `method`
+        :type args: list
+        :param expect_reply: whether reply is required
+        :type expect_reply: bool
+        :return: success or failure
+        :rtype: int
+        """
+        message = self._create_message(method, args)
+
+        if sys.version_info >= (3, 0):
+            message = bytes(message, encoding='ascii')
+
+        reply = self._send_message(message, True)
+
+        if expect_reply:
+            self._parse_reply(reply)
+            return 0
+        else:
+            return 0
+
+    def get_stats(self):
+        """ Returns node stats as python dictionary. """
+        return json.loads((requests.get('http://{ip}:80/stats'.format(ip=self.__endpoint[0]))).content)
+
+    def get_block(self, block_uid):
+        """ Gets block with unique id as python dictionary. """
+        request = requests.get('http://{ip}:80/block/{uid}'.format(ip=self.__endpoint[0], uid=block_uid))
+        return json.loads(request.content)
+
+    def _create_message(self, method, args):
+        """ Creates a dictionary with the method, arguments and an id in the format Babble accepts.
+
+        :param method: name of the remote procedure
+        :type method: str
+        :param args: list of arguments for the remote procedure
+        :type args: list
+        :return: str version of json
+        :rtype: str
+        """
+        if sys.version_info >= (3, 0):
+            args = [str(arg, encoding='ascii') for arg in args]
+
+        return json.dumps(dict(method=method,
+                               params=args,
+                               unique_id=six.next(self._uid())))
+
+    def _uid(self):
+        """ Generates a unique ID for each tx. """
+        while True:
+            self.__id_counter += 1
+            yield self.__id_counter
+
+    def _parse_reply(self, reply):
+        pass
+
+    def _send_message(self, message, expect_reply=False):
+        """ Send message to Babble node and get reply.
+
+        :param message: the tx details.
+        :type message: bytes
+        :param expect_reply: whether reply is required
+        :type expect_reply: bool
+        :return: data
+        """
+        if not isinstance(message, six.binary_type):
+            raise TypeError('Bytes expected')
+
+        s = socket.socket(socket.AF_INET, socket.SOCK_STREAM)
+        s.connect(self.__endpoint)
+        s.sendall(message)
+        data = s.recv(1024)
+        s.close()
+
+        if expect_reply:
+            return data
+        else:
+            return 0
---
+++ packages/pybabblesdk/pybabblesdk-0.2.1.tar.gz/pybabblesdk-0.2.1/setup.py
@@ -0,0 +1,22 @@
+import os
+
+from setuptools import setup, find_packages
+
+
+def read(file_name):
+    return open(os.path.join(os.path.dirname(__file__), file_name)).read()
+
+
+setup(
+    name='pybabblesdk',
+    version='0.2.1',
+    description='An SDK for developing Babble clients in python.',
+    long_description=read('README.md'),
+    packages=find_packages(exclude=['tests', 'demo']),
+    keywords='babble, babblesdk, sdk, pybabble, python babble, babble python, pybabblesdk',
+    author='Mosaic Networks',
+    author_email='kevin@babble.io',
+    maintainer='Kevin Jones',
+    maintainer_email='kevin@babble.io',
+    url='http://github.com/babbleio/pybabblesdk', install_requires=['six', 'requests']
+)
