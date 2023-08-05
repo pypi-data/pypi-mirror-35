898480c3135c503ea04bc87e121c5dc147807f27
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-05 22:09:07 +0000

    Add package pyepub/0.3.2/pyepub-0.3.2.tar.gz

---
+++ packages/pyepub/pyepub-0.3.2.tar.gz/pyepub-0.3.2/setup.py
@@ -0,0 +1,12 @@
+from distutils.core import setup
+
+setup(
+    name='pyepub',
+    version='0.3.2',
+    packages=['pyepub'],
+    url='https://github.com/Gluejar/pyepub',
+    license='MIT',
+    author='Gabriele Alese, Eric Hellman',
+    author_email='eric@hellman.net',
+    description='Enhanced EPUB library'
+)
