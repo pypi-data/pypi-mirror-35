843b50820e4960c411fbdf7629771a2c829ec900
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-05 15:24:54 +0000

    Add package rasa-addons/0.4.3/rasa_addons-0.4.3.tar.gz

---
+++ packages/rasa-addons/rasa_addons-0.4.3.tar.gz/rasa_addons-0.4.3/setup.py
@@ -0,0 +1,13 @@
+from setuptools import setup
+
+setup(
+    name='rasa_addons',
+    description='Rasa Add-ons',
+    version='0.4.3',
+    author='Nathan Zylbersztejn',
+    install_requires=['flask', 'flask_socketio', 'flask_cors', 'jsonmerge', 'pyyaml', 'schema'],
+    licence='Apache 2.0',
+    url='http://mrbot.ai',
+    author_email='human@mrbot.ai',
+    packages=['rasa_addons', 'rasa_addons.webchat', 'rasa_addons.superagent', 'rasa_addons.domains']
+)
