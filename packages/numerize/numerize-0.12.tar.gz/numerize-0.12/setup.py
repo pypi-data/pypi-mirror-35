893079ed2ea54857560f40385a1b558efec356c9
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-05 15:25:18 +0000

    Add package numerize/0.12/numerize-0.12.tar.gz

---
+++ packages/numerize/numerize-0.12.tar.gz/numerize-0.12/setup.py
@@ -0,0 +1,23 @@
+from setuptools import setup
+
+try:
+    import pypandoc
+    long_description = pypandoc.convert('README.md', 'rst')
+except(IOError, ImportError):
+    long_description = open('README.md').read()
+
+setup(
+   name='numerize',
+   version='0.12',
+   description='Convert large numbers into readable numbers for humans.',
+   long_description=long_description,
+   long_description_content_type='text/markdown',
+   author='David Sa',
+   author_email='davidsa03@gmail.com',
+   packages=['numerize'],  #same as name
+   url='https://github.com/davidsa03/numerize',
+   license='MIT',
+   install_requires=[
+      # -*- Extra requirements: -*-
+   ],
+)
