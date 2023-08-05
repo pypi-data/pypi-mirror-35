522a0595d9c02bb36a672f4ab35e1e92b25207da
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-05 18:10:48 +0000

    Add package udata/2.0.0.dev10947/udata-2.0.0.dev10947-py2.py3-none-any.whl

---
+++ packages/udata/udata-2.0.0.dev10947-py2.py3-none-any.whl/udata/static/site.js
@@ -0,0 +1,2 @@
+webpackJsonp([45],{0:function(e,t,u){"use strict";function _interopRequireDefault(e){return e&&e.__esModule?e:{default:e}}u(1024);var i=u(106),n=_interopRequireDefault(i),r=u(5),a=_interopRequireDefault(r),f=u(32),l=_interopRequireDefault(f);new l.default({mixins:[n.default],ready:function(){a.default.debug("Site page")}})},1024:1029});
+//# sourceMappingURL=site.js.map
