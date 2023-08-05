97425df046f9176d92f0fefac7d0a2e32f6efc09
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-05 18:10:01 +0000

    Add package fasvadms-client/1.1.0/fasvadms_client-1.1.0-py3-none-any.whl

---
+++ packages/fasvadms-client/fasvadms_client-1.1.0-py3-none-any.whl/fasvadms_client/api/__init__.py
@@ -0,0 +1,11 @@
+from __future__ import absolute_import
+
+# flake8: noqa
+
+# import apis into api package
+from fasvadms_client.api.drive_api import DriveApi
+from fasvadms_client.api.scene_api import SceneApi
+from fasvadms_client.api.sensor_api import SensorApi
+from fasvadms_client.api.signal_api import SignalApi
+from fasvadms_client.api.signal_type_api import SignalTypeApi
+from fasvadms_client.api.vehicle_api import VehicleApi
