c0375ec9ac9a64c883bde9117797078e7de2d2db
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-05 18:10:13 +0000

    Add package ticdat/0.2.16/ticdat-0.2.16.tar.gz

---
+++ packages/ticdat/ticdat-0.2.16.tar.gz/ticdat-0.2.16/setup.py
@@ -0,0 +1,33 @@
+import ticdat
+from setuptools import setup, find_packages
+
+with open('README.md') as f:
+    long_description = f.read()
+
+setup(
+	name = 'ticdat',
+	packages = find_packages(),
+	version = ticdat.__version__,
+	long_description = long_description,
+	long_description_content_type='text/markdown',
+	license = 'BSD 2-Clause',
+	author = 'Opalytics Inc',
+	author_email= 'pcacioppi@opalytics.com',
+	maintainer_email = 'snelson@opalytics.com',
+	url = 'https://github.com/opalytics/opalytics-ticdat',
+	classifiers = [
+		"Development Status :: 5 - Production/Stable",
+		"Intended Audience :: Developers",
+		"Intended Audience :: Science/Research",
+		"Operating System :: OS Independent",
+		"Programming Language :: Python",
+		"Programming Language :: Python :: 2",
+		"Programming Language :: Python :: 2.7",
+		"Programming Language :: Python :: 3",
+		"Programming Language :: Python :: 3.4",
+		"Programming Language :: Python :: 3.5",
+		"Topic :: Scientific/Engineering",
+		"Topic :: Scientific/Engineering :: Mathematics"
+	],
+	platforms = 'any'
+)
