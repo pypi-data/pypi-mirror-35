571076255a2ed97a0d2d79ac1cfc4ddc0c05dac7
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-05 18:10:58 +0000

    Add package nameparser/0.5.8/nameparser-0.5.8-py2.py3-none-any.whl

---
+++ packages/nameparser/nameparser-0.5.8-py2.py3-none-any.whl/nameparser/config/prefixes.py
@@ -0,0 +1,36 @@
+# -*- coding: utf-8 -*-
+from __future__ import unicode_literals
+
+#: Name pieces that appear before a last name. They join to the piece that follows them to make one new piece.
+PREFIXES = set([
+    'abu',
+    'bin',
+    'bon',
+    'da',
+    'dal',
+    'de',
+    'degli',
+    'dei',
+    'del',
+    'dela',
+    'della',
+    'delle',
+    'delli',
+    'dello',
+    'der',
+    'di',
+    'dí',
+    'do',
+    'dos',
+    'du',
+    'ibn',
+    'la',
+    'le',
+    'san',
+    'santa',
+    'st',
+    'ste',
+    'van',
+    'vel',
+    'von',
+])
