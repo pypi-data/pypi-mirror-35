cd58f0e4fdca49639e61ffd512b8815133958e91
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-05 20:11:43 +0000

    Add package todolint/0.0.1/todolint-0.0.1.tar.gz

---
+++ packages/todolint/todolint-0.0.1.tar.gz/todolint-0.0.1/setup.py
@@ -0,0 +1,19 @@
+import os
+from setuptools import setup, find_packages
+
+setup(
+    name = "todolint",
+    version = '0.0.1',
+    packages = find_packages(),
+    description='todolint',
+    long_description='todolint',
+    author='Alister Cordiner',
+    author_email='alister@cordiner.net',
+    license='Restricted',
+    platforms='Any',
+    classifiers=[
+        'Programming Language :: Python',
+        'Intended Audience :: Financial and Insurance Industry',
+        'Operating System :: OS Independent',
+    ],
+)
