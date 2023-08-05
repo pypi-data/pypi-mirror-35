0875fda88f51a1f3924c3155a2f9248fceee1007
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-05 22:10:18 +0000

    Add package punyverse/0.5/punyverse-0.5.tar.gz

---
+++ packages/punyverse/punyverse-0.5.tar.gz/punyverse-0.5/README.md
@@ -0,0 +1,17 @@
+# punyverse [![Linux Build Status](https://img.shields.io/travis/quantum5/punyverse.svg?logo=linux)](https://travis-ci.org/quantum5/punyverse) [![Windows Build Status](https://img.shields.io/appveyor/ci/quantum5/punyverse.svg?logo=windows)](https://ci.appveyor.com/project/quantum5/punyverse) [![PyPI](https://img.shields.io/pypi/v/punyverse.svg)](https://pypi.org/project/punyverse/) [![PyPI - Format](https://img.shields.io/pypi/format/punyverse.svg)](https://pypi.org/project/punyverse/) [![PyPI - Python Version](https://img.shields.io/pypi/pyversions/punyverse.svg)](https://pypi.org/project/punyverse/)
+
+Python simulator of a puny universe. (How many words can I stick into one?)
+
+![Punyverse Preview](https://guanzhong.ca/assets/projects/punyverse-c91f1cb1415c2922c5dcdf9773bc8d86e52e86e1c1b01d33a0969af7b669b8c4.png)
+
+## Installation
+
+To install, run `pip install punyverse`.
+
+Then, run `punyverse` to launch the simulator, or `punyversew` to launch without the console.
+
+### A Note on Textures
+
+If your graphics card doesn't support the massive texture sizes this module comes with, you can shrink them.
+
+To do this, run `punyverse_small_images`.
---
+++ packages/punyverse/punyverse-0.5.tar.gz/punyverse-0.5/setup.py
@@ -0,0 +1,95 @@
+from __future__ import print_function
+import sys
+import os
+
+from setuptools import setup, Extension
+
+has_pyx = os.path.exists(os.path.join(os.path.dirname(__file__), 'punyverse', '_glgeom.pyx'))
+
+try:
+    from Cython.Build import cythonize
+except ImportError:
+    if has_pyx:
+        print('You need to install cython first before installing punyverse.', file=sys.stderr)
+        print('Run: pip install cython', file=sys.stderr)
+        print('Or if you do not have pip: easy_install cython', file=sys.stderr)
+        sys.exit(1)
+    cythonize = lambda x: x
+
+if has_pyx:
+    pyx_path = lambda x: x
+else:
+    pyx_path = lambda x: x.replace('.pyx', '.c')
+
+if os.name == 'nt':
+    gl_libs = ['opengl32']
+else:
+    gl_libs = ['GL']
+
+with open(os.path.join(os.path.dirname(__file__), 'README.md')) as f:
+    long_description = f.read()
+
+setup(
+    name='punyverse',
+    version='0.5',
+    packages=['punyverse'],
+    package_data={
+        'punyverse': [
+            'world.json',
+            'assets/textures.txt',
+            'assets/textures/*.jpg',
+            'assets/textures/*.png',
+            'assets/textures/moons/*',
+            'assets/models/asteroids/*',
+            'assets/models/satellites/*.mtl',
+            'assets/models/satellites/*.obj',
+            'assets/models/satellites/*.jpg',
+            'assets/models/satellites/*.png',
+            'assets/models/satellites/cassini/*',
+        ],
+    },
+    ext_modules=cythonize([
+        Extension('punyverse._glgeom', sources=[pyx_path('punyverse/_glgeom.pyx')], libraries=gl_libs),
+        Extension('punyverse._model', sources=[pyx_path('punyverse/_model.pyx')], libraries=gl_libs),
+    ]),
+
+    entry_points={
+        'console_scripts': [
+            'punyverse = punyverse.__main__:main',
+            'punyverse_small_images = punyverse.small_images:main',
+        ],
+        'gui_scripts': [
+            'punyversew = punyverse.__main__:main'
+        ]
+
+    },
+    install_requires=['pyglet', 'Pillow', 'six'],
+
+    author='quantum',
+    author_email='quantum2048@gmail.com',
+    url='https://github.com/quantum5/punyverse',
+    description='Python simulator of a puny universe.',
+    long_description=long_description,
+    long_description_content_type='text/markdown',
+    keywords='universe simulator',
+    classifiers=[
+        'Development Status :: 4 - Beta',
+        'Environment :: Win32 (MS Windows)',
+        'Environment :: X11 Applications',
+        'Intended Audience :: End Users/Desktop',
+        'License :: OSI Approved :: GNU General Public License v2 (GPLv2)',
+        'Operating System :: Microsoft :: Windows',
+        'Operating System :: POSIX :: Linux',
+        'Programming Language :: Python',
+        'Programming Language :: Python :: 2',
+        'Programming Language :: Python :: 2.7',
+        'Programming Language :: Python :: 3',
+        'Programming Language :: Python :: 3.4',
+        'Programming Language :: Python :: 3.5',
+        'Programming Language :: Python :: 3.6',
+        'Programming Language :: Python :: 3.7',
+        'Topic :: Games/Entertainment :: Simulation',
+        'Topic :: Multimedia :: Graphics :: 3D Rendering',
+        'Topic :: Scientific/Engineering :: Visualization',
+    ],
+)
