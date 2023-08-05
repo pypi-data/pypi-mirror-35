2edfb9a1bf7bc06f1b440fbba5b23d682740d9d4
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-05 17:08:04 +0000

    Add package mbstrdecoder/0.4.2/mbstrdecoder-0.4.2.tar.gz

---
+++ packages/mbstrdecoder/mbstrdecoder-0.4.2.tar.gz/mbstrdecoder-0.4.2/test/test_func.py
@@ -0,0 +1,39 @@
+# encoding: utf-8
+
+"""
+.. codeauthor:: Tsuyoshi Hombashi <tsuyoshi.hombashi@gmail.com>
+"""
+
+from __future__ import unicode_literals
+
+from textwrap import dedent
+
+import pytest
+from mbstrdecoder._func import detect_file_encoding, to_codec_name
+
+
+@pytest.mark.parametrize(
+    ["value", "expected"],
+    [["utf8", "utf8"], ["utf-8", "utf_8"], ["UTF8", "utf8"], ["UTF-8", "utf_8"], [None, None]],
+)
+def test_to_codec_name(value, expected):
+    assert to_codec_name(value) == expected
+
+
+@pytest.mark.parametrize(["value", "expected"], [["utf8", "utf_8"], ["utf16", "utf_16"]])
+def test_detect_file_encoding(tmpdir, value, expected):
+    data = dedent(
+        """\
+        # ほげほげ -----
+        print(con.fetch_attr_name_list(table_name))
+        result = con.select(select="*", table_name=table_name)
+        for record in result.fetchall():
+        print(record)
+        """
+    )
+    p_csv = tmpdir.join("test_detect_file_encoding")
+
+    with open(str(p_csv), "wb") as f:
+        f.write(data.encode(value))
+
+    assert detect_file_encoding(str(p_csv)) == expected
