1796e9ee1be91796027005f74c952251c77fbaf5
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-05 20:12:27 +0000

    Add package dispatch/0.5.1/dispatch-0.5.1.tar.gz

---
+++ packages/dispatch/dispatch-0.5.1.tar.gz/dispatch-0.5.1/setup.py
@@ -0,0 +1,26 @@
+from setuptools import setup, find_packages
+
+setup(name='dispatch',
+    version='0.5.1',
+    description='A publishing platform for modern newspapers',
+    url='https://github.com/ubyssey/dispatch',
+    author='Peter Siemens',
+    author_email='peterjsiemens@gmail.com',
+    license='GPL',
+    packages=find_packages(),
+    scripts=['dispatch/bin/dispatch-admin'],
+    include_package_data=True,
+    install_requires=[
+        'django == 1.11',
+        'djangorestframework == 3.6.2',
+        'pillow',
+        'requests == 2.6.0',
+        'jsonfield',
+        'mutagen',
+    ],
+    extras_require={
+        'dev': [
+            'coverage'
+        ]
+    },
+    zip_safe=False)
