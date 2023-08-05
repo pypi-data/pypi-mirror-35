70e3974bc900f5598582ccacf54eb859532e32b2
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-05 22:11:46 +0000

    Add package PyFunceble-dev/0.99.3/PyFunceble_dev-0.99.3-py3-none-any.whl

---
+++ packages/PyFunceble-dev/PyFunceble_dev-0.99.3-py3-none-any.whl/PyFunceble/lookup.py
@@ -0,0 +1,158 @@
+#!/usr/bin/env python3
+
+# pylint:disable=line-too-long
+"""
+The tool to check domains or IP availability.
+
+::
+
+
+    :::::::::  :::   ::: :::::::::: :::    ::: ::::    :::  ::::::::  :::::::::: :::::::::  :::        ::::::::::
+    :+:    :+: :+:   :+: :+:        :+:    :+: :+:+:   :+: :+:    :+: :+:        :+:    :+: :+:        :+:
+    +:+    +:+  +:+ +:+  +:+        +:+    +:+ :+:+:+  +:+ +:+        +:+        +:+    +:+ +:+        +:+
+    +#++:++#+    +#++:   :#::+::#   +#+    +:+ +#+ +:+ +#+ +#+        +#++:++#   +#++:++#+  +#+        +#++:++#
+    +#+           +#+    +#+        +#+    +#+ +#+  +#+#+# +#+        +#+        +#+    +#+ +#+        +#+
+    #+#           #+#    #+#        #+#    #+# #+#   #+#+# #+#    #+# #+#        #+#    #+# #+#        #+#
+    ###           ###    ###         ########  ###    ####  ########  ########## #########  ########## ##########
+
+This submodule will provide the lookup interface.
+
+Author:
+    Nissar Chababy, @funilrys, contactTATAfunilrysTODTODcom
+
+Special thanks:
+    https://pyfunceble.readthedocs.io/en/dev/special-thanks.html
+
+Contributors:
+    http://pyfunceble.readthedocs.io/en/dev/special-thanks.html
+
+Project link:
+    https://github.com/funilrys/PyFunceble
+
+Project documentation:
+    https://pyfunceble.readthedocs.io
+
+Project homepage:
+    https://funilrys.github.io/PyFunceble/
+
+License:
+::
+
+
+    MIT License
+
+    Copyright (c) 2017-2018 Nissar Chababy
+
+    Permission is hereby granted, free of charge, to any person obtaining a copy
+    of this software and associated documentation files (the "Software"), to deal
+    in the Software without restriction, including without limitation the rights
+    to use, copy, modify, merge, publish, distribute, sublicense, and/or sell
+    copies of the Software, and to permit persons to whom the Software is
+    furnished to do so, subject to the following conditions:
+
+    The above copyright notice and this permission notice shall be included in all
+    copies or substantial portions of the Software.
+
+    THE SOFTWARE IS PROVIDED "AS IS", WITHOUT WARRANTY OF ANY KIND, EXPRESS OR
+    IMPLIED, INCLUDING BUT NOT LIMITED TO THE WARRANTIES OF MERCHANTABILITY,
+    FITNESS FOR A PARTICULAR PURPOSE AND NONINFRINGEMENT. IN NO EVENT SHALL THE
+    AUTHORS OR COPYRIGHT HOLDERS BE LIABLE FOR ANY CLAIM, DAMAGES OR OTHER
+    LIABILITY, WHETHER IN AN ACTION OF CONTRACT, TORT OR OTHERWISE, ARISING FROM,
+    OUT OF OR IN CONNECTION WITH THE SOFTWARE OR THE USE OR OTHER DEALINGS IN THE
+    SOFTWARE.
+"""
+# pylint: enable=line-too-long
+
+import PyFunceble
+from PyFunceble import socket
+
+
+class Lookup:
+    """
+    This class can be used to NSLOOKUP or WHOIS lookup.
+    """
+
+    @classmethod
+    def nslookup(cls):
+        """
+        Implementation of UNIX nslookup.
+        """
+
+        try:
+            socket.getaddrinfo(
+                PyFunceble.CONFIGURATION["domain"], 80, 0, 0, socket.IPPROTO_TCP
+            )
+
+            return True
+
+        except (OSError, socket.herror, socket.gaierror):
+            return False
+
+    @classmethod
+    def whois(cls, whois_server, domain=None, timeout=None):  # pragma: no cover
+        """
+        Implementation of UNIX whois.
+
+        Arguments:
+            - whois_server: str
+                The whois server to use to get the record.
+            - domain: str
+                The domain to get the whois record from.
+            - timeout: int
+                The timeout to apply to the request.
+
+        Returns: None or str
+            None: No whois record catched.
+            str: The whois record.
+        """
+
+        if domain is None:
+            domain = PyFunceble.CONFIGURATION["domain"]
+
+        if timeout is None:
+            timeout = PyFunceble.CONFIGURATION["seconds_before_http_timeout"]
+
+        if whois_server:
+
+            req = socket.socket(socket.AF_INET, socket.SOCK_STREAM)
+
+            if PyFunceble.CONFIGURATION["seconds_before_http_timeout"] % 3 == 0:
+                req.settimeout(timeout)
+            else:
+                req.settimeout(3)
+
+            try:
+                req.connect((whois_server, 43))
+            except socket.error:
+                return None
+
+            req.send((domain + "\r\n").encode())
+            response = b""
+
+            while True:
+                try:
+                    try:
+                        data = req.recv(4096)
+                    except ConnectionResetError:
+                        req.close()
+
+                        return None
+
+                except socket.timeout:
+                    req.close()
+
+                    return None
+
+                response += data
+                if not data:
+                    break
+
+            req.close()
+
+            try:
+                return response.decode()
+
+            except UnicodeDecodeError:
+                return response.decode("utf-8", "replace")
+
+        return None
