c135f18ff7fe8272de39b81d72e4ef008b50d70d
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-05 15:25:06 +0000

    Add package pypairs/2.0.0/pypairs-2.0.0.tar.gz

---
+++ packages/pypairs/pypairs-2.0.0.tar.gz/pypairs-2.0.0/setup.py
@@ -0,0 +1,13 @@
+import setuptools
+from distutils.core import setup
+
+setup(
+    name='pypairs',
+    version='2.0.0',
+    packages=['pypairs'],
+    url='https://github.com/rfechtner/pypairs',
+    license='https://github.com/rfechtner/pypairs/blob/master/LICENSE',
+    author='Ron Fechtner',
+    author_email='ronfechtner@gmail.com',
+    description='A Python-reimplementation of the Pairs algorithm described by A. Scialdone et al. (2015)'
+)
