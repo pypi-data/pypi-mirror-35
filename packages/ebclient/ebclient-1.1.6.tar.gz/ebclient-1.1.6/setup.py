a01dafdfbeba866a16be2d0aa64eda951ac58685
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-05 21:09:16 +0000

    Add package ebclient/1.1.6/ebclient-1.1.6.tar.gz

---
+++ packages/ebclient/ebclient-1.1.6.tar.gz/ebclient-1.1.6/setup.py
@@ -0,0 +1,81 @@
+import sys
+
+from setuptools import setup
+from setuptools import find_packages
+
+version = '1.1.6'
+
+# Please update tox.ini when modifying dependency version requirements
+install_requires = [
+    'pycrypto>=2.6',
+    'requests',
+    'setuptools>=1.0',
+    'six', 'Crypto'
+]
+
+# TLS SNI for older python
+if sys.version_info < (2, 7, 10):
+    install_requires.extend([
+        'pyopenssl',
+        'ndg-httpsclient',
+        'pyasn1'
+    ])
+
+# env markers in extras_require cause problems with older pip: #517
+# Keep in sync with conditional_requirements.py.
+if sys.version_info < (2, 7):
+    install_requires.extend([
+        # only some distros recognize stdlib argparse as already satisfying
+        'argparse',
+        'mock<1.1.0',
+    ])
+else:
+    if sys.version_info >= (3, 0):
+        'past'
+    install_requires.append('mock')
+
+
+dev_extras = [
+    'nose',
+    'pep8',
+    'tox',
+]
+
+docs_extras = [
+    'Sphinx>=1.0',  # autodoc_member_order = 'bysource', autodoc_default_flags
+    'sphinx_rtd_theme',
+    'sphinxcontrib-programoutput',
+]
+
+
+setup(
+    name='ebclient',
+    version=version,
+    description='EnigmaBridge Python Client',
+    url='https://github.com/EnigmaBridge/client.py',
+    author="Enigma Bridge",
+    author_email='support@enigmabridge.com',
+    license=open('LICENSE').read(),
+    long_description=open('README.rst').read(),
+    classifiers=[
+        'Development Status :: 3 - Alpha',
+        'Intended Audience :: Developers',
+        'Programming Language :: Python',
+        'Programming Language :: Python :: 2.6',
+        'Programming Language :: Python :: 2.7',
+        'Programming Language :: Python :: 3.3',
+        'Programming Language :: Python :: 3.4',
+        'Programming Language :: Python :: 3.5',
+        'Programming Language :: Python :: 3.6',
+        'Topic :: Internet :: WWW/HTTP',
+        'Topic :: Security',
+    ],
+
+    packages=find_packages(),
+    include_package_data=True,
+    install_requires=install_requires,
+    extras_require={
+        'dev': dev_extras,
+        'docs': docs_extras,
+    }
+)
