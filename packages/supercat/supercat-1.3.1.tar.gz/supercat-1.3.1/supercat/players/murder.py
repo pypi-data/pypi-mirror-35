61fe2a7b36684ce2dd70dba5d58ca62742539a49
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-05 17:08:24 +0000

    Add package supercat/1.3.1/supercat-1.3.1.tar.gz

---
+++ packages/supercat/supercat-1.3.1.tar.gz/supercat-1.3.1/supercat/players/murder.py
@@ -0,0 +1,31 @@
+"""
+# Ordered Player
+
+The ordered player, plays the first available move from top to bottom,
+from left to right
+"""
+from supercat.classes import BasePlayer
+from supercat.utils import boxes
+
+
+class Player(BasePlayer):
+
+    name = 'murder'
+
+    def play(self, world, game, move_num, last_move):
+        # lazy game
+        if game is not None:
+            # should play this game
+            for row, col in boxes():
+                if world[game][2-row, col] is None:
+                    return game, (2-row, col)
+        else:
+            # free play!
+            for grand_row, grand_col in boxes():
+                if world[grand_row, grand_col]['owner'] is not None:
+                    continue
+
+                for row, col in boxes():
+                    if world[grand_row, grand_col][2-row, col] is None:
+                        return (grand_row, grand_col), (2-row, col)
+        return None, None
