ca3b375e6ae839e760e1ced08d7e990f9f31feb3
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-05 15:25:50 +0000

    Add package class-only-design/0.2.0/class_only_design-0.2.0.tar.gz

---
+++ packages/class-only-design/class_only_design-0.2.0.tar.gz/class_only_design-0.2.0/class_only_design/__init__.py
@@ -0,0 +1,11 @@
+# -*- coding: utf-8 -*-
+
+"""Top-level package for Class Only."""
+
+__author__ = """Tom Rutherford"""
+__email__ = "foreverwintr@gmail.com"
+__version__ = "0.2.0"
+
+from class_only_design.api import class_only
+from class_only_design.api import constant
+from class_only_design.api import namespace
