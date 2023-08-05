851c0f8f37d99f227ee2a961f151e88fd370e760
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-05 20:12:23 +0000

    Add package albumentations/0.0.7/albumentations-0.0.7-py3.6.egg

---
+++ packages/albumentations/albumentations-0.0.7-py3.6.egg/albumentations/__init__.py
@@ -0,0 +1,8 @@
+from __future__ import absolute_import
+
+__version__ = '0.0.7'
+
+from .core.composition import *
+from .core.transforms_interface import *
+from .augmentations.transforms import *
+from .imgaug.transforms import *
