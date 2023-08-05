aaf2d1bf7bb6609a00c3c0aafa7ba09aae9731e5
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-05 15:04:35 +0000

    Add package versuchung/1.3.1/versuchung-1.3.1.tar.gz

---
+++ packages/versuchung/versuchung-1.3.1.tar.gz/versuchung-1.3.1/setup.py
@@ -0,0 +1,61 @@
+#!/usr/bin/env python
+
+from __future__ import print_function
+
+from distutils.core import setup
+from distutils.cmd import Command
+from distutils.spawn import spawn
+
+import sys
+
+try:
+    from sphinx.setup_command import BuildDoc
+    cmdclass = {'doc': BuildDoc}
+except:
+    print("No Sphinx installed (python-sphinx) so no documentation can be build")
+    cmdclass = {}
+
+
+class TestCommand(Command):
+    user_options = []
+    def run(self):
+        spawn(["make", "-C", "tests", "PYTHON=%s" % (sys.executable,)], verbose = 1)
+
+    def initialize_options(self):
+        pass
+
+    def finalize_options(self):
+        pass
+
+cmdclass["test"] = TestCommand
+
+with open("README.md", "r") as fh:
+    long_description = fh.read()
+
+version_info = {
+    'name': 'versuchung',
+    'version': '1.3.1',
+    'description': 'A toolbox for experiments',
+    'author': 'Christian Dietrich',
+    'author_email': 'stettberger@dokucode.de',
+    'url': 'http://github.de/stettberger/versuchung',
+    'long_description': long_description,
+    'long_description_content_type': "text/markdown",
+    'license': 'GPLv3',
+    'classifiers': [
+        'Development Status :: 4 - Beta',
+        'Environment :: Console',
+        'Intended Audience :: Science/Research',
+        'License :: OSI Approved :: GNU General Public License v3 or later (GPLv3+)',
+        'Operating System :: OS Independent',
+        'Programming Language :: Python :: 2.6',
+        'Programming Language :: Python :: 3.5'
+    ],
+}
+
+
+setup(packages = ["versuchung"],
+      package_dir = {'versuchung': 'src/versuchung'},
+      cmdclass = cmdclass,
+      **version_info
+  )
