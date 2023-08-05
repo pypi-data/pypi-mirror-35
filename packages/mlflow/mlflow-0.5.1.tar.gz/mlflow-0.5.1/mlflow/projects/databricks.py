14faba281b8a6e3bae1d1b80e29a07d26afd1c04
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-05 21:11:24 +0000

    Add package mlflow/0.5.1/mlflow-0.5.1.tar.gz

---
+++ packages/mlflow/mlflow-0.5.1.tar.gz/mlflow-0.5.1/mlflow/projects/databricks.py
@@ -0,0 +1,308 @@
+import hashlib
+import json
+import os
+import shutil
+import tempfile
+import textwrap
+import time
+
+from six.moves import shlex_quote, urllib
+
+from mlflow.entities import RunStatus
+
+from mlflow.projects import _fetch_project
+from mlflow.projects.submitted_run import SubmittedRun
+from mlflow.utils import rest_utils, file_utils
+from mlflow.utils.exception import ExecutionException
+from mlflow.utils.logging_utils import eprint
+from mlflow import tracking
+from mlflow.version import VERSION
+
+# Base directory within driver container for storing files related to MLflow
+DB_CONTAINER_BASE = "/databricks/mlflow"
+# Base directory within driver container for storing project archives
+DB_TARFILE_BASE = os.path.join(DB_CONTAINER_BASE, "project-tars")
+# Base directory directory within driver container for storing extracted project directories
+DB_PROJECTS_BASE = os.path.join(DB_CONTAINER_BASE, "projects")
+# Name to use for project directory when archiving it for upload to DBFS; the TAR will contain
+# a single directory with this name
+DB_TARFILE_ARCHIVE_NAME = "mlflow-project"
+# Base directory within DBFS for storing code for project runs for experiments
+DBFS_EXPERIMENT_DIR_BASE = "mlflow-experiments"
+
+
+def _jobs_runs_get(databricks_run_id):
+    return rest_utils.databricks_api_request(
+        endpoint="jobs/runs/get", method="GET", json={"run_id": databricks_run_id})
+
+
+def _jobs_runs_cancel(databricks_run_id):
+    return rest_utils.databricks_api_request(
+        endpoint="jobs/runs/cancel", method="POST", json={"run_id": databricks_run_id})
+
+
+def _jobs_runs_submit(req_body_json):
+    return rest_utils.databricks_api_request(
+        endpoint="jobs/runs/submit", method="POST", json=req_body_json)
+
+
+def _get_databricks_run_cmd(dbfs_fuse_tar_uri, run_id, entry_point, parameters):
+    """
+    Generates MLflow CLI command to run on Databricks cluster in order to launch a run on Databricks
+    """
+    # Strip ".gz" and ".tar" file extensions from base filename of the tarfile
+    tar_hash = os.path.splitext(os.path.splitext(os.path.basename(dbfs_fuse_tar_uri))[0])[0]
+    container_tar_path = os.path.abspath(os.path.join(DB_TARFILE_BASE,
+                                                      os.path.basename(dbfs_fuse_tar_uri)))
+    project_dir = os.path.join(DB_PROJECTS_BASE, tar_hash)
+    mlflow_run_arr = list(map(shlex_quote, ["mlflow", "run", project_dir,
+                                            "--entry-point", entry_point]))
+    if run_id:
+        mlflow_run_arr.extend(["--run-id", run_id])
+    if parameters:
+        for key, value in parameters.items():
+            mlflow_run_arr.extend(["-P", "%s=%s" % (key, value)])
+    mlflow_run_cmd = " ".join(mlflow_run_arr)
+    shell_command = textwrap.dedent("""
+    export PATH=$DB_HOME/conda/bin:$DB_HOME/python/bin:$PATH &&
+    mlflow --version &&
+    # Make local directories in the container into which to copy/extract the tarred project
+    mkdir -p {tarfile_base} {projects_base} &&
+    # Rsync from DBFS FUSE to avoid copying archive into local filesystem if it already exists
+    rsync -a -v --ignore-existing {dbfs_fuse_tar_path} {tarfile_base} &&
+    # Extract project into a temporary directory. We don't extract directly into the desired
+    # directory as tar extraction isn't guaranteed to be atomic
+    cd $(mktemp -d) &&
+    tar --no-same-owner -xzvf {container_tar_path} &&
+    # Atomically move the extracted project into the desired directory
+    mv -T {tarfile_archive_name} {work_dir} &&
+    {mlflow_run}
+    """.format(tarfile_base=DB_TARFILE_BASE, projects_base=DB_PROJECTS_BASE,
+               dbfs_fuse_tar_path=dbfs_fuse_tar_uri, container_tar_path=container_tar_path,
+               tarfile_archive_name=DB_TARFILE_ARCHIVE_NAME, work_dir=project_dir,
+               mlflow_run=mlflow_run_cmd))
+    return ["bash", "-c", shell_command]
+
+
+def _check_databricks_auth_available():
+    """
+    Verifies that information for making API requests to Databricks is available to MLflow, raising
+    an exception if not.
+    """
+    rest_utils.get_databricks_http_request_kwargs_or_fail()
+
+
+def _upload_to_dbfs(src_path, dbfs_fuse_uri):
+    """
+    Uploads the file at `src_path` to the specified DBFS URI within the Databricks workspace
+    corresponding to the default Databricks CLI profile.
+    """
+    eprint("=== Uploading project to DBFS path %s ===" % dbfs_fuse_uri)
+    http_endpoint = dbfs_fuse_uri
+    http_request_kwargs = rest_utils.get_databricks_http_request_kwargs_or_fail()
+    with open(src_path, 'rb') as f:
+        rest_utils.http_request(
+            endpoint=http_endpoint, method='POST', data=f,
+            **http_request_kwargs)
+
+
+def _dbfs_path_exists(dbfs_uri):
+    """
+    Returns True if the passed-in path exists in DBFS for the workspace corresponding to the
+    default Databricks CLI profile.
+    """
+    dbfs_path = _parse_dbfs_uri_path(dbfs_uri)
+    json_response_obj = rest_utils.databricks_api_request(
+        endpoint="dbfs/get-status", method="GET", json={"path": dbfs_path})
+    # If request fails with a RESOURCE_DOES_NOT_EXIST error, the file does not exist on DBFS
+    error_code_field = "error_code"
+    if error_code_field in json_response_obj:
+        if json_response_obj[error_code_field] == "RESOURCE_DOES_NOT_EXIST":
+            return False
+        raise ExecutionException("Got unexpected error response when checking whether file %s "
+                                 "exists in DBFS: %s" % json_response_obj)
+    return True
+
+
+def _upload_project_to_dbfs(project_dir, experiment_id):
+    """
+    Tars a project directory into an archive in a temp dir and uploads it to DBFS, returning
+    the HDFS-style URI of the tarball in DBFS (e.g. dbfs:/path/to/tar).
+
+    :param project_dir: Path to a directory containing an MLflow project to upload to DBFS (e.g.
+                        a directory containing an MLproject file).
+    """
+    temp_tarfile_dir = tempfile.mkdtemp()
+    temp_tar_filename = file_utils.build_path(temp_tarfile_dir, "project.tar.gz")
+
+    def custom_filter(x):
+        return None if os.path.basename(x.name) == "mlruns" else x
+
+    try:
+        file_utils.make_tarfile(temp_tar_filename, project_dir, DB_TARFILE_ARCHIVE_NAME,
+                                custom_filter=custom_filter)
+        with open(temp_tar_filename, "rb") as tarred_project:
+            tarfile_hash = hashlib.sha256(tarred_project.read()).hexdigest()
+        # TODO: Get subdirectory for experiment from the tracking server
+        dbfs_fuse_uri = os.path.join("/dbfs", DBFS_EXPERIMENT_DIR_BASE, str(experiment_id),
+                                     "projects-code", "%s.tar.gz" % tarfile_hash)
+        if not _dbfs_path_exists(dbfs_fuse_uri):
+            _upload_to_dbfs(temp_tar_filename, dbfs_fuse_uri)
+            eprint("=== Finished uploading project to %s ===" % dbfs_fuse_uri)
+        else:
+            eprint("=== Project already exists in DBFS ===")
+    finally:
+        shutil.rmtree(temp_tarfile_dir)
+    return dbfs_fuse_uri
+
+
+def _get_run_result_state(databricks_run_id):
+    """
+    Returns the run result state (string) of the Databricks run with the passed-in ID, or None
+    if the run is still active. See possible values at
+    https://docs.databricks.com/api/latest/jobs.html#runresultstate.
+    """
+    res = _jobs_runs_get(databricks_run_id)
+    return res["state"].get("result_state", None)
+
+
+def _run_shell_command_job(project_uri, command, env_vars, cluster_spec):
+    """
+    Runs the specified shell command on a Databricks cluster.
+    :param project_uri: URI of the project from which our shell command originates
+    :param command: Shell command to run
+    :param env_vars: Environment variables to set in the process running `command`
+    :param cluster_spec: Dictionary describing the cluster, expected to contain the fields for a
+                         NewCluster (see
+                         https://docs.databricks.com/api/latest/jobs.html#jobsclusterspecnewcluster)
+    :return: The ID of the Databricks Job Run. Can be used to query the run's status via the
+             Databricks Runs Get API (https://docs.databricks.com/api/latest/jobs.html#runs-get).
+    """
+    # Make jobs API request to launch run.
+    req_body_json = {
+        'run_name': 'MLflow Run for %s' % project_uri,
+        'new_cluster': cluster_spec,
+        'shell_command_task': {
+            'command': command,
+            "env_vars": env_vars
+        },
+        "libraries": [{"pypi": {"package": "mlflow==%s" % VERSION}}]
+    }
+    run_submit_res = _jobs_runs_submit(req_body_json)
+    databricks_run_id = run_submit_res["run_id"]
+    eprint("=== Launched MLflow run as Databricks job run with ID %s. Getting run status "
+           "page URL... ===" % databricks_run_id)
+    run_info = _jobs_runs_get(databricks_run_id)
+    jobs_page_url = run_info["run_page_url"]
+    eprint("=== Check the run's status at %s ===" % jobs_page_url)
+    return databricks_run_id
+
+
+def _parse_dbfs_uri_path(dbfs_uri):
+    """
+    Parses and returns the absolute path within DBFS of the file with the specified URI. For
+    example, given an input of "dbfs:/my/dbfs/path", this method will return "/my/dbfs/path"
+    """
+    return urllib.parse.urlparse(dbfs_uri).path
+
+
+def _fetch_and_clean_project(uri, version=None, git_username=None, git_password=None):
+    """
+    Fetches the project at the passed-in URI & prepares it for upload to DBFS. Returns the path of
+    the temporary directory into which the project was fetched.
+    """
+    work_dir = _fetch_project(
+        uri=uri, force_tempdir=True, version=version, git_username=git_username,
+        git_password=git_password)
+    # Remove the mlruns directory from the fetched project to avoid cache-busting
+    mlruns_dir = os.path.join(work_dir, "mlruns")
+    if os.path.exists(mlruns_dir):
+        shutil.rmtree(mlruns_dir)
+    return work_dir
+
+
+def _before_run_validations(tracking_uri, cluster_spec):
+    """Validations to perform before running a project on Databricks."""
+    _check_databricks_auth_available()
+    if cluster_spec is None:
+        raise ExecutionException("Cluster spec must be provided when launching MLflow project runs "
+                                 "on Databricks.")
+    if tracking.utils._is_local_uri(tracking_uri):
+        raise ExecutionException(
+            "When running on Databricks, the MLflow tracking URI must be set to a remote URI "
+            "accessible to both the current client and code running on Databricks. Got local "
+            "tracking URI %s." % tracking_uri)
+
+
+def run_databricks(remote_run, uri, entry_point, work_dir, parameters, experiment_id, cluster_spec):
+    """
+    Runs the project at the specified URI on Databricks, returning a `SubmittedRun` that can be
+    used to query the run's status or wait for the resulting Databricks Job run to terminate.
+    """
+    tracking_uri = tracking.get_tracking_uri()
+    _before_run_validations(tracking_uri, cluster_spec)
+
+    dbfs_fuse_uri = _upload_project_to_dbfs(work_dir, experiment_id)
+    env_vars = {
+        tracking._TRACKING_URI_ENV_VAR: tracking_uri,
+        tracking._EXPERIMENT_ID_ENV_VAR: experiment_id,
+    }
+    run_id = remote_run.info.run_uuid
+    eprint("=== Running entry point %s of project %s on Databricks. ===" % (entry_point, uri))
+    # Launch run on Databricks
+    with open(cluster_spec, 'r') as handle:
+        try:
+            cluster_spec = json.load(handle)
+        except ValueError:
+            eprint("Error when attempting to load and parse JSON cluster spec from file "
+                   "%s. " % cluster_spec)
+            raise
+    command = _get_databricks_run_cmd(dbfs_fuse_uri, run_id, entry_point, parameters)
+    db_run_id = _run_shell_command_job(uri, command, env_vars, cluster_spec)
+    return DatabricksSubmittedRun(db_run_id, run_id)
+
+
+def _cancel_databricks(databricks_run_id):
+    _jobs_runs_cancel(databricks_run_id)
+
+
+def _monitor_databricks(databricks_run_id, sleep_interval=30):
+    """
+    Polls a Databricks Job run (with run ID `databricks_run_id`) for termination, checking the
+    run's status every `sleep_interval` seconds.
+    """
+    result_state = _get_run_result_state(databricks_run_id)
+    while result_state is None:
+        time.sleep(sleep_interval)
+        result_state = _get_run_result_state(databricks_run_id)
+    return result_state == "SUCCESS"
+
+
+class DatabricksSubmittedRun(SubmittedRun):
+    """
+    Instance of SubmittedRun corresponding to a Databricks Job run launched to run an MLflow
+    project. Note that run_id may be None, e.g. if we did not launch the run against a tracking
+    server accessible to the local client.
+    """
+    def __init__(self, databricks_run_id, run_id):
+        super(DatabricksSubmittedRun, self).__init__()
+        self.databricks_run_id = databricks_run_id
+        self.run_id = run_id
+
+    def wait(self):
+        return _monitor_databricks(self.databricks_run_id)
+
+    def cancel(self):
+        _cancel_databricks(self.databricks_run_id)
+        self.wait()
+
+    def _get_status(self):
+        run_state = _get_run_result_state(self.databricks_run_id)
+        if run_state is None:
+            return RunStatus.RUNNING
+        if run_state == "SUCCESS":
+            return RunStatus.FINISHED
+        return RunStatus.FAILED
+
+    def get_status(self):
+        return RunStatus.to_string(self._get_status())
---
+++ packages/mlflow/mlflow-0.5.1.tar.gz/mlflow-0.5.1/mlflow/pyfunc/__init__.py
@@ -0,0 +1,343 @@
+# -*- coding: utf-8 -*-
+
+"""Export and import of generic Python models.
+
+This module defines generic filesystem format for Python models and provides utilities
+for saving and loading to and from this format. The format is self contained in the sense
+that it includes all necessary information for anyone to load it and use it. Dependencies
+are either stored directly with the model or referenced via a Conda environment.
+
+The convention for pyfunc models is to have a ``predict`` method or function with the following
+signature::
+
+    predict(data: pandas.DataFrame) -> numpy.ndarray | pandas.Series | pandas.DataFrame
+
+This convention is relied on by other MLflow components.
+
+Pyfunc model format is defined as a directory structure containing all required data, code, and
+configuration:
+
+.. code::
+
+    ./dst-path/
+            ./MLmodel: configuration
+            <code>: code packaged with the model (specified in the MLmodel file)
+            <data>: data packaged with the model (specified in the MLmodel file)
+            <env>: Conda environment definition (specified in the MLmodel file)
+
+It must contain MLmodel file in its root with "python_function" format with the following
+parameters:
+
+- loader_module [required]:
+         Python module that can load the model. Expected as module identifier
+         e.g. ``mlflow.sklearn``, it will be imported via ``importlib.import_module``.
+         The imported module must contain function with the following signature::
+
+          load_pyfunc(path: string) -> <pyfunc model>
+
+         The path argument is specified by the ``data`` parameter and may refer to a file or
+         directory.
+
+- code [optional]:
+        Relative path to a directory containing the code packaged with this model.
+        All files and directories inside this directory are added to the Python path
+        prior to importing the model loader.
+
+- data [optional]:
+         Relative path to a file or directory containing model data.
+         The path is passed to the model loader.
+
+- env [optional]:
+         Relative path to an exported Conda environment. If present this environment
+         should be activated prior to running the model.
+
+Example:
+
+.. code:: shell
+
+  >tree example/sklearn_iris/mlruns/run1/outputs/linear-lr
+  ├── MLmodel
+  ├── code
+  │   ├── sklearn_iris.py
+  │  
+  ├── data
+  │   └── model.pkl
+  └── mlflow_env.yml
+
+  >cat example/sklearn_iris/mlruns/run1/outputs/linear-lr/MLmodel
+  python_function:
+    code: code
+    data: data/model.pkl
+    loader_module: mlflow.sklearn
+    env: mlflow_env.yml
+    main: sklearn_iris
+"""
+
+import importlib
+import os
+import shutil
+import sys
+import pandas
+
+from mlflow.tracking.fluent import active_run, log_artifacts
+from mlflow import tracking
+from mlflow.models import Model
+from mlflow.utils import PYTHON_VERSION, get_major_minor_py_version
+from mlflow.utils.file_utils import TempDir
+from mlflow.utils.logging_utils import eprint
+
+FLAVOR_NAME = "python_function"
+MAIN = "loader_module"
+CODE = "code"
+DATA = "data"
+ENV = "env"
+PY_VERSION = "python_version"
+
+
+def add_to_model(model, loader_module, data=None, code=None, env=None):
+    """
+    Add a pyfunc spec to the model configuration.
+
+    Defines pyfunc configuration schema. Caller can use this to create a valid pyfunc model flavor
+    out of an existing directory structure. For example, other model flavors can use this to specify
+    how to use their output as a pyfunc.
+
+    NOTE: all paths are relative to the exported model root directory.
+
+    :param loader_module: The module to be used to load the model.
+    :param model: Existing model.
+    :param data: Path to the model data.
+    :param code: Path to the code dependencies.
+    :param env: Conda environment.
+    :return: Updated model configuration.
+    """
+    parms = {MAIN: loader_module}
+    parms[PY_VERSION] = PYTHON_VERSION
+    if code:
+        parms[CODE] = code
+    if data:
+        parms[DATA] = data
+    if env:
+        parms[ENV] = env
+    return model.add_flavor(FLAVOR_NAME, **parms)
+
+
+def _load_model_conf(path, run_id=None):
+    """Load a model configuration stored in Python function format."""
+    if run_id:
+        path = tracking.utils._get_model_log_dir(path, run_id)
+    conf_path = os.path.join(path, "MLmodel")
+    model = Model.load(conf_path)
+    if FLAVOR_NAME not in model.flavors:
+        raise Exception("Format '{format}' not found not in {path}.".format(format=FLAVOR_NAME,
+                                                                            path=conf_path))
+    return model.flavors[FLAVOR_NAME]
+
+
+def _load_model_env(path, run_id=None):
+    """
+        Get ENV file string from a model configuration stored in Python Function format.
+        Returned value is a model-relative path to a Conda Environment file,
+        or None if none was specified at model save time
+    """
+    return _load_model_conf(path, run_id).get(ENV, None)
+
+
+def load_pyfunc(path, run_id=None, suppress_warnings=False):
+    """
+    Load a model stored in Python function format.
+
+    :param suppress_warnings: If True, non-fatal warning messages associated with the model
+                              loading process will be suppressed. If False, these warning messages
+                              will be emitted.
+    """
+    if run_id:
+        path = tracking.utils._get_model_log_dir(path, run_id)
+    conf = _load_model_conf(path)
+    model_py_version = conf.get(PY_VERSION)
+    if not suppress_warnings:
+        _warn_potentially_incompatible_py_version_if_necessary(model_py_version=model_py_version)
+    if CODE in conf and conf[CODE]:
+        code_path = os.path.join(path, conf[CODE])
+        sys.path = [code_path] + _get_code_dirs(code_path) + sys.path
+    data_path = os.path.join(path, conf[DATA]) if (DATA in conf) else path
+    return importlib.import_module(conf[MAIN]).load_pyfunc(data_path)
+
+
+def _warn_potentially_incompatible_py_version_if_necessary(model_py_version):
+    if model_py_version is None:
+        eprint("The specified model does not have a specified Python version. It may be"
+               " incompatible with the version of Python that is currently running:"
+               " Python {version}".format(
+                   version=PYTHON_VERSION))
+    elif get_major_minor_py_version(model_py_version) != get_major_minor_py_version(PYTHON_VERSION):
+        eprint("The version of Python that the model was saved in, Python {model_version}, differs"
+               " from the version of Python that is currently running, Python {system_version},"
+               " and may be incompatible".format(
+                   model_version=model_py_version, system_version=PYTHON_VERSION))
+
+
+def _get_code_dirs(src_code_path, dst_code_path=None):
+    if not dst_code_path:
+        dst_code_path = src_code_path
+    return [(os.path.join(dst_code_path, x))
+            for x in os.listdir(src_code_path) if not x.endswith(".py") and not
+            x.endswith(".pyc") and not x == "__pycache__"]
+
+
+def spark_udf(spark, path, run_id=None, result_type="double"):
+    """
+    Return a Spark UDF that can be used to invoke the Python function formatted model.
+
+    Parameters passed to the UDF are forwarded to the model as a DataFrame where the names are
+    simply ordinals (0, 1, ...).
+
+    Example:
+
+    .. code:: python
+
+        predict = mlflow.pyfunc.spark_udf(spark, "/my/local/model")
+        df.withColumn("prediction", predict("name", "age")).show()
+
+    :param spark: A SparkSession object.
+    :param path: A path containing a pyfunc model.
+    :param run_id: ID of the run that produced this model. If provided, ``run_id`` is used to
+                   retrieve the model logged with MLflow.
+    :param result_type: Spark UDF type returned by the model's prediction method. Default double.
+
+    """
+
+    # Scope Spark import to this method so users don't need pyspark to use non-Spark-related
+    # functionality.
+    from mlflow.pyfunc.spark_model_cache import SparkModelCache
+    from pyspark.sql.functions import pandas_udf
+
+    if run_id:
+        path = tracking.utils._get_model_log_dir(path, run_id)
+
+    archive_path = SparkModelCache.add_local_model(spark, path)
+
+    def predict(*args):
+        model = SparkModelCache.get_or_load(archive_path)
+        schema = {str(i): arg for i, arg in enumerate(args)}
+        # Explicitly pass order of columns to avoid lexicographic ordering (i.e., 10 < 2)
+        columns = [str(i) for i, _ in enumerate(args)]
+        pdf = pandas.DataFrame(schema, columns=columns)
+        result = model.predict(pdf)
+        return pandas.Series(result)
+
+    return pandas_udf(predict, result_type)
+
+
+def _copy_file_or_tree(src, dst, dst_dir):
+    name = os.path.join(dst_dir, os.path.basename(os.path.abspath(src)))
+    if dst_dir:
+        os.mkdir(os.path.join(dst, dst_dir))
+    if os.path.isfile(src):
+        shutil.copy(src=src, dst=os.path.join(dst, name))
+    else:
+        shutil.copytree(src=src, dst=os.path.join(dst, name))
+    return name
+
+
+def save_model(dst_path, loader_module, data_path=None, code_path=(), conda_env=None,
+               model=Model()):
+    """
+    Export model as a generic Python function model.
+
+    :param dst_path: Path where the model is stored.
+    :param loader_module: The module to be used to load the model.
+    :param data_path: Path to a file or directory containing model data.
+    :param code_path: List of paths (file or dir) contains code dependencies not present in
+                      the environment. Every path in the ``code_path`` is added to the Python
+                      path before the model is loaded.
+    :param conda_env: Path to the Conda environment definition. This environment is activated
+                      prior to running model code.
+    :return: Model configuration containing model info.
+
+    """
+    if os.path.exists(dst_path):
+        raise Exception("Path '{}' already exists".format(dst_path))
+    os.makedirs(dst_path)
+    code = None
+    data = None
+    env = None
+
+    if data_path:
+        model_file = _copy_file_or_tree(src=data_path, dst=dst_path, dst_dir="data")
+        data = model_file
+
+    if code_path:
+        for path in code_path:
+            _copy_file_or_tree(src=path, dst=dst_path, dst_dir="code")
+        code = "code"
+
+    if conda_env:
+        shutil.copy(src=conda_env, dst=os.path.join(dst_path, "mlflow_env.yml"))
+        env = "mlflow_env.yml"
+
+    add_to_model(model, loader_module=loader_module, code=code, data=data, env=env)
+    model.save(os.path.join(dst_path, 'MLmodel'))
+    return model
+
+
+def log_model(artifact_path, **kwargs):
+    """
+    Export model in Python function form and log it with current MLflow tracking service.
+
+    Model is exported by calling :py:meth:`save_model` and logging the result with
+    :py:meth:`mlflow.tracking.log_artifacts`.
+    """
+    with TempDir() as tmp:
+        local_path = tmp.path(artifact_path)
+        run_id = active_run().info.run_uuid
+        if 'model' in kwargs:
+            raise Exception("Unused argument 'model'. log_model creates a new model object")
+
+        save_model(dst_path=local_path, model=Model(artifact_path=artifact_path, run_id=run_id),
+                   **kwargs)
+        log_artifacts(local_path, artifact_path)
+
+
+def get_module_loader_src(src_path, dst_path):
+    """
+    Generate Python source of the model loader.
+
+    Model loader contains ``load_pyfunc`` method with no parameters. It hardcodes model
+    loading of the given model into a Python source. This is done so that the exported model has no
+    unnecessary dependencies on MLflow or any other configuration file format or parsing library.
+
+    :param src_path: Current path to the model.
+    :param dst_path: Relative or absolute path where the model will be stored in the deployment
+                     environment.
+    :return: Python source code of the model loader as string.
+
+    """
+    conf_path = os.path.join(src_path, "MLmodel")
+    model = Model.load(conf_path)
+    if FLAVOR_NAME not in model.flavors:
+        raise Exception("Format '{format}' not found not in {path}.".format(format=FLAVOR_NAME,
+                                                                            path=conf_path))
+    conf = model.flavors[FLAVOR_NAME]
+    update_path = ""
+    if CODE in conf and conf[CODE]:
+        src_code_path = os.path.join(src_path, conf[CODE])
+        dst_code_path = os.path.join(dst_path, conf[CODE])
+        code_path = ["os.path.abspath('%s')" % x
+                     for x in [dst_code_path] + _get_code_dirs(src_code_path, dst_code_path)]
+        update_path = "sys.path = {} + sys.path; ".format("[%s]" % ",".join(code_path))
+
+    data_path = os.path.join(dst_path, conf[DATA]) if (DATA in conf) else dst_path
+    return loader_template.format(update_path=update_path, main=conf[MAIN], data_path=data_path)
+
+
+loader_template = """
+
+import importlib
+import os
+import sys
+
+def load_pyfunc():
+    {update_path}return importlib.import_module('{main}').load_pyfunc('{data_path}')
+
+"""
