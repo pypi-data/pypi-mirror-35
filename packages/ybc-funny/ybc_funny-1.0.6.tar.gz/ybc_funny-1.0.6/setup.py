feef0fe5a52893d4639097b801a6693066522df3
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-05 15:24:20 +0000

    Add package ybc-funny/1.0.6/ybc_funny-1.0.6.tar.gz

---
+++ packages/ybc-funny/ybc_funny-1.0.6.tar.gz/ybc_funny-1.0.6/setup.py
@@ -0,0 +1,12 @@
+from distutils.core import setup
+setup(
+  name = 'ybc_funny',
+  packages = ['ybc_funny'],
+  version = '1.0.6',
+  description = 'Get The Funny Text',
+  long_description='Get The Funny Text',
+  author = 'KingHS',
+  author_email = '382771946@qq.com',
+  keywords = ['pip3', 'python3','python','brain','funny','joke'],
+  license='MIT',
+)
