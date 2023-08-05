8316f861823931b6bb0d0a8c248676b4f808544e
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-05 17:05:59 +0000

    Add package negima/0.1.3/negima-0.1.3.tar.gz

---
+++ packages/negima/negima-0.1.3.tar.gz/negima-0.1.3/setup.py
@@ -0,0 +1,40 @@
+from setuptools import setup, find_packages
+
+try:
+    with open('README.md') as f:
+        readme = f.read()
+except IOError:
+    readme = ''
+
+
+def _requires_from_file(filename):
+    return open(filename).read().splitlines()
+
+
+setup(
+    name='negima',
+    version='0.1.3',
+    url='https://github.com/cocodrips/negima',
+    author='cocodrips',
+    author_email='cocodrips@gmail.com',
+    description='Extract phrases in Japanese text '
+                'by using the part-of-speeches based rules you defined.',
+    python_requires='>=3.4',
+    install_requires=[
+        'mecab-python3>=0.7',
+        'pandas>=0.19',
+        'xlrd>=1.1.0'
+    ],
+    extras_require={
+        'dev': [
+            'pytest>=3',
+        ],
+    },
+    long_description=readme,
+    long_description_content_type='text/markdown',
+    license='MIT',
+    packages=find_packages(),
+    classifiers=[
+        'Programming Language :: Python :: 3',
+    ],
+)
