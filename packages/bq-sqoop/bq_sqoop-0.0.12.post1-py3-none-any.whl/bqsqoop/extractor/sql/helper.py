f2981f2cdac53662ba874ead40efe45bbf69c0a0
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-05 15:06:03 +0000

    Add package bq-sqoop/0.0.12.post1/bq_sqoop-0.0.12.post1-py3-none-any.whl

---
+++ packages/bq-sqoop/bq_sqoop-0.0.12.post1-py3-none-any.whl/bqsqoop/extractor/sql/helper.py
@@ -0,0 +1,93 @@
+import os
+import uuid
+import time
+import json
+import logging
+import traceback
+import pandas as pd
+
+import sqlalchemy
+from datetime import datetime
+from bqsqoop.utils import parquet_util, pandas_util
+
+
+def get_results_cursor(sql_bind, query, pool_timeout=300):
+    engine = sqlalchemy.create_engine(sql_bind, pool_timeout=pool_timeout)
+    connection = engine.connect()
+    return connection.execute(query)
+
+
+def get_streaming_result_proxy(sql_bind, query, pool_timeout=300):
+    engine = sqlalchemy.create_engine(sql_bind, pool_timeout=pool_timeout)
+    connection = engine.connect()
+    proxy = connection.execution_options(
+        stream_results=True).execute(query)
+    return proxy
+
+
+def get_table_schema(sql_bind, table_name, pool_timeout=300):
+    engine = sqlalchemy.create_engine(sql_bind, pool_timeout=pool_timeout)
+    insp = sqlalchemy.inspect(engine)
+    columns = insp.get_columns(table_name)
+    table_schema = {}
+    for column in columns:
+        table_schema[column["name"]] = column["type"].python_type
+    return table_schema
+
+
+def export_to_parquet(worker_id, sql_bind, query, filter_field, start_pos,
+                      end_pos, output_folder, progress_bar=True,
+                      fetch_size=100, table_schema=None):
+    try:
+        start_time = int(time.time())
+        output_file = os.path.join(output_folder, "{}.parq".format(
+            str(uuid.uuid4())[:8]))
+        if start_pos and end_pos:
+            filter_query = "{0} >= {1} AND {0} <= {2}".format(
+                filter_field, start_pos, end_pos)
+            query = query % filter_query
+        parquetUtil = parquet_util.ParquetUtil(output_file)
+        parquet_schema = None
+        if table_schema:
+            parquet_schema = parquet_util.build_pyarrow_schema(table_schema)
+        logging.debug(output_file)
+        logging.debug(query)
+        proxy = get_streaming_result_proxy(sql_bind, query)
+        results = proxy.fetchmany(fetch_size)
+        columns = [key[0] for key in proxy.cursor.description]
+        while True:
+            if results:
+                items = []
+                for row in results:
+                    row_dict = {}
+                    for x, y in zip(columns, row):
+                        row_dict[x] = _data_type_transform(y)
+                    items.append(row_dict)
+                df = pd.DataFrame.from_dict(items)
+                df = pandas_util.PandasUtil.fix_dataframe(
+                    df, drop_timezones=True)
+                parquetUtil.append_df_to_parquet(df, schema=parquet_schema)
+                results = proxy.fetchmany(fetch_size)
+            else:
+                break
+        parquetUtil.close()
+        logging.info(
+            "Dump -> worker_id: {0}, time_taken: {1} secs".format(
+                worker_id, int(time.time()) - start_time))
+        return output_file
+    except Exception:
+        logging.error(traceback.format_exc())
+        raise
+
+
+def _data_type_transform(value, compound_types_to_json_string=True):
+    value_transformed = value
+    if type(value) is datetime:
+        if(value.utcoffset()):  # if timestamp is given with timezone
+            value_transformed = value.replace(tzinfo=None) - value.utcoffset()
+        else:   # time is already in UTC
+            value_transformed = value.replace(tzinfo=None)
+    elif compound_types_to_json_string and \
+            isinstance(value, (dict, list, tuple)):
+        value_transformed = json.dumps(value)
+    return value_transformed
---
+++ packages/bq-sqoop/bq_sqoop-0.0.12.post1-py3-none-any.whl/bqsqoop/utils/parquet_util.py
@@ -0,0 +1,116 @@
+import pyarrow as pa
+import pyarrow.parquet as pq
+from decimal import Decimal
+from datetime import datetime
+
+
+class ParquetUtil():
+    def __init__(self, output_file):
+        """Helper for writing Parquet files
+
+        Args:
+            output_file (str): output file name with full path
+        """
+        self._output_file = output_file
+        self._pqwriter = None
+
+    def build_pyarrow_schema(self, column_schema):
+        """Convert columnname-type KV pairs to pyarrow.schema
+
+        Args:
+            column_schema (dict): A schema of columne_name -> type,
+                if any of the provided columns are not found,
+                this will help create a null column for it. eg.,
+                    {
+                        "column_name_1": "string",
+                        "column_name_2": "date",
+                        "column_name_3": "int",
+                        ..etc
+                    }
+
+        Returns: (pyarrow.Schema)
+            returns built schema for pyarrow
+        """
+        fields = []
+        column_types_map = {
+            "string": pa.string(),
+            "str": pa.string(),
+            "text": pa.string(),
+            "integer": pa.int64(),
+            "long": pa.int64(),
+            "float": pa.float64(),
+            "bool": pa.binary(),
+            "boolean": pa.binary(),
+            "date": pa.timestamp('ns'),
+            "datetime": pa.timestamp('ns'),
+            # For python types
+            int: pa.int64(),
+            Decimal: pa.float64(),
+            str: pa.string(),
+            datetime: pa.timestamp('ns'),
+            bool: pa.binary(),
+        }
+        for col_name, col_type in column_schema.items():
+            fields.append(
+                pa.field(
+                    name=col_name,
+                    type=column_types_map.get(col_type, pa.string()),
+                    nullable=True))
+        return pa.schema(fields=fields)
+
+    def write_df_to_parquet(self, df, preserve_index=False, close_writer=True,
+                            schema=None):
+        """Writes Pandas Dataframe to a new Parquet file
+
+        Closes the writer after writing all of dataframe and before returning
+
+        Args:
+            df (Pandas Datafram): Data to be written to parquet file.
+            preserve_index (bool): Set this to True if you want to
+                preserve the index of pandas dataframe, default is False
+                and indexes are dropped.
+            close_writer (bool): Set to true closes the writer at the end
+                Default: True
+            schema (pyarrow.Schema, optional): The expected schema of the
+                Arrow Table. This can be used to indicate the type of columns
+                if we cannot infer it automatically.
+        """
+        table = pa.Table.from_pandas(df, preserve_index=preserve_index,
+                                     schema=schema)
+        self._pqwriter = pq.ParquetWriter(self._output_file, table.schema)
+        self._pqwriter.write_table(table)
+        if close_writer:
+            self.close()
+
+    def append_df_to_parquet(self, df, preserve_index=False, schema=None):
+        """Writes Pandas Dataframe to a new Parquet file in Append mode
+
+        Can be used to append data to the same parquet file multiple times,
+        need to call close() funciton after done with all appends.
+
+        Args:
+            df (Pandas Datafram): Data to be written to parquet file.
+            preserve_index (bool): Set this to True if you want to
+                preserve the index of pandas dataframe, default is False
+                and indexes are dropped.
+            schema (pyarrow.Schema, optional): The expected schema of the
+                Arrow Table. This can be used to indicate the type of columns
+                if we cannot infer it automatically.
+        """
+        if not self._pqwriter:
+            self.write_df_to_parquet(df, preserve_index=preserve_index,
+                                     close_writer=False, schema=schema)
+        else:
+            table = pa.Table.from_pandas(
+                df, preserve_index=preserve_index, schema=schema)
+            self._pqwriter.write_table(table)
+
+    def close(self):
+        """Closes the parquet writer to the output file
+
+        It's safe to call it multiple times, will only close if a writer
+        is open.
+        """
+        if self._pqwriter:
+            self._pqwriter.close()
+            self._pqwriter = None
