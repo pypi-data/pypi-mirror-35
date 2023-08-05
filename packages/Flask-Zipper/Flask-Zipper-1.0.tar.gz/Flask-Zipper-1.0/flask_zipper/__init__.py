fa8224d71fc45c6177bcebbef147bd978fd6ea3c
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-05 20:11:26 +0000

    Add package Flask-Zipper/1.0/Flask-Zipper-1.0.tar.gz

---
+++ packages/Flask-Zipper/Flask-Zipper-1.0.tar.gz/Flask-Zipper-1.0/flask_zipper/__init__.py
@@ -0,0 +1,2 @@
+from .zipper import Zipper
+from .decorators import encode_brotli, encode_deflate, encode_gzip
---
+++ packages/Flask-Zipper/Flask-Zipper-1.0.tar.gz/Flask-Zipper-1.0/flask_zipper/compressor.py
@@ -0,0 +1,44 @@
+import gzip
+import zlib
+
+import brotli
+from flask import current_app
+
+
+def encode_response_data_with_brotli(response):
+    """
+    A response compressor uses brotli algorithm
+
+    :param response: Flask response object
+    """
+    response.data = brotli.compress(
+        string=response.data,
+        mode=current_app.config['BROTLI_MODE'],
+        quality=current_app.config['BROTLI_QUALITY_LEVEL'],
+        lgwin=current_app.config['BROTLI_SLIDING_WINDOW_SIZE'],
+        lgblock=current_app.config['BROTLI_MAX_INPUT_BLOCK_SIZE']
+    )
+
+
+def encode_response_data_with_deflate(response):
+    """
+    A response compressor uses zlib
+
+    :param response: Flask response object
+    """
+    response.data = zlib.compress(
+        response.data,
+        current_app.config['DEFLATE_COMPRESS_LEVEL']
+    )
+
+
+def encode_response_data_with_gzip(response):
+    """
+    A response compressor uses gzip
+
+    :param response: Flask response object
+    """
+    response.data = gzip.compress(
+        data=response.data,
+        compresslevel=current_app.config['GZIP_COMPRESS_LEVEL']
+    )
