eb57e5b8283001ffe06326d05b4f29bbc13ada3e
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-05 18:08:21 +0000

    Add package msb_client/0.7.5/msb_client-0.7.5-py2.py3-none-any.whl

---
+++ packages/msb_client/msb_client-0.7.5-py2.py3-none-any.whl/msb_client/ComplexDataFormat.py
@@ -0,0 +1,94 @@
+# -*- coding: utf-8 -*-
+"""
+Copyright (c) 2017
+Fraunhofer Institute for Manufacturing Engineering
+and Automation (IPA)
+Author: Daniel Stock
+mailto: daniel DOT stock AT ipa DOT fraunhofer DOT com
+See the file "LICENSE" for the full license governing this code.
+"""
+
+from .DataType import getDataType
+
+
+class ComplexDataFormat:
+    def __init__(self, objectName):
+        self.objectName = objectName
+        dataFormat = {}
+        dataFormat[objectName] = {}
+        dataFormat["dataObject"] = {}
+        dataFormat[objectName]["type"] = "object"
+        dataFormat["dataObject"]["$ref"] = "#/definitions/" + self.objectName
+        self.dataFormat = dataFormat
+
+    objectName = ""
+
+    def getDataFormat(self):
+        return self.dataFormat
+
+    def addProperty(self, propertyName, dataType, isArray=None):
+        if isArray:
+            if "properties" not in self.dataFormat[self.objectName].keys():
+                self.dataFormat[self.objectName]["properties"] = {}
+            if isinstance(dataType, ComplexDataFormat):
+                dt = dataType.getDataFormat()[dataType.objectName]
+                if dt != {}:
+                    self.dataFormat[self.objectName]["properties"][propertyName] = {}
+                    self.dataFormat[self.objectName]["properties"][propertyName][
+                        "items"
+                    ] = {}
+                    self.dataFormat[self.objectName]["properties"][propertyName][
+                        "type"
+                    ] = "array"
+                    self.dataFormat[self.objectName]["properties"][propertyName][
+                        "items"
+                    ]["$ref"] = ("#/definitions/" + dataType.objectName)
+                    self.dataFormat[dataType.objectName] = dt
+            else:
+                dt = getDataType(dataType)
+                if dt != {}:
+                    if "properties" in self.dataFormat[self.objectName].keys():
+                        self.dataFormat[self.objectName]["properties"][
+                            propertyName
+                        ] = {}
+                        self.dataFormat[self.objectName]["properties"][propertyName][
+                            "type"
+                        ] = "array"
+                        self.dataFormat[self.objectName]["properties"][propertyName][
+                            "items"
+                        ] = dt
+                    else:
+                        self.dataFormat[self.objectName]["properties"] = {}
+                        self.dataFormat[self.objectName]["properties"][
+                            propertyName
+                        ] = {}
+                        self.dataFormat[self.objectName]["properties"][propertyName][
+                            "type"
+                        ] = "array"
+                        self.dataFormat[self.objectName]["properties"][propertyName][
+                            "items"
+                        ] = dt
+        else:
+            if "properties" not in self.dataFormat[self.objectName].keys():
+                self.dataFormat[self.objectName]["properties"] = {}
+            if isinstance(dataType, ComplexDataFormat):
+                self.dataFormat[dataType.objectName] = {}
+                dt = dataType.getDataFormat()[dataType.objectName]
+                if dt != {}:
+                    self.dataFormat[self.objectName]["properties"][propertyName] = {}
+                    self.dataFormat[self.objectName]["properties"][propertyName][
+                        "$ref"
+                    ] = ("#/definitions/" + dataType.objectName)
+                    self.dataFormat[dataType.objectName] = dt
+            else:
+                dt = getDataType(dataType)
+                if dt != {}:
+                    if "properties" in self.dataFormat[self.objectName].keys():
+                        self.dataFormat[self.objectName]["properties"][
+                            propertyName
+                        ] = dt
+                    else:
+                        self.dataFormat[self.objectName]["properties"] = {}
+                        self.dataFormat[self.objectName]["properties"][
+                            propertyName
+                        ] = dt
---
+++ packages/msb_client/msb_client-0.7.5-py2.py3-none-any.whl/msb_client/Function.py
@@ -0,0 +1,54 @@
+# -*- coding: utf-8 -*-
+"""
+Copyright (c) 2017
+Fraunhofer Institute for Manufacturing Engineering
+and Automation (IPA)
+Author: Daniel Stock
+mailto: daniel DOT stock AT ipa DOT fraunhofer DOT com
+See the file "LICENSE" for the full license governing this code.
+"""
+
+import json
+import copy
+
+from .ComplexDataFormat import ComplexDataFormat
+from .DataFormat import DataFormat
+import datetime
+
+
+class Function:
+    def __init__(
+        self,
+        functionId,
+        function_name,
+        function_description,
+        function_dataformat,
+        fnpointer=None,
+        isArray=False,
+        responseEvents=None,
+    ):
+        self.functionId = functionId
+        self.name = function_name
+        self.description = function_description
+        self.isArray = isArray
+        if isinstance(function_dataformat, DataFormat) or isinstance(
+            function_dataformat, ComplexDataFormat
+        ):
+            self.dataFormat = copy.deepcopy(function_dataformat).getDataFormat()
+        elif type(function_dataformat) == type(datetime):
+            self.dataFormat = DataFormat(function_dataformat, isArray).getDataFormat()
+        elif function_dataformat is None:
+            self.dataFormat = None
+        else:
+            try:
+                json_object = {"dataObject": json.loads(function_dataformat)}
+                self.dataFormat = json_object
+            except Exception:
+                self.dataFormat = DataFormat(
+                    function_dataformat, isArray
+                ).getDataFormat()
+        if responseEvents is None:
+            self.responseEvents = []
+        else:
+            self.responseEvents = responseEvents
+        self.implementation = fnpointer
