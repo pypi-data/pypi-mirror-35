a0475300ad4e1fa1fbee66a3fef269a85ec7ee17
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-05 18:10:38 +0000

    Add package pyloniex/0.0.8/pyloniex-0.0.8.tar.gz

---
+++ packages/pyloniex/pyloniex-0.0.8.tar.gz/pyloniex-0.0.8/setup.py
@@ -0,0 +1,23 @@
+#!/usr/bin/env python3
+# -*- coding: utf-8 -*-
+from setuptools import find_packages
+from setuptools import setup
+
+
+setup(
+    name='pyloniex',
+    version='0.0.8',
+
+    packages=find_packages(),
+
+    install_requires=[
+        'cryptography',
+        'requests',
+        'tenacity',
+    ],
+
+    author='Adam Rothman',
+    author_email='rothman.adam@gmail.com',
+    description='Python bindings for the Poloniex API',
+    url='https://github.com/adamrothman/pyloniex',
+)
