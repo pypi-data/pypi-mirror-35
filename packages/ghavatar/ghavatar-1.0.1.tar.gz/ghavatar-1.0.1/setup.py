367d2f2517cb70ac8864938b78055af54ae885c0
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-05 16:56:45 +0000

    Add package ghavatar/1.0.1/ghavatar-1.0.1.tar.gz

---
+++ packages/ghavatar/ghavatar-1.0.1.tar.gz/ghavatar-1.0.1/setup.py
@@ -0,0 +1,31 @@
+import setuptools
+
+with open("README.md", "r") as fh:
+    long_description = fh.read()
+
+setuptools.setup(
+    name = "ghavatar",
+    packages = ["ghavatar"],
+    long_description = long_description,
+    long_description_content_type = "text/markdown",
+    version = "1.0.1",
+    description = "Get the avatar of a GitHub user",
+    author = "Yoginth",
+    author_email = "yoginth@zoho.com",
+    url = "https://yoginth.gitlab.io",
+    classifiers=(
+        "Programming Language :: Python",
+        "Natural Language :: English",
+        "Environment :: Plugins",
+        "Operating System :: OS Independent",
+        "Intended Audience :: Developers",
+        "License :: OSI Approved :: MIT License",
+    ),
+    project_urls={
+        'Patreon': 'https://www.patreon.com/yoginth',
+        'Source': 'https://gitlab.com/yoginth/ghavatar',
+    },
+    install_requires=[
+        'ghuser',
+    ],
+)
