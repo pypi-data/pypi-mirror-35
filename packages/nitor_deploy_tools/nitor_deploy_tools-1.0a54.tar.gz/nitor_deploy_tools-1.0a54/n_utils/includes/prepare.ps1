2fad5d0180332dddad4af64c8af94998dafb0d7f
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-05 18:11:00 +0000

    Add package nitor_deploy_tools/1.0a54/nitor_deploy_tools-1.0a54.tar.gz

---
+++ packages/nitor_deploy_tools/nitor_deploy_tools-1.0a54.tar.gz/nitor_deploy_tools-1.0a54/n_utils/includes/prepare.ps1
@@ -0,0 +1,6 @@
+$url = "https://download.microsoft.com/download/7/9/6/796EF2E4-801B-4FC4-AB28-B59FBF6D907B/VCForPython27.msi"
+$output = "C:\nitor\VCForPython27.msi"
+(New-Object System.Net.WebClient).DownloadFile($url, $output)
+Start-Process msiexec -ArgumentList @("/i", "C:\nitor\VCForPython27.msi", "/passive", "/quiet") -NoNewWindow -Wait
+
+C:\Python27\python.exe -m pip install -U pip setuptools awscli boto3 nitor-deploy-tools
