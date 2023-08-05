1bba40dc8a7f03a1897f9257ab0478e830fe79ce
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-05 15:24:38 +0000

    Add package playground-data/0.7.0/playground-data-0.7.0.tar.gz

---
+++ packages/playground-data/playground-data-0.7.0.tar.gz/playground-data-0.7.0/plygdata/__init__.py
@@ -0,0 +1,19 @@
+
+""" This folder is processed as 'plygdata package'. Initialization modules for this package. """
+
+from plygdata import _version
+__version__ = _version.__version__
+
+# For importing main classes
+from plygdata.datacolor import DataColor
+##from .scalelinear import ScaleLinear   # This is not for user.
+from plygdata.datahelper import DatasetType, InputType, DataHelper
+from plygdata.dataset import DataGenerator
+from plygdata.playground import Player
+
+# For importing all modules
+##from plygdata import datacolor
+##from plygdata import scalelinear
+##from plygdata import dataset
+##from plygdata import datahelper
+##__all__ = ["datacolor", "scalelinear", "dataset", "datahelper"]
