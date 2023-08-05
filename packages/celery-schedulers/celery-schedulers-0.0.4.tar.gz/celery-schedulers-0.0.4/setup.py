d655aa8f29301bc1a2ffec33d291a78e3136dff3
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-05 22:13:09 +0000

    Add package celery-schedulers/0.0.4/celery-schedulers-0.0.4.tar.gz

---
+++ packages/celery-schedulers/celery-schedulers-0.0.4.tar.gz/celery-schedulers-0.0.4/setup.py
@@ -0,0 +1,31 @@
+#!/usr/bin/python
+from setuptools import setup, find_packages
+
+setup(
+    name='celery-schedulers',
+    version='0.0.4',
+    author='Brent Tubbs',
+    author_email='brent.tubbs@gmail.com',
+    packages=find_packages(),
+    include_package_data=True,
+    url='http://bits.btubbs.com/celery-schedulers',
+    description='Celery scheduler backends for Redis and MongoDB',
+    install_requires=[
+        'six',
+    ],
+    tests_require=[
+        'celery',
+        'redis',
+        'pymongo',
+    ],
+    classifiers=[
+        'Development Status :: 4 - Beta',
+        'Intended Audience :: Developers',
+        'License :: OSI Approved :: BSD License',
+        'Operating System :: OS Independent',
+        'Programming Language :: Python',
+        'Programming Language :: Python :: 2',
+        'Programming Language :: Python :: 2.7',
+        'Programming Language :: Python :: 3',
+    ],
+)
