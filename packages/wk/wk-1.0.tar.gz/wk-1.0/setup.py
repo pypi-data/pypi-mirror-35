f18ae49934923e035fa3ba8d982d1d5db9c4e5a6
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-05 15:05:36 +0000

    Add package wk/1.0/wk-1.0.tar.gz

---
+++ packages/wk/wk-1.0.tar.gz/wk-1.0/setup.py
@@ -0,0 +1,11 @@
+from distutils.core import setup
+
+setup(
+    name="wk",
+    version="1.0",
+    author="wangkui",
+    py_modules=['__init__', 'test'],
+    author_email="18860356303@163.com"
+)
+
+
