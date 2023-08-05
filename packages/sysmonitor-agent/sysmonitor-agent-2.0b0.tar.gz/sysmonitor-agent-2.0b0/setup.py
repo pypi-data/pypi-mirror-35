5f35aa83fe224e4229ca4b814337c23f769deece
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-05 21:11:30 +0000

    Add package sysmonitor-agent/2.0b0/sysmonitor-agent-2.0b0.tar.gz

---
+++ packages/sysmonitor-agent/sysmonitor-agent-2.0b0.tar.gz/sysmonitor-agent-2.0b0/setup.py
@@ -0,0 +1,30 @@
+#pylint: disable=all
+import os
+from setuptools import setup, find_packages
+
+# Load release variables
+release_file = os.path.join(os.path.dirname(__file__),
+                            "sysmonitor", "agent", "release.py")
+vals =open(release_file, "rb").read() 
+print(vals)
+exec(open(release_file, "rb").read())
+
+def requires():
+    with open("requirements.txt", "r") as rfile:
+        lines = rfile.read().splitlines()
+    return lines
+
+setup (
+        name="sysmonitor-agent",
+        version=version,
+        description=description,
+        long_description=long_desc,
+        url=url,
+        packages=find_packages(),
+        license=license,
+        author=author,
+        author_email=author_email,
+        classifiers=[c for c in classifiers.split("\n") if c],
+        install_requires=requires(),
+        python_requires=">=3.5",
+        )
