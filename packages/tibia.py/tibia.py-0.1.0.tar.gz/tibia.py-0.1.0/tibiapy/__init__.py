9bae4d812d13afc4b78c07d3baca66ce4e5e73b9
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-05 16:56:57 +0000

    Add package tibia.py/0.1.0/tibia.py-0.1.0.tar.gz

---
+++ packages/tibia.py/tibia.py-0.1.0.tar.gz/tibia.py-0.1.0/tibiapy/__init__.py
@@ -0,0 +1,7 @@
+from . import abc, utils
+from .character import Character, Death, OtherCharacter, Killer
+from .const import *
+from .errors import *
+from .guild import Guild, GuildMember, GuildInvite
+
+__version__ = '0.1.0'
