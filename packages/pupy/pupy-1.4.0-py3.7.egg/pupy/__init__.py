b77e97be3042362474ad64d258db37b040799747
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-05 21:09:13 +0000

    Add package pupy/1.4.0/pupy-1.4.0-py3.7.egg

---
+++ packages/pupy/pupy-1.4.0-py3.7.egg/pupy/__init__.py
@@ -0,0 +1,11 @@
+#!/usr/bin/env python
+# -*- coding: utf-8 -*-
+# ~ Jesse K. Rubin ~ Pretty Useful Python
+from __future__ import print_function, division, with_statement
+try: range
+except NameError: range = xrange
+from pupy.amazon_prime import *
+from pupy.decorations import *
+from pupy.listless import *
+from pupy.maths import *
+from pupy.werd import *
