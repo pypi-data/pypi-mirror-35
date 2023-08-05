d779fdc8db2a6c5fab128ea74aa50cd43082830b
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-05 15:23:56 +0000

    Add package whispyr/0.3.0/whispyr-0.3.0-py2.py3-none-any.whl

---
+++ packages/whispyr/whispyr-0.3.0-py2.py3-none-any.whl/whispyr/__init__.py
@@ -0,0 +1,24 @@
+# -*- coding: utf-8 -*-
+
+"""Top-level package for whispyr."""
+
+__author__ = """Grigory Starinkin"""
+__email__ = 'starinkin@gmail.com'
+__version__ = '0.3.0'
+
+from .whispyr import Whispir, WhispirRetry
+
+from .whispyr import Message, MessageStatus, MessageResponse, Template, \
+    Workspace, ResponseRule, Contact, App
+
+from .whispyr import WhispirError, ClientError, ServerError, JSONDecodeError
+
+__all__ = [
+    # Client
+    'Whispir', 'WhispirRetry',
+    # Resources
+    'Message', 'MessageStatus', 'MessageResponse', 'Template', 'Workspace',
+    'ResponseRule', 'Contact', 'App',
+    # Errors
+    'WhispirError', 'ClientError', 'ServerError', 'JSONDecodeError'
+]
