4ea02a76fabf86ee61e1a624f04ba8cb7f19673d
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-05 22:13:46 +0000

    Add package simple-pytimer/0.0.6/simple_pytimer-0.0.6.tar.gz

---
+++ packages/simple-pytimer/simple_pytimer-0.0.6.tar.gz/simple_pytimer-0.0.6/README.md
@@ -0,0 +1,2 @@
+# simple_pytimer
+Simple timer code to record and log execution times of Python code in a pythonic way.
---
+++ packages/simple-pytimer/simple_pytimer-0.0.6.tar.gz/simple_pytimer-0.0.6/setup.py
@@ -0,0 +1,8 @@
+from setuptools import setup, find_packages
+
+setup(
+    name='simple_pytimer',
+    version='0.0.6',
+    license='',
+    packages=find_packages()
+)
