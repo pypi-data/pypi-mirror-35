923214407b39bf18613eab2e966b3999cefc8010
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-05 22:13:32 +0000

    Add package channels-irc/0.5.1/channels_irc-0.5.1.tar.gz

---
+++ packages/channels-irc/channels_irc-0.5.1.tar.gz/channels_irc-0.5.1/setup.py
@@ -0,0 +1,28 @@
+import os
+from setuptools import setup, find_packages
+
+from channels_irc import __version__
+
+readme = os.path.join(os.path.dirname(__file__), 'PYPI_README.rst')
+
+setup(
+    name='channels_irc',
+    version=__version__,
+    url='https://github.com/AdvocatesInc/channels-chatbot',
+    author='Advocates, Inc',
+    author_email='admin@adv.gg',
+    description='Interface server connecting Django\'s channels and IRC',
+    long_description=open(readme).read(),
+    license='Proprietary and confidential',
+    zip_safe=False,
+    packages=find_packages(),
+    include_package_data=True,
+    install_requires=[
+        'irc>=16.4',
+        'asgiref>=2.3.0',
+        'channels>=2.0.2',
+    ],
+    entry_points={'console_scripts': [
+        'channels-irc = channels_irc.cli:CLI.entrypoint'
+    ]},
+)
