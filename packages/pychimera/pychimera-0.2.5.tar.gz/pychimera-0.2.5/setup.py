933895b4feadd6fbd253221b897d5cfd23748724
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-06 14:37:47 +0000

    Add package pychimera/0.2.5/pychimera-0.2.5.tar.gz

---
+++ packages/pychimera/pychimera-0.2.5.tar.gz/pychimera-0.2.5/setup.py
@@ -0,0 +1,42 @@
+#!/usr/bin/env python
+# -*- coding: utf-8 -*-
+
+from setuptools import setup
+import os
+import sys
+import versioneer
+
+def read(fname):
+    return open(os.path.join(os.path.dirname(__file__), fname)).read()
+
+version = versioneer.get_version()
+if sys.platform.startswith('win') or sys.platform == 'cygwin':
+    scripts = {'scripts': ['scripts/pychimera'],
+               'entry_points': {'console_scripts': ['pychimera = pychimera.__main__:run']}}
+else:
+    scripts = {'scripts': ['scripts/pychimera']}
+setup(
+    name='pychimera',
+    version=version,
+    cmdclass=versioneer.get_cmdclass(),
+    url='https://github.com/insilichem/pychimera',
+    download_url='https://github.com/insilichem/pychimera/tarball/v' + version,
+    license='LGPL',
+    author="Jaime Rodríguez-Guerra",
+    author_email='jaime.rogue@gmail.com',
+    description='Use UCSF Chimera Python API in a standard Python 2.7 interpreter.',
+    long_description=read('README.rst'),
+    packages=['pychimera', 'pychimera.platforms'],
+    platforms='any',
+    classifiers=[
+        'Programming Language :: Python :: 2.7',
+        'Development Status :: 3 - Alpha',
+        'Natural Language :: English',
+        'Environment :: Console',
+        'Intended Audience :: Science/Research',
+        'License :: OSI Approved :: GNU Library or Lesser General Public License (LGPL)',
+        'Operating System :: OS Independent',
+        'Topic :: Scientific/Engineering :: Chemistry',
+    ],
+    **scripts
+)
