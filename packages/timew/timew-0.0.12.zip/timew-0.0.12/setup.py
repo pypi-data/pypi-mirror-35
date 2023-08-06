1f64f5e335664140290bb04550fdf9f55b390b76
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-06 16:16:12 +0000

    Add package timew/0.0.12/timew-0.0.12.zip

---
+++ packages/timew/timew-0.0.12.zip/timew-0.0.12/setup.py
@@ -0,0 +1,27 @@
+#!/usr/bin/env python3
+
+from setuptools import find_packages, setup
+
+setup(
+    setup_requires=['setuptools-markdown', 'setuptools-scm'],
+    name='timew',
+    use_scm_version=True,
+    author='Tjaart van der Walt',
+    author_email='tjaart@tjaart.org',
+    summary='Python bindings for your timewarrior database',
+    long_description_markdown_filename='README.md',
+    project_urls={
+        'Homepage': 'https://tjaart.gitlab.io/python-timew',
+        'Bug Tracker': 'https://gitlab.com/tjaart/python-timew/issues',
+        'Documentation': 'https://gitlab.com/tjaart/python-timew',
+        'Source Code': 'https://gitlab.com/tjaart/python-timew'
+    },
+    packages=find_packages('timew'),
+    classifiers=[
+        'Development Status :: 4 - Beta',
+        'Intended Audience :: Developers',
+        'Operating System :: POSIX',
+        'Programming Language :: Python :: 3'],
+    keywords='timewarrior',
+    license='MIT'
+)
