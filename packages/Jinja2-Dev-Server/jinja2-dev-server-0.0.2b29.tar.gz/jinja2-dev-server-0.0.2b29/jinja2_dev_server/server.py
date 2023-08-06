8e341d0f908b72accf8f0ec2fabea663cf14dfe9
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-06 13:20:44 +0000

    Add package Jinja2-Dev-Server/0.0.2b29/jinja2-dev-server-0.0.2b29.tar.gz

---
+++ packages/Jinja2-Dev-Server/jinja2-dev-server-0.0.2b29.tar.gz/jinja2-dev-server-0.0.2b29/jinja2_dev_server/server.py
@@ -0,0 +1,54 @@
+import os
+
+from flask import Flask
+from flask import request, render_template
+
+templates = {}
+
+debug = False
+
+app = Flask(__name__, template_folder=".")
+
+
+def run_server(dirs, debug_opt):
+    global debug
+    debug = debug_opt
+    if type(dirs) == str:
+        dirs = [dirs]
+    for directory in dirs:
+        read_files(directory)
+
+    start_flask_server()
+
+
+def read_files(dir):
+    global debug
+    thisdir = os.getcwd()
+
+    # r=root, d=directories, f = files
+    for r, d, f in os.walk(thisdir):
+        for file in f:
+            if debug:
+                print(file)
+            if ".jinja2" in file:
+                if debug:
+                    print("Found " + file)
+                templates[file[:file.find(".")]] = os.path.join(r, file)
+
+    if len(templates) == 0:
+        raise Exception("No files found")
+    if debug:
+        print(templates)
+
+
+def start_flask_server():
+    app.run(debug=True)
+
+
+@app.route('/<path:path>')
+def serve_template(path):
+    if debug:
+        print(path, path in templates)
+    if path in templates:
+        return render_template(templates[path])
+    return 404
