5923129c25b759e881f1715fd0f99b603fc67bdc
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-06 14:59:26 +0000

    Add package rundeck-resources/0.1.0/rundeck_resources-0.1.0-py3-none-any.whl

---
+++ packages/rundeck-resources/rundeck_resources-0.1.0-py3-none-any.whl/rundeck_resources/config.py
@@ -0,0 +1,22 @@
+import configparser
+from .common import check_file
+
+
+def read_config(path: str) -> dict:
+    """
+    Method to read the init configuration file.
+
+    :param path: The path to the init configuration file.
+    :type path: str
+    :returns: The content of the configuration file.
+    :rtype: dict
+    """
+    config_path = check_file(path)
+    config = configparser.ConfigParser()
+    config.read(config_path)
+    configuration = {}
+    for section in config.sections():
+        configuration[section] = {}
+        for key, val in config.items(section):
+            configuration[section][key] = val
+    return configuration
