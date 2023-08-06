f135f580f4d319683dc93098e0f1d006396d2971
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-06 13:22:18 +0000

    Add package cursebox-lib/1.0.1/cursebox-lib-1.0.1.tar.gz

---
+++ packages/cursebox-lib/cursebox-lib-1.0.1.tar.gz/cursebox-lib-1.0.1/setup.py
@@ -0,0 +1,33 @@
+# -*- encoding: utf-8 -*-
+
+from setuptools import setup, find_packages
+
+with open("README.md", "r") as f:
+    README = f.read()
+
+setup(
+    name="cursebox-lib",
+    packages=find_packages(),
+    version="1.0.1",
+    description="Curses made simple",
+    long_description=README,
+    long_description_content_type="text/markdown",
+    author="Tenchi",
+    author_email="tenchi@team2xh.net",
+    url="https://github.com/Tenchi2xh/cursebox",
+    project_urls={
+        "Documentation": "https://cursebox.readthedocs.io/en/latest/"
+    },
+    keywords=["terminal", "termbox", "curses"],
+    classifiers=[
+        "Development Status :: 4 - Beta",
+        'Programming Language :: Python :: 2.7',
+        'Programming Language :: Python :: 3.3',
+        'Programming Language :: Python :: 3.4',
+        'Programming Language :: Python :: 3.5'
+    ],
+    install_requires=["six"],
+    extras_require={
+        "test": ["pytest"]
+    }
+)
