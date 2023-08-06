95fa786c74fb98ba0d81fcee71380e2c2770780a
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-06 14:55:01 +0000

    Add package utah/0.1/utah-0.1.tar.gz

---
+++ packages/utah/utah-0.1.tar.gz/utah-0.1/setup.py
@@ -0,0 +1,11 @@
+from setuptools import setup
+
+setup(name='utah',
+      version='0.1',
+      description='The official module for Utah Python / SLCPython',
+      url='http://github.com/slcpython/utah',
+      author='Faris Chebib',
+      author_email='faris@theluckybead.com',
+      license='MIT',
+      packages=['utah'],
+      zip_safe=False)
