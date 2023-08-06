0b90c50ad546caa3a9524cf5e9b9f6def5fe3f30
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-06 04:13:57 +0000

    Add package AMONG-py/0.0.3/AMONG-py-0.0.3.tar.gz

---
+++ packages/AMONG-py/AMONG-py-0.0.3.tar.gz/AMONG-py-0.0.3/README.md
@@ -0,0 +1,14 @@
+# AMONG-py
+This project is a python part of AMONG project
+
+## version 0.0.1
+* visualization.result_chart_js : json output for chart.js
+
+## version 0.0.1.2
+* analysis.get_recommended_project : json output for recommended project(very simple way)
+
+## version 0.0.2
+* input adjusted for exam_logs
+
+## version 0.0.3
+* .py files are moved toAMONGpy directory
---
+++ packages/AMONG-py/AMONG-py-0.0.3.tar.gz/AMONG-py-0.0.3/setup.py
@@ -0,0 +1,18 @@
+import setuptools
+
+setuptools.setup(
+    name="AMONG-py",
+    version="0.0.3",
+    license='MIT',
+    author="BlessingDev",
+    author_email="blessingdev@khu.ac.kr",
+    long_description="python part of AMON Project",
+    long_description_content_type="text/markdown",
+    url="",
+    packages=setuptools.find_packages(),
+    classifiers=[
+        "Programming Language :: Python :: 3",
+        "License :: OSI Approved :: MIT License",
+        "Operating System :: OS Independent"
+    ],
+)
