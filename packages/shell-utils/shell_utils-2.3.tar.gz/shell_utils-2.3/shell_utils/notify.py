4fc0459feaed76c5f163d7eb9350f2443046a9a7
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-06 14:56:25 +0000

    Add package shell-utils/2.3/shell_utils-2.3.tar.gz

---
+++ packages/shell-utils/shell_utils-2.3.tar.gz/shell_utils-2.3/shell_utils/notify.py
@@ -0,0 +1,112 @@
+import subprocess as sp
+import typing as typ
+import logging
+import shlex
+import sys
+from functools import wraps
+
+import click
+
+
+def notify(message: str, title=None, subtitle=None, sound=None):
+    """
+    Send a Mac OS notification.
+
+    Args:
+        message: the notification body
+        title: the title of the notification
+        subtitle: the subtitle of the notification
+        sound: the sound the notification makes
+
+    see https://apple.stackexchange.com/questions/57412/how-can-i-trigger-a-notification-center-notification-from-an-applescript-or-shel/115373#115373
+    """
+    if title is None:
+        title = 'heads up'
+    if subtitle is None:
+        subtitle = 'something happened'
+
+    if sys.platform != 'darwin':
+        logging.warning('This function is designed to work on Mac OS')
+
+    # There is probably a less hacky way to escape single quotes safely
+    # but I haven't gotten to it
+
+    message = message.replace("'", '')
+
+    command = f"""osascript -e 'display notification "{message}" with title "{title}" subtitle "{subtitle}" sound name "{sound}"' """
+    sp.run(shlex.split(command), check=False)
+
+
+def notice(message: typ.Optional[str] = None, title=None, subtitle=None, sound=None):
+    """
+    Returns a decorator that allows you to be notified when a function returns.
+
+    Args:
+        message: the notification body
+        title: the title of the notification
+        subtitle: the subtitle of the notification
+        sound: the sound the notification makes
+
+    Returns: a function
+    """
+
+    def decorator(func):
+        """
+        Send notification that task has finished.
+
+        Especially useful for long-running tasks
+        """
+
+        @wraps(func)
+        def inner(*args, **kwargs):
+            nonlocal message, title, subtitle, sound
+
+            title = getattr(func, '__name__') + \
+                ' finished' if title is None else title
+
+            _subtitle = None
+
+            _sound = None
+
+            result = None
+
+            try:
+                _result = func(*args, **kwargs)
+                if isinstance(_result, str):
+                    result = _result
+            except:
+                _subtitle = 'Failure'
+                _sound = 'Basso'
+                raise
+            finally:
+                if message is not None:
+                    message = message
+                elif result is not None:
+                    message = result
+                else:
+                    message = ''
+
+                if subtitle is not None:
+                    subtitle = subtitle
+                elif _subtitle is not None:
+                    subtitle = _subtitle
+                else:
+                    subtitle = 'Success!'
+
+                sound = sound or _sound
+
+                notify(message, title=title, subtitle=subtitle, sound=sound)
+
+        return inner
+
+    return decorator
+
+
+@click.command()
+@click.argument('message')
+@click.option('--title', help='the notification title')
+@click.option('--subtitle', help='the notification subtitle')
+@click.option('--sound', help='the notification sound')
+def notify_command(message, title, subtitle, sound):
+    """Notification cli tool."""
+    notify(message, title=title, subtitle=subtitle, sound=sound)
