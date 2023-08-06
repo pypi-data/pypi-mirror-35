ad085a0d6ec6f1f461b78d0c8b6a3c4790829b19
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-06 15:30:39 +0000

    Add package django-conditional-views/0.1.0/django_conditional_views-0.1.0.tar.gz

---
+++ packages/django-conditional-views/django_conditional_views-0.1.0.tar.gz/django_conditional_views-0.1.0/src/django_conditional_views/__init__.py
@@ -0,0 +1,8 @@
+__version__ = '0.1.0'
+
+from .views import *
+from .elements import etag as etag_elements, last_modified as last_modified_elements
+
+__all__ = [__version__, 'ConditionalGetMixin', 'ConditionalGetTemplateViewMixin',
+           'ConditionalGetDetailViewMixin', 'ConditionalGetListViewMixin', 'etag_elements',
+           'last_modified_elements']
