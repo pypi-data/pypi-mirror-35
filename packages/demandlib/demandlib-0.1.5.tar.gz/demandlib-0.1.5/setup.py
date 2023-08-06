7f9a66a9f96c0f98fb15bb80dc0c9553b94f4d8d
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-06 14:56:25 +0000

    Add package demandlib/0.1.5/demandlib-0.1.5.tar.gz

---
+++ packages/demandlib/demandlib-0.1.5.tar.gz/demandlib-0.1.5/setup.py
@@ -0,0 +1,28 @@
+#! /usr/bin/env python
+
+"""Setup information of demandlib.
+"""
+
+from setuptools import setup, find_packages
+import os
+
+
+def read(fname):
+    return open(os.path.join(os.path.dirname(__file__), fname)).read()
+
+
+setup(name='demandlib',
+      version='0.1.5',
+      author='oemof developer group',
+      url='https://oemof.org/',
+      license='GPL3',
+      author_email='oemof@rl-institut.de',
+      description='Demandlib of the open energy modelling framework',
+      long_description=read('README.rst'),
+      packages=find_packages(),
+      install_requires=['numpy >= 1.7.0, <= 1.15',
+                        'pandas >= 0.18.0, <= 0.23.4'],
+      package_data={
+          'demandlib': [os.path.join('bdew_data', '*.csv')],
+          'demandlib.examples': ['*.csv']},
+      )
