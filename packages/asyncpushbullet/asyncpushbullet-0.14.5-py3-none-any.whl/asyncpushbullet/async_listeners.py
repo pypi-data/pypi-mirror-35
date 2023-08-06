b62dc55271651bf01c046e332b6609fcda7ebd72
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-06 07:10:59 +0000

    Add package asyncpushbullet/0.14.5/asyncpushbullet-0.14.5-py3-none-any.whl

---
+++ packages/asyncpushbullet/asyncpushbullet-0.14.5-py3-none-any.whl/asyncpushbullet/async_listeners.py
@@ -0,0 +1,206 @@
+# -*- coding: utf-8 -*-
+"""
+Asynchronous listeners for connecting to Pushbullet's realtime event stream.
+
+Pushbullet's API: https://docs.pushbullet.com/#realtime-event-stream
+"""
+import asyncio
+import json
+import logging
+import time
+from typing import AsyncIterator, Set, Iterable
+
+import aiohttp  # pip install aiohttp
+
+from asyncpushbullet import AsyncPushbullet
+from asyncpushbullet.websocket_client import WebsocketClient
+
+__author__ = 'Robert Harder'
+__email__ = "rob@iharder.net"
+
+
+class PushListener2:
+    WEBSOCKET_URL = 'wss://stream.pushbullet.com/websocket/'
+
+    def __init__(self, account: AsyncPushbullet,
+                 ignore_inactive: bool = True,
+                 ignore_dismissed: bool = True,
+                 only_this_device_nickname: str = None,
+                 types: Iterable[str] = ("push",)):
+        """Creates a Pushlistener2 to await pushes using asyncio.
+
+        The types parameter can be used to limit which kinds of pushes
+        are returned in an async for loop or the next_push() call.
+        The default is to show actual pushes only, not ephemerals.
+        The possible values in the tuple at this time are only
+        push, ephemeral, and ephemeral:xxx where xxx is matched to
+        the "type" parameter if the ephemeral payload has that.
+        For instance to listen only for the universal copy/paste
+        pushes, you could pass in the tuple ("ephemeral:clip",).
+
+        :param account: the AsyncPushbullet object that represents the account
+        :param ignore_inactive: ignore inactive pushes, defaults to true
+        :param ignore_dismissed:  ignore dismissed pushes, defaults to true
+        :param only_this_device_nickname: only show pushes from this device
+        :param types: the types of pushes to show
+        """
+        self.log = logging.getLogger(__name__ + "." + self.__class__.__name__)
+
+        self.account = account  # type: AsyncPushbullet
+        self._last_update = 0  # type: float
+        self._ignore_inactive = ignore_inactive  # type: bool
+        self._ignore_dismissed = ignore_dismissed  # type: bool
+        self._only_this_device_nickname = only_this_device_nickname  # type: str
+        self._ws_client = None  # type: WebsocketClient
+        self._loop = None  # type: asyncio.BaseEventLoop
+        self._queue = None  # type: asyncio.Queue
+
+        self.push_types = set(types)  # type: Set[str]
+        self.ephemeral_types = tuple([x.split(":")[-1] for x in self.push_types if len(x.split(":")) > 1])
+
+    async def close(self):
+        await self._ws_client.close()
+
+    async def __aenter__(self):
+        self._queue = asyncio.Queue()
+
+        async def _listen_for_pushes(_wc: WebsocketClient):
+            try:
+
+                # Stay here for a while receiving messages
+                async for msg in _wc:
+                    await self._process_websocket_message(msg)
+
+            except Exception as e:
+                sai = StopAsyncIteration(e)
+                await self._queue.put(sai)
+            else:
+                sai = StopAsyncIteration()
+                await self._queue.put(sai)
+
+        session = await self.account.aio_session()
+        wc = WebsocketClient(url=self.WEBSOCKET_URL + self.account.api_key,
+                             proxy=self.account.proxy,
+                             session=session)
+        self._ws_client = await wc.__aenter__()
+        asyncio.get_event_loop().create_task(_listen_for_pushes(wc))
+        await asyncio.sleep(0)
+
+        return self
+
+    async def _process_websocket_message(self, msg: aiohttp.WSMessage):
+
+        # Process websocket message
+        self._last_update = time.time()
+
+        if msg.type == aiohttp.WSMsgType.CLOSED:
+            err_msg = "Websocket closed: {}".format(msg)
+            self.log.warning(err_msg)
+            await self._queue.put(StopAsyncIteration(err_msg))
+
+        elif msg.type == aiohttp.WSMsgType.ERROR:
+            err_msg = "Websocket error: {}".format(msg)
+            self.log.debug(err_msg)
+            await self._queue.put(StopAsyncIteration(err_msg))
+
+        else:
+            await self._process_pushbullet_message(json.loads(msg.data))
+
+    async def _process_pushbullet_message(self, msg: dict):
+
+        # Look for ephemeral messages
+        # Takes special processing to sort through ephemerals.
+        # Example values in self.push_types: ephemeral, ephemeral:clip
+        if "type" in msg and "push" in msg:  # Ephemeral
+
+            # If we're looking for ALL ephemerals, that's easy
+            if not self.push_types or "ephemeral" in self.push_types:
+                await self._queue.put(msg)
+
+            elif self.ephemeral_types:  # If items in the list
+
+                # See if there is a sub-type in the ephemeral
+                sub_push = msg.get("push")
+                if type(sub_push) is dict:
+                    sub_type = sub_push.get("type")
+                    if sub_type is not None and sub_type in self.ephemeral_types:
+                        await self._queue.put(msg)
+
+        # Look for websocket message announcing new pushes
+        elif ("push" in self.push_types or not self.push_types) and \
+                msg == {'type': 'tickle', 'subtype': 'push'}:
+
+            pushes = await self.account.async_get_pushes(modified_after=self.account.most_recent_timestamp,
+                                                         filter_inactive=self._ignore_inactive)
+            self.log.debug("Retrieved {} pushes".format(len(pushes)))
+
+            # Update timestamp for most recent push so we only get "new" pushes
+            if len(pushes) > 0 and pushes[0].get('modified', 0) > self.account.most_recent_timestamp:
+                self.account.most_recent_timestamp = pushes[0]['modified']
+
+            # Process each push
+            for push in pushes:
+
+                # Filter dismissed pushes if requested
+                if self._ignore_dismissed is not None and bool(push.get("dismissed")):
+                    self.log.debug("Skipped push because it was dismissed: {}".format(push))
+                    continue  # skip this push
+
+                # Filter on device if requested
+                if self._only_this_device_nickname is not None:
+
+                    # Does push have a target device
+                    target_iden = push.get("target_device_iden")
+                    if target_iden is None:
+                        self.log.debug("Skipped push because it was not target device: {}".format(push))
+                        continue  # skip push
+
+                    # Does target device have the right nickname?
+                    target_dev = await self.account.async_get_device(iden=target_iden)
+                    if target_dev is None:
+                        self.log.error("Received a target_device_iden in push that did not map to a device: {}"
+                                       .format(target_iden))
+                        continue  # skip this push
+                    if target_dev.nickname != self._only_this_device_nickname:
+                        continue  # skip push - wrong device
+
+                # Passed all filters - accept push
+                self.log.debug("Adding to push queue: {}".format(push))
+                await self._queue.put(push)
+
+        elif "type" in msg and msg["type"] in self.push_types:
+            # According to the API, there are no other types, but this can
+            # be used to catch "nop" if you really want to.
+            await self._queue.put(msg)
+
+    async def __aexit__(self, exc_type, exc_val, exc_tb):
+        await self._ws_client.__aexit__(exc_type, exc_val, exc_tb)
+        await self._ws_client.close()
+
+    def __aiter__(self) -> AsyncIterator[dict]:
+        return self
+
+    async def __anext__(self) -> dict:
+        if self._ws_client.closed:
+            raise StopAsyncIteration("The websocket has closed.")
+
+        try:
+            push = await self.next_push()
+
+        except Exception as e:
+            raise StopAsyncIteration(e)
+
+        if type(push) == StopAsyncIteration:
+            raise push
+
+        return push
+
+    async def next_push(self, timeout: float = None) -> dict:
+        if timeout is None:
+            push = await self._queue.get()
+            if type(push) == StopAsyncIteration:
+                raise push
+            return push
+        else:
+            push = await asyncio.wait_for(self.next_push(), timeout=timeout)
+            return push
---
+++ packages/asyncpushbullet/asyncpushbullet-0.14.5-py3-none-any.whl/asyncpushbullet/command_line_push.py
@@ -0,0 +1,250 @@
+#!/usr/bin/env python3
+# -*- coding: utf-8 -*-
+"""
+A command line script for sending pushes.
+
+usage: command_line_push.py [-h] [-k KEY] [--key-file KEY_FILE]
+                            [--proxy PROXY] [-t TITLE] [-b BODY] [-d DEVICE]
+                            [--list-devices] [-u URL] [-f FILE]
+                            [--transfer.sh] [-q]
+
+optional arguments:
+  -h, --help            show this help message and exit
+  -k KEY, --key KEY     Your Pushbullet.com API key
+  --key-file KEY_FILE   Text file containing your Pushbullet.com API key
+  --proxy PROXY         Optional web proxy
+  -t TITLE, --title TITLE
+                        Title of your push
+  -b BODY, --body BODY  Body of your push (- means read from stdin)
+  -d DEVICE, --device DEVICE
+                        Destination device nickname
+  --list-devices        List registered device names
+  -u URL, --url URL     URL of link being pushed
+  -f FILE, --file FILE  Pathname to file to push
+  --transfer.sh         Use www.transfer.sh website for uploading files (use
+                        with --file)
+  -q, --quiet           Suppress all output
+
+"""
+import argparse
+import asyncio
+import contextlib
+import io
+import os
+import sys
+
+from asyncpushbullet import AsyncPushbullet
+from asyncpushbullet import Device
+from asyncpushbullet import InvalidKeyError
+from asyncpushbullet import PushbulletError
+from asyncpushbullet.filetype import get_file_type
+from asyncpushbullet.tqio import tqio
+
+# Exit codes
+__ERR_API_KEY_NOT_GIVEN__ = 1
+__ERR_INVALID_API_KEY__ = 2
+__ERR_CONNECTING_TO_PB__ = 3
+__ERR_FILE_NOT_FOUND__ = 4
+__ERR_DEVICE_NOT_FOUND__ = 5
+__ERR_NOTHING_TO_DO__ = 6
+
+# logging.basicConfig(logging.ERROR)
+
+# sys.argv.append("--help")
+# sys.argv.append("--list-devices")
+# sys.argv += ["-t", "test to device", "--device", "netmem"]
+# sys.argv += ["--file", __file__]
+# sys.argv.append("--transfer.sh")
+# sys.argv += ["--device", "netmem"]
+
+
+# sys.argv += ["-t", "foo"]
+# sys.argv += ["--key-file", "../api_key.txt"]
+
+
+# sys.argv += ["--key", "badkey"]
+
+# sys.argv.append("--quiet")
+
+def main():
+    args = parse_args()
+    do_main(args)
+
+
+def do_main(args):
+    # global API_KEY
+
+    # Key
+    api_key = ""
+    if "PUSHBULLET_API_KEY" in os.environ:
+        api_key = os.environ["PUSHBULLET_API_KEY"].strip()
+
+    if args.key:
+        api_key = args.key.strip()
+
+    if args.key_file:
+        with open(args.key_file) as f:
+            api_key = f.read().strip()
+        print("Read API key from file {}".format(args.key_file))
+
+    if api_key == "":
+        print(
+            "You must specify an API key, either at the command line or with the PUSHBULLET_API_KEY environment variable.",
+            file=sys.stderr)
+        sys.exit(__ERR_API_KEY_NOT_GIVEN__)
+
+    # Proxy
+    proxy = args.proxy or os.environ.get("https_proxy") or os.environ.get("http_proxy")
+
+    # Make connection
+    pb = AsyncPushbullet(api_key, proxy=proxy)  # type: AsyncPushbullet
+
+    loop = asyncio.get_event_loop()
+    try:
+        loop.run_until_complete(pb.async_verify_key())
+    except InvalidKeyError as exc:
+        print(exc, file=sys.stderr)
+        pb.close_all_threadsafe()
+        sys.exit(__ERR_INVALID_API_KEY__)
+    except PushbulletError as exc:
+        print(exc, file=sys.stderr)
+        pb.close_all_threadsafe()
+        sys.exit(__ERR_CONNECTING_TO_PB__)
+
+    # List devices?
+    if args.list_devices:
+        print("Devices:")
+        for dev in pb.devices:
+            print("\t", dev.nickname)
+        pb.close_all_threadsafe()
+        sys.exit(0)
+
+    # Transfer file?
+    if args.file:
+        if not os.path.isfile(args.file):
+            print("File not found:", args.file, file=sys.stderr)
+            sys.exit(__ERR_FILE_NOT_FOUND__)
+
+        dev = None  # type: Device
+        if args.device:
+            dev = loop.run_until_complete(pb.async_get_device(nickname=args.device))
+            if dev is None:
+                print("Device not found:", args.device, file=sys.stderr)
+                sys.exit(__ERR_DEVICE_NOT_FOUND__)
+
+        file_name = os.path.basename(args.file)  # type: str
+        file_url = None  # type: str
+        file_type = None  # type: str
+        if args.transfer_sh:
+            if not args.quiet:
+                print("Uploading file to transfer.sh ... {}".format(args.file))
+
+            if args.quiet:
+                # Upload without any progress indicator
+                with open(args.file, "rb") as f:
+                    resp = loop.run_until_complete(pb._async_post_data(
+                        "https://transfer.sh/",
+                        data={"file": f}))
+            else:
+                # Upload with progress indicator
+                with tqio(args.file) as f:
+                    resp = loop.run_until_complete(pb._async_post_data(
+                        "https://transfer.sh/",
+                        # data=data))
+                        data={"file": f}))
+
+            file_url = resp["raw"].strip()
+            file_type = get_file_type(args.file)
+
+        else:
+            if not args.quiet:
+                print("Uploading file to Pushbullet ... {}".format(args.file))
+            stats = loop.run_until_complete(pb.async_upload_file(args.file))
+
+            file_url = stats["file_url"]
+            file_type = stats["file_type"]
+            file_name = stats["file_name"]
+
+        if not args.quiet:
+            print("Pushing file ... {}".format(file_url))
+        resp = loop.run_until_complete(pb.async_push_file(
+            file_name=file_name,
+            file_type=file_type,
+            file_url=file_url,
+            title=args.title or "File: {}".format(file_name),
+            body=args.body or file_url,
+            device=dev))
+
+        if not args.quiet:
+            print(resp)
+
+        # loop.run_until_complete(pb.close())
+        pb.close_all_threadsafe()
+        # loop.close()
+
+
+    # Push note
+    elif args.title or args.body:
+        title = args.title
+        body = args.body
+
+        dev = None  # type: Device
+        if args.device:
+            dev = pb.get_device(nickname=args.device)
+            if dev is None:
+                print("Device not found:", args.device, file=sys.stderr)
+                sys.exit(__ERR_DEVICE_NOT_FOUND__)
+
+        if body is not None and body == "-":
+            body = sys.stdin.read()
+        url = args.url
+        if url is None:
+            resp = pb.push_note(title=title, body=body, device=dev)
+        else:
+            resp = pb.push_link(title=title, url=url, body=body, device=dev)
+
+        if not args.quiet:
+            print(resp)
+        loop.run_until_complete(pb.close())
+        loop.close()
+    else:
+        print("Nothing to do.")
+        loop.run_until_complete(pb.close())
+        loop.close()
+        sys.exit(__ERR_NOTHING_TO_DO__)
+
+
+def parse_args():
+    parser = argparse.ArgumentParser()
+    parser.add_argument("-k", "--key", help="Your Pushbullet.com API key")
+    parser.add_argument("--key-file", help="Text file containing your Pushbullet.com API key")
+    parser.add_argument("--proxy", help="Optional web proxy")
+    parser.add_argument("-t", "--title", help="Title of your push")
+    parser.add_argument("-b", "--body", help="Body of your push (- means read from stdin)")
+    parser.add_argument("-d", "--device", help="Destination device nickname")
+    parser.add_argument("--list-devices", action="store_true", help="List registered device names")
+    parser.add_argument("-u", "--url", help="URL of link being pushed")
+    parser.add_argument("-f", "--file", help="Pathname to file to push")
+    parser.add_argument("--transfer.sh", dest="transfer_sh", action="store_true",
+                        help="Use www.transfer.sh website for uploading files (use with --file)")
+    parser.add_argument("-q", "--quiet", action="store_true", help="Suppress all output")
+
+    args = parser.parse_args()
+
+    if len(sys.argv) == 1:
+        parser.print_help()
+        sys.exit(__ERR_NOTHING_TO_DO__)
+
+    return args
+
+
+@contextlib.contextmanager
+def nostdout():
+    save_stdout = sys.stdout
+    sys.stdout = io.TextIOBase()
+    yield
+    sys.stdout = save_stdout
+
+
+if __name__ == "__main__":
+    main()
