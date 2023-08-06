fc067e3c24e682a879c4d59c21bece0b56bc11c5
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-06 07:10:29 +0000

    Add package coffee-ninja/1.0.45/coffee-ninja-1.0.45.tar.gz

---
+++ packages/coffee-ninja/coffee-ninja-1.0.45.tar.gz/coffee-ninja-1.0.45/setup.py
@@ -0,0 +1,20 @@
+import setuptools
+
+with open("README.md", "r") as fh:
+    long_description = fh.read()
+
+
+setuptools.setup(
+    name="coffee-ninja",
+    version="1.0.45",
+    author="CoffeeNinja",
+    description="Coffee Ninja",
+    long_description=long_description,
+    long_description_content_type="text/markdown",
+    url="http://example.com/",
+    packages=setuptools.find_packages(),
+    classifiers=(
+        "Programming Language :: Python",
+        "Operating System :: OS Independent",
+    )
+)
