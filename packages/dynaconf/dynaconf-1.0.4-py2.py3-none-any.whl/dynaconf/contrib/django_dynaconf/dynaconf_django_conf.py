39cd27adcba7686e60f2edca6980383954b0c7d6
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-06 16:18:31 +0000

    Add package dynaconf/1.0.4/dynaconf-1.0.4-py2.py3-none-any.whl

---
+++ packages/dynaconf/dynaconf-1.0.4-py2.py3-none-any.whl/dynaconf/contrib/django_dynaconf/dynaconf_django_conf.py
@@ -0,0 +1,14 @@
+from dynaconf import LazySettings
+from django.conf import settings as django_settings
+
+
+dj = {}
+for key in dir(django_settings):
+    if key.isupper() and key != 'SETTINGS_MODULE':
+        dj[key] = getattr(django_settings, key, None)
+    dj['ORIGINAL_SETTINGS_MODULE'] = django_settings.SETTINGS_MODULE
+
+
+dj.setdefault('GLOBAL_ENV_FOR_DYNACONF', 'DJANGO')
+
+settings = LazySettings(**dj)
