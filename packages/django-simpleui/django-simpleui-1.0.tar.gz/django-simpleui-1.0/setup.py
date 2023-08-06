6d579d6f66e47110d14db10acb7d6930fb7c5488
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-06 14:56:55 +0000

    Add package django-simpleui/1.0/django-simpleui-1.0.tar.gz

---
+++ packages/django-simpleui/django-simpleui-1.0.tar.gz/django-simpleui-1.0/setup.py
@@ -0,0 +1,18 @@
+from setuptools import setup
+
+long_description = "\n".join([
+    open('README.rst', 'r').read(),
+])
+
+setup(
+    name='django-simpleui',
+    version='1.0',
+    packages=['simpleui'],
+    url='https://github.com/newpanjing/simpleui',
+    license='Apache License 2.0',
+    author='panjing',
+    long_description=long_description,
+    author_email='newpanjing@icloud.com',
+    description='django admin 后台模板',
+    install_requires=['django'],
+)
