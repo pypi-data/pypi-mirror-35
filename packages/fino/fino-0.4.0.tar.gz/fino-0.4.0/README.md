0aee4dc08ce3335ea3b9237c9ce0982ab1469cef
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-06 11:51:49 +0000

    Add package fino/0.4.0/fino-0.4.0.tar.gz

---
+++ packages/fino/fino-0.4.0.tar.gz/fino-0.4.0/README.md
@@ -0,0 +1,10 @@
+fino
+====
+
+[![Build Status](https://travis-ci.org/hugovk/fino.svg?branch=master)](https://travis-ci.org/hugovk/fino)
+[![Coverage Status](https://coveralls.io/repos/github/hugovk/fino/badge.svg?branch=master)](https://coveralls.io/github/hugovk/fino?branch=master)
+[![Code Health](https://landscape.io/github/hugovk/fino/master/landscape.svg)](https://landscape.io/github/hugovk/fino/master)
+
+Output the Finnish word for a given integer.
+
+ * Used by [hugovk/everyfinnishno](https://github.com/hugovk/everyfinnishno) to tweet [@EveryFinnishNo](https://twitter.com/EveryFinnishNo).
---
+++ packages/fino/fino-0.4.0.tar.gz/fino-0.4.0/setup.py
@@ -0,0 +1,35 @@
+#!/usr/bin/env python
+
+from setuptools import setup
+
+setup(
+    name="fino",
+    version="0.4.0",
+    description="Output the Finnish word for a given integer.",
+    url="https://github.com/hugovk/fino",
+    author="hugovk",
+    keywords=["Finnish", "suomi", "numbers", "integers"],
+    py_modules=("fino",),
+    zip_safe=False,
+    python_requires=">=3.4",
+    classifiers=[
+        "Development Status :: 6 - Mature",
+        "Programming Language :: Python :: 3",
+        "Programming Language :: Python :: 3.4",
+        "Programming Language :: Python :: 3.5",
+        "Programming Language :: Python :: 3.6",
+        "Programming Language :: Python :: 3.7",
+        "Programming Language :: Python :: 3 :: Only",
+        "Programming Language :: Python :: Implementation :: CPython",
+        "Programming Language :: Python :: Implementation :: PyPy",
+        "Intended Audience :: Developers",
+        "Intended Audience :: Education",
+        "Intended Audience :: Science/Research",
+        "Natural Language :: English",
+        "Natural Language :: Finnish",
+        "Topic :: Artistic Software",
+        "Topic :: Scientific/Engineering :: Artificial Intelligence",
+    ],
+    )
+
+# End of file
