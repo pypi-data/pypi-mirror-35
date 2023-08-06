0e7d6a3ddd8d4b0080f38cce5d37f5c13de372c0
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-06 01:56:30 +0000

    Add package leveldbs/0.1.25/leveldbs-0.1.25.tar.gz

---
+++ packages/leveldbs/leveldbs-0.1.25.tar.gz/leveldbs-0.1.25/setup.py
@@ -0,0 +1,35 @@
+#!/usr/bin/env python3
+#-*- coding:utf-8 -*-
+
+#############################################
+# File Name: setup.py
+# Author: InfinityFuture
+# Mail: infinityfuture@foxmail.com
+# Created Time: 2018-08-09 02:00:00
+#############################################
+
+import os
+from setuptools import setup, find_packages
+
+version = os.path.join(
+    os.path.realpath(os.path.dirname(__file__)),
+    'version.txt'
+)
+
+setup(
+    name = 'leveldbs',
+    version = open(version, 'r').read().strip(),
+    keywords = ('pip', 'leveldb'),
+    description = 'time and path tool',
+    long_description = 'time and path tool',
+    license = 'MIT Licence',
+
+    url = 'https://github.com/infinityfuture/leveldb-server',
+    author = 'infinityfuture',
+    author_email = 'infinityfuture@foxmail.com',
+
+    packages = find_packages(),
+    include_package_data = True,
+    platforms = 'any',
+    install_requires = ['pyzmq']
+)
