349d4c57ad4d0525c1374c2b3920867166b2bf20
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-06 17:09:27 +0000

    Add package OrzMC/1.0.4/OrzMC-1.0.4.tar.gz

---
+++ packages/OrzMC/OrzMC-1.0.4.tar.gz/OrzMC-1.0.4/OrzMC/OrzMC.py
@@ -0,0 +1,11 @@
+from .GameDownloader import GameDownloader
+
+def startClient():
+    game = GameDownloader('1.13')
+    game.downloadGameJSON()
+    game.downloadClient()
+    game.downloadServer()
+    game.downloadAssetIndex()
+    game.downloadAssetObjects()
+    game.donwloadLibraries()
+    game.startCient(user='guest')
