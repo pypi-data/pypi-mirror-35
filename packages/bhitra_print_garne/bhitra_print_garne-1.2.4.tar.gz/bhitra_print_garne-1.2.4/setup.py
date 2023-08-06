cd82a83fa8fe159ad8e611f40d2840bfcd13aed3
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-06 07:10:44 +0000

    Add package bhitra_print_garne/1.2.4/bhitra_print_garne-1.2.4.tar.gz

---
+++ packages/bhitra_print_garne/bhitra_print_garne-1.2.4.tar.gz/bhitra_print_garne-1.2.4/setup.py
@@ -0,0 +1,11 @@
+from distutils.core import setup
+
+setup(
+        name = 'bhitra_print_garne',
+        version = '1.2.4',
+        py_modules = [ 'bhitra_print_garne'],
+        author = 'subbaMan27',
+        author_email = 'anupam.subba27@gmail.com',
+        description = 'A simple printer of nested lists',
+     )
+
