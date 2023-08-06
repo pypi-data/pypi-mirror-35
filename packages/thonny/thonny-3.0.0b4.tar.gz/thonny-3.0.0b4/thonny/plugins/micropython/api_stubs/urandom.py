8e7a80e396750b962a4a79119c948f105f103d17
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-06 13:23:46 +0000

    Add package thonny/3.0.0b4/thonny-3.0.0b4.tar.gz

---
+++ packages/thonny/thonny-3.0.0b4.tar.gz/thonny-3.0.0b4/thonny/plugins/micropython/api_stubs/urandom.py
@@ -0,0 +1,20 @@
+def choice():
+    pass
+
+def getrandbits():
+    pass
+
+def randint():
+    pass
+
+def random():
+    pass
+
+def randrange():
+    pass
+
+def seed():
+    pass
+
+def uniform():
+    pass
