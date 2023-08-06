347ac1eb577ee4ed6750ca6853d208a244f855dd
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-06 17:13:40 +0000

    Add package autobasedoc/0.0.19/autobasedoc-0.0.19.tar.gz

---
+++ packages/autobasedoc/autobasedoc-0.0.19.tar.gz/autobasedoc-0.0.19/autobasedoc/__init__.py
@@ -0,0 +1,19 @@
+# -*- coding: utf-8 -*-
+"""
+Created on Wed Aug 26 11:40:26 2015
+
+@author: ecksjoh
+"""
+import sys
+from .version import version
+
+__version__ = version
+__license__ = __doc__
+is_python3 = (sys.version_info.major == 3)
+_baseFontNames = dict(normal='Helvetica',
+                      bold='Helvetica-Bold',
+                      italic='Helvetica-Oblique',
+                      bold_italic='Helvetica-BoldOblique')
+
+import autobasedoc.autorpt as ar
+import autobasedoc.autoplot as ap
