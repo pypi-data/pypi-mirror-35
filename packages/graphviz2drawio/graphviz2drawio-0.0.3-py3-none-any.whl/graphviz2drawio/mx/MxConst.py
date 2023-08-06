cb427f6bbaa9d0bf2bf35da363640640f5d36c88
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-06 04:13:54 +0000

    Add package graphviz2drawio/0.0.3/graphviz2drawio-0.0.3-py3-none-any.whl

---
+++ packages/graphviz2drawio/graphviz2drawio-0.0.3-py3-none-any.whl/graphviz2drawio/mx/MxConst.py
@@ -0,0 +1,20 @@
+DECLARATION = '<?xml version="1.0"?>'
+
+BLOCK = "block"
+DIAMOND = "diamond"
+
+CENTER = "center"
+START = "start"
+
+DEFAULT_TEXT_SIZE = 10
+DEFAULT_FONT_FAMILY = "Helvetica"
+
+DEFAUT_STROKE = "#000000"
+DEFAUT_FILL = "#ffffff"
+
+CELL = "mxCell"
+GEO = "mxGeometry"
+GRAPH = "mxGraphModel"
+ROOT = "root"
+
+NONE = "none"
