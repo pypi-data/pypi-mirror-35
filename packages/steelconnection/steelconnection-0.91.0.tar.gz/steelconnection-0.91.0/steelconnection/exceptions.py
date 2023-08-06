70d9ad5974606e2422aa9b6f0b83059cedcc2208
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-06 01:53:42 +0000

    Add package steelconnection/0.91.0/steelconnection-0.91.0.tar.gz

---
+++ packages/steelconnection/steelconnection-0.91.0.tar.gz/steelconnection-0.91.0/steelconnection/exceptions.py
@@ -0,0 +1,26 @@
+# -*- coding: utf-8 -*-
+
+"""steelconnection.exceptions
+
+Contains exceptions raised by steelconnection.
+"""
+
+
+class BadRequest(RuntimeError):
+    """400 - Bad Request."""
+
+
+class AuthenticationError(RuntimeError):
+    """401 - Authentication Failed."""
+
+
+class InvalidResource(RuntimeError):
+    """404 - Path or resource not found."""
+
+
+class ResourceGone(RuntimeError):
+    """410 - Gone. Resource no longer available."""
+
+
+class APINotEnabled(RuntimeError):
+    """502 - REST API not enabled."""
