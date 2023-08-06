856be9b6154ec35eb240223347f6ea84253a9260
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-06 16:17:39 +0000

    Add package pytest-djangoapp/0.9.0/pytest_djangoapp-0.9.0-py2.py3-none-any.whl

---
+++ packages/pytest-djangoapp/pytest_djangoapp-0.9.0-py2.py3-none-any.whl/pytest_djangoapp/fixtures/utils.py
@@ -0,0 +1,23 @@
+# -*- encoding: utf-8 -*-
+from __future__ import unicode_literals
+
+from sys import version_info
+from time import time
+
+
+PY2 = version_info[0] == 2
+PY3 = version_info[0] == 3
+
+
+if PY3:
+    string_types = (str,)
+else:
+    string_types = (str, unicode)
+
+
+def get_stamp():
+    """Returns curent timestamp as string.
+
+    :rtype: str|unicode
+    """
+    return '%s' % time()
