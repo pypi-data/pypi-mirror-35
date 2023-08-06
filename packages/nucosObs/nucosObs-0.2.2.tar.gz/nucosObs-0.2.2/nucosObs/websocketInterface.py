7b472b981c9e4f392e39c65616a528e292e315c5
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-06 13:20:36 +0000

    Add package nucosObs/0.2.2/nucosObs-0.2.2.tar.gz

---
+++ packages/nucosObs/nucosObs-0.2.2.tar.gz/nucosObs-0.2.2/nucosObs/websocketInterface.py
@@ -0,0 +1,89 @@
+import websockets
+import asyncio as aio
+
+try:
+    from nucosCR import random
+    isCR = True
+except:
+    import random
+    isCR = False
+from nucosObs import loop, debug
+from nucosObs.observer import broadcast
+
+
+class WebsocketInterface(object):
+    def __init__(self, broker, auth=None, closeOnClientQuit=False):
+        self.ws = None
+        self.loop = loop
+        self.broker = broker
+        self.server = None
+        self.auth = auth
+        self.closeOnClientQuit = closeOnClientQuit
+
+    async def connect(self, ip, port):
+        if debug[-1]:
+            print("try to connect")
+        websocket = await websockets.connect('ws://%s:%s' % (ip, str(port)))
+        self.ws = websocket
+        await self.listener()
+
+    async def serve(self, ip, port):
+        if debug[-1]:
+            print("try to start server")
+        self.server = await websockets.serve(self.handler, ip, port)
+        self.handshake = True
+
+    async def handler(self, websocket, path):
+        self.ws = websocket
+        if debug[-1]:
+            print("Client connected")
+        if isCR:
+            self.nonce = random(24).decode()
+        else:
+            self.nonce = bytes([random.getrandbits(8) for i in range(24)])
+        if self.auth:
+            await self.ws.send("startAuth " + self.nonce)
+        await self.listener()
+
+    async def shutdown(self):
+        if debug[-1]:
+            print("Websocket connection closed...")
+        await broadcast.put("broadcast stop")
+
+    async def listener(self):
+        while True:
+            if self.ws is not None:
+                if self.ws.open:
+                    try:
+                        msg = await self.ws.recv()
+                    except:
+                        if self.server is None:
+                            await self.shutdown()
+                            break
+                        else:
+                            msg = ""
+                    if msg:
+                        if self.auth is not None and not self.auth.approved:
+                            authmsg = "startAuth " + self.nonce + " " + msg
+                            await self.broker.put(authmsg)
+                        await self.broker.put(msg)
+                else:
+                    if self.server is None:
+                        await self.shutdown()
+                        break
+                    else:
+                        if debug[-1]:
+                            print("client died ...")
+                        await self.broker.put("client exit")
+                        self.ws = None
+                        if self.auth:
+                            self.auth.approved = False
+            else:
+                if self.closeOnClientQuit:
+                    if debug[-1]:
+                        print("shutdown ...")
+                    break
+                else:
+                    await aio.sleep(1.0)
+        if debug[-1]:
+            print("WebsocketInterface stopped")
