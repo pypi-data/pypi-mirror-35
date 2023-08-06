08a99d1dc2bbcffe5ce6211a1e73a156e33f2a27
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-06 04:14:41 +0000

    Add package indent-zsh/1.0.0/indent-zsh-1.0.0.tar.gz

---
+++ packages/indent-zsh/indent-zsh-1.0.0.tar.gz/indent-zsh-1.0.0/setup.py
@@ -0,0 +1,10 @@
+from distutils.core import setup
+setup(
+		name 			= 	'indent-zsh',
+		version 		=	'1.0.0',
+		py_modules		=	['indent-zsh'],
+		author			=	'fairytail',
+		author_email 	= 	'zhangshenghui410@163.com',
+		url				=	'https://user.qzone.qq.com/787392165/infocenter',
+		description		=	'a simple printer of nested lists',
+)
