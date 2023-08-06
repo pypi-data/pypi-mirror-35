2bebf8e922dd3a7d6730661595fd65f9a2e43aef
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-06 14:55:52 +0000

    Add package huotaihe-nester/1.0.0/huotaihe_nester-1.0.0.tar.gz

---
+++ packages/huotaihe-nester/huotaihe_nester-1.0.0.tar.gz/huotaihe_nester-1.0.0/setup.py
@@ -0,0 +1,10 @@
+from distutils.core import setup
+setup(
+    name = 'huotaihe_nester',
+    version = '1.0.0',
+    py_modules = ['huotaihe_nester'],
+    author = 'huotahie',
+    author_email = 'huotaihe@163.com',
+    url = 'http://www.huotaihe.com',
+    description = 'A simple printer of nested lists',
+)
