7d0ceb48245c0b6a75f1294ef1139cdb66b051ad
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-06 14:28:33 +0000

    Add package FortniteTrackerAPI/0.0.1/FortniteTrackerAPI-0.0.1-py3-none-any.whl

---
+++ packages/FortniteTrackerAPI/FortniteTrackerAPI-0.0.1-py3-none-any.whl/FortniteAPI/FortniteAPI.py
@@ -0,0 +1,41 @@
+import requests
+from FortniteAPI import Stats
+
+api_key = None
+
+
+def get_stats(json):
+    return Stats.FortniteStats(json)
+
+
+class FortniteAPI:
+    base_url = "https://api.fortnitetracker.com/v1/profile/"
+
+    def __init__(self, platform, user):
+        self.user = user
+        self.platform = platform
+        self.url = ""
+        self.update_url()
+        self.stats = None
+        if self.api_key is not None:
+            self.make_request()
+
+    def set_user(self, user):
+        self.user = user
+        self.update_url()
+
+    def set_platform(self, platform):
+        self.platform = platform
+        self.update_url()
+
+    def update_url(self):
+        self.url = self.base_url + self.platform + "/" + self.user
+        if self.api_key is not None:
+            self.make_request()
+
+    def make_request(self):
+        header = {"TRN-Api-Key": self.api_key}
+        req = requests.get(self.url, headers=header)
+        req.json()
+        req.close()
+        self.stats = get_stats(req.json())
