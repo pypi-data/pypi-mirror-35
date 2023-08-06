9a9a58833da530f31ad59e44edf010d5c1c8cdfd
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-06 01:56:49 +0000

    Add package pg-db-migrator/0.1.1/pg_db_migrator-0.1.1.tar.gz

---
+++ packages/pg-db-migrator/pg_db_migrator-0.1.1.tar.gz/pg_db_migrator-0.1.1/setup.py
@@ -0,0 +1,13 @@
+from setuptools import setup
+
+setup(
+    name='pg_db_migrator',
+    version='0.1.1',
+    packages=['pg_db_migrator'],
+    url='',
+    license='',
+    author='Ziyang Hu',
+    author_email='hu.ziyang@cantab.net',
+    description='',
+    install_requires=['psycopg2-binary', 'tarjan']
+)
