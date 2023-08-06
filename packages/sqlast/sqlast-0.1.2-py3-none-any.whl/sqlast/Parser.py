fec1553931f83b18e613b694ac32019411730435
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-06 17:15:46 +0000

    Add package sqlast/0.1.2/sqlast-0.1.2-py3-none-any.whl

---
+++ packages/sqlast/sqlast-0.1.2-py3-none-any.whl/sqlast/Parser.py
@@ -0,0 +1,36 @@
+# -*- coding: utf-8 -*-
+from lark import Lark
+
+from .Grammar import Grammar
+from .Indenter import CustomIndenter
+from .Transformer import Transformer
+
+
+class Parser:
+    def __init__(self, algo='lalr', ebnf_file='grammar/grammar.ebnf'):
+        self.algo = algo
+        self.ebnf_file = ebnf_file
+
+    @staticmethod
+    def indenter():
+        """
+        Initialize the indenter
+        """
+        return CustomIndenter()
+
+    @staticmethod
+    def transformer():
+        """
+        Initialize the transformer
+        """
+        return Transformer()
+
+    def lark(self):
+        grammar = Grammar.grammar(self.ebnf_file)
+        return Lark(grammar, parser=self.algo, postlex=self.indenter())
+
+    def parse(self, source):
+        source = '{}\n'.format(source)
+        lark = self.lark()
+        tree = lark.parse(source)
+        return self.transformer().transform(tree)
