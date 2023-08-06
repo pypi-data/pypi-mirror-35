a95c899ac8ba5941e86d7fc35b74f23d33d1a78b
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-06 16:18:49 +0000

    Add package leakz/1.1/leakz-1.1.tar.gz

---
+++ packages/leakz/leakz-1.1.tar.gz/leakz-1.1/setup.py
@@ -0,0 +1,33 @@
+from setuptools import setup
+
+setup(
+    name='leakz',
+    version='1.1',
+    packages=['leakz'],
+    url='https://github.com/thehappydinoa/leakz',
+    license='MIT',
+    author='thehappydinoa',
+    author_email='thehappydinoa@gmail.com',
+    description='Python API wrapper for lea.kz',
+
+    classifiers=[
+        #   3 - Alpha
+        #   4 - Beta
+        #   5 - Production/Stable
+        'Development Status :: 4 - Beta',
+
+        'Intended Audience :: Developers',
+        'Topic :: Software Development :: Build Tools',
+
+        'License :: OSI Approved :: MIT License',
+
+        'Programming Language :: Python :: 2',
+        'Programming Language :: Python :: 2.6',
+        'Programming Language :: Python :: 2.7',
+        'Programming Language :: Python :: 3',
+        'Programming Language :: Python :: 3.4',
+        'Programming Language :: Python :: 3.5',
+        'Programming Language :: Python :: 3.6',
+        'Programming Language :: Python :: 3.7'
+    ]
+)
