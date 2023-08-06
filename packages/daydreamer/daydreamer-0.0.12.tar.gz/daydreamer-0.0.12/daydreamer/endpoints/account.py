d3d8528944a13440a5288e48ce608ebc54868b27
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-06 01:56:22 +0000

    Add package daydreamer/0.0.12/daydreamer-0.0.12.tar.gz

---
+++ packages/daydreamer/daydreamer-0.0.12.tar.gz/daydreamer-0.0.12/daydreamer/endpoints/account.py
@@ -0,0 +1,19 @@
+import datetime
+
+from daydreamer import resources
+from daydreamer.endpoints.baseendpoint import BaseEndpoint
+
+
+class Account(BaseEndpoint):
+
+    def get_account(self, session=None):
+        """
+        Get account info.
+        
+        :return: account info
+        """
+        date_time_sent = datetime.datetime.utcnow()
+        response = self.request("GET", method='v1/client/balance', session=session)
+        return self.process_response(
+            response.json(), resources.AccountDetails, date_time_sent, datetime.datetime.utcnow()
+        )
