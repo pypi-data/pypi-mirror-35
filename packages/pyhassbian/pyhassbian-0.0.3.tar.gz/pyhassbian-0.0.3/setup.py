625e335105db0b9b7a28ea25b01744a332e8d558
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-06 16:16:37 +0000

    Add package pyhassbian/0.0.3/pyhassbian-0.0.3.tar.gz

---
+++ packages/pyhassbian/pyhassbian-0.0.3.tar.gz/pyhassbian-0.0.3/setup.py
@@ -0,0 +1,21 @@
+"""Setup configuration."""
+import setuptools
+
+with open("README.md", "r") as fh:
+    LONG = fh.read()
+setuptools.setup(
+    name="pyhassbian",
+    version="0.0.3",
+    author="Joakim Sorensen",
+    author_email="ludeeus@gmail.com",
+    description="",
+    long_description=LONG,
+    long_description_content_type="text/markdown",
+    url="https://github.com/ludeeus/pyhassbian",
+    packages=setuptools.find_packages(),
+    classifiers=(
+        "Programming Language :: Python :: 3",
+        "License :: OSI Approved :: MIT License",
+        "Operating System :: OS Independent",
+    ),
+)
