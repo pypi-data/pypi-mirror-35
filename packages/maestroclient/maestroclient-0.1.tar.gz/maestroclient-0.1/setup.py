7de047ae61c3ddc1ddd4e1a95616f9cc051c6092
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-06 11:54:09 +0000

    Add package maestroclient/0.1/maestroclient-0.1.tar.gz

---
+++ packages/maestroclient/maestroclient-0.1.tar.gz/maestroclient-0.1/setup.py
@@ -0,0 +1,10 @@
+from setuptools import setup
+
+setup(name='maestroclient',
+      version='0.1',
+      description='Client for accessing Maestro API',
+      author='Sam Dela Cruz',
+      author_email='sammydc@gmail.com',
+      license='MIT',
+      packages=['maestroclient'],
+      zip_safe=False)
