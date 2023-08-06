79365d81046800500bce8b4c517bf0ca1a2d09b0
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-06 14:21:48 +0000

    Add package nester_daodao/1.0.0/nester_daodao-1.0.0.tar.gz

---
+++ packages/nester_daodao/nester_daodao-1.0.0.tar.gz/nester_daodao-1.0.0/nester.py
@@ -0,0 +1,6 @@
+def  print_lol(the_list):
+    for each_item in the_list:
+            if isinstance(each_time,list):
+                print_lol(the_list)
+            else:
+                print(each_item)
---
+++ packages/nester_daodao/nester_daodao-1.0.0.tar.gz/nester_daodao-1.0.0/setup.py
@@ -0,0 +1,10 @@
+from distutils.core import setup
+setup(
+    name = 'nester_daodao',
+    version = '1.0.0',
+    py_modules = ['nester'],
+    author = 'Walte_Liu',
+    author_email = 'liu.wendao@foxmail.com',
+    url = 'http://headfirstlabs.com',
+    description = 'a simple printer of nested lists'
+)
