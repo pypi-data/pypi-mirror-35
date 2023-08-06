214b6673f38e28f79529d5e4ed37dd26b6d5707c
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-06 13:21:36 +0000

    Add package shippai/0.3.2/shippai-0.3.2.tar.gz

---
+++ packages/shippai/shippai-0.3.2.tar.gz/shippai-0.3.2/setup.py
@@ -0,0 +1,15 @@
+from setuptools import setup, find_packages
+
+setup(
+    name='shippai',
+    version='0.3.2',
+    description='Interface with Rust errors seamlessly',
+    long_description=open('README.rst').read(),
+    author='Markus Unterwaditzer',
+    author_email='markus-python@unterwaditzer.net',
+    url='https://github.com/untitaker/shippai',
+    packages=find_packages(exclude=('tests',)),
+    include_package_data=True,
+    license='MIT',
+    classifiers=['License :: OSI Approved :: MIT License']
+)
