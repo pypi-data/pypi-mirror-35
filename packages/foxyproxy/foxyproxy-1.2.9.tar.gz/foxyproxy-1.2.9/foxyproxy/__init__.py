b8f0634903886afe0f7185710b6184614bf04815
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-06 17:16:26 +0000

    Add package foxyproxy/1.2.9/foxyproxy-1.2.9.tar.gz

---
+++ packages/foxyproxy/foxyproxy-1.2.9.tar.gz/foxyproxy-1.2.9/foxyproxy/__init__.py
@@ -0,0 +1,19 @@
+#!/usr/bin/env python
+# -*- coding: utf-8 -*-
+
+# Copyright (C) Smart Arcs Ltd, Enigma Bridge Ltd, registered in the United Kingdom.
+# Unauthorized copying of this file, via any medium is strictly prohibited
+# Proprietary and confidential
+# Written by Dan Cvrcek <support@smartarchitects.co.uk>, August 2018
+
+__copyright__ = "Smart Arcs Ltd, Enigma Bridge Ltd"
+__email__ = "support@smartarchitects.co.uk"
+__status__ = "Beta"
+
+try:
+    import pkg_resources
+    pkg_resources.declare_namespace(__name__)
+except ImportError:
+    import pkgutil
+    __path__ = pkgutil.extend_path(__path__, __name__)
+
