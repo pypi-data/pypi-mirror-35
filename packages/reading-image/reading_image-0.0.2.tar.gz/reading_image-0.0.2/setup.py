f02631c916b5d2270ed29d1879a80f96626a966d
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-06 17:15:43 +0000

    Add package reading-image/0.0.2/reading_image-0.0.2.tar.gz

---
+++ packages/reading-image/reading_image-0.0.2.tar.gz/reading_image-0.0.2/setup.py
@@ -0,0 +1,11 @@
+from setuptools import setup
+
+setup(name='reading_image',
+      version='0.0.2',
+      description='OCR package',
+      url='http://bitbucket.org/DannyHoskin/reading_image',
+      author='DannyHoskin',
+      author_email='dannyhoskin2@gmail.com',
+      license='DannyHoskin',
+      packages=['reading_image'],
+      zip_safe=False)
