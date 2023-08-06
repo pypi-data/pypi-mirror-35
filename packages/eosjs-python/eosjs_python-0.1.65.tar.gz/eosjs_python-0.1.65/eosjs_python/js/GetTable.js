8787bd53f0c120b03c097d96e7f636bc7befca5b
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-06 17:16:12 +0000

    Add package eosjs-python/0.1.65/eosjs_python-0.1.65.tar.gz

---
+++ packages/eosjs-python/eosjs_python-0.1.65.tar.gz/eosjs_python-0.1.65/eosjs_python/js/GetTable.js
@@ -0,0 +1,25 @@
+Eos = require('eosjs') // Eos = require('./src')
+
+const httpEndpointAddress = process.argv[2];
+const wif = process.argv[3];
+const code = process.argv[4];
+const scope = process.argv[5];
+const table = process.argv[6];
+
+
+eos = Eos({
+  keyProvider: wif,
+  httpEndpoint: httpEndpointAddress
+})
+
+eos.getTableRows({
+    code: code,
+    scope: scope,
+    table: table,
+    json: true,
+}).then(response => {
+  console.log(JSON.stringify(response));
+}).catch(function (e) {
+      console.error(e);
+      process.exit(1);
+      })
