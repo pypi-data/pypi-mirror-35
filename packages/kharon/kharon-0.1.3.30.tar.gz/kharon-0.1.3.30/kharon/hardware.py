c48ce2416ed9ff8183022a59530202d78397ba8e
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-06 16:19:37 +0000

    Add package kharon/0.1.3.30/kharon-0.1.3.30.tar.gz

---
+++ packages/kharon/kharon-0.1.3.30.tar.gz/kharon-0.1.3.30/kharon/hardware.py
@@ -0,0 +1,34 @@
+# LED, Analog_Pin, Digital_pin, Switch, Pushbutton
+
+
+class Hardware:
+    c = ''
+
+    def __init__(self):
+        pass
+
+    def declaration(self, name):
+        pass
+
+    def setup(self, name):
+        pass
+
+
+class AnalogPin(Hardware):
+    c = ''
+
+
+class DigitalPin(Hardware):
+    c = ''
+
+
+class Led(Hardware):
+    c = ''
+
+
+class Switch(Hardware):
+    c = ''
+
+
+class Button(Hardware):
+    c = ''
