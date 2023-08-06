34bc18f98912c97e3233fae4786b8aa7e583fb8b
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-06 01:54:32 +0000

    Add package pyinstrument/2.2.1/pyinstrument-2.2.1.tar.gz

---
+++ packages/pyinstrument/pyinstrument-2.2.1.tar.gz/pyinstrument-2.2.1/pyinstrument/middleware.py
@@ -0,0 +1,51 @@
+from django.http import HttpResponse
+from django.conf import settings
+from pyinstrument import Profiler
+import time
+import os
+try:
+    from django.utils.deprecation import MiddlewareMixin
+except ImportError:
+    MiddlewareMixin = object
+
+
+class ProfilerMiddleware(MiddlewareMixin):
+    def process_request(self, request):
+        profile_dir = getattr(settings, 'PYINSTRUMENT_PROFILE_DIR', None)
+
+        if getattr(settings, 'PYINSTRUMENT_URL_ARGUMENT', 'profile') in request.GET or profile_dir:
+            profiler = Profiler()
+            profiler.start()
+
+            request.profiler = profiler
+
+
+    def process_response(self, request, response):
+        if hasattr(request, 'profiler'):
+            request.profiler.stop()
+
+            output_html = request.profiler.output_html()
+
+            profile_dir = getattr(settings, 'PYINSTRUMENT_PROFILE_DIR', None)
+
+            if profile_dir:
+                filename = '{total_time:.3f}s {path} {timestamp:.0f}.html'.format(
+                    total_time=request.profiler.root_frame().time(),
+                    path=request.get_full_path().replace('/', '_'),
+                    timestamp=time.time()
+                )
+
+                file_path = os.path.join(profile_dir, filename)
+
+                if not os.path.exists(profile_dir):
+                    os.mkdir(profile_dir)
+
+                with open(file_path, 'w') as f:
+                    f.write(output_html)
+
+            if getattr(settings, 'PYINSTRUMENT_URL_ARGUMENT', 'profile') in request.GET:
+                return HttpResponse(output_html)
+            else:
+                return response
+        else:
+            return response
---
+++ packages/pyinstrument/pyinstrument-2.2.1.tar.gz/pyinstrument-2.2.1/pyinstrument/profiler.py
@@ -0,0 +1,127 @@
+# -*- coding: utf-8 -*-
+import timeit
+import warnings
+
+from . import recorders
+from . import renderers
+from .util import object_with_import_path
+from pyinstrument_cext import setstatprofile
+
+timer = timeit.default_timer
+
+
+class NotMainThreadError(Exception):
+    '''deprecated as of 0.14'''
+    pass
+
+
+class SignalUnavailableError(Exception):
+    '''deprecated as of 0.14'''
+    pass
+
+
+class Profiler(object):
+    def __init__(self, use_signal=None, recorder='time_aggregating'):
+        if use_signal is not None:
+            warnings.warn('use_signal is deprecated and should no longer be used.', DeprecationWarning, stacklevel=2)
+
+        self.interval = 0.001
+        self.last_profile_time = 0
+
+        self.recorder = get_recorder_class(recorder)()
+
+    def start(self):
+        self.last_profile_time = timer()
+        setstatprofile(self._profile, self.interval)
+
+    def stop(self):
+        setstatprofile(None)
+
+    def __enter__(self):
+        self.start()
+        return self
+
+    def __exit__(self, *args):
+        self.stop()
+
+    # pylint: disable=W0613
+    def _profile(self, frame, event, arg):
+        now = timer()
+        time_since_last_signal = now - self.last_profile_time
+
+        if time_since_last_signal < self.interval:
+            return
+
+        if event == 'call':
+            frame = frame.f_back
+
+        self.recorder.record_frame(frame, time_since_last_signal)
+
+        self.last_profile_time = now
+
+    def root_frame(self):
+        """
+        Returns the parsed results in the form of a tree of Frame objects
+        """
+        if not hasattr(self, '_root_frame'):
+            self._root_frame = self.recorder.root_frame()
+
+        return self._root_frame
+
+    def first_interesting_frame(self):
+        """
+        Traverse down the frame hierarchy until a frame is found with more than one child
+        """
+        frame = self.root_frame()
+
+        while len(frame.children) <= 1:
+            if frame.children:
+                frame = frame.children[0]
+            else:
+                # there are no branches
+                return self.root_frame()
+
+        return frame
+
+    def starting_frame(self, root=False):
+        if root:
+            return self.root_frame()
+        else:
+            return self.first_interesting_frame()
+
+    def output_text(self, root=False, unicode=False, color=False):
+        return self.output(renderer='text', root=root, unicode=unicode, color=color)
+
+    def output_html(self, root=False):
+        return self.output(renderer='html', root=root)
+
+    def output(self, renderer, root=False, **renderer_kwargs):
+        if not isinstance(renderer, renderers.Renderer):
+            renderer_class = get_renderer_class(renderer)
+            renderer = renderer_class(**renderer_kwargs)
+
+        return renderer.render(self.starting_frame(root=root))
+
+
+def get_recorder_class(name):
+    if name == 'time_aggregating':
+        return recorders.TimeAggregatingRecorder
+    elif name == 'timeline':
+        return recorders.TimelineRecorder
+    else:
+        return object_with_import_path(name)
+
+
+def get_renderer_class(renderer):
+    if callable(renderer):
+        # allow just passing the class object itself
+        return renderer
+
+    if renderer == 'text':
+        return renderers.ConsoleRenderer
+    elif renderer == 'html':
+        return renderers.HTMLRenderer
+    elif renderer == 'json':
+        return renderers.JSONRenderer
+    else:
+        return object_with_import_path(renderer)
