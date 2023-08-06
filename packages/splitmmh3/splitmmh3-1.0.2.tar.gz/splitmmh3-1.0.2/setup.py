0d042ed5f26e49efd3bee447383cda880a380fee
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-06 07:12:30 +0000

    Add package splitmmh3/1.0.2/splitmmh3-1.0.2.tar.gz

---
+++ packages/splitmmh3/splitmmh3-1.0.2.tar.gz/splitmmh3-1.0.2/setup.py
@@ -0,0 +1,27 @@
+from distutils.core import setup, Extension
+
+mmh3module = Extension('mmh3', sources=['mmh3module.cpp', 'murmur_hash_3.cpp'])
+
+classifiers = [
+    'Development Status :: 5 - Production/Stable',
+    'Intended Audience :: Developers',
+    'License :: Public Domain',
+    'Programming Language :: Python :: 2',
+    'Programming Language :: Python :: 3',
+    'Topic :: Utilities'
+]
+
+setup(
+    name='splitmmh3',
+    version='1.0.2',
+    description=('An adaption of a public domain library'
+                 'with python2.7 support'),
+    license='Public Domain',
+    author='V G, Split Software',
+    author_email='python@split.io',
+    url='http://packages.python.org/splitmmh3',
+    ext_modules=[mmh3module],
+    keywords=['hash', 'MurmurHash'],
+    long_description=open('README.rst').read(),
+    classifiers=classifiers
+)
