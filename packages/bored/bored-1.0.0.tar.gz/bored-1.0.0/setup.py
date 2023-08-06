1502ce7189f494bd6d65e1135a30b299c3ed1ec6
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-06 14:24:05 +0000

    Add package bored/1.0.0/bored-1.0.0.tar.gz

---
+++ packages/bored/bored-1.0.0.tar.gz/bored-1.0.0/setup.py
@@ -0,0 +1,18 @@
+import setuptools
+
+with open("README.md", "r") as fh:
+    long_description = fh.read()
+
+setuptools.setup(
+    name="bored",
+    version="1.0.0",
+    description="Unofficial Wrapper for Bored API",
+    long_description=long_description,
+    long_description_content_type="text/markdown",
+    url="http://henryb.uk",
+    author="Henry Bersey",
+    author_email="henry@bersey.com",
+    packages=["bored"],
+    install_requires=["requests"],
+    zip_save=False
+)
