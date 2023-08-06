f9e9fe21b39d28be84e9a2ae828449c97e2c6562
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-06 14:53:53 +0000

    Add package janetlist/1.1.2/janetlist-1.1.2.tar.gz

---
+++ packages/janetlist/janetlist-1.1.2.tar.gz/janetlist-1.1.2/setup.py
@@ -0,0 +1,13 @@
+from distutils.core import setup
+
+setup(
+    name = 'janetlist', 
+    version  = '1.1.2',
+    py_modules = ['janetlist'],
+    author = 'ZhangPeng',
+    author_email = '444327149@qq.com',
+    url = 'http://www.headfirstlabs.com',
+    description = 'A simple printer of nested lists',
+    )
+
+
