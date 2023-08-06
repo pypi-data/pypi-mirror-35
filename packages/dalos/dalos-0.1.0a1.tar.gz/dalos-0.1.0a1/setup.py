337ba72280a2031972cac15c696055ee9e75dfcb
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-06 01:56:26 +0000

    Add package dalos/0.1.0a1/dalos-0.1.0a1.tar.gz

---
+++ packages/dalos/dalos-0.1.0a1.tar.gz/dalos-0.1.0a1/setup.py
@@ -0,0 +1,9 @@
+from setuptools import setup
+
+setup(name='dalos',
+      version='0.1.0-alpha1',
+      description='Implementations of common algorithms and data structures',
+      url='https://github.com/onelharrison/dalos',
+      author='Onel Harrison',
+      license='GNU General Public License v3',
+      packages=['dalos'])
