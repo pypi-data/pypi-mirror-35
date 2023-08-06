f8065f2ec9d7bfab0548331a86b93fe6307b77ff
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-06 04:09:23 +0000

    Add package xmlschema/1.0.3/xmlschema-1.0.3.tar.gz

---
+++ packages/xmlschema/xmlschema-1.0.3.tar.gz/xmlschema-1.0.3/xmlschema/validators/assertions.py
@@ -0,0 +1,23 @@
+# -*- coding: utf-8 -*-
+#
+# Copyright (c), 2016-2018, SISSA (International School for Advanced Studies).
+# All rights reserved.
+# This file is distributed under the terms of the MIT License.
+# See the file 'LICENSE' in the root directory of the present
+# distribution, or http://opensource.org/licenses/MIT.
+#
+# @author Davide Brunato <brunato@sissa.it>
+#
+"""
+This module contains classes for other XML Schema 1.1 assertions.
+"""
+from ..qnames import XSD_ASSERT_TAG, XSD_ASSERTION_TAG
+from .xsdbase import XsdComponent, ValidationMixin
+
+
+class XsdAssert(XsdComponent, ValidationMixin):
+    admitted_tags = {XSD_ASSERT_TAG}
+
+
+class XsdAssertion(XsdComponent, ValidationMixin):
+    admitted_tags = {XSD_ASSERTION_TAG}
