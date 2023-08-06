044371bb128ce7df940c7b56bfbd3c655df470d5
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-06 13:20:34 +0000

    Add package Jinja2-Dev-Server/0.0.2b5/jinja2-dev-server-0.0.2b5.tar.gz

---
+++ packages/Jinja2-Dev-Server/jinja2-dev-server-0.0.2b5.tar.gz/jinja2-dev-server-0.0.2b5/jinja2_dev_server/command_line.py
@@ -0,0 +1,17 @@
+import argparse
+
+from jinja2_dev_server import server
+
+
+def main():
+    parser = argparse.ArgumentParser(
+        description='Runs a Flask-based Jinja2 development server')
+    parser.add_argument('dirs', metavar='dir', type=str, nargs='+', default="./",
+                        help='directory to be parsed. Defaults to \.')
+
+    args = parser.parse_args()
+
+    args = parser.parse_args()
+    dirs = args.dirs
+
+    server.run_server(dirs)
