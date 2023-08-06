b8779bb79c797dc853ec2b028e4d22f28d9d9fa8
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-06 07:11:20 +0000

    Add package historical/0.3.16/historical-0.3.16.tar.gz

---
+++ packages/historical/historical-0.3.16.tar.gz/historical-0.3.16/historical/models.py
@@ -0,0 +1,84 @@
+"""
+.. module: historical.models
+    :platform: Unix
+    :copyright: (c) 2017 by Netflix Inc., see AUTHORS for more
+    :license: Apache, see LICENSE for more details.
+.. author:: Kevin Glisson <kglisson@netflix.com>
+.. author:: Mike Grima <mgrima@netflix.com>
+"""
+import time
+from datetime import datetime
+
+from marshmallow import Schema, fields
+from historical.attributes import EventTimeAttribute, HistoricalDecimalAttribute
+
+from pynamodb.attributes import UnicodeAttribute, MapAttribute, NumberAttribute
+
+from historical.constants import TTL_EXPIRY
+
+
+EPHEMERAL_PATHS = []
+
+
+def default_ttl():
+    return int(time.time() + TTL_EXPIRY)
+
+
+def default_event_time():
+    return datetime.utcnow().replace(tzinfo=None, microsecond=0).isoformat() + "Z"
+
+
+class DurableHistoricalModel(object):
+    eventTime = EventTimeAttribute(range_key=True, default=default_event_time)
+
+
+class CurrentHistoricalModel(object):
+    eventTime = EventTimeAttribute(default=default_event_time)
+    ttl = NumberAttribute(default=default_ttl())
+    eventSource = UnicodeAttribute()
+
+
+class AWSHistoricalMixin(object):
+    arn = UnicodeAttribute(hash_key=True)
+    accountId = UnicodeAttribute()
+    userIdentity = MapAttribute(null=True)
+    principalId = UnicodeAttribute(null=True)
+    configuration = MapAttribute(null=True)
+    userAgent = UnicodeAttribute(null=True)
+    sourceIpAddress = UnicodeAttribute(null=True)
+    requestParameters = MapAttribute(null=True)
+    version = HistoricalDecimalAttribute()
+
+
+class HistoricalPollingEventDetail(Schema):
+    # You must replace these:
+    event_source = fields.Str(dump_to="eventSource", load_from="eventSource", required=True)
+    event_name = fields.Str(dump_to="eventName", load_from="eventName", required=True)
+    request_parameters = fields.Dict(dump_to="requestParameters", load_from="requestParameters", required=True)
+
+    event_time = fields.Str(dump_to="eventTime", load_from="eventTime", required=True,
+                            default=default_event_time, missing=default_event_time)
+
+
+class HistoricalPollingBaseModel(Schema):
+    version = fields.Str(required=True)
+    account = fields.Str(required=True)
+
+    detail_type = fields.Str(load_from="detail-type", dump_to="detail-type", required=True,
+                             missing='Historical Polling Event', default='Historical Polling Event')
+    source = fields.Str(required=True, missing='historical', default='historical')
+    time = fields.Str(required=True, default=default_event_time, missing=default_event_time)
+
+    # You must replace this:
+    detail = fields.Nested(HistoricalPollingEventDetail, required=True)
+
+
+class HistoricalPollerTaskEventModel(Schema):
+    account_id = fields.Str(required=True)
+    region = fields.Str(required=True)
+
+    def serialize_me(self, account_id, region):
+        return self.dumps({
+            "account_id": account_id,
+            "region": region
+        }).data
---
+++ packages/historical/historical-0.3.16.tar.gz/historical-0.3.16/historical/security_group/models.py
@@ -0,0 +1,86 @@
+"""
+.. module: historical.security_group.models
+    :platform: Unix
+    :copyright: (c) 2017 by Netflix Inc., see AUTHORS for more
+    :license: Apache, see LICENSE for more details.
+.. author:: Kevin Glisson <kglisson@netflix.com>
+"""
+from marshmallow import Schema, fields, post_dump
+
+from pynamodb.models import Model
+from pynamodb.indexes import GlobalSecondaryIndex, AllProjection
+from pynamodb.attributes import UnicodeAttribute, NumberAttribute, MapAttribute
+
+from historical.constants import CURRENT_REGION
+from historical.models import (
+    HistoricalPollingEventDetail,
+    HistoricalPollingBaseModel,
+    DurableHistoricalModel,
+    CurrentHistoricalModel,
+    AWSHistoricalMixin
+)
+
+VERSION = 1
+
+
+class SecurityGroupModel(object):
+    GroupId = UnicodeAttribute()
+    GroupName = UnicodeAttribute()
+    VpcId = UnicodeAttribute(null=True)
+    Tags = MapAttribute()
+    Region = UnicodeAttribute()
+
+
+class DurableSecurityGroupModel(Model, DurableHistoricalModel, AWSHistoricalMixin, SecurityGroupModel):
+    class Meta:
+        table_name = 'HistoricalSecurityGroupDurableTable'
+        region = CURRENT_REGION
+
+
+class CurrentSecurityGroupModel(Model, CurrentHistoricalModel, AWSHistoricalMixin, SecurityGroupModel):
+    class Meta:
+        table_name = 'HistoricalSecurityGroupCurrentTable'
+        region = CURRENT_REGION
+
+
+class ViewIndex(GlobalSecondaryIndex):
+    class Meta:
+        projection = AllProjection()
+        region = CURRENT_REGION
+
+    view = NumberAttribute(default=0, hash_key=True)
+
+
+class SecurityGroupPollingRequestParamsModel(Schema):
+    group_id = fields.Str(dump_to='groupId', load_from='groupId', required=True)
+    owner_id = fields.Str(dump_to='ownerId', load_from='ownerId', required=True)
+
+
+class SecurityGroupPollingEventDetail(HistoricalPollingEventDetail):
+    @post_dump
+    def add_required_security_group_polling_data(self, data):
+        data['eventSource'] = 'historical.ec2.poller'
+        data['eventName'] = 'HistoricalPoller'
+        return data
+
+
+class SecurityGroupPollingEventModel(HistoricalPollingBaseModel):
+    detail = fields.Nested(SecurityGroupPollingEventDetail, required=True)
+
+    @post_dump()
+    def dump_security_group_polling_event_data(self, data):
+        data['version'] = '1'
+        return data
+
+    def serialize(self, account, group):
+        return self.dumps({
+            'account': account,
+            'detail': {
+                'request_parameters': {
+                    'groupId': group['GroupId']
+                }
+            }
+        }).data
+
+
+security_group_polling_schema = SecurityGroupPollingEventModel(strict=True)
