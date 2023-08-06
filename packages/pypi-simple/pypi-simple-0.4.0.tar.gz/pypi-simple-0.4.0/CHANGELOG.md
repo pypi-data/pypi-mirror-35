7feac8706be388fd07a88482f9822344c902efcc
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-06 15:30:11 +0000

    Add package pypi-simple/0.4.0/pypi-simple-0.4.0.tar.gz

---
+++ packages/pypi-simple/pypi-simple-0.4.0.tar.gz/pypi-simple-0.4.0/CHANGELOG.md
@@ -0,0 +1,22 @@
+v0.4.0 (2018-09-06)
+-------------------
+- Publicly (i.e., in the README) document the utility functions
+- Gave `PyPISimple` an `auth` parameter for specifying login/authentication
+  details
+
+v0.3.0 (2018-09-03)
+-------------------
+- When fetching the list of files for a project, the project name is now used
+  to resolve ambiguous filenames.
+- The filename parser now requires all filenames to be all-ASCII (except for
+  wheels).
+
+v0.2.0 (2018-09-01)
+-------------------
+- The filename parser now rejects invalid project names, blatantly invalid
+  versions, and non-ASCII digits.
+- RPM packages are now recognized.
+
+v0.1.0 (2018-08-31)
+-------------------
+Initial release
---
+++ packages/pypi-simple/pypi-simple-0.4.0.tar.gz/pypi-simple-0.4.0/test/test_distrib_pkg.py
@@ -0,0 +1,67 @@
+import pytest
+from   pypi_simple import DistributionPackage
+
+def test_filename_parsed():
+    pkg = DistributionPackage(
+        filename='qypi-0.1.0-py3-none-any.whl',
+        project='qypi',
+        version='0.1.0',
+        package_type='wheel',
+        url="https://files.pythonhosted.org/packages/82/fc/9e25534641d7f63be93079bc07fa92bab136ddf5d4181059a1308a346f96/qypi-0.1.0-py3-none-any.whl#sha256=da69d28dcd527c0e372b3fa7b92fc333b327f8470175f035abc4e351b539189f",
+        requires_python=None,
+        has_sig=False,
+    )
+    assert pkg.project == 'qypi'
+    assert pkg.version == '0.1.0'
+    assert pkg.package_type == 'wheel'
+
+@pytest.mark.parametrize('fragment', ['', '#', '#sha256', '#sha256='])
+def test_get_no_digests(fragment):
+    pkg = DistributionPackage(
+        filename='qypi-0.1.0-py3-none-any.whl',
+        project='qypi',
+        version='0.1.0',
+        package_type='wheel',
+        url="https://files.pythonhosted.org/packages/82/fc/9e25534641d7f63be93079bc07fa92bab136ddf5d4181059a1308a346f96/qypi-0.1.0-py3-none-any.whl" + fragment,
+        requires_python=None,
+        has_sig=False,
+    )
+    assert pkg.get_digests() == {}
+
+def test_get_digests():
+    pkg = DistributionPackage(
+        filename='qypi-0.1.0-py3-none-any.whl',
+        project='qypi',
+        version='0.1.0',
+        package_type='wheel',
+        url="https://files.pythonhosted.org/packages/82/fc/9e25534641d7f63be93079bc07fa92bab136ddf5d4181059a1308a346f96/qypi-0.1.0-py3-none-any.whl#sha256=da69d28dcd527c0e372b3fa7b92fc333b327f8470175f035abc4e351b539189f",
+        requires_python=None,
+        has_sig=False,
+    )
+    assert pkg.get_digests() == {
+        "sha256": "da69d28dcd527c0e372b3fa7b92fc333b327f8470175f035abc4e351b539189f"
+    }
+
+def test_get_no_sig_url():
+    pkg = DistributionPackage(
+        filename='qypi-0.1.0-py3-none-any.whl',
+        project='qypi',
+        version='0.1.0',
+        package_type='wheel',
+        url="https://files.pythonhosted.org/packages/82/fc/9e25534641d7f63be93079bc07fa92bab136ddf5d4181059a1308a346f96/qypi-0.1.0-py3-none-any.whl#sha256=da69d28dcd527c0e372b3fa7b92fc333b327f8470175f035abc4e351b539189f",
+        requires_python=None,
+        has_sig=False,
+    )
+    assert pkg.sig_url is None
+
+def test_get_sig_url():
+    pkg = DistributionPackage(
+        filename='qypi-0.1.0-py3-none-any.whl',
+        project='qypi',
+        version='0.1.0',
+        package_type='wheel',
+        url="https://files.pythonhosted.org/packages/82/fc/9e25534641d7f63be93079bc07fa92bab136ddf5d4181059a1308a346f96/qypi-0.1.0-py3-none-any.whl#sha256=da69d28dcd527c0e372b3fa7b92fc333b327f8470175f035abc4e351b539189f",
+        requires_python=None,
+        has_sig=True,
+    )
+    assert pkg.sig_url == "https://files.pythonhosted.org/packages/82/fc/9e25534641d7f63be93079bc07fa92bab136ddf5d4181059a1308a346f96/qypi-0.1.0-py3-none-any.whl.asc"
