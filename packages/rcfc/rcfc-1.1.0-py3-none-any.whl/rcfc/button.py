052dcd346e33a2684f1a80baac2d9e4ec5b5d672
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-06 13:22:31 +0000

    Add package rcfc/1.1.0/rcfc-1.1.0-py3-none-any.whl

---
+++ packages/rcfc/rcfc-1.1.0-py3-none-any.whl/rcfc/button.py
@@ -0,0 +1,34 @@
+"""
+A collection of buttons to be displayed in the GUI
+"""
+
+from rcfc.server import register_post, register_post_with_state
+
+
+def simple(text, group=None):
+    """ A simple press-button decorator that takes a title """
+    def wrapper(func):
+        register_post({"text": text,
+                       "type": "button.simple",
+                       "groups": _convertGroup(group)},
+                      func)
+    return wrapper
+
+
+def toggle(text, getter, group=None):
+    """ A on/off slider toggle """
+    def wrapper(setter):
+        button = {"text": text,
+                  "type": "button.toggle",
+                  "groups": _convertGroup(group)}
+        register_post_with_state(button, getter, setter)
+    return wrapper
+
+
+def _convertGroup(group):
+    if(isinstance(group, str)):
+        return [group]
+    if group is None:
+        return []
+    assert isinstance(group, list), f"{group} must be a list or string"
+    return group
---
+++ packages/rcfc/rcfc-1.1.0-py3-none-any.whl/rcfc/static/index.js
@@ -0,0 +1,92 @@
+/*jslint browser: true*/
+/*jshint esversion: 6 */
+/*global  $, jQuery, console*/
+/*global console*/
+
+(function () {
+    "use strict";
+    "esversion: 6";
+
+    function loadPage() {
+
+    }
+
+    function getId(id) {
+        return "button" + id;
+    }
+
+    function displayButton(button) {
+        if (button.type === "button.simple") {
+            displaySimpleButton(button);
+        }
+        if (button.type === "button.toggle") {
+            displayToggleButton(button);
+        }
+    }
+
+    function displaySimpleButton(button) {
+        $("#groupContainer").append("<div class='row'><button class='btn-lg btn-info' id='" + getId(button.id) + "'>" + button.text + "</button></div>");
+        $("#" + getId(button.id)).click(function () {
+            $.post({ url: "/buttons/" + button.id });
+        });
+    }
+
+    function displayToggleButton(button) {
+        $("#groupContainer").append("<div class='row'>" + button.text + ": <label class='switch'><input type='checkbox' id='" + getId(button.id) + "'><span class='slider round'></span></label></button></div>");
+        $("#" + getId(button.id)).click(function () {
+            $.post({ url: "/buttons/" + button.id, data: JSON.stringify({ value: $("#" + getId(button.id)).prop('checked') }), contentType: "application/json" });
+        });
+        if (button.state) {
+            $("#" + getId(button.id)).prop('checked', true);
+        }
+
+    }
+
+    function displayGroup(group) {
+        $("#group-tabs").append('<button class=\"w3-bar-item w3-button\" name="'+ group +'")\">' + group + '</button>');
+    }
+
+    function displayGroupTabs(data, groups) {
+        if(groups.length !== 0) {
+            groups = ["All", ...groups, "Unassigned"];
+        }
+        groups.forEach(displayGroup);
+        $("#group-tabs button").click((event) => displayButtons(data, event.target.name));
+        $("#groups").append('<div id="groupContainer" class="container col-xs-offset-3 col-xs-6 text-center group"></div>');
+    }
+
+
+    function displayButtons(data, group) {
+        $("#groupContainer").empty();
+        data.buttons
+            .filter(b => isCorrectGroup(b, group))
+            .forEach(b => displayButton(b) );
+            
+    }
+
+    function isCorrectGroup(button, group) {
+        return button.groups.indexOf(group) !== -1 ||
+               group === "All" ||
+               group === "Unassigned" && button.groups.length === 0;   
+    }
+
+    function loadButtons() {
+        $.ajax({
+            url: "/buttons",
+            success: function (data) {
+                displayGroupTabs(data, getGroups(data));
+                displayButtons(data, "All");
+            },
+            dataType: "json"
+        });
+    }
+
+    function getGroups(data) {
+        return [].concat
+                 .apply([], data.buttons.map((button) => button.groups))
+                 .filter((value, index, self) => self.indexOf(value) === index);
+    }
+
+
+    $(document).ready(loadButtons);
+}());
