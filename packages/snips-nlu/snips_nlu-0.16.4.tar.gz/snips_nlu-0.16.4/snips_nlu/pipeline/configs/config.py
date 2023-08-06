a2d66b4882def0ae89e2732f10568aaaf4286205
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-06 16:17:18 +0000

    Add package snips-nlu/0.16.4/snips_nlu-0.16.4.tar.gz

---
+++ packages/snips-nlu/snips_nlu-0.16.4.tar.gz/snips_nlu-0.16.4/snips_nlu/pipeline/configs/config.py
@@ -0,0 +1,29 @@
+from abc import ABCMeta, abstractmethod
+from builtins import object
+
+from future.utils import with_metaclass
+
+from snips_nlu.utils import classproperty
+
+
+class Config(with_metaclass(ABCMeta, object)):
+    @abstractmethod
+    def to_dict(self):
+        raise NotImplementedError
+
+    @classmethod
+    def from_dict(cls, obj_dict):
+        raise NotImplementedError
+
+
+class ProcessingUnitConfig(with_metaclass(ABCMeta, Config)):
+    """Represents the configuration object needed to initialize a
+        :class:`.ProcessingUnit`"""
+
+    @classproperty
+    def unit_name(self):
+        raise NotImplementedError
+
+    @abstractmethod
+    def get_required_resources(self):
+        raise NotImplementedError
---
+++ packages/snips-nlu/snips_nlu-0.16.4.tar.gz/snips_nlu-0.16.4/snips_nlu/slot_filler/features_utils.py
@@ -0,0 +1,61 @@
+from __future__ import unicode_literals
+
+from snips_nlu_utils import compute_all_ngrams
+
+from snips_nlu.builtin_entities import is_builtin_entity
+from snips_nlu.constants import (DATA, END, ENTITIES, ENTITY, INTENTS,
+                                 RES_MATCH_RANGE, START, UTTERANCES)
+from snips_nlu.utils import LimitedSizeDict
+
+_NGRAMS_CACHE = LimitedSizeDict(size_limit=1000)
+
+
+def get_all_ngrams(tokens):
+    if not tokens:
+        return []
+    key = "<||>".join(tokens)
+    if key not in _NGRAMS_CACHE:
+        ngrams = compute_all_ngrams(tokens, len(tokens))
+        _NGRAMS_CACHE[key] = ngrams
+    return _NGRAMS_CACHE[key]
+
+
+def get_word_chunk(word, chunk_size, chunk_start, reverse=False):
+    if chunk_size < 1:
+        raise ValueError("chunk size should be >= 1")
+    if chunk_size > len(word):
+        return None
+    start = chunk_start - chunk_size if reverse else chunk_start
+    end = chunk_start if reverse else chunk_start + chunk_size
+    return word[start:end]
+
+
+def initial_string_from_tokens(tokens):
+    current_index = 0
+    s = ""
+    for t in tokens:
+        if t.start > current_index:
+            s += " " * (t.start - current_index)
+        s += t.value
+        current_index = t.end
+    return s
+
+
+def entity_filter(entity, start, end):
+    entity_start = entity[RES_MATCH_RANGE][START]
+    entity_end = entity[RES_MATCH_RANGE][END]
+    return (entity_start <= start < entity_end) \
+           and (entity_start < end <= entity_end)
+
+
+def get_intent_custom_entities(dataset, intent):
+    intent_entities = set()
+    for utterance in dataset[INTENTS][intent][UTTERANCES]:
+        for c in utterance[DATA]:
+            if ENTITY in c:
+                intent_entities.add(c[ENTITY])
+    custom_entities = dict()
+    for ent in intent_entities:
+        if not is_builtin_entity(ent):
+            custom_entities[ent] = dataset[ENTITIES][ent]
+    return custom_entities
