a52688ae3de356baa15c524396641f881de598b2
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-06 13:53:55 +0000

    Add package unrestricted-advex/0.0.3/unrestricted_advex-0.0.3-py3-none-any.whl

---
+++ packages/unrestricted-advex/unrestricted_advex-0.0.3-py3-none-any.whl/unrestricted_advex/attacks.py
@@ -0,0 +1,264 @@
+from __future__ import absolute_import
+from __future__ import division
+from __future__ import print_function
+
+from itertools import product, repeat
+
+import numpy as np
+import tensorflow as tf
+from cleverhans.attacks import SPSA
+from cleverhans.model import Model
+from foolbox.attacks import BoundaryAttack as FoolboxBoundaryAttack
+from six.moves import xrange
+
+
+class Attack(object):
+  name = 'unnamed_attack'
+
+  def __call__(self, *args, **kwargs):
+    raise NotImplementedError()
+
+
+class NullAttack(Attack):
+  name = 'null_attack'
+
+  def __call__(self, model_fn, images_batch_nchw, y_np):
+    del y_np, model_fn  # unused
+    return images_batch_nchw
+
+
+class SpsaAttack(Attack):
+  name = 'spsa_attack'
+
+  def __init__(self, model, image_shape_hwc, epsilon=(16. / 255), is_debug=False):
+    self.graph = tf.Graph()
+
+    with self.graph.as_default():
+      self.sess = tf.Session(graph=self.graph)
+
+      self.x_input = tf.placeholder(tf.float32, shape=(1,) + image_shape_hwc)
+      self.y_label = tf.placeholder(tf.int32, shape=(1,))
+
+      self.model = model
+      attack = SPSA(CleverhansPyfuncModelWrapper(self.model), sess=self.sess)
+      self.x_adv = attack.generate(
+        self.x_input,
+        y=self.y_label,
+        epsilon=epsilon,
+        num_steps=200,
+        early_stop_loss_threshold=-1.,
+        batch_size=32,
+        is_debug=is_debug)
+
+    self.graph.finalize()
+
+  def __call__(self, model, x_np, y_np):  # (4. / 255)):
+    if model != self.model:
+      raise NotImplementedError('Cannot call spsa attack on different models')
+    del model  # unused except to check that we already wired it up right
+
+    with self.graph.as_default():
+      all_x_adv_np = []
+      for i in xrange(len(x_np)):
+        x_adv_np = self.sess.run(self.x_adv, feed_dict={
+          self.x_input: np.expand_dims(x_np[i], axis=0),
+          self.y_label: np.expand_dims(y_np[i], axis=0),
+        })
+        all_x_adv_np.append(x_adv_np)
+      return np.concatenate(all_x_adv_np)
+
+
+class BoundaryAttack(object):
+  name = "boundary"
+
+  def __init__(self, model, max_l2_distortion=4):
+    self.max_l2_distortion = max_l2_distortion
+
+    class Model:
+      def bounds(self):
+        return [0, 1]
+
+      def predictions(self, img):
+        return model(img[np.newaxis, :, :, :])[0]
+
+      def batch_predictions(self, img):
+        return model(img)
+
+    self.attack = FoolboxBoundaryAttack(model=Model())
+
+  def __call__(self, model, x_np, y_np):
+    r = []
+    for i in range(len(x_np)):
+      adv = self.attack(x_np[i], y_np[i],
+                        log_every_n_steps=100,  # Reduce verbosity of the attack
+                        )
+      distortion = np.sum((x_np[i] - adv) ** 2) ** .5
+      if distortion > self.max_l2_distortion:
+        # project to the surface of the L2 ball
+        adv = x_np[i] + (adv - x_np[i]) / distortion * self.max_l2_distortion
+
+      r.append(adv)
+    return r
+
+
+class SpatialGridAttack(Attack):
+  """Attack from "A Rotation and a Translation Suffice: Fooling CNNs with
+  Simple Transformations", Engstrom et al. 2018
+
+  https://arxiv.org/pdf/1712.02779.pdf
+  """
+  name = 'spatial'
+
+  def __init__(self, image_shape_hwc,
+               spatial_limits,
+               grid_granularity,
+               black_border_size,
+               valid_check=None,
+               ):
+    """
+    :param model_fn: a callable: batch-input -> batch-probability in [0, 1]
+    :param spatial_limits:
+    :param grid_granularity:
+    """
+    self.limits = spatial_limits
+    self.granularity = grid_granularity
+    self.valid_check = valid_check
+
+    # Construct graph for spatial attack
+    self.graph = tf.Graph()
+    with self.graph.as_default():
+      self._x_for_trans = tf.placeholder(tf.float32, shape=[None] + list(image_shape_hwc))
+      self._t_for_trans = tf.placeholder(tf.float32, shape=[None, 3])
+
+      x = apply_black_border(
+        self._x_for_trans,
+        image_height=image_shape_hwc[0],
+        image_width=image_shape_hwc[1],
+        border_size=black_border_size
+      )
+
+      self._tranformed_x_op = apply_transformation(
+        x,
+        transform=self._t_for_trans,
+        image_height=image_shape_hwc[0],
+        image_width=image_shape_hwc[1],
+      )
+      self.session = tf.Session()
+
+    self.grid_store = []
+
+  def __call__(self, model_fn, x_np, y_np):
+    n = len(x_np)
+    grid = product(*list(np.linspace(-l, l, num=g)
+                         for l, g in zip(self.limits, self.granularity)))
+
+    worst_x = np.copy(x_np)
+    max_xent = np.zeros(n)
+    all_correct = np.ones(n).astype(bool)
+
+    trans_np = np.stack(
+      repeat([0, 0, 0], n))
+    with self.graph.as_default():
+      x_downsize_np = self.session.run(self._tranformed_x_op, feed_dict={
+        self._x_for_trans: x_np,
+        self._t_for_trans: trans_np,
+
+      })
+
+    for horizontal_trans, vertical_trans, rotation in grid:
+      trans_np = np.stack(
+        repeat([horizontal_trans, vertical_trans, rotation], n))
+
+      # Apply the spatial attack
+      with self.graph.as_default():
+        x_np = self.session.run(self._tranformed_x_op, feed_dict={
+          self._x_for_trans: x_np,
+          self._t_for_trans: trans_np,
+
+        })
+      # See how the model_fn performs on the perturbed input
+      logits = model_fn(x_np)
+      preds = np.argmax(logits, axis=1)
+
+      cur_xent = _sparse_softmax_cross_entropy_with_logits_from_numpy(
+        logits, y_np, self.graph, self.session)
+
+      cur_xent = np.asarray(cur_xent)
+      cur_correct = np.equal(y_np, preds)
+
+      if self.valid_check:
+        is_valid = self.valid_check(x_downsize_np, x_np)
+        cur_correct |= ~is_valid
+        cur_xent -= is_valid * 1e9
+
+      # Select indices to update: we choose the misclassified transformation
+      # of maximum xent (or just highest xent if everything else if correct).
+      idx = (cur_xent > max_xent) & (cur_correct == all_correct)
+      idx = idx | (cur_correct < all_correct)
+      max_xent = np.maximum(cur_xent, max_xent)
+      all_correct = cur_correct & all_correct
+
+      idx = np.expand_dims(idx, axis=-1)  # shape (bsize, 1)
+
+      idx = np.expand_dims(idx, axis=-1)
+      idx = np.expand_dims(idx, axis=-1)  # shape (bsize, 1, 1, 1)
+      worst_x = np.where(idx, x_np, worst_x, )  # shape (bsize, 32, 32, 3)
+
+    return worst_x
+
+
+def _sparse_softmax_cross_entropy_with_logits_from_numpy(logits_np, labels_np, graph, sess):
+  """Helper that calls the TF sparse_softmax_cross_entropy_with_logits function"""
+  with graph.as_default():
+    labels_tf = tf.placeholder(tf.int32, [None])
+    logits_tf = tf.placeholder(tf.float32, [None, None])
+    xent_tf = tf.nn.sparse_softmax_cross_entropy_with_logits(
+      labels=labels_tf, logits=logits_tf)
+    return sess.run(xent_tf, feed_dict={
+      labels_tf: labels_np, logits_tf: logits_np})
+
+
+def apply_black_border(x, image_height, image_width, border_size):
+  x = tf.image.resize_images(x, (image_width - border_size,
+                                 image_height - border_size))
+  x = tf.pad(x, [[0, 0],
+                 [border_size, border_size],
+                 [border_size, border_size],
+                 [0, 0]], 'CONSTANT')
+  return x
+
+
+def apply_transformation(x, transform, image_height, image_width):
+  # Map a transformation onto the input
+  trans_x, trans_y, rot = tf.unstack(transform, axis=1)
+  rot *= np.pi / 180  # convert degrees to radians
+
+  # Pad the image to prevent two-step rotation / translation
+  # resulting in a cropped image
+  x = tf.pad(x, [[0, 0], [50, 50], [50, 50], [0, 0]], 'CONSTANT')
+
+  # rotate and translate image
+  ones = tf.ones(shape=tf.shape(trans_x))
+  zeros = tf.zeros(shape=tf.shape(trans_x))
+  trans = tf.stack([ones, zeros, -trans_x,
+                    zeros, ones, -trans_y,
+                    zeros, zeros], axis=1)
+  x = tf.contrib.image.rotate(x, rot, interpolation='BILINEAR')
+  x = tf.contrib.image.transform(x, trans, interpolation='BILINEAR')
+  return tf.image.resize_image_with_crop_or_pad(
+    x, image_height, image_width)
+
+
+class CleverhansPyfuncModelWrapper(Model):
+  nb_classes = 2
+
+  def __init__(self, model_fn):
+    """
+    Wrap a callable function that takes a numpy array of shape (N, C, H, W),
+    and outputs a numpy vector of length N, with each element in range [0, 1].
+    """
+    self.model_fn = model_fn
+
+  def fprop(self, x, **kwargs):
+    logits_op = tf.py_func(self.model_fn, [x], tf.float32)
+    return {'logits': logits_op}
---
+++ packages/unrestricted-advex/unrestricted_advex-0.0.3-py3-none-any.whl/unrestricted_advex/eval_kit.py
@@ -0,0 +1,172 @@
+"""Evaluate a model with attacks."""
+from __future__ import absolute_import
+from __future__ import division
+from __future__ import print_function
+
+import os
+
+import bird_or_bicyle
+import numpy as np
+from tqdm import tqdm
+from unrestricted_advex import attacks, plotting
+
+EVAL_WITH_ATTACKS_DIR = '/tmp/eval_with_attacks'
+
+
+def _validate_logits(logits, batch_size):
+  """Validate the model_fn to help the researcher with debugging"""
+
+  desired_logits_shape = (batch_size, 2)
+  assert logits.shape == desired_logits_shape, \
+    "Your model_fn must return logits with shape %s. Got %s instead." % (
+      desired_logits_shape, logits.shape)
+
+  assert logits.dtype == np.float32, \
+    "Your model_fn must return logits as np.float32. Got %s instead. \
+    Try using logits_np.astype(np.float32)" % logits.dtype
+
+
+def run_attack(model, data_iter, attack_fn):
+  """ Runs an attack on the model_fn for every batch in data_iter and returns the results
+
+  :param model: Callable batch-input -> batch-probability in [0, 1]
+  :param data_iter: NHWC data iterator
+  :param attack_fn: Callable (model_fn, x_np, y_np) -> x_adv
+  :return: (logits, labels, x_adv)
+  """
+  all_labels = []
+  all_logits = []
+  all_xadv = []
+
+  # TODO: Add assertion about the model's throughput
+  for i_batch, (x_np, y_np) in enumerate(tqdm(data_iter)):
+    assert x_np.shape[-1] == 3 or x_np.shape[-1] == 1, "Data was {}, should be NHWC".format(
+      x_np.shape)
+
+    x_adv = attack_fn(model, x_np, y_np)
+    logits = model(x_adv)
+
+    _validate_logits(logits, batch_size=len(x_np))
+
+    all_labels.append(y_np)
+    all_logits.append(logits)
+    all_xadv.append(x_adv)
+
+  return (np.concatenate(all_logits),
+          np.concatenate(all_labels),
+          np.concatenate(all_xadv))
+
+
+def _evaluate_two_class_unambiguous_model(model_fn, data_iter, attack_list, model_name=None):
+  """
+  Evaluates a model_fn on a set of attacks and creates plots
+  :param model_fn: A function mapping images to logits
+  :param dataset_iter: An iterable that returns (batched_images, batched_labels)
+  :param attack_list: A list of callable Attacks
+  :param model_name: An optional model_fn name
+  """
+  for attack in attack_list:
+    print("Executing attack: %s" % attack.name)
+
+    logits, labels, x_adv = run_attack(model_fn, data_iter, attack)
+
+    preds = (logits[:, 0] < logits[:, 1]).astype(np.int64)
+    correct = np.equal(preds, labels).astype(np.float32)
+    correct_fracs = np.sum(correct, axis=0) / len(labels)
+    print("Fraction correct under %s: %.3f" % (attack.name, correct_fracs))
+
+    results_dir = os.path.join(EVAL_WITH_ATTACKS_DIR, attack.name)
+    plotting.save_correct_and_incorrect_adv_images(x_adv, correct, results_dir)
+
+    # Confidence is the value of the larger of the two logits
+    confidences = np.max(logits, axis=1)
+
+    # We will plot accuracy at various coverages
+    coverages = np.linspace(0.01, .99, 99)
+
+    cov_to_confident_error_idxs = _get_coverage_to_confident_error_idxs(
+      coverages, preds, confidences, labels, )
+
+    plotting.plot_confident_error_rate(
+      coverages, cov_to_confident_error_idxs, len(labels), attack.name, results_dir,
+      legend=model_name)
+
+
+def _get_coverage_to_confident_error_idxs(coverages, preds, confidences, y_true):
+  """Returns a list of confident error indices for each coverage"""
+  sorted_confidences = list(sorted(confidences, reverse=True))
+
+  cov_to_confident_error_idxs = []
+
+  for coverage in coverages:
+    threshold = sorted_confidences[int(coverage * len(preds))]
+    confident_mask = confidences >= threshold
+    confident_error_mask = (y_true != preds) * confident_mask
+    confident_error_idx = confident_error_mask.nonzero()[0]
+
+    cov_to_confident_error_idxs.append(confident_error_idx)
+
+  return cov_to_confident_error_idxs
+
+
+def evaluate_two_class_mnist_model(model_fn, dataset_iter=None, model_name=None):
+  """
+  Evaluates a two-class MNIST model_fn on a default set of attacks and creates plots
+  :param model_fn: A function mapping images to logits
+  :param dataset_iter: An iterable that returns (batched_images, batched_labels)
+  :param model_name: An optional model_fn name
+  """
+
+  def _mnist_valid_check(before, after):
+    weight_before = np.sum(np.abs(before), axis=(1, 2, 3))
+    weight_after = np.sum(np.abs(after), axis=(1, 2, 3))
+    return np.abs(weight_after - weight_before) < weight_before * .1
+
+  attack_list = [
+    attacks.BoundaryAttack(
+      model_fn,
+      max_l2_distortion=4),
+    attacks.NullAttack(),
+    attacks.SpsaAttack(
+      model_fn,
+      image_shape_hwc=(28, 28, 1),
+      epsilon=0.3),
+    attacks.SpatialGridAttack(
+      image_shape_hwc=(28, 28, 1),
+      spatial_limits=[10, 10, 10],
+      grid_granularity=[10, 10, 10],
+      black_border_size=4,
+      valid_check=_mnist_valid_check),
+  ]
+
+  return _evaluate_two_class_unambiguous_model(
+    model_fn, dataset_iter,
+    model_name=model_name,
+    attack_list=attack_list)
+
+
+def evaluate_bird_or_bicycle_model(model_fn, dataset_iter=None, model_name=None):
+  """
+  Evaluates a bird_or_bicycle classifier on a default set of attacks and creates plots
+  :param model_fn: A function mapping images to logits
+  :param dataset_iter: An iterable that returns (batched_images, batched_labels)
+  :param model_name: An optional model_fn name
+  """
+  if dataset_iter is None:
+    dataset_iter = bird_or_bicyle.get_iterator('test')
+
+  attack_list = [
+    attacks.NullAttack(),
+    attacks.SpsaAttack(
+      model_fn,
+      image_shape_hwc=(224, 224, 3),
+      epsilon=(16. / 255)),
+    attacks.SpatialGridAttack(
+      image_shape_hwc=(224, 224, 3),
+      spatial_limits=[18, 18, 30],
+      grid_granularity=[5, 5, 31],
+      black_border_size=0)
+  ]
+  return _evaluate_two_class_unambiguous_model(model_fn, dataset_iter,
+                                               model_name=model_name,
+                                               attack_list=attack_list)
