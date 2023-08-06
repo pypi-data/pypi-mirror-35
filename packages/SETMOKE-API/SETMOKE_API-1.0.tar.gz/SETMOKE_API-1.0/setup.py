ca608936a9407116c8ee39081592f8069a94c0a9
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-06 15:30:25 +0000

    Add package SETMOKE-API/1.0/SETMOKE_API-1.0.tar.gz

---
+++ packages/SETMOKE-API/SETMOKE_API-1.0.tar.gz/SETMOKE_API-1.0/setup.py
@@ -0,0 +1,23 @@
+import setuptools
+
+setuptools.setup(
+    name="SETMOKE_API",
+    version="v1.0",
+    author="rehab shahzadi",
+    author_email="rehab.shahzadi@kics.edu.pk",
+    description="An API for social media data extraction",
+    url="https://github.com/rehabshahzadi/SETMOK_API",
+    packages=setuptools.find_packages(),
+
+    classifiers = (
+                  "Programming Language :: Python :: 3",
+                  "Operating System :: OS Independent",
+
+              ),
+     install_requires=[
+        'google-api-python-client',
+        'tweepy==3.6.0',
+        'twitter==1.18.0'
+    ],
+
+)
