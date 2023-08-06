01ab061b4bf6aa8a9b959f772b7b8102827de250
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-06 07:10:31 +0000

    Add package py-gltf/0.2.4/py-gltf-0.2.4.tar.gz

---
+++ packages/py-gltf/py-gltf-0.2.4.tar.gz/py-gltf-0.2.4/setup.py
@@ -0,0 +1,12 @@
+#!/usr/bin/env python
+
+from distutils.core import setup
+
+setup(
+    name='py-gltf',
+    version='0.2.4',
+    author='Trey Nelson',
+    packages=['gltf'],
+    install_requires=['pyquaternion']
+)
+
