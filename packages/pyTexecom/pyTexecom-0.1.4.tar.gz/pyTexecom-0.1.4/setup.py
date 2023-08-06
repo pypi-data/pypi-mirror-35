d828f2fba43f53ada0e688612d47d1895604c470
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-06 01:54:46 +0000

    Add package pyTexecom/0.1.4/pyTexecom-0.1.4.tar.gz

---
+++ packages/pyTexecom/pyTexecom-0.1.4.tar.gz/pyTexecom-0.1.4/setup.py
@@ -0,0 +1,21 @@
+import setuptools
+
+with open("README.md", "r") as fh:
+    long_description = fh.read()
+
+setuptools.setup(
+    name="pyTexecom",
+    version="0.1.4",
+    author="James Drew Williams",
+    author_email="james@jmtechnical.co.uk",
+    description="Texecom Interface Libary",
+    long_description=long_description,
+    long_description_content_type="text/markdown",
+    url="https://github.com/JDW2018/pyTexecom",
+    packages=setuptools.find_packages(),
+    classifiers=[
+        "Programming Language :: Python :: 3",
+        "License :: OSI Approved :: MIT License",
+        "Operating System :: OS Independent",
+    ],
+)
