cd7e6dd520d24eb6adf9316b1c064c280fbc1305
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-06 13:21:44 +0000

    Add package resolwe/12.0.0a1/resolwe-12.0.0a1-py3-none-any.whl

---
+++ packages/resolwe/resolwe-12.0.0a1-py3-none-any.whl/resolwe/permissions/filters.py
@@ -0,0 +1,30 @@
+""".. Ignore pydocstyle D400.
+
+==================
+Permissions Filter
+==================
+
+"""
+from rest_framework.filters import BaseFilterBackend
+
+from resolwe.permissions.shortcuts import get_objects_for_user
+
+
+class ResolwePermissionsFilter(BaseFilterBackend):
+    """Permissions filter."""
+
+    def filter_queryset(self, request, queryset, view):
+        """Filter permissions queryset."""
+        user = request.user
+
+        app_label = queryset.model._meta.app_label  # pylint: disable=protected-access
+        model_name = queryset.model._meta.model_name  # pylint: disable=protected-access
+
+        kwargs = {}
+        if model_name == 'storage':
+            model_name = 'data'
+            kwargs['perms_filter'] = 'data__pk__in'
+
+        permission = '{}.view_{}'.format(app_label, model_name)
+
+        return get_objects_for_user(user, permission, queryset, **kwargs)
