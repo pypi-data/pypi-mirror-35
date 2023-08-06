c6d8b27c4c9a3c119b1efedaf86d674bf737be2d
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-06 11:49:25 +0000

    Add package jetdc-neaster/1.1.0/jetdc-neaster-1.1.0.tar.gz

---
+++ packages/jetdc-neaster/jetdc-neaster-1.1.0.tar.gz/jetdc-neaster-1.1.0/jetdc-neaster.py
@@ -0,0 +1,10 @@
+﻿"""练习1，函数定义"""
+def print_lol(the_list, level=0):
+ """列表打印函数"""
+ for each_item in the_list:
+  if isinstance(each_item, list):
+   print_lol(each_item, level)
+  else:
+   for tab_stop in range(level+1):
+    print("\t",end='')
+   print(each_item)
---
+++ packages/jetdc-neaster/jetdc-neaster-1.1.0.tar.gz/jetdc-neaster-1.1.0/setup.py
@@ -0,0 +1,11 @@
+from distutils.core import setup
+
+setup(
+    name='jetdc-neaster',
+    version='1.1.0',
+    py_modules=['jetdc-neaster'],
+    author='jetdc',
+    author_email='jetdc@163.com',
+    url='http://www.163.com',
+    description='just for test',
+    )
