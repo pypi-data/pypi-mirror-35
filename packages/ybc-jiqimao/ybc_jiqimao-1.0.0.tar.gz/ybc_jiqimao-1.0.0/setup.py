d178fcf80cdb9a0e7b071e94de22407a268dafce
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-06 14:56:35 +0000

    Add package ybc-jiqimao/1.0.0/ybc_jiqimao-1.0.0.tar.gz

---
+++ packages/ybc-jiqimao/ybc_jiqimao-1.0.0.tar.gz/ybc_jiqimao-1.0.0/setup.py
@@ -0,0 +1,12 @@
+from distutils.core import setup
+setup(
+  name = 'ybc_jiqimao',
+  packages = ['ybc_jiqimao'],
+  version = '1.0.0',
+  description = 'draw a jiqimao',
+  long_description='draw a jiqimao',
+  author = 'KingHS',
+  author_email = '382771946@qq.com',
+  keywords = ['pip3', 'turtle', 'jiqimao'],
+  license='MIT',
+)
