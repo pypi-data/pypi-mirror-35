bfcdac76283943804715039ca08b63281fe65314
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-06 14:22:48 +0000

    Add package apssh/0.13.1/apssh-0.13.1.tar.gz

---
+++ packages/apssh/apssh-0.13.1.tar.gz/apssh-0.13.1/apssh/__init__.py
@@ -0,0 +1,45 @@
+"""
+the apssh package
+
+https://github.com/parmentelat/apssh
+http://apssh.readthedocs.io/
+
+"""
+
+from .version import __version__
+
+# protect for install-time when dependencies are not yet installed
+try:
+    import asyncssh
+
+    # basic tools to deal with keys
+    from .keys import load_private_keys, load_agent_keys, import_private_key
+
+    # basic ssh connections and sessions
+    from .sshproxy import SshProxy
+
+    # how to format outputs
+    from .formatters import (
+        RawFormatter, TerminalFormatter,
+        ColonFormatter, TimeColonFormatter, CaptureFormatter
+    )
+
+    from .commands import Run, RunScript, RunString, Push, Pull
+
+    # jobs for asynciojobs
+    from .sshjob import SshJob, CommandFailedError
+
+    # SshNode is just an SshProxy with a slightly different
+    #  default for keys management
+    # LocalNode is helpful to add local commands in a scenario
+    from .nodes import SshNode, LocalNode
+
+    from .topology import (
+        close_ssh_in_scheduler, co_close_ssh_in_scheduler,
+        topology_graph, topology_dot, topology_as_dotfile, topology_as_pngfile
+    )
+
+
+except Exception as exc:                                # pylint: disable=W0703
+    print("Warning: could not import module asyncssh")
+    print(exc)
