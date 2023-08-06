2c10e8961186cdf2c715a2faa08bf447ca826da5
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-06 15:34:56 +0000

    Add package crawlib/0.0.25/crawlib-0.0.25-py2-none-any.whl

---
+++ packages/crawlib/crawlib-0.0.25-py2-none-any.whl/crawlib/pipeline/mongodb/__init__.py
@@ -0,0 +1,6 @@
+#!/usr/bin/env python
+# -*- coding: utf-8 -*-
+
+from . import query_builder
+from .orm import ExtendedDocument, ExtendedDocumentSingleStatus
+from .spider import execute_one_to_many_job
