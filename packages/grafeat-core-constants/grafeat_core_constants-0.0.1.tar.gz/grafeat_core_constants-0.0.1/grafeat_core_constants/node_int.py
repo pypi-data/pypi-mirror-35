909b7eab19884ddd577bd6733647aec04fdd6458
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-06 07:07:39 +0000

    Add package grafeat-core-constants/0.0.1/grafeat_core_constants-0.0.1.tar.gz

---
+++ packages/grafeat-core-constants/grafeat_core_constants-0.0.1.tar.gz/grafeat_core_constants-0.0.1/grafeat_core_constants/node_int.py
@@ -0,0 +1,42 @@
+# *********************************************************************
+# +++ IMPORTS
+# *********************************************************************
+from typing import Optional
+from grafeat_api.base.node import Node
+
+
+# *********************************************************************
+# +++ CLASS
+# *********************************************************************
+class NodeInt(Node):
+    # =====================================================================
+    # +++ CONSTANTS
+    # =====================================================================
+    Param_Value = 0
+    Output_Value = 0
+
+    # =====================================================================
+    # +++ CONSTRUCTOR
+    # =====================================================================
+    def __init__(self, id_: Optional[int] = None):
+        super(NodeInt, self).__init__(id_)
+        self._display_name = 'Integer'
+
+        self._create_parameter(self.Param_Value, 'Value', 0, int)
+        self._create_output(self.Output_Value, 'Out', str)
+
+    # =====================================================================
+    # +++ STATIC OVERRIDES
+    # =====================================================================
+    @staticmethod
+    def get_description() -> str:
+        return 'Creates a constant integer value.'
+
+    # =====================================================================
+    # +++ OVERRIDES
+    # =====================================================================
+    def _validate(self):
+        return True
+
+    def _compute_outputs(self):
+            self._outputs[self.Output_Value].value = f'{self._parameters[self.Param_Value].value}'
---
+++ packages/grafeat-core-constants/grafeat_core_constants-0.0.1.tar.gz/grafeat_core_constants-0.0.1/setup.py
@@ -0,0 +1,14 @@
+from setuptools import setup
+
+setup(
+    name='grafeat_core_constants',
+    version='0.0.1',
+    packages=['grafeat_core_constants'],
+    install_requires=[],
+    url='https://github.com/syntheticastudio/grafeat_plugins',
+    author='Ruben Henares',
+    author_email='ruben@syntheticastudio.com',
+    description='Core plugins for grafeat.',
+    keywords=['grafeat', 'plugins']
+)
+
