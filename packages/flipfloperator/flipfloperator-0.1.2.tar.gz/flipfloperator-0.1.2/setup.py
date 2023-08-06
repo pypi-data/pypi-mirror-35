08ca08bcf1d653bbc6b9f24cc82f3c09a135db30
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-06 01:54:40 +0000

    Add package flipfloperator/0.1.2/flipfloperator-0.1.2.tar.gz

---
+++ packages/flipfloperator/flipfloperator-0.1.2.tar.gz/flipfloperator-0.1.2/setup.py
@@ -0,0 +1,15 @@
+import setuptools
+
+with open('README.md', 'r') as f:
+    long_description = f.read()
+
+setuptools.setup(
+    name='flipfloperator',
+    version='0.1.2',
+    author='Peter Ward',
+    author_email='peteraward@gmail.com',
+    description='Sorry.',
+    long_description=long_description,
+    long_description_content_type='text/markdown',
+    py_modules=['flipfloperator'],
+)
