6b4437cfe5f973b813b91c79840934b859b033fb
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-06 17:14:38 +0000

    Add package stdclasses/0.0.12/stdclasses-0.0.12.tar.gz

---
+++ packages/stdclasses/stdclasses-0.0.12.tar.gz/stdclasses-0.0.12/setup.py
@@ -0,0 +1,27 @@
+from setuptools import setup
+
+setup(
+    name='stdclasses',
+    description='Ia to help',
+    long_description='Ia to help students',
+    version='0.0.12',
+    url='https://github.com/guimartino/stdclasses',
+    author='Guilherme Martino',
+    author_email='gui.martino@hotmail.com',
+    license='MIT',
+    classifiers=[
+        'Development Status :: 4 - Beta',
+        'Intended Audience :: System Administrators',
+        'License :: OSI Approved :: Apache Software License',
+        'Programming Language :: Python :: 3'
+    ],
+    packages=['stdclasses'],
+    install_requires=['pandas>=0.23.4']
+)
+
+# rm -rf ~/Library/Caches/pip
+# python setup.py sdist bdist_wheel
+# git add .
+# git commit -m "version"
+# git push origin master
+# python -m twine upload --repository-url https://upload.pypi.org/legacy/ dist/*
