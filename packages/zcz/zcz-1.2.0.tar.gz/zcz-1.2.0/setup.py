fe302c0302769aa2c082ba36059cd5b6bff7b02c
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-06 16:16:23 +0000

    Add package zcz/1.2.0/zcz-1.2.0.tar.gz

---
+++ packages/zcz/zcz-1.2.0.tar.gz/zcz-1.2.0/setup.py
@@ -0,0 +1,11 @@
+from distutils.core import setup
+
+setup(
+		name         = 'zcz',
+		version      = '1.2.0',
+		py_modules   = ['zhoufirst'],
+		author       = 'changzhiZhou',
+		author_email = '1059482037@qq.com',
+		url          = 'http://baidu.com',
+		description  = 'A simple printer of nested lists'
+		)
---
+++ packages/zcz/zcz-1.2.0.tar.gz/zcz-1.2.0/zhoufirst.py
@@ -0,0 +1,12 @@
+"""这是周长智写的第一个python模块"""
+def print_cz(the_list, indent=False, level=0):
+	"""This is the first python 
+	   codes."""
+	for each_item in the_list:
+		if isinstance(each_item, list):
+			print_cz(each_item, indent, level+1)
+		else:
+			if indent:
+				for i in range(level):
+					print('\t',end='')
+			print(each_item)
