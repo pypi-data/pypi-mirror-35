7811da2ae28f2cf9cfd5c62dd04f70b9a46f1001
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-06 07:10:10 +0000

    Add package pyculiarity/0.0.7/pyculiarity-0.0.7.tar.gz

---
+++ packages/pyculiarity/pyculiarity-0.0.7.tar.gz/pyculiarity-0.0.7/setup.py
@@ -0,0 +1,41 @@
+"""
+Usage details and source available here: https://github.com/nicolasmiller/pyculiarity.
+
+The original R source and examples are available here: https://github.com/twitter/AnomalyDetection.
+
+Copyright and License
+
+Python port Copyright 2015 Nicolas Steven Miller
+
+Original R source Copyright 2015 Twitter, Inc and other contributors
+
+Licensed under the GPLv3
+"""
+
+from setuptools import setup, find_packages
+
+setup(
+    name='pyculiarity',
+    version='0.0.7',
+    description='A Python port of Twitter\'s AnomalyDetection R Package.',
+    long_description=__doc__,
+    url='https://github.com/nicolasmiller/pyculiarity',
+    author='Nicolas Steven Miller',
+    author_email='nicolasmiller@gmail.com',
+    license='GPL',
+
+    classifiers=[
+        'Development Status :: 3 - Alpha',
+        'Intended Audience :: Developers',
+        'Topic :: Scientific/Engineering',
+        'License :: OSI Approved :: GNU General Public License v3 (GPLv3)',
+        'Programming Language :: Python :: 2.7',
+    ],
+    keywords='data anomaly detection pandas timeseries',
+    packages=['pyculiarity'],
+    install_requires=['numpy', 'scipy', 'pandas', 'pytz',
+                      'statsmodels', 'rstl'],
+    extras_require={
+        'test': ['nose', 'mock']
+    }
+)
