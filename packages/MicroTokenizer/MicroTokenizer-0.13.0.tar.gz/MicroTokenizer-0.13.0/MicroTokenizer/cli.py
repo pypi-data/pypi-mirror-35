febbce9a8217c5d94a27535cefc51485ec21c36d
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-06 14:21:39 +0000

    Add package MicroTokenizer/0.13.0/MicroTokenizer-0.13.0.tar.gz

---
+++ packages/MicroTokenizer/MicroTokenizer-0.13.0.tar.gz/MicroTokenizer-0.13.0/MicroTokenizer/cli.py
@@ -0,0 +1,34 @@
+# -*- coding: utf-8 -*-
+
+"""Console script for MicroTokenizer."""
+import plac
+import sys
+from .cli_command import train
+
+
+def main(args=None):
+    commands = {
+        'train': train,
+    }
+    if len(sys.argv) == 1:
+        print("{title}: {content}, exits: {exits}".format(
+            content=', '.join(commands),
+            title="Available commands",
+            exits=1)
+        )
+
+    command = sys.argv.pop(1)
+    sys.argv[0] = 'spacy %s' % command
+
+    if command in commands:
+        plac.call(commands[command], sys.argv[1:])
+    else:
+        print("{title}: {content}, exits: {exits}".format(
+            content="Available: %s" % ', '.join(commands),
+            title="Unknown command: %s" % command,
+            exits=1)
+        )
+
+
+if __name__ == "__main__":
+    main()
