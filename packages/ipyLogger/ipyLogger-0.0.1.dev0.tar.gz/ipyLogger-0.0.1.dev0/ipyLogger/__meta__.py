b39df9ad568cdae3fd06385bf5d6bcee6baf6d8a
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-06 07:07:40 +0000

    Add package ipyLogger/0.0.1.dev0/ipyLogger-0.0.1.dev0.tar.gz

---
+++ packages/ipyLogger/ipyLogger-0.0.1.dev0.tar.gz/ipyLogger-0.0.1.dev0/ipyLogger/__meta__.py
@@ -0,0 +1,33 @@
+
+__name__ = 'ipyLogger'
+name_url = __name__.replace('_', '-')
+
+__packages__ = [__name__]
+__version__ = '0.0.1dev'
+__description__ = 'Efficient Logging in Jupyter Notebook'
+__long_description__ = 'See README'
+__author__ = 'echo5india'
+__author_email__ = 'elior.illouz@gmail.com'
+__url__ = 'https://sgithub.fr.world.socgen/eillouz032618/ipyLogger'
+
+
+__keywords__ = ['logger',
+                'logging',
+                'notebook',
+                'jupyter',]
+__license__ = 'MIT'
+__classifiers__= [
+    'Development Status :: 4 - Beta',
+    'Intended Audience :: Developers',
+     'License :: OSI Approved :: MIT License',
+    'Programming Language :: Python :: 3.5',
+    'Programming Language :: Python :: 3.6',
+]
+__include_package_data__ = True
+__package_data__ = {
+
+
+}
+__zip_safe__ = False
+__entry_points__ = {}
+
