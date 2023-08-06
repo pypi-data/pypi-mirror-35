d109d2a3f0e9f9fee2fc54425d127cd186fc513a
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-06 04:14:16 +0000

    Add package tokenizers-collection/0.1.2/tokenizers_collection-0.1.2.tar.gz

---
+++ packages/tokenizers-collection/tokenizers_collection-0.1.2.tar.gz/tokenizers_collection-0.1.2/setup.py
@@ -0,0 +1,54 @@
+#!/usr/bin/env python
+# -*- coding: utf-8 -*-
+
+"""The setup script."""
+from setuptools import setup, find_packages
+
+with open('README.rst') as readme_file:
+    readme = readme_file.read()
+
+with open('HISTORY.rst') as history_file:
+    history = history_file.read()
+
+requirements = ['Click>=6.0', 'requests', 'jieba', 'thulac', 'pynlpir', 'pyltp']
+
+setup_requirements = [ ]
+
+test_requirements = [ ]
+
+setup(
+    author="Xiaoquan Kong",
+    author_email='u1mail2me@gmail.com',
+    classifiers=[
+        'Development Status :: 2 - Pre-Alpha',
+        'Intended Audience :: Developers',
+        'License :: OSI Approved :: MIT License',
+        'Natural Language :: English',
+        "Programming Language :: Python :: 2",
+        'Programming Language :: Python :: 2.7',
+        'Programming Language :: Python :: 3',
+        'Programming Language :: Python :: 3.4',
+        'Programming Language :: Python :: 3.5',
+        'Programming Language :: Python :: 3.6',
+        'Programming Language :: Python :: 3.7',
+    ],
+    description="A simple iterator for using a set of Chinese tokenizer",
+    entry_points={
+        'console_scripts': [
+            'tokenizers_collection=tokenizers_collection.cli:main',
+        ],
+    },
+    install_requires=requirements,
+    license="MIT license",
+    long_description=readme + '\n\n' + history,
+    include_package_data=True,
+    keywords='tokenizers_collection',
+    name='tokenizers_collection',
+    packages=find_packages(include=['tokenizers_collection']),
+    setup_requires=setup_requirements,
+    test_suite='tests',
+    tests_require=test_requirements,
+    url='https://github.com/howl-anderson/tokenizers_collection',
+    version='0.1.2',
+    zip_safe=False,
+)
