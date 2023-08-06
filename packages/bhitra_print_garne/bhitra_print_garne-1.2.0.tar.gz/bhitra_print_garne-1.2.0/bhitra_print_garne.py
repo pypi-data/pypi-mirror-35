839aaf07d58f37ad36c2279a61f4f43f11e26476
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-06 07:10:41 +0000

    Add package bhitra_print_garne/1.2.0/bhitra_print_garne-1.2.0.tar.gz

---
+++ packages/bhitra_print_garne/bhitra_print_garne-1.2.0.tar.gz/bhitra_print_garne-1.2.0/bhitra_print_garne.py
@@ -0,0 +1,8 @@
+def print_bhitra(given_list,level=0):
+	for each_item in given_list:
+		if isinstance(each_item,list):
+			print_bhitra(each_item,level+1)
+		else:
+			for tab_stop in range(level):
+				print("\t", end = '')
+		print(each_item)
