42fbcd7b127a89f13636d038b4ba930c42add8e7
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-06 17:09:58 +0000

    Add package bayrell-file-system/0.0.1/bayrell-file-system-0.0.1.tar.gz

---
+++ packages/bayrell-file-system/bayrell-file-system-0.0.1.tar.gz/bayrell-file-system-0.0.1/BayrellFileSystem/__init__.py
@@ -0,0 +1,13 @@
+# -*- coding: utf-8 -*-
+
+
+
+__name__ = "bayrell-file-system"
+__version__ = "0.0.1"
+__description__ = "Bayrell runtime library for python"
+__license__ = "Apache License Version 2.0"
+__author__ = "Ildar Bikmamatov"
+__email__ = "ildar@bayrell.org"
+__copyright__ = "Copyright 2016-2018"
+__url__ = "https://github.com/bayrell/fs_py3"
+
---
+++ packages/bayrell-file-system/bayrell-file-system-0.0.1.tar.gz/bayrell-file-system-0.0.1/README.md
@@ -0,0 +1,5 @@
+# Description
+
+Bayrell File System Provider
+
+
