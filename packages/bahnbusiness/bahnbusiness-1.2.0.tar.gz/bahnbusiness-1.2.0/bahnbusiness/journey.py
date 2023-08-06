5e285224fd8b5f111f73867598fd56ff67ee98ed
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-06 07:07:47 +0000

    Add package bahnbusiness/1.2.0/bahnbusiness-1.2.0.tar.gz

---
+++ packages/bahnbusiness/bahnbusiness-1.2.0.tar.gz/bahnbusiness-1.2.0/bahnbusiness/journey.py
@@ -0,0 +1,26 @@
+import datetime
+
+
+class Journey:
+  """ Represents a single journey (e.g. A and B traveling from Berlin to Hamburg on 2018-01-15) """
+
+  @classmethod
+  def _parse_date(cls, date):
+    return datetime.datetime.strptime(date, "%d.%m.%Y").date()
+
+  def __init__(self, id, booking_date, name_booking, journey_date, name_travel, description, route, canceled, *args):
+    self.id = id
+    self.booking_date = self._parse_date(booking_date)
+    self.name_booking = name_booking
+    self.journey_date = self._parse_date(journey_date)
+    self.name_travel = name_travel
+    self.description = description
+    self.route = route
+    self.canceled = canceled
+
+  def __repr__(self):
+    return "<Journey id={id} name_booking='{name_booking}' route='{route}'>".format(
+      id=self.id,
+      name_booking=self.name_booking,
+      route=self.route,
+    )
