c5a8c3106d18ba97e55661d3fbf60bc83887cdec
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-06 01:55:09 +0000

    Add package torndb_for_python3/0.2.3/torndb_for_python3-0.2.3.tar.gz

---
+++ packages/torndb_for_python3/torndb_for_python3-0.2.3.tar.gz/torndb_for_python3-0.2.3/README.md
@@ -0,0 +1,5 @@
+
+# how to use this:
+
+from torndb_for_python3 import Connection
+It is used in the same way as torndb packets
---
+++ packages/torndb_for_python3/torndb_for_python3-0.2.3.tar.gz/torndb_for_python3-0.2.3/test/test_torndb_for_python3.py
@@ -0,0 +1,7 @@
+#coding=utf8
+"""
+@author:Administrator
+@file: test_torndb_for_python3.py
+@time: 2018/08/26
+"""
+import torndb_for_python3
