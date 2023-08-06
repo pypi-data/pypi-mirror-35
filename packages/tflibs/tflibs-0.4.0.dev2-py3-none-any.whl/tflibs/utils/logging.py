573551fed1bd8a218f5f7a466d96671ad3b501d4
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-06 14:25:18 +0000

    Add package tflibs/0.4.0.dev2/tflibs-0.4.0.dev2-py3-none-any.whl

---
+++ packages/tflibs/tflibs-0.4.0.dev2-py3-none-any.whl/tflibs/utils/logging.py
@@ -0,0 +1,12 @@
+""" Logging """
+from __future__ import absolute_import
+from __future__ import division
+from __future__ import print_function
+
+import tensorflow as tf
+
+
+def log_parse_args(parse_args, argument_class='Arguments'):
+    arg_dict = parse_args.__dict__
+    arg_str = reduce(lambda a, b: a + b, map(lambda (k, v): '{}: {}\n'.format(k, v), arg_dict.iteritems()), '')
+    tf.logging.info('{}: %s'.format(argument_class), arg_str)
