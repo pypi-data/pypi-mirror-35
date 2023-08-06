094f827085f83f24b1ffe9594e988578ccffe45b
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-06 13:54:37 +0000

    Add package caster-client/0.1.1/caster_client-0.1.1.tar.gz

---
+++ packages/caster-client/caster_client-0.1.1.tar.gz/caster_client-0.1.1/setup.py
@@ -0,0 +1,21 @@
+#!/usr/bin/env python
+# coding=utf-8
+
+from setuptools import setup, find_packages
+
+setup(
+    name = "caster_client",
+    version = "0.1.1",
+    keywords = ("pip", "Mock_Server", "Caster_Union", "caster"),
+    description = "client of Caster_Union",
+    long_description = "It's the client of Caster_Union, which is a MockServer framework.",
+    license = "Yitu Licence",
+
+    author = "zhenyu.yin",
+    author_email = "823243347@qq.com",
+
+    packages = find_packages(),
+    include_package_data = True,
+    platforms = "any",
+    install_requires = []
+)
