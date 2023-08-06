2583e885b553ac745b13a9f2b35387945a903576
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-06 11:51:46 +0000

    Add package starlette/0.2.2/starlette-0.2.2.tar.gz

---
+++ packages/starlette/starlette-0.2.2.tar.gz/starlette-0.2.2/starlette/__init__.py
@@ -0,0 +1,27 @@
+from starlette.app import App
+from starlette.response import (
+    FileResponse,
+    HTMLResponse,
+    JSONResponse,
+    RedirectResponse,
+    Response,
+    PlainTextResponse,
+    StreamingResponse,
+)
+from starlette.request import Request
+from starlette.testclient import TestClient
+
+
+__all__ = (
+    "App",
+    "FileResponse",
+    "HTMLResponse",
+    "JSONResponse",
+    "PlainTextResponse",
+    "RedirectResponse",
+    "Response",
+    "StreamingResponse",
+    "Request",
+    "TestClient",
+)
+__version__ = "0.2.2"
---
+++ packages/starlette/starlette-0.2.2.tar.gz/starlette-0.2.2/starlette/routing.py
@@ -0,0 +1,105 @@
+from starlette.response import Response
+from starlette.types import Scope, ASGIApp, ASGIInstance
+import re
+import typing
+
+
+class Route:
+    def matches(self, scope: Scope) -> typing.Tuple[bool, Scope]:
+        raise NotImplementedError()  # pragma: no cover
+
+    def __call__(self, scope: Scope) -> ASGIInstance:
+        raise NotImplementedError()  # pragma: no cover
+
+
+class Path(Route):
+    def __init__(
+        self,
+        path: str,
+        app: ASGIApp,
+        methods: typing.Sequence[str] = (),
+        protocol: str = None,
+    ) -> None:
+        self.path = path
+        self.app = app
+        self.protocol = protocol
+        self.methods = methods
+        regex = "^" + path + "$"
+        regex = re.sub("{([a-zA-Z_][a-zA-Z0-9_]*)}", r"(?P<\1>[^/]+)", regex)
+        self.path_regex = re.compile(regex)
+
+    def matches(self, scope: Scope) -> typing.Tuple[bool, Scope]:
+        if self.protocol is None or scope["type"] == self.protocol:
+            match = self.path_regex.match(scope["path"])
+            if match:
+                kwargs = dict(scope.get("kwargs", {}))
+                kwargs.update(match.groupdict())
+                child_scope = dict(scope)
+                child_scope["kwargs"] = kwargs
+                return True, child_scope
+        return False, {}
+
+    def __call__(self, scope: Scope) -> ASGIInstance:
+        if self.methods and scope["method"] not in self.methods:
+            return Response("Method not allowed", 405, media_type="text/plain")
+        return self.app(scope)
+
+
+class PathPrefix(Route):
+    def __init__(
+        self, path: str, app: ASGIApp, methods: typing.Sequence[str] = ()
+    ) -> None:
+        self.path = path
+        self.app = app
+        self.methods = methods
+        regex = "^" + path
+        regex = re.sub("{([a-zA-Z_][a-zA-Z0-9_]*)}", r"(?P<\1>[^/]*)", regex)
+        self.path_regex = re.compile(regex)
+
+    def matches(self, scope: Scope) -> typing.Tuple[bool, Scope]:
+        match = self.path_regex.match(scope["path"])
+        if match:
+            kwargs = dict(scope.get("kwargs", {}))
+            kwargs.update(match.groupdict())
+            child_scope = dict(scope)
+            child_scope["kwargs"] = kwargs
+            child_scope["root_path"] = scope.get("root_path", "") + match.string
+            child_scope["path"] = scope["path"][match.span()[1] :]
+            return True, child_scope
+        return False, {}
+
+    def __call__(self, scope: Scope) -> ASGIInstance:
+        if self.methods and scope["method"] not in self.methods:
+            return Response("Method not allowed", 405, media_type="text/plain")
+        return self.app(scope)
+
+
+class Router:
+    def __init__(self, routes: typing.List[Route], default: ASGIApp = None) -> None:
+        self.routes = routes
+        self.default = self.not_found if default is None else default
+
+    def __call__(self, scope: Scope) -> ASGIInstance:
+        for route in self.routes:
+            matched, child_scope = route.matches(scope)
+            if matched:
+                return route(child_scope)
+        return self.not_found(scope)
+
+    def not_found(self, scope: Scope) -> ASGIInstance:
+        if scope["type"] == "websocket":
+
+            async def close(receive, send):
+                await send({"type": "websocket.close"})
+
+            return close
+        return Response("Not found", 404, media_type="text/plain")
+
+
+class ProtocolRouter:
+    def __init__(self, protocols: typing.Dict[str, ASGIApp]):
+        self.protocols = protocols
+
+    def __call__(self, scope: Scope) -> ASGIInstance:
+        app = self.protocols[scope["type"]]
+        return app(scope)
