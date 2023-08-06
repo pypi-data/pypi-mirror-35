eaad87897f9dcd586991bded6debe5c11eee20eb
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-06 17:10:48 +0000

    Add package example-kanchan/0.1.0/example_kanchan-0.1.0.tar.gz

---
+++ packages/example-kanchan/example_kanchan-0.1.0.tar.gz/example_kanchan-0.1.0/setup.py
@@ -0,0 +1,14 @@
+import setuptools
+
+setuptools.setup(
+    name = 'example_kanchan',
+    version = '0.1.0',
+    description = 'A Python package example',
+    author = 'Kanchan',
+    author_email = 'kbartwal94@gmail.com',
+    url = 'https://github.com',
+    py_modules=['test'],
+    install_requires=[
+        # list of this package dependencies
+    ]
+)
