a6fff9af37537d0b79aa798746eeaee40e9d60cc
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-06 17:16:51 +0000

    Add package animals-math/0.0.7/animals_math-0.0.7-py3-none-any.whl

---
+++ packages/animals-math/animals_math-0.0.7-py3-none-any.whl/animals/animalMath.py
@@ -0,0 +1,10 @@
+# math for animals
+
+def number_of_giraffes(asia, africa):
+    total = africa + asia
+    return total
+
+
+def number_of_gazelles(gazelles, crocodiles):
+    total = gazelles - crocodiles
+    return total
