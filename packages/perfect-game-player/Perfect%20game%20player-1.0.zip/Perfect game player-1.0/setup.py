b422d9d0b69f4d4d4a46825ea59f3b89345c5f68
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-06 13:21:45 +0000

    Add package perfect-game-player/1.0/Perfect%20game%20player-1.0.zip

---
+++ packages/perfect-game-player/Perfect%20game%20player-1.0.zip/Perfect game player-1.0/setup.py
@@ -0,0 +1,12 @@
+from setuptools import setup
+
+setup(name='Perfect game player',
+      version='1.0',
+      description='Plays board games using Minimax algorithm',
+      url='https://github.com/dsaw/perfect-game-player',
+      author='Devesh Sawant',
+      author_email='devesh47cool@gmail.com',
+      license='GPLv3',
+      install_requires=[],
+      zip_safe=False
+)
