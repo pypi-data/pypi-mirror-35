3e61524c3c89b26ff739cae3111a406c9dcea071
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-06 15:33:39 +0000

    Add package scalable-cuckoo-filter/1.1/scalable_cuckoo_filter-1.1-py2.py3-none-any.whl

---
+++ packages/scalable-cuckoo-filter/scalable_cuckoo_filter-1.1-py2.py3-none-any.whl/cuckoo/exception.py
@@ -0,0 +1,17 @@
+'''
+Cuckoo filter internal exception.
+'''
+
+
+class CapacityException(Exception):
+    '''
+    Raise when a filter reaches its capacity.
+    '''
+    pass
+
+
+class InconsistencyException(Exception):
+    '''
+    Raise when a filter becomes inconsistent.  All bets are off.
+    '''
+    pass
