1709cc2aac606cc545df1f42a60f6fccf6d673ab
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-06 11:51:42 +0000

    Add package mippy/2.0.9/mippy-2.0.9.tar.gz

---
+++ packages/mippy/mippy-2.0.9.tar.gz/mippy-2.0.9/mippy/mdicom/siemens.py
@@ -0,0 +1,26 @@
+import pydicom
+from nibabel.nicom import csareader as csar
+
+def get_ascii_header(ds):
+        """
+        Expects a pydicom dataset.
+        Uses nibabel's "nicom" module to extract CSA data
+        """
+        csa = csar.get_csa_header(ds,'series')
+        
+        return csa['tags']['MrPhoenixProtocol']['items'][0]
+
+def get_ascconv(ds):
+        ascii = get_ascii_header(ds)
+        start_string = '### ASCCONV BEGIN'
+        end_string = 'ASCCONV END ###'
+        ascconv = ascii[ascii.find(start_string):ascii.find(end_string)+len(end_string)].split('\n')
+        asc_split = []
+        for i in range(len(ascconv)-1):
+                if i == 0:
+                        continue
+                asc_split.append(ascconv[i].replace(' ','').replace('\t','').split('='))
+        #~ for i in range(10):
+                #~ print asc_split[i]
+        return asc_split
+        
