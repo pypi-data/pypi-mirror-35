4893f21148886a761a36870021be8f718caae30a
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-06 14:57:30 +0000

    Add package quarter/0.0.2/quarter-0.0.2.tar.gz

---
+++ packages/quarter/quarter-0.0.2.tar.gz/quarter-0.0.2/setup.py
@@ -0,0 +1,21 @@
+import setuptools
+
+with open("README.md", "r") as fh:
+    long_description = fh.read()
+
+setuptools.setup(
+    name="quarter",
+    version="0.0.2",
+    author="Patrick Fant",
+    author_email="patrick.fant@recordedfuture.com",
+    description="datetime-like package to work with financial quarters.",
+    long_description=long_description,
+    long_description_content_type="text/markdown",
+    url="https://github.com/PatrickFant/quarter",
+    packages=setuptools.find_packages(),
+    classifiers=[
+        "Programming Language :: Python :: 3",
+        "License :: OSI Approved :: MIT License",
+        "Operating System :: OS Independent",
+    ],
+)
