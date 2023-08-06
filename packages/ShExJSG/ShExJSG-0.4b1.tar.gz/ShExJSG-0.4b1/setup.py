c34710b3d93e2377641e9a2eca502f743f877301
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-06 15:30:47 +0000

    Add package ShExJSG/0.4b1/ShExJSG-0.4b1.tar.gz

---
+++ packages/ShExJSG/ShExJSG-0.4b1.tar.gz/ShExJSG-0.4b1/setup.py
@@ -0,0 +1,28 @@
+import sys
+try:
+    from setuptools import setup
+except ImportError:
+    from distutils.core import setup
+
+if sys.version_info < (3, 6):
+    print("This module requires python 3.6 or later")
+    sys.exit(1)
+
+setup(
+    name='ShExJSG',
+    version='0.4b1',
+    packages=['ShExJSG'],
+    url="http://github.com/hsolbrig/ShExJSG",
+    license='Apache 2.0',
+    author='Harold Solbrig',
+    author_email='solbrig@solbrig-informatics.com',
+    description='"ShExJSG - Astract Syntax Tree for the ShEx 2.0 language',
+    install_requires=['PyJSG==0.8b2'],
+    tests_require = ['yadict-compare', 'requests', 'PyShExC>=0.4.0', 'jsonasobj'],
+    classifiers=[
+        'Development Status :: 3 - Alpha',
+        'Environment :: Console',
+        'Intended Audience :: Developers',
+        'Topic :: Software Development :: Compilers',
+        'Programming Language :: Python :: 3.6']
+)
