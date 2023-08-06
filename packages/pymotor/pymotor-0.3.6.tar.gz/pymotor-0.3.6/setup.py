ee538a1909545bd9938ef6c0594dfa4437cedfdf
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-06 13:53:22 +0000

    Add package pymotor/0.3.6/pymotor-0.3.6.tar.gz

---
+++ packages/pymotor/pymotor-0.3.6.tar.gz/pymotor-0.3.6/setup.py
@@ -0,0 +1,30 @@
+
+import setuptools
+
+with open("README.md", "r") as fh:
+    long_description = fh.read()
+
+setuptools.setup(
+    name="pymotor",
+    version="0.3.6",
+    install_requires=[
+            'matplotlib',
+            'numpy',
+            'pandas',
+            'scipy',
+      ],
+    author="Randy Rubin",
+    author_email="randymrubin@gmail.com",
+    description="Generates motion, force and torque profiles for electric motor selection.",
+    long_description=long_description,
+    long_description_content_type="text/markdown",
+    url="https://github.com/rmrubin/pymotor",
+    packages=setuptools.find_packages(),
+    classifiers=(
+        "Development Status :: 3 - Alpha",
+        "Topic :: Scientific/Engineering :: Physics",
+        "Programming Language :: Python :: 3",
+        "License :: OSI Approved :: MIT License",
+        "Operating System :: OS Independent",
+    ),
+)
