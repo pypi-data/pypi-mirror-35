e12879a14af8e0e5f12038826539d981eb24b910
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-06 15:34:40 +0000

    Add package argparse-utils/1.1.0/argparse_utils-1.1.0-py3-none-any.whl

---
+++ packages/argparse-utils/argparse_utils-1.1.0-py3-none-any.whl/argparse_utils/__init__.py
@@ -0,0 +1,11 @@
+from argparse import ArgumentParser
+
+from argparse_utils.mapping import mapping_action
+
+__version__ = "1.1.0"
+
+__all__ = ["ArgumentParser", "mapping_action", "enum_action"]
+
+
+def enum_action(enum_class):
+    return mapping_action(enum_class.__members__)
