7df1f15735d93cd2e789591a0d6ece2f53695231
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-06 14:58:17 +0000

    Add package cassiopeia-championgg/1.0.6/cassiopeia-championgg-1.0.6.tar.gz

---
+++ packages/cassiopeia-championgg/cassiopeia-championgg-1.0.6.tar.gz/cassiopeia-championgg-1.0.6/cassiopeia_championgg/dto.py
@@ -0,0 +1,17 @@
+from cassiopeia.dto.common import DtoObject
+
+
+class ChampionGGStatsListDto(DtoObject):
+    pass
+
+
+class ChampionGGStatsDto(DtoObject):
+    pass
+
+
+class ChampionGGMatchupListDto(DtoObject):
+    pass
+
+
+class ChampionGGMatchupDto(DtoObject):
+    pass
