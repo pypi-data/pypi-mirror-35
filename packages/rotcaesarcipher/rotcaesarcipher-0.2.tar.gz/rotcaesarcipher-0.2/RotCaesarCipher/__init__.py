80b125da1280c019739b9885ebc2d336f87c39a0
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-06 16:17:51 +0000

    Add package rotcaesarcipher/0.2/rotcaesarcipher-0.2.tar.gz

---
+++ packages/rotcaesarcipher/rotcaesarcipher-0.2.tar.gz/rotcaesarcipher-0.2/RotCaesarCipher/__init__.py
@@ -0,0 +1,15 @@
+import sys
+from model import E,D
+
+#Crifra uma frase
+#salt = 1 a 25
+#frase = string ascii
+def encode(salt, frase):
+    e = [model.E(salt, p) for p in frase]
+    return str("".join(e)).upper()
+#Crifra uma frase
+#salt = 1 a 25
+#frase = string ascii
+def decode(salt, frase):
+    d = [model.D(salt, c) for c in frase]
+    return "".join(d)
