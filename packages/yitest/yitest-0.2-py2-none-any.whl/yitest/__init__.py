d3353a6e31ebc3e985cdd8965863494d81a87b65
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-06 04:14:44 +0000

    Add package yitest/0.2/yitest-0.2-py2-none-any.whl

---
+++ packages/yitest/yitest-0.2-py2-none-any.whl/yitest/__init__.py
@@ -0,0 +1,7 @@
+def joke():
+    return (u'Wenn ist das Nunst\u00fcck git und Slotermeyer? Ja! ... '
+            u'Beiherhund das Oder die Flipperwaldt gersput.')
+
+
+ def second_function():
+ 	print('second function is good to use.')
