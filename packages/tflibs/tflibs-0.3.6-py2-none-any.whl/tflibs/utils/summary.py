9fb927061991043ec54ad456cf9180aaa285b10f
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-06 01:54:32 +0000

    Add package tflibs/0.3.6/tflibs-0.3.6-py2-none-any.whl

---
+++ packages/tflibs/tflibs-0.3.6-py2-none-any.whl/tflibs/utils/summary.py
@@ -0,0 +1,23 @@
+""" Summary """
+
+import tensorflow as tf
+
+
+def strip_illegal_summary_name(name):
+    """
+    Strips illegal summary name
+    :param str name:
+    :return: Stripped name
+    """
+    return name.rstrip(':0')
+
+
+def histogram_trainable_vars():
+    trainable_vars = tf.trainable_variables()
+    for var in trainable_vars:
+        tf.summary.histogram(strip_illegal_summary_name(var.name), var, family='Trainable_Variables')
+
+
+def image_summary(name, image):
+    batch_size = image.shape.as_list()[0]
+    tf.summary.image(name, image, max_outputs=batch_size, family='Images')
