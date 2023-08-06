0b01b0a065378650f580ea1b84c78bf20e5a9afe
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-06 15:30:38 +0000

    Add package pytest-csv/1.1.2/pytest_csv-1.1.2-py2.py3-none-any.whl

---
+++ packages/pytest-csv/pytest_csv-1.1.2-py2.py3-none-any.whl/pytest_csv/_reporter.py
@@ -0,0 +1,84 @@
+# ----------------------------------------------------------------------
+# pytest-csv - https://github.com/nicoulaj/pytest-csv
+# copyright (c) 2018 pytest-csv contributors
+#
+# This program is free software: you can redistribute it and/or modify
+# it under the terms of the GNU General Public License as published by
+# the Free Software Foundation, either version 3 of the License, or
+# (at your option) any later version.
+#
+# This program is distributed in the hope that it will be useful,
+# but WITHOUT ANY WARRANTY; without even the implied warranty of
+# MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the
+# GNU General Public License for more details.
+#
+# You should have received a copy of the GNU General Public License
+# along with this program.  If not, see <http://www.gnu.org/licenses/>.
+# ----------------------------------------------------------------------
+
+import csv
+import os
+
+import pytest
+import six
+
+
+class CSVReporter(object):
+    def __init__(self,
+                 csv_path,
+                 columns,
+                 delimiter,
+                 quote_char):
+        self._csv_path = csv_path
+        self._columns = columns
+        self._delimiter = delimiter
+        self._quote_char = quote_char
+        self._rows = []
+
+    @pytest.mark.hookwrapper
+    def pytest_runtest_makereport(self, item, call):
+        outcome = yield
+        report = outcome.get_result()
+        report.test_doc = item.obj.__doc__ or ''
+        report.test_args = item.callspec.params if hasattr(item, 'callspec') else {}
+        if hasattr(item, 'iter_markers'):
+            report.test_markers = list(item.iter_markers())
+        else:  # pytest <3.8 backward compatibility
+            from _pytest.mark import MarkInfo
+            report.test_markers = [v for v in six.itervalues(item.keywords) if isinstance(v, MarkInfo)]
+
+    def pytest_runtest_logreport(self, report):
+        if report.when != 'call' and report.passed:
+            return
+        self._rows.append({column: dict(column(report)) if callable(column) else {key: str(column)}
+                           for key, column in six.iteritems(self._columns)})
+
+    def pytest_sessionfinish(self, session):
+        if not self._rows or hasattr(session.config, 'slaveinput'):
+            return
+
+        directory = os.path.dirname(self._csv_path)
+        if directory and not os.path.exists(directory):
+            os.makedirs(directory)
+
+        headers = {column: list(sorted(set(header
+                                           for row in self._rows
+                                           for header in six.iterkeys(row[column]))))
+                   for column in six.itervalues(self._columns)}
+
+        with open(self._csv_path, 'w') as out:
+            writer = csv.writer(out, delimiter=self._delimiter, quotechar=self._quote_char, quoting=csv.QUOTE_MINIMAL)
+
+            writer.writerow([header
+                             for column in six.itervalues(self._columns)
+                             for header in headers[column]])
+
+            for row in self._rows:
+                writer.writerow([row[column].get(header, '')
+                                 for column in six.itervalues(self._columns)
+                                 for header in headers[column]])
+
+        session.config.hook.pytest_csv_written(csv_path=self._csv_path)
+
+    def pytest_terminal_summary(self, terminalreporter):
+        terminalreporter.write_sep('-', 'CSV report: %s' % self._csv_path)
---
+++ packages/pytest-csv/pytest_csv-1.1.2-py2.py3-none-any.whl/pytest_csv/column/_builtin.py
@@ -0,0 +1,153 @@
+# ----------------------------------------------------------------------
+# pytest-csv - https://github.com/nicoulaj/pytest-csv
+# copyright (c) 2018 pytest-csv contributors
+#
+# This program is free software: you can redistribute it and/or modify
+# it under the terms of the GNU General Public License as published by
+# the Free Software Foundation, either version 3 of the License, or
+# (at your option) any later version.
+#
+# This program is distributed in the hope that it will be useful,
+# but WITHOUT ANY WARRANTY; without even the implied warranty of
+# MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the
+# GNU General Public License for more details.
+#
+# You should have received a copy of the GNU General Public License
+# along with this program.  If not, see <http://www.gnu.org/licenses/>.
+# ----------------------------------------------------------------------
+
+import datetime
+import os
+
+import six
+
+from ._ids import *
+from ._status import *
+from ._utils import parse_node_id, format_mark_info, format_mark_info_args
+
+
+def column_id(report):
+    yield ID, report.nodeid
+
+
+def column_module(report):
+    mod, cls, func, params = parse_node_id(report.nodeid)
+    yield MODULE, mod or ''
+
+
+def column_class(report):
+    mod, cls, func, params = parse_node_id(report.nodeid)
+    yield CLASS, cls or ''
+
+
+def column_function(report):
+    mod, cls, func, params = parse_node_id(report.nodeid)
+    yield FUNCTION, func or ''
+
+
+def column_name(report):
+    mod, cls, func, params = parse_node_id(report.nodeid)
+    yield NAME, '%s[%s]' % (func, params) if params else func
+
+
+def column_file(report):
+    yield FILE, report.location[0]
+
+
+def column_doc(report):
+    yield DOC, getattr(report, 'test_doc', '').strip()
+
+
+def column_status(report):
+    if report.passed:
+        yield STATUS, XPASSED if hasattr(report, 'wasxfail') else PASSED
+    elif report.failed:
+        yield STATUS, XFAILED if hasattr(report, 'wasxfail') else ERROR if report.when != 'call' else FAILED
+    elif report.skipped:
+        yield STATUS, XFAILED if hasattr(report, 'wasxfail') else SKIPPED
+
+
+def column_success(report):
+    yield SUCCESS, str(not bool(report.failed))
+
+
+def column_message(report):
+    if report.passed:
+        if hasattr(report, 'wasxfail'):
+            yield MESSAGE, report.wasxfail
+        else:
+            yield MESSAGE, ''
+
+    elif report.failed:
+        if hasattr(report, 'wasxfail'):
+            yield MESSAGE, report.wasxfail
+        else:
+            if hasattr(report.longrepr, 'reprcrash'):
+                yield MESSAGE, report.longrepr.reprcrash.message
+            elif isinstance(report.longrepr, six.string_types):
+                yield MESSAGE, report.longrepr
+            else:
+                yield MESSAGE, str(report.longrepr)
+
+    elif report.skipped:
+        if hasattr(report, 'wasxfail'):
+            yield MESSAGE, report.wasxfail
+        else:
+            _, _, message = report.longrepr
+            yield MESSAGE, message
+
+
+def column_duration(report):
+    yield DURATION, str(getattr(report, 'duration', ''))
+
+
+def column_duration_formatted(report):
+    yield DURATION_FORMATTED, str(datetime.timedelta(seconds=getattr(report, 'duration', 0)))
+
+
+def column_markers(report):
+    yield MARKERS, ','.join(format_mark_info(mark, False)
+                            for mark in sorted(getattr(report, 'test_markers', []), key=lambda mark: mark.name))
+
+
+def column_markers_with_args(report):
+    yield MARKERS, ','.join(format_mark_info(mark, True)
+                            for mark in sorted(getattr(report, 'test_markers', []), key=lambda mark: mark.name))
+
+
+def column_markers_as_columns(report):
+    for mark in getattr(report, 'test_markers', []):
+        yield mark.name, format_mark_info_args(mark) or str(True)
+
+
+def column_markers_args_as_columns(report):
+    for mark in getattr(report, 'test_markers', []):
+        if not mark.args and not mark.kwargs:
+            yield mark.name, str(True)
+        else:
+            for i, arg in enumerate(mark.args):
+                yield '%s.%i' % (mark.name, i), str(arg)
+            for name, value in six.iteritems(mark.kwargs):
+                yield '%s.%s' % (mark.name, name), str(value)
+
+
+def column_parameters(report):
+    yield PARAMETERS, ','.join('%s=%s' % (k, v) for k, v in sorted(six.iteritems(getattr(report, 'test_args', {}))))
+
+
+def column_parameters_as_columns(report):
+    for name, value in six.iteritems(getattr(report, 'test_args', {})):
+        yield name, str(value)
+
+
+def column_properties(report):
+    yield PROPERTIES, ','.join('%s=%s' % (k, v) for k, v in sorted(getattr(report, 'user_properties', [])))
+
+
+def column_properties_as_columns(report):
+    for name, value in sorted(getattr(report, 'user_properties', [])):
+        yield name, str(value)
+
+
+def WorkingDirectoryColumn(report):
+    yield WORKING_DIRECTORY, os.getcwd()
