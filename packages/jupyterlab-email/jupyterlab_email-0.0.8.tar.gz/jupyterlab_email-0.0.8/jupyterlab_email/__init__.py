5b74c9a3f8da07d221d0aa26b31f463c53c3921a
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-06 14:27:12 +0000

    Add package jupyterlab-email/0.0.8/jupyterlab_email-0.0.8.tar.gz

---
+++ packages/jupyterlab-email/jupyterlab_email-0.0.8.tar.gz/jupyterlab_email-0.0.8/jupyterlab_email/__init__.py
@@ -0,0 +1,11 @@
+__version__ = '0.0.8'
+
+from .nbconvert import run as run_nbconvert
+from ._email import email as email_notebook
+from .attachments import attach
+
+
+def _jupyter_server_extension_paths():
+    return [{
+        "module": "jupyterlab_email.extension"
+    }]
