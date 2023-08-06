8853ef92aa2a6219d4b2f5b98e553eb5701d8222
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-06 15:34:02 +0000

    Add package jcaptcha/1.0.1/jcaptcha-1.0.1.tar.gz

---
+++ packages/jcaptcha/jcaptcha-1.0.1.tar.gz/jcaptcha-1.0.1/setup.py
@@ -0,0 +1,25 @@
+#!/usr/bin/env python
+# encoding: utf-8
+
+from setuptools import setup, find_packages, Extension
+
+
+setup(
+    name='jcaptcha',
+      version='1.0.1',
+      description='验证码生成',
+      url='http://github.com/xiaofeng283',
+      author='Jervon',
+      author_email='xiaofeng283@gmail.com',
+      license='MIT',
+      packages = ['jcaptcha'],
+      include_package_data = True,
+      package_dir={'jcaptcha': 'jcaptcha',},
+      zip_safe=False,
+      install_requires = [
+        "captcha",
+        "numpy",
+        "matplotlib",
+        "Pillow",
+        ]
+)
