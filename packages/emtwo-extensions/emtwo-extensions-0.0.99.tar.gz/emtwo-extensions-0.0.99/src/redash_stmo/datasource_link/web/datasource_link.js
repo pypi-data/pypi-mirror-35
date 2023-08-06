b8d559101f6c63941df777647b63389b6354f39d
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-06 14:53:54 +0000

    Add package emtwo-extensions/0.0.99/emtwo-extensions-0.0.99.tar.gz

---
+++ packages/emtwo-extensions/emtwo-extensions-0.0.99.tar.gz/emtwo-extensions-0.0.99/src/redash_stmo/datasource_link/web/datasource_link.js
@@ -0,0 +1,43 @@
+import React from 'react';
+import PropTypes from 'prop-types';
+import { react2angular } from 'react2angular';
+
+class DatasourceLink extends React.Component {
+  constructor(props) {
+    super(props);
+    this.state = {
+      type_name: "",
+      doc_url: "",
+    };
+  }
+
+  componentDidMount() {
+    const query = this.props.$route.current.locals.query;
+    fetch(`${this.props.clientConfig.basePath}api/data_sources/${query.data_source_id}/link`)
+      .then(response => { 
+        if (response.status === 200) {
+          response.json();
+        }
+        return {};
+      })
+      .then(json => {
+        const {type_name, doc_url} = json.message;
+        this.setState({type_name, doc_url});
+      });
+  }
+
+  render() {
+    if (!this.state.doc_url) {
+      return null;
+    }
+    return (
+      <span id="datasource-link">
+          <a href={this.state.doc_url}> {this.state.type_name} documentation</a>
+      </span>
+    ); 
+  }
+}
+
+export default function init(ngModule) {
+    ngModule.component('datasourceLink', react2angular(DatasourceLink, [], ['$route', 'clientConfig']));
+}
---
+++ packages/emtwo-extensions/emtwo-extensions-0.0.99.tar.gz/emtwo-extensions-0.0.99/tests/test_datasource_link.py
@@ -0,0 +1,63 @@
+import mock
+
+from tests import BaseTestCase
+from flask import Flask
+
+from redash.models import DataSource
+from redash.query_runner.pg import PostgreSQL
+from redash_stmo.datasource_link import datasource_link, BaseQueryRunner
+
+
+class TestDatasourceLink(BaseTestCase):
+    EXPECTED_DOC_URL = "www.example.com"
+    def setUp(self):
+        super(TestDatasourceLink, self).setUp()
+        self.patched_query_runners = self._setup_mock('redash_stmo.datasource_link.query_runners')
+        self.patched_query_runners.return_value = {}
+
+        app = Flask("test")
+        datasource_link(app)
+
+    def _setup_mock(self, function_to_patch):
+        patcher = mock.patch(function_to_patch)
+        patched_function = patcher.start()
+        self.addCleanup(patcher.stop)
+        return patched_function
+
+    '''
+    def test_updates_data_source(self):
+        admin = self.factory.create_admin()
+        new_name = 'New Name'
+        new_options = {"dbname": "newdb", "doc_url": self.EXPECTED_DOC_URL}
+        rv = self.make_request('post', self.path,
+                               data={
+                                    'name': new_name,
+                                    'type': 'pg',
+                                    'type_name': 'Postgres',
+                                    'options': new_options},
+                               user=admin)
+        self.assertEqual(rv.status_code, 200)
+        data_source = DataSource.query.get(self.factory.data_source.id)
+        self.assertEqual(data_source.name, new_name)
+        self.assertEqual(data_source.options.to_dict(), new_options)
+        self.assertEqual(rv.json['type_name'], data_source.query_runner.name())
+        self.assertEqual(rv.json['options']["doc_url"], self.EXPECTED_DOC_URL)
+
+    def test_creates_data_source(self):
+        admin = self.factory.create_admin()
+        rv = self.make_request('post', '/api/data_sources',
+                               data={'name': 'DS 1', 'type': 'pg',
+                                     'options': {"dbname": "redash", "doc_url": self.EXPECTED_DOC_URL},
+                                     'doc_url': None}, user=admin)
+        self.assertEqual(rv.status_code, 200)
+        data_source = DataSource.query.get(self.factory.data_source.id)
+        self.assertIsNotNone(DataSource.query.get(rv.json['id']))
+        self.assertEqual(rv.json['type_name'], data_source.query_runner.name())
+        self.assertEqual(rv.json['options']["doc_url"], self.EXPECTED_DOC_URL)
+    '''
+
+    def test_gets_datasource_link_and_type(self):
+        admin = self.factory.create_admin()
+        data_source = self.factory.create_data_source()
+        rv = self.make_request('get', '/api/data_sources/{}/link'.format(data_source.id), user=admin),
+        self.assertEqual(200, rv.status_code)
