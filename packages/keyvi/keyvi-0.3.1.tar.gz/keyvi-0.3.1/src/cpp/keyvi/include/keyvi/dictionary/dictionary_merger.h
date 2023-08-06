95bebac3c3537722aa670d5cadaa22b5b3eeb798
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-06 13:22:40 +0000

    Add package keyvi/0.3.1/keyvi-0.3.1.tar.gz

---
+++ packages/keyvi/keyvi-0.3.1.tar.gz/keyvi-0.3.1/src/cpp/keyvi/include/keyvi/dictionary/dictionary_merger.h
@@ -0,0 +1,315 @@
+/* * keyvi - A key value store.
+ *
+ * Copyright 2015, 2016 Hendrik Muhs<hendrik.muhs@gmail.com>
+ *
+ * Licensed under the Apache License, Version 2.0 (the "License");
+ * you may not use this file except in compliance with the License.
+ * You may obtain a copy of the License at
+ *
+ *   http://www.apache.org/licenses/LICENSE-2.0
+ *
+ * Unless required by applicable law or agreed to in writing, software
+ * distributed under the License is distributed on an "AS IS" BASIS,
+ * WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
+ * See the License for the specific language governing permissions and
+ * limitations under the License.
+ */
+
+/*
+ * dictionary_merger.h
+ *
+ *  Created on: Feb 27, 2016
+ *      Author: hendrik
+ */
+
+#ifndef KEYVI_DICTIONARY_DICTIONARY_MERGER_H_
+#define KEYVI_DICTIONARY_DICTIONARY_MERGER_H_
+
+#include <algorithm>
+#include <fstream>
+#include <functional>
+#include <memory>
+#include <queue>
+#include <string>
+#include <vector>
+
+#include <boost/filesystem.hpp>
+
+#include <msgpack.hpp>
+
+#include "dictionary/fsa/automata.h"
+#include "dictionary/fsa/entry_iterator.h"
+#include "dictionary/fsa/generator_adapter.h"
+#include "dictionary/fsa/internal/constants.h"
+#include "util/configuration.h"
+
+// #define ENABLE_TRACING
+#include "dictionary/util/trace.h"
+
+namespace keyvi {
+namespace dictionary {
+
+/**
+ * Exception class for dictionary merger
+ */
+
+struct merger_exception : public std::runtime_error {
+  using std::runtime_error::runtime_error;
+};
+
+struct MergeStats {
+  size_t number_of_keys_ = 0;
+  size_t deleted_keys_ = 0;
+  size_t updated_keys_ = 0;
+};
+
+template <class PersistenceT, class ValueStoreT = fsa::internal::NullValueStore>
+class DictionaryMerger final {
+  using GeneratorAdapter = fsa::GeneratorAdapterInterface<PersistenceT, ValueStoreT>;
+  using parameters_t = keyvi::util::parameters_t;
+
+ private:
+  class SegmentIterator {
+    using EntryIteratorPtr = std::shared_ptr<fsa::EntryIterator>;
+
+   public:
+    /**
+     *
+     * @param segment_index, merge segment index also used as a priority
+     * indicator
+     *                          when comparing two keys with the same value.
+     */
+    SegmentIterator(const fsa::EntryIterator& e, size_t segment_index)
+        : entry_iterator_ptr_(std::make_shared<fsa::EntryIterator>(e)), segment_index_(segment_index) {}
+
+    bool operator<(const SegmentIterator& rhs) const {
+      // very important difference in semantics: we have to ensure that in case
+      // of equal key,
+      // the iterator with the higher index (priority) is taken
+
+      if (segment_index_ < rhs.segment_index_) {
+        return entryIterator() > rhs.entryIterator();
+      }
+
+      return rhs.entryIterator() < entryIterator();
+    }
+
+    operator bool() const { return entryIterator() != endIterator(); }
+
+    SegmentIterator& operator++() {
+      ++(*entry_iterator_ptr_);
+      return *this;
+    }
+
+    const fsa::EntryIterator& entryIterator() const { return *entry_iterator_ptr_; }
+
+    const size_t segmentIndex() const { return segment_index_; }
+
+   private:
+    static const fsa::EntryIterator& endIterator() {
+      static fsa::EntryIterator end_it;
+      return end_it;
+    }
+
+   private:
+    EntryIteratorPtr entry_iterator_ptr_;
+    size_t segment_index_;
+  };
+
+ public:
+  /**
+   * Instantiate a dictionary merger.
+   *
+   * @params params merger parameters
+   */
+  explicit DictionaryMerger(const parameters_t& params = parameters_t())
+      : dicts_to_merge_(), params_(params), stats_() {
+    params_[TEMPORARY_PATH_KEY] = keyvi::util::mapGetTemporaryPath(params);
+
+    append_merge_ = MERGE_APPEND == keyvi::util::mapGet<std::string>(params_, MERGE_MODE, "");
+  }
+
+  void Add(const std::string& filename) {
+    if (std::count(inputFiles_.begin(), inputFiles_.end(), filename)) {
+      throw std::invalid_argument("File is added already: " + filename);
+    }
+
+    fsa::automata_t fsa;
+    if (append_merge_) {
+      fsa.reset(new fsa::Automata(filename, loading_strategy_types::lazy, false));
+    } else {
+      fsa.reset(new fsa::Automata(filename));
+    }
+
+    if (fsa->GetValueStoreType() != ValueStoreT::GetValueStoreType()) {
+      throw std::invalid_argument("Dictionaries must have the same type.");
+    }
+
+    // check whether dictionary is completely empty
+    const auto segment_iterator = SegmentIterator(fsa::EntryIterator(fsa), segments_pqueue_.size());
+    if (!segment_iterator) {
+      return;
+    }
+
+    // push back deleted keys (list might be empty)
+    deleted_keys_.push_back(TryLoadDeletedKeys(filename));
+
+    segments_pqueue_.push(segment_iterator);
+    inputFiles_.push_back(filename);
+    dicts_to_merge_.push_back(fsa);
+  }
+
+  /**
+   * Set a custom manifest to be embedded into the index file.
+   *
+   * @param manifest as JSON string
+   */
+  void SetManifestFromString(const std::string& manifest) { manifest_ = manifest; }
+
+  void Merge(const std::string& filename) {
+    Merge();
+    generator_->SetManifestFromString(manifest_);
+    generator_->WriteToFile(filename);
+  }
+
+  void Merge() {
+    size_t sparse_array_size_sum = 0;
+    for (auto fsa : dicts_to_merge_) {
+      sparse_array_size_sum += fsa->SparseArraySize();
+    }
+
+    ValueStoreT* value_store = append_merge_ ? new ValueStoreT(inputFiles_) : new ValueStoreT(params_);
+
+    generator_ = GeneratorAdapter::CreateGenerator(sparse_array_size_sum, params_, value_store);
+
+    std::string top_key;
+
+    while (!segments_pqueue_.empty()) {
+      auto segment_it = segments_pqueue_.top();
+      segments_pqueue_.pop();
+
+      top_key = segment_it.entryIterator().GetKey();
+
+      // check for same keys and merge only the most recent one
+      while (!segments_pqueue_.empty() && segments_pqueue_.top().entryIterator().operator==(top_key)) {
+        ++stats_.updated_keys_;
+        auto to_inc = segments_pqueue_.top();
+
+        segments_pqueue_.pop();
+        if (++to_inc) {
+          TRACE("push iterator");
+          segments_pqueue_.push(to_inc);
+        }
+      }
+
+      if (!deleted_keys_[segment_it.segmentIndex()].empty() &&
+          top_key == deleted_keys_[segment_it.segmentIndex()].back()) {
+        deleted_keys_[segment_it.segmentIndex()].pop_back();
+        ++stats_.deleted_keys_;
+        // check the other deleted_keys for duplicates
+        for (auto& deleted_keys : deleted_keys_) {
+          if (!deleted_keys.empty() && top_key == deleted_keys.back()) {
+            ++stats_.deleted_keys_;
+            deleted_keys.pop_back();
+          }
+        }
+
+      } else {
+        fsa::ValueHandle handle;
+        handle.no_minimization = false;
+
+        // get the weight value, for now simple: does not require access to the
+        // value store itself
+        handle.weight = value_store->GetMergeWeight(segment_it.entryIterator().GetValueId());
+
+        if (append_merge_) {
+          handle.value_idx =
+              value_store->GetMergeValueId(segment_it.segmentIndex(), segment_it.entryIterator().GetValueId());
+        } else {
+          handle.value_idx =
+              value_store->GetValue(segment_it.entryIterator().GetFsa()->GetValueStore()->GetValueStorePayload(),
+                                    segment_it.entryIterator().GetValueId(), &handle.no_minimization);
+        }
+
+        TRACE("Add key: %s", top_key.c_str());
+        ++stats_.number_of_keys_;
+        generator_->Add(std::move(top_key), handle);
+      }
+      if (++segment_it) {
+        segments_pqueue_.push(segment_it);
+      }
+    }
+
+    dicts_to_merge_.clear();
+
+    TRACE("finished iterating, do final compile.");
+
+    generator_->CloseFeeding();
+  }
+
+  void Write(std::ostream& stream) {
+    if (!generator_) {
+      throw merger_exception("not merged yet");
+    }
+
+    generator_->Write(stream);
+  }
+
+  void WriteToFile(const std::string& filename) {
+    if (!generator_) {
+      throw merger_exception("not merged yet");
+    }
+    generator_->WriteToFile(filename);
+  }
+
+  const MergeStats& GetStats() const { return stats_; }
+
+ private:
+  typename GeneratorAdapter::AdapterPtr generator_;
+  bool append_merge_ = false;
+  std::vector<fsa::automata_t> dicts_to_merge_;
+  std::vector<std::vector<std::string>> deleted_keys_;
+  std::vector<std::string> inputFiles_;
+  std::priority_queue<SegmentIterator> segments_pqueue_;
+  parameters_t params_;
+  std::string manifest_ = std::string();
+  MergeStats stats_;
+
+  /**
+   * Load a file with deleted keys if it exists
+   */
+  std::vector<std::string> TryLoadDeletedKeys(const std::string& filename) {
+    std::vector<std::string> deleted_keys;
+    boost::filesystem::path deleted_keys_file{filename};
+    deleted_keys_file += ".dk";
+
+    TRACE("check for deleted keys file: %s", deleted_keys_file.string().c_str());
+    std::ifstream deleted_keys_stream(deleted_keys_file.string(), std::ios::binary);
+
+    if (deleted_keys_stream.good()) {
+      TRACE("found deleted keys file");
+
+      {
+        // reads the buffer as 1 big chunk, could be improved
+        // msgpack v2.x provides a better interface (visitor)
+        std::stringstream buffer;
+        buffer << deleted_keys_stream.rdbuf();
+
+        msgpack::unpacked unpacked_object;
+        msgpack::unpack(unpacked_object, buffer.str().data(), buffer.str().size());
+
+        unpacked_object.get().convert(deleted_keys);
+      }
+
+      // sort in reverse order
+      std::sort(deleted_keys.begin(), deleted_keys.end(), std::greater<std::string>());
+    }
+
+    return deleted_keys;
+  }
+};
+
+} /* namespace dictionary */
+} /* namespace keyvi */
+
+#endif  // KEYVI_DICTIONARY_DICTIONARY_MERGER_H_
---
+++ packages/keyvi/keyvi-0.3.1.tar.gz/keyvi-0.3.1/src/cpp/keyvi/include/keyvi/dictionary/fsa/internal/ivalue_store.h
@@ -0,0 +1,180 @@
+/* * keyvi - A key value store.
+ *
+ * Copyright 2015 Hendrik Muhs<hendrik.muhs@gmail.com>
+ *
+ * Licensed under the Apache License, Version 2.0 (the "License");
+ * you may not use this file except in compliance with the License.
+ * You may obtain a copy of the License at
+ *
+ *   http://www.apache.org/licenses/LICENSE-2.0
+ *
+ * Unless required by applicable law or agreed to in writing, software
+ * distributed under the License is distributed on an "AS IS" BASIS,
+ * WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
+ * See the License for the specific language governing permissions and
+ * limitations under the License.
+ */
+
+/*
+ * ivalue_store.h
+ *
+ *  Created on: Jun 12, 2014
+ *      Author: hendrik
+ */
+
+#ifndef KEYVI_DICTIONARY_FSA_INTERNAL_IVALUE_STORE_H_
+#define KEYVI_DICTIONARY_FSA_INTERNAL_IVALUE_STORE_H_
+
+#include <map>
+#include <string>
+#include <vector>
+
+#include <boost/container/flat_map.hpp>
+#include <boost/filesystem.hpp>
+#include <boost/interprocess/file_mapping.hpp>
+#include <boost/interprocess/mapped_region.hpp>
+#include <boost/variant.hpp>
+
+#include "dictionary/dictionary_merger_fwd.h"
+#include "util/configuration.h"
+#include "util/json_value.h"
+
+namespace keyvi {
+namespace dictionary {
+namespace fsa {
+namespace internal {
+
+/**
+ * "Registry" for value stores, required to differ between the stores.
+ *
+ * Do not forget to add the new value store to ValueStoreFactory
+ */
+enum value_store_t {
+  NULL_VALUE_STORE = 1,               //!< NullValueStore
+  INT_VALUE_STORE = 2,                //!< IntValueStore
+  STRING_VALUE_STORE = 3,             //!< StringValueStore
+  JSON_VALUE_STORE_DEPRECATED = 4,    // !< JsonValueStoreDeprecated
+  JSON_VALUE_STORE = 5,               // !< JsonValueStore
+  INT_INNER_WEIGHTS_VALUE_STORE = 6,  // !< IntInnerWeightsValueStore
+};
+
+/* Writing value stores is based on template (duck-typing).
+ * Base class / Interface definition for writing to the value store.
+ *
+ * The following types/constants/methods are required:
+ *
+ * typedef {type} value_t;
+ * static const {type} no_value = 0;
+ *
+ * uint64_t GetValue(value_t value, bool* no_minimization)
+ *
+ * value_store_t GetValueStoreType
+ *
+ * void Write(std::ostream& stream)
+ */
+class IValueStoreWriter {
+ public:
+  IValueStoreWriter() : IValueStoreWriter(keyvi::util::parameters_t()) {}
+
+  /**
+   * Default constructor. Override if the value store implementation requires
+   * extra data.
+   *
+   * @param parameters a map of string parameter values. It is up to the
+   *                   value store what parameters it wants to use (if any).
+   */
+  explicit IValueStoreWriter(const keyvi::util::parameters_t& parameters) : parameters_(parameters) {}
+
+  /// TODO: workaround till ValueStore merger classes are separated from ValueStore writer
+  explicit IValueStoreWriter(const std::vector<std::string>&) : IValueStoreWriter(keyvi::util::parameters_t()) {}
+
+  virtual ~IValueStoreWriter() {}
+
+  uint64_t GetMergeValueId(size_t fileIndex, uint64_t oldIndex) { return 0; }
+
+  /**
+   * Get the weight for merging dictionaries.
+   *
+   * Note: for now only the fsa_value is given, in future this might change, so that also the payload is required.
+   *
+   * @param fsa_value
+   * @return weight to be used as inner weight
+   */
+  uint32_t GetMergeWeight(uint64_t fsa_value) { return 0; }
+
+ protected:
+  keyvi::util::parameters_t parameters_;
+};
+
+/**
+ * Base class / Interface definition for reading from the value store.
+ *
+ * Each ValueStore Reader implementation should inherit this interface.
+ *
+ * In addition
+ */
+class IValueStoreReader {
+ public:
+  typedef boost::container::flat_map<std::string, boost::variant<std::string, int, double, bool>> attributes_raw_t;
+  typedef std::shared_ptr<attributes_raw_t> attributes_t;
+
+  /**
+   * Default constructor. Override if the value store implementation requires extra data.
+   *
+   * @param stream The stream to read from
+   * @param file_mapping The file_mapping instance of the loader to use memory mapping
+   */
+  IValueStoreReader(std::istream& stream, boost::interprocess::file_mapping* file_mapping) {}
+
+  virtual ~IValueStoreReader() {}
+
+  virtual value_store_t GetValueStoreType() const = 0;
+
+  /**
+   * Generic value format.
+   *
+   * @param fsa_value numeric value
+   * @return The value in form of attributes
+   */
+  virtual attributes_t GetValueAsAttributeVector(uint64_t fsa_value) const = 0;
+
+  /**
+   * Get Value as string in raw format
+   *
+   * Note: The raw format is an implementation detail of keyvi, not an official binary interface.
+   * Value store implementers can override this method for performance reasons.
+   *
+   * @param fsa_value
+   * @return the value as string without any decompression
+   */
+  virtual std::string GetRawValueAsString(uint64_t fsa_value) const {
+    return keyvi::util::EncodeJsonValue(GetValueAsString(fsa_value));
+  }
+
+  /**
+   * Get Value as string (for dumping or communication)
+   *
+   * @param fsa_value
+   * @return the value as string
+   */
+  virtual std::string GetValueAsString(uint64_t fsa_value) const = 0;
+
+  /**
+   * Get statistical information about the storage.
+   */
+
+  virtual std::string GetStatistics() const { return ""; }
+
+ private:
+  template <typename, typename>
+  friend class ::keyvi::dictionary::DictionaryMerger;
+
+  virtual const char* GetValueStorePayload() const { return 0; }
+};
+
+} /* namespace internal */
+} /* namespace fsa */
+} /* namespace dictionary */
+} /* namespace keyvi */
+
+#endif  // KEYVI_DICTIONARY_FSA_INTERNAL_IVALUE_STORE_H_
