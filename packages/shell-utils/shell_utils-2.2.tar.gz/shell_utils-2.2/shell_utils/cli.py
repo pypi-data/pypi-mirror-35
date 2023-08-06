19ef47aae598d56d00bbf9ed588b90eee43ecc2b
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-06 14:25:02 +0000

    Add package shell-utils/2.2/shell_utils-2.2.tar.gz

---
+++ packages/shell-utils/shell_utils-2.2.tar.gz/shell_utils-2.2/shell_utils/cli.py
@@ -0,0 +1,55 @@
+"""
+Usage:  [OPTIONS] COMMAND [ARGS]...
+
+  A cli for shell-utils.
+
+Options:
+  --help  Show this message and exit.
+"""
+import sys
+import subprocess as sp
+
+from pathlib import Path
+
+from shell_utils import shell
+
+import click
+
+
+@click.group()
+def cli():
+    """A cli for shell_utils."""
+    pass
+
+
+@cli.command()
+def generate_runner():
+    """Generate a run.py script in the current directory."""
+    from shell_utils import runner
+
+    runner_path = Path('run.py')
+
+    if runner_path.exists():
+        raise EnvironmentError('run.py already exists in current directory')
+
+    click.secho('writing content to run.py', fg='yellow')
+
+    runner_path.write_text(Path(runner.__file__).read_text())
+
+    shell('chmod +x run.py')
+
+
+def invoke_runner():
+    """
+    This invokes the run.py script in the current directory.
+    """
+    if not Path(Path(), 'run.py').exists():
+        raise SystemExit(
+            click.secho('run.py not found in current directory', fg='red')
+        )
+
+    sp.run(['python', 'run.py'] + sys.argv[1:])
+
+
+if __name__ == "__main__":
+    cli()
