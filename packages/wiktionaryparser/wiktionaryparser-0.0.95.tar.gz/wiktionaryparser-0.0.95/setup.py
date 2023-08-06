bda45d9790349d24053d8ba36406411dba969343
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-06 11:49:35 +0000

    Add package wiktionaryparser/0.0.95/wiktionaryparser-0.0.95.tar.gz

---
+++ packages/wiktionaryparser/wiktionaryparser-0.0.95.tar.gz/wiktionaryparser-0.0.95/setup.py
@@ -0,0 +1,23 @@
+from setuptools import setup,find_packages
+
+with open('wiktionaryparser/readme.md', 'r') as readme:
+  long_desc = readme.read()
+
+setup(
+  name = 'wiktionaryparser',
+  version = '0.0.95',
+  description = 'A tool to parse word data from wiktionary.com into a JSON object',
+  long_description = long_desc,
+  long_description_content_type='text/markdown',
+  packages = ['wiktionaryparser', 'wiktionaryparser.tests', 'wiktionaryparser.utils'],
+  data_files=[('testOutput', ['wiktionaryparser/tests/testOutput.json'])],
+  author = 'Suyash Behera',
+  author_email = 'sne9x@outlook.com',
+  url = 'https://github.com/Suyash458/WiktionaryParser', 
+  download_url = 'https://github.com/Suyash458/WiktionaryParser/archive/master.zip', 
+  keywords = ['Parser', 'Wiktionary'],
+  install_requires = ['beautifulsoup4','requests'],
+  classifiers=[
+   'Development Status :: 5 - Production/Stable',
+  ],
+)
