4f93f8149d4b17eafbdc3eb2eaef3554d78d24e9
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-06 07:11:12 +0000

    Add package felsimetl/0.1.22/felsimetl-0.1.22.tar.gz

---
+++ packages/felsimetl/felsimetl-0.1.22.tar.gz/felsimetl-0.1.22/felsim/config/getactualflowstableconfig.py
@@ -0,0 +1,28 @@
+def get_actual_flows_table_config():
+    return (
+        (
+            ("A", "Semana"),
+            ("B", "Año"),
+            ("C", "Flujo"),
+            ("D", "Flexibilidad"),
+            ("E", "Tipo"),
+            ("F", "Rubro"),
+            ("G", "Fecha"),
+            ("H", "Cuenta"),
+            ("I", "Detalle"),
+            ("J", "Ingreso"),
+            ("K", "Egreso"),
+        ), (
+            ("A", "week"),
+            ("B", "year"),
+            ("C", "flow"),
+            ("D", "flexibility"),
+            ("E", "type"),
+            ("F", "category"),
+            ("G", "date"),
+            ("H", "account"),
+            ("I", "details"),
+            ("J", "income"),
+            ("K", "expense")
+        )
+    )
