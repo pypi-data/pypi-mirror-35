91f3609060e92aac6b92519d66777c773041acf7
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-06 01:56:20 +0000

    Add package recursive-decorator/1.0.7/recursive_decorator-1.0.7.tar.gz

---
+++ packages/recursive-decorator/recursive_decorator-1.0.7.tar.gz/recursive_decorator-1.0.7/setup.py
@@ -0,0 +1,40 @@
+"""Setup file for handling packaging and distribution."""
+from setuptools import setup
+
+__version__ = "1.0.7"
+
+setup(
+    name="recursive_decorator",
+    version=__version__,
+    long_description=open("README.rst").read(),
+    description="Decorator to apply given decorator recursively on functions",
+    license="MIT",
+    author="yakobu",
+    author_email="ronenya4321@gmail.com",
+    url="https://github.com/ronen-y/recursive_decorator",
+    keywords="decorator recursive recursive_decorator recursive-decorator",
+    install_requires=["codetransformer", "cached-property"],
+    packages=["recursive_decorator"],
+    extras_require={
+        'dev': [
+            'mock',
+            'flake8',
+            'pytest',
+            'pytest-cov',
+        ],
+    },
+    package_data={'': ['*.xls', '*.xsd', '*.json',
+                       '*.css', '*.xml', '*.rst']},
+    classifiers=[
+        'Development Status :: 3 - Alpha',
+        'Natural Language :: English',
+        'Programming Language :: Python :: 3.4',
+        'Programming Language :: Python :: 3.5',
+        'Programming Language :: Python :: 3.6',
+        'Programming Language :: Python :: 3 :: Only',
+        'Programming Language :: Python :: Implementation :: CPython',
+        'Operating System :: POSIX',
+        'Topic :: Software Development :: Pre-processors',
+    ],
+    zip_safe=False
+)
