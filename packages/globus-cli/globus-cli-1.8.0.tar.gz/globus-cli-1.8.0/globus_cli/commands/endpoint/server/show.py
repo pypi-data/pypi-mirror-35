30ff2a497bc83a1de1846676ae04599d45f3ea81
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-06 07:11:27 +0000

    Add package globus-cli/1.8.0/globus-cli-1.8.0.tar.gz

---
+++ packages/globus-cli/globus-cli-1.8.0.tar.gz/globus-cli-1.8.0/globus_cli/commands/endpoint/server/show.py
@@ -0,0 +1,41 @@
+import click
+
+from globus_cli.parsing import common_options, endpoint_id_arg, server_id_arg
+from globus_cli.safeio import formatted_print, FORMAT_TEXT_RECORD
+
+from globus_cli.services.transfer import get_client
+
+
+@click.command('show', help='Show a server belonging to an endpoint')
+@common_options
+@endpoint_id_arg
+@server_id_arg
+def server_show(endpoint_id, server_id):
+    """
+    Executor for `globus endpoint server show`
+    """
+    client = get_client()
+
+    server_doc = client.get_endpoint_server(endpoint_id, server_id)
+    if not server_doc['uri']:  # GCP endpoint server
+        fields = (('ID', 'id'), ('Is Connected', 'is_connected'),
+                  ('Is Paused (macOS only)', 'is_paused'))
+    else:
+        def advertised_port_summary(server):
+            def get_range_summary(start, end):
+                return ('unspecified' if not start and not end
+                        else 'unrestricted' if start == 1024 and end == 65535
+                        else '{}-{}'.format(start, end))
+
+            return "incoming {}, outgoing {}".format(
+                       get_range_summary(server['incoming_data_port_start'],
+                                         server['incoming_data_port_end']),
+                       get_range_summary(server['outgoing_data_port_start'],
+                                         server['outgoing_data_port_end']),
+                   )
+
+        fields = (('ID', 'id'), ('URI', 'uri'), ('Subject', 'subject'),
+                  ('Data Ports', advertised_port_summary))
+
+    formatted_print(server_doc, text_format=FORMAT_TEXT_RECORD,
+                    fields=fields)
---
+++ packages/globus-cli/globus-cli-1.8.0.tar.gz/globus-cli-1.8.0/globus_cli/commands/endpoint/show.py
@@ -0,0 +1,39 @@
+import click
+
+from globus_cli.parsing import common_options, endpoint_id_arg
+from globus_cli.safeio import formatted_print, FORMAT_TEXT_RECORD
+
+from globus_cli.services.transfer import get_client
+
+
+@click.command('show', help='Display a detailed endpoint definition')
+@common_options
+@endpoint_id_arg
+def endpoint_show(endpoint_id):
+    """
+    Executor for `globus endpoint show`
+    """
+    client = get_client()
+
+    res = client.get_endpoint(endpoint_id)
+
+    def _managed_endpoint(x):
+        """ Helper for converting subscription_id into managed_endpoint """
+        return bool(x["subscription_id"])
+    formatted_print(
+        res, text_format=FORMAT_TEXT_RECORD,
+        fields=(("Display Name", "display_name"), ("ID", "id"),
+                ("Owner", "owner_string"), ("Activated", "activated"),
+                ("Shareable", "shareable"), ("Department", "department"),
+                ("Keywords", "keywords"), ("Endpoint Info Link", "info_link"),
+                ("Contact E-mail", "contact_email"),
+                ("Organization", "organization"), ("Department", "department"),
+                ("Other Contact Info", "contact_info"),
+                ("Visibility", "public"),
+                ("Default Directory", "default_directory"),
+                ("Force Encryption", "force_encryption"),
+                ("Managed Endpoint", _managed_endpoint),
+                ("Subscription ID", "subscription_id"),
+                ("Legacy Name", "canonical_name"),
+                ("Local User Info Available", "local_user_info_available"))
+        )
