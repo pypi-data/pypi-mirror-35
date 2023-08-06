a9de7fa491a74a6de5da3a0c81d78ae3853f11ef
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-06 17:15:30 +0000

    Add package stdclasses/0.0.1/stdclasses-0.0.1.tar.gz

---
+++ packages/stdclasses/stdclasses-0.0.1.tar.gz/stdclasses-0.0.1/setup.py
@@ -0,0 +1,19 @@
+from setuptools import setup
+
+setup(
+    name='stdclasses',
+    description='Ia to help',
+    long_description='Ia to help students',
+    version='0.0.1',
+    url='https://github.com/guimartino/stdclasses',
+    author='Guilherme Martino',
+    author_email='gui.martino@hotmail.com',
+    license='MIT',
+    classifiers=[
+        'Development Status :: 4 - Beta',
+        'Intended Audience :: System Administrators',
+        'License :: OSI Approved :: Apache Software License',
+        'Programming Language :: Python :: 3'
+    ],
+    packages=['stdclasses']
+)
