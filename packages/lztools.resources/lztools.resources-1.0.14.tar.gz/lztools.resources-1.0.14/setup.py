8a6cdfc19d32d28c75374d9a0d95d286431bf2e6
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-06 15:31:53 +0000

    Add package lztools.resources/1.0.14/lztools.resources-1.0.14.tar.gz

---
+++ packages/lztools.resources/lztools.resources-1.0.14.tar.gz/lztools.resources-1.0.14/setup.py
@@ -0,0 +1,52 @@
+#!/usr/bin/env python3.7
+from setuptools import setup
+from subprocess import call
+from setup_requires import apt_requires, pip_requires
+call(["sudo", "apt-get", "-qq", "-y", "install", *apt_requires])
+
+# try:
+#     codecs.lookup('mbcs')
+# except LookupError:
+#     ascii = codecs.lookup('ascii')
+#     func = lambda name, enc=ascii: {True: enc}.get(name=='mbcs')
+#     codecs.register(func)
+
+setup(
+    name='lztools.resources',
+    author='Laz aka Zanzes',
+    author_email='ubuntuea@gmail.com',
+    version='1.0.14',
+    license='MIT License',
+    description='Template',
+    url='',
+    entry_points={
+        'console_scripts': [
+            'lvault              = cli.lvault:main',
+            '__lztools_resources = cli.__lztools_resources:main'
+        ],
+    },
+    install_requires=pip_requires,
+    packages=['lztools', 'lztools.shared_resources', 'cli'],
+    zip_safe=False,
+    include_package_data=True,
+    package_data={'lztools': ['resources/*']},
+    classifiers=[  # Optional
+        # How mature is this project? Common values are
+        #   3 - Alpha
+        #   4 - Beta
+        #   5 - Production/Stable
+        'Development Status :: 3 - Alpha',
+
+        # Indicate who your project is intended for
+        'Intended Audience :: Developers',
+        'Topic :: System :: Systems Administration',
+
+        # Pick your license as you wish
+        'License :: OSI Approved :: MIT License',
+
+        # Specify the Python versions you support here. In particular, ensure
+        # that you indicate whether you support Python 2, Python 3 or both.
+        'Programming Language :: Python :: 3.7',
+    ],
+)
+
