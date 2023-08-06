5da68024472cb37ccfa2286a6905519551a8a05d
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-06 13:21:57 +0000

    Add package django-telegram-blog/0.4.0/django-telegram-blog-0.4.0.tar.gz

---
+++ packages/django-telegram-blog/django-telegram-blog-0.4.0.tar.gz/django-telegram-blog-0.4.0/telegram_blog/management/commands/telegramblogsetwebhook.py
@@ -0,0 +1,26 @@
+from django.core.management.base import BaseCommand
+from telegram_blog.utils import request, get_webhook_url
+from django.conf import settings
+
+
+class Command(BaseCommand):
+    help = 'Set Telegram webhook'
+
+    def handle(self, *args, **options):
+
+        if not getattr(settings, 'TELEGRAM_BLOG_USE_WEBHOOK'):
+            self.stdout.write('warning: setting webhook while TELEGRAM_BLOG_USE_WEBHOOK is False')
+        max_connections = getattr(settings, 'TELEGRAM_BLOG_WEBHOOK_MAX_CONNECTIONS', 40)
+        webhook_url = get_webhook_url()
+        data = request('getWebhookInfo').get('result')
+        self.stdout.write(f'existing webhook: {data}')
+        if data['url'] != webhook_url or data['max_connections'] != max_connections:
+            self.stdout.write(f'setting webhook with url {webhook_url} and max connections: {max_connections}')
+            response = request('setWebhook', {
+                'url': webhook_url,
+                'max_connections': max_connections
+            })
+            self.stdout.write(f'setWebhook response: {response}')
+
+
+
