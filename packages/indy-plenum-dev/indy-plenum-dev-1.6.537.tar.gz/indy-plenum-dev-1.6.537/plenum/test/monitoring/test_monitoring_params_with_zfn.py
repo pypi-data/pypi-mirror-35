23b1dd67285dd81ef5c8bb39031beec9e9729da8
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-06 14:37:47 +0000

    Add package indy-plenum-dev/1.6.537/indy-plenum-dev-1.6.537.tar.gz

---
+++ packages/indy-plenum-dev/indy-plenum-dev-1.6.537.tar.gz/indy-plenum-dev-1.6.537/plenum/test/monitoring/test_monitoring_params_with_zfn.py
@@ -0,0 +1,46 @@
+from statistics import median_high
+
+import pytest
+
+from plenum.common.measurements import RevivalSpikeResistantEMAThroughputMeasurement
+from plenum.test.helper import get_key_from_req
+
+nodeCount = 7
+
+@pytest.fixture(scope="module")
+def tconf(tconf):
+    old_throughput_measurement_class = tconf.throughput_measurement_class
+    old_throughput_measurement_params = tconf.throughput_measurement_params
+    old_min_latency_count = tconf.MIN_LATENCY_COUNT
+
+    tconf.throughput_measurement_class = RevivalSpikeResistantEMAThroughputMeasurement
+    tconf.throughput_measurement_params = {
+        'window_size': 5,
+        'min_cnt': 2
+    }
+    tconf.MIN_LATENCY_COUNT = 1
+
+    yield tconf
+
+    tconf.MIN_LATENCY_COUNT = old_min_latency_count
+    tconf.throughput_measurement_class = old_throughput_measurement_class
+    tconf.throughput_measurement_params = old_throughput_measurement_params
+
+
+def testThroughputThreshold(looper, txnPoolNodeSet, tconf, requests):
+    looper.runFor(tconf.throughput_measurement_params['window_size'] *
+                  tconf.throughput_measurement_params['min_cnt'])
+    for node in txnPoolNodeSet:
+        masterThroughput, avgBackupThroughput = node.monitor.getThroughputs(
+            node.instances.masterId)
+        for r in node.replicas:
+            print("{} stats: {}".format(r, repr(r.stats)))
+        assert masterThroughput / avgBackupThroughput >= node.monitor.Delta
+
+
+def testReqLatencyThreshold(looper, txnPoolNodeSet, requests):
+    for node in txnPoolNodeSet:
+        for rq in requests:
+            key = get_key_from_req(rq)
+            assert key in node.monitor.masterReqLatenciesTest
+            assert node.monitor.masterReqLatenciesTest[key] <= node.monitor.Lambda
