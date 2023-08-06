36bc53032a3ba891e6fe5eddcd7c39e8e142e3ad
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-06 15:30:23 +0000

    Add package dativatools/2.10.5/dativatools-2.10.5.tar.gz

---
+++ packages/dativatools/dativatools-2.10.5.tar.gz/dativatools-2.10.5/dativa/tools/aws/__init__.py
@@ -0,0 +1,17 @@
+from .athena import AthenaClient, AthenaClientError
+from .s3_lib import S3Client, S3ClientError
+from .task_queue import TaskQueue
+from .s3csv2parquet import S3Csv2Parquet, S3Csv2ParquetConversionError
+from .task_queue import RetryException
+from .pipeline_api import PipelineClientException, PipelineClient
+
+__all__ = ['AthenaClient',
+           'AthenaClientError',
+           'S3Client',
+           'S3ClientError',
+           'S3Csv2Parquet',
+           'TaskQueue',
+           'S3Csv2ParquetConversionError',
+           'RetryException',
+           'PipelineClientException',
+           'PipelineClient']
