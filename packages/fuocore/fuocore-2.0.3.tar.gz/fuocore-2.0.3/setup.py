5223a0f0f6c08da6ae73097f39d0467f7403a4c8
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-06 01:55:49 +0000

    Add package fuocore/2.0.3/fuocore-2.0.3.tar.gz

---
+++ packages/fuocore/fuocore-2.0.3.tar.gz/fuocore-2.0.3/setup.py
@@ -0,0 +1,58 @@
+#!/usr/bin/env python3
+
+from setuptools import setup
+
+from fuocore import __version__
+
+
+requires = [
+    'pycrypto>=2.6.1',
+    'requests',
+    'beautifulsoup4>=4.5.3',
+    'marshmallow>=2.13.5',
+    'mutagen>=1.37',
+    'fuzzywuzzy',
+]
+
+
+setup(
+    name='fuocore',
+    version=__version__,
+    description='feeluown core',
+    author='Cosven',
+    author_email='yinshaowen241@gmail.com',
+    py_modules=['mpv'],
+    packages=[
+        'fuocore',
+        'fuocore.local',
+        'fuocore.netease',
+        'fuocore.xiami',
+        'fuocore.qqmusic',
+        'fuocore.protocol',
+        'fuocore.protocol.handlers',
+        ],
+    package_data={
+        '': []
+        },
+    url='https://github.com/cosven/feeluown-core',
+    keywords=['media', 'player', 'api'],
+    classifiers=(
+        'Development Status :: 3 - Alpha',
+        'Programming Language :: Python :: 3.5',
+        'Programming Language :: Python :: 3.6',
+        'Programming Language :: Python :: 3.7',
+        'Programming Language :: Python :: 3 :: Only',
+        ),
+    install_requires=requires,
+    setup_requires=['pytest-runner'],
+    test_suite="tests",
+    tests_require=[
+        'pytest',
+        'mock',
+    ],
+    entry_points={
+        'console_scripts': [
+            'feeluown_core_test = fuocore.main:main',
+        ],
+    },
+)
