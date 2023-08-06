5b7d95580351c3b37347baea43775dbe0e5fdf84
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-06 14:56:55 +0000

    Add package ttools/0.1/ttools-0.1.tar.gz

---
+++ packages/ttools/ttools-0.1.tar.gz/ttools-0.1/setup.py
@@ -0,0 +1,10 @@
+from setuptools import setup
+setup(name='ttools',
+      version='0.1',
+      description='alone THC\'s tools',
+      url='http://gitee.com/thc1234/ttools',
+      author='thc333',
+      author_email='thc333@yeah.net',
+      license='MIT',
+      packages=['ttools'],
+      zip_safe=False)
