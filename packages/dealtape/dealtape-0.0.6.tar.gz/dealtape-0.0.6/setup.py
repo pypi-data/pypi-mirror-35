cc9f4824225c6cce8bb5e8c3e1c1ff0f6515ca9a
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-06 07:08:07 +0000

    Add package dealtape/0.0.6/dealtape-0.0.6.tar.gz

---
+++ packages/dealtape/dealtape-0.0.6.tar.gz/dealtape-0.0.6/setup.py
@@ -0,0 +1,22 @@
+import re
+
+try:
+    from setuptools import setup
+except ImportError:
+    from distutils.core import setup
+
+with open('README.rst', 'rb') as f:
+    readme = f.read().decode('utf-8')
+
+setup(name='dealtape',
+      version="0.0.6",
+      description='DealTape SDK for Python',
+      long_description=readme,
+      author='Yutao Zhang',
+      author_email='yutao@recurrent.ai',
+      packages=['dealtape'],
+      license='MIT',
+      platforms=['any'],
+      install_requires=['requests!=2.9.0',],
+      url='https://github.com/rcrai/dealtape-python-sdk'
+)
