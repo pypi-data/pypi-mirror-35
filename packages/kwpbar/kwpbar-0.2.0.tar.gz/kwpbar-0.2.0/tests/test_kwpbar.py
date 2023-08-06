eec0a72d14b2d0802d89e7bf8a9a61de661c0cd5
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-06 14:23:32 +0000

    Add package kwpbar/0.2.0/kwpbar-0.2.0.tar.gz

---
+++ packages/kwpbar/kwpbar-0.2.0.tar.gz/kwpbar-0.2.0/tests/test_kwpbar.py
@@ -0,0 +1,37 @@
+#!/usr/bin/env python
+# -*- encoding: utf-8 -*-
+# KwPBar for Python — test suite
+# Copyright © 2015-2018, Chris Warrick.
+# See /LICENSE for licensing information.
+
+import pytest
+import kwpbar
+import kwpbar.demo
+
+
+def test_printing():
+    """Test progressbar printing."""
+    kwpbar.pbar(0, 2)
+    kwpbar.pbar(1, 2)
+    kwpbar.pbar(2, 2)
+
+
+def test_erasing():
+    """Test progressbar erasing."""
+    kwpbar.pbar(2, 3)
+    kwpbar.erase_pbar()
+
+
+def test_fail1():
+    with pytest.raises(ValueError):
+        kwpbar.pbar(1, 0)
+
+
+def test_fail2():
+    with pytest.raises(ValueError):
+        kwpbar.pbar(2, 1)
+
+
+def test_fail3():
+    with pytest.raises(ValueError):
+        kwpbar.pbar(-2, 1)
