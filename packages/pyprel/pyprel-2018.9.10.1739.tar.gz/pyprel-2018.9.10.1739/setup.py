d8861b7c5cebc05c2edaa7e3a1ede5465202a0c0
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-06 17:14:16 +0000

    Add package pyprel/2018.9.10.1739/pyprel-2018.9.10.1739.tar.gz

---
+++ packages/pyprel/pyprel-2018.9.10.1739.tar.gz/pyprel-2018.9.10.1739/setup.py
@@ -0,0 +1,50 @@
+#!/usr/bin/python
+# -*- coding: utf-8 -*-
+
+import os
+
+import setuptools
+
+def main():
+
+    setuptools.setup(
+        name             = "pyprel",
+        version          = "2018.09.10.1739",
+        description      = "Python print elegant",
+        long_description = long_description(),
+        url              = "https://github.com/wdbm/pyprel",
+        author           = "Will Breaden Madden",
+        author_email     = "wbm@protonmail.ch",
+        license          = "GPLv3",
+        py_modules       = [
+                           "pyprel"
+                           ],
+        install_requires = [
+                           "numpy",
+                           "pandas",
+                           "Pillow",
+                           "pyfiglet",
+                           "shijian"
+                           ],
+        entry_points     = """
+                           [console_scripts]
+                           pyprel = pyprel:pyprel
+                           """
+    )
+
+def long_description(
+    filename = "README.md"
+    ):
+
+    if os.path.isfile(os.path.expandvars(filename)):
+        try:
+            import pypandoc
+            long_description = pypandoc.convert_file(filename, "rst")
+        except ImportError:
+            long_description = open(filename).read()
+    else:
+        long_description = ""
+    return long_description
+
+if __name__ == "__main__":
+    main()
