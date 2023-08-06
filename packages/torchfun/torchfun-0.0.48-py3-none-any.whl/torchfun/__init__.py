fdcb5ced5a34f0869d6dec6885ca43d838650f61
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-06 16:16:48 +0000

    Add package torchfun/0.0.48/torchfun-0.0.48-py3-none-any.whl

---
+++ packages/torchfun/torchfun-0.0.48-py3-none-any.whl/torchfun/__init__.py
@@ -0,0 +1,18 @@
+name = 'torchfun'
+from .torchfun import flatten,imshow
+from .torchfun import *
+
+__version__ = open('version').read()
+
+__all__ = locals()
+
+del_list=[
+'os',
+'io',
+'np',
+'torch',
+'tqdm',
+'print_function']
+
+for del_item in del_list:
+    del __all__[del_item]
