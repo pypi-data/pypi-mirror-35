756660078f53dc44191510db9ce33a80b09a414f
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-06 15:35:11 +0000

    Add package fontmake/1.7.3/fontmake-1.7.3.zip

---
+++ packages/fontmake/fontmake-1.7.3.zip/fontmake-1.7.3/setup.py
@@ -0,0 +1,214 @@
+# Copyright 2015 Google Inc. All Rights Reserved.
+#
+# Licensed under the Apache License, Version 2.0 (the "License");
+# you may not use this file except in compliance with the License.
+# You may obtain a copy of the License at
+#
+#     http://www.apache.org/licenses/LICENSE-2.0
+#
+# Unless required by applicable law or agreed to in writing, software
+# distributed under the License is distributed on an "AS IS" BASIS,
+# WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
+# See the License for the specific language governing permissions and
+# limitations under the License.
+
+import sys
+from setuptools import setup, find_packages, Command
+from distutils import log
+from io import open
+
+
+class bump_version(Command):
+
+    description = "increment the package version and commit the changes"
+
+    user_options = [
+        ("major", None, "bump the first digit, for incompatible API changes"),
+        ("minor", None, "bump the second digit, for new backward-compatible features"),
+        ("patch", None, "bump the third digit, for bug fixes (default)"),
+    ]
+
+    def initialize_options(self):
+        self.minor = False
+        self.major = False
+        self.patch = False
+
+    def finalize_options(self):
+        part = None
+        for attr in ("major", "minor", "patch"):
+            if getattr(self, attr, False):
+                if part is None:
+                    part = attr
+                else:
+                    from distutils.errors import DistutilsOptionError
+                    raise DistutilsOptionError(
+                        "version part options are mutually exclusive")
+        self.part = part or "patch"
+
+    def bumpversion(self, part, **kwargs):
+        """ Run bumpversion.main() with the specified arguments.
+        """
+        import bumpversion
+
+        args = ['--verbose'] if self.verbose > 1 else []
+        for k, v in kwargs.items():
+            k = "--{}".format(k.replace("_", "-"))
+            is_bool = isinstance(v, bool) and v is True
+            args.extend([k] if is_bool else [k, str(v)])
+        args.append(part)
+
+        log.debug(
+            "$ bumpversion %s" % " ".join(a.replace(" ", "\\ ") for a in args))
+
+        bumpversion.main(args)
+
+    def run(self):
+        log.info("bumping '%s' version" % self.part)
+        self.bumpversion(self.part)
+
+
+class release(bump_version):
+    """Drop the developmental release '.devN' suffix from the package version,
+    open the default text $EDITOR to write release notes, commit the changes
+    and generate a git tag.
+
+    Release notes can also be set with the -m/--message option, or by reading
+    from standard input.
+    """
+
+    description = "tag a new release"
+
+    user_options = [
+        ("message=", 'm', "message containing the release notes"),
+        ("sign", "s", "make a GPG-signed tag, using the default key"),
+    ]
+
+    def initialize_options(self):
+        self.message = None
+        self.sign = False
+
+    def finalize_options(self):
+        import re
+
+        current_version = self.distribution.metadata.get_version()
+        if not re.search(r"\.dev[0-9]+", current_version):
+            from distutils.errors import DistutilsSetupError
+            raise DistutilsSetupError(
+                "current version (%s) has no '.devN' suffix.\n       "
+                "Run 'setup.py bump_version' with any of "
+                "--major, --minor, --patch options" % current_version)
+
+        message = self.message
+        if message is None:
+            if sys.stdin.isatty():
+                # stdin is interactive, use editor to write release notes
+                message = self.edit_release_notes()
+            else:
+                # read release notes from stdin pipe
+                message = sys.stdin.read()
+
+        if not message.strip():
+            from distutils.errors import DistutilsSetupError
+            raise DistutilsSetupError("release notes message is empty")
+
+        self.message = u"v{new_version}\n\n%s" % message
+        self.sign = bool(self.sign)
+
+    @staticmethod
+    def edit_release_notes():
+        """Use the default text $EDITOR to write release notes.
+        If $EDITOR is not set, use 'nano'."""
+        from tempfile import mkstemp
+        import os
+        import shlex
+        import subprocess
+
+        text_editor = shlex.split(os.environ.get('EDITOR', 'nano'))
+
+        fd, tmp = mkstemp(prefix='bumpversion-')
+        try:
+            os.close(fd)
+            with open(tmp, 'w', encoding='utf-8') as f:
+                f.write(u"\n\n# Write release notes.\n"
+                         "# Lines starting with '#' will be ignored.")
+            subprocess.check_call(text_editor + [tmp])
+            with open(tmp, 'r', encoding='utf-8') as f:
+                changes = u"".join(
+                    l for l in f.readlines() if not l.startswith('#'))
+        finally:
+            os.remove(tmp)
+        return changes
+
+    def run(self):
+        log.info("stripping developmental release suffix")
+        # drop '.dev0' suffix, commit with given message and create git tag
+        self.bumpversion("release",
+                         tag=True,
+                         message="Release {new_version}",
+                         tag_message=self.message,
+                         sign_tags=self.sign)
+
+
+needs_wheel = {'bdist_wheel'}.intersection(sys.argv)
+wheel = ['wheel'] if needs_wheel else []
+needs_bump2version = {'release', 'bump_version'}.intersection(sys.argv)
+bump2version = ['bump2version >= 0.5.7'] if needs_bump2version else []
+
+with open('README.rst', 'r', encoding='utf-8') as f:
+    long_description = f.read()
+
+setup(
+    name="fontmake",
+    version="1.7.3",
+    description=("Compile fonts from sources (UFO, Glyphs) to binary "
+                 "(OpenType, TrueType)."),
+    long_description=long_description,
+    url="https://github.com/googlei18n/fontmake",
+    license="Apache Software License 2.0",
+    packages=find_packages("Lib"),
+    package_dir={'': 'Lib'},
+    entry_points={
+        'console_scripts': [
+            'fontmake = fontmake.__main__:main'
+        ]
+    },
+    setup_requires=wheel + bump2version,
+    install_requires=[
+        "fonttools>=3.28.0",
+        "cu2qu>=1.5.0",
+        "glyphsLib>=3.0.3",
+        "ufo2ft>=2.2.0",
+        "MutatorMath>=2.1.1",
+        "defcon>=0.5.2",
+        "booleanOperations>=0.8.0",
+        "ufoLib[lxml]>=2.3.1",
+    ],
+    extras_require = {
+        "pathops": [
+            "skia-pathops>=0.2.0",
+        ],
+        # this is now default; kept here for backward compatibility
+        "lxml": [
+            # "lxml>=4.2.4",
+        ],
+    },
+    cmdclass={
+        "release": release,
+        "bump_version": bump_version,
+    },
+    classifiers=[
+        'Development Status :: 4 - Beta',
+        "Environment :: Console",
+        "Intended Audience :: Developers",
+        "License :: OSI Approved :: Apache Software License",
+        "Natural Language :: English",
+        "Operating System :: OS Independent",
+        "Programming Language :: Python",
+        "Programming Language :: Python :: 2",
+        "Programming Language :: Python :: 3",
+        "Topic :: Multimedia :: Graphics",
+        "Topic :: Multimedia :: Graphics :: Graphics Conversion",
+        "Topic :: Multimedia :: Graphics :: Editors :: Vector-Based",
+        "Topic :: Text Processing :: Fonts",
+    ],
+)
