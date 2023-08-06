c6ff6df2bf3abc903c57d9f28ee386e42df594ec
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-06 17:14:25 +0000

    Add package imikrokosmos/0.1.8/imikrokosmos-0.1.8.tar.gz

---
+++ packages/imikrokosmos/imikrokosmos-0.1.8.tar.gz/imikrokosmos-0.1.8/README.md
@@ -0,0 +1,38 @@
+# IMikrokosmos
+**IMikrokosmos** is a Jupyter/IPython kernel for
+the [Mikrokosmos](https://github.com/mroman42/mikrokosmos) lambda
+interpreter.
+
+**Important**: Please note that this Jupyter kernel only works on
+Windows on its `windows-support` branch. Certain features, such
+as unicode output, cause problems in this case.
+
+![mikrokosmos2](https://user-images.githubusercontent.com/5337877/28381708-11a1608a-6cbc-11e7-80da-2292d4716cdb.png)
+
+
+## Installation
+
+```
+sudo pip install imikrokosmos
+```
+
+
+### Manual installation
+The `kernel.json` file must be placed inside a folder named
+`jupyter-mikrokosmos` in one of the following paths
+
+```
+~/.local/share/jupyter/kernels/jupyter-mikrokosmos/
+/usr/share/jupyter/kernels/jupyter-mikrokosmos/
+```
+
+the `mikrokosmoskernel.py` file must be available for Python on the
+working directory, or, more generally, on
+the [sys-path](https://docs.python.org/2/library/sys.html#sys.path).
+
+## Support and contributing
+
+If you are an user seeking support, please use the 
+[GitHub issue tracker](https://github.com/mroman42/mikrokosmos/issues)
+of the main repository. The main repository also provides a CONTRIBUTING
+file where contribution guidelines are detailed.
---
+++ packages/imikrokosmos/imikrokosmos-0.1.8.tar.gz/imikrokosmos-0.1.8/setup.py
@@ -0,0 +1,56 @@
+#!/usr/bin/env python
+import sys
+import os
+from jupyter_client.kernelspec import install_kernel_spec
+from IPython.utils.tempdir import TemporaryDirectory
+from os.path import dirname,abspath
+from shutil import copy as file_copy
+from setuptools import setup, find_packages
+
+# Post installation of the Jupyter kernelspec
+from setuptools.command.develop import develop
+from setuptools.command.install import install
+
+class PostDevelopCommand(develop):
+    """Post-installation for development mode."""
+    def run(self):
+        install_my_kernel_spec()
+        develop.run(self)
+        
+class PostInstallCommand(install):
+    """Post-installation for installation mode."""
+    def run(self):
+        install_my_kernel_spec()
+        install.run(self)
+
+# This script to easily install the kernel spec has been taken from
+# the Sebastian Gutsche implementation of the Singular Jupyter kernel,
+# which can be read here
+#  https://github.com/sebasguts/jupyter_kernel_singular
+def install_my_kernel_spec(user=False):
+    with TemporaryDirectory() as td:
+        os.chmod(td, 0o755) # Starts off as 700, not user readable
+        path_of_file = dirname( abspath(__file__) )
+        file_copy(path_of_file + "/mikrokosmoskernel/kernel.js", td)
+        file_copy(path_of_file + "/mikrokosmoskernel/kernel.json", td)
+        print('Installing Jupyter kernel spec')
+        install_kernel_spec(td, 'IMikrokosmos', user=user, replace=True)
+    
+# pip setup
+setup( name='imikrokosmos'
+     , description='A Jupyter kernel for the mikrokosmos lambda interpreter'
+     , version='0.1.8'
+     , url='https://github.com/mroman42/jupyter-mikrokosmos'
+     , author='Mario Román'
+     , author_email='mromang08@gmail.com'
+     , license='GPLv3'
+     , packages=['mikrokosmoskernel']
+     , package_dir={"mikrokosmoskernel": "mikrokosmoskernel"}
+     , package_data={"mikrokosmoskernel":
+                     ["kernel.js"
+                     ,"kernel.json"]}
+     , cmdclass={
+        'develop': PostDevelopCommand,
+        'install': PostInstallCommand,
+       }
+     )
