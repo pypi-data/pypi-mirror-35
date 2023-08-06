6651431695a475e97d91f167cd7c6c917e1a99bc
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-06 01:56:36 +0000

    Add package afsutil/0.8.1/afsutil-0.8.1.tar.gz

---
+++ packages/afsutil/afsutil-0.8.1.tar.gz/afsutil-0.8.1/setup.py
@@ -0,0 +1,46 @@
+try:
+    from setuptools import setup
+except ImportError:
+    # Fallback to the standard library distutils.
+    from distutils.core import setup
+
+exec(open('afsutil/__version__.py').read())
+
+setup(
+    name='afsutil',
+    version=VERSION,
+    description='Utilities to setup OpenAFS clients and servers',
+    long_description=open('README.rst').read(),
+    author='Michael Meffie',
+    author_email='mmeffie@sinenomine.net',
+    url='https://github.com/openafs-contrib/afsutil',
+    license='BSD',
+    packages=[
+        'afsutil',
+        'afsutil.system',
+    ],
+    scripts=[
+        'bin/afsutil',
+    ],
+    include_package_data=True,
+    test_suite='test',
+    zip_safe=False,
+    options = {
+        'bdist_rpm':{
+            'post_install': 'packaging/rpm/post_install.sh',
+            'post_uninstall': 'packaging/rpm/post_uninstall.sh',
+        },
+    },
+    classifiers=[
+        'Development Status :: 4 - Beta',
+        'Environment :: Console',
+        'Intended Audience :: Developers',
+        'License :: OSI Approved :: MIT License',
+        'Operating System :: POSIX :: Linux',
+        'Operating System :: POSIX :: SunOS/Solaris',
+        'Programming Language :: Python :: 2',
+        'Programming Language :: Python :: 2.6',
+        'Programming Language :: Python :: 2.7',
+        'Topic :: Utilities',
+    ],
+)
