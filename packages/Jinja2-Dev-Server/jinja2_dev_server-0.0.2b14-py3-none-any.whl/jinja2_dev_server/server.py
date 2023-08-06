cb2cb2813fea9709a88a7f37c45b95916276618b
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-06 13:20:37 +0000

    Add package Jinja2-Dev-Server/0.0.2b14/jinja2_dev_server-0.0.2b14-py3-none-any.whl

---
+++ packages/Jinja2-Dev-Server/jinja2_dev_server-0.0.2b14-py3-none-any.whl/jinja2_dev_server/server.py
@@ -0,0 +1,41 @@
+import os
+
+from flask import Flask
+from flask import request, render_template
+
+templates = {}
+
+
+def run_server(dirs):
+    if type(dirs) == str:
+        dirs = [dirs]
+    for directory in dirs:
+        read_files(directory)
+
+    start_flask_server()
+
+
+def read_files(dir):
+    thisdir = os.getcwd()
+
+    # r=root, d=directories, f = files
+    for r, d, f in os.walk(thisdir):
+        for file in f:
+            if ".jinja2" in file.name:
+                print("Found " + file.name)
+                templates[:file.name.index(".")] = os.path.join(r, file)
+
+    if len(templates) == 0:
+        raise Exception("No files found")
+    print(templates)
+
+
+def start_flask_server():
+    app = Flask(__name__)
+
+    @app.route('{template}')
+    def serve_template():
+        template = request.match_info['user']
+        if template in templates:
+            return render_template(templates[template])
+        return 404
