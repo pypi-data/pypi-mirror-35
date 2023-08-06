7fd3c4a72a7282df0ae91b89f912aedd5df334c1
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-06 15:31:26 +0000

    Add package eth-bloom/1.0.1/eth-bloom-1.0.1.tar.gz

---
+++ packages/eth-bloom/eth-bloom-1.0.1.tar.gz/eth-bloom-1.0.1/setup.py
@@ -0,0 +1,36 @@
+#!/usr/bin/env python
+# -*- coding: utf-8 -*-
+from setuptools import setup, find_packages
+
+
+setup(
+    name='eth-bloom',
+    # *IMPORTANT*: Don't manually change the version here. Use the 'bumpversion' utility.
+    version='1.0.1',
+    description="""Python implementation of the Ethereum Trie structure""",
+    long_description_markdown_filename='README.md',
+    author='Piper Merriam',
+    author_email='pipermerriam@gmail.com',
+    url='https://github.com/ethereum/eth-bloom',
+    include_package_data=True,
+    py_modules=['eth_bloom'],
+    setup_requires=['setuptools-markdown'],
+    install_requires=[
+        "eth-hash>=0.1.0a3,<0.3.0",
+    ],
+    license="MIT",
+    zip_safe=False,
+    keywords='ethereum blockchain evm trie merkle',
+    packages=find_packages(exclude=["tests", "tests.*"]),
+    classifiers=[
+        'Development Status :: 2 - Pre-Alpha',
+        'Intended Audience :: Developers',
+        'License :: OSI Approved :: MIT License',
+        'Natural Language :: English',
+        'Programming Language :: Python :: 3',
+        'Programming Language :: Python :: 3.4',
+        'Programming Language :: Python :: 3.5',
+        'Programming Language :: Python :: 3.6',
+        'Programming Language :: Python :: Implementation :: PyPy',
+    ],
+)
