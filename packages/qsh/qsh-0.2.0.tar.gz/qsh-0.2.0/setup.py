134a173625256b05f8cae21905643b7666cc6176
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-06 13:23:03 +0000

    Add package qsh/0.2.0/qsh-0.2.0.tar.gz

---
+++ packages/qsh/qsh-0.2.0.tar.gz/qsh-0.2.0/setup.py
@@ -0,0 +1,10 @@
+from setuptools import setup
+
+setup(
+    name='qsh',
+    version='0.2.0',
+    url='https://github.com/nethask/qsh',
+    author='Artyom Knyazev',
+    author_email='nethask@gmail.com',
+    license='MIT'
+)
