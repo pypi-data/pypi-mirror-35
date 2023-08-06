8001e44fa0e7508cfd94e85f40ce9456266b7ab0
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-06 04:08:47 +0000

    Add package serafin/0.10.2/serafin-0.10.2.tar.gz

---
+++ packages/serafin/serafin-0.10.2.tar.gz/serafin-0.10.2/src/serafin/core/__init__.py
@@ -0,0 +1,7 @@
+# -*- coding: utf-8 -*-
+""" This package contains all core functionality of serafin.
+
+This package should be self sufficient (not depend on any other code in the
+code base).
+"""
+from __future__ import absolute_import, unicode_literals
