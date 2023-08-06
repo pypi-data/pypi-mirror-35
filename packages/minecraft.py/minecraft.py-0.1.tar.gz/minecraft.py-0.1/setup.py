b0894bacb5b7ebefca4125ad2322a343072800bb
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-06 01:54:50 +0000

    Add package minecraft.py/0.1/minecraft.py-0.1.tar.gz

---
+++ packages/minecraft.py/minecraft.py-0.1.tar.gz/minecraft.py-0.1/setup.py
@@ -0,0 +1,12 @@
+import setuptools
+
+setuptools.setup(
+    name='minecraft.py',
+    version='0.1',
+    description='Minecraft API',
+    author='William Evans',
+    author_email='w@william.gg',
+    url='https://github.com/IkumiGames/mc-api-py',
+    packages=setuptools.find_packages(),
+
+)
