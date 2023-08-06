2c28024edf7fcfc76d00dac407b075d9a5829ce1
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-06 01:55:35 +0000

    Add package django-password-reset/2.0/django-password-reset-2.0.tar.gz

---
+++ packages/django-password-reset/django-password-reset-2.0.tar.gz/django-password-reset-2.0/setup.py
@@ -0,0 +1,39 @@
+# -*- coding: utf-8 -*-
+try:
+    from setuptools import setup
+except ImportError:
+    from distutils.core import setup
+
+from setuptools import find_packages
+
+
+setup(
+    name='django-password-reset',
+    version=__import__('password_reset').__version__,
+    author='Bruno Renie',
+    author_email='bruno@renie.fr',
+    packages=find_packages(),
+    include_package_data=True,
+    url='https://github.com/brutasse/django-password-reset',
+    license='BSD licence, see LICENSE file',
+    description='Class-based views for password reset.',
+    long_description=open('README.rst').read(),
+    install_requires=[
+        'Django>=1.11',
+    ],
+    classifiers=[
+        'Development Status :: 4 - Beta',
+        'Environment :: Web Environment',
+        'Framework :: Django',
+        'Framework :: Django :: 1.11',
+        'Framework :: Django :: 2.0',
+        'Framework :: Django :: 2.1',
+        'Intended Audience :: Developers',
+        'License :: OSI Approved :: BSD License',
+        'Natural Language :: English',
+        'Programming Language :: Python',
+        'Programming Language :: Python :: 3',
+    ],
+    test_suite='runtests.runtests',
+    zip_safe=False,
+)
