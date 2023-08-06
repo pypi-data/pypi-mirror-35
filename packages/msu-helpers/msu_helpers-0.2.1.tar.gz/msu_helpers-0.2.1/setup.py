1b3ebfd1d03987a9d2c80b093ae91a0e9528c30d
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-06 17:11:09 +0000

    Add package msu-helpers/0.2.1/msu_helpers-0.2.1.tar.gz

---
+++ packages/msu-helpers/msu_helpers-0.2.1.tar.gz/msu_helpers-0.2.1/setup.py
@@ -0,0 +1,15 @@
+#!/usr/bin/env python
+# -*- coding: utf-8 -*-
+
+from setuptools import setup
+
+setup(
+    name='msu_helpers',
+    packages=['msu_helpers'],
+    description='Package created for the university project to store common applications logic in one place. I am pretty sure that you do not need it',
+    version='0.2.1',
+    url='https://github.com/Ujinjinjin/msu_helpers',
+    author='ujinjinjin',
+    author_email='gallkam@outlook.com ',
+    keywords=['pip','msu_sqluniversity']
+    )
