9f038f7c6a718192abf8b371125ee20e393b1736
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-06 04:09:27 +0000

    Add package pdfconduit-gui/0.1.2/pdfconduit-gui-0.1.2.tar.gz

---
+++ packages/pdfconduit-gui/pdfconduit-gui-0.1.2.tar.gz/pdfconduit-gui-0.1.2/setup.py
@@ -0,0 +1,41 @@
+import re
+from setuptools import setup, find_packages
+
+long_description = """
+GUI wrapper for pdf.
+"""
+
+# Retrieve version number
+VERSIONFILE = "pdf/gui/_version.py"
+verstrline = open(VERSIONFILE, "rt").read()
+VSRE = r"^__version__ = ['\"]([^'\"]*)['\"]"
+mo = re.search(VSRE, verstrline, re.M)
+if mo:
+    verstr = mo.group(1)
+else:
+    raise RuntimeError("Unable to find version string in %s." % (VERSIONFILE))
+
+setup(
+    install_requires=[
+        'PyPDF3>=0.0.6',
+        'pdfrw',
+        'PyMuPDF',
+        'Pillow',
+        'PySimpleGUI>=2.9.0',
+        'reportlab',
+        'looptools',
+        'pdfconduit',
+    ],
+    name='pdfconduit-gui',
+    version=verstr,
+    packages=find_packages(),
+    namespace_packages=['pdf'],
+    include_package_data=True,
+    url='https://github.com/mrstephenneal/pdfconduit',
+    license='',
+    author='Stephen Neal',
+    author_email='stephen@stephenneal.net',
+    description='GUI wrapper for pdfconduit.',
+    long_description=long_description,
+)
+
