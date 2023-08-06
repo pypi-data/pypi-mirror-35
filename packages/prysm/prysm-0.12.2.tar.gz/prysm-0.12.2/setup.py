5ecc2e04be4fda44b8164c5616ee9afe5f31c86f
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-06 13:53:06 +0000

    Add package prysm/0.12.2/prysm-0.12.2.tar.gz

---
+++ packages/prysm/prysm-0.12.2.tar.gz/prysm-0.12.2/setup.py
@@ -0,0 +1,28 @@
+"""prysm, a python optics module."""
+from setuptools import setup
+
+setup(
+    name='prysm',
+    version='0.12.2',
+    description='A python optics module',
+    long_description='Uses geometrical and fourier optics to model optical systems',
+    license='MIT',
+    author='Brandon Dube',
+    author_email='brandondube@gmail.com',
+    url='https://github.com/brandondube/prysm',
+    packages=['prysm'],
+    install_requires=['numpy', 'scipy', 'matplotlib'],
+    extras_require={
+        'cpu+': ['numba'],
+        'cuda': ['cupy'],
+        'Mx': ['h5py'],
+        'img': ['imageio'],
+        'mtf+': ['pandas'],
+    },
+    classifiers=[
+        'Development Status :: 4 - Beta',
+        'Intended Audience :: Science/Research',
+        'License :: OSI Approved :: MIT License',
+        'Programming Language :: Python :: 3.6',
+    ]
+)
