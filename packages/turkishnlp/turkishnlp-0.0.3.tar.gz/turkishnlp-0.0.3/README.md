4dc04d38e66f5a8cc9c42d5ae6b7589da9a2f4c1
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-06 07:07:49 +0000

    Add package turkishnlp/0.0.3/turkishnlp-0.0.3.tar.gz

---
+++ packages/turkishnlp/turkishnlp-0.0.3.tar.gz/turkishnlp-0.0.3/README.md
@@ -0,0 +1,21 @@
+# Turkish NLP
+
+Very early version of the TurkishNLP. For now it has basically 2 main functions; Detecting Turkish Language and correcting typos in Turkish words.
+
+## Dataset
+Dataset was created by parsing and filtering a Turkish wikipedia dump. 
+
+## Getting Started
+
+
+
+### Downloading the data
+
+
+
+
+### Usage
+
+
+
+
---
+++ packages/turkishnlp/turkishnlp-0.0.3.tar.gz/turkishnlp-0.0.3/setup.py
@@ -0,0 +1,12 @@
+from distutils.core import setup
+import setuptools
+setup(
+    name='turkishnlp',
+    version='0.0.3',
+    packages=['turkishnlp'],
+    url='https://github.com/MeteHanC/turkishnlp',
+    license='MIT',
+    author='Metehan Cetinkaya',
+    author_email='metehancet@gmail.com',
+    description='A python script for Turkish NLP'
+)
