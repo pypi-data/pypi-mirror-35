ffea331cf4a08b1f6edf8394ec67c7fbf4228bb5
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-06 15:30:57 +0000

    Add package aqualogic/0.8/aqualogic-0.8.tar.gz

---
+++ packages/aqualogic/aqualogic-0.8.tar.gz/aqualogic-0.8/setup.py
@@ -0,0 +1,30 @@
+from setuptools import setup
+setup(
+  name = 'aqualogic',
+  packages = ['aqualogic'], # this must be the same as the name above
+  version = '0.8',
+  description = 'Library for interfacing with a Hayward/Goldline AquaLogic/ProLogic pool controller.',
+  author = 'Sean Wilson',
+  author_email = 'sean.wilson@live.ca',
+  url = 'https://github.com/swilson/aqualogic',
+  license='MIT',
+  classifiers=[
+      # How mature is this project? Common values are
+      #   3 - Alpha
+      #   4 - Beta
+      #   5 - Production/Stable
+      'Development Status :: 4 - Beta',
+
+      # Indicate who your project is intended for
+      'Intended Audience :: Developers',
+      'Topic :: Software Development :: Embedded Systems',
+
+      'License :: OSI Approved :: MIT License',
+
+      'Programming Language :: Python :: 3',
+      'Programming Language :: Python :: 3.2',
+      'Programming Language :: Python :: 3.3',
+      'Programming Language :: Python :: 3.4',
+  ],
+  install_requires=['zope.event>=4.3']
+)
