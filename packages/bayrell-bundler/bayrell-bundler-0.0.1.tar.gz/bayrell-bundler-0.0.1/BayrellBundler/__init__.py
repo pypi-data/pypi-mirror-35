be54de35a7d487b8919b3e03315027e95dc0df70
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-06 17:09:58 +0000

    Add package bayrell-bundler/0.0.1/bayrell-bundler-0.0.1.tar.gz

---
+++ packages/bayrell-bundler/bayrell-bundler-0.0.1.tar.gz/bayrell-bundler-0.0.1/BayrellBundler/__init__.py
@@ -0,0 +1,16 @@
+# -*- coding: utf-8 -*-
+
+
+def bundler_out():
+	print ("Hello world!!!")
+
+
+__name__ = "bayrell-bundler"
+__version__ = "0.0.1"
+__description__ = "Bayrell Bundler"
+__license__ = "Apache License Version 2.0"
+__author__ = "Ildar Bikmamatov"
+__email__ = "ildar@bayrell.org"
+__copyright__ = "Copyright 2016-2018"
+__url__ = "https://github.com/bayrell/bundler_py3"
+
