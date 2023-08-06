6e70a3ce347099c5cfdcd3f6cd6e2a9108f2fd0d
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-06 15:30:31 +0000

    Add package veriservice/0.0.4/veriservice-0.0.4.tar.gz

---
+++ packages/veriservice/veriservice-0.0.4.tar.gz/veriservice-0.0.4/veriservice/__init__.py
@@ -0,0 +1,25 @@
+import grpc
+
+from veriservice import veriservice_pb2 as pb
+from veriservice import veriservice_pb2_grpc as pb_grpc
+
+class VeriClient:
+    def __init__(self, service):
+        self.service = service # eg.: 'localhost:50051'
+        self.channel = grpc.insecure_channel(self.service)
+        self.stub = pb_grpc.VeriServiceStub(self.channel)
+
+    def insert(self, feature, label, grouplabel, timestamp):
+        request = pb.InsertionRequest(timestamp = timestamp, label = label, grouplabel = grouplabel, feature = feature)
+        response = self.stub.Insert(request)
+        return response
+
+    def getKnn(self, feature, k=10, id='', timestamp=0, timeout=1000):
+        request = pb.KnnRequest(id=id, timestamp=timestamp, timeout=timeout, k=k, feature=feature)
+        response = self.stub.GetKnn(request)
+        return response
+
+    def get(self, label):
+        request = pb.GetRequest(label = label)
+        response = self.stub.Get(request)
+        return response
