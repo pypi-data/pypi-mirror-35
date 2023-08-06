f88e0c9f8bad5242a2ec26a61d3e4c52ff596a4a
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-06 16:19:41 +0000

    Add package innate/0.1.2/innate-0.1.2.tar.gz

---
+++ packages/innate/innate-0.1.2.tar.gz/innate-0.1.2/setup.py
@@ -0,0 +1,11 @@
+from setuptools import find_packages, setup
+from innate.__version__ import __version__
+
+setup(
+    name="innate",
+    version=__version__,
+    packages=find_packages(),
+    license="MIT License",
+    author="Jordan Eremieff",
+    author_email="jermff@gmail.com",
+)
