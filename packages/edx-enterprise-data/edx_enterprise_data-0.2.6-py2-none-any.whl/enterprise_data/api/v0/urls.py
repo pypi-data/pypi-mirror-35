191253be3fc1cc425caaf375655e2edac387643c
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-06 16:17:53 +0000

    Add package edx-enterprise-data/0.2.6/edx_enterprise_data-0.2.6-py2-none-any.whl

---
+++ packages/edx-enterprise-data/edx_enterprise_data-0.2.6-py2-none-any.whl/enterprise_data/api/v0/urls.py
@@ -0,0 +1,23 @@
+# -*- coding: utf-8 -*-
+"""
+URL definitions for enterprise data api version 1 endpoint.
+"""
+from __future__ import absolute_import, unicode_literals
+
+from rest_framework.routers import DefaultRouter
+
+from enterprise_data.api.v0 import views
+
+router = DefaultRouter()  # pylint: disable=invalid-name
+router.register(
+    r'enterprise/(?P<enterprise_id>.+)/enrollments',
+    views.EnterpriseEnrollmentsViewSet,
+    'enterprise-enrollments',
+)
+router.register(
+    r'enterprise/(?P<enterprise_id>.+)/users',
+    views.EnterpriseUsersViewSet,
+    'enterprise-users',
+)
+
+urlpatterns = router.urls
