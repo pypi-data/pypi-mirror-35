644a73aeb7f482b604fef2d656e311b08c1c679e
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-06 04:12:07 +0000

    Add package panlex_API/1.2.0/panlex_API-1.2.0.tar.gz

---
+++ packages/panlex_API/panlex_API-1.2.0.tar.gz/panlex_API-1.2.0/README.md
@@ -0,0 +1,5 @@
+# panlex_python_API
+
+A Python wrapper for the PanLex API. See panlex.py and PanLex API Documentation for more details:
+
+http://dev.panlex.org/api/
---
+++ packages/panlex_API/panlex_API-1.2.0.tar.gz/panlex_API-1.2.0/setup.py
@@ -0,0 +1,15 @@
+from setuptools import setup, find_packages
+
+setup(
+    name='panlex_API',
+    version='1.2.0',
+    author="Maxwell Joslyn; Caroline Glazer; Gary Krug; Alex DelPriore; Ben Yang",
+    author_email="info@panlex.org",
+    py_modules=["panlex"],
+    url="https://github.com/longnow/panlex_python_API",
+    description='Python wrapper for PanLex API',
+    install_requires=['ratelimit','requests'],
+    classifiers=["Development Status :: 5 - Production/Stable", "Programming Language :: Python",
+                 "Programming Language :: Python :: 3", "Operating System :: OS Independent",
+                 "License :: OSI Approved :: MIT License", "Topic :: Software Development :: Libraries :: Python Modules"]
+)
