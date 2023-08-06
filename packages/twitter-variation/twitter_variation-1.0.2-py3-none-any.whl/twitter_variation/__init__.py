607c82b800540b958c3bfb1fc0193b77826a177d
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-06 13:53:43 +0000

    Add package twitter-variation/1.0.2/twitter_variation-1.0.2-py3-none-any.whl

---
+++ packages/twitter-variation/twitter_variation-1.0.2-py3-none-any.whl/twitter_variation/__init__.py
@@ -0,0 +1,18 @@
+import pickle
+import os
+import sys
+
+name="twitter_variation"
+version="1.0.2"
+author="Shubhanshu Sharma"
+
+english_variation_file_path = os.path.join(os.path.dirname(__file__),'variation_to_english_dict.pickle')
+with open(english_variation_file_path,'rb') as f:
+    variation_dict = pickle.load(f)
+
+def variation_to_english(sent=''):
+    for k,v in variation_dict.items():
+        sent = sent.replace(k,v)
+    if sys.version_info[0]<3:
+        return sent.encode()
+    return sent
