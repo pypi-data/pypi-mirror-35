85d8724f63745403146b196a83c41463f5d2facd
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-06 07:11:26 +0000

    Add package sniputils/0.0.7/sniputils-0.0.7.tar.gz

---
+++ packages/sniputils/sniputils-0.0.7.tar.gz/sniputils-0.0.7/src/upstream/track_inject.py
@@ -0,0 +1,16 @@
+from sys import path
+from os.path import abspath, dirname, isfile, join
+
+
+def path_inject(file):
+    """
+    append upstream folder path in env PATH while folder is module
+    to resolve relative import as absolute grammar
+    :param file: path of file
+    """
+    folder_path = dirname(file)
+    while folder_path and folder_path != '/':
+        module = abspath(folder_path)
+        if isfile(join(module, '__init__.py')):
+            path.append(module)
+        folder_path = dirname(folder_path)
