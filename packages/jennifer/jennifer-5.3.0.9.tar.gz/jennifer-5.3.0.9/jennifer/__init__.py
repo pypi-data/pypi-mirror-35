7b01ee4a5e1e91872381d8a271edbccb34f5cb0b
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-06 13:21:41 +0000

    Add package jennifer/5.3.0.9/jennifer-5.3.0.9.tar.gz

---
+++ packages/jennifer/jennifer-5.3.0.9.tar.gz/jennifer-5.3.0.9/jennifer/__init__.py
@@ -0,0 +1,10 @@
+"""Aries agent for python wsgi application server
+Jennifer version 5 agent for python
+"""
+
+from . import config
+from . import wrap
+
+
+__version__ = '5.3.0.9'
+__author__ = 'Luavis'
