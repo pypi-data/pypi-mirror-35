60070e52930497d74081d48510d7c148de208104
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-06 04:14:58 +0000

    Add package cloud-storage-client/0.0.12/cloud_storage_client-0.0.12.tar.gz

---
+++ packages/cloud-storage-client/cloud_storage_client-0.0.12.tar.gz/cloud_storage_client-0.0.12/setup.py
@@ -0,0 +1,22 @@
+import os
+from setuptools import setup
+
+def read(fname):
+    return open(os.path.join(os.path.dirname(__file__), fname)).read()
+
+setup(
+    name = "cloud_storage_client",
+    version = "0.0.12",
+    author = "Pablo Aguirre",
+    author_email = "paguirrerubio@gmail.com",
+    license = "MIT",
+    url = "http://packages.python.org/cloud_storage_client",
+    packages=['cloud_storage_client'],
+    long_description=read('README.md'),
+    install_requires=[
+        'boto3==1.4.1',
+        'google.cloud==0.32.0',
+        'azure==3.0.0',
+        'pysftp==0.2.9'
+    ],
+)
