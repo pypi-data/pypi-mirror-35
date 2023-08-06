f2ee9c649cd6eb5e2dc54f389e9445d20e049c0d
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-06 07:11:00 +0000

    Add package spirack/0.1.7/spirack-0.1.7-py3-none-any.whl

---
+++ packages/spirack/spirack-0.1.7-py3-none-any.whl/spirack/chip_mode.py
@@ -0,0 +1,31 @@
+"""SPI modes and speeds for all used chips
+
+Lists all the used chips so far with the corresponding SPI mode and speeds.
+Necessary for setting the chip select in the SPI Rack
+"""
+# ADC
+AD7175_MODE = 3
+MCP320x_MODE = 0
+# DAC
+LTC2758_MODE = 0
+MAX521x_MODE = 1
+MAX5702_MODE = 1
+AD9106_MODE = 0
+# Other
+ADF4351_MODE = 0  # Frequency synthesizer
+LMK01010_MODE = 0 # Clock distribution/division/delay
+BICPINS_MODE = 0
+ADT7301_MODE = 0 # C1b temperature sensor
+
+# SPI clock = MCK/SCBR = 84Mc/SCBR (range 14..255)
+LTC2758_SPEED = 6       # 14Mc (40Mc later)
+LTC2758_RD_SPEED = 8    # 10.5Mc
+AD9106_SPEED = 6        # 14Mc (Can go higher)
+MAX5702_SPEED = 6       # 14Mc
+MAX521x_SPEED = 6       # 14Mc
+LMK01010_SPEED = 6      # 14Mc
+ADF4351_SPEED = 21      #  4Mc (slow for test)
+AD7175_SPEED = 21       #  4Mc (slow for test)
+MCP320x_SPEED = 84      #  1Mc
+BICPINS_SPEED = 84      #  1Mc
+ADT7301_SPEED = 84      #  1Mc
