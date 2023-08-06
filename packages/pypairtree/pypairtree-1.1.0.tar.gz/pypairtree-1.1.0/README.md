35e2d4fcd21fd39e4e959da0b657803794ce220f
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-06 07:07:38 +0000

    Add package pypairtree/1.1.0/pypairtree-1.1.0.tar.gz

---
+++ packages/pypairtree/pypairtree-1.1.0.tar.gz/pypairtree-1.1.0/README.md
@@ -0,0 +1,40 @@
+# pypairtree [![Build Status](https://travis-ci.org/unt-libraries/pypairtree.svg?branch=master)](https://travis-ci.org/unt-libraries/pypairtree)
+
+pypairtree is a Python implementation of Pairtree for storing objects
+in a filesystem hierarchy that maps object identifiers to two character
+directory paths (http://tools.ietf.org/html/draft-kunze-pairtree-01).
+
+## License
+
+See LICENSE.txt.
+
+## Acknowledgements
+
+pypairtree was developed at the UNT Libraries and has been worked on
+by a number of developers over the years including
+
+- [Kurt Nordstrom](https://github.com/kurtnordstrom)
+- Brandon Fredericks
+- [Mark Phillips](https://github.com/vphill)
+- [Lauren Ko](https://github.com/ldko)
+- [Joey Liechty](https://github.com/yeahdef)
+- [Gio Gottardi](https://github.com/somexpert)
+
+If you have questions about the project feel free to contact Mark Phillips
+at mark.phillips@unt.edu.
+
+## Installation
+
+Install with:
+```sh
+$ python setup.py install
+```
+
+## Tests
+
+Unit tests are provided. To run them via Tox, ensure you are in the
+pypairtree directory with the tox.ini file and execute:
+
+```sh
+$ tox
+```
---
+++ packages/pypairtree/pypairtree-1.1.0.tar.gz/pypairtree-1.1.0/pypairtree/pairtreelist.py
@@ -0,0 +1,50 @@
+#!/usr/bin/env python
+
+"""Provides a function to print object identifiers in a Pairtree.
+
+__main__ function allows this to be run from the commandline.
+"""
+
+from __future__ import print_function
+
+import os
+import sys
+
+from optparse import OptionParser
+
+from pypairtree import pairtree
+
+
+def listIDs(basedir):
+    """Lists digital object identifiers of Pairtree directory structure.
+
+    Walks a Pairtree directory structure to get IDs. Prepends prefix
+    found in pairtree_prefix file. Outputs to standard output.
+    """
+    prefix = ''
+    # check for pairtree_prefix file
+    prefixfile = os.path.join(basedir, 'pairtree_prefix')
+    if os.path.isfile(prefixfile):
+        rff = open(prefixfile, 'r')
+        prefix = rff.readline().strip()
+        rff.close()
+    # check for pairtree_root dir
+    root = os.path.join(basedir, 'pairtree_root')
+    if os.path.isdir(root):
+        objects = pairtree.findObjects(root)
+        for obj in objects:
+            doi = os.path.split(obj)[1]
+            # print with prefix and original chars in place
+            print(prefix + pairtree.deSanitizeString(doi))
+    else:
+        print('pairtree_root directory not found')
+
+
+if __name__ == '__main__':
+    usage = 'usage: %prog [options] <pairtreedir>'
+    parser = OptionParser(usage)
+    (options, args) = parser.parse_args()
+    if len(args) != 1:
+        parser.print_help()
+        sys.exit(1)
+    listIDs(args[0])
