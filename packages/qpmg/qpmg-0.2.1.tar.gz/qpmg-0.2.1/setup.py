4e36e12c3219d8a54c986c045d8b9470419e8fc0
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-06 16:16:46 +0000

    Add package qpmg/0.2.1/qpmg-0.2.1.tar.gz

---
+++ packages/qpmg/qpmg-0.2.1.tar.gz/qpmg-0.2.1/setup.py
@@ -0,0 +1,18 @@
+from setuptools import setup
+
+setup(
+    name = 'qpmg',
+    description = 'Quick Plotting for MESA or GYRE',
+    long_description=open('README.rst').read(),
+    version = '0.2.1',
+    install_requires = ['numpy', 'matplotlib'],
+    author = 'Warrick Ball',
+    author_email = 'W.H.Ball@bham.ac.uk',
+    url = 'https://github.com/warrickball/qpmg',
+    scripts = ['qpmg'],
+    license = 'MIT',
+    classifiers=[
+        "License :: OSI Approved :: MIT License",
+        "Programming Language :: Python"
+        ]
+)
