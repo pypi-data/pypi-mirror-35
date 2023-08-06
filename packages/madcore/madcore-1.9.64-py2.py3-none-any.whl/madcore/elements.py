cd01b75946fbfa5f76f2c05779444c7fca425609
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-06 01:56:23 +0000

    Add package madcore/1.9.64/madcore-1.9.64-py2.py3-none-any.whl

---
+++ packages/madcore/madcore-1.9.64-py2.py3-none-any.whl/madcore/elements.py
@@ -0,0 +1,89 @@
+"""
+MIT License
+
+Copyright (c) 2016-2018 Madcore Ltd
+
+Permission is hereby granted, free of charge, to any person obtaining a copy
+of this software and associated documentation files (the "Software"), to deal
+in the Software without restriction, including without limitation the rights
+to use, copy, modify, merge, publish, distribute, sublicense, and/or sell
+copies of the Software, and to permit persons to whom the Software is
+furnished to do so, subject to the following conditions:
+
+The above copyright notice and this permission notice shall be included in all
+copies or substantial portions of the Software.
+
+THE SOFTWARE IS PROVIDED "AS IS", WITHOUT WARRANTY OF ANY KIND, EXPRESS OR
+IMPLIED, INCLUDING BUT NOT LIMITED TO THE WARRANTIES OF MERCHANTABILITY,
+FITNESS FOR A PARTICULAR PURPOSE AND NONINFRINGEMENT. IN NO EVENT SHALL THE
+AUTHORS OR COPYRIGHT HOLDERS BE LIABLE FOR ANY CLAIM, DAMAGES OR OTHER
+LIABILITY, WHETHER IN AN ACTION OF CONTRACT, TORT OR OTHERWISE, ARISING FROM,
+OUT OF OR IN CONNECTION WITH THE SOFTWARE OR THE USE OR OTHER DEALINGS IN THE
+SOFTWARE.
+"""
+
+from static import Static
+from cmd import Cmd
+import subprocess
+import os
+import sys
+import localtemplate
+import cmdkubectl
+import cmdhelm
+import time
+
+class Struct:
+    def __init__(self, **entries):
+        self.__dict__.update(entries)
+
+class Elements(object):
+    settings = None
+    localtemplate = None
+    CmdKubectl = None
+    CmdHelm = None
+
+    def __init__(self, in_settings):
+        self.settings = in_settings
+        self.localtemplate = localtemplate.LocalTemplate(self.settings)
+        self.CmdKubectl = cmdkubectl.CmdKubectl(self.settings)
+        self.CmdHelm = cmdhelm.CmdHelm(self.settings)
+
+    def install_elements(self, stage):
+        name = "ELEMENT"
+        Static.figletcyber('{0} {1}'.format(name, stage.upper()))
+
+        self.CmdKubectl.get_master_ip()
+        self.CmdKubectl.get_ingress_ips()
+
+        for element in self.settings.elements[stage]:
+            self.create_stage(element)
+
+
+    def create_stage(self, stage):
+        element_item = Struct(**stage)
+        self.localtemplate.generate_template_element(element_item)
+
+        # before add taint
+        if hasattr(element_item, "taint"):
+            if "before" in element_item.taint:
+                if element_item.taint["before"] == 'master-remove-all':
+                    self.CmdKubectl.taint_remove_from_master()
+
+        # helm or kubectl
+        if hasattr(element_item, "chart"):
+            # process as helm component
+            self.CmdHelm.install(element_item)
+        else:
+            # process as kubectl component
+            self.CmdKubectl.apply(element_item)
+
+        # after add taint
+        if hasattr(element_item, "taint"):
+            if "after" in element_item.taint:
+                if element_item.taint["after"] == 'master-add-noschedule':
+                    self.CmdKubectl.wait_until_kube_system_ready()
+                    self.CmdKubectl.taint_add_to_master_noschedule()
+
+        time.sleep(3)
+        print
+
---
+++ packages/madcore/madcore-1.9.64-py2.py3-none-any.whl/madcore/localtemplate.py
@@ -0,0 +1,88 @@
+"""
+MIT License
+
+Copyright (c) 2016-2018 Madcore Ltd
+
+Permission is hereby granted, free of charge, to any person obtaining a copy
+of this software and associated documentation files (the "Software"), to deal
+in the Software without restriction, including without limitation the rights
+to use, copy, modify, merge, publish, distribute, sublicense, and/or sell
+copies of the Software, and to permit persons to whom the Software is
+furnished to do so, subject to the following conditions:
+
+The above copyright notice and this permission notice shall be included in all
+copies or substantial portions of the Software.
+
+THE SOFTWARE IS PROVIDED "AS IS", WITHOUT WARRANTY OF ANY KIND, EXPRESS OR
+IMPLIED, INCLUDING BUT NOT LIMITED TO THE WARRANTIES OF MERCHANTABILITY,
+FITNESS FOR A PARTICULAR PURPOSE AND NONINFRINGEMENT. IN NO EVENT SHALL THE
+AUTHORS OR COPYRIGHT HOLDERS BE LIABLE FOR ANY CLAIM, DAMAGES OR OTHER
+LIABILITY, WHETHER IN AN ACTION OF CONTRACT, TORT OR OTHERWISE, ARISING FROM,
+OUT OF OR IN CONNECTION WITH THE SOFTWARE OR THE USE OR OTHER DEALINGS IN THE
+SOFTWARE.
+"""
+
+from jinja2 import Environment, PackageLoader, FileSystemLoader
+import os
+from prettytable import PrettyTable
+import static
+
+
+class Struct:
+    def __init__(self, **entries):
+        self.__dict__.update(entries)
+
+
+class LocalTemplate(object):
+
+    settings = None
+    path_populated = None
+
+    def __init__(self, in_settings):
+        self.settings = in_settings
+
+    def generate_template(self, name):
+
+        #env = Environment(loader=PackageLoader('localtemplate', 'templates'))
+        env = Environment(loader=FileSystemLoader(self.settings.folder_app_templates))
+        template = env.get_template(name)
+        rendered = template.render(settings=self.settings)
+
+        self.path_populated = "{0}/{1}".format(self.settings.folder_user_populated, name)
+        with open(self.path_populated, "wb") as f:
+            f.write(rendered.encode("UTF-8"))
+        f.close()
+
+    def generate_template_node(self, file_template, file_populated, ig):
+        env = Environment(loader=FileSystemLoader(self.settings.folder_app_templates))
+        template = env.get_template(file_template)
+        rendered = template.render(ig=ig, settings=self.settings)
+
+        self.path_populated = "{0}/{1}".format(self.settings.folder_user_populated, file_populated)
+        with open(self.path_populated, "wb") as f:
+            f.write(rendered.encode("UTF-8"))
+        f.close()
+
+    def generate_template_element(self, item):
+        env = Environment(loader=FileSystemLoader(self.settings.folder_app_templates))
+        template = env.get_template(item.template)
+        rendered = template.render(component=item, settings=self.settings)
+
+        if self.settings.provision.cloud == "minikube":
+            rendered = self.overwrite_nodeselector_for_minikube (rendered)
+
+        self.path_populated = "{0}/{1}".format(self.settings.folder_user_populated, item.template)
+        with open(self.path_populated, "wb") as f:
+            f.write(rendered.encode("UTF-8"))
+        f.close()
+
+    def overwrite_nodeselector_for_minikube(self, data):
+        out = ''
+        lines = data.split('\n')
+        for line in lines:
+            if "kops.k8s.io/instancegroup:" in line:
+                number_of_leading_spaces = len(line) - len(line.lstrip())
+                line = "{0}kubernetes.io/hostname: minikube".format(' ' * number_of_leading_spaces)
+            out += '{0}\n'.format(line)
+        return out
+
