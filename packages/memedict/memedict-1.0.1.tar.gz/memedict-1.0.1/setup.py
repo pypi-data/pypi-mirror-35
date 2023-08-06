317583f65613f9b9c6c4e2aa038c525150c91177
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-06 01:52:54 +0000

    Add package memedict/1.0.1/memedict-1.0.1.tar.gz

---
+++ packages/memedict/memedict-1.0.1.tar.gz/memedict-1.0.1/setup.py
@@ -0,0 +1,34 @@
+#!/usr/bin/env python
+
+# Copyright (c) 2018 Fabrice Laporte - kray.me
+# The MIT License http://www.opensource.org/licenses/mit-license.php
+
+import os
+import time
+
+from setuptools import setup
+
+VERSION = '1.0.1'
+DIRPATH = os.path.dirname(__file__)
+
+setup(name='memedict',
+    version=VERSION if not VERSION.endswith('dev') else '%s%s' % (VERSION, int(time.time())),
+    description='Knowyourmeme.com definitions scraper',
+    long_description=open(os.path.join(DIRPATH, 'README.md')).read(),
+    author='Fabrice Laporte',
+    author_email='kraymer@gmail.com',
+    url='https://github.com/KraYmer/memedict',
+    license='MIT',
+    platforms='ALL',
+
+    packages=[
+      'memedict',
+    ],
+    install_requires=open(os.path.join(DIRPATH, 'requirements.txt')).read().split('\n'),
+    classifiers=[
+        'License :: OSI Approved :: MIT License',
+        'Programming Language :: Python',
+        'Topic :: Communications :: Chat',
+    ],
+    keywords="Know Your Meme desciptions web scraper.",
+    )
