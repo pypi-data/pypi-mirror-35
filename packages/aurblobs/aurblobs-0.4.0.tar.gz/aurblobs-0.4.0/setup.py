7ff9da9677c3fdf1277a943757c8388fd936d6ac
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-06 16:19:46 +0000

    Add package aurblobs/0.4.0/aurblobs-0.4.0.tar.gz

---
+++ packages/aurblobs/aurblobs-0.4.0.tar.gz/aurblobs-0.4.0/setup.py
@@ -0,0 +1,47 @@
+import codecs
+import os.path
+
+import sys
+from setuptools import setup, find_packages
+
+from aurblobs import __VERSION__
+
+
+if sys.argv[-1] == "publish":
+    os.system("python setup.py sdist bdist_wheel upload")
+    sys.exit()
+
+here = os.path.abspath(os.path.dirname(__file__))
+
+
+# use README as long description
+with codecs.open(os.path.join(here, 'README.rst'), encoding='utf-8') as handle:
+    long_description = handle.read()
+
+# required dependencies
+required = [
+    'click',
+    'docker>=3.0.0',
+    'GitPython',
+    'pretty-bad-protocol>=3.1.1',
+    'requests',
+    'xdg'
+]
+
+
+setup(
+    name='aurblobs',
+    version=__VERSION__,
+    description='Automatically build AUR packages and serve them from a '
+                'repository',
+    long_description=long_description,
+    author='Martin Weinelt',
+    author_email='martin+aurblobs@linuxlounge.net',
+    url='https://www.github.com/aurblobs/aurblobs',
+    license='AGPL',
+    install_requires=required,
+    packages=find_packages(),
+    entry_points={
+        'console_scripts': ['aurblobs=aurblobs:cli']
+    },
+)
