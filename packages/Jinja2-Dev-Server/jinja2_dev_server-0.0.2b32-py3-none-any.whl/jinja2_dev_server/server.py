bd8fc3e6c49ac673e5a3182cc0511f0125b00f00
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-06 13:20:44 +0000

    Add package Jinja2-Dev-Server/0.0.2b32/jinja2_dev_server-0.0.2b32-py3-none-any.whl

---
+++ packages/Jinja2-Dev-Server/jinja2_dev_server-0.0.2b32-py3-none-any.whl/jinja2_dev_server/server.py
@@ -0,0 +1,65 @@
+import os
+
+from flask import Flask
+from flask import request, render_template
+import jinja2
+
+templates = {}
+
+debug = False
+
+app = Flask(__name__)
+my_loader = jinja2.ChoiceLoader([
+    app.jinja_loader,
+    jinja2.FileSystemLoader([os.getcwd()]),
+])
+app.jinja_loader = my_loader
+
+cwd = os.getcwd()
+
+
+def run_server(dirs, debug_opt):
+    global debug
+    debug = debug_opt
+    if type(dirs) == str:
+        dirs = [dirs]
+    for directory in dirs:
+        read_files(directory)
+
+    start_flask_server()
+
+
+def read_files(dir):
+    global debug
+    thisdir = os.getcwd()
+
+    # r=root, d=directories, f = files
+    for r, d, f in os.walk(thisdir):
+        for file in f:
+            if debug:
+                print(file)
+            if ".jinja2" in file:
+                if debug:
+                    print("Found " + file)
+
+                filepath = os.path.join(r, file)
+
+                templates[file[:file.find(".")]] = filepath[len(cwd) + 1:]
+
+    if len(templates) == 0:
+        raise Exception("No files found")
+    if debug:
+        print(templates)
+
+
+def start_flask_server():
+    app.run(debug=True)
+
+
+@app.route('/<path:path>')
+def serve_template(path):
+    if debug:
+        print(path, path in templates)
+    if path in templates:
+        return render_template(templates[path])
+    return 404
