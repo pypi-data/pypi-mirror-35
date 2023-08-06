123b9494f2cfe1d706682eb74d489846b8f43e71
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-06 01:56:31 +0000

    Add package proxyscrape/0.1.0/proxyscrape-0.1.0.tar.gz

---
+++ packages/proxyscrape/proxyscrape-0.1.0.tar.gz/proxyscrape-0.1.0/CHANGELOG.rst
@@ -0,0 +1,20 @@
+Changelog
+=========
+All notable changes to this project will be documented in this file.
+
+The format is based on `Keep a Changelog`_ and this project adheres to `Semantic Versioning`_.
+
+.. _Keep a Changelog: http://keepachangelog.com/en/1.0.0/
+.. _Semantic Versioning: http://semver.org/spec/v2.0.0.html
+
+`Unreleased`_
+-------------
+
+`0.1.0`_ - 2018-08-26
+---------------------
+Added
+^^^^^
+- Initial Version
+
+.. _Unreleased: https://github.com/JaredLGillespie/proxyscrape/compare/v0.1.0...HEAD
+.. _0.1.0: https://github.com/JaredLGillespie/proxyscrape/releases/tag/v0.1.0
---
+++ packages/proxyscrape/proxyscrape-0.1.0.tar.gz/proxyscrape-0.1.0/setup.py
@@ -0,0 +1,37 @@
+from setuptools import setup
+from os import path
+
+here = path.abspath(path.dirname(__file__))
+
+with open(path.join(here, 'README.rst'), encoding='utf-8') as f:
+    long_description = f.read()
+
+setup(
+    name='proxyscrape',
+    version='0.1.0',
+    description='A library for retrieving free proxies (HTTP, HTTPS, SOCKS4, SOCKS5).',
+    long_description=long_description,
+    url='https://github.com/jaredlgillespie/proxyscrape',
+    author='Jared Gillespie',
+    author_email='jaredlgillespie@hotmail.com',
+    license='MIT',
+    classifiers=[
+        'Development Status :: 4 - Beta',
+        'Intended Audience :: Developers',
+        'License :: OSI Approved :: MIT License',
+        'Natural Language :: English',
+        'Operating System :: OS Independent',
+        'Programming Language :: Python :: 3',
+        'Programming Language :: Python :: 3.6',
+        'Programming Language :: Python :: Implementation :: CPython',
+        'Topic :: Software Development :: Libraries :: Python Modules',
+        'Topic :: Utilities'
+    ],
+    keywords='proxyscrape proxy scrape scraper',
+    packages=['proxyscrape'],
+    test_suite='tests',
+    install_requires=[
+        'BeautifulSoup4',
+        'requests'
+    ]
+)
