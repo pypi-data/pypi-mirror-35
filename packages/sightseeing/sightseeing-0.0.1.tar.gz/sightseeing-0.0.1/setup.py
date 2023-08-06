a8cc2cf0ee618f7c14dabbb5a6f585abdc9fe9b2
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-06 11:52:47 +0000

    Add package sightseeing/0.0.1/sightseeing-0.0.1.tar.gz

---
+++ packages/sightseeing/sightseeing-0.0.1.tar.gz/sightseeing-0.0.1/setup.py
@@ -0,0 +1,28 @@
+from setuptools import setup
+from setuptools import find_packages
+
+setup(
+    name='sightseeing',
+    version='0.0.1',
+    description='',
+    author='Yusuke Sugomori',
+    author_email='me@yusugomori.com',
+    url='https://github.com/micin-jp/sightseeing',
+    download_url='',
+    install_requires=[],
+    classifiers=[
+        'Development Status :: 1 - Planning',
+        'Intended Audience :: Developers',
+        'Intended Audience :: Education',
+        'Intended Audience :: Science/Research',
+        'Programming Language :: Python :: 3',
+        'Topic :: Scientific/Engineering',
+        'Topic :: Scientific/Engineering :: Mathematics',
+        'Topic :: Scientific/Engineering :: Artificial Intelligence',
+        'Topic :: Software Development',
+        'Topic :: Software Development :: Libraries',
+        'Topic :: Software Development :: Libraries :: Python Modules',
+    ],
+    keywords='machine learning',
+    packages=find_packages()
+)
