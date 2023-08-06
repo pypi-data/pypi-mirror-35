32d57ad4b3922f835e74ae6f004d5e084da83ff8
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-06 14:56:34 +0000

    Add package pyinspector/1.3/pyinspector-1.3.tar.gz

---
+++ packages/pyinspector/pyinspector-1.3.tar.gz/pyinspector-1.3/setup.py
@@ -0,0 +1,26 @@
+from setuptools import setup, find_packages
+import os
+
+
+base_dir = os.path.dirname(__file__)
+readme_path = '%s%s' % (''.join(base_dir), 'README.md')
+with open(readme_path) as stream:
+    long_description = stream.read()
+
+setup(
+    name='pyinspector',
+    url='https://github.com/omauger/pyinspector',
+    author='Ophélie Mauger',
+    packages=find_packages(where='.'),
+    entry_points={
+        'console_scripts': ['pyinspector=pyinspector.__main__:main'],
+    },
+    install_requires=[
+        'flake8', 'coverage', 'pylint', 'radon',
+        'xenon', 'coloredlogs', 'verboselogs'
+    ],
+    version='1.3',
+    license='MIT',
+    description='A module to easely run quality pipeline for python project',
+    long_description=long_description,
+)
