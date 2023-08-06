dfe0fff7e5530a5c8fd829cb51b23b1a71ded173
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-06 14:28:51 +0000

    Add package castle/2.2.1/castle-2.2.1-py2.py3-none-any.whl

---
+++ packages/castle/castle-2.2.1-py2.py3-none-any.whl/castle/commands/authenticate.py
@@ -0,0 +1,18 @@
+from castle.command import Command
+from castle.utils import timestamp
+from castle.context.merger import ContextMerger
+from castle.context.sanitizer import ContextSanitizer
+from castle.validators.present import ValidatorsPresent
+
+
+class CommandsAuthenticate(object):
+    def __init__(self, context):
+        self.context = context
+
+    def build(self, options):
+        ValidatorsPresent.call(options, 'event', 'user_id')
+        context = ContextMerger.call(self.context, options.get('context'))
+        context = ContextSanitizer.call(context)
+        options.update({'sent_at': timestamp(), 'context': context})
+
+        return Command(method='post', path='authenticate', data=options)
