f2e93f522b3cf2724ae917690225acb7a7e2b2bd
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-06 01:56:42 +0000

    Add package wagtail-react-streamfield/0.8.3/wagtail-react-streamfield-0.8.3.tar.gz

---
+++ packages/wagtail-react-streamfield/wagtail-react-streamfield-0.8.3.tar.gz/wagtail-react-streamfield-0.8.3/wagtail_react_streamfield/blocks/list_block.py
@@ -0,0 +1,36 @@
+from wagtail.core.blocks import ListBlock
+
+from ..exceptions import RemovedError
+
+
+class NewListBlock(ListBlock):
+    def get_definition(self):
+        definition = super(ListBlock, self).get_definition()
+        definition.update(
+            children=[self.child_block.get_definition()],
+            # TODO: Modify Wagtail to add min_num & max_num to ListBlock.
+            # minNum=self.meta.min_num,
+            # maxNum=self.meta.max_num,
+        )
+        return definition
+
+    def render_list_member(self, *args, **kwargs):
+        raise RemovedError
+
+    def html_declarations(self):
+        raise RemovedError
+
+    def js_initializer(self):
+        raise RemovedError
+
+    def render_form(self, *args, **kwargs):
+        raise RemovedError
+
+    def value_from_datadict(self, data, files, prefix):
+        return [
+            self.child_block.value_from_datadict(child_block_data, files,
+                                                 prefix)
+            for child_block_data in data['value']]
+
+    def value_omitted_from_data(self, *args, **kwargs):
+        raise RemovedError
