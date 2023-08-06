7ab314b1836ef768bd630b43785b96ea76a59383
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-06 15:34:41 +0000

    Add package abeona/0.22.1/abeona-0.22.1.tar.gz

---
+++ packages/abeona/abeona-0.22.1.tar.gz/abeona-0.22.1/setup.py
@@ -0,0 +1,50 @@
+#!/usr/bin/env python
+# -*- encoding: utf-8 -*-
+
+from glob import glob
+from os.path import splitext, basename
+
+from setuptools import find_packages, setup
+
+with open('README.rst', 'r', encoding='utf-8') as f:
+    readme = f.read()
+
+setup(
+    name='abeona',
+    version='0.22.1',
+    description='',
+    long_description=readme,
+    author='Warren W. Kretzschmar',
+    author_email='warrenk@kth.se',
+    maintainer='Warren W. Kretzschmar',
+    maintainer_email='warrenk@kth.se',
+    url='https://github.com/winni2k/abeona',
+    license='Apache-2.0',
+
+    classifiers=[
+        'Development Status :: 4 - Beta',
+        'Intended Audience :: Developers',
+        'License :: OSI Approved :: Apache Software License',
+        'Natural Language :: English',
+        'Operating System :: OS Independent',
+        'Programming Language :: Python :: 3.6',
+        'Programming Language :: Python :: Implementation :: CPython',
+        'Programming Language :: Python :: Implementation :: PyPy',
+    ],
+    entry_points={
+        'console_scripts': ['abeona=abeona.__main__:main'],
+    },
+
+    install_requires="""
+    cortexpy >= 0.41.0
+    snakemake
+    pandas
+    progressbar2
+    """.split('\n'),
+
+    packages=find_packages('src'),
+    package_dir={'': 'src'},
+    py_modules=[splitext(basename(path))[0] for path in glob('src/*.py')],
+    include_package_data=True,
+    zip_safe=False,
+)
