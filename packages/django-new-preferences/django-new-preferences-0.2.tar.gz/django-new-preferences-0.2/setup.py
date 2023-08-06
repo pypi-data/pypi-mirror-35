9216ccc5a1472d66bae5567daa8db9f06aa2b285
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-06 13:54:24 +0000

    Add package django-new-preferences/0.2/django-new-preferences-0.2.tar.gz

---
+++ packages/django-new-preferences/django-new-preferences-0.2.tar.gz/django-new-preferences-0.2/setup.py
@@ -0,0 +1,30 @@
+from setuptools import setup, find_packages
+
+setup(
+    name="django-new-preferences",
+    version="0.2",
+    description="Django app allowing users to set app specific preferences through the admin interface.",
+    long_description=open("README.rst", "r").read() + open("AUTHORS.rst", "r").read() + open("CHANGELOG.rst", "r").read(),
+    author="Praekelt Consulting",
+    author_email="dev@praekelt.com",
+    license="BSD",
+    test_suite="setuptest.setuptest.SetupTestSuite",
+    url="http://github.com/praekelt/django-new-preferences",
+    packages=find_packages(),
+    include_package_data=True,
+    install_requires=[
+        "django"
+    ],
+    tests_require=[
+        "tox"
+    ],
+    classifiers=[
+        "Programming Language :: Python",
+        "License :: OSI Approved :: BSD License",
+        "Development Status :: 5 - Production/Stable",
+        "Operating System :: OS Independent",
+        "Framework :: Django",
+        "Intended Audience :: Developers",
+        "Topic :: Internet :: WWW/HTTP :: Dynamic Content",
+    ],
+)
