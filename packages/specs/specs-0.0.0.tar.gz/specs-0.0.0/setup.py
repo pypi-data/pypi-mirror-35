48874f21edd32c01ddf390f646e77796482a33f8
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-06 16:16:32 +0000

    Add package specs/0.0.0/specs-0.0.0.tar.gz

---
+++ packages/specs/specs-0.0.0.tar.gz/specs-0.0.0/setup.py
@@ -0,0 +1,19 @@
+from setuptools import setup
+
+
+setup(
+    name='specs',
+    version='0.0.0',
+    author='Armin Ronacher',
+    license='MIT',
+    author_email='armin.ronacher@acrtive-4.com',
+    description='WIP',
+    long_description='WIP',
+    include_package_data=True,
+    zip_safe=False,
+    platforms='any',
+    install_requires=[
+    ],
+    setup_requires=[
+    ]
+)
