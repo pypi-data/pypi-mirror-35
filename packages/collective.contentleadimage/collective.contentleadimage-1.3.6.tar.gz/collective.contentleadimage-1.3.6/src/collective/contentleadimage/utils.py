df37b5926fdff647d5670f8e65d88e3861aadada
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-06 14:57:46 +0000

    Add package collective.contentleadimage/1.3.6/collective.contentleadimage-1.3.6.tar.gz

---
+++ packages/collective.contentleadimage/collective.contentleadimage-1.3.6.tar.gz/collective.contentleadimage-1.3.6/src/collective/contentleadimage/utils.py
@@ -0,0 +1,16 @@
+from collective.contentleadimage.interfaces import ILeadImageable
+from collective.contentleadimage.config import IMAGE_FIELD_NAME
+
+from plone.indexer import indexer
+from zope.component import provideAdapter
+
+
+@indexer(ILeadImageable)
+def hasContentLeadImage(obj):
+    field = obj.getField(IMAGE_FIELD_NAME)
+    if field is not None:
+        value = field.get(obj)
+        return not not value
+    else:
+        raise AttributeError
+provideAdapter(hasContentLeadImage, name='hasContentLeadImage')
