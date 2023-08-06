0624888d8e3c92026c4ea444989742a00172d0e9
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-06 01:55:24 +0000

    Add package my-first-helloworld-script/0.11/my-first-helloworld-script-0.11.tar.gz

---
+++ packages/my-first-helloworld-script/my-first-helloworld-script-0.11.tar.gz/my-first-helloworld-script-0.11/setup.py
@@ -0,0 +1,23 @@
+import setuptools
+
+setuptools.setup(
+    name='my-first-helloworld-script',
+    version='0.11',
+    author="lian zheng",
+    author_email="lianzheng07@163.com",
+    description="This is my first pip package",
+    long_description=open('README.md').read(),
+    long_description_content_type="text/markdown",
+    url="https://github.com/LeonLian/pipPackagingTest.git",
+    # packages=setuptools.find_packages(),
+    scripts=['helloworld.py']
+)
+
+
+
+
+
+
+
+
+
