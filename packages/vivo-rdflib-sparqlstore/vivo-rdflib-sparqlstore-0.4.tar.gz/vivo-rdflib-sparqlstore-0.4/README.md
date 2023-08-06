4f440ba696f0bf2b2eb34a39b694d3ccd4d7610c
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-06 13:54:59 +0000

    Add package vivo-rdflib-sparqlstore/0.4/vivo-rdflib-sparqlstore-0.4.tar.gz

---
+++ packages/vivo-rdflib-sparqlstore/vivo-rdflib-sparqlstore-0.4.tar.gz/vivo-rdflib-sparqlstore-0.4/README.md
@@ -0,0 +1,82 @@
+## vivo-rdflib-sparqlstore
+
+A RDFLib [SPARQLStore implementation](http://rdflib.readthedocs.org/en/latest/apidocs/rdflib.plugins.stores.html#rdflib.plugins.stores.sparqlstore.SPARQLUpdateStore) for the [VIVO](http://vivoweb.org/) [SPARQL query and update](https://wiki.duraspace.org/display/VIVODOC110x/VIVO+APIs) APIs.
+
+This library supports Python 2.7 and 3.5+.
+
+The VIVO SPARQL APIs require a user email and password for authentication.
+
+See [RDFLib's docs](http://rdflib.readthedocs.org/en/latest/apidocs/rdflib.plugins.stores.html?highlight=sparqlstore#module-rdflib.plugins.stores.sparqlstore) for detailed documentation on the SPARQLStore implementation.
+
+[![Build Status](https://travis-ci.org/lawlesst/vivo-rdflib-sparqlstore.svg?branch=master)](https://travis-ci.org/lawlesst/vivo-rdflib-sparqlstore)
+
+## Install
+
+```
+pip install vivo-rdflib-sparqlstore
+```
+
+For the latest development version, install directly from Github.
+
+```
+pip install git+https://github.com/lawlesst/rdflib-vivo-sparqlstore.git
+```
+
+## API Usage
+
+See [example.py](./example.py) for example usage.
+
+### basic usage
+```
+from vstore import VIVOUpdateStore, VIVOStore
+from rdflib.namespace import SKOS, RDF
+from rdflib.graph import Dataset
+
+#Define the VIVO store
+query_endpoint = 'http://localhost:8983/vivo/api/sparqlQuery'
+update_endpoint = 'http://localhost:8983/vivo/api/sparqlUpdate'
+
+#Setup the VIVO store
+store = VIVOUpdateStore('vivo_root@school.edu', 'passwd')
+store.open((query_endpoint, update_endpoint))
+
+ds = Dataset(store=store)
+
+#All skos:Concepts in the store.
+for subj in ds.subjects(predicate=RDF.type, object=SKOS.Concept):
+    print 'Concept: ', subj
+```
+
+With a default VIVO installation, several URIS for skos:Concepts should be included, e.g.:
+
+```
+Concept:  http://vivoweb.org/ontology/core#yearMonthPrecision
+Concept:  http://vivoweb.org/ontology/core#yearPrecision
+Concept:  http://vivoweb.org/ontology/core#yearMonthDayTimePrecision
+Concept:  http://vivoweb.org/ontology/core#yearMonthDayPrecision
+```
+
+## Command line tool
+A command line tool is included for adding or removing files of triples to/from VIVO. This can be a quick way to insert data into VIVO. 
+
+```
+$ vivoUpdate --help
+Usage: vivoUpdate [OPTIONS] TRIPLE_FILE ACTION
+
+  Pass in the file to add or remove and the action 'add' or 'remove'.
+
+Options:
+  --email TEXT        [required]
+  --password TEXT     [required]
+  --url TEXT          [required]
+  --named_graph TEXT
+  --help              Show this message and exit.
+```
+
+The email, password, and url options will be read from environment variables if set.
+
+```
+$ export VIVO_EMAIL=you@example.org
+$ export VIVO_PASSWORD=xxxx
+$ export VIVO_URL=http://vivo.example.org/
+```
---
+++ packages/vivo-rdflib-sparqlstore/vivo-rdflib-sparqlstore-0.4.tar.gz/vivo-rdflib-sparqlstore-0.4/vstore/graph_utils.py
@@ -0,0 +1,108 @@
+from itertools import islice
+
+from rdflib import ConjunctiveGraph, URIRef, Graph
+
+import logging
+logger = logging.getLogger(__name__)
+
+# Size of graphs to post by default.
+DEFAULT_CHUNK_SIZE = 8000
+
+DEFAULT_GRAPH = "http://vitro.mannlib.cornell.edu/default/vitro-kb-2"
+
+
+class VIVOUtilsGraph(ConjunctiveGraph):
+
+    @staticmethod
+    def make_batch(size, graph):
+        """
+        Split graphs into n sized chunks.
+        See: http://stackoverflow.com/a/1915307/758157
+
+        :param size: int
+        :param graph: graph
+        :return: graph
+        """
+        i = iter(graph)
+        chunk = list(islice(i, size))
+        while chunk:
+            yield chunk
+            chunk = list(islice(i, size))
+
+    def nt_yielder(self, graph, size):
+        """
+        Yield n sized ntriples for a given graph.
+        Used in sending chunks of data to the VIVO
+        SPARQL API.
+        """
+        for grp in self.make_batch(size, graph):
+            tmpg = Graph()
+            # Add statements as list to tmp graph
+            tmpg += grp
+            yield (len(tmpg), tmpg.serialize(format='nt'))
+
+    def bulk_update(self, named_graph, graph, size, is_add=True):
+        """
+        Bulk adds or deletes. Triples are chunked into n size groups before
+        sending to API. This prevents the API endpoint from timing out.
+        """
+        context = URIRef(named_graph)
+        total = len(graph)
+        if total > 0:
+            for set_size, nt in self.nt_yielder(graph, size):
+                if is_add is True:
+                    logger.info("Adding {} statements to <{}>.".format(set_size, named_graph))
+                    self.update(u'INSERT DATA { GRAPH %s { %s } }' % (context.n3(), nt.decode('utf-8')))
+                else:
+                    logger.info("Removing {} statements from <{}>.".format(set_size, named_graph))
+                    self.update(u'DELETE DATA { GRAPH %s { %s } }' % (context.n3(), nt.decode('utf-8')))
+        return total
+
+    def bulk_add(self, named_graph, add, size=DEFAULT_CHUNK_SIZE):
+        """
+        Add batches of statements in n-sized chunks.
+        """
+        return self.bulk_update(named_graph, add, size)
+
+    def bulk_remove(self, named_graph, add, size=DEFAULT_CHUNK_SIZE):
+        """
+        Remove batches of statements in n-sized chunks.
+        """
+        return self.bulk_update(named_graph, add, size, is_add=False)
+
+    def merge_uris(self, uri1, uri2, graph=DEFAULT_GRAPH):
+        """
+        Generate statements to merge two URIS in a specified graph.
+        """
+        rq = """
+        CONSTRUCT {
+            ?uri ?p ?o .
+            ?other ?p2 ?uri .
+        }
+        WHERE {
+          GRAPH ?g {
+            {
+                ?uri ?p ?o
+            } UNION {
+                ?other ?p2 ?uri
+            }
+          }
+        }
+        """
+        for var in [uri1, uri2, graph]:
+            assert type(var) == URIRef
+        addg = Graph()
+        removeg = Graph()
+        rsp2 = self.query(rq, initBindings=dict(uri=uri2, g=graph))
+
+        # reassign triples were the merged uri is the subject
+        for pred, obj in rsp2.graph.predicate_objects(subject=uri2):
+            addg.add((uri1, pred, obj))
+            removeg.add((uri2, pred, obj))
+
+        # reassign triples were the merged uri is the object
+        for subj, pred in rsp2.graph.subject_predicates(object=uri2):
+            addg.add((subj, pred, uri1))
+            removeg.add((subj, pred, uri2))
+
+        return addg, removeg
