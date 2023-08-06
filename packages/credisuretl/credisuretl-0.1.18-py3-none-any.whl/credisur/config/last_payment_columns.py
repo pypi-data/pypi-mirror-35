de32c9bcbc1068359b376536bf2bf0e4904e8c17
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-06 14:54:23 +0000

    Add package credisuretl/0.1.18/credisuretl-0.1.18-py3-none-any.whl

---
+++ packages/credisuretl/credisuretl-0.1.18-py3-none-any.whl/credisur/config/last_payment_columns.py
@@ -0,0 +1,7 @@
+def get_last_payment_columns():
+    config_list = list()
+    config_list.append(("A", "Ciudad", 'city'))
+    config_list.append(("B", "Cliente", 'customer'))
+    config_list.append(("C", "Dirección", 'address'))
+    config_list.append(("D", "Motivo", 'reason'))
+    return config_list
