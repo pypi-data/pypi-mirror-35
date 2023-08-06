b9593f9a8e244129372f44edc2363bfc25a00714
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-06 01:56:15 +0000

    Add package rupdater/1.1.0a0/rupdater-1.1.0a0.tar.gz

---
+++ packages/rupdater/rupdater-1.1.0a0.tar.gz/rupdater-1.1.0a0/README.md
@@ -0,0 +1,7 @@
+# rupdater
+
+rupdater is a super lightweight and dependency-free updater, that provide
+remote version parsing, downloading and getting hash sum of an update.
+
+## Installation
+
---
+++ packages/rupdater/rupdater-1.1.0a0.tar.gz/rupdater-1.1.0a0/setup.py
@@ -0,0 +1,23 @@
+#!/usr/bin/env python3
+# -*- coding: utf-8 -*-
+
+from setuptools import setup
+
+
+setup(
+    name='rupdater',
+    version='1.1.0a0',
+    description='Lightweight dependency free update software',
+    author='Ranx',
+    author_email='mod34@yandex.ru',
+    url='https://github.com/r4nx/rupdater',
+    py_modules=['rupdater'],
+    keywords=['update', 'updater'],
+    python_requires='>=3.5',
+    classifiers=[
+        'Development Status :: 3 - Alpha',
+        'Intended Audience :: Developers',
+        'Programming Language :: Python :: 3.7',
+        'Topic :: Software Development :: Libraries'
+    ]
+)
