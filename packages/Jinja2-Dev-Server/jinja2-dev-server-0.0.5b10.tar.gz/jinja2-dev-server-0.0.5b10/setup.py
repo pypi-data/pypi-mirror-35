80bc15f628d495642c3351147b5f7c427a1e37db
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-06 13:22:13 +0000

    Add package Jinja2-Dev-Server/0.0.5b10/jinja2-dev-server-0.0.5b10.tar.gz

---
+++ packages/Jinja2-Dev-Server/jinja2-dev-server-0.0.5b10.tar.gz/jinja2-dev-server-0.0.5b10/setup.py
@@ -0,0 +1,19 @@
+import setuptools
+from distutils.core import setup
+
+setup(
+    name='jinja2-dev-server',
+    version="0.0.5b10",
+    author="Eamonn Nugent",
+    author_email="eamonn.nugent@demilletech.net",
+    packages=['jinja2_dev_server'],
+    license='MIT',
+    long_description=open('README.md').read(),
+    long_description_content_type="text/markdown",
+    install_requires=[
+        'flask',
+    ],
+    entry_points={
+        'console_scripts': ['jinja2-dev-server=jinja2_dev_server.command_line:main'],
+    }
+)
