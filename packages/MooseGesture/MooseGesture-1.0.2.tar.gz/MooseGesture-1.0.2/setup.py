9b5dc072b3bf1ec5f9c03ee9abde744c9748310a
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-06 13:54:06 +0000

    Add package MooseGesture/1.0.2/MooseGesture-1.0.2.tar.gz

---
+++ packages/MooseGesture/MooseGesture-1.0.2.tar.gz/MooseGesture-1.0.2/setup.py
@@ -0,0 +1,51 @@
+import re
+import os
+from setuptools import setup
+
+
+# Load version from module (without loading the whole module)
+with open('moosegesture/__init__.py', 'r') as fd:
+    version = re.search(r'^__version__\s*=\s*[\'"]([^\'"]*)[\'"]',
+                        fd.read(), re.MULTILINE).group(1)
+
+here = os.path.abspath(os.path.dirname(__file__))
+
+# Get the long description from the README file
+with open(os.path.join(here, 'README.rst'), encoding='utf-8') as f:
+    long_description = f.read()
+
+setup(
+    name='MooseGesture',
+    version=version,
+    url='https://github.com/asweigart/moosegesture',
+    author='Al Sweigart',
+    author_email='al@inventwithpython.com',
+    description=('A mouse gesture recognition module for Python.'),
+    license='BSD',
+    long_description=long_description,
+    packages=['moosegesture'],
+    test_suite='tests',
+    keywords="mouse gesture",
+    classifiers=[
+        'Development Status :: 3 - Alpha',
+        'Environment :: Win32 (MS Windows)',
+        'Environment :: X11 Applications',
+        'Environment :: MacOS X',
+        'Intended Audience :: Developers',
+        'License :: OSI Approved :: BSD License',
+        'Operating System :: OS Independent',
+        'Programming Language :: Python',
+        'Programming Language :: Python :: 2',
+        'Programming Language :: Python :: 2.5',
+        'Programming Language :: Python :: 2.6',
+        'Programming Language :: Python :: 2.7',
+        'Programming Language :: Python :: 3',
+        'Programming Language :: Python :: 3.1',
+        'Programming Language :: Python :: 3.2',
+        'Programming Language :: Python :: 3.3',
+        'Programming Language :: Python :: 3.4',
+        'Programming Language :: Python :: 3.5',
+        'Programming Language :: Python :: 3.6',
+        'Programming Language :: Python :: 3.7',
+    ],
+)
