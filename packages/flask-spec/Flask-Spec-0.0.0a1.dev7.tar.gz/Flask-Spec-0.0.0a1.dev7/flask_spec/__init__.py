d8f84e9e34c7ae70ab3837ce337e8368dd80c09d
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-06 14:57:31 +0000

    Add package flask-spec/0.0.0a1.dev7/Flask-Spec-0.0.0a1.dev7.tar.gz

---
+++ packages/flask-spec/Flask-Spec-0.0.0a1.dev7.tar.gz/Flask-Spec-0.0.0a1.dev7/flask_spec/__init__.py
@@ -0,0 +1,11 @@
+# -*- coding: utf-8 -*-
+from .extensions import SpecBlueprint
+from .registars import FlaskSpec
+
+
+__version__ = '0.0.0a1.dev7'
+
+__all__ = [
+    'SpecBlueprint',
+    'FlaskSpec',
+]
