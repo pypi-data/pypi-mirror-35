49dcaf5e7f47e005efefad4e83eceef4812497c5
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-06 13:53:55 +0000

    Add package imagesize/1.1.0/imagesize-1.1.0.tar.gz

---
+++ packages/imagesize/imagesize-1.1.0.tar.gz/imagesize-1.1.0/test/test_get.py
@@ -0,0 +1,39 @@
+import unittest
+import os
+import sys
+sys.path.insert(0, os.path.dirname(os.path.dirname(__file__)))
+import imagesize
+
+imagedir = os.path.join(os.path.dirname(__file__), "images")
+
+
+class GetTest(unittest.TestCase):
+    def test_load_png(self):
+        width, height = imagesize.get(os.path.join(imagedir, "test.png"))
+        self.assertEqual(width, 802)
+        self.assertEqual(height, 670)
+
+    def test_load_jpeg(self):
+        width, height = imagesize.get(os.path.join(imagedir, "test.jpg"))
+        self.assertEqual(width, 802)
+        self.assertEqual(height, 670)
+
+    def test_load_jpeg2000(self):
+        width, height = imagesize.get(os.path.join(imagedir, "test.jp2"))
+        self.assertEqual(width, 802)
+        self.assertEqual(height, 670)
+
+    def test_load_gif(self):
+        width, height = imagesize.get(os.path.join(imagedir, "test.gif"))
+        self.assertEqual(width, 802)
+        self.assertEqual(height, 670)
+
+    def test_bigendien_tiff(self):
+        width, height = imagesize.get(os.path.join(imagedir, "test.tiff"))
+        self.assertEqual(width, 802)
+        self.assertEqual(height, 670)
+
+    def test_littleendien_tiff(self):
+        width, height = imagesize.get(os.path.join(imagedir, "multipage_tiff_example.tif"))
+        self.assertEqual(width, 800)
+        self.assertEqual(height, 600)
