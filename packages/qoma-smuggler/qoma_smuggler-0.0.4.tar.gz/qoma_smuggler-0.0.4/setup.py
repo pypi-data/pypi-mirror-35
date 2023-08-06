d2a6ab1c541bb8aa5304267bece10814a42ce7c5
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-06 01:57:03 +0000

    Add package qoma-smuggler/0.0.4/qoma_smuggler-0.0.4.tar.gz

---
+++ packages/qoma-smuggler/qoma_smuggler-0.0.4.tar.gz/qoma_smuggler-0.0.4/setup.py
@@ -0,0 +1,18 @@
+from setuptools import setup
+
+setup(name='qoma_smuggler',
+        scripts=['scripts/smuggler_test.py'],    
+        version='0.0.4',
+        py_modules= ['qoma_smuggler'],
+        description='Transport Data and Commands Across the FAME / Python Border',
+        long_description='Transport data and commands across the FAME / Python border. '+
+        'A set of utilities for: reading FAME databases into R; writing '+
+        'Python data into FAME databases; executing FAME commands in the Python environment; and, executing ' +
+        'Python commands in the FAME environment.',
+        install_requires=['numpy>=1.13.3','pandas>=0.23.4','pyhli>=0.0.11'],
+        author='Qoma LLC',
+        author_email='info@qoma.io',
+        url='http://github.com/qomaio',
+        license='AGPLv3',
+        platforms=['any'],
+        include_package_data=True)
