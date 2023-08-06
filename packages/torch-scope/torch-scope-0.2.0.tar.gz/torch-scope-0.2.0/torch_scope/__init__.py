8fecd10057e935d69d79eb041174615011d47dbc
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-06 16:18:56 +0000

    Add package torch-scope/0.2.0/torch-scope-0.2.0.tar.gz

---
+++ packages/torch-scope/torch-scope-0.2.0.tar.gz/torch-scope-0.2.0/torch_scope/__init__.py
@@ -0,0 +1,8 @@
+__author__ = "Liyuan Liu"
+
+__license__ = "Apache License 2.0"
+__maintainer__ = "Liyuan Liu"
+__email__ = "llychinalz@gmail.com"
+
+from torch_scope.wrapper import wrapper
+from torch_scope.wrapper import basic_wrapper
