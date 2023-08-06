a218dad7dbd9f59f8c1b28fe6caa2af256672355
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-06 14:58:22 +0000

    Add package politico-civic-election-night/0.11.3/politico_civic_election_night-0.11.3-py3-none-any.whl

---
+++ packages/politico-civic-election-night/politico_civic_election_night-0.11.3-py3-none-any.whl/electionnight/management/commands/methods/bootstrap/executive_office.py
@@ -0,0 +1,39 @@
+from django.contrib.contenttypes.models import ContentType
+
+from electionnight.models import PageContent, PageType
+
+from .executive_office_states import ExecutiveOfficeStates
+
+
+class ExecutiveOffice(ExecutiveOfficeStates):
+    def bootstrap_executive_office(self, election):
+        """
+        For executive offices, create page content for the office.
+
+        For the president, create pages for each state result.
+        """
+        division = election.race.office.jurisdiction.division
+        content_type = ContentType.objects.get_for_model(election.race.office)
+        PageContent.objects.get_or_create(
+            content_type=content_type,
+            object_id=election.race.office.pk,
+            election_day=election.election_day,
+            division=division
+        )
+        if division.level == self.NATIONAL_LEVEL:
+            self.bootstrap_executive_office_states(election)
+        else:
+            # Create state governor page type
+            page_type, created = PageType.objects.get_or_create(
+                model_type=ContentType.objects.get(
+                    app_label='government',
+                    model='office'
+                ),
+                election_day=election.election_day,
+                division_level=self.STATE_LEVEL,
+            )
+            PageContent.objects.get_or_create(
+                content_type=ContentType.objects.get_for_model(page_type),
+                object_id=page_type.pk,
+                election_day=election.election_day,
+            )
