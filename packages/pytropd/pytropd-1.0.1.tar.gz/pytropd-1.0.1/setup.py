82ec4129b87cb087c5a923e53d959e34f7b5d816
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-06 14:54:02 +0000

    Add package pytropd/1.0.1/pytropd-1.0.1.tar.gz

---
+++ packages/pytropd/pytropd-1.0.1.tar.gz/pytropd-1.0.1/setup.py
@@ -0,0 +1,20 @@
+#!/usr/bin/env python
+
+# PyTropD installation script
+from setuptools import setup
+from os import path
+
+setup (name="pytropd",
+	version="1.0.1",
+        description = "Calculation of metrics of tropical width",
+	long_description = """
+PyTropD is a software package designed to calculate various metrics of tropical width and is the python equivalent of the Matlab package,TropD""",
+        license = "GPL-3",
+        author="Alison Ming, Paul William Staten",
+        author_email="admg26@gmail.com",
+        url="https://tropd.github.io/pytropd/index.html",
+	requires=['numpy','matplotlib','scipy'],
+	packages=["pytropd"],
+        classifiers=["Programming Language :: Python :: 2"],
+)
+
