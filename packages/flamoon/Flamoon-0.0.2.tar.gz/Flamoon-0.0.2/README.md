abc99166e186b5f14213af13c60d0273198fbfa9
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-06 13:21:58 +0000

    Add package flamoon/0.0.2/Flamoon-0.0.2.tar.gz

---
+++ packages/flamoon/Flamoon-0.0.2.tar.gz/Flamoon-0.0.2/README.md
@@ -0,0 +1,7 @@
+# flamoon
+
+[![Gitter flamoon/Lobby](https://badges.gitter.im/flamoon/Lobby.svg)](https://gitter.im/japronto/Lobby)
+[![PyPI](https://img.shields.io/pypi/v/flamoon.svg)](https://pypi.python.org/pypi/flamoon)
+[![PyPI version](https://img.shields.io/pypi/pyversions/flamoon.svg)](https://pypi.python.org/pypi/flamoon/)
+
+Надстройка над Flask, для быстрой и удобной web-разработки.
---
+++ packages/flamoon/Flamoon-0.0.2.tar.gz/Flamoon-0.0.2/setup.py
@@ -0,0 +1,58 @@
+# setup.py
+
+# Фомирует необходимые данные для установки пакета flamoon
+
+# :::::::::::::::::::::::::::::::::::::::::::::::::::::::::::::::::::::::::
+
+from collections import OrderedDict
+from setuptools import setup, find_packages
+
+# :::::::::::::::::::::::::::::::::::::::::::::::::::::::::::::::::::::::::
+
+PACKAGE = 'flamoon'
+VERSION = __import__(PACKAGE).__version__
+NAME = 'Flamoon'
+DESCRIPTION = 'Надстройка над Flask, для быстрой и удобной web-разработки.'
+AUTHOR = 'volitilov'
+AUTHOR_EMAIL = 'volitilov@gmail.com'
+URL = 'https://github.com/volitilov/flamoon'
+
+
+with open("README.md", "r") as fh:
+    README = fh.read()
+    
+
+setup(
+    name=NAME, 
+    version=VERSION, 
+    description=DESCRIPTION, 
+    long_description=README,
+    long_description_content_type="text/markdown",
+    classifiers=[
+        'Intended Audience :: Developers',
+        'Natural Language :: Russian',
+        'Development Status :: 1 - Planning',
+        'License :: OSI Approved :: MIT License', 
+        'Programming Language :: Python :: 3.5',
+        'Topic :: Software Development :: Libraries :: Application Frameworks',
+        'Operating System :: POSIX :: Linux'],  
+    url=URL, 
+    project_urls=OrderedDict((
+        ('Documentation', '#'),
+        ('Code', URL),
+        ('Issue tracker', URL+'/issues'),
+    )),
+    author=AUTHOR, 
+    author_email=AUTHOR_EMAIL, 
+    license='MIT', 
+    packages=find_packages(), 
+    install_requires=['Flask==1.0.2', ], 
+    include_package_data=True,
+    zip_safe=False,
+    test_suite='tests/test_basic',
+    entry_points={
+        'console_scripts': [
+            'flamoon = flamoon.cli:main',
+        ],
+    },
+)
