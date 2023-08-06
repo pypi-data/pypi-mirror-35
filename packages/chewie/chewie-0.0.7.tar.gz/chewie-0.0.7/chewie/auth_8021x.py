70de578abb7ee333d2740b7270aa1b3c8195f0bd
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-06 15:30:37 +0000

    Add package chewie/0.0.7/chewie-0.0.7.tar.gz

---
+++ packages/chewie/chewie-0.0.7.tar.gz/chewie-0.0.7/chewie/auth_8021x.py
@@ -0,0 +1,30 @@
+import struct
+
+def hwaddr_to_string(hwaddr):
+    return ":".join(["%02x" % x for x in hwaddr])
+
+AUTH_8021X_HEADER_LENGTH = 1 + 1 + 2
+
+class Auth8021x(object):
+    def __init__(self, version, packet_type, data):
+        self.version = version
+        self.packet_type = packet_type
+        self.data = data
+
+    @classmethod
+    def parse(cls, packed_message):
+        version, packet_type, length = struct.unpack("!BBH", packed_message[:AUTH_8021X_HEADER_LENGTH])
+        data = packed_message[AUTH_8021X_HEADER_LENGTH:AUTH_8021X_HEADER_LENGTH+length]
+        return cls(version, packet_type, data)
+
+    def pack(self):
+        header = struct.pack("!BBH", self.version, self.packet_type, len(self.data))
+        return header + self.data
+
+    def __repr__(self):
+        return "%s(version=%s, packet_type=%s, data=%s)" % \
+            (self.__class__.__name__, self.version, self.packet_type, self.data)
+
+    def __str__(self):
+        return "%s<packet_type=%d, data=%s>" % \
+            (self.__class__.__name__, self.packet_type, self.data)
---
+++ packages/chewie/chewie-0.0.7.tar.gz/chewie-0.0.7/chewie/event.py
@@ -0,0 +1,49 @@
+class Event(object):
+    TIMER_EXPIRED = 1
+    MESSAGE_RECEIVED = 2
+    SHUTDOWN = 3
+    PORT_ENABLED = 4
+    PORT_DISABLED = 5
+
+
+class EventTimerExpired(Event):
+    def __init__(self, state_machine=None, sent_count=None):
+        # will work but please do this properly
+        self.type = self.TIMER_EXPIRED
+        self.state_machine = state_machine
+        self.sent_count = sent_count
+
+
+class EventMessageReceived(Event):
+    def __init__(self, message, port_id):
+        # will work but please do this properly
+        self.type = self.MESSAGE_RECEIVED
+        self.message = message
+        self.port_id = port_id
+
+
+class EventPortStatusChange(Event):
+
+    def __init__(self, port_status):
+        """
+        Args:
+            port_status (bool): True if port is enabled, False otherwise.
+        """
+        self.port_status = port_status
+        if port_status:
+            self.type = self.PORT_ENABLED
+        else:
+            self.type = self.PORT_DISABLED
+
+
+class EventRadiusMessageReceived(EventMessageReceived):
+
+    def __init__(self, message, state):
+        super().__init__(message, None)
+        self.state = state
+
+
+class EventShutdown(Event):
+    def __init__(self):
+        # will work but please do this properly
+        self.type = self.SHUTDOWN
