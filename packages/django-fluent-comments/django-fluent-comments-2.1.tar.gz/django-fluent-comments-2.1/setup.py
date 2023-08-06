42ca7eb3b5d57678c17e48f9a6b350002a27e5e0
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-06 01:55:37 +0000

    Add package django-fluent-comments/2.1/django-fluent-comments-2.1.tar.gz

---
+++ packages/django-fluent-comments/django-fluent-comments-2.1.tar.gz/django-fluent-comments-2.1/setup.py
@@ -0,0 +1,93 @@
+#!/usr/bin/env python
+from __future__ import print_function
+from setuptools import setup, find_packages
+from os import path
+import codecs
+import os
+import re
+import sys
+
+
+# When creating the sdist, make sure the django.mo file also exists:
+if 'sdist' in sys.argv or 'develop' in sys.argv:
+    os.chdir('fluent_comments')
+    try:
+        from django.core import management
+        management.call_command('compilemessages', stdout=sys.stderr, verbosity=1)
+    except ImportError:
+        if 'sdist' in sys.argv:
+            raise
+    finally:
+        os.chdir('..')
+
+
+def read(*parts):
+    file_path = path.join(path.dirname(__file__), *parts)
+    return codecs.open(file_path, encoding='utf-8').read()
+
+
+def find_version(*parts):
+    version_file = read(*parts)
+    version_match = re.search(r"^__version__ = ['\"]([^'\"]*)['\"]", version_file, re.M)
+    if version_match:
+        return str(version_match.group(1))
+    raise RuntimeError("Unable to find version string.")
+
+
+setup(
+    name='django-fluent-comments',
+    version=find_version('fluent_comments', '__init__.py'),
+    license='Apache 2.0',
+
+    install_requires = [
+        'django-crispy-forms>=1.1.1',
+        'django-tag-parser>=3.1',
+        'django-contrib-comments>=1.8',
+        'python-akismet>=0.4.1',  # Python 3 port, replaces Python 2-only "akismet" library.
+    ],
+    requires=[
+        'Django (>=1.8)',
+    ],
+    tests_require = [
+        'mock',
+    ],
+    extras_require = {
+        'threadedcomments': ['django-threadedcomments>=1.1'],
+    },
+    description='A modern, ajax-based appearance for django_comments',
+    long_description=read('README.rst'),
+
+    author='Diederik van der Boor',
+    author_email='opensource@edoburu.nl',
+
+    url='https://github.com/edoburu/django-fluent-comments',
+    download_url='https://github.com/edoburu/django-fluent-comments/zipball/master',
+
+    packages=find_packages(exclude=('example*',)),
+    include_package_data=True,
+
+    zip_safe=False,
+    classifiers=[
+        'Development Status :: 5 - Production/Stable',
+        'Environment :: Web Environment',
+        'Framework :: Django',
+        'Intended Audience :: Developers',
+        'License :: OSI Approved :: Apache Software License',
+        'Operating System :: OS Independent',
+        'Programming Language :: Python',
+        'Programming Language :: Python :: 2.7',
+        'Programming Language :: Python :: 3.4',
+        'Programming Language :: Python :: 3.5',
+        'Programming Language :: Python :: 3.6',
+        'Framework :: Django',
+        'Framework :: Django :: 1.8',
+        'Framework :: Django :: 1.9',
+        'Framework :: Django :: 1.10',
+        'Framework :: Django :: 1.11',
+        'Framework :: Django :: 2.0',
+        'Topic :: Internet :: WWW/HTTP',
+        'Topic :: Internet :: WWW/HTTP :: Dynamic Content',
+        'Topic :: Software Development :: Libraries :: Application Frameworks',
+        'Topic :: Software Development :: Libraries :: Python Modules',
+    ]
+)
