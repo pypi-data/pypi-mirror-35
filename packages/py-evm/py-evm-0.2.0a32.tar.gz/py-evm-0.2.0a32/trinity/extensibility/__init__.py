a99633e03987634d9ef34a6d318b0213b32fe651
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-06 14:28:55 +0000

    Add package py-evm/0.2.0a32/py-evm-0.2.0a32.tar.gz

---
+++ packages/py-evm/py-evm-0.2.0a32.tar.gz/py-evm-0.2.0a32/trinity/extensibility/__init__.py
@@ -0,0 +1,16 @@
+from trinity.extensibility.events import (  # noqa: F401
+    BaseEvent
+)
+from trinity.extensibility.plugin import (  # noqa: F401
+    BasePlugin,
+    BaseIsolatedPlugin,
+    DebugPlugin,
+    PluginContext,
+    PluginProcessScope,
+)
+from trinity.extensibility.plugin_manager import (  # noqa: F401
+    MainAndIsolatedProcessScope,
+    PluginManager,
+    ManagerProcessScope,
+    SharedProcessScope,
+)
---
+++ packages/py-evm/py-evm-0.2.0a32.tar.gz/py-evm-0.2.0a32/trinity/protocol/common/handlers.py
@@ -0,0 +1,44 @@
+from abc import ABC, abstractmethod
+from typing import (
+    Any,
+    Dict,
+    Type,
+)
+
+from p2p.peer import BasePeer
+
+from trinity.protocol.common.exchanges import (
+    BaseExchange,
+)
+from trinity.protocol.common.managers import (
+    ExchangeManager,
+)
+
+
+class BaseExchangeHandler(ABC):
+    @property
+    @abstractmethod
+    def _exchange_config(self) -> Dict[str, Type[BaseExchange[Any, Any, Any]]]:
+        pass
+
+    def __init__(self, peer: BasePeer) -> None:
+        self._peer = peer
+
+        for attr, exchange_cls in self._exchange_config.items():
+            if hasattr(self, attr):
+                raise AttributeError(
+                    "Unable to set manager on attribute `{0}` which is already "
+                    "present on the class: {1}".format(attr, getattr(self, attr))
+                )
+            manager: ExchangeManager[Any, Any, Any]
+            manager = ExchangeManager(self._peer, exchange_cls.response_cmd_type, peer.cancel_token)
+            exchange = exchange_cls(manager)
+            setattr(self, attr, exchange)
+
+    def get_stats(self) -> Dict[str, str]:
+        exchanges = tuple(getattr(self, key) for key in self._exchange_config.keys())
+        return {
+            exchange.response_cmd_type.__name__: exchange.tracker.get_stats()
+            for exchange
+            in exchanges
+        }
