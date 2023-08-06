1045e8d15bf8fbba59fff1d50d900f5366774e91
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-06 16:15:48 +0000

    Add package photoarchive/0.4/photoarchive-0.4-py3-none-any.whl

---
+++ packages/photoarchive/photoarchive-0.4-py3-none-any.whl/photoarchive/persist.py
@@ -0,0 +1,58 @@
+from pathlib import Path
+import json
+
+from filedata import Filedata
+
+def ReadDictionary():
+    filedict = dict()
+
+    if (not Path("filedict.json").exists()):
+        return filedict
+
+    with open('filedict.json') as f:
+        tmpArr = json.load(f)
+
+        for item in tmpArr:
+            fdata = Filedata(**item)
+            #print(fdata.filename)
+            filedict[fdata.hash] = fdata
+
+    return filedict
+
+def SaveDictionary(filedict):
+    tmpArr = []
+    for item in filedict.values():
+        tmpArr.append(item.__dict__)
+    
+    with open('filedict.json', 'w') as f:
+        json.dump(tmpArr, f, indent=4)
+
+def FindInDict(fdata, filedict):
+    for item in filedict.values():
+        if (fdata.EqualTo(item)):
+            return True
+
+    return False
+
+def ReadTreatedFiles():
+    treatedfiles = set()
+
+    if (not Path("treatedfiles.txt").exists()):
+        return treatedfiles
+
+    with open('treatedfiles.txt', encoding="utf_8") as f:
+        for line in f:
+            a = line.split(sep=",", maxsplit=1)
+            line = a[0]
+            line = line.rstrip("\n")
+            treatedfiles.add(line)
+
+    return treatedfiles
+
+def SaveTreatedFiles(treatedfiles):    
+    with open('treatedfiles.txt', 'a', encoding="utf_8") as f:
+        for tf in treatedfiles:
+            f.write(tf + "\n")
+
+    print("Saved {} treated files".format(len(treatedfiles)))
+    
---
+++ packages/photoarchive/photoarchive-0.4-py3-none-any.whl/photoarchive/photocopy.py
@@ -0,0 +1,95 @@
+import piexif
+import os
+import datetime
+import shutil
+from pathlib import Path
+
+class PhotoCopy:
+    destroot = "./Bilder"
+
+    @staticmethod
+    def CopyFile(srcpath):
+        dateparts = PhotoCopy.GetDateFromExif(srcpath)
+        if (dateparts is None):
+            dateparts = PhotoCopy.GetDateFromStat(srcpath)
+
+        year, month, day = dateparts
+        destdir = Path(PhotoCopy.destroot).resolve()
+        destdir = destdir / Path(year, month, day)
+        destpath = destdir / Path(srcpath).name
+        print(destpath)
+        if (not destdir.exists()):
+            print("Creating dir: " + str(destdir))
+            destdir.mkdir(parents=True, exist_ok=True)
+        
+        if (destpath.exists()):
+            destpath = PhotoCopy.Uniquify(destpath)
+
+        shutil.copy2(srcpath, destpath)
+
+        return str(destpath)
+
+    @staticmethod
+    def Uniquify(path):
+        i = 0
+        newpath = path
+        while (newpath.exists()):
+            i = i+1
+            file = path.stem + "_" + str(i) + path.suffix
+            newpath = path.parent / file
+
+        print("Creating unique path: " + str(newpath))
+        return newpath
+
+    @staticmethod 
+    def GetDateFromExif(srcpath):
+        try:
+            exif_dict = piexif.load(srcpath)
+        except:
+            return None
+
+        tags = [("Exif", "DateTimeOriginal", 36867),
+                ("Exif", "DateTimeDigitized", 36868),
+                ("0th", "DateTime", 306),
+                ("1st", "DateTime", 306) ]
+
+        for ifd, _, tag in tags:
+            d1 = exif_dict.get(ifd)
+            if (d1 is not None):
+                datestring = exif_dict[ifd].get(tag)
+                if (datestring is not None):
+                    datestring = datestring.decode("utf-8", "strict") 
+                    if (not datestring.startswith("0000")):
+                        print("Date from exif: " + datestring)
+                        return (datestring[0:4], datestring[5:7], datestring[8:10])
+
+        return None
+
+    @staticmethod
+    def GetDateFromStat(srcpath):
+        statinfo = os.stat(srcpath)
+        date = datetime.datetime.fromtimestamp(statinfo.st_ctime)
+        print("Date from file")
+        return (str(date.year), str(date.month).zfill(2), str(date.day).zfill(2))
+
+    @staticmethod
+    def PrintExif(srcpath, all=False):
+        exif_dict = piexif.load(srcpath)
+        for ifd in ("0th", "Exif", "GPS", "1st"):
+            for tag in exif_dict[ifd]:
+                if (all or piexif.TAGS[ifd][tag]["name"].find("Date") >= 0):
+                    print(ifd, piexif.TAGS[ifd][tag]["name"], exif_dict[ifd][tag])
+
+#PrintExif(r"C:\Users\g\Pictures\Gitarr\20130506_004442.jpg")
+
+#PhotoCopy.PrintExif(r"C:\Users\g\Documents\Python\photoarchive\photoarchive\Bilder\2006\12\15\klassfotocroppad.jpg", all=True)
+#allan =  PhotoCopy.CopyFile(r"C:\Users\g\Pictures\Scannat\110-film\2018-03-14_77.TIF")
+# allan = PhotoCopy.Uniquify(Path(r"C:\Users\g\Pictures\Scannat\110-film\2018-03-14_77.TIF"))
+# print(allan)
+
+#print(GetDateFromStat(r"C:\Users\g\Pictures\Scannat\110-film\2018-03-14_77.TIF"))
+# for ifd in ("0th", "Exif", "GPS", "1st"):
+#     for tag in piexif.TAGS[ifd]:
+#         if (str(piexif.TAGS[ifd][tag]["name"]).find("DateTime") != -1):
+#             print(ifd, piexif.TAGS[ifd][tag]["name"], tag)
+
