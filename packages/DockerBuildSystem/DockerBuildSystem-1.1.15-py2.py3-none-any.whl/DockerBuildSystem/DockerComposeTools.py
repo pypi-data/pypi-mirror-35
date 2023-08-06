66107974904bdf1c7544c1028b3184ec8976ed4c
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-06 11:51:47 +0000

    Add package DockerBuildSystem/1.1.15/DockerBuildSystem-1.1.15-py2.py3-none-any.whl

---
+++ packages/DockerBuildSystem/DockerBuildSystem-1.1.15-py2.py3-none-any.whl/DockerBuildSystem/DockerComposeTools.py
@@ -0,0 +1,102 @@
+import yaml
+from DockerBuildSystem import TerminalTools, DockerImageTools
+
+
+def MergeComposeFiles(composeFiles, outputComposeFile):
+    terminalCommand = "docker-compose"
+    terminalCommand += MergeComposeFileToTerminalCommand(composeFiles)
+    terminalCommand += " config>" + outputComposeFile
+    TerminalTools.ExecuteTerminalCommands([terminalCommand], True)
+
+
+def DockerComposeBuild(composeFiles):
+    terminalCommand = "docker-compose"
+    terminalCommand += MergeComposeFileToTerminalCommand(composeFiles)
+    terminalCommand += " build"
+    TerminalTools.ExecuteTerminalCommands([terminalCommand], True)
+
+
+def DockerComposeUp(composeFiles, abortOnContainerExit = True):
+    terminalCommand = "docker-compose"
+    terminalCommand += MergeComposeFileToTerminalCommand(composeFiles)
+    if abortOnContainerExit:
+        terminalCommand += " up --abort-on-container-exit"
+    else:
+        terminalCommand += " up"
+    TerminalTools.ExecuteTerminalCommands([terminalCommand], False)
+
+
+def DockerComposeDown(composeFiles):
+    terminalCommand = "docker-compose"
+    terminalCommand += MergeComposeFileToTerminalCommand(composeFiles)
+    terminalCommand += " down"
+    TerminalTools.ExecuteTerminalCommands([terminalCommand])
+
+
+def DockerComposeRemove(composeFiles):
+    terminalCommand = "docker-compose"
+    terminalCommand += MergeComposeFileToTerminalCommand(composeFiles)
+    terminalCommand += " rm"
+    TerminalTools.ExecuteTerminalCommands([terminalCommand])
+
+
+def DockerComposePush(composeFiles):
+    terminalCommand = "docker-compose"
+    terminalCommand += MergeComposeFileToTerminalCommand(composeFiles)
+    terminalCommand += " push"
+    TerminalTools.ExecuteTerminalCommands([terminalCommand], True)
+
+
+def DockerComposePull(composeFiles):
+    terminalCommand = "docker-compose"
+    terminalCommand += MergeComposeFileToTerminalCommand(composeFiles)
+    terminalCommand += " pull"
+    TerminalTools.ExecuteTerminalCommands([terminalCommand], True)
+
+
+def PublishDockerImages(composeFile):
+    dockerComposeStream = open(composeFile, 'r')
+    dockerComposeMap = yaml.safe_load(dockerComposeStream)
+    for service in dockerComposeMap['services']:
+        sourceImage = dockerComposeMap['services'][service]['image']
+        DockerImageTools.PushImage(sourceImage)
+
+
+def PublishDockerImagesWithNewTag(composeFile, newTag):
+    dockerComposeStream = open(composeFile, 'r')
+    dockerComposeMap = yaml.safe_load(dockerComposeStream)
+    for service in dockerComposeMap['services']:
+        sourceImage = dockerComposeMap['services'][service]['image']
+        tagIndex = sourceImage.rfind(':')
+        targetImage = sourceImage[:tagIndex+1] + str(newTag)
+        DockerImageTools.TagImage(sourceImage, targetImage)
+        DockerImageTools.PushImage(targetImage)
+
+
+def ExecuteComposeTests(composeFiles, testContainerNames, removeTestContainers = True):
+    DockerComposeBuild(composeFiles)
+    DockerComposeUp(composeFiles)
+    exitCode = 0
+    for testContainerName in testContainerNames:
+        exitCode = DockerImageTools.GetContainerExitCode(testContainerName)
+    DockerComposeDown(composeFiles)
+    if removeTestContainers:
+        DockerComposeRemove(composeFiles)
+    if exitCode > 0:
+        raise Exception("Container test '" + testContainerName + "' FAILED!")
+    print(testContainerName + " container test finished with success.")
+
+
+def CreateLocalNetwork(networkName):
+    print("Creating local network: " + networkName)
+    dockerCommand = "docker network create "
+    dockerCommand += "--attachable "
+    dockerCommand += networkName
+    TerminalTools.ExecuteTerminalCommands([dockerCommand])
+
+
+def MergeComposeFileToTerminalCommand(composeFiles):
+    terminalCommand = ""
+    for composeFile in composeFiles:
+        terminalCommand += " -f " + composeFile
+    return terminalCommand
