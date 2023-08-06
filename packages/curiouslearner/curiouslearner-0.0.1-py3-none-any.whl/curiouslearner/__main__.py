6d2866c2bc8c70f7081101978d227a5dd26fa9ea
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-06 13:21:42 +0000

    Add package curiouslearner/0.0.1/curiouslearner-0.0.1-py3-none-any.whl

---
+++ packages/curiouslearner/curiouslearner-0.0.1-py3-none-any.whl/curiouslearner/__main__.py
@@ -0,0 +1,23 @@
+"""
+Module to give intro of Sanyam Khurana (CuriousLearner)
+"""
+
+
+def main():
+    print("""
+class CuriousLearner(object):
+
+    name = 'Sanyam Khurana'
+    nick = 'CuriousLearner'
+    email = 'Sanyam@SanyamKhurana.com'
+    website = 'https://www.SanyamKhurana.com'
+
+    def __who_am_i__(self):
+        print("Open Source Contributor")
+        print("CPython Bug Triager")
+        print("GSoC Mentor in Debian")
+""")
+
+
+if __name__ == "__main__":
+    main()
