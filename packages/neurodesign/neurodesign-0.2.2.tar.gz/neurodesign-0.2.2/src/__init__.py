6666322f1737efc0c262c81bb7604dad0450af0d
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-06 01:53:40 +0000

    Add package neurodesign/0.2.2/neurodesign-0.2.2.tar.gz

---
+++ packages/neurodesign/neurodesign-0.2.2.tar.gz/neurodesign-0.2.2/src/__init__.py
@@ -0,0 +1,19 @@
+'''
+This module contains code to find an optimal experimental design for fMRI experiments using the Genetic Algorithm.
+
+.. moduleauthor:: Joke Durnez <joke.durnez@gmail.com>
+
+'''
+
+from neurodesign import design, experiment, optimisation
+
+from .info import (
+    __version__,
+    __author__,
+    __license__,
+    __email__,
+    __status__,
+    __url__,
+    __packagename__,
+
+)
