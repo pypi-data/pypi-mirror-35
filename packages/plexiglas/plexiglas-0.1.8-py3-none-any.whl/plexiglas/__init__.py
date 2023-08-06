d9260cb7dd83a1d2930a14eb7e9c4a680df21ab4
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-06 11:51:16 +0000

    Add package plexiglas/0.1.8/plexiglas-0.1.8-py3-none-any.whl

---
+++ packages/plexiglas/plexiglas-0.1.8-py3-none-any.whl/plexiglas/__init__.py
@@ -0,0 +1,19 @@
+"""
+Plexiglas is a script which helps you to copy your content from a Plex server to any other storage.
+I'm using it especially for copying movies and tv-shows from my main server to a "travel" instance, which is set up
+on my external HDD (WD My Passport Wireless Pro).
+"""
+
+__version__ = '0.1.8'
+
+import logging
+
+try:
+    import keyring
+    from keyring.core import set_keyring
+except ImportError:
+    import keyring_stub as keyring
+    set_keyring = lambda x: None
+
+
+log = logging.getLogger('plexiglas')
