a688d3e94aebea141ccf570a59daabe743d0a438
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-06 17:16:12 +0000

    Add package dpcontracts/0.5.0/dpcontracts-0.5.0.tar.gz

---
+++ packages/dpcontracts/dpcontracts-0.5.0.tar.gz/dpcontracts-0.5.0/setup.py
@@ -0,0 +1,22 @@
+#!/usr/bin/env python
+
+from setuptools import setup
+import dpcontracts
+
+setup(name="dpcontracts",
+      version="0.5.0",
+      author="Rob King",
+      author_email="jking@deadpixi.com",
+      url="https://github.com/deadpixi/contracts",
+      description="A simple implementation of contracts for Python.",
+      py_modules=['dpcontracts'],
+      long_description=dpcontracts.__doc__,
+      license="https://www.gnu.org/licenses/lgpl.txt",
+      classifiers=["Development Status :: 3 - Alpha",
+                   "Intended Audience :: Developers",
+                   "License :: OSI Approved :: GNU Lesser General Public License v3 or later (LGPLv3+)",
+                   "Operating System :: OS Independent",
+                   "Programming Language :: Python",
+                   "Programming Language :: Python :: 2",
+                   "Programming Language :: Python :: 3",
+                   "Topic :: Software Development :: Libraries"])
