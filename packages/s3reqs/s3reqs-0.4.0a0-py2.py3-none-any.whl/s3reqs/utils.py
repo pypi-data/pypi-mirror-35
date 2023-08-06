5a332234065b8eb8056dbe4e34023bb651767802
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-06 07:11:21 +0000

    Add package s3reqs/0.4.0a0/s3reqs-0.4.0a0-py2.py3-none-any.whl

---
+++ packages/s3reqs/s3reqs-0.4.0a0-py2.py3-none-any.whl/s3reqs/utils.py
@@ -0,0 +1,23 @@
+import boto3
+import os
+import sys
+import zipfile
+
+from envs import env
+
+s3 = boto3.client('s3')
+
+
+def download_packages(bucket=None,req_key=None):
+    LOCAL_ENV = env('LOCAL_ENV',True,var_type='boolean')
+    if LOCAL_ENV != True:
+        tmp_folder = '/tmp/s3reqs/'
+        tmp_file = '/tmp/{}'.format(req_key)
+        file_exists = os.path.isfile(tmp_file)
+        if not file_exists:
+            s3.download_file(bucket, req_key, tmp_file)
+            with zipfile.ZipFile(tmp_file,'r') as zip_ref:
+                zip_ref.extractall(tmp_folder)
+        if tmp_folder not in sys.path:
+            sys.path.insert(0,tmp_folder)
+
