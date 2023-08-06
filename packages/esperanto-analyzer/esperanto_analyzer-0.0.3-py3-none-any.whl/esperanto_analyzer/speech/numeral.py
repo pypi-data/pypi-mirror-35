fc47c48f3c2da1dc1a760a933b3d449affac5f22
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-06 14:27:58 +0000

    Add package esperanto-analyzer/0.0.3/esperanto_analyzer-0.0.3-py3-none-any.whl

---
+++ packages/esperanto-analyzer/esperanto_analyzer-0.0.3-py3-none-any.whl/esperanto_analyzer/speech/numeral.py
@@ -0,0 +1,13 @@
+"""
+This class represent one word beloging to grammar class classified as 'Numeral'
+
+What's a Numeral?
+===
+In linguistics, a numeral is a member of a part of speech(word) characterized by the
+designation of numbers;
+"""
+from .word import Word
+
+# pylint: disable=too-few-public-methods,missing-docstring,no-self-use
+class Numeral(Word):
+    pass
