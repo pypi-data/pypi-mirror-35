283419124af26bf77a9249cd4f410e7cf0c22b9e
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-06 04:12:09 +0000

    Add package lalsuite/6.48.1.dev20180828/lalsuite-6.48.1.dev20180828-cp36-cp36m-manylinux1_x86_64.whl

---
+++ packages/lalsuite/lalsuite-6.48.1.dev20180828-cp36-cp36m-manylinux1_x86_64.whl/lalmetaio/__init__.py
@@ -0,0 +1,7 @@
+# Import SWIG wrappings, if available
+from .lalmetaio import *
+
+__version__ = "1.3.1.1"
+
+## \addtogroup lalmetaio_python
+"""This package provides Python wrappings and extensions to LALMetaIO"""
