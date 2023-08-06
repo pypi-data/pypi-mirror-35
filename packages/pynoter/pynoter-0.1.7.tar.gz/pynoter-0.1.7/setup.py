7c93e1f21530a9723e3dcd5274ebb2d8aa7509e1
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-06 04:14:24 +0000

    Add package pynoter/0.1.7/pynoter-0.1.7.tar.gz

---
+++ packages/pynoter/pynoter-0.1.7.tar.gz/pynoter-0.1.7/setup.py
@@ -0,0 +1,22 @@
+from setuptools import setup
+
+def readme():
+    with open('README.rst') as f:
+        return f.read()
+
+setup(name='pynoter',
+      version='0.1.7',
+      description='Powerpoint presentations into org or tex files',
+      long_description='Allows users to convert powerpoint presentations into raw text for editing in latex or org-mode',
+      classifiers=['Programming Language :: Python :: 2.7', 'Topic :: Text Processing :: Markup :: LaTeX'],
+      url='https://github.com/maxrousseau/pynoter',
+      author='Maxime Rousseau',
+      author_email='maximerousseau08@gmail.com',
+      test_suite='nose.collector',
+      tests_require=['nose'],
+      scripts=['bin/py-noter'],
+      include_package_data=True,
+      license='MIT',
+      packages=['pynoter'],
+      install_requires=['python-pptx'],
+      zip_safe=False)
