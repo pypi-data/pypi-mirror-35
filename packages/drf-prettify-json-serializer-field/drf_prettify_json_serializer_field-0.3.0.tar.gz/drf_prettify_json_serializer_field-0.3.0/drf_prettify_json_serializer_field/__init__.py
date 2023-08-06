9c398a412a64a251bf45273896f3427ebf07b3e1
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-06 16:17:38 +0000

    Add package drf-prettify-json-serializer-field/0.3.0/drf_prettify_json_serializer_field-0.3.0.tar.gz

---
+++ packages/drf-prettify-json-serializer-field/drf_prettify_json_serializer_field-0.3.0.tar.gz/drf_prettify_json_serializer_field-0.3.0/drf_prettify_json_serializer_field/__init__.py
@@ -0,0 +1,5 @@
+from drf_prettify_json_serializer_field.fields import PrettifyDataFromJsonField
+from drf_prettify_json_serializer_field.fields import (Charfield,
+                                                       EmailField,
+                                                       DecimalField,
+                                                       IntegerField)
