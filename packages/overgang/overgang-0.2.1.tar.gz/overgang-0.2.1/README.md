e7ed01c18d903ba016f0947ae31c403b95ddf2ca
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-06 13:55:13 +0000

    Add package overgang/0.2.1/overgang-0.2.1.tar.gz

---
+++ packages/overgang/overgang-0.2.1.tar.gz/overgang-0.2.1/README.md
@@ -0,0 +1,18 @@
+
+# overgang -- Markov Models
+
+The package includes 
+
+* Continous-Time Markov model `ctmc_fit`
+
+
+### Installation
+
+It's a pip package at https://pypi.org/project/overgang/
+```
+pip install overgang
+```
+
+
+### Examples
+Check the [examples](examples) folder for notebooks.
---
+++ packages/overgang/overgang-0.2.1.tar.gz/overgang-0.2.1/setup.py
@@ -0,0 +1,14 @@
+from setuptools import setup
+
+setup(name='overgang',
+      version='0.2.1',
+      description='Markov Modeling',
+      url='http://github.com/kmedian/overgang',
+      author='Ulf Hamster',
+      author_email='554c46@gmail.com',
+      license='MIT',
+      packages=['overgang'],
+      install_requires=['numpy', 'datetime', 'scipy'],
+      python_requires='>=3',
+      zip_safe=False)
+
