d8158598b4e9a42f01afcc2c5973d0056635611a
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-06 04:12:32 +0000

    Add package fecfile/0.3.0/fecfile-0.3.0-py3-none-any.whl

---
+++ packages/fecfile/fecfile-0.3.0-py3-none-any.whl/fecfile/__init__.py
@@ -0,0 +1,29 @@
+from . import fecparser
+import requests
+
+
+def loads(input):
+    return fecparser.loads(input)
+
+
+def from_http(file_number):
+    url = 'http://docquery.fec.gov/dcdev/posted/{n}.fec'.format(n=file_number)
+    r = requests.get(url, headers={'User-Agent': 'Mozilla/5.0'})
+    if r.status_code == 404:
+        url = 'http://docquery.fec.gov/paper/posted/{n}.fec'.format(
+            n=file_number
+        )
+        r = requests.get(url, headers={'User-Agent': 'Mozilla/5.0'})
+    return fecparser.loads(r.text)
+
+
+def from_file(file_path):
+    parsed = {}
+    with open(file_path) as file:
+        unparsed = file.read()
+        parsed = fecparser.loads(unparsed)
+    return parsed
+
+
+def print_example(parsed):
+    fecparser.print_example(parsed)
