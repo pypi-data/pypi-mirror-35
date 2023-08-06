4db1e38a0b6543c3bb48fe83a407a9fbc5536d94
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-06 01:54:41 +0000

    Add package gensimplus/4.2/gensimplus-4.2.tar.gz

---
+++ packages/gensimplus/gensimplus-4.2.tar.gz/gensimplus-4.2/setup.py
@@ -0,0 +1,39 @@
+# coding=utf-8
+
+
+from setuptools import setup, find_packages
+
+
+setup(
+        name='gensimplus',
+        version=4.2,
+        description=(
+            'gensim在项目中的公共应用方法'
+        ),
+        long_description=open('README.rst').read(),
+        author='秦海宁',
+        author_email='qinhaining@ultrapower.com.cn',
+        maintainer='秦海宁',
+        maintainer_email='qinhaining@ultrapower.com.cn',
+        license='BSD License',
+        packages=find_packages(),
+        #packages=['source'],
+        package_dir={"":"../gensimplus"},
+        platforms=["all"],
+    url='http://www.github.com/scmsqhn/gensimplus',
+    classifiers=[
+                'Development Status :: 4 - Beta',
+                'Operating System :: OS Independent',
+                'Intended Audience :: Developers',
+                'License :: OSI Approved :: BSD License',
+                'Programming Language :: Python',
+                'Programming Language :: Python :: Implementation',
+                'Programming Language :: Python :: 2',
+                'Programming Language :: Python :: 2.7',
+                'Programming Language :: Python :: 3',
+                'Programming Language :: Python :: 3.4',
+                'Programming Language :: Python :: 3.5',
+                'Programming Language :: Python :: 3.6',
+                'Topic :: Software Development :: Libraries'
+    ],
+)
