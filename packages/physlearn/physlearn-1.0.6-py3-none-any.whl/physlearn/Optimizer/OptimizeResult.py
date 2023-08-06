df226665e5a33ed2fdda7c7270ac7a8b45ae8cbf
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-06 04:08:44 +0000

    Add package physlearn/1.0.6/physlearn-1.0.6-py3-none-any.whl

---
+++ packages/physlearn/physlearn-1.0.6-py3-none-any.whl/physlearn/Optimizer/OptimizeResult.py
@@ -0,0 +1,27 @@
+class OptimizeResult:
+    cost_list = []
+
+    def __init__(self, is_converge, amount_of_iterations, cost_list, exit_code, reason_of_exit, x):
+        self.is_converge = is_converge
+        self.amount_of_iterations = amount_of_iterations
+        self.cost_function = cost_list[-1]
+        self.reason_of_exit = reason_of_exit
+        self.cost_list = cost_list
+        self.exit_code = exit_code
+        self.x = x
+
+    def __repr__(self):
+        is_converge_str = "Is converge: " + str(self.is_converge) + '\n'
+        amount_of_iterations_str = "Amount of iterations: " + str(self.amount_of_iterations) + '\n'
+        cost_function_str = "Reached function value: " + str(self.cost_function) + '\n'
+        reason_of_exit_str = "Reason of break: " + str(self.reason_of_exit) + '\n'
+        result = is_converge_str + amount_of_iterations_str + cost_function_str + reason_of_exit_str
+        return result
+
+    def __str__(self):
+        is_converge_str = "Is converge: " + str(self.is_converge) + '\n'
+        amount_of_iterations_str = "Amount of iterations: " + str(self.amount_of_iterations) + '\n'
+        cost_function_str = "Reached function value: " + str(self.cost_function) + '\n'
+        reason_of_exit_str = "Reason of break: " + str(self.reason_of_exit) + '\n'
+        result = is_converge_str + amount_of_iterations_str + cost_function_str + reason_of_exit_str
+        return result
