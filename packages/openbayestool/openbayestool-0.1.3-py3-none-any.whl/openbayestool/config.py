6ce37d361f4936611c1220d8b3c65b4d284b91f6
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-06 17:10:12 +0000

    Add package openbayestool/0.1.3/openbayestool-0.1.3-py3-none-any.whl

---
+++ packages/openbayestool/openbayestool-0.1.3-py3-none-any.whl/openbayestool/config.py
@@ -0,0 +1,8 @@
+# encoding: utf-8
+
+import os
+
+callback_url = os.getenv('JOB_UPDATE_URL', None)
+uaa_token_url = os.getenv('UAA_TOKEN_URL', 'http://localhost:8080/users/login')
+uaa_username = os.getenv("UAA_USERNAME", "admin")
+uaa_password = os.getenv("UAA_PASSWORD", "123")
