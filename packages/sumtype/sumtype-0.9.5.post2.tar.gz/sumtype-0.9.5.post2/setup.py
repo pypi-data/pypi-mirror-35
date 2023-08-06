0f81413f1a57c8ac2607eba9c919a78c631226e6
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-06 11:51:17 +0000

    Add package sumtype/0.9.5.post2/sumtype-0.9.5.post2.tar.gz

---
+++ packages/sumtype/sumtype-0.9.5.post2.tar.gz/sumtype-0.9.5.post2/setup.py
@@ -0,0 +1,30 @@
+from setuptools import setup, find_packages
+from os import path
+
+
+here = path.abspath(path.dirname(__file__))
+with open(path.join(here, 'README.md'), encoding='utf-8') as f:
+    long_description = f.read()
+
+setup(
+	name     = 'sumtype',
+	version  = '0.9.5.post2',
+	packages = find_packages(exclude=['tests']),
+	install_requires = [
+		'indented',
+		'typing;python_version < "3.5"',
+	],
+	python_requires = '>=3',
+
+	description      = 'A namedtuple-style library for defining immutable sum types in Python.',
+	long_description = long_description,
+	long_description_content_type = "text/markdown",
+	keywords = 'sum variant tagged union enum type types',
+
+	author       = 'J Uryga',
+	author_email = 'lolzatu2@gmail.com',
+	project_urls = {
+		'Source': 'https://github.com/lubieowoce/sumtype',
+	},
+)
+
