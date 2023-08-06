6016e39f43311138186b2d13005a41aeac85f08a
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-06 14:54:55 +0000

    Add package peer2peer/2.1.0/peer2peer-2.1.0.tar.gz

---
+++ packages/peer2peer/peer2peer-2.1.0.tar.gz/peer2peer-2.1.0/setup.py
@@ -0,0 +1,18 @@
+#!/usr/bin/env python
+try:
+    from setuptools import setup
+except ImportError:
+    from distutils.core import setup
+setup(name='peer2peer',
+      version='2.1.0',
+      description='peer 2 peer',
+      py_modules=['peer2peer'],
+      scripts=['peer2peer.py'],
+      license='MIT',
+      platforms='any',
+      install_requires=[
+          'websocket-client',
+          'gevent-websocket',
+          'gevent','future','docopt'
+          ],
+)
