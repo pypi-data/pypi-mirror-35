7c6f09bd1829ba787a4dbc490cea87a57a21dfab
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-06 01:54:33 +0000

    Add package bsmedit/3.2.1/bsmedit-3.2.1.tar.gz

---
+++ packages/bsmedit/bsmedit-3.2.1.tar.gz/bsmedit-3.2.1/readme.md
@@ -0,0 +1,8 @@
+**bsmedit** is a C/C++/SystemC Visualizer.
+
+Full doc: http://bsmedit.feiyilin.com
+
+<img src="/web/images/start_run_plot.gif"></img>
+
+SystemC is licensed under SystemC Open Source License.
+See [here](/systemc-2.3.1/docs/SystemC_Open_Source_License.pdf) for more details.
---
+++ packages/bsmedit/bsmedit-3.2.1.tar.gz/bsmedit-3.2.1/setup.py
@@ -0,0 +1,20 @@
+from setuptools import setup, find_packages
+from bsmedit.version import BSM_VERSION
+setup(name='bsmedit',
+      version=BSM_VERSION,
+      description='C/C++/SystemC Visualizer',
+      author='Tianzhu Qiao',
+      author_email='tq@feiyilin.com',
+      url='http://bsmedit.feiyilin.com',
+      license="MIT",
+      platforms=["any"],
+      packages=find_packages(),
+      include_package_data = True,
+      entry_points={
+          'gui_scripts': [
+              'bsmedit = bsmedit.main:main'
+          ]
+      },
+      install_requires=['wxpython', 'matplotlib', 'numpy', 'click', 'PyOpenGL',
+                        'PyOpenGL_accelerate']
+     )
