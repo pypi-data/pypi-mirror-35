73d3d4e0839110219c1b4af6e173bb374aadfeb7
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-06 14:56:08 +0000

    Add package hutils/0.1.4/hutils-0.1.4.tar.gz

---
+++ packages/hutils/hutils-0.1.4.tar.gz/hutils-0.1.4/setup.py
@@ -0,0 +1,26 @@
+# -*- coding: utf-8 -*-
+import pypandoc
+from setuptools import setup
+
+import hutils
+
+setup(
+    name='hutils',
+    version=hutils.__version__,
+    description='a charming python web util-library',
+    long_description=pypandoc.convert('README.md', 'rst'),
+    author='ZaiHui Dev',
+    author_email='llk@kezaihui.com',
+    url='https://github.com/zaihui/hutils/',
+    license='MIT License',
+    install_requires=open('requirements.txt').readlines(),
+    packages=['hutils'],
+    classifiers=[
+        'License :: OSI Approved :: MIT License',
+        'Programming Language :: Python :: 3',
+        'Programming Language :: Python :: 3.4',
+        'Programming Language :: Python :: 3.5',
+        'Programming Language :: Python :: 3.6',
+        'Programming Language :: Python :: 3.7',
+    ],
+)
