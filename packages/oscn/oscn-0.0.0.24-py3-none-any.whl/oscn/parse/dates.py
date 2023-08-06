f80fce23919b1c7135167f5f03704ae259fb04ef
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-06 01:52:43 +0000

    Add package oscn/0.0.0.24/oscn-0.0.0.24-py3-none-any.whl

---
+++ packages/oscn/oscn-0.0.0.24-py3-none-any.whl/oscn/parse/dates.py
@@ -0,0 +1,22 @@
+import re
+
+
+def make_pattern_finder(pattern):
+    find = re.compile(pattern, re.M)
+
+    def find_pattern(oscn_html):
+        search = find.search(oscn_html)
+        try:
+            return search.group(1)
+        except AttributeError:
+            return None
+
+    return find_pattern
+
+find_filed = make_pattern_finder(r'Filed:\s*([\/\d]*)')
+find_filed.__name__ = 'filed'
+setattr(find_filed, 'target', ['Case'])
+
+find_closed = make_pattern_finder(r'Closed:\s*([\/\d]*)')
+find_closed.__name__ = 'closed'
+setattr(find_closed, 'target', ['Case'])
