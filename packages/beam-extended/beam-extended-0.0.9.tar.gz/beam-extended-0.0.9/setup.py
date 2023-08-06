74f844464d1121a7f4b54179262f09bab9e62abc
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-06 01:56:44 +0000

    Add package beam-extended/0.0.9/beam-extended-0.0.9.tar.gz

---
+++ packages/beam-extended/beam-extended-0.0.9.tar.gz/beam-extended-0.0.9/setup.py
@@ -0,0 +1,26 @@
+import setuptools
+
+with open("../../README.md", "r") as fh:
+    long_description = fh.read()
+
+setuptools.setup(
+    name="beam-extended",
+    version="0.0.9",
+    description="Extend Apache Beam python API with new modules",
+    long_description=long_description,
+    long_description_content_type="text/markdown",
+    author="Pascal Gula",
+    author_email="pascal@plantix.net",
+    classifiers=(
+        "Programming Language :: Python :: 2",
+        "License :: OSI Approved :: Apache Software License",
+        "Operating System :: OS Independent",
+    ),
+    keywords=['Apache', 'Beam', 'python'],
+    url="https://github.com/PEAT-AI/beam-extended",
+    packages=setuptools.find_packages(),
+    install_requires=[
+        'apache-beam',
+        'pymongo'
+    ]
+)
