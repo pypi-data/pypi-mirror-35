be55ad7a73750f06e55a2d2c14d435742437030e
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-06 07:10:51 +0000

    Add package hdkey/0.0.0/hdkey-0.0.0.tar.gz

---
+++ packages/hdkey/hdkey-0.0.0.tar.gz/hdkey-0.0.0/setup.py
@@ -0,0 +1,24 @@
+# flake8: noqa
+
+from setuptools import setup, find_packages
+
+setup(
+    name='hdkey',
+    version='0.0.0',
+    description=('HD key utils (placeholder)'),
+    author='James Prestwich',
+    author_email='james@summa.one',
+    license='LGPLv3.0',
+    install_requires=[],
+    packages=find_packages(),
+    keywords = 'bitcoin litecoin cryptocurrency decred blockchain development',
+    classifiers = [
+        'Programming Language :: Python',
+        'Programming Language :: Python :: 3 :: Only',
+        'Topic :: Security :: Cryptography',
+        'Intended Audience :: Developers',
+        'License :: OSI Approved :: GNU Lesser General Public License v3 or later (LGPLv3+)',
+        'Operating System :: OS Independent',
+        'Topic :: Software Development :: Libraries :: Python Modules',
+        ],
+)
