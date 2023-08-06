337122b993229e053fee085061f546da997c9f94
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-06 16:16:50 +0000

    Add package kharon/0.1.2.6/kharon-0.1.2.6.tar.gz

---
+++ packages/kharon/kharon-0.1.2.6.tar.gz/kharon-0.1.2.6/kharon/command_line.py
@@ -0,0 +1,33 @@
+import os
+from pkg_resources import resource_filename, Requirement
+from shutil import copyfile
+import argparse
+
+def kharon():
+    parser = argparse.ArgumentParser(description='Create a new Kharon Project')
+    parser.add_argument('command', action='store_true', help='The name of the new directory')
+    subparsers = parser.add_subparsers(help='sub-command help')
+
+    # create the parser for the "makeproject" command
+    parser_run = subparsers.add_parser('run', help='Makes a new Kharon project.')
+    parser_run.add_argument(
+        'name',
+        type=str,
+        help='The name of the new project'
+    )
+
+    #set function corresponding to command
+    parser_run.set_defaults(func=makeproject)
+
+    # run function correspondign to command
+    args = parser.parse_args()
+    args.func(args)
+
+def makeproject(args):
+    template_path = resource_filename(Requirement.parse("kharon"), "kharon/projecttemplates/")
+    template_files = os.listdir(template_path)
+
+    for filename in template_files:
+        if not filename in ['__init__.py', '__pycache__']:
+            print(template_path + filename)
+            copyfile(template_path + filename, os.getcwd() + "/" + args.name + "/" + filename)
