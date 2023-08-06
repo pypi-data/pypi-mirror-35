73cb9d53d3681f518cd7fec48c079aad3602d39c
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-06 14:29:20 +0000

    Add package django-processengine/0.2.1/django-processengine-0.2.1.tar.gz

---
+++ packages/django-processengine/django-processengine-0.2.1.tar.gz/django-processengine-0.2.1/processengine/management/commands/run_task.py
@@ -0,0 +1,106 @@
+import json
+from contextlib import suppress
+
+from django.conf import settings
+from django.core.management.base import BaseCommand
+from processengine.models import Process
+
+from processengine.helpers import slack_notification
+
+SERVICE_NAME = "Order Service"
+
+SPACER_LINE = """
+===============================================================================
+"""
+
+
+class Command(BaseCommand):
+    help = 'Runs a specified task through the process engine.'
+
+    def add_arguments(self, parser):
+        # Positional arguments
+        parser.add_argument('task', nargs='+')
+
+    def handle(self, *args, **options):
+        """
+        Run the specified task through the process engine.
+        """
+        task_name = options['task'][0]
+        slack_channel = settings.SLACK_PROCESS_CHANNEL
+        slack_username = settings.SLACK_PROCESS_USERNAME
+        slack_emoji = settings.SLACK_PROCESS_EMOJI
+
+        if task_name in settings.PROCESS_MAP:
+            param_string = None
+            with suppress(IndexError):
+                param_string = options['task'][1]
+            if param_string:
+                try:
+                    params = json.loads(param_string)
+                except Exception as e:
+                    if not settings.DEBUG and settings.SLACK_WEBHOOK:
+                        param_string = str(param_string)
+                        title = SERVICE_NAME + ": Process Creation Failed"
+                        message = (
+                            "Arguments for task {} don't appear to be valid"
+                            " JSON.\nArguments were {}")
+                        message = message.format(task_name, param_string)
+                        slack_notification(message=message,
+                                           channel=settings.SLACK_PROCESS_CHANNEL,
+                                           username=settings.SLACK_PROCESS_USERNAME,
+                                           emoji=settings.SLACK_PROCESS_EMOJI,
+                                           title=title)
+                    else:
+                        print(SPACER_LINE)
+                        print("Arguments for task {} do not appear to be valid"
+                              " JSON.".format(
+                                  task_name))
+                        print(SPACER_LINE)
+                    return
+                if not type(params) == dict:
+                    if not settings.DEBUG and settings.SLACK_WEBHOOK:
+                        title = SERVICE_NAME + ": Process Creation Failed"
+                        message = ("Arguments for task {} are not a valid dict"
+                                   ".\nArguments were {}")
+                        message = message.format(task_name, param_string)
+                        slack_notification(message=message,
+                                           channel=settings.SLACK_PROCESS_CHANNEL,
+                                           username=settings.SLACK_PROCESS_USERNAME,
+                                           emoji=settings.SLACK_PROCESS_EMOJI,
+                                           title=title)
+                    else:
+                        print(SPACER_LINE)
+                        print("Arguments for task {} do not appear to be a "
+                              "JSON dict.".format(task_name))
+                        print(SPACER_LINE)
+                    return
+            process = Process.objects.create(name=task_name, context={})
+            message = "Running task '{}' with the following task ids:\n{}"
+            message = message.format(task_name, process.task_ids)
+            if settings.DEBUG:
+                print(SPACER_LINE)
+                print(message)
+                print(SPACER_LINE)
+            elif settings.SLACK_WEBHOOK:
+                title = SERVICE_NAME + ": Process Creation Succeeded"
+                slack_notification(message=message,
+                                   channel=settings.SLACK_PROCESS_CHANNEL,
+                                   username=settings.SLACK_PROCESS_USERNAME,
+                                   emoji=settings.SLACK_PROCESS_EMOJI,
+                                   title=title)
+        else:
+            if not settings.DEBUG and settings.SLACK_WEBHOOK:
+                title = SERVICE_NAME + ": Process Creation Failed"
+                message = (
+                    "We attempted to create a process running task *'{}'*, "
+                    "but this task is unknown.")
+                message = message.format(task_name)
+                slack_notification(message=message,
+                                   channel=settings.SLACK_PROCESS_CHANNEL,
+                                   username=settings.SLACK_PROCESS_USERNAME,
+                                   emoji=settings.SLACK_PROCESS_EMOJI,
+                                   title=title)
+            else:
+                print(SPACER_LINE)
+                print("Couldn't find task '{}'".format(task_name))
+                print(SPACER_LINE)
---
+++ packages/django-processengine/django-processengine-0.2.1.tar.gz/django-processengine-0.2.1/tests/test_commands.py
@@ -0,0 +1,115 @@
+import json
+
+import responses
+from django.conf import settings
+from django.core.management import call_command
+from django.test import TestCase, override_settings
+from processengine.models import Process
+from rq import SimpleWorker
+from unittest.mock import patch
+from django_rq import get_queue
+from processengine.helpers import retry_handler
+
+@override_settings(SLACK_WEBHOOK="http://example.com",
+                   SLACK_PROCESS_CHANNEL = "#Processes",
+                   SLACK_PROCESS_USERNAME = "Someuser",
+                   SLACK_PROCESS_EMOJI = ":ghost:",
+                   DEBUG=False)
+@override_settings(CELERY_ALWAYS_EAGER = True)
+class RunTaskTestCase(TestCase):
+
+    @patch.object(Process, 'run')
+    def setUp(self, mock_process):
+        self.queue = get_queue(name=settings.RQ_DEFAULT_QUEUE, async=False)
+        self.worker = SimpleWorker(
+            self.queue,
+            connection=self.queue.connection, exception_handlers=[retry_handler])
+
+    @responses.activate
+    def test_call_valid_task_no_params(self):
+        responses.add(responses.POST, url=settings.SLACK_WEBHOOK, status=200)
+        # Check that we start with zero Processes
+        process_count = Process.objects.count()
+        self.assertEqual(process_count, 0)
+        # Run the command
+        args = ['ping']
+        opts = {}
+        call_command('run_task', *args, **opts)
+        self.worker.work(burst=True)
+        # Check that we now have 1 Process created
+        process_count = Process.objects.count()
+
+        self.assertEqual(len(responses.calls), 1)
+        self.assertIn("Process Creation Succeeded",
+                      responses.calls[0].request.body)
+        self.assertEqual(process_count, 1)
+
+
+    @responses.activate
+    def test_call_invalid_task_no_params(self):
+        responses.add(responses.POST, url=settings.SLACK_WEBHOOK, status=200)
+        # Check that we start with zero Processes
+        process_count = Process.objects.count()
+        self.assertEqual(process_count, 0)
+        # Run the command
+        args = ['Mickey Mouse']
+        opts = {}
+        call_command('run_task', *args, **opts)
+        # Check that we still have 0 Processes
+        process_count = Process.objects.count()
+        self.assertEqual(len(responses.calls), 1)
+        self.assertIn("this task is unknown", responses.calls[0].request.body)
+        self.assertEqual(process_count, 0)
+
+    @responses.activate
+    def test_call_valid_task_with_params(self):
+        responses.add(responses.POST, url=settings.SLACK_WEBHOOK, status=200)
+        # Check that we start with zero Processes
+        process_count = Process.objects.count()
+        self.assertEqual(process_count, 0)
+        # Run the command
+        param1 = json.dumps({'a': "aaaaa"})
+        args = ['ping', param1]
+        opts = {}
+        call_command('run_task', *args, **opts)
+        # Check that we now have 1 Process created
+        process_count = Process.objects.count()
+        self.assertEqual(len(responses.calls), 1)
+        self.assertIn("Process Creation Succeeded",
+                      responses.calls[0].request.body)
+        self.assertEqual(process_count, 1)
+
+    @responses.activate
+    def test_call_valid_task_with_non_json_params(self):
+        responses.add(responses.POST, url=settings.SLACK_WEBHOOK, status=200)
+        # Check that we start with zero Processes
+        process_count = Process.objects.count()
+        self.assertEqual(process_count, 0)
+        # Run the command
+        param1 = {'a': "aaaaa"}
+        args = ['ping', param1]
+        opts = {}
+        call_command('run_task', *args, **opts)
+        # Check that we now have 1 Process created
+        process_count = Process.objects.count()
+        self.assertEqual(len(responses.calls), 1)
+        self.assertIn("don't appear to be valid JSON",
+                      responses.calls[0].request.body)
+        self.assertEqual(process_count, 0)
+
+    @responses.activate
+    def test_call_valid_task_with_non_dict_params(self):
+        responses.add(responses.POST, url=settings.SLACK_WEBHOOK, status=200)
+        # Check that we start with zero Processes
+        process_count = Process.objects.count()
+        self.assertEqual(process_count, 0)
+        # Run the command
+        param1 = json.dumps("aaa")
+        args = ['ping', param1]
+        opts = {}
+        call_command('run_task', *args, **opts)
+        # Check that we now have 1 Process created
+        process_count = Process.objects.count()
+        self.assertEqual(len(responses.calls), 1)
+        self.assertIn("not a valid dict", responses.calls[0].request.body)
+        self.assertEqual(process_count, 0)
