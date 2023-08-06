834603cdf629ab8513911d3325a46863f3781485
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-06 01:54:56 +0000

    Add package surgen/0.0.10/surgen-0.0.10-py2.py3-none-any.whl

---
+++ packages/surgen/surgen-0.0.10-py2.py3-none-any.whl/surgen/target/base.py
@@ -0,0 +1,27 @@
+from clint.textui import puts
+
+
+class TargetBase(object):
+    TARGET_TYPE = "base"
+
+    def __init__(self, target):
+        self._target = target
+
+    def log(self, msg):
+        puts(msg)
+
+    @property
+    def workspace(self):
+        raise NotImplementedError()
+
+    def prepare(self):
+        pass
+
+    def commit(self, summary):
+        pass
+
+    def cleanup(self):
+        pass
+
+    def __str__(self):
+        return "{0}: ({1})".format(self.__class__.__name__, self._target)
