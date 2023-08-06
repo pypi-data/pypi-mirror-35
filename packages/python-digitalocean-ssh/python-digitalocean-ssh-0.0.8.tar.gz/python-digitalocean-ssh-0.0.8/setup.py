a0fa1deb5c9ca4eb2cfb63368478a5f34c9f8aa9
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-06 17:12:49 +0000

    Add package python-digitalocean-ssh/0.0.8/python-digitalocean-ssh-0.0.8.tar.gz

---
+++ packages/python-digitalocean-ssh/python-digitalocean-ssh-0.0.8.tar.gz/python-digitalocean-ssh-0.0.8/setup.py
@@ -0,0 +1,24 @@
+import setuptools
+
+with open('README.md', 'r') as fh:
+    long_description = fh.read()
+
+setuptools.setup(
+    name='python-digitalocean-ssh',
+    version='0.0.8',
+    author='Alexandros Solanos',
+    author_email='solanosalex@gmail.com',
+    description='Combine DO droplets with your ssh configuration',
+    long_description=long_description,
+    long_description_content_type='text/markdown',
+    url='https://github.com/hytromo/digital-ocean-to-ssh-config',
+    packages=setuptools.find_packages(),
+    install_requires=[
+        'python-digitalocean',
+    ],
+    classifiers=[
+        'Programming Language :: Python :: 3',
+        'License :: OSI Approved :: MIT License',
+        'Operating System :: OS Independent',
+    ],
+)
