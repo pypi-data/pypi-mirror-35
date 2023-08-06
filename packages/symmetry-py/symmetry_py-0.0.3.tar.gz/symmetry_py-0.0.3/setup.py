1a184af3ad914e3257b780300e167b381e1f6d01
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-06 01:53:31 +0000

    Add package symmetry-py/0.0.3/symmetry_py-0.0.3.tar.gz

---
+++ packages/symmetry-py/symmetry_py-0.0.3.tar.gz/symmetry_py-0.0.3/setup.py
@@ -0,0 +1,13 @@
+from  distutils.core import setup
+
+setup(
+    name = 'symmetry_py',
+    packages = ['symmetry_py'],
+    version = '0.0.3',
+    description = 'Symmetry',
+    license = 'MIT',
+    author = 'Logistio Software',
+    author_email = 'sudo@logistio.ie',
+    url = 'https://github.com/logistio/symmetry_py',
+    classifiers = []
+)
