3625ad25181af70ffe54fb805f770ccb3669c84c
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-06 07:11:01 +0000

    Add package gecosistema-database/0.0.65/gecosistema_database-0.0.65-py2-none-any.whl

---
+++ packages/gecosistema-database/gecosistema_database-0.0.65-py2-none-any.whl/gecosistema_database/functions.py
@@ -0,0 +1,41 @@
+# -------------------------------------------------------------------------------
+# Licence:
+# Copyright (c) 2012-2018 Luzzi Valerio 
+#
+# The above copyright notice and this permission notice shall be
+# included in all copies or substantial portions of the Software.
+#
+# THE SOFTWARE IS PROVIDED "AS IS", WITHOUT WARRANTY OF ANY KIND,
+# EXPRESS OR IMPLIED, INCLUDING BUT NOT LIMITED TO THE WARRANTIES
+# OF MERCHANTABILITY, FITNESS FOR A PARTICULAR PURPOSE AND
+# NONINFRINGEMENT. IN NO EVENT SHALL THE AUTHORS OR COPYRIGHT
+# HOLDERS BE LIABLE FOR ANY CLAIM, DAMAGES OR OTHER LIABILITY,
+# WHETHER IN AN ACTION OF CONTRACT, TORT OR OTHERWISE, ARISING
+# FROM, OUT OF OR IN CONNECTION WITH THE SOFTWARE OR THE USE OR
+# OTHER DEALINGS IN THE SOFTWARE.
+#
+#
+# Name:        functions.py
+# Purpose:
+#
+# Author:      Luzzi Valerio
+#
+# Created:     29/08/2018
+# -------------------------------------------------------------------------------
+from gecosistema_core import mapify
+from .sqlitedb import SqliteDB
+
+def SQL_EXEC(sql, args):
+    """
+    SQL_EXEC - run a query o a file.sql
+    """
+    try:
+        env = mapify(args, sep=' ', kvsep='=', strip_char=' ', glue='"')
+        SqliteDB.Execute(sql, env, verbose=False)
+        return 1
+    except Exception as ex:
+        print("--------------------------")
+        print(ex)
+        print("--------------------------")
+
+    return 0
