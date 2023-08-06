1dc212e0520f9da1c6d7852f7cb25d010d095c47
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-06 16:16:33 +0000

    Add package HSAuth/1.0.3/HSAuth-1.0.3.tar.gz

---
+++ packages/HSAuth/HSAuth-1.0.3.tar.gz/HSAuth-1.0.3/setup.py
@@ -0,0 +1,24 @@
+from distutils.core import setup
+from setuptools import find_packages
+
+setup(name='HSAuth',  # 包名
+      version='1.0.3',  # 版本号
+      description='',
+      long_description='',
+      author='',
+      author_email='',
+      url='https://gitlab.com/zmbgitlab',
+      license='',
+      install_requires=['Flask', 'requests'],
+      classifiers=[
+          'Intended Audience :: Developers',
+          'Operating System :: OS Independent',
+          'Natural Language :: Chinese (Simplified)',
+          'Programming Language :: Python :: 3.5',
+          'Topic :: Utilities'
+      ],
+      keywords='',
+      packages=find_packages('src'),  # 必填
+      package_dir={'': 'src'},  # 必填
+      include_package_data=True,
+      )
