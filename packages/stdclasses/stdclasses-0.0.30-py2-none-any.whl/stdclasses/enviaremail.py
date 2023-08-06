46e8ff66e75f8fe0faf33d4a28f794338a1bda6a
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-06 17:11:05 +0000

    Add package stdclasses/0.0.30/stdclasses-0.0.30-py2-none-any.whl

---
+++ packages/stdclasses/stdclasses-0.0.30-py2-none-any.whl/stdclasses/enviaremail.py
@@ -0,0 +1,49 @@
+import smtplib
+import base64
+
+class EnviarEmail:
+
+    def __init__(self):
+        self.nome = None
+
+
+
+def sendMail(self, params):
+ 
+  fromaddr = params["mail_from"]
+  toaddr = params["mail_to"]
+  cc = params["mail_cc"]
+  bcc = params["mail_bcc"]
+  msg = MIMEMultipart()
+  
+  vbcc = [] if params["mail_bcc"] == "" else bcc.split(";")
+  vcc = [] if params["mail_cc"] == "" else cc.split(";")
+  rcpt = vbcc  + vcc + [toaddr]
+  msg['From'] = fromaddr
+  msg['To'] = toaddr
+  msg['Cc'] = cc
+  msg['Bcc'] = bcc
+  msg['Subject'] = params["subject"]
+   
+  body = params["content"]
+   
+  msg.attach(MIMEText(body, 'html'))
+   
+  if params["attach_file"]:
+    filename = params["file_name"][params["file_name"].rfind("/")+1:]
+    attachment = open(params["file_name"], "rb")
+     
+    part = MIMEBase('application', 'octet-stream')
+    part.set_payload((attachment).read())
+    encoders.encode_base64(part)
+    part.add_header('Content-Disposition', "attachment; filename= %s" % filename)
+     
+    msg.attach(part)
+  
+  #169.254.8.75 
+  #180.128.170.65
+  server = smtplib.SMTP('180.128.170.65', 25) 
+  text = msg.as_string()
+  server.sendmail(fromaddr, rcpt, text)
+  server.quit()
+  
