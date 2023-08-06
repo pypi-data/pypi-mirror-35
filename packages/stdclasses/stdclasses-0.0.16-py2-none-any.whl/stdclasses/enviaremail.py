710a9a46801943a7c9fac2b0793302b8c750dcb2
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-06 17:15:42 +0000

    Add package stdclasses/0.0.16/stdclasses-0.0.16-py2-none-any.whl

---
+++ packages/stdclasses/stdclasses-0.0.16-py2-none-any.whl/stdclasses/enviaremail.py
@@ -0,0 +1,13 @@
+class EnviarEmail:
+    def teste(self, teste):
+        print teste
+
+    def gui (string):
+        print "Guilherme Martino"
+        return "oie"
+
+    def setNome(self, nome):
+        self.nome = nome
+
+    def getNome(self):
+        return self.nome
