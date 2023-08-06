917c45dd31c3ff2609fbcd71271b0ee6bcad90a9
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-06 04:14:45 +0000

    Add package yitest/0.2/yitest-0.2.tar.gz

---
+++ packages/yitest/yitest-0.2.tar.gz/yitest-0.2/setup.py
@@ -0,0 +1,11 @@
+from setuptools import setup
+
+setup(name='yitest',
+      version='0.2',
+      description='The funniest joke in the world',
+      url='https://github.com/dymodi/machine_learning',
+      author='yi',
+      author_email='dymodi@gmail.com',
+      license='MIT',
+      packages=['yitest'],
+      zip_safe=False)
