944c5760ac0b02996f30ac0d1891f8f76601667e
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-06 14:57:22 +0000

    Add package bignmf/1.0.5/bignmf-1.0.5.tar.gz

---
+++ packages/bignmf/bignmf-1.0.5.tar.gz/bignmf-1.0.5/setup.py
@@ -0,0 +1,28 @@
+import setuptools
+
+with open("README.md", "r") as fh:
+    long_description = fh.read()
+
+setuptools.setup(
+    name="bignmf",
+    version="1.0.5",
+    author="Haran Rajkumar, Vaibhav Kulshrestha",
+    author_email="haranrajkumar97@gmail.com, vaibhav1kulshrestha@gmail.com",
+    description="Non-negative matrix factorization",
+    long_description=long_description,
+    long_description_content_type="text/markdown",
+    url="https://github.com/thenmf/bignmf",
+    packages=setuptools.find_packages(),
+    classifiers=(
+        "Programming Language :: Python :: 3",
+        "License :: OSI Approved :: MIT License",
+        "Operating System :: OS Independent",
+    ),
+    install_requires=[
+        'numpy',
+        'pandas',
+        'fastcluster',
+        'scipy',
+    ],
+    include_package_data=True
+)
