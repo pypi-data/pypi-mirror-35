1855f214b06a5e8b84e9a6f1e020599dd31b040c
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-06 01:56:14 +0000

    Add package daydreamer/0.0.11/daydreamer-0.0.11.tar.gz

---
+++ packages/daydreamer/daydreamer-0.0.11.tar.gz/daydreamer-0.0.11/daydreamer/apiclient.py
@@ -0,0 +1,19 @@
+from daydreamer.baseclient import BaseClient
+from daydreamer import endpoints
+
+
+class APIClient(BaseClient):
+
+    def __init__(self, username, password=None):
+        super(APIClient, self).__init__(username, password)
+
+        self.betting = endpoints.Betting(self)
+        self.account = endpoints.Account(self)
+        self.market_data = endpoints.MarketData(self)
+        self.reference_data = endpoints.ReferenceData(self)
+
+    def __repr__(self):
+        return '<APIClient [%s]>' % self.username
+
+    def __str__(self):
+        return 'APIClient'
---
+++ packages/daydreamer/daydreamer-0.0.11.tar.gz/daydreamer-0.0.11/setup.py
@@ -0,0 +1,16 @@
+
+from setuptools import setup, find_packages
+from daydreamer import __version__
+
+setup(
+    name="daydreamer",
+    version=__version__,
+    author="Nick Georgiadis",
+    author_email="nickgeo_@hotmail.com",
+    description="Daydreamer Nick API Python wrapper",
+    url="https://github.com/rozzac90/pinnacle",
+    packages=find_packages(),
+    install_requires=[line.strip() for line in open("requirements.txt")],
+    long_description=open('README.md').read(),
+    tests_require=['pytest'],
+)
