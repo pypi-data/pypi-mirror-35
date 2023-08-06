9a23b5bbf6e1d7faa9b94b5b40e37adad1546228
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-06 17:12:03 +0000

    Add package confect/0.2.0/confect-0.2.0.tar.gz

---
+++ packages/confect/confect-0.2.0.tar.gz/confect-0.2.0/confect/__init__.py
@@ -0,0 +1,10 @@
+
+from .conf import Conf
+from .error import (ConfGroupExistsError, FrozenConfGroupError,
+                    FrozenConfPropError, UnknownConfError)
+
+__all__ = [
+    Conf,
+    FrozenConfPropError, FrozenConfGroupError,
+    UnknownConfError, ConfGroupExistsError
+]
