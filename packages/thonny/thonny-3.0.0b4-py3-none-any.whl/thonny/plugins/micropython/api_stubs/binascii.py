e73273bb4445ee6c085dc6e94c7a0fc0fcea5776
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-06 13:23:45 +0000

    Add package thonny/3.0.0b4/thonny-3.0.0b4-py3-none-any.whl

---
+++ packages/thonny/thonny-3.0.0b4-py3-none-any.whl/thonny/plugins/micropython/api_stubs/binascii.py
@@ -0,0 +1,11 @@
+def a2b_base64():
+    pass
+
+def b2a_base64():
+    pass
+
+def hexlify():
+    pass
+
+def unhexlify():
+    pass
---
+++ packages/thonny/thonny-3.0.0b4-py3-none-any.whl/thonny/plugins/micropython/api_stubs/struct.py
@@ -0,0 +1,14 @@
+def calcsize():
+    pass
+
+def pack():
+    pass
+
+def pack_into():
+    pass
+
+def unpack():
+    pass
+
+def unpack_from():
+    pass
