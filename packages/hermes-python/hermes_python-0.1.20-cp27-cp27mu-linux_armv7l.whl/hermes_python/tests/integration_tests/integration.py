8662a16b50f91e532871238451c2fa30da900a7e
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-06 16:15:47 +0000

    Add package hermes-python/0.1.20/hermes_python-0.1.20-cp27-cp27mu-linux_armv7l.whl

---
+++ packages/hermes-python/hermes_python-0.1.20-cp27-cp27mu-linux_armv7l.whl/hermes_python/tests/integration_tests/integration.py
@@ -0,0 +1,10 @@
+# coding : utf-8
+from hermes_python.hermes import Hermes
+
+def callback_encoding_test(hermes, intentMessage):
+    print intentMessage
+
+with Hermes("raspi3-14.local") as h:
+    h.subscribe_intent("lightsSet", callback_encoding_test).loop_forever()
+
+
---
+++ packages/hermes-python/hermes_python-0.1.20-cp27-cp27mu-linux_armv7l.whl/tests/test_ontology.py
@@ -0,0 +1,5 @@
+import hermes_python
+
+
+
+
