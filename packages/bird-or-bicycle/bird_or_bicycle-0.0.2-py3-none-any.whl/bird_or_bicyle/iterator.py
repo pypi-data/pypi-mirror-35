714ed5d4590a786c68c2d10cf519d14e65b098aa
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-06 13:53:55 +0000

    Add package bird-or-bicycle/0.0.2/bird_or_bicycle-0.0.2-py3-none-any.whl

---
+++ packages/bird-or-bicycle/bird_or_bicycle-0.0.2-py3-none-any.whl/bird_or_bicyle/iterator.py
@@ -0,0 +1,36 @@
+import bird_or_bicyle
+import torch
+import torchvision
+from bird_or_bicyle import BICYCLE_IDX, BIRD_IDX
+
+
+def get_iterator(split='train', batch_size=32, shuffle=True):
+  """ Create a backend-agnostic iterator for the dataset.
+  Images are formatted in channels-last in the Tensorflow style
+  :param split: One of ['train', 'test', 'extras']
+  :param batch_size: The number of images and labels in each batch
+  :param shuffle: Whether or not to shuffle
+  :return:  An iterable that returns (batched_images, batched_labels)
+  """
+  data_dir = bird_or_bicyle.get_dataset(split)
+
+  image_preprocessing = torchvision.transforms.Compose([
+    torchvision.transforms.Resize(224),
+    torchvision.transforms.ToTensor(),
+    lambda x: torch.einsum('chw->hwc', [x]),
+  ])
+
+  train_dataset = torchvision.datasets.ImageFolder(
+    data_dir, transform=image_preprocessing
+  )
+
+  data_loader = torch.utils.data.DataLoader(
+    train_dataset,
+    batch_size=batch_size,
+    shuffle=shuffle)
+
+  assert train_dataset.class_to_idx['bicycle'] == BICYCLE_IDX
+  assert train_dataset.class_to_idx['bird'] == BIRD_IDX
+
+  dataset_iter = [(x.numpy(), y.numpy()) for (x, y) in iter(data_loader)]
+  return dataset_iter
