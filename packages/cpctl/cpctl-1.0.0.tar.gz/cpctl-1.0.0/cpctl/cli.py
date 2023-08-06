ce21cf7fa8e9dda2419c818c152609848196fc62
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-06 04:12:24 +0000

    Add package cpctl/1.0.0/cpctl-1.0.0.tar.gz

---
+++ packages/cpctl/cpctl-1.0.0.tar.gz/cpctl-1.0.0/cpctl/cli.py
@@ -0,0 +1,119 @@
+#!/usr/bin/env python3
+# -*- coding: utf-8 -*-
+'''CLI module.'''
+
+import datetime
+import os
+import sys
+import time
+import click
+import re
+from .at import AT, ATException
+
+class CliException(Exception):
+    '''Generic cli error exception.'''
+    pass
+
+
+def get_ports():
+    if os.name == 'nt' or sys.platform == 'win32':
+        from serial.tools.list_ports_windows import comports
+    elif os.name == 'posix':
+        from serial.tools.list_ports_posix import comports
+    return sorted(comports())
+
+
+@click.group()
+@click.version_option()
+@click.option('--device', '-d', type=str, help='Device path.')
+@click.pass_context
+def cli(ctx, device=None):
+    '''Cooper Control Tool.'''
+    if device:
+        ctx.obj['at'] = AT(device)
+
+
+@cli.command()
+def devices():
+    '''Print available devices.'''
+    for port in get_ports():
+        click.echo(port[0], err=True)
+
+
+@cli.group()
+@click.pass_context
+def node(ctx):
+    '''Manage the nodes'''
+    if 'at' not in ctx.obj:
+        ports = get_ports()
+        for i, port in enumerate(ports):
+            click.echo("%i %s" % (i, port[0]), err=True)
+        d = click.prompt('Please enter device')
+        for port in ports:
+            if port[0] == d:
+                device = port[0]
+                break
+        else:
+            try:
+                device = ports[int(d)][0]
+                print(device)
+            except Exception as e:
+                raise CliException("Unknown device")
+
+        ctx.obj['at'] = AT(device)
+
+
+@node.command("list")
+@click.pass_context
+def node_list(ctx):
+    '''List attached nodes'''
+    for serial in ctx.obj['at'].command("$LIST"):
+        click.echo(serial)
+
+
+@node.command("attach")
+@click.argument('serial')
+@click.pass_context
+def node_attach(ctx, serial):
+    '''Attach node'''
+    if not re.match("^[0-9a-fA-F]{12}$", serial):
+        raise CliException("serial bad format")
+
+    node_list = ctx.obj['at'].command("$LIST")
+
+    if serial in node_list:
+        raise CliException("Node is in node list")
+
+    ctx.obj['at'].command("$ATTACH=" + serial)
+    click.echo('OK')
+
+
+@node.command("detach")
+@click.argument('serial')
+@click.pass_context
+def node_detach(ctx, serial):
+    '''Detach node'''
+    if not re.match("^[0-9a-fA-F]{12}$", serial):
+        raise CliException("serial bad format")
+
+    node_list = ctx.obj['at'].command("$LIST")
+
+    if not serial in node_list:
+        raise CliException("Node is not in node list")
+
+    ctx.obj['at'].command("$DETACH=" + serial)
+    click.echo('OK')
+
+
+def main():
+    '''Application entry point.'''
+    try:
+        cli(obj={}),
+    except ATException as e:
+        click.echo(str(e), err=True)
+        sys.exit(1)
+    except CliException as e:
+        click.echo(str(e), err=True)
+        sys.exit(1)
+    except KeyboardInterrupt:
+        pass
