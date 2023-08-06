406ea73619b6c925a9a517768c8a5cd289656e8f
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-06 01:56:26 +0000

    Add package daydreamer/0.0.14/daydreamer-0.0.14.tar.gz

---
+++ packages/daydreamer/daydreamer-0.0.14.tar.gz/daydreamer-0.0.14/daydreamer/resources/accountresources.py
@@ -0,0 +1,11 @@
+from daydreamer.resources.baseresource import BaseResource
+
+
+class AccountDetails(BaseResource):
+    class Meta(BaseResource.Meta):
+        attributes = {
+            'availableBalance': 'availableBalance',
+            'outstandingTransactions': 'outstandingTransactions',
+            'givenCredit': 'givenCredit',
+            'currency': 'currency',
+        }
