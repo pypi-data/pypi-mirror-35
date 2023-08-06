f337f72af12d99499c6bbe5b6b61421e64450c9b
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-06 14:22:47 +0000

    Add package elastico/0.0.31/elastico-0.0.31.tar.gz

---
+++ packages/elastico/elastico-0.0.31.tar.gz/elastico-0.0.31/elastico/cli/index.py
@@ -0,0 +1,27 @@
+from .cli import command, arg
+
+index_command = command.add_subcommands('index',help="work with indices")
+
+@index_command('ls', arg('index', help="index name(s)", nargs="*"))
+def cmd_indices(config):
+    '''list indices'''
+    from ..connection import elasticsearch
+    es = elasticsearch(config)
+    indexes = config.get('index.ls.index')
+    if not indexes:
+        indexes = ['_all']
+    for indexpattern in indexes:
+        for idx in es.indices.get(indexpattern).keys():
+            print(idx)
+
+@index_command('rm', arg('index', help="index name(s)", nargs="+"))
+def cmd_indices(config):
+    '''list indices'''
+    from ..connection import elasticsearch
+    es = elasticsearch(config)
+    for idx in config.get('index.rm.index'):
+        es.indices.delete(idx)
+
+    # for idx in es.indices.get('_all').keys():
+    # for idx in es.indices.get('_all').keys():
+    #     print(idx)
