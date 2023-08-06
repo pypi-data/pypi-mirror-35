fb69af305197af966f7dc6e611943d80db42fc10
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-06 01:53:25 +0000

    Add package pylint-topology/0.1.0/pylint-topology-0.1.0.tar.gz

---
+++ packages/pylint-topology/pylint-topology-0.1.0.tar.gz/pylint-topology-0.1.0/setup.py
@@ -0,0 +1,14 @@
+# -*- coding: utf-8 -*-
+from setuptools import setup
+
+setup(
+    name='pylint-topology',
+    version='0.1.0',
+    description='check module topology in pylint',
+    author='Lirian Su',
+    author_email='liriansu@gmail.com',
+    url='https://github.com/LKI/pylint_topology',
+    license='GPLv2',
+    packages=['pylint_topology'],
+    install_requirements=['pylint>=2.0'],
+)
