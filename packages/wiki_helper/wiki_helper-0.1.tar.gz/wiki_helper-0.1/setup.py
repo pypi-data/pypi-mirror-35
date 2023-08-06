a8a274101263ebc6b370f4468142b7371f641f73
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-06 01:54:35 +0000

    Add package wiki_helper/0.1/wiki_helper-0.1.tar.gz

---
+++ packages/wiki_helper/wiki_helper-0.1.tar.gz/wiki_helper-0.1/setup.py
@@ -0,0 +1,11 @@
+from setuptools import setup
+
+setup(name='wiki_helper',
+      version='0.1',
+      description='Helpers for extracting some data from Wikipedia',
+      url='https://bitbucket.org/amist/wiki_helper',
+      author='Amitay Stern',
+      author_email='stern.amitay@gmail.com',
+      license='MIT',
+      packages=['wiki_helper'],
+      zip_safe=False)
