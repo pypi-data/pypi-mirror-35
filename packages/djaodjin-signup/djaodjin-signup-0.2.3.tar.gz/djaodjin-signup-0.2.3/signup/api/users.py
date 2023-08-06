ad4d545f00f89f2f9efc333129921580250bbf82
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-06 16:18:40 +0000

    Add package djaodjin-signup/0.2.3/djaodjin-signup-0.2.3.tar.gz

---
+++ packages/djaodjin-signup/djaodjin-signup-0.2.3.tar.gz/djaodjin-signup-0.2.3/signup/api/users.py
@@ -0,0 +1,169 @@
+# Copyright (c) 2018, DjaoDjin inc.
+# All rights reserved.
+#
+# Redistribution and use in source and binary forms, with or without
+# modification, are permitted provided that the following conditions are met:
+#
+# 1. Redistributions of source code must retain the above copyright notice,
+#    this list of conditions and the following disclaimer.
+# 2. Redistributions in binary form must reproduce the above copyright
+#    notice, this list of conditions and the following disclaimer in the
+#    documentation and/or other materials provided with the distribution.
+#
+# THIS SOFTWARE IS PROVIDED BY THE COPYRIGHT HOLDERS AND CONTRIBUTORS
+# "AS IS" AND ANY EXPRESS OR IMPLIED WARRANTIES, INCLUDING, BUT NOT LIMITED
+# TO, THE IMPLIED WARRANTIES OF MERCHANTABILITY AND FITNESS FOR A PARTICULAR
+# PURPOSE ARE DISCLAIMED. IN NO EVENT SHALL THE COPYRIGHT HOLDER OR
+# CONTRIBUTORS BE LIABLE FOR ANY DIRECT, INDIRECT, INCIDENTAL, SPECIAL,
+# EXEMPLARY, OR CONSEQUENTIAL DAMAGES (INCLUDING, BUT NOT LIMITED TO,
+# PROCUREMENT OF SUBSTITUTE GOODS OR SERVICES; LOSS OF USE, DATA, OR PROFITS;
+# OR BUSINESS INTERRUPTION) HOWEVER CAUSED AND ON ANY THEORY OF LIABILITY,
+# WHETHER IN CONTRACT, STRICT LIABILITY, OR TORT (INCLUDING NEGLIGENCE OR
+# OTHERWISE) ARISING IN ANY WAY OUT OF THE USE OF THIS SOFTWARE, EVEN IF
+# ADVISED OF THE POSSIBILITY OF SUCH DAMAGE.
+
+import logging
+
+from django.contrib.auth import logout as auth_logout
+from django.db.models import Q
+from django.http import Http404
+from django.contrib.auth import update_session_auth_hash
+from rest_framework.generics import (ListAPIView, RetrieveUpdateDestroyAPIView,
+    UpdateAPIView)
+
+from ..compat import User
+from ..serializers import (PasswordChangeSerializer, UserSerializer,
+    NotificationsSerializer)
+
+
+LOGGER = logging.getLogger(__name__)
+
+
+class PasswordChangeAPIView(UpdateAPIView):
+    """
+    Changes the password for a user.
+
+    **Example
+
+    .. code-block:: http
+
+        PUT /api/users/donny/password/ HTTP/1.1
+
+    responds
+
+    .. code-block:: json
+
+        {
+          "password": "yeye"
+        }
+    """
+
+    lookup_field = 'username'
+    lookup_url_kwarg = 'user'
+    serializer_class = PasswordChangeSerializer
+    queryset = User.objects.all()
+
+    def perform_update(self, serializer):
+        password = serializer.validated_data['password']
+        serializer.instance.set_password(password)
+        serializer.instance.save()
+        # Updating the password logs out all other sessions for the user
+        # except the current one if
+        # django.contrib.auth.middleware.SessionAuthenticationMiddleware
+        # is enabled.
+        update_session_auth_hash(self.request, serializer.instance)
+
+
+class UserNotificationsAPIView(UpdateAPIView):
+    """
+    Changes notifications preferences for a user.
+
+    **Example
+
+    .. code-block:: http
+
+        POST /api/users/donny/notifications/ HTTP/1.1
+
+    .. code-block:: json
+
+        {
+          "notifications": ["user_registered_notice"]
+        }
+
+    responds
+
+    .. code-block:: json
+
+        {
+          "notifications": ["user_registered_notice"]
+        }
+    """
+    lookup_field = 'username'
+    lookup_url_kwarg = 'user'
+    serializer_class = NotificationsSerializer
+    queryset = User.objects.all()
+
+
+class UserProfileAPIView(RetrieveUpdateDestroyAPIView):
+    """
+    Retrieves, updates or deletes the profile information of a user.
+
+    **Example
+
+    .. code-block:: http
+
+        GET /api/users/donny HTTP/1.1
+
+    responds
+
+    .. code-block:: json
+
+        {
+         "username": "donny",
+         "email": "donny.smith@example.com"
+         "full_name": "Donny Smith"
+        }
+    """
+
+    lookup_field = 'username'
+    lookup_url_kwarg = 'user'
+    serializer_class = UserSerializer
+    queryset = User.objects.all()
+
+    def perform_destroy(self, instance):
+        LOGGER.info("user %s deleted.", instance,
+            extra={'event': 'delete', 'request': self.request})
+        requires_logout = (self.request.user == instance)
+        instance.delete()
+        if requires_logout:
+            auth_logout(self.request)
+
+
+class UserListAPIView(ListAPIView):
+    """
+    Returns the list of users registered with the service.
+
+    **Example
+
+    .. code-block:: http
+
+        GET /api/users/ HTTP/1.1
+
+    responds
+
+    .. code-block:: json
+
+        [{
+         "username": "donny",
+         "email": "donny.smith@example.com"
+         "full_name": "Donny Smith"
+        }]
+    """
+    serializer_class = UserSerializer
+
+    def get_queryset(self):
+        startswith = self.request.GET.get('q', None)
+        if not startswith:
+            raise Http404
+        return User.objects.filter(Q(username__startswith=startswith)
+            | Q(email__startswith=startswith))
---
+++ packages/djaodjin-signup/djaodjin-signup-0.2.3.tar.gz/djaodjin-signup-0.2.3/signup/utils.py
@@ -0,0 +1,163 @@
+# Copyright (c) 2018, DjaoDjin inc.
+# All rights reserved.
+#
+# Redistribution and use in source and binary forms, with or without
+# modification, are permitted provided that the following conditions are met:
+#
+# 1. Redistributions of source code must retain the above copyright notice,
+#    this list of conditions and the following disclaimer.
+# 2. Redistributions in binary form must reproduce the above copyright
+#    notice, this list of conditions and the following disclaimer in the
+#    documentation and/or other materials provided with the distribution.
+#
+# THIS SOFTWARE IS PROVIDED BY THE COPYRIGHT HOLDERS AND CONTRIBUTORS
+# "AS IS" AND ANY EXPRESS OR IMPLIED WARRANTIES, INCLUDING, BUT NOT LIMITED
+# TO, THE IMPLIED WARRANTIES OF MERCHANTABILITY AND FITNESS FOR A PARTICULAR
+# PURPOSE ARE DISCLAIMED. IN NO EVENT SHALL THE COPYRIGHT HOLDER OR
+# CONTRIBUTORS BE LIABLE FOR ANY DIRECT, INDIRECT, INCIDENTAL, SPECIAL,
+# EXEMPLARY, OR CONSEQUENTIAL DAMAGES (INCLUDING, BUT NOT LIMITED TO,
+# PROCUREMENT OF SUBSTITUTE GOODS OR SERVICES; LOSS OF USE, DATA, OR PROFITS;
+# OR BUSINESS INTERRUPTION) HOWEVER CAUSED AND ON ANY THEORY OF LIABILITY,
+# WHETHER IN CONTRACT, STRICT LIABILITY, OR TORT (INCLUDING NEGLIGENCE OR
+# OTHERWISE) ARISING IN ANY WAY OUT OF THE USE OF THIS SOFTWARE, EVEN IF
+# ADVISED OF THE POSSIBILITY OF SUCH DAMAGE.
+
+import datetime, re
+
+from dateutil.parser import parse
+from django.apps import apps as django_apps
+from django.core.exceptions import ImproperlyConfigured
+from django.db import IntegrityError
+from django.utils import six
+from django.utils.timezone import utc
+from django.utils.translation import ugettext_lazy as _
+import jwt
+from rest_framework import serializers
+
+from . import settings
+from .compat import User
+
+
+def datetime_or_now(dtime_at=None):
+    if not dtime_at:
+        return datetime.datetime.utcnow().replace(tzinfo=utc)
+    if isinstance(dtime_at, six.string_types):
+        dtime_at = parse(dtime_at)
+    if dtime_at.tzinfo is None:
+        dtime_at = dtime_at.replace(tzinfo=utc)
+    return dtime_at
+
+
+def as_timestamp(dtime_at=None):
+    if not dtime_at:
+        dtime_at = datetime_or_now()
+    return int((
+        dtime_at - datetime.datetime(1970, 1, 1, tzinfo=utc)).total_seconds())
+
+
+def full_name_natural_split(full_name):
+    """
+    This function splits a full name into a natural first name, last name
+    and middle initials.
+    """
+    parts = full_name.strip().split(' ')
+    first_name = ""
+    if parts:
+        first_name = parts.pop(0)
+    if first_name.lower() == "el" and parts:
+        first_name += " " + parts.pop(0)
+    last_name = ""
+    if parts:
+        last_name = parts.pop()
+    if (last_name.lower() == 'i' or last_name.lower() == 'ii'
+        or last_name.lower() == 'iii' and parts):
+        last_name = parts.pop() + " " + last_name
+    middle_initials = ""
+    for middle_name in parts:
+        if middle_name:
+            middle_initials += middle_name[0]
+    return first_name, middle_initials, last_name
+
+
+def get_accept_list(request):
+    http_accept = request.META.get('HTTP_ACCEPT', '*/*')
+    return [item.strip() for item in http_accept.split(',')]
+
+
+def get_account_model():
+    """
+    Returns the ``Account`` model that is active in this project.
+    """
+    try:
+        return django_apps.get_model(settings.ACCOUNT_MODEL)
+    except ValueError:
+        raise ImproperlyConfigured(
+            "ACCOUNT_MODEL must be of the form 'app_label.model_name'")
+    except LookupError:
+        raise ImproperlyConfigured("ACCOUNT_MODEL refers to model '%s'"\
+" that has not been installed" % settings.ACCOUNT_MODEL)
+
+
+def has_invalid_password(user):
+    return user.password.startswith('!')
+
+
+def printable_name(user):
+    full_name = user.get_full_name()
+    if full_name:
+        return full_name
+    return user.username
+
+
+def update_db_row(instance, form):
+    """
+    Updates the record in the underlying database, or adds a validation
+    error in the form. When an error is added, the form is returned otherwise
+    this function returns `None`.
+    """
+    try:
+        instance.save()
+        return None
+    except IntegrityError as err:
+        err_msg = str(err).splitlines().pop()
+        # PostgreSQL unique constraint.
+        look = re.match(
+            r'DETAIL:\s+Key \(([a-z_]+)\)=\(.*\) already exists\.', err_msg)
+        if look:
+            form.add_error(look.group(1),
+                _("This %s is already taken.") % look.group(1))
+            return form
+        # SQLite unique constraint.
+        look = re.match(
+            r'UNIQUE constraint failed: [a-z_]+\.([a-z_]+)', err_msg)
+        if look:
+            form.add_error(look.group(1),
+                _("This %s is already taken.") % look.group(1))
+            return form
+        raise
+
+
+def verify_token(token):
+    try:
+        payload = jwt.decode(
+            token,
+            settings.JWT_SECRET_KEY,
+            True, # verify
+            options={'verify_exp': True},
+            algorithms=[settings.JWT_ALGORITHM])
+    except jwt.ExpiredSignature:
+        raise serializers.ValidationError(
+            _("Signature has expired."))
+    except jwt.DecodeError:
+        raise serializers.ValidationError(
+            _("Error decoding signature."))
+    username = payload.get('username', None)
+    if not username:
+        raise serializers.ValidationError(
+            _("Missing username in payload"))
+    # Make sure user exists
+    try:
+        user = User.objects.get(username=username)
+    except User.DoesNotExist:
+        raise serializers.ValidationError(_("User doesn't exist."))
+    return user
