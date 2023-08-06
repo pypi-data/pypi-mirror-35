e3899649eb0a0b4812e870e0c6b7eabf9323b632
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-06 11:49:34 +0000

    Add package pink/0.2.0/pink-0.2.0-py2.py3-none-any.whl

---
+++ packages/pink/pink-0.2.0-py2.py3-none-any.whl/pink.py
@@ -0,0 +1,31 @@
+#!/usr/bin/env python
+# coding=utf-8
+
+import os
+import sys
+
+import glob2
+import black
+
+PEP8_MAXLINE = 79
+
+
+def command_line_runner():
+    """
+    主函数
+    """
+    python = sys.executable
+    black_exec = black.__file__.rstrip("cdo")
+
+    path = " ".join(sys.argv[1:])
+    for p in path:
+        for file in glob2.glob(p):
+            res = os.popen(
+                f"{python} {black_exec} {file} --line-length {PEP8_MAXLINE}"
+            ).read()
+            print(res)
+    sys.exit()
+
+
+if __name__ == "__main__":
+    command_line_runner()
