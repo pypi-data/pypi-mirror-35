24c62bc678db6830b299846df1b24d3e8c61154a
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-06 07:10:33 +0000

    Add package brazilian-utils/0.1.0/brazilian-utils-0.1.0.tar.gz

---
+++ packages/brazilian-utils/brazilian-utils-0.1.0.tar.gz/brazilian-utils-0.1.0/setup.py
@@ -0,0 +1,12 @@
+from setuptools import setup
+
+setup(
+    name='brazilian-utils',
+    version='0.1.0',
+    packages=['brazilian_utils'],
+    url='https://github.com/brazilian-utils/python',
+    license='MIT',
+    author='Alex Rocha',
+    author_email='alex.rochas@yahoo.com.br',
+    description='Utils library for specific Brazilian businesses.'
+)
