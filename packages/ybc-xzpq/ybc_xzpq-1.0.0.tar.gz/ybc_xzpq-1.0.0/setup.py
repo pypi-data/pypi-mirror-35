17e0f44fe8b5497f12a4c1c250292bcf38c09efa
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-06 14:56:36 +0000

    Add package ybc-xzpq/1.0.0/ybc_xzpq-1.0.0.tar.gz

---
+++ packages/ybc-xzpq/ybc_xzpq-1.0.0.tar.gz/ybc_xzpq-1.0.0/setup.py
@@ -0,0 +1,12 @@
+from distutils.core import setup
+setup(
+  name = 'ybc_xzpq',
+  packages = ['ybc_xzpq'],
+  version = '1.0.0',
+  description = 'draw a xiaozhupeiqi',
+  long_description='draw a xiaozhupeiqi',
+  author = 'KingHS',
+  author_email = '382771946@qq.com',
+  keywords = ['pip3', 'turtle', 'xiaozhupeiqi'],
+  license='MIT',
+)
