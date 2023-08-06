042edb09c40f507718aa2105edf3264612af9fbd
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-06 17:09:22 +0000

    Add package tradeoftimes/0.2.0/tradeoftimes-0.2.0-py3-none-any.whl

---
+++ packages/tradeoftimes/tradeoftimes-0.2.0-py3-none-any.whl/tradet/__init__.py
@@ -0,0 +1,223 @@
+import wx
+import string
+import random
+import json
+import time
+import peertable
+import os
+import traceback
+import ipgetter
+
+from tradet.main_window import MainWindow
+
+
+class TradeGamePeer(peertable.PeerApplication):
+    def __init__(self):
+        self.messages = []
+        self.glues = []
+        self.app = wx.App.Get()
+
+    def timeout_messages(self):
+        mm = []
+    
+        for m in self.messages:
+            if time.time() - m[1] < 90:
+                mm.append(m)
+                
+        self.messages = mm
+        
+    def game_turn(self, server, client, data, message):
+        pass # no game yet...
+        
+    def receive_data(self, server, client, id, mtype, data, message):
+        if mtype == "CHAT":
+            server.broadcast(message)
+            c_id = data.decode('utf-8').split(':')[0]
+            content = ':'.join(data.split(':')[1:])
+            self.app.frame.chatWindow.SetValue(self.app.frame.chatWindow.GetValue()\
+                + "    <{}> {}\n".format(self.app.nicks.get(c_id, c_id), content)
+            )
+            
+        elif mtype == "QUIT":
+            self.app.nicks.pop(data.decode('utf-8'))
+            self.app.nick_update()
+            self.app.remove_user(data.decode('utf-8'))
+            
+        elif mtype == "SETNAME":
+            id, nick = data.decode('utf-8').split(':')
+            n = self.app.nicks.get(id)
+            self.app.nicks[id] = nick
+            self.app.nick_update()
+            
+            if n is not None:
+                self.app.frame.chatWindow.SetValue(self.app.frame.chatWindow.GetValue()\
+                    + "*** {} is now known as {}\n".format(n, self.app.nicks.get(client.id, client.id))
+                )
+            
+            server.broadcast(message)
+            
+        elif mtype == "GAMETURN" and client.id == self.app.host:
+            self.game_turn(server, client, data, message)
+        
+    def add_message(self, msg):
+        self.app.frame.chatWindow.SetValue(self.app.frame.chatWindow.GetValue()\
+            + msg + "\n"
+        )
+        
+    def broadcast_name(self, server):
+        server.broadcast(peertable.Message(True, 'SETNAME', ":".join([server.id, self.app.nick])))
+        
+    def check_message(self, server, client, message):
+        self.timeout_messages()
+        id = message.payload[:50].decode('utf-8')
+        
+        if id in [x[0] for x in self.messages]:
+            return
+            
+        self.receive_data(server, client, message.payload[:50].decode('utf-8'), message.message_type.rstrip(' '), message.payload[50:], message)
+        
+    def receive(self, server, client, message):
+        if message.message_type == "NETWGLUE":
+            glue = message.payload.decode('utf-8')
+            self.app.last_host_req = time.time()
+            server.broadcast(True, peertable.Message(True, 'WHOSHOST', server.id))
+        
+            if glue in self.glues:
+                return
+        
+            self.broadcast_name(server)
+            server.broadcast(message)
+            
+        elif message.message_type == 'WHOSHOST':
+            if self.app.elected and self.app.host == server.id:
+                server.send_id(message.payload.decode('utf-8'), peertable.Message(True, "IAMHOST"))
+                
+            elif not self.app.elected:
+                server.broadcast(peertable.Message(True, "MKMEHOST", server.id))
+        
+        elif message.message_type == "IAMHOST" and time.time() - self.app.last_host_req < 30 and not self.app.elected:
+            self.app.host = client.id
+            self.app.elected = True
+           
+        elif message.message_type == "MKMEHOST" and not self.app.elected:
+            id = message.payload.decode('utf-8')
+            
+            if self.app.host != id:
+                self.app.host = id
+                self.app.elected = True
+                server.broadcast(message)
+        
+        elif message.message_type == "MYNAMEIS":
+            self.app.nicks[client.id] = message.payload.decode('utf-8')
+            self.app.nick_update()
+            self.app.add_user(client.id)
+    
+        elif message.message_type == "HOSTDEAD":
+            self.app.elected = False
+            server.broadcast(peertable.Message(True, "WHOSHOST", server.id))
+    
+        else:
+            self.check_message(server, client, message)
+            
+    def disconnected(self, server, client):
+        if self.app.elected and self.app.host == client.id:
+            server.broadcast(peertable.Message(True, 'HOSTDEAD'))
+            self.app.elected = False
+
+class TradeGameApp(wx.App):
+    def OnInit(self):
+        self.nicks = {}
+    
+        if os.path.isfile('settings.json'):
+            config = json.load(open('settings.json'))
+            self.server = peertable.PeerServer(config['External IP'], port=config['Listen Port'], remote_port=config['External Port'])
+            nick = config['Nickname']
+        
+        else:
+            eip = ipgetter.myip()
+            nick = random.choice(("_Guest", "_Newbie", "_Invitee", "_Rookie")) + '_' + ''.join([random.choice(string.digits) for _ in range(7)])
+            
+            self.server = peertable.PeerServer(eip, port=2913)
+            open('settings.json', 'w').write(json.dumps({
+                "External IP": eip,
+                "Listen Port": 2913,
+                "External Port": 2913,
+                "Nickname": nick,
+            }))
+            
+        self.nick = nick
+        self.host = self.server.id # Corrected Once Connected
+        self.elected = False
+            
+        self.init_frame()
+        self.peer_app = TradeGamePeer()
+        self.nick_update()
+        self.server.register_app(self.peer_app)
+        self.server.start_loop()
+        
+        return True
+        
+    def write_config(self, eip=None, listen_port=None, ext_port=None):
+        open('settings.json', 'w').write(json.dumps({
+            "External IP": eip or self.server.address,
+            "Listen Port": listen_port or self.server.port,
+            "External Port": ext_port or self.server.remote_port,
+            "Nickname": self.nick
+        }))
+        
+        print("Updated settings.json file.")
+        
+    def nick_update(self):
+        res = self.nick + "\n"
+        
+        for n in self.nicks.values():
+            res += n + "\n"
+            
+        self.frame.userList.SetValue(res)
+        
+    def set_ext_addr(self, extip):
+        try:
+            if ':' in extip:
+                addr = extip.split(':')
+                port = int(addr[1])
+                addr = addr[0]
+        
+            else:
+                addr = extip
+                port = self.server.remote_port
+            
+        except BaseException as err:
+            traceback.print_exc()
+            return False
+    
+        self.server.address = addr
+        self.server.remote_port = port
+        
+        return True
+        
+    def init_frame(self):
+        self.frame = MainWindow(None)
+        self.frame.Show()
+        
+    def send_chat(self, message):
+        self.peer_app.add_message("    <{}> {}".format(self.nick, message))
+        self.server.broadcast(peertable.Message(True, 'CHAT', ''.join([random.choice(string.digits + string.ascii_letters) for _ in range(50)]) + ':'.join([self.server.id, message])))
+    
+    def quit(self):
+        self.frame.Close(True)
+        
+    def connect(self, addr):
+        self.server.connect(addr)
+        self.server.broadcast(True, peertable.Message(True, 'NETWGLUE', self.server.id))
+        self.last_host_req = time.time()
+        self.server.broadcast(True, peertable.Message(True, 'WHOSHOST', self.server.id))
+        
+    def set_nick(self, n):
+        self.nick_update()
+        self.peer_app.add_message("*** You, previously {}, are now known as {}".format(self.nick, n))
+        self.nick = n
+        self.peer_app.broadcast_name(self.server)
+        self.write_config()
+        
+    def set_listen_port(self, port):
+        pass
---
+++ packages/tradeoftimes/tradeoftimes-0.2.0-py3-none-any.whl/tradet/settings_diag.py
@@ -0,0 +1,163 @@
+# -*- coding: utf-8 -*- 
+
+###########################################################################
+## Python code generated with wxFormBuilder (version Aug  8 2018)
+## http://www.wxformbuilder.org/ - hand modified to fit the application.
+###########################################################################
+
+import wx
+import wx.xrc
+
+###########################################################################
+## Class SettingsDialog
+###########################################################################
+
+class SettingsDialog ( wx.Dialog ):
+    
+    def __init__( self, parent ):
+        wx.Dialog.__init__ ( self, parent, id = wx.ID_ANY, title = u"Trade of Times - Network Settings Page", pos = wx.DefaultPosition, size = wx.Size( 509,346 ), style = wx.DEFAULT_DIALOG_STYLE|wx.RESIZE_BORDER )
+        
+        self.SetSizeHints( wx.DefaultSize, wx.DefaultSize )
+        
+        bSizer5 = wx.BoxSizer( wx.VERTICAL )
+        
+        self.labelal = wx.StaticText( self, wx.ID_ANY, u"Network Settings", wx.DefaultPosition, wx.DefaultSize, 0 )
+        self.labelal.Wrap( -1 )
+        
+        self.labelal.SetFont( wx.Font( wx.NORMAL_FONT.GetPointSize(), wx.FONTFAMILY_DEFAULT, wx.FONTSTYLE_NORMAL, wx.FONTWEIGHT_BOLD, False, wx.EmptyString ) )
+        
+        bSizer5.Add( self.labelal, 0, wx.ALL, 5 )
+        
+        fgSizer6 = wx.FlexGridSizer( 2, 3, 0, 0 )
+        fgSizer6.AddGrowableCol( 1 )
+        fgSizer6.SetFlexibleDirection( wx.HORIZONTAL )
+        fgSizer6.SetNonFlexibleGrowMode( wx.FLEX_GROWMODE_SPECIFIED )
+        
+        self.m_staticText14 = wx.StaticText( self, wx.ID_ANY, u"External IP", wx.DefaultPosition, wx.DefaultSize, 0 )
+        self.m_staticText14.Wrap( -1 )
+        
+        fgSizer6.Add( self.m_staticText14, 0, wx.ALL, 5 )
+        
+        self.ext_ip = wx.TextCtrl( self, wx.ID_ANY, wx.EmptyString, wx.DefaultPosition, wx.DefaultSize, 0 )
+        fgSizer6.Add( self.ext_ip, 1, wx.ALL|wx.EXPAND, 5 )
+        
+        self.externalIpHelpButton = wx.Button( self, wx.ID_ANY, u"?", wx.DefaultPosition, wx.DefaultSize, wx.BU_EXACTFIT )
+        fgSizer6.Add( self.externalIpHelpButton, 0, wx.ALL, 5 )
+        
+        self.m_staticText15 = wx.StaticText( self, wx.ID_ANY, u"Peer Listen Port", wx.DefaultPosition, wx.DefaultSize, 0 )
+        self.m_staticText15.Wrap( -1 )
+        
+        fgSizer6.Add( self.m_staticText15, 0, wx.ALL, 5 )
+        
+        self.listen_port = wx.SpinCtrl( self, wx.ID_ANY, wx.EmptyString, wx.DefaultPosition, wx.DefaultSize, wx.SP_ARROW_KEYS, 2800, 2999, 2913 )
+        fgSizer6.Add( self.listen_port, 0, wx.ALL|wx.EXPAND, 5 )
+        
+        self.externalIpHelpButton1 = wx.Button( self, wx.ID_ANY, u"?", wx.DefaultPosition, wx.DefaultSize, wx.BU_EXACTFIT )
+        fgSizer6.Add( self.externalIpHelpButton1, 0, wx.ALL, 5 )
+        
+        
+        bSizer5.Add( fgSizer6, 0, wx.EXPAND, 5 )
+        
+        self.m_staticline2 = wx.StaticLine( self, wx.ID_ANY, wx.DefaultPosition, wx.DefaultSize, wx.LI_HORIZONTAL )
+        bSizer5.Add( self.m_staticline2, 0, wx.EXPAND |wx.ALL, 5 )
+        
+        bSizer17 = wx.BoxSizer( wx.VERTICAL )
+        
+        self.helplabel = wx.StaticText( self, wx.ID_ANY, u"Help Box", wx.DefaultPosition, wx.DefaultSize, wx.ALIGN_CENTER_HORIZONTAL )
+        self.helplabel.Wrap( -1 )
+        
+        self.helplabel.SetFont( wx.Font( wx.NORMAL_FONT.GetPointSize(), wx.FONTFAMILY_DEFAULT, wx.FONTSTYLE_NORMAL, wx.FONTWEIGHT_BOLD, False, wx.EmptyString ) )
+        
+        bSizer17.Add( self.helplabel, 0, wx.ALL|wx.EXPAND, 5 )
+        
+        self.helpInfo = wx.StaticText( self, wx.ID_ANY, u"Click a \"?\" button to get helpful information on the field right next to it here.", wx.DefaultPosition, wx.DefaultSize, 0 )
+        self.helpInfo.Wrap( -1 )
+        
+        self.helpInfo.SetFont( wx.Font( 9, wx.FONTFAMILY_DEFAULT, wx.FONTSTYLE_NORMAL, wx.FONTWEIGHT_NORMAL, False, wx.EmptyString ) )
+        
+        bSizer17.Add( self.helpInfo, 1, wx.ALL|wx.EXPAND, 5 )
+        
+        
+        bSizer5.Add( bSizer17, 1, wx.EXPAND, 5 )
+        
+        self.m_staticline1 = wx.StaticLine( self, wx.ID_ANY, wx.DefaultPosition, wx.DefaultSize, wx.LI_HORIZONTAL )
+        bSizer5.Add( self.m_staticline1, 0, wx.EXPAND |wx.ALL, 5 )
+        
+        bSizer6 = wx.BoxSizer( wx.HORIZONTAL )
+        
+        self.cancelButton = wx.Button( self, wx.ID_ANY, u"Cancel", wx.DefaultPosition, wx.DefaultSize, 0 )
+        bSizer6.Add( self.cancelButton, 0, wx.ALL, 5 )
+        
+        
+        bSizer6.Add( ( 0, 0), 1, wx.EXPAND, 5 )
+        
+        self.applyButton = wx.Button( self, wx.ID_ANY, u"Apply", wx.DefaultPosition, wx.DefaultSize, 0 )
+        bSizer6.Add( self.applyButton, 0, wx.ALL, 5 )
+        
+        self.okButton = wx.Button( self, wx.ID_ANY, u"Okay", wx.DefaultPosition, wx.DefaultSize, 0 )
+        bSizer6.Add( self.okButton, 0, wx.ALL, 5 )
+        
+        
+        bSizer5.Add( bSizer6, 0, wx.BOTTOM|wx.EXPAND, 5 )
+        
+        
+        self.SetSizer( bSizer5 )
+        self.Layout()
+        
+        self.Centre( wx.BOTH )
+        
+        # Connect Events
+        self.externalIpHelpButton.Bind( wx.EVT_BUTTON, self.external_ip_help )
+        self.externalIpHelpButton1.Bind( wx.EVT_BUTTON, self.listen_port_help )
+        self.cancelButton.Bind( wx.EVT_BUTTON, self.cancel )
+        self.applyButton.Bind( wx.EVT_BUTTON, self.apply )
+        self.okButton.Bind( wx.EVT_BUTTON, self.apply_quit )
+    
+    def __del__( self ):
+        pass
+    
+    def external_ip_help( self, event ):
+        self.helpInfo.SetLabel(
+                'In the "External IP" field, you must insert the IP to which others can connect to send you\n'
+                'events about their games. If not on NAT, just use your machine\'s Internet IP address.\n'
+                'Otherwise, either port forward and use your NAT\'s external IP address, or use a tunnel\n'
+                '(like ngrok) and insert your tunnel address (along with the port) here. '
+        )
+    
+    def listen_port_help( self, event ):
+        self.helpInfo.SetLabel(
+                'In the "Listen Port" field, insert the port on which to listen to connections with other\n'
+                'games. This is the port to which they will connect (along with the IP address through the\n1'
+                'which your game peer is accessible). '
+        )
+    
+    def cancel( self, event ):
+        self.Close(True)
+    
+    def apply( self, event ):
+        if not wx.App.Get().set_ext_addr(self.ext_ip.GetValue()):
+            return
+        
+        wx.App.Get().set_listen_port(self.listen_port.GetValue())
+        
+        eaddr = self.ext_ip.GetValue()
+        eport = eaddr.split(':')
+        
+        if len(eport) < 2:
+            eport = None
+            eaddr = eaddr[0]
+            
+        else:
+            eport, eaddr = eaddr
+            
+            try:
+                eport = int(eport)
+                
+            except ValueError:
+                return
+        
+        wx.App.Get().write_config(eip=eaddr, listen_port=self.listen_port.GetValue(), ext_port=eport)
+    
+    def apply_quit( self, event ):
+        self.apply(event)
+        self.Close(True)
