b08e3ecaa876721d1c5811e4e70b49c921a9ab74
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-06 11:52:38 +0000

    Add package qqai/0.2.0/qqai-0.2.0.tar.gz

---
+++ packages/qqai/qqai-0.2.0.tar.gz/qqai-0.2.0/qqai/__init__.py
@@ -0,0 +1,6 @@
+__all__ = ['nlp', 'aai', 'vision', 'Detectface', 'TextChat', 'ImgToText']
+
+import qqai.nlp
+import qqai.aai
+import qqai.vision
+from qqai.qqai import Detectface, TextChat, ImgToText
