749c5196ba95e5dcb982b1494e13dff84c54099d
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-06 14:53:58 +0000

    Add package django-debug-toolbar-alchemy/0.1.5/django-debug-toolbar-alchemy-0.1.5.tar.gz

---
+++ packages/django-debug-toolbar-alchemy/django-debug-toolbar-alchemy-0.1.5.tar.gz/django-debug-toolbar-alchemy-0.1.5/HISTORY.rst
@@ -0,0 +1,40 @@
+.. :changelog:
+
+History
+-------
+
+0.1.5 (2018-09-04)
+~~~~~~~~~~~~~~~~~~
+
+* Fixed when ``multiparams`` are name synonyms in ``UPDATE`` statements.
+* Added support for ``similar`` queries added in debug toolbar base ``SQLPanel``.
+
+0.1.4 (2018-09-04)
+~~~~~~~~~~~~~~~~~~
+
+* Added template to be included in ``dist`` package.
+
+0.1.3 (2018-09-03)
+~~~~~~~~~~~~~~~~~~
+
+* Added a way to run ``SELECT`` and ``EXPLAIN`` to queries in debug toolbar.
+  Tested in ``sqlite``, ``postgresql`` and ``oracle``.
+
+0.1.2 (2018-08-30)
+~~~~~~~~~~~~~~~~~~
+
+* Fix bug by attaching SQLAlchemy events at most once.
+  Otherwise in some cases when multiple threads are used,
+  event handlers ``deque`` would be mutated while
+  other SQLAlchemy events are running since
+  event handling is not thread-safe in SQA.
+
+0.1.1 (2018-07-13)
+~~~~~~~~~~~~~~~~~~
+
+* Fix bug which did not correctly track plain string clauses.
+
+0.1.0 (2017-10-04)
+~~~~~~~~~~~~~~~~~~
+
+* First release on PyPI.
---
+++ packages/django-debug-toolbar-alchemy/django-debug-toolbar-alchemy-0.1.5.tar.gz/django-debug-toolbar-alchemy-0.1.5/debug_toolbar_alchemy/panels/sql/panel.py
@@ -0,0 +1,49 @@
+# -*- coding: utf-8 -*-
+from __future__ import absolute_import, print_function, unicode_literals
+
+from debug_toolbar.panels import sql as panels
+from django.conf.urls import url
+
+from .tracking import unwrap_engine, wrap_engine
+from .utils import add_newlines, get_connections, swap_fields
+from .views import sql_explain, sql_select
+
+
+class SQLPanel(panels.SQLPanel):
+    template = "debug_toolbar/panels/sql-alchemy.html"
+
+    @property
+    def panel_id(self):
+        return "Alchemy" + super(SQLPanel, self).panel_id
+
+    @property
+    def engines(self):
+        return get_connections()
+
+    def enable_instrumentation(self):
+        for alias, engine in self.engines.items():
+            wrap_engine(engine, alias, self)
+
+    def disable_instrumentation(self):
+        for alias, engine in self.engines.items():
+            unwrap_engine(engine, alias, self)
+
+    def record_stats(self, stats):
+        for q in stats.get("queries", []):
+            if q.get("sql"):
+                q["sql"] = add_newlines(swap_fields(q["sql"]))
+
+        super(SQLPanel, self).record_stats(stats)
+
+        # dont allow stats to be generated multiple times per request
+        # some panels (e.g. history panel)
+        # might call it multiple times and since we massage the sql above
+        # it causes SQL to be rendered badly
+        self.generate_stats = lambda *args, **kwargs: None
+
+    @classmethod
+    def get_urls(cls):
+        return [
+            url(r"^sql_alchemy_select/$", sql_select, name="sql_alchemy_select"),
+            url(r"^sql_alchemy_explain/$", sql_explain, name="sql_alchemy_explain"),
+        ]
