7b22678778aa044929979d1ad36a5f98fc2f73b2
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-06 07:09:42 +0000

    Add package edx-enterprise-data/0.2.5/edx_enterprise_data-0.2.5-py2-none-any.whl

---
+++ packages/edx-enterprise-data/edx_enterprise_data-0.2.5-py2-none-any.whl/enterprise_data/api/v0/serializers.py
@@ -0,0 +1,26 @@
+# -*- coding: utf-8 -*-
+"""
+Serializers for enterprise api version 0 endpoint.
+"""
+from __future__ import absolute_import, unicode_literals
+
+from rest_framework import serializers
+
+from enterprise_data.models import EnterpriseEnrollment
+
+
+class EnterpriseEnrollmentSerializer(serializers.ModelSerializer):
+    """
+    Serializer for EnterpriseEnrollment model.
+    """
+    course_api_url = serializers.SerializerMethodField()
+
+    def get_course_api_url(self, obj):
+        """Constructs course api url"""
+        return '/enterprise/v1/enterprise-catalogs/{enterprise_id}/courses/{course_id}'.format(
+            enterprise_id=obj.enterprise_id, course_id=obj.course_id
+        )
+
+    class Meta:
+        model = EnterpriseEnrollment
+        exclude = ('created', )
