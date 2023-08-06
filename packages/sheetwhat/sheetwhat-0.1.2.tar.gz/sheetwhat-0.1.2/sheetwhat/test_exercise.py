1ca832f6b7f876e37c10b4b7ef41d0f56490be73
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-06 17:11:43 +0000

    Add package sheetwhat/0.1.2/sheetwhat-0.1.2.tar.gz

---
+++ packages/sheetwhat/sheetwhat-0.1.2.tar.gz/sheetwhat-0.1.2/sheetwhat/test_exercise.py
@@ -0,0 +1,35 @@
+from protowhat.Test import TestFail
+from protowhat.Reporter import Reporter
+
+from sheetwhat.sct_syntax import SCT_CTX
+from sheetwhat.State import State
+
+
+def test_exercise(sct, student_data, solution_data):
+    """
+    """
+
+    assert isinstance(sct, list)
+    assert isinstance(student_data, dict)
+    assert isinstance(solution_data, dict)
+
+    # Make sure state is created
+    if len(sct) == 0:
+        sct = [{}]
+
+    for single_sct in sct:
+        state = State(
+            student_data=student_data,
+            solution_data=solution_data,
+            sct_range=single_sct.get("range"),
+            reporter=Reporter(),
+        )
+
+        SCT_CTX["Ex"].root_state = state
+
+        try:
+            exec("\n".join(single_sct.get("sct", [])), SCT_CTX)
+        except TestFail as tf:
+            return tf.payload
+
+    return state.reporter.build_final_payload()
