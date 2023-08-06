ff7134ad0e999b0256d0ec5289dfedc7c8da536d
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-06 01:53:05 +0000

    Add package alosi/1.1.2/alosi-1.1.2.tar.gz

---
+++ packages/alosi/alosi-1.1.2.tar.gz/alosi-1.1.2/setup.py
@@ -0,0 +1,20 @@
+from setuptools import setup
+
+setup(
+    name='alosi',
+    version='1.1.2',
+    description='Utilities for the ALOSI adaptive learning system',
+    url='https://github.com/harvard-vpal/alosi',
+    author='Andrew Ang',
+    author_email='andrew_ang@harvard.edu',
+    license='Apache-2.0',
+    packages=['alosi'],
+    install_requires=[
+        'requests==2.18.4',
+        'numpy==1.14.0',
+        'pandas==0.22.0',
+        'google-auth==1.5.0',
+        'google-auth-oauthlib==0.2.0',
+        'gspread==3.0.0'
+    ]
+)
