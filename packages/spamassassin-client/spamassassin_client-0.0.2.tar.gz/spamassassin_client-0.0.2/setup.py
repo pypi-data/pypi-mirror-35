c5d8bd977e63d413149fd7649ec5d204dabe612a
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-06 04:12:44 +0000

    Add package spamassassin-client/0.0.2/spamassassin_client-0.0.2.tar.gz

---
+++ packages/spamassassin-client/spamassassin_client-0.0.2.tar.gz/spamassassin_client-0.0.2/setup.py
@@ -0,0 +1,21 @@
+import setuptools
+
+with open("README.md", "r") as fh:
+    long_description = fh.read()
+
+setuptools.setup(
+    name="spamassassin_client",
+    version="0.0.2",
+    author="Peter Matkovski",
+    author_email="p.matkovski@gmail.com",
+    description="Wrapper for SpamAssassin SPAMC deamon",
+    long_description=long_description,
+    long_description_content_type="text/markdown",
+    url="https://github.com/petermat/spamassassin_client",
+    packages=setuptools.find_packages(exclude=['tests*']),
+    classifiers=[
+        "Programming Language :: Python :: 3",
+        "License :: OSI Approved :: MIT License",
+        "Operating System :: OS Independent",
+    ],
+)
