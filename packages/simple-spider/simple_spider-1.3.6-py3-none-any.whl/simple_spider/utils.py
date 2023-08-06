fe07c3bcc76223623049144300a8c615a98c0c4c
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-06 13:21:55 +0000

    Add package simple-spider/1.3.6/simple_spider-1.3.6-py3-none-any.whl

---
+++ packages/simple-spider/simple_spider-1.3.6-py3-none-any.whl/simple_spider/utils.py
@@ -0,0 +1,23 @@
+import time
+from urllib import parse
+
+
+class Utils:
+
+    @staticmethod
+    def build_get_url(base, path, query={}):
+        if '?' in path:
+            tmp = path.split('?')
+            path = tmp[0]
+
+        query['t'] = time.time()
+        return parse.urljoin(base, "%s?%s" % (path, parse.urlencode(query)))
+
+    @staticmethod
+    def url_encode(str):
+        return parse.quote(str)
+
+    @staticmethod
+    def url_decode(str):
+        return parse.unquote(str)
+
