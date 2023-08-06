88fde35e880f952fc2b3011b5e355955359a8326
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-06 14:56:19 +0000

    Add package bilib/0.1/bilib-0.1.tar.gz

---
+++ packages/bilib/bilib-0.1.tar.gz/bilib-0.1/bilib/__init__.py
@@ -0,0 +1,2 @@
+from .file_utils import *
+from .misc_utils import *
---
+++ packages/bilib/bilib-0.1.tar.gz/bilib-0.1/setup.py
@@ -0,0 +1,13 @@
+from setuptools import setup
+
+setup(name='bilib',
+      version='0.1',
+      description="Bily's personal library",
+      author='bilylee',
+      author_email='bily.lee@qq.com',
+      license='MIT',
+      packages=['bilib'],
+      install_requires=[
+        'ipdb',
+      ],
+      zip_safe=False)
