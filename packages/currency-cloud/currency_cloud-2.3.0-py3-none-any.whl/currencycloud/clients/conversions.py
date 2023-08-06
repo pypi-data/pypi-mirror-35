66e3dc769f67f057c4b24ac12f1ae19a57ab2be0
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-06 13:22:15 +0000

    Add package currency-cloud/2.3.0/currency_cloud-2.3.0-py3-none-any.whl

---
+++ packages/currency-cloud/currency_cloud-2.3.0-py3-none-any.whl/currencycloud/clients/conversions.py
@@ -0,0 +1,41 @@
+'''This module provides a class for Conversions calls to the CC API'''
+
+from currencycloud.http import Http
+from currencycloud.resources import PaginatedCollection, Conversion
+
+
+class Conversions(Http):
+    '''This class provides an interface to the Conversions endpoints of the CC API'''
+
+    def create(self, **kwargs):
+        '''Returns a json structure containing the details of the requested conversion.'''
+        return Conversion(self, **self.post('/v2/conversions/create', kwargs))
+
+    def find(self, **kwargs):
+        '''
+        Return an array containing json structures of details of the conversions matching the
+        search criteria for the logged in user.
+        '''
+        response = self.get('/v2/conversions/find', query=kwargs)
+        data = [Conversion(self, **fields) for fields in response['conversions']]
+        return PaginatedCollection(data, response['pagination'])
+
+    def first(self, **params):
+        params['per_page'] = 1
+        return self.find(**params)[0]
+
+    def retrieve(self, resource_id, **kwargs):
+        '''Returns a json structure containing the details of the requested conversion.'''
+        return Conversion(self, **self.get('/v2/conversions/' + resource_id, query=kwargs))
+
+    def cancel(self, resource_id, **kwargs):
+        '''Returns a json structure containing the details of the conversion cancellation.'''
+        return Conversion(self, **self.post('/v2/conversions/' + resource_id + '/cancel', kwargs))
+
+    def date_change(self, resource_id, **kwargs):
+        '''Returns a json structure containing the details of the conversion date change rate.'''
+        return Conversion(self, **self.post('/v2/conversions/' + resource_id + '/date_change', kwargs))
+
+    def split(self, resource_id, **kwargs):
+        '''Returns a json structure containing split results as parent and child conversions.'''
+        return Conversion(self, **self.post('/v2/conversions/' + resource_id + '/split', kwargs))
