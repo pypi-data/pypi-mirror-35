4c5c092cfed815aa273920c6744ee970ec19c9eb
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-06 01:52:55 +0000

    Add package mixel/0.0.1/mixel-0.0.1.tar.gz

---
+++ packages/mixel/mixel-0.0.1.tar.gz/mixel-0.0.1/setup.py
@@ -0,0 +1,25 @@
+from setuptools import setup,find_packages
+from setuptools.extension import Extension
+from Cython.Build import cythonize
+import numpy
+
+setup(
+  name='mixel',
+  version='0.0.1',
+  author='Lawrence Pang',
+  author_email='lawrencepang36@gmail.com',
+  description='a small python module for recoloring images with pixels from a palette image',
+  url='https://github.com/lpang36/mixel',
+  packages=find_packages(),
+  ext_modules=[Extension('anneal',['anneal.c'])],
+  include_dirs=[numpy.get_include()],
+  py_modules=['main'],
+  platforms=['any'],
+  license='MIT',
+  classifiers=[
+    "Programming Language :: Python :: 3",
+    "Programming Language :: Python :: 2",
+    "License :: OSI Approved :: MIT License",
+    "Operating System :: OS Independent"
+  ]
+)
