361b233a3d780bc8509ffb4416b1f7b97f07e8f1
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-06 07:11:35 +0000

    Add package pingstats/0.5a1/pingstats-0.5a1.tar.gz

---
+++ packages/pingstats/pingstats-0.5a1.tar.gz/pingstats-0.5a1/setup.py
@@ -0,0 +1,39 @@
+from setuptools import setup, find_packages
+
+with open("README.rst", "r") as fh:
+    long_description = fh.read()
+
+with open("requirements.txt", "r") as f:
+    required = f.read().splitlines()
+
+setup(
+    name='pingstats',
+    version='0.5.a1',
+    description='Simple ping visualization on the CLI',
+    long_description=long_description,
+    long_description_content_type="text/x-rst",
+    url='http://gitlab.com/eclectickmediasolutions/pingstats',
+    author='Ariana Giroux',
+    author_email='ariana.giroux@gmail.com',
+    license='MIT',
+    packages=find_packages(),
+    install_requires=required,
+    zip_safe=True,
+    entry_points={
+        'console_scripts': ['pingstats=pingstats:run'],
+    },
+    classifiers=(
+                "Programming Language :: Python :: 3",
+                "License :: OSI Approved :: MIT License",
+                "Operating System :: OS Independent",
+                "Development Status :: 4 - Beta",
+                "Environment :: Console",
+                "Intended Audience :: Developers",
+                "Intended Audience :: End Users/Desktop",
+                "Intended Audience :: Information Technology",
+                "License :: OSI Approved :: MIT License",
+                "Operating System :: Unix",
+                "Topic :: System :: Networking :: Monitoring",
+                "Topic :: Utilities",
+    ),
+)
