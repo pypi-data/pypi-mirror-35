f4d69197292d2651a5ff3387ddc87b15f5c6b4a0
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-06 13:22:29 +0000

    Add package eeweather/0.3.7/eeweather-0.3.7.tar.gz

---
+++ packages/eeweather/eeweather-0.3.7.tar.gz/eeweather-0.3.7/eeweather/__version__.py
@@ -0,0 +1,8 @@
+__title__ = 'eeweather'
+__description__ = 'Weather for Open Energy Efficiency Meter'
+__url__ = 'http://github.com/openeemeter/eeweather'
+__version__ = '0.3.7'
+__author__ = 'Phil Ngo'
+__author_email__ = 'phil@openee.io'
+__license__ = 'Apache 2.0'
+__copyright__ = 'Copyright 2018 Open Energy Efficiency, Inc.'
