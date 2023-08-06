fc96bd2c39d05799229f617962e717965a369e6f
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-06 11:49:50 +0000

    Add package url-package/1.0.0.1/url_package-1.0.0.1-py2-none-any.whl

---
+++ packages/url-package/url_package-1.0.0.1-py2-none-any.whl/url_package/gglsbl/__init__.py
@@ -0,0 +1,11 @@
+#!/usr/bin/env python
+
+__all__ = [
+    'SafeBrowsingList'
+]
+
+from client import SafeBrowsingList
+
+from _version import get_versions
+__version__ = get_versions()['version']
+del get_versions
