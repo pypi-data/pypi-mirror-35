b3f62fcfc18cb0a29a4594dc8f2154ed9bd56935
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-06 14:25:15 +0000

    Add package tflibs/0.4.0.dev0/tflibs-0.4.0.dev0-py2-none-any.whl

---
+++ packages/tflibs/tflibs-0.4.0.dev0-py2-none-any.whl/tflibs/datasets/__init__.py
@@ -0,0 +1,7 @@
+from __future__ import absolute_import
+from __future__ import division
+from __future__ import print_function
+
+from feature_spec import *
+from dataset import *
+from input_pipeline import *
---
+++ packages/tflibs/tflibs-0.4.0.dev0-py2-none-any.whl/tflibs/training/__init__.py
@@ -0,0 +1,7 @@
+from __future__ import absolute_import
+from __future__ import division
+from __future__ import print_function
+
+from dispatcher import *
+from optimizer import *
+from hooks import *
