f2737d8d5dccb27e43392d031bf0c637dd64cae5
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-06 17:10:27 +0000

    Add package K-Format/0.1.1/K-Format-0.1.1.tar.gz

---
+++ packages/K-Format/K-Format-0.1.1.tar.gz/K-Format-0.1.1/kformat/kclass.py
@@ -0,0 +1,38 @@
+from . import kproperty
+
+
+__all__ = ['kclass']
+
+
+def _kclass(cls):
+
+    props = [(k, prop) for (k, prop) in cls.__annotations__.items()]
+
+    def init(self, *args):
+        prop_bytes = []
+
+        for ((k, prop), v) in zip(props, args):
+            assert isinstance(prop, kproperty.KProperty), \
+                f'{prop.__name__} is not subtype of KProperty'
+            assert type(v) in prop.expected_types, \
+                f'{prop.__class__.__name__} cannot ' \
+                f'accept {type(v).__name__} type'
+
+            setattr(self, k, (prop, v))
+            prop_bytes.append(prop.to_bytes(v))
+
+        setattr(cls, 'bytes', b''.join(prop_bytes))
+
+    setattr(cls, '__init__', init)
+
+    return cls
+
+
+def kclass(_cls=None):
+    def wrap(cls):
+        return _kclass(cls)
+
+    if _cls is None:
+        return wrap
+
+    return wrap(_cls)
