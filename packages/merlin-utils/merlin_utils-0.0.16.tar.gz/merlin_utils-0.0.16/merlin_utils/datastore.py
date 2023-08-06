5c034fa6547b8779bbfbe9ad36994823c9cb5d1d
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-06 11:51:17 +0000

    Add package merlin-utils/0.0.16/merlin_utils-0.0.16.tar.gz

---
+++ packages/merlin-utils/merlin_utils-0.0.16.tar.gz/merlin_utils-0.0.16/merlin_utils/datastore.py
@@ -0,0 +1,20 @@
+from google.cloud import datastore
+import os
+
+
+class DatastoreClient:
+    class __DatastoreClient:
+        def __init__(self):
+            self.client = datastore.Client(os.environ.get("PROJECT"))
+
+    instance = None
+
+    def __init__(self):
+        if not DatastoreClient.instance:
+            DatastoreClient.instance = DatastoreClient.__DatastoreClient()
+
+    def get_client(self):
+        return self.client
+
+    def __getattr__(self, name):
+        return getattr(self.instance, name)
