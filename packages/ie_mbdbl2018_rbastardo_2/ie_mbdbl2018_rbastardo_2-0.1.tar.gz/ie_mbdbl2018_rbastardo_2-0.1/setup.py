984cc50d8bfa9261f7622b1e3dee4a35b5319256
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-06 11:51:02 +0000

    Add package ie_mbdbl2018_rbastardo_2/0.1/ie_mbdbl2018_rbastardo_2-0.1.tar.gz

---
+++ packages/ie_mbdbl2018_rbastardo_2/ie_mbdbl2018_rbastardo_2-0.1.tar.gz/ie_mbdbl2018_rbastardo_2-0.1/setup.py
@@ -0,0 +1,11 @@
+from setuptools import setup
+
+setup(name='ie_mbdbl2018_rbastardo_2',
+      version='0.1',  # Development release
+      description='Function to return a random number using the Method of linear Congruences.',
+      url='https://github.com/rbastardo13/ie_mbdbl2018_rbastardo_2',
+      author='Rafael Bastardo',
+      author_email='rbastardo@student.ie.edu',
+      license='MIT',
+          packages=['ie_mbdbl2018_rbastardo_2'],
+      zip_safe=False)
