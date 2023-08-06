f1210e1f5e33099017762a5d7a9e30bc1cfbb459
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-06 07:10:42 +0000

    Add package hbi/2.0.4/hbi-2.0.4.tar.gz

---
+++ packages/hbi/hbi-2.0.4.tar.gz/hbi-2.0.4/setup.py
@@ -0,0 +1,54 @@
+import os
+import sys
+
+import setuptools
+
+# Avoid polluting the .tar.gz with ._* files under Mac OS X
+os.putenv('COPYFILE_DISABLE', 'true')
+
+root = os.path.dirname(__file__)
+
+description = "Hosting Based Interfacing"
+
+with open(os.path.join(root, 'README'), encoding='utf-8') as f:
+    long_description = '\n\n'.join(f.read().split('\n\n')[1:])
+
+version = '??'
+with open(os.path.join(root, 'hbi', 'version.py'), encoding='utf-8') as f:
+    exec(f.read())
+
+py_version = sys.version_info[:2]
+
+if py_version < (3, 6):
+    raise Exception("hbi requires Python >= 3.6.")
+
+packages = [
+    'hbi', 'hbi.interop', 'hbi.pool', 
+    'hbi.aio', 'hbi.util', 
+    'hbi.echo', 'hbi.cat',
+]
+
+setuptools.setup(
+    name='hbi',
+    version=version,
+    author='Compl Yue',
+    author_email='',
+    url='https://github.com/complyue/hbipy',
+    description=description,
+    long_description=long_description,
+    download_url='https://pypi.python.org/pypi/hbi',
+    packages=packages,
+    extras_require={
+    },
+    classifiers=[
+        "Development Status :: 4 - Beta",
+        "Intended Audience :: Developers",
+        "License :: OSI Approved :: BSD License",
+        "Operating System :: OS Independent",
+        "Programming Language :: Python",
+        "Programming Language :: Python :: 3",
+        "Programming Language :: Python :: 3.6",
+    ],
+    platforms='all',
+    license='BSD'
+)
