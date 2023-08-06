5449ec06c8b90ebaf622fe3f2c94c51fe5e7f287
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-06 04:11:25 +0000

    Add package lalsuite/6.48.1.dev20180828/lalsuite-6.48.1.dev20180828-cp27-cp27m-macosx_10_11_x86_64.whl

---
+++ packages/lalsuite/lalsuite-6.48.1.dev20180828-cp27-cp27m-macosx_10_11_x86_64.whl/laldetchar/__init__.py
@@ -0,0 +1,7 @@
+# Import SWIG wrappings, if available
+from .laldetchar import *
+
+__version__ = "0.3.5.1"
+
+## \addtogroup laldetchar_python
+"""This package provides Python wrappings and extensions to LALDetChar"""
