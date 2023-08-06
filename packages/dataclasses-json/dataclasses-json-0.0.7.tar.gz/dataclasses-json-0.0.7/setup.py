cd4b393e32bebc43c336ae6aed7e15e8a3d80b84
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-06 01:55:47 +0000

    Add package dataclasses-json/0.0.7/dataclasses-json-0.0.7.tar.gz

---
+++ packages/dataclasses-json/dataclasses-json-0.0.7.tar.gz/dataclasses-json-0.0.7/setup.py
@@ -0,0 +1,19 @@
+from setuptools import setup, find_packages
+
+setup(
+    name="dataclasses-json",
+    version="0.0.7",
+    packages=find_packages(exclude=("tests*",)),
+    author="lidatong",
+    author_email="charles.dt.li@gmail.com",
+    description="Easily serialize dataclasses to and from JSON",
+    url="https://github.com/lidatong/dataclasses-json",
+    license="Unlicense",
+    keywords="dataclasses json",
+    install_requires=[],
+    python_requires=">=3.7",
+    extras_require={
+        "dev": ["pytest", "ipython", "mypy"]
+    },
+    include_package_data=True
+)
