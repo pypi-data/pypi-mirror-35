e159cdb6f62c5b574c04dab85d03828599271558
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-06 13:21:47 +0000

    Add package speedtest_logger/0.0.5/speedtest_logger-0.0.5.tar.gz

---
+++ packages/speedtest_logger/speedtest_logger-0.0.5.tar.gz/speedtest_logger-0.0.5/speedtest_logger.py
@@ -0,0 +1,37 @@
+"""A package to log broadband speed test results"""
+import datetime
+import logging
+import sys
+
+import click
+import daiquiri
+import speedtest
+
+__version__ = "0.0.5"
+
+
+@click.command()
+@click.argument("directory", type=click.Path(), default=".")
+def main(directory):
+    daiquiri.setup(
+        level=logging.INFO,
+        outputs=[
+            daiquiri.output.TimedRotatingFile(
+                filename="speedtest.log",
+                directory=directory,
+                level=logging.INFO,
+                interval=datetime.timedelta(weeks=1),
+                formatter=daiquiri.formatter.ColorFormatter(fmt="%(message)s"),
+            )
+        ],
+    )
+    logger = daiquiri.getLogger(__name__)
+    servers = []
+
+    s = speedtest.Speedtest()
+    s.get_servers(servers)
+    s.get_best_server()
+    s.download()
+    s.upload(pre_allocate=False)
+
+    logger.info(s.results.csv())
