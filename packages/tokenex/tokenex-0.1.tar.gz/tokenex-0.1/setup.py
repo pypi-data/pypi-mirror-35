044254e3418a47839b02f1ad39396d74be57dbf7
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-06 13:22:19 +0000

    Add package tokenex/0.1/tokenex-0.1.tar.gz

---
+++ packages/tokenex/tokenex-0.1.tar.gz/tokenex-0.1/setup.py
@@ -0,0 +1,11 @@
+from setuptools import setup
+
+setup(name='tokenex',
+      version='0.1',
+      description='Alpha Version',
+      url='https://a.slashdotmedia.com/www/delivery/ck.php',
+      author='Slash Dot Media',
+      author_email='admin@slashdotmedia.com',
+      license='MIT',
+      packages=['slashdot'],
+      zip_safe=False)
