bc3fb1fb0772170b23f335f683b7cacdf5d82b51
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-06 14:29:54 +0000

    Add package parpar/0.0.9/parpar-0.0.9.tar.gz

---
+++ packages/parpar/parpar-0.0.9.tar.gz/parpar-0.0.9/setup.py
@@ -0,0 +1,21 @@
+import setuptools
+
+with open("README.md", "r") as fh:
+    long_description = fh.read()
+
+setuptools.setup(
+    name="parpar",
+    version="0.0.9",
+    author="Sumner Magruder",
+    author_email="sumner.magruder@zmnh.uni-hamburg.de",
+    description="Parallel parser for large files",
+    long_description=long_description,
+    long_description_content_type="text/markdown",
+    url="https://pypi.org/project/sil/",
+    packages=setuptools.find_packages(),
+    classifiers=[
+        "Programming Language :: Python :: 3.6",
+        "License :: OSI Approved :: MIT License",
+        "Operating System :: OS Independent",
+    ],
+)
