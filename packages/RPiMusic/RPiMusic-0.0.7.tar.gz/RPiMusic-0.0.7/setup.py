97da71d10126a862f7755537f19c0c92ef3b7c54
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-06 04:08:47 +0000

    Add package RPiMusic/0.0.7/RPiMusic-0.0.7.tar.gz

---
+++ packages/RPiMusic/RPiMusic-0.0.7.tar.gz/RPiMusic-0.0.7/setup.py
@@ -0,0 +1,27 @@
+import setuptools
+
+with open("README.md", "r") as fh:
+    long_description = fh.read()
+
+setuptools.setup(
+    name="RPiMusic",
+    version="0.0.7",
+    author="Aljoscha Vollmerhaus",
+    author_email='pydev@aljoscha.vollmerhaus.net',
+    description="Play URLs from AMQP messages via mpv",
+    long_description=long_description,
+    long_description_content_type="text/markdown",
+    url="https://github.com/avollmerhaus/RPiMusic",
+    classifiers=[
+        "Programming Language :: Python :: 3",
+        "License :: OSI Approved :: GNU General Public License v3 (GPLv3)",
+        "Operating System :: POSIX :: Linux",
+        "Environment :: Console"
+    ],
+    install_requires=['pika'],
+    packages=setuptools.find_packages('src'),
+    package_dir={'':'src'},
+    entry_points={
+        'console_scripts': [ 'rpimusicd = RPiMusic.rpimusicd:rpimusicd', ],
+    },
+)
