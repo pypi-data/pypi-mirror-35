b9c06ad2951512f3a8b972813246b9bbb2cda738
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-06 14:56:20 +0000

    Add package plucky/0.4.3/plucky-0.4.3.tar.gz

---
+++ packages/plucky/plucky-0.4.3.tar.gz/plucky-0.4.3/setup.py
@@ -0,0 +1,32 @@
+#!/usr/bin/env python
+from setuptools import setup
+
+setup(
+    name='plucky',
+    version='0.4.3',
+    description='Plucking (deep) keys/paths safely from python collections has never been easier.',
+    long_description=open('README.rst').read(),
+    author='Radomir Stevanovic',
+    author_email='radomir.stevanovic@gmail.com',
+    url='https://github.com/randomir/plucky',
+    license='MIT',
+    packages=['plucky'],
+    package_dir={'plucky': 'plucky'},
+    classifiers=[
+        'Development Status :: 4 - Beta',
+        'Intended Audience :: Developers',
+        'Programming Language :: Python',
+        'Programming Language :: Python :: 2',
+        'Programming Language :: Python :: 2.6',
+        'Programming Language :: Python :: 2.7',
+        'Programming Language :: Python :: 3',
+        'Programming Language :: Python :: 3.3',
+        'Programming Language :: Python :: 3.4',
+        'Programming Language :: Python :: 3.5',
+        'Programming Language :: Python :: 3.6',
+        'License :: OSI Approved :: MIT License',
+        'Operating System :: OS Independent',
+        'Topic :: Software Development :: Libraries :: Python Modules'
+    ],
+    keywords='pluck itemgetter safe nested get'
+)
