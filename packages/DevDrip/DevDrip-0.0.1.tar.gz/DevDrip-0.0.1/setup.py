3874ebf363db168289bfeedc0d8e79be65759a40
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-06 01:53:17 +0000

    Add package DevDrip/0.0.1/DevDrip-0.0.1.tar.gz

---
+++ packages/DevDrip/DevDrip-0.0.1.tar.gz/DevDrip-0.0.1/setup.py
@@ -0,0 +1,11 @@
+import setuptools
+
+setuptools.setup(
+    name="DevDrip",
+    version="0.0.1",
+    author="Alex Roe",
+    author_email="alex@wabisabiventures.com",
+    description="DevDrip client",
+    url="https://www.wabisabiventures.com",
+    packages=setuptools.find_packages(),
+)
