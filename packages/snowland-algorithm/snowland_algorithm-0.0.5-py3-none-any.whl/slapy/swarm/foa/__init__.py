860e6ecbc1c496692cd69cbb852b9624d67288c0
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-06 17:09:19 +0000

    Add package snowland-algorithm/0.0.5/snowland_algorithm-0.0.5-py3-none-any.whl

---
+++ packages/snowland-algorithm/snowland_algorithm-0.0.5-py3-none-any.whl/slapy/swarm/foa/__init__.py
@@ -0,0 +1,12 @@
+#!/usr/bin/env python
+# -*- coding: utf-8 -*-
+# @Author  : 河北雪域网络科技有限公司 A.Star
+# @contact: astar@snowland.ltd
+# @site: www.snowland.ltd
+# @file: __init__.py.py
+# @time: 2018/9/4 18:23
+# @Software: PyCharm
+
+
+from slapy.swarm.foa.Fly import Fly
+from slapy.swarm.foa.FOAEngine import FOAEngine
