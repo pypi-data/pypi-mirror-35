e043fa2d27f8e9893d7983db84609ee24a65e922
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-06 14:24:32 +0000

    Add package partial-readonly/0.0.3/partial_readonly-0.0.3.tar.gz

---
+++ packages/partial-readonly/partial_readonly-0.0.3.tar.gz/partial_readonly-0.0.3/setup.py
@@ -0,0 +1,22 @@
+#!/usr/bin/python
+
+from setuptools import setup, find_packages
+
+__author__  = 'book987'
+__version__ = '0.0.3'
+__license__ = 'MIT'
+
+setup(
+    name='partial_readonly',
+    version=__version__,
+    py_modules=['partial_readonly'],
+    author=__author__,
+    author_email='book78987book@gmail.com',
+    description='make some of the dataclass fields read-only',
+    long_description=open('README.md', 'r', encoding='utf-8').read(),
+    long_description_content_type='text/markdown',
+    url='https://github.com/book987/partial_readonly',
+    license=__license__,
+    keywords='dataclass readonly',
+    python_requires='>=3.7'
+)
