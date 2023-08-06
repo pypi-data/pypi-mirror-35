af6fa7b17c835f929944399eceb3b8df59655bc3
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-06 16:18:43 +0000

    Add package Nikola/8.0.0rc1/Nikola-8.0.0rc1.tar.gz

---
+++ packages/Nikola/Nikola-8.0.0rc1.tar.gz/Nikola-8.0.0rc1/scripts/langmatrix.py
@@ -0,0 +1,20 @@
+#!/usr/bin/env python3
+"""A matrix! Of languages!"""
+import nikola.nikola
+
+keys = ['_WINDOWS_LOCALE_GUESSES', 'MOMENTJS_LOCALES', 'PYPHEN_LOCALES', 'DOCUTILS_LOCALES']
+keys_short = ['language', 'windows', 'moment', 'pyphen', 'docutils']
+print('\t'.join(keys_short))
+
+for tr in nikola.nikola.LEGAL_VALUES['TRANSLATIONS']:
+    if isinstance(tr, tuple):
+        tr = tr[0]
+    out = tr
+    if len(out) < 8:
+        out += '\t'
+    for key in keys:
+        out += '\t'
+        out += '\x1b[37;42;1m+' if tr in nikola.nikola.LEGAL_VALUES[key] else '\x1b[37;41;1m-'
+    print(out + '\x1b[0m')
+
+print('\t'.join(keys_short))
