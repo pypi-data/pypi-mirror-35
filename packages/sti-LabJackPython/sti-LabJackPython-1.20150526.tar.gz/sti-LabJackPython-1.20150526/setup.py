3a1931f815ba449171f241d33956b06734ddc1d5
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-06 07:09:35 +0000

    Add package sti-LabJackPython/1.20150526/sti-LabJackPython-1.20150526.tar.gz

---
+++ packages/sti-LabJackPython/sti-LabJackPython-1.20150526.tar.gz/sti-LabJackPython-1.20150526/setup.py
@@ -0,0 +1,48 @@
+import sys
+try:
+    from setuptools import setup
+except ImportError:
+    from distutils.core import setup
+
+if sys.version_info[:2] < (2, 6):
+    msg = ("LabJackPython requires Python 2.6 or later. "
+           "You are using version %s.  Please "
+           "install using a supported version." % sys.version)
+    sys.stderr.write(msg)
+    sys.exit(1)
+
+CLASSIFIERS = [
+    'Development Status :: 5 - Production/Stable',
+    'Environment :: Console',
+    'Intended Audience :: Developers',
+    'License :: OSI Approved :: MIT License',
+    'Operating System :: MacOS :: MacOS X',
+    'Operating System :: Microsoft :: Windows',
+    'Operating System :: POSIX :: Linux',
+    'Programming Language :: Python',
+    'Programming Language :: Python :: 2',
+    'Programming Language :: Python :: 2.6',
+    'Programming Language :: Python :: 2.7',
+    'Programming Language :: Python :: 3',
+    'Programming Language :: Python :: 3.3',
+    'Programming Language :: Python :: 3.4',
+    'Programming Language :: Python :: 3.5',
+    'Programming Language :: Python :: 3.6',
+    'Topic :: Software Development',
+    'Topic :: Software Development :: Embedded Systems',
+    'Topic :: System :: Hardware'
+    ]
+
+setup(name='sti-LabJackPython',
+      version='1.20150526',
+      description='The LabJack Python modules for the LabJack U3, U6, UE9 and U12.',
+      license='MIT X11',
+      url='http://labjack.com/support/labjackpython',
+      author='LabJack Corporation',
+      author_email='support@labjack.com',
+      maintainer='Erich Beyer',
+      maintainer_email='erich.beyer@servertech.com',
+      classifiers=CLASSIFIERS,
+      package_dir = {'': 'src'},
+      py_modules=['LabJackPython', 'Modbus', 'u3', 'u6', 'ue9', 'u12']
+      )
