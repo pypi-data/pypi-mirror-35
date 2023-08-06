aee367265b40517ba02ae205bec1158085d524e6
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-06 16:19:09 +0000

    Add package user-query-optimizer/0.1.6/user-query-optimizer-0.1.6.tar.gz

---
+++ packages/user-query-optimizer/user-query-optimizer-0.1.6.tar.gz/user-query-optimizer-0.1.6/setup.py
@@ -0,0 +1,16 @@
+from setuptools import setup
+
+setup(
+    name='user-query-optimizer',
+    version='0.1.6',
+    author='Swathi Iyer',
+    author_email='swathii@stanford.edu',
+    packages=['user_query_optimizer'],
+    url='https://github.com/mozilla/user-query-optimizer',
+    license='LICENSE.txt',
+    description='SQL query optimization hints',
+    long_description=open('README.md').read(),
+    install_requires=[
+          'clickhouse_cli',
+      ]
+)
---
+++ packages/user-query-optimizer/user-query-optimizer-0.1.6.tar.gz/user-query-optimizer-0.1.6/user_query_optimizer/optimizations/approximates.py
@@ -0,0 +1,36 @@
+import re
+from sqlparse.sql import IdentifierList, Identifier
+from sqlparse.tokens import Keyword, DML
+
+# Optimization #1
+#   Suggest using approximate algorithms (e.g. approx_distinct() instead of COUNT(DISTINCT ...));
+#   Consider other approximations later on (when to use approx_percentile?);
+#   Extracts SELECT identifiers, looks for "COUNT(DISTINCT ....)" and adds
+#   statement as key, and (line no, "use approximation") tuple to corresponding
+#   list in self.optimizations dictionary if found;
+#   Same for all databases I think?
+
+
+def checkApproximates(optimizations, parsed_queries, *db_params):
+    for stmt_list in parsed_queries:
+        for stmt in stmt_list:
+            seen_stmt = ""
+            select_seen = False
+            for token in stmt.tokens:
+                if select_seen:
+                    if token.ttype is Keyword and token.value.upper() == "FROM":
+                        break
+                    else:
+                        if isinstance(token, IdentifierList):
+                            for identifier in token.get_identifiers():
+                                if re.search("COUNT\s*\(\s*DISTINCT", str(identifier), re.IGNORECASE):
+                                    # newlines in sqlparse sometimes group clauses together - need to recalculate
+                                    lineno = seen_stmt.count("\n")
+                                    optimizations[stmt].append((lineno, "use approximation - " + db_params[0]))
+                        elif isinstance(token, Identifier):
+                            if re.search("COUNT\s*\(\s*DISTINCT", str(token), re.IGNORECASE):
+                                lineno = seen_stmt.count("\n")
+                                optimizations[stmt].append((lineno, "use approximation - " + db_params[0]))
+                if token.ttype is DML and token.value.upper() == "SELECT":
+                    select_seen = True
+                seen_stmt += str(token)
