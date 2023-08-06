234a809171752f90f9d878d3b6e16366deccda06
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-06 16:19:33 +0000

    Add package single/0.2.0/single-0.2.0.tar.gz

---
+++ packages/single/single-0.2.0.tar.gz/single-0.2.0/setup.py
@@ -0,0 +1,28 @@
+
+from distutils.core import setup
+    
+setup(
+    name = "single",
+    version = "0.2.0",
+    py_modules = ["single"],
+    scripts = ["single.py"],
+    license = "LGPL",
+    description = "Ensure single instance of a command without leaving stale lock files.",
+    author = "tengu",
+    author_email = "karasuyamatengu@gmail.com",
+    url = "https://github.com/tengu/py-single",
+    classifiers = [
+        "License :: OSI Approved :: GNU Library or Lesser General Public License (LGPL)",
+        "Operating System :: POSIX :: Linux",
+        "Programming Language :: Python",
+        "Development Status :: 4 - Beta",
+        "Environment :: Console",
+        "Topic :: Utilities",
+        ],
+    long_description = """
+    Wrap a command with `single` to ensure there is at most single instance of the command at a time.
+    This comes in handy when periodically invoking potentially long-running commands by cron.
+    `single` avoids the problem of stale lock suffered by lock file approach. Even if the command
+    crashes the lock is cleared.
+    """
+    )
