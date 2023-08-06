86fdcc86aac4cf2079f73222c6bf2c08f5a3df1d
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-06 13:53:19 +0000

    Add package radpy/0.1dev/radpy-0.1dev.tar.gz

---
+++ packages/radpy/radpy-0.1dev.tar.gz/radpy-0.1dev/setup.py
@@ -0,0 +1,9 @@
+from distutils.core import setup
+
+setup(
+    name='radpy',
+    version='0.1dev',
+    packages=['radpy',],
+    license='BSD',
+    long_description='Rapid Algorithm Deployment in Python',
+)
