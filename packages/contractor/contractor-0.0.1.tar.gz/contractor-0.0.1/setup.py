4d7ab5d97ecb6fda877a206648d04f2577795ead
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-06 11:50:50 +0000

    Add package contractor/0.0.1/contractor-0.0.1.tar.gz

---
+++ packages/contractor/contractor-0.0.1.tar.gz/contractor-0.0.1/setup.py
@@ -0,0 +1,14 @@
+# -*- coding: utf-8 -*-
+
+from setuptools import setup
+
+setup(
+    name='contractor',
+    version='0.0.1',
+    description='Coming soon ...',
+    url='http://google.com',
+    author='Vladimir Argunov',
+    author_email='warg.silencer@gmail.com',
+    license='MIT',
+    packages=['contractor']
+)
