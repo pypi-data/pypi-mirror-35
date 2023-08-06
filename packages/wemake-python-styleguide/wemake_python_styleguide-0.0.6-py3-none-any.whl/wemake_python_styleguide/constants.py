b5a2d96d7e86ea637ac826832d1f46344c7ea145
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-06 04:12:27 +0000

    Add package wemake-python-styleguide/0.0.6/wemake_python_styleguide-0.0.6-py3-none-any.whl

---
+++ packages/wemake-python-styleguide/wemake_python_styleguide-0.0.6-py3-none-any.whl/wemake_python_styleguide/constants.py
@@ -0,0 +1,95 @@
+# -*- coding: utf-8 -*-
+
+"""
+This module contains list of white- and black-listed ``python`` members.
+
+It contains lists of keywords and built-in functions we discourage to use.
+It also contains some exceptions that we allow to use in our codebase.
+"""
+
+#: List of functions we forbid to use.
+BAD_FUNCTIONS = frozenset((
+    # Code generation:
+    'eval',
+    'exec',
+    'compile',
+
+    # Magic:
+    'globals',
+    'locals',
+    'vars',
+    'dir',
+
+    # IO:
+    'input',
+    'help',
+
+    # Attribute access:
+    'hasattr',
+    'delattr',
+
+    # Misc:
+    'copyright',
+
+    # Dynamic imports:
+    '__import__',
+
+    # OOP:
+    'staticmethod',
+))
+
+#: List of module metadata we forbid to use.
+BAD_MODULE_METADATA_VARIABLES = frozenset((
+    '__author__',
+    '__all__',
+    '__version__',
+    '__about__',
+))
+
+#: List of variable names we forbid to use.
+BAD_VARIABLE_NAMES = frozenset((
+    'data',
+    'result',
+    'results',
+    'item',
+    'items',
+    'value',
+    'values',
+    'val',
+    'vals',
+    'var',
+    'vars',
+    'content',
+    'contents',
+    'info',
+    'handle',
+    'handler',
+    'file',
+    'klass',
+
+    # Compatibility with `python3.7`:
+    'async',
+    'await',
+))
+
+#: List of magic methods that are forbiden to use.
+BAD_MAGIC_METHODS = frozenset((
+    '__del__',
+))
+
+#: List of nested classes' names we allow to use.
+NESTED_CLASSES_WHITELIST = frozenset((
+    'Meta',
+))
+
+#: List of nested functions' names we allow to use.
+NESTED_FUNCTIONS_WHITELIST = frozenset((
+    'decorator',
+    'factory',
+))
+
+#: List of allowed ``__future__`` imports.
+FUTURE_IMPORTS_WHITELIST = frozenset((
+    'annotations',
+    'generator_stop',
+))
