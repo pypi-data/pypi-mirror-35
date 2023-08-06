aa01e42ec4a018ccb762ab9fc5a33c7914e23564
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-06 14:28:21 +0000

    Add package yann/0.0.11/yann-0.0.11-py3-none-any.whl

---
+++ packages/yann/yann-0.0.11-py3-none-any.whl/yann/viz/plot.py
@@ -0,0 +1,68 @@
+from matplotlib import pylab as plt
+import numpy as np
+import datetime
+import pathlib
+
+
+def plot_line(
+    y,
+    x=None,
+    figsize=None,
+    window=1,
+    xlim=None,
+    ylim=None,
+    line_width=2,
+    stroke='-',
+    title=None,
+    xlabel=None,
+    ylabel=None,
+    show=True,
+    save=False,
+    legend=True,
+    name=None,
+    grid=True
+):
+
+  if figsize:
+    fig = plt.figure(figsize=figsize)
+
+  if xlim:
+    plt.xlim(xlim)
+  if ylim:
+    plt.ylim(ylim)
+
+  if xlabel:
+    plt.xlabel(xlabel)
+  if ylabel:
+    plt.ylabel(ylabel)
+  if title:
+    plt.title(title)
+
+  if window != 1:
+    y = [np.mean(y[i:i + window])
+         for i in range(0, len(y), window)]
+
+  if x is None:
+    x = [n * window for n in range(len(y))]
+  elif len(x) != len(y):
+    x = [np.mean(x[i:i + window])
+         for i in range(0, len(x), window)]
+
+  plt.plot(x, y, stroke, lw=line_width, label=name and str(name))
+  if grid:
+    plt.grid()
+
+  if legend:
+    plt.legend(loc='best')
+
+  if save and show:
+    raise ValueError("Can't show and save at the same time")
+  if show:
+    plt.show()
+  if save:
+    plt.savefig(save if isinstance(save, (str, pathlib.Path)) else
+                f"{title or ylabel or datetime.datetime.utcnow().strftime('%y-%m-%dT%H%M%S')}.jpg")
+    plt.gcf().clear()
+
+
+
