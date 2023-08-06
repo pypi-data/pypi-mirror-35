f066f69aae2be745b89e2c378f7cf34853d16d7e
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-06 17:10:00 +0000

    Add package bayrell-common/0.0.2/bayrell-common-0.0.2.tar.gz

---
+++ packages/bayrell-common/bayrell-common-0.0.2.tar.gz/bayrell-common-0.0.2/BayrellCommon/__init__.py
@@ -0,0 +1,13 @@
+# -*- coding: utf-8 -*-
+
+
+
+__name__ = "bayrell-common"
+__version__ = "0.0.2"
+__description__ = "Bayrell Common Library"
+__license__ = "Apache License Version 2.0"
+__author__ = "Ildar Bikmamatov"
+__email__ = "ildar@bayrell.org"
+__copyright__ = "Copyright 2016-2018"
+__url__ = "https://github.com/bayrell/common_py3"
+
