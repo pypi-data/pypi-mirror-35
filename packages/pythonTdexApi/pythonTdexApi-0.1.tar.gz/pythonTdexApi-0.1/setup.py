62b9038837c4a642fbf1fa33f58c8577e86df1cc
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-06 11:52:35 +0000

    Add package pythonTdexApi/0.1/pythonTdexApi-0.1.tar.gz

---
+++ packages/pythonTdexApi/pythonTdexApi-0.1.tar.gz/pythonTdexApi-0.1/setup.py
@@ -0,0 +1,11 @@
+import setuptools
+from setuptools import setup
+setup(name='pythonTdexApi',
+      version='0.1',
+      description='python-tdex-api',
+      url='https://github.com/tdex-exchange/python-tdex-api',
+      author='lamsam',
+      author_email='sam_yulam@163.com',
+      license='MIT',
+      packages=setuptools.find_packages(),
+      zip_safe=False)
