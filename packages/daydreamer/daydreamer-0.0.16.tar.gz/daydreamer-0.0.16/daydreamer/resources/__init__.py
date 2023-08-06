8f1c633ffd45ce02aac266d6b193b4a79837d68b
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-06 01:56:30 +0000

    Add package daydreamer/0.0.16/daydreamer-0.0.16.tar.gz

---
+++ packages/daydreamer/daydreamer-0.0.16.tar.gz/daydreamer-0.0.16/daydreamer/resources/__init__.py
@@ -0,0 +1,28 @@
+from daydreamer.resources.baseresource import BaseResource
+
+from daydreamer.resources.referencedataresources import (
+    SportsDetails,
+    CurrencyDetails,
+    PeriodDetails,
+    LeagueDetails,
+)
+
+from daydreamer.resources.accountresources import AccountDetails
+
+from daydreamer.resources.marketdataresources import (
+    FixtureDetails,
+    SettledFixtureDetails,
+    SpecialFixtureDetails,
+    SettledSpecialFixtureDetails,
+    OddsDetails,
+    SpecialOddsDetails,
+    LineDetails,
+    SpecialLineDetails,
+    InRunningDetails,
+)
+
+from daydreamer.resources.bettingresources import (
+    BetDetails,
+    PlaceBetDetails,
+    PlaceSpecialBetDetails,
+)
