e37647c61105e62b9e0ea14ae47d0d571f8a7f5e
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-06 01:56:42 +0000

    Add package pynumparser/1.4.1/pynumparser-1.4.1.tar.gz

---
+++ packages/pynumparser/pynumparser-1.4.1.tar.gz/pynumparser-1.4.1/setup.py
@@ -0,0 +1,42 @@
+#!/usr/bin/env python
+#
+#  Usage:
+#     python2 setup.py sdist -uroot -groot
+#     twine upload -u NVRAM $(find dist -type f -mtime -1)
+#
+
+from distutils.core import setup
+
+import pynumparser
+
+name = 'pynumparser'
+url = 'https://gitlab.com/n2vram/' + name
+
+setup(
+    name=name,
+    version=pynumparser.version,
+    description=pynumparser.description,
+    long_description=open('README.rst').read(),
+    license='MIT',
+    author='NVRAM',
+    author_email='nvram@users.sourceforge.net',
+    url=url,
+    classifiers=[
+        'Development Status :: 5 - Production/Stable',
+        'Intended Audience :: Developers',
+        'License :: OSI Approved :: MIT License',
+        'Operating System :: OS Independent',
+        'Programming Language :: Python',
+        'Programming Language :: Python :: 2.7',
+        'Programming Language :: Python :: 3',
+        'Topic :: Text Processing :: Filters',
+        'Topic :: Utilities',
+        'Topic :: Software Development :: Libraries :: Python Modules',
+    ],
+    keywords=['argparse', 'numparser', 'parser', 'numbers', 'parsing',
+              'ArgumentParser', 'command-line', 'sequences'],
+
+    py_modules=[name],
+    download_url=(url + '/archive/' + pynumparser.version + '.zip'),
+    platforms=['any'],
+)
