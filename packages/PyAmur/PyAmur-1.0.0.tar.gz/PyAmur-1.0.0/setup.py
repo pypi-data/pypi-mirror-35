4917c002d2e81fba4ea728714a4192bba1da041a
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-06 14:57:56 +0000

    Add package PyAmur/1.0.0/PyAmur-1.0.0.tar.gz

---
+++ packages/PyAmur/PyAmur-1.0.0.tar.gz/PyAmur-1.0.0/setup.py
@@ -0,0 +1,10 @@
+from setuptools import setup, find_packages
+
+setup(
+    name='PyAmur',
+    version='1.0.0',
+    packages=find_packages(),
+    author='Roman Inozemtsev',
+    author_email='johnsmith@example.com',
+    url='https://github.com/amur-host/pyamur',
+)
