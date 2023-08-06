c938d376e78aa0a96869b90d06ff1ab41410882d
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-06 15:34:06 +0000

    Add package pagilist/0.0.1/pagilist-0.0.1.tar.gz

---
+++ packages/pagilist/pagilist-0.0.1.tar.gz/pagilist-0.0.1/README.md
@@ -0,0 +1,4 @@
+# Pagilist
+
+Simple module to paginate lists using page or limit/offset. Source code available at
+[Github](https://github.com/jersobh/pagilist).
---
+++ packages/pagilist/pagilist-0.0.1.tar.gz/pagilist-0.0.1/setup.py
@@ -0,0 +1,21 @@
+import setuptools
+
+with open("README.md", "r") as fh:
+    long_description = fh.read()
+
+setuptools.setup(
+    name="pagilist",
+    version="0.0.1",
+    author="jersobh",
+    author_email="jersobh@gmail.com",
+    description="List pagination by page or limit,offset",
+    long_description=long_description,
+    long_description_content_type="text/markdown",
+    url="https://github.com/pypa/sampleproject",
+    packages=setuptools.find_packages(),
+    classifiers=[
+        "Programming Language :: Python :: 3",
+        "License :: OSI Approved :: MIT License",
+        "Operating System :: OS Independent",
+    ],
+)
