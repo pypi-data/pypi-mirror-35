571c0887ff71a20567f4f3171fe18f4f6f1c7ea9
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-06 07:09:27 +0000

    Add package publicdata/0.2.2/publicdata-0.2.2.tar.gz

---
+++ packages/publicdata/publicdata-0.2.2.tar.gz/publicdata-0.2.2/publicdata/__init__.py
@@ -0,0 +1,8 @@
+# Copyright (c) 2017 Civic Knowledge. This file is licensed under the terms of the
+# MIT, included in this distribution as LICENSE
+
+""" """
+
+from rowgenerators.core import get_generator
+from rowgenerators.appurl import parse_app_url
+from rowgenerators.appurl.web import Downloader
