aa5fc850f51a7059947e11795dd6c9865d0944eb
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-06 14:25:43 +0000

    Add package doo/0.2.4/doo-0.2.4.tar.gz

---
+++ packages/doo/doo-0.2.4.tar.gz/doo-0.2.4/doo/__init__.py
@@ -0,0 +1,34 @@
+import os
+import sys
+from shutil import copyfile
+
+
+def mkdir(path):
+    path = path.strip()
+    path = path.rstrip("\\")
+
+    isExists = os.path.exists(path)
+
+    if not isExists:
+        #print(path + ' 创建成功')
+        os.makedirs(path)
+        return True
+    else:
+        print(path + ' 目录已存在')
+        return False
+
+
+def doo():
+    sweetest_dir = os.path.dirname(os.path.realpath(__file__))
+    current_dir = os.getcwd()
+    doo_folder = os.path.join(current_dir, 'doo_example')
+    if not mkdir(doo_folder):
+        return
+    copyfile(os.path.join(sweetest_dir, 'example.xlsx'),
+             os.path.join(doo_folder, 'example.xlsx'))
+    copyfile(os.path.join(sweetest_dir, 'app.py'),
+             os.path.join(doo_folder, 'app.py'))
+    copyfile(os.path.join(sweetest_dir, 'doo.postman_collection.json'),
+             os.path.join(doo_folder, 'doo.postman_collection.json'))
+
+    print('\n生成 doo example 成功\n')
