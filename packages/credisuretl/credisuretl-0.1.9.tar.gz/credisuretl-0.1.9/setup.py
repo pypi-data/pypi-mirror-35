5027a9e3fa202604ced1e7cabcaf7c32b646b9d0
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-06 07:07:57 +0000

    Add package credisuretl/0.1.9/credisuretl-0.1.9.tar.gz

---
+++ packages/credisuretl/credisuretl-0.1.9.tar.gz/credisuretl-0.1.9/setup.py
@@ -0,0 +1,20 @@
+from setuptools import setup, find_packages
+
+setup(name='credisuretl',
+      version='0.1.9',
+      description='ETL for Felsim',
+      url='http://github.com/fedegos/credisuretl',
+      author='Federico Gosman',
+      author_email='federico@equipogsm.com',
+      license='MIT',
+      packages=find_packages(),
+      entry_points={
+          'console_scripts': [
+              'credisur = credisur.__main__:main'
+          ]
+      },
+      install_requires=[
+            'openpyxl==2.4.7',
+            'xlrd'
+      ],
+      zip_safe=False)
