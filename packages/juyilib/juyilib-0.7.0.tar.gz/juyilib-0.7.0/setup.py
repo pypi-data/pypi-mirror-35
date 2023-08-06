fba87d754719dc6752e63c17a31303e05451ea14
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-06 13:54:15 +0000

    Add package juyilib/0.7.0/juyilib-0.7.0.tar.gz

---
+++ packages/juyilib/juyilib-0.7.0.tar.gz/juyilib-0.7.0/setup.py
@@ -0,0 +1,22 @@
+from __future__ import print_function
+from setuptools import setup, find_packages
+import sys
+
+setup(
+    name="juyilib",
+    version="0.7.0",
+    author="Seakingii",
+    author_email="seakingii@gmail.com",
+    description="A Python library for myself.",
+    long_description="",
+    license="MIT",
+    url="http://bittool.pro",
+    packages=find_packages(),
+    classifiers=[       
+        'License :: OSI Approved :: MIT License',        
+        "Operating System :: OS Independent",
+        'Programming Language :: Python :: 3',
+
+    ],
+    zip_safe=True,
+)
