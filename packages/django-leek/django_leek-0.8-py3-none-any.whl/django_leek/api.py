71ea22ba709c52bcc15f5e74258daa5cb1527e99
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-06 15:34:42 +0000

    Add package django-leek/0.8/django_leek-0.8-py3-none-any.whl

---
+++ packages/django-leek/django_leek-0.8-py3-none-any.whl/django_leek/api.py
@@ -0,0 +1,28 @@
+import socket
+from functools import wraps, partial
+from . import helpers
+from .settings import HOST, PORT
+
+
+class Leek(object):
+    def task(self, f, pool=None):
+        pool_name = pool or f.__name__
+        @wraps(f)
+        def _offload(*args, **kwargs):
+            return push_task_to_queue(f, pool_name=pool_name, *args, **kwargs)
+        f.offload = _offload
+        return f
+
+
+def push_task_to_queue(a_callable, *args, **kwargs):
+    """Original API"""
+    pool_name = kwargs.pop('pool_name', None)
+    sock = socket.socket(socket.AF_INET, socket.SOCK_STREAM)    
+    new_task = partial(a_callable, *args, **kwargs)
+    queued_task = helpers.save_task_to_db(new_task, pool_name)
+    sock.connect((HOST, PORT))
+    sock.send("{}".format(queued_task.id).encode())
+    received = sock.recv(1024)
+    sock.close()
+
+    return received
