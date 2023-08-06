4697d943d1512aa33fa3660a24410a68d69e4f90
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-06 13:20:43 +0000

    Add package m-threadpool/0.2/m-threadpool-0.2.tar.gz

---
+++ packages/m-threadpool/m-threadpool-0.2.tar.gz/m-threadpool-0.2/setup.py
@@ -0,0 +1,10 @@
+from setuptools import setup
+setup(name='m-threadpool',
+      version='0.2',
+      description='',
+      url='https://github.com/mobiovn',
+      author='MOBIO',
+      author_email='contact@mobio.vn',
+      license='MIT',
+      packages=['mobio/libs/thread_pool'],
+      install_requires=[])
