0812c17e134e46d7e848cc3f5208bf4fdb1efb5d
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-06 13:55:12 +0000

    Add package whippy/0.8.2/whippy-0.8.2.tar.gz

---
+++ packages/whippy/whippy-0.8.2.tar.gz/whippy-0.8.2/README.md
@@ -0,0 +1,10 @@
+Whippy
+======
+
+Whippy scaffolds WordPress sites on the LEMP stack. There's still quite
+a bit of work to complete...here's what's coming:
+
+1. Expand beyond sub-directories to full websites (with corresponding NGINX configuration)
+1. Custom NGINX configurations
+1. SSL support
+1. ???
---
+++ packages/whippy/whippy-0.8.2.tar.gz/whippy-0.8.2/setup.py
@@ -0,0 +1,16 @@
+from setuptools import setup, find_packages
+
+setup(name='whippy',
+      version='0.8.2',
+      description='Rapidly scaffold WordPress sites on Linux NGINX servers from the command line. Unrelated to the wppy'\
+                  ' project, although both fill the same hole.',
+      url='https://github.com/erikcaineolson/whippy',
+      author='Erik C. Olson',
+      author_email='erikcaineolson@gmail.com',
+      license='MIT',
+      packages=find_packages(),
+      classifiers=[
+          "Programming Language :: Python :: 3",
+          "License :: OSI Approved :: MIT License",
+          "Operating System :: OS Independent",
+      ], )
