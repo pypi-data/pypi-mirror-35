c7394312028c194c89dcfbe74426fdea9aa145b4
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-06 04:09:21 +0000

    Add package padar-parallel/0.2.2/padar_parallel-0.2.2.tar.gz

---
+++ packages/padar-parallel/padar_parallel-0.2.2.tar.gz/padar_parallel-0.2.2/setup.py
@@ -0,0 +1,15 @@
+from setuptools import setup, find_packages
+
+setup(
+    name='padar_parallel',
+    version='0.2.2',
+    packages=find_packages(),
+    include_package_data=True,
+    description='''Extension to do parallel computing on grouped chunks or files
+    for padar package''',
+    long_description=open('README.md').read(),
+    install_requires=[
+        "dask[complete]",
+        "padar_converter"
+    ],
+)
