f774d913b93c1dc626f72b390abc1a7b35090411
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-06 14:29:20 +0000

    Add package qaviton/0.0.2/qaviton-0.0.2.tar.gz

---
+++ packages/qaviton/qaviton-0.0.2.tar.gz/qaviton-0.0.2/qaviton/settings.py
@@ -0,0 +1,16 @@
+# TODO: change these to qaviton hub url
+
+
+webdriver_url = 'http://127.0.0.1:4444/wd/hub'
+mobiledriver_url = 'http://localhost:4723/wd/hub'
+
+
+webdriver_retries = 2
+webdriver_page_load_timeout = 30
+webdriver_connection_timeout = 60
+
+mobiledriver_retries = 0
+mobiledriver_page_load_timeout = 30
+mobiledriver_connection_timeout = 500
+
+page_timeout = 60
