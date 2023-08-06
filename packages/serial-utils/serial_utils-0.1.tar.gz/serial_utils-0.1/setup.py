869dca90c7dc6743f58166095e6760604c3e60c8
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-06 16:18:23 +0000

    Add package serial-utils/0.1/serial_utils-0.1.tar.gz

---
+++ packages/serial-utils/serial_utils-0.1.tar.gz/serial_utils-0.1/setup.py
@@ -0,0 +1,10 @@
+from setuptools import setup
+
+setup(
+    name='serial_utils',    # This is the name of your PyPI-package.
+    version='0.1',                          # Update the version number for new releases
+    scripts=['serial_utils'],
+    url='https://github.com/ryanGT/serial_utils',
+    author='Ryan Krauss',
+    author_email='ryanwkrauss@gmail.com'
+)
