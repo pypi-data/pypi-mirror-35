6c77bf28fe5a63da3d9f0737d56e5ba13263f798
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-06 13:22:16 +0000

    Add package pipupgrade/0.2.2/pipupgrade-0.2.2.tar.gz

---
+++ packages/pipupgrade/pipupgrade-0.2.2.tar.gz/pipupgrade-0.2.2/pipupgrade/_compat.py
@@ -0,0 +1,17 @@
+# imports - standard imports
+import sys
+
+PY2 = int(sys.version.split(".")[0]) == 2
+
+if PY2:
+    import __builtin__ as builtins
+
+    # moves
+    from urllib2  import urlopen
+    from urllib2  import HTTPError
+else:
+    import builtins
+
+    # moves
+    from urllib.request import urlopen
+    from urllib.error   import HTTPError
