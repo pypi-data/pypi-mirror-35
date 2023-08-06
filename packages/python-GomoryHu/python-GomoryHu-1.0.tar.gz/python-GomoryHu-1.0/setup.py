e6e6459a929dcc5a3749a3323520531691b41b80
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-06 17:16:19 +0000

    Add package python-GomoryHu/1.0/python-GomoryHu-1.0.tar.gz

---
+++ packages/python-GomoryHu/python-GomoryHu-1.0.tar.gz/python-GomoryHu-1.0/setup.py
@@ -0,0 +1,11 @@
+import setuptools
+from distutils.core import setup
+
+setup(name='python-GomoryHu',
+      version='1.0',
+      description='Gomory-Hu tree Data Structure Algorithm for Python with Gusfield Algorithm',
+      author='Marios Papachristou',
+      author_email='papachristoumarios@gmail.com',
+      url='https://github.com/papachristoumarios/python-GomoryHu',
+      packages=['gomory_hu'],
+     )
