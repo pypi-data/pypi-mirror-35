0006c8a2cd294827f42e63324b52770a1381409d
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-06 04:13:57 +0000

    Add package funniestyi/0.1/funniestyi-0.1.tar.gz

---
+++ packages/funniestyi/funniestyi-0.1.tar.gz/funniestyi-0.1/setup.py
@@ -0,0 +1,11 @@
+from setuptools import setup
+
+setup(name='funniestyi',
+      version='0.1',
+      description='The funniest joke in the world',
+      url='https://github.com/dymodi/machine_learning',
+      author='Flying Circus',
+      author_email='dymodi@gmail.com',
+      license='MIT',
+      packages=['funniest'],
+      zip_safe=False)
