17c9d4387d70538e7c2376cdfc62bd76dbbb348c
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-06 14:25:57 +0000

    Add package pytf2/2.2.1/pytf2-2.2.1.tar.gz

---
+++ packages/pytf2/pytf2-2.2.1.tar.gz/pytf2-2.2.1/pytf2/exceptions.py
@@ -0,0 +1,25 @@
+class Error(Exception):
+    pass
+
+
+class BadStatusError(Error):
+    def __init__(self, url, status, response):
+        super().__init__("{} returned the bad status code {}".format(url, status))
+
+        self.url = url
+        self.status = status
+        self.response = response
+
+
+class BadResponseError(Error):
+    def __init__(self, url, response):
+        super().__init__("{} gave a bad response (see BadResponseError.response - type varies)".format(url))
+
+        self.response = response
+
+
+class KeyNotSetError(Error):
+    def __init__(self, var):
+        super().__init__("{} not set".format(var))
+
+        self.var = var
