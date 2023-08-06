addbe1dd678b9c832888d96c63200298a1301bb2
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-06 16:16:22 +0000

    Add package zcz/1.1.0/zcz-1.1.0.tar.gz

---
+++ packages/zcz/zcz-1.1.0.tar.gz/zcz-1.1.0/zhoufirst.py
@@ -0,0 +1,11 @@
+"""这是周长智写的第一个python模块"""
+def print_cz(the_list, level):
+	"""This is the first python 
+	   codes."""
+	for each_item in the_list:
+		if isinstance(each_item, list):
+			print_cz(each_item, level+1)
+		else:
+			for i in range(level):
+				print('\t',end='')
+			print(each_item)
