c12ec20df7d353c17d02e03e8bab1e2843ddfc4e
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-06 13:22:25 +0000

    Add package botstat/0.0.4/botstat-0.0.4.tar.gz

---
+++ packages/botstat/botstat-0.0.4.tar.gz/botstat-0.0.4/setup.py
@@ -0,0 +1,41 @@
+from setuptools import setup
+
+setup(
+    name='botstat',
+    version='0.0.4',
+    description='Search engine analyser for nginx/apache web servers',
+    long_description=open('README').read(),
+    license='MIT',
+    url='https://www.endurantdevs.com',
+    author='Endurant Devs',
+    author_email='info@endurantdevs.com',
+    classifiers=[
+        'Development Status :: 4 - Beta',
+        'License :: OSI Approved :: MIT License',
+        'Environment :: Console',
+        'Intended Audience :: Developers',
+        'Intended Audience :: System Administrators',
+        'Programming Language :: Python :: 2',
+        'Programming Language :: Python :: 2.6',
+        'Programming Language :: Python :: 2.7',
+        'Programming Language :: Python :: 3',
+        'Programming Language :: Python :: 3.2',
+        'Programming Language :: Python :: 3.3',
+        'Programming Language :: Python :: 3.4',
+        'Programming Language :: Python :: 3.5',
+        'Programming Language :: Python :: 3.6',
+        'Programming Language :: Python :: 3.7',
+    ],
+    keywords='cli apache nginx system',
+    packages=['botstat'],
+    setup_requires=["pytest-runner"],
+    tests_require=["pytest"],
+    install_requires=['pyparsing', 'dateutils', 'ua-parser',
+                      'user-agents', 'apache-log-parser',
+                      'ConfigArgParse'],
+    entry_points={
+        'console_scripts': [
+            'botstat = botstat.botstat:main',
+        ],
+    },
+)
