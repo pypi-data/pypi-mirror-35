dedfbd2a3f32fb6f7b240d908ebbd8ffff986167
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-06 14:24:18 +0000

    Add package probspellchecker/0.1.2/probspellchecker-0.1.2.tar.gz

---
+++ packages/probspellchecker/probspellchecker-0.1.2.tar.gz/probspellchecker-0.1.2/setup.py
@@ -0,0 +1,23 @@
+from setuptools import setup
+
+with open("README.md") as f:
+    long_description = f.read()
+
+setup(
+    name="probspellchecker",
+    version="0.1.2",
+    description="probabilistic spell checker",
+    license="LGPLv3",
+    long_description=long_description,
+    long_description_content_type="text/markdown",
+    author="digitalarbeiter",
+    author_email="digitalarbeiter@talbriefkasten.de",
+    url="https://github.com/digitalarbeiter/probspellchecker",
+    packages=["probspellchecker"],
+    install_requires=[
+    ],
+    scripts=[
+        "scripts/probdict-from-dewiki.py",
+        "scripts/probdict-from-text.py",
+    ]
+)
