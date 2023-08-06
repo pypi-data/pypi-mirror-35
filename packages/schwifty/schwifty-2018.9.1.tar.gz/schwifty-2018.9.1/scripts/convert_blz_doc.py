d5e53f9ae4869de5ed3aabb1d7f36e72ed6862e1
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-06 15:30:16 +0000

    Add package schwifty/2018.9.1/schwifty-2018.9.1.tar.gz

---
+++ packages/schwifty/schwifty-2018.9.1.tar.gz/schwifty-2018.9.1/scripts/convert_blz_doc.py
@@ -0,0 +1,32 @@
+import csv
+import json
+import sys
+
+
+if __name__ == '__main__':
+    with open(sys.argv[1], 'r') as fp:
+        all_fieldnames = [
+            'id', 'bank_code', 'feature', 'name', 'postal_code', 'place', 'short_name', 'pan',
+            'bic', 'check_digit_method', 'record_number', 'mod_number', 'tbd',
+            'successor_bank_code']
+        data = csv.DictReader(fp, all_fieldnames, delimiter=';')
+
+        fieldnames = ('bank_code', 'name', 'short_name', 'bic', 'tbd')
+        cleaned = []
+        next(data)  # Skip header line
+        for row in data:
+            if not row['bank_code'].strip():
+                continue
+            for key, value in row.items():
+                if key == 'feature':
+                    row['primary'] = value.strip() == '1'
+                if key not in fieldnames:
+                    del row[key]
+                    continue
+                row[key] = value.strip()
+            row['country_code'] = 'DE'
+            if row['bic']:
+                cleaned.append(row)
+
+    with open('schwifty/bank-registry.json', 'w') as fp:
+        json.dump(cleaned, fp, indent=2)
