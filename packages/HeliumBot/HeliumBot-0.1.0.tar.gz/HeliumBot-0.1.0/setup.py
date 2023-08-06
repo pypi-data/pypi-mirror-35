d974fad69f344ae31f951091ff65beb9e2668687
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-06 16:17:19 +0000

    Add package HeliumBot/0.1.0/HeliumBot-0.1.0.tar.gz

---
+++ packages/HeliumBot/HeliumBot-0.1.0.tar.gz/HeliumBot-0.1.0/setup.py
@@ -0,0 +1,23 @@
+#!/usr/bin/env python
+
+import setuptools
+from distutils.core import setup
+
+setup(
+  name='HeliumBot',
+  version='0.1.0',
+  description='A Discord bot framework in Hy. Supports loading Hy and Python plugins',
+  author='Nickolay Ilyushin',
+  author_email='nickolay02@inbox.ru',
+  url='https://github.com/handicraftsman/heliumbot',
+  packages=['heliumbot', 'heliumbot.bot', 'heliumbot.cmd', 'heliumbot.plugins'],
+  classifiers=[
+    'Development Status :: 3 - Alpha',
+    'Programming Language :: Python :: 3 :: Only',
+    'Programming Language :: Lisp',
+    'License :: OSI Approved :: MIT License',
+    'Topic :: Communications :: Chat'
+  ],
+  license='MIT',
+  keywords=['discord', 'bot', 'framework', 'hy']
+)
