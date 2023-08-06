bf26973712894b8ee1fb30bc8f3e8f7de8b7e5ea
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-06 04:13:53 +0000

    Add package pymetis-mesh/0.2.1/pymetis_mesh-0.2.1.tar.gz

---
+++ packages/pymetis-mesh/pymetis_mesh-0.2.1.tar.gz/pymetis_mesh-0.2.1/pymetis_mesh/__init__.py
@@ -0,0 +1,16 @@
+import pymetis_mesh.errors as errors
+from ._version import __version__
+from ._wrapper import *
+
+
+DBG_NULL = 0
+DBG_INFO = 1
+DBG_TIME = 2
+DBG_COARSEN = 4
+DBG_REFINE = 8
+DBG_IPART = 16
+DBG_MOVEINFO = 32
+DBG_SEPINFO = 64
+DBG_CONNINFO = 128
+DBG_CONTIGINFO = 256
+DBG_MEMORY = 2048
