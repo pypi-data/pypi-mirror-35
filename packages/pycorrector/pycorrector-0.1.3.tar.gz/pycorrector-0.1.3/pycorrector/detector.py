bb2749f0b2944a4b49c7acc442aa7267bea0b69e
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-06 07:07:47 +0000

    Add package pycorrector/0.1.3/pycorrector-0.1.3.tar.gz

---
+++ packages/pycorrector/pycorrector-0.1.3.tar.gz/pycorrector-0.1.3/pycorrector/detector.py
@@ -0,0 +1,172 @@
+# -*- coding: utf-8 -*-
+# Author: XuMing <xuming624@qq.com>
+# Brief: error word detector
+import codecs
+import kenlm
+import os
+
+import numpy as np
+
+import pycorrector.config as config
+from pycorrector.utils.io_utils import dump_pkl
+from pycorrector.utils.io_utils import get_logger
+from pycorrector.utils.io_utils import load_pkl
+from pycorrector.utils.text_utils import uniform, tokenize
+
+pwd_path = os.path.abspath(os.path.dirname(__file__))
+default_logger = get_logger(__file__)
+# trigram_word_path = os.path.join(pwd_path, 'data/kenlm/people_words.klm')
+# trigram_word = kenlm.Model(trigram_word_path)
+# print('Loaded trigram_word language model from {}'.format(trigram_word_path))
+
+trigram_char_path = os.path.join(pwd_path, config.language_model_path)
+trigram_char = kenlm.Model(trigram_char_path)
+default_logger.debug('Loaded trigram_word language model from {}'.format(trigram_char_path))
+
+PUNCTUATION_LIST = "。，,、？：；{}[]【】“‘’”《》/！%……（）<>@#$~^￥%&*\"\'=+-"
+
+
+def load_word_freq_dict(path):
+    word_freq = {}
+    with codecs.open(path, 'r', encoding='utf-8') as f:
+        for line in f:
+            info = line.split()
+            word = info[0]
+            freq = int(info[1])
+            word_freq[word] = freq
+    return word_freq
+
+
+# 字频统计
+word_freq_path = os.path.join(pwd_path, config.word_freq_path)
+word_freq_model_path = os.path.join(pwd_path, config.word_freq_model_path)
+if os.path.exists(word_freq_model_path):
+    word_freq = load_pkl(word_freq_model_path)
+else:
+    default_logger.debug('load word freq from text file:', word_freq_path)
+    word_freq = load_word_freq_dict(word_freq_path)
+    dump_pkl(word_freq, word_freq_model_path)
+
+
+def get_ngram_score(chars, mode=trigram_char):
+    """
+    取n元文法得分
+    :param chars: list, 以词或字切分
+    :param mode:
+    :return:
+    """
+    return mode.score(' '.join(chars), bos=False, eos=False)
+
+
+def get_ppl_score(words, mode=trigram_char):
+    """
+    取语言模型困惑度得分，越小句子越通顺
+    :param words: list, 以词或字切分
+    :param mode:
+    :return:
+    """
+    return mode.perplexity(' '.join(words))
+
+
+def get_frequency(word):
+    """
+    取词在样本中的词频
+    :param word:
+    :return:
+    """
+    return word_freq.get(word, 0)
+
+
+def _get_maybe_error_index(scores, ratio=0.6745, threshold=1.4):
+    """
+    取疑似错字的位置，通过平均绝对离差（MAD）
+    :param scores: np.array
+    :param threshold: 阈值越小，得到疑似错别字越多
+    :return:
+    """
+    scores = np.array(scores)
+    if len(scores.shape) == 1:
+        scores = scores[:, None]
+    median = np.median(scores, axis=0)  # get median of all scores
+    margin_median = np.sqrt(np.sum((scores - median) ** 2, axis=-1))  # deviation from the median
+    # 平均绝对离差值
+    med_abs_deviation = np.median(margin_median)
+    if med_abs_deviation == 0:
+        return []
+    y_score = ratio * margin_median / med_abs_deviation
+    # 打平
+    scores = scores.flatten()
+    maybe_error_indices = np.where((y_score > threshold) & (scores < median))
+    # 取全部疑似错误字的index
+    return list(maybe_error_indices[0])
+
+
+def detect(sentence):
+    maybe_error_indices = set()
+    # 文本归一化
+    sentence = uniform(sentence)
+    # 切词
+    tokens = tokenize(sentence)
+    # 未登录词加入疑似错误字典
+    for word, begin_idx, end_idx in tokens:
+        # fixed: pass num alpha
+        if word.isalnum(): continue
+        # punctuation
+        if word in PUNCTUATION_LIST: continue
+        # in dict
+        if word in word_freq.keys(): continue
+        for i in range(begin_idx, end_idx):
+            maybe_error_indices.add(i)
+    # 语言模型检测疑似错字
+    ngram_avg_scores = []
+    try:
+        for n in [2, 3]:
+            scores = []
+            for i in range(len(sentence) - n + 1):
+                word = sentence[i:i + n]
+                score = get_ngram_score(list(word), mode=trigram_char)
+                scores.append(score)
+            if not scores: continue
+            # 移动窗口补全得分
+            for _ in range(n - 1):
+                scores.insert(0, scores[0])
+                scores.append(scores[-1])
+            avg_scores = [sum(scores[i:i + n]) / len(scores[i:i + n]) for i in range(len(sentence))]
+            ngram_avg_scores.append(avg_scores)
+
+        # 取拼接后的ngram平均得分
+        sent_scores = list(np.average(np.array(ngram_avg_scores), axis=0))
+        maybe_error_char_indices = _get_maybe_error_index(sent_scores)
+        # 合并字、词错误
+        maybe_error_indices |= set(maybe_error_char_indices)
+    except IndexError as ie:
+        default_logger.warn("index error, sentence:" + sentence + ie)
+    except Exception as e:
+        default_logger.warn("detect error, sentence:" + sentence + e)
+    return sorted(maybe_error_indices)
+
+
+if __name__ == '__main__':
+    sent = '少先队员因该为老人让坐'
+    # sent = '机七学习是人工智能领遇最能体现智能的一个分知'
+    error_list = detect(sent)
+    print(error_list)
+
+    sent_chars = [sent[i] for i in error_list]
+    print(sent_chars)
+
+    from pycorrector.utils.text_utils import segment, tokenize
+
+    print(get_ngram_score(segment(sent)))
+    print(get_ppl_score(segment(sent)))
+
+    print(get_ngram_score(list(sent), mode=trigram_char))
+    print(get_ppl_score(list(sent), mode=trigram_char))
+
+    sent = '少先队员应该为老人让座'
+    print(detect(sent))
+    print(get_ngram_score(segment(sent)))
+    print(get_ppl_score(segment(sent)))
+
+    print(get_ngram_score(list(sent), mode=trigram_char))
+    print(get_ppl_score(list(sent), mode=trigram_char))
---
+++ packages/pycorrector/pycorrector-0.1.3.tar.gz/pycorrector-0.1.3/pycorrector/seq2seq/infer.py
@@ -0,0 +1,146 @@
+# -*- coding: utf-8 -*-
+# Author: XuMing <xuming624@qq.com>
+# Brief: 
+
+
+import numpy as np
+from keras.layers import Input
+from keras.models import Model, load_model
+
+from pycorrector.seq2seq import cged_config as config
+from pycorrector.seq2seq.corpus_reader import CGEDReader, load_word_dict
+from pycorrector.seq2seq.reader import EOS_TOKEN
+from pycorrector.utils.io_utils import get_logger
+
+logger = get_logger(__name__)
+
+
+def decode_sequence(model, rnn_hidden_dim, input_token_index,
+                    num_decoder_tokens, target_token_index, encoder_input_data,
+                    max_decoder_seq_length):
+    # construct the encoder and decoder
+    encoder_inputs = model.input[0]  # input_1
+    encoder_outputs, state_h_enc, state_c_enc = model.layers[2].output  # lstm_1
+    encoder_states = [state_h_enc, state_c_enc]
+    encoder_model = Model(encoder_inputs, encoder_states)
+
+    decoder_inputs = model.input[1]  # input_2
+    decoder_state_input_h = Input(shape=(rnn_hidden_dim,), name='input_3')
+    decoder_state_input_c = Input(shape=(rnn_hidden_dim,), name='input_4')
+    decoder_states_inputs = [decoder_state_input_h, decoder_state_input_c]
+    decoder_lstm = model.layers[3]
+    decoder_outputs, state_h_dec, state_c_dec = decoder_lstm(
+        decoder_inputs, initial_state=decoder_states_inputs)
+    decoder_states = [state_h_dec, state_c_dec]
+    decoder_dense = model.layers[4]
+    decoder_outputs = decoder_dense(decoder_outputs)
+    decoder_model = Model(
+        [decoder_inputs] + decoder_states_inputs,
+        [decoder_outputs] + decoder_states)
+
+    # Reverse-lookup token index to decode sequences back to
+    # something readable.
+    reverse_input_char_index = dict(
+        (i, char) for char, i in input_token_index.items())
+    reverse_target_char_index = dict(
+        (i, char) for char, i in target_token_index.items())
+
+    # Encode the input as state vectors.
+    states_value = encoder_model.predict(encoder_input_data)
+
+    # Generate empty target sequence of length 1.
+    target_seq = np.zeros((1, 1, num_decoder_tokens))
+    # Populate the first character of target sequence with the start character.
+    # target_seq[0, 0, target_token_index[first_char]] = 1.
+
+    # Sampling loop for a batch of sequences
+    # (to simplify, here we assume a batch of size 1).
+    stop_condition = False
+    decoded_sentence = ''
+
+    while not stop_condition:
+        output_tokens, h, c = decoder_model.predict(
+            [target_seq] + states_value)
+
+        # Sample a token
+        sampled_token_index = np.argmax(output_tokens[0, -1, :])
+        sampled_char = reverse_target_char_index[sampled_token_index]
+        decoded_sentence += sampled_char
+
+        # Exit condition: either hit max length
+        # or find stop character.
+        if (sampled_char == EOS_TOKEN or
+                    len(decoded_sentence) > max_decoder_seq_length):
+            stop_condition = True
+
+        # Update the target sequence (of length 1).
+        target_seq = np.zeros((1, 1, num_decoder_tokens))
+        target_seq[0, 0, sampled_token_index] = 1.
+
+        # Update states
+        states_value = [h, c]
+
+    return decoded_sentence
+
+
+def infer(train_path=None,
+          test_path=None,
+          save_model_path=None,
+          save_input_token_path=None,
+          save_target_token_path=None,
+          rnn_hidden_dim=200):
+    data_reader = CGEDReader(train_path)
+    input_texts, target_texts = data_reader.build_dataset(test_path)
+
+    max_encoder_seq_len = max([len(text) for text in input_texts])
+    max_decoder_seq_len = max([len(text) for text in target_texts])
+
+    print('num of samples:', len(input_texts))
+    print('max sequence length for inputs:', max_encoder_seq_len)
+    print('max sequence length for outputs:', max_decoder_seq_len)
+
+    input_token_index = load_word_dict(save_input_token_path)
+    target_token_index = load_word_dict(save_target_token_path)
+
+    encoder_input_data = np.zeros((len(input_texts), max_encoder_seq_len, len(input_token_index)), dtype='float32')
+
+    # one hot representation
+    for i, input_text in enumerate(input_texts):
+        for t, char in enumerate(input_text):
+            if char in input_token_index:
+                encoder_input_data[i, t, input_token_index[char]] = 1.0
+    logger.info("Data loaded.")
+
+    # model
+    logger.info("Infer seq2seq model...")
+    model = load_model(save_model_path)
+
+    for seq_index in range(10):
+        # Take one sequence (part of the test set)
+        # for trying out decoding.
+        input_seq = encoder_input_data[seq_index: seq_index + 1]
+        decoded_sentence = decode_sequence(model, rnn_hidden_dim, input_token_index,
+                                           len(target_token_index), target_token_index, input_seq,
+                                           max_decoder_seq_len)
+
+        print('Input sentence:', input_texts[seq_index])
+        print('Decoded sentence:', decoded_sentence)
+        print('-')
+
+    logger.info("Infer has finished.")
+
+
+if __name__ == "__main__":
+    infer(train_path=config.train_path,
+          test_path=config.test_path,
+          save_model_path=config.save_model_path,
+          save_input_token_path=config.input_vocab_path,
+          save_target_token_path=config.target_vocab_path,
+          rnn_hidden_dim=config.rnn_hidden_dim)
+
+# Input sentence: ['由', '我', '起', '开', '始', '做', '。']
+# Decoded sentence: 的，。的，的也的也的也的也的也的也的也的也的也的也的也的也的也的也的也的也的的也的也的也的也的也的的也的也的也的也的也的的也的也的也的也的也的的也的也的也的也的也的的也的也的也的也的的也的也的也的也的也的的也的也的也的也的的
+# -
+# Input sentence: ['我', '父', '母', '生', '活', '的', '世', '纪', '最', '贫', '穷', '的', '世', '纪', '，', '我', '生', '活', '的', '世', '纪', '是', '什', '么', '都', '丰', '富', '的', '世', '纪', '。']
+# Decoded sentence: 的，。的，的也的也的也的也的也的也的也的也的也的也的也的也的也的也的也的也的的也的也的也的也的也的的也的也的也的也的也的的也的也的也的也的也的的也的也的也的也的也的的也的也的也的也的的也的也的也的也的也的的也的也的也的也的的
+# 这结果也是醉了，seq2seq 一般对局部起作用，全部输给模型，基本上什么都学不到。。。
