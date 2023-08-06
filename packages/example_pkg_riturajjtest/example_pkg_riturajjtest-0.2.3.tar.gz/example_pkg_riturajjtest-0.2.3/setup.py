564c0f49c66f58583765d18e97fc78f23eedcf93
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-06 15:32:46 +0000

    Add package example_pkg_riturajjtest/0.2.3/example_pkg_riturajjtest-0.2.3.tar.gz

---
+++ packages/example_pkg_riturajjtest/example_pkg_riturajjtest-0.2.3.tar.gz/example_pkg_riturajjtest-0.2.3/setup.py
@@ -0,0 +1,15 @@
+import setuptools
+
+with open("README.md", "r") as fh:
+    long_description = fh.read()
+
+setuptools.setup(
+    name="example_pkg_riturajjtest",
+    version="0.2.3",
+    author="Rituraj Jagtap",
+    author_email="riturajj@google.com",
+    description="A small example package",
+    long_description=long_description,
+    url="https://github.com/riturajj/example_pkg_riturajjtest",
+    packages=setuptools.find_packages()
+)
