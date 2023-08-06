97deb6b2d820f84d809c95056ec1a44d4898e29b
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-06 14:57:32 +0000

    Add package ciperror/1.0.5/ciperror-1.0.5-py3-none-any.whl

---
+++ packages/ciperror/ciperror-1.0.5-py3-none-any.whl/ciperror/__init__.py
@@ -0,0 +1,10 @@
+from .base_ciperror import *
+from .database_errors import *
+from .general_api_errors import *
+from .general_expected_errors import *
+from .general_unexpected_errors import *
+from .general_view_errors import *
+from .input_output_errors import *
+from .general_file_system_errors import *
+from .encoding_factory_errors import *
+
