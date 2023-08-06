2fc02cca02a8d2a9c2ff757e1953a3d25f2fb168
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-06 17:14:55 +0000

    Add package animals-math/0.0.2/animals_math-0.0.2.tar.gz

---
+++ packages/animals-math/animals_math-0.0.2.tar.gz/animals_math-0.0.2/setup.py
@@ -0,0 +1,11 @@
+import setuptools
+
+setuptools.setup(
+    name="animals_math",
+    version="0.0.2",
+    author="thunderB",
+    author_email="thunderB@protonmail.com",
+    licence="MIT",
+    description="A package for animals and their math",
+    url="https://github.com/thunder-B"
+)
