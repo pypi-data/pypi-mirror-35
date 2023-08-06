21a3631d091d3932fedf16ed036b277818b11fe3
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-06 13:53:49 +0000

    Add package pyravendb-embedded/4.1.1.3/pyravendb-embedded-4.1.1.3.tar.gz

---
+++ packages/pyravendb-embedded/pyravendb-embedded-4.1.1.3.tar.gz/pyravendb-embedded-4.1.1.3/setup.py
@@ -0,0 +1,20 @@
+from setuptools import setup, find_packages
+
+setup(
+    name='pyravendb-embedded',
+    packages=find_packages(),
+    long_description=open('README.rst').read(),
+    version='4.1.1.3',
+    description='RavenDB Embedded library to run ravendb in embedded way',
+    author='RavenDB',
+    author_email='support@ravendb.net',
+    url='https://github.com/ravendb/ravendb-python-embedded',
+    license='Custom EULA',
+    keywords='pyravendb embedded database nosql doc db',
+    install_requires=
+    [
+        'pyravendb >= 4.0.4.9',
+    ],
+    include_package_data=True,
+    zip_safe=False
+)
