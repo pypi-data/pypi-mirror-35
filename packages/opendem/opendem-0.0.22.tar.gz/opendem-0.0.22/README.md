9318de62ec14797bd7e1844d556af7413c831dbf
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-06 01:54:39 +0000

    Add package opendem/0.0.22/opendem-0.0.22.tar.gz

---
+++ packages/opendem/opendem-0.0.22.tar.gz/opendem-0.0.22/README.md
@@ -0,0 +1,36 @@
+# dem
+See test_dem.py for examples of how this is used.
+
+Collect elevation data from AWS or Thredds, merge, reproject, resample.
+
+No API key needed!
+
+`pip install opendem`
+
+`$ python`
+
+`>>> import dem`
+
+Return elevation, aspect, or slope, that has been correctly transformed to fit rasters you are already working on.
+
+![Montana Slope](https://github.com/dgketchum/dem/blob/master/Montana_Slope.png)
+
+Pulls a web map tile service (AWS) list of tiles, based on input zoom level and bounding box, merges it,
+reprojects it, and resamples it to a raster you are working with.
+
+This is intended for work where one needs a dem (elevation), slope, or aspect in a grid that matches another
+dataset they are working on.  
+
+For example, I work on Landsat images that come in WGS UTM coordinate reference systems.  With [bounds](https://github.com/dgketchum/bounds),
+[satellite_image](https://github.com/dgketchum/satellite_image), and [Landsat578](https://github.com/dgketchum/Landsat578), I can easily download and unzip a Landsat scene, read it into a
+Landsat5 (or 7, 8) object, pass the object attributes to dem, and get back a numpy array that is reprojected
+into the same local coordinate system for accurate scientific analysis with the cloud mask, reflectance, albedo and other
+handy data returned by methods in satellite_image.
+
+This is intended to replace the task of downloading NED files and going through a long process in a GIS to 
+merge, reproject, resample, etc, which often requires time consuming file management and use of proprietary software.
+
+This pulls elevation data from the web, much like an app on your phone does.  Don't spend all your time dealing with
+NED tiles!
+
+Big ups to [rasterio](https://github.com/mapbox/rasterio).
---
+++ packages/opendem/opendem-0.0.22.tar.gz/opendem-0.0.22/setup.py
@@ -0,0 +1,63 @@
+# ===============================================================================
+# Copyright 2017 dgketchum
+#
+# Licensed under the Apache License, Version 2.0 (the "License");
+# you may not use this file except in compliance with the License.
+# You may obtain a copy of the License at
+#
+# http://www.apache.org/licenses/LICENSE-2.0
+#
+# Unless required by applicable law or agreed to in writing, software
+# distributed under the License is distributed on an "AS IS" BASIS,
+# WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
+# See the License for the specific language governing permissions and
+# limitations under the License.
+# ===============================================================================
+
+import os
+
+from setuptools import setup
+
+os.environ['TRAVIS_CI'] = 'True'
+
+try:
+    from setuptools import setup
+
+    setup_kwargs = {}
+except ImportError:
+    from distutils.core import setup
+
+    setup_kwargs = {}
+
+with open('README.md') as f:
+    readme = f.read()
+
+tag = '0.0.22'
+
+setup(name='opendem',
+      version=tag,
+      description='Very simple API to download, merge, resample, project DEM tiles from AWS and Thredds',
+      long_description=readme,
+      setup_requires=['nose>=1.0'],
+      py_modules=['dem'],
+      license='Apache',
+      classifiers=[
+          'Development Status :: 3 - Alpha',
+          'Intended Audience :: Science/Research',
+          'Topic :: Scientific/Engineering :: GIS',
+          'License :: OSI Approved :: Apache Software License',
+          'Programming Language :: Python :: 2.7',
+          'Programming Language :: Python :: 3.6'],
+      keywords='DEM digital elevation model',
+      author='David Ketchum',
+      author_email='dgketchum@gmail.com',
+      platforms='Posix; MacOS X; Windows',
+      packages=[],
+      download_url='https://github.com/{}/{}/archive/{}.tar.gz'.format('dgketchum', 'dem', tag),
+      url='https://github.com/dgketchum',
+      test_suite='tests.test_dem',
+      install_requires=['numpy', 'requests', 'scipy', 'future', 'rasterio', 'xarray', 'pyproj'],
+      **setup_kwargs)
+
+
+# ============= EOF ==============================================================
