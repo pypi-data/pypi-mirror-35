b851b41e7be6ca2f398d25e79a4becb9f095f73a
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-06 16:18:52 +0000

    Add package automlib/0.16/automlib-0.16.tar.gz

---
+++ packages/automlib/automlib-0.16.tar.gz/automlib-0.16/setup.py
@@ -0,0 +1,13 @@
+from distutils.core import setup
+setup(
+  name='automlib',
+  packages=['automlib'],
+  version='0.16',
+  description='Configurable Machine Learning Library',
+  author='Tony Wu',
+  author_email='wuyuehao@gmail.com',
+  url='https://github.com/wuyuehao/ghost', 
+  download_url= 'https://github.com/wuyuehao/ghost/tarball/0.16',
+  keywords = ['AI', 'ML'],
+  classifiers = [],
+) 
