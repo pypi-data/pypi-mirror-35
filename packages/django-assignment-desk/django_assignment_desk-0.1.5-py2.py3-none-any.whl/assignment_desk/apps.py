427a0482760b7d5dbb2a49e88759c6c472b74600
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-06 14:55:15 +0000

    Add package django-assignment-desk/0.1.5/django_assignment_desk-0.1.5-py2.py3-none-any.whl

---
+++ packages/django-assignment-desk/django_assignment_desk-0.1.5-py2.py3-none-any.whl/assignment_desk/apps.py
@@ -0,0 +1,12 @@
+# -*- coding: utf-8 -*-
+from __future__ import unicode_literals
+
+from django.apps import AppConfig
+
+
+class AssignmentDeskConfig(AppConfig):
+    name = 'assignment_desk'
+
+    def ready(self):
+        import assignment_desk.checks
+        import assignment_desk.signals
