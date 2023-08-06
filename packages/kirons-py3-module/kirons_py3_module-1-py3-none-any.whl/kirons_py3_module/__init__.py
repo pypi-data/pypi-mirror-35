6fff8dc384fe92645780460729ffe0e58f540086
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-06 15:34:57 +0000

    Add package kirons-py3-module/1/kirons_py3_module-1-py3-none-any.whl

---
+++ packages/kirons-py3-module/kirons_py3_module-1-py3-none-any.whl/kirons_py3_module/__init__.py
@@ -0,0 +1,8 @@
+def i_am_new():
+    print("""Since you're new to Kiron's python module you should check the docs on:
+[url]
+
+If you try something but it says that the function is not defined or whatever you should
+update kirons_python_module""")
+
+__all__ = ["youtuber_streaming_asyncio"]
