8194d87cfcfa060092471b486799a14873ce7278
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-06 14:29:26 +0000

    Add package qaviton/0.0.3/qaviton-0.0.3-py3-none-any.whl

---
+++ packages/qaviton/qaviton-0.0.3-py3-none-any.whl/qaviton/scripts/examples/simple_web/pages/google_home.py
@@ -0,0 +1,18 @@
+from tests.pages.components.page import Page
+from tests.pages.locators import locator
+from tests.pages.components.google_search_bar import GoogleSearchBar
+from selenium.webdriver.common.keys import Keys
+
+
+class GoogleHomePage(Page):
+    def __init__(self, driver):
+        Page.__init__(self, driver)
+        self.search_bar = GoogleSearchBar(driver)
+
+    def google_search_button(self):
+        return self.find(locator.google_search_button)
+
+    def navigate_to_GoogleSearchPage(self, weight=5, *args, **kwargs):
+        self.search_bar.button().send('search').send_keys(Keys.ENTER)
+        self.wait_until_page_loads()
+
