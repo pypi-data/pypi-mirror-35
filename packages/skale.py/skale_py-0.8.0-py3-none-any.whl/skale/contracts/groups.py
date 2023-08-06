58ee40ad2ec5fbd6c193df2a208f1e7f087e7a66
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-06 07:08:08 +0000

    Add package skale.py/0.8.0/skale_py-0.8.0-py3-none-any.whl

---
+++ packages/skale.py/skale_py-0.8.0-py3-none-any.whl/skale/contracts/groups.py
@@ -0,0 +1,22 @@
+from skale.contracts import BaseContract
+from skale.utils.helper import format
+
+class Groups(BaseContract):
+    fields = [
+        'owner',
+        'name',
+        'storageBytes',
+        'cpu',
+        'transactionThroughput',
+        'lifetime',
+        'creationDate',
+        'maxNodes',
+        'deposit'
+    ]
+
+    def __get_raw(self, name):
+        return self.contract.functions.getSchain(name).call()
+
+    @format(fields)
+    def get(self, name):
+        return self.__get_raw(name)
