7bf485c01b608370175e1747c4550c17b6b982e9
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-06 04:08:44 +0000

    Add package pyjvmgui/1.2.6/pyjvmgui-1.2.6.tar.gz

---
+++ packages/pyjvmgui/pyjvmgui-1.2.6.tar.gz/pyjvmgui-1.2.6/setup.py
@@ -0,0 +1,58 @@
+import os
+from os import path
+from sys import path as sys_path
+
+from setuptools import find_packages, setup
+from setuptools.command.develop import develop
+from setuptools.command.install import install
+
+this_directory = path.abspath(path.dirname(__file__))
+srcdir = path.join(this_directory, "src/")
+sys_path.insert(0, srcdir)
+
+import rt.get_rt as get_rt
+
+
+def post_install():
+    rt_path = os.path.join(os.path.expanduser("~"), '.pyjvmgui')
+    os.makedirs(rt_path)
+    get_rt.download(rt_path)
+
+
+class DevelopWithRt(develop):
+    def run(self):
+        develop.run(self)
+        post_install()
+
+
+class InstallWithRt(install):
+    def run(self):
+        install.run(self)
+        post_install()
+
+
+setup(
+    name='pyjvmgui',
+    version='1.2.6',
+    url='https://github.com/MatevzFa/pyjvm',
+    license='GPL-3.0',
+    author='matevz',
+    author_email='mf6422@student.uni-lj.si',
+    description='A GUI for the PyJVM',
+
+    packages=find_packages('src'),
+    py_modules=['pyjvmgui'],
+
+    package_dir={'': 'src'},
+
+    entry_points={
+        'console_scripts': ['pyjvmgui=pyjvmgui:main']
+    },
+
+    install_requires=['PySide2'],
+
+    cmdclass={
+        'install': InstallWithRt,
+        'develop': DevelopWithRt,
+    },
+)
