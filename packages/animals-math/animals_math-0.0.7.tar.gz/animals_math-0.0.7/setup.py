f14505bf9221ea56b6bc35f05e22df617519ad5a
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-06 17:16:52 +0000

    Add package animals-math/0.0.7/animals_math-0.0.7.tar.gz

---
+++ packages/animals-math/animals_math-0.0.7.tar.gz/animals_math-0.0.7/setup.py
@@ -0,0 +1,19 @@
+import setuptools
+
+import os
+directory_name = os.path.abspath(os.path.dirname(__file__))
+description = open(directory_name + "/README.md", 'r')
+long_description = description.read()
+
+setuptools.setup(
+    name="animals_math",
+    version="0.0.7",
+    author="thunderB",
+    author_email="thunderB@protonmail.com",
+    licence="MIT",
+    description="A package for animals and their math",
+    long_description=long_description,
+    long_description_content_type="text/markdown",
+    url="https://github.com/thunder-B",
+    packages=["animals", "animals.anims"]
+)
