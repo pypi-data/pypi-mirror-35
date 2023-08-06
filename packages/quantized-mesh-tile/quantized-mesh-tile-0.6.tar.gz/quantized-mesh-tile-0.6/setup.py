6e57a17a72bd0dbe9918a05c32719bc5e36baafe
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-06 01:53:27 +0000

    Add package quantized-mesh-tile/0.6/quantized-mesh-tile-0.6.tar.gz

---
+++ packages/quantized-mesh-tile/quantized-mesh-tile-0.6.tar.gz/quantized-mesh-tile-0.6/setup.py
@@ -0,0 +1,25 @@
+# -*- coding: utf-8 -*-
+
+import os
+# HACK for `nose.collector` to work on python 2.7.3 and earlier
+import multiprocessing
+from setuptools import setup, find_packages
+
+# HACK READTHEDOCS (find a better solution)
+if '/home/docs/checkouts/readthedocs' in os.getcwd():
+    requires = []
+else:
+    requires = ['shapely', 'numpy']
+
+setup(name='quantized-mesh-tile',
+      version='0.6',
+      description='Quantized-Mesh format reader and writer',
+      author=u'Loic Gasser',
+      author_email='loicgasser4@gmail.com',
+      license='MIT',
+      url='https://github.com/loicgasser/quantized-mesh-tile',
+      packages=find_packages(exclude=['tests', 'doc']),
+      zip_safe=False,
+      test_suite='nose.collector',
+      install_requires=requires,
+      )
