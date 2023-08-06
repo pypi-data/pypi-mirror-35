d4fe5e963aae90e2a603010f2f8aedb80280b7cc
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-06 01:52:54 +0000

    Add package dreamgraph/1.1.1/dreamgraph-1.1.1.tar.gz

---
+++ packages/dreamgraph/dreamgraph-1.1.1.tar.gz/dreamgraph-1.1.1/setup.py
@@ -0,0 +1,28 @@
+from setuptools import setup
+
+description = """
+**The Python module for Telegraph API.**
+
+DreamGraph is built to make it easy to use Telegraph API which is presented by pupular Messaging platform Telegram.
+The module is licensed under GNU GPL license and it is open-source.
+
+Docs: http://dreamgraph.ml
+
+Module works in both: Python 2 and Python 3.
+
+"""
+
+setup(
+  name = 'dreamgraph',
+  packages = ['dreamgraph'],
+  version = '1.1.1',
+  long_description=description,
+  description = 'The Python module for Telegraph API',
+  author = 'Jasur Nurboev',
+  author_email = 'bluestacks6523@gmail.com',
+  url = 'https://github.com/JasurbekNURBOYEV/DreamGraph',
+  download_url = 'https://github.com/JasurbekNURBOYEV/DreamGraph/archive/1.1.1.tar.gz',
+  keywords = ['telegraph', 'telegraph-api', 'python-module', 'dreamgraph'],
+  classifiers = [],
+  install_requires=['requests'],
+)
