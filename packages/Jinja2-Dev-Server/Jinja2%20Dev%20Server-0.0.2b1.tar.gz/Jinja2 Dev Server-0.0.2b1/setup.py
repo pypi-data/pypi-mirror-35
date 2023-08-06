0ad25985364d35f5f422e04fbc1f3f68f507e093
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-06 13:23:05 +0000

    Add package Jinja2-Dev-Server/0.0.2b1/Jinja2%20Dev%20Server-0.0.2b1.tar.gz

---
+++ packages/Jinja2-Dev-Server/Jinja2%20Dev%20Server-0.0.2b1.tar.gz/Jinja2 Dev Server-0.0.2b1/setup.py
@@ -0,0 +1,13 @@
+import setuptools
+from distutils.core import setup
+
+setup(
+    name='Jinja2 Dev Server',
+    version="0.0.2b1",
+    author="Eamonn Nugent",
+    author_email="eamonn.nugent@demilletech.net",
+    packages=['jinja2-dev-server', ],
+    license='MIT',
+    long_description=open('README.md').read(),
+    long_description_content_type="text/markdown",
+)
