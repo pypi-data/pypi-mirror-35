b80e5852d7c1d48be8ffd6cf52d9d8f6630ffed2
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-06 14:21:30 +0000

    Add package qlite/0.0.2/qlite-0.0.2.tar.gz

---
+++ packages/qlite/qlite-0.0.2.tar.gz/qlite-0.0.2/setup.py
@@ -0,0 +1,10 @@
+from setuptools import setup, find_packages
+
+setup(
+    name='qlite',
+    version='0.0.2',
+    packages=find_packages(),
+    author='BorisPlus',
+    author_email='boris-plus@mail.ru',
+    url='https://github.com/BorisPlus/otus_webpython_005',
+)
