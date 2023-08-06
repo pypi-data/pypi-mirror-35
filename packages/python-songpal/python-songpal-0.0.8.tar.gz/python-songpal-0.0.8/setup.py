af1a77f3ed0dc4aa08c84bd38a604a9e5ba6f8b4
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-06 11:52:11 +0000

    Add package python-songpal/0.0.8/python-songpal-0.0.8.tar.gz

---
+++ packages/python-songpal/python-songpal-0.0.8.tar.gz/python-songpal-0.0.8/setup.py
@@ -0,0 +1,38 @@
+from setuptools import setup
+
+def readme():
+    with open("README.rst") as f:
+        return f.read()
+
+setup(
+    name='python-songpal',
+
+    version='0.0.8',
+    description="Python library for interfacing with Sony's Songpal devices",
+    long_description=readme(),
+    url='https://github.com/rytilahti/python-songpal',
+
+    author='Teemu Rytilahti',
+    author_email='tpr@iki.fi',
+
+    license='GPLv3',
+
+    classifiers=[
+        'Development Status :: 3 - Alpha',
+        'Intended Audience :: Developers',
+        'License :: OSI Approved :: GNU General Public License v3 (GPLv3)',
+        'Programming Language :: Python :: 3',
+    ],
+
+    keywords='sony songpal soundbar',
+
+    packages=['songpal'],
+
+    install_requires=['click', 'aiohttp', 'upnpclient', 'attrs'],
+    python_requires='>=3.5',
+    entry_points={
+        'console_scripts': [
+            'songpal=songpal.main:cli',
+        ],
+    },
+)
