b5d68f4d163709218ce87aa33590e72d7e6b4a8d
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-06 01:56:46 +0000

    Add package cauldron-notebook/0.3.11/cauldron_notebook-0.3.11-py2.py3-none-any.whl

---
+++ packages/cauldron-notebook/cauldron_notebook-0.3.11-py2.py3-none-any.whl/cauldron/cli/server/routes/display.py
@@ -0,0 +1,20 @@
+import mimetypes
+import os
+
+import cauldron
+import flask
+from cauldron.cli.server import run as server_run
+
+
+@server_run.APPLICATION.route('/view/<path:route>', methods=['GET', 'POST'])
+def view(route: str):
+    project = cauldron.project.get_internal_project()
+    results_path = project.results_path if project else None
+    if not project or not results_path:
+        return '', 204
+
+    path = os.path.join(results_path, route)
+    if not os.path.exists(path):
+        return '', 204
+
+    return flask.send_file(path, mimetype=mimetypes.guess_type(path)[0])
