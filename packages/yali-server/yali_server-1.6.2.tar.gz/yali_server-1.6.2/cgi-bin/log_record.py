98e8cf940cd7141b65dc1c0a09ce00708a02a64e
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-06 13:54:01 +0000

    Add package yali-server/1.6.2/yali_server-1.6.2.tar.gz

---
+++ packages/yali-server/yali_server-1.6.2.tar.gz/yali_server-1.6.2/cgi-bin/log_record.py
@@ -0,0 +1,37 @@
+# -*- coding: UTF-8 -*-
+#!/usr/bin/env python
+#!/usr/bin/env python3
+
+# CGI处理模块
+import cgi
+import time
+import os
+
+def index():
+    # 创建 FieldStorage 的实例化
+    form = cgi.FieldStorage()
+
+    # 获取日志数据
+    if form.getvalue('log'):
+        log_text = form.getvalue('log')
+    else:
+        log_text = "---------terminal error----------"
+
+    file_name= os.getcwd()+"/cgi-bin/"+time.strftime("%Y-%m-%d",time.localtime(time.time()))+".txt"
+
+    try:
+        with open(file_name,"a+") as f:
+            f.write(log_text+"\n")
+    except:
+        print("\033[32m open fail.... \033[0m")
+
+index()
+
+
+
+
+
+
+
+
+
