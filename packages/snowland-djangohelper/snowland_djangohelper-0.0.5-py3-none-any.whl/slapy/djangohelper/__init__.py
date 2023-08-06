998b89ec037a4bb7356c786adc37584f0f7bb551
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-06 11:49:51 +0000

    Add package snowland-djangohelper/0.0.5/snowland_djangohelper-0.0.5-py3-none-any.whl

---
+++ packages/snowland-djangohelper/snowland_djangohelper-0.0.5-py3-none-any.whl/slapy/djangohelper/__init__.py
@@ -0,0 +1,11 @@
+#!/usr/bin/env python
+# -*- coding: utf-8 -*-
+# @Author  : 河北雪域网络科技有限公司 A.Star
+# @contact: astar@snowland.ltd
+# @site: 
+# @file: __init__.py.py
+# @time: 2018/7/23 14:46
+# @Software: PyCharm
+
+from .util import create_admin, createView, trans
+from .project2lines import project_to_lines
