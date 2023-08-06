bfe7f7a9858bcd222551ac8621879facc3e3bebd
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-06 13:20:36 +0000

    Add package CloudShip/0.12.1/CloudShip-0.12.1.tar.gz

---
+++ packages/CloudShip/CloudShip-0.12.1.tar.gz/CloudShip-0.12.1/setup.py
@@ -0,0 +1,14 @@
+from setuptools import setup
+
+setup(
+    name="CloudShip",
+    version='0.12.1',
+    py_modules=['shipment'],
+    install_requires=[
+        'Click',
+    ],
+    entry_points='''
+        [console_scripts]
+        CloudShip=cli:cli
+    ''',
+)
