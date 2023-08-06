884ce30e57e5de70de365d5e8b462cd31446ad14
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-06 07:12:01 +0000

    Add package lahja/0.5.0/lahja-0.5.0-py3-none-any.whl

---
+++ packages/lahja/lahja-0.5.0-py3-none-any.whl/lahja/endpoint.py
@@ -0,0 +1,112 @@
+import asyncio
+import multiprocessing
+from typing import (  # noqa: F401
+    Any,
+    AsyncIterable,
+    Callable,
+    Dict,
+    List,
+    Optional,
+    Type,
+    cast,
+)
+import uuid
+
+from .async_util import (
+    async_get,
+)
+from .misc import (
+    BaseEvent,
+    BroadcastConfig,
+    Subscription,
+)
+
+
+class Endpoint:
+
+    def __init__(self,
+                 name: str,
+                 sending_queue: multiprocessing.Queue,
+                 receiving_queue: multiprocessing.Queue) -> None:
+
+        self.name = name
+        self._sending_queue = sending_queue
+        self._receiving_queue = receiving_queue
+        self._futures: Dict[str, asyncio.Future] = {}
+        self._handler: Dict[Type[BaseEvent], List[Callable[[BaseEvent], Any]]] = {}
+        self._queues: Dict[Type[BaseEvent], List[asyncio.Queue]] = {}
+
+    def broadcast(self, item: BaseEvent, config: Optional[BroadcastConfig] = None) -> None:
+        item._origin = self.name
+        self._sending_queue.put_nowait((item, config))
+
+    async def request(self, item: BaseEvent) -> BaseEvent:
+        item._origin = self.name
+        item._id = str(uuid.uuid4())
+
+        future: asyncio.Future = asyncio.Future()
+        self._futures[item._id] = future
+
+        self._sending_queue.put_nowait((item, None))
+
+        result = await future
+
+        return cast(BaseEvent, result)
+
+    def connect(self, loop: Optional[asyncio.AbstractEventLoop] = None) -> None:
+        if loop is not None:
+            asyncio.ensure_future(self._connect(), loop=loop)
+        else:
+            asyncio.ensure_future(self._connect())
+
+    async def _connect(self) -> None:
+        while True:
+            (item, config) = await async_get(self._receiving_queue)
+            has_config = config is not None
+
+            event_type = type(item)
+            in_futures = has_config and config.filter_event_id in self._futures
+            in_queue = event_type in self._queues
+            in_handler = event_type in self._handler
+
+            if not in_queue and not in_handler and not in_futures:
+                continue
+
+            if in_futures:
+                future = self._futures[config.filter_event_id]
+                future.set_result(item)
+                self._futures.pop(config.filter_event_id)
+
+            if in_queue:
+                for queue in self._queues[event_type]:
+                    queue.put_nowait(item)
+
+            if in_handler:
+                for handler in self._handler[event_type]:
+                    handler(item)
+
+    def subscribe(self,
+                  event_type: Type[BaseEvent],
+                  handler: Callable[[BaseEvent], None]) -> Subscription:
+
+        if event_type not in self._handler:
+            self._handler[event_type] = []
+
+        self._handler[event_type].append(handler)
+
+        return Subscription(lambda: self._handler[event_type].remove(handler))
+
+    async def stream(self, event_type: Type[BaseEvent]) -> AsyncIterable[BaseEvent]:
+        queue: asyncio.Queue = asyncio.Queue()
+
+        if event_type not in self._queues:
+            self._queues[event_type] = []
+
+        self._queues[event_type].append(queue)
+
+        while True:
+            event = await queue.get()
+            try:
+                yield event
+            except GeneratorExit:
+                self._queues[event_type].remove(queue)
---
+++ packages/lahja/lahja-0.5.0-py3-none-any.whl/lahja/misc.py
@@ -0,0 +1,41 @@
+from typing import (  # noqa: F401
+    Any,
+    Callable,
+    Optional,
+)
+
+
+class Subscription:
+
+    def __init__(self, unsubscribe_fn: Callable[[], Any]) -> None:
+        self._unsubscribe_fn = unsubscribe_fn
+
+    def unsubscribe(self) -> None:
+        self._unsubscribe_fn()
+
+
+class BroadcastConfig:
+
+    def __init__(self,
+                 filter_endpoint: Optional[str] = None,
+                 filter_event_id: Optional[str] = None) -> None:
+
+        self.filter_endpoint = filter_endpoint
+        self.filter_event_id = filter_event_id
+
+    def allowed_to_receive(self, endpoint: str) -> bool:
+        return self.filter_endpoint is None or self.filter_endpoint == endpoint
+
+
+class BaseEvent:
+
+    def __init__(self) -> None:
+        self._origin = ''
+        self._id: Optional[str] = None
+        self._config: Optional[BroadcastConfig] = None
+
+    def broadcast_config(self) -> BroadcastConfig:
+        return BroadcastConfig(
+            filter_endpoint=self._origin,
+            filter_event_id=self._id
+        )
