ff5501e728313f99ede9680c29c6c5fc14faf92f
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-06 07:10:42 +0000

    Add package mmpy-bot/1.3.0/mmpy_bot-1.3.0-py2.py3-none-any.whl

---
+++ packages/mmpy-bot/mmpy_bot-1.3.0-py2.py3-none-any.whl/mmpy_bot/cli.py
@@ -0,0 +1,20 @@
+# -*- coding: utf-8 -*-
+import sys
+import logging
+
+from mmpy_bot import bot, settings
+
+
+def main():
+    logging.basicConfig(**{
+        'format': '[%(asctime)s] %(message)s',
+        'datefmt': '%m/%d/%Y %H:%M:%S',
+        'level': logging.DEBUG if settings.DEBUG else logging.INFO,
+        'stream': sys.stdout,
+    })
+
+    try:
+        b = bot.Bot()
+        b.run()
+    except KeyboardInterrupt:
+        pass
