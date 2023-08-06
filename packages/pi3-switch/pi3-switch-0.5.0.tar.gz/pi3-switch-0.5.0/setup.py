c052249358e2ad6aa8b411bd0dc05ed4ca74b030
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-06 17:14:42 +0000

    Add package pi3-switch/0.5.0/pi3-switch-0.5.0.tar.gz

---
+++ packages/pi3-switch/pi3-switch-0.5.0.tar.gz/pi3-switch-0.5.0/setup.py
@@ -0,0 +1,26 @@
+from setuptools import setup
+
+with open("README.rst", "r") as fh:
+    long_description = fh.read()
+
+setup(
+    name="pi3-switch",
+    description="More advanced workspace switcher for i3.",
+    long_description=long_description,
+    version="0.5.0",
+    license="MIT",
+    author="Michał Wieluński",
+    author_email="michal@landmaj.pl",
+    url="https://gitlab.com/landmaj/pi3-switch",
+    install_requires=["i3ipc==1.5.1", "pynput==1.4"],
+    packages=["pi3"],
+    zip_safe=True,
+    entry_points={"console_scripts": ["pi3-switch = pi3.switch:main"]},
+    classifiers=[
+        "Development Status :: 4 - Beta",
+        "License :: OSI Approved :: MIT License",
+        "Operating System :: POSIX :: Linux",
+        "Programming Language :: Python :: 3",
+        "Topic :: Desktop Environment :: Window Managers",
+    ],
+)
