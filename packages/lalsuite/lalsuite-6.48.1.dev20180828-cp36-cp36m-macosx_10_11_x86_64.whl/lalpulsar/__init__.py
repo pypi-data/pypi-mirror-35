a954d7cec8d28c8196e516c8bd6dc556a3570ebf
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-06 04:12:02 +0000

    Add package lalsuite/6.48.1.dev20180828/lalsuite-6.48.1.dev20180828-cp36-cp36m-macosx_10_11_x86_64.whl

---
+++ packages/lalsuite/lalsuite-6.48.1.dev20180828-cp36-cp36m-macosx_10_11_x86_64.whl/lalpulsar/__init__.py
@@ -0,0 +1,7 @@
+# Import SWIG wrappings, if available
+from .lalpulsar import *
+
+__version__ = "1.16.0.1"
+
+## \addtogroup lalpulsar_python
+"""This package provides Python wrappings and extensions to LALPulsar"""
